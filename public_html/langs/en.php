60ec644792e991c21472ba6b225af7b58432e314
Primary Server <mashpia@host1.mashpia.com>
2018-02-12 14:28:08 -0500

    not sure if this does anything
    Former-commit-id: d9e2f67296f658695d3f8351007f517f19f0f530

---
+++ public_html/langs/en.php
@@ -0,0 +1,9 @@
+<?
+$dir = "LTR";
+$prev_Arr = "&lArr;";
+$next_Arr = "&rArr;";
+$prev_arr = "&larr;";
+$next_arr = "&rarr;";
+$align_start = "left";
+$align_end = "right";
+?>
---
+++ public_html/langs/he.php
@@ -0,0 +1,19 @@
+<?
+$dir = "RTL";
+$prev_Arr = "&rArr;";
+$next_Arr = "&lArr;";
+$prev_arr = "&rarr;";
+$next_arr = "&larr;";
+$align_start = "right";
+$align_end = "left";
+
+$x2196 = '&#x2197;';
+$x2197 = '&#x2196;';
+$x2198 = '&#x2199;';
+$x2199 = '&#x2198;';
+
+$x21d6 = '&#x21d7;';
+$x21d7 = '&#x21d6;';
+$x21d8 = '&#x21d9;';
+$x21d9 = '&#x21d8;';
+?>
