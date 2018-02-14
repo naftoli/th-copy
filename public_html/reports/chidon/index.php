f57e75e84a7f8d7d2e282cff2aff64f2a6f5d668
Primary Server <mashpia@host1.mashpia.com>
2018-02-14 14:08:52 -0500

    fix report path
    Former-commit-id: bf13cd5840deffc115ee2cdf40037bd14c70d2e6

--- public_html/reports/chidon/index.php
+++ public_html/reports/chidon/index.php
@@ -70,7 +70,7 @@ require_once($_SERVER["DOCUMENT_ROOT"].'/header.php');
                     <span class="link-text">Grades / Learning Registered Report</span>
                 </div>
             </a>
-            <a href="shabbaton_enrollment_report.php<?=$debug ? "?debug=true": "";?>">
+            <a href="shabbaton_enrollment.php<?=$debug ? "?debug=true": "";?>">
                 <div class="button">
                     <img src="/images/icon_report.png" height="32" alt="tickets"/>
                     <span class="link-text">Shabbaton Enrollment Report</span>
