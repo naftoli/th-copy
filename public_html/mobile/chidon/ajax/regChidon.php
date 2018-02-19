032a79046231a5c5d416c31417a0c627b94e6f65
Primary Server <mashpia@host1.mashpia.com>
2018-02-19 10:20:28 -0500

    prevent duplicates
    Former-commit-id: a1dcc2fd1cb446fb6f3f49cffcc4c89578b39323

--- public_html/mobile/chidon/ajax/regChidon.php
+++ public_html/mobile/chidon/ajax/regChidon.php
@@ -10,13 +10,27 @@ $size = mysql_real_escape_string($_POST['size']);
 require '../../reg/ajax/encrypt.php';
 $admin_id = encrypt_decrypt('decrypt', $admin_id);
 
-$sql = "insert into th_chidon 
-        set user_id = " . intval($user) . ",
-        school_id = " . intval($school) . ",
-        year = " . intval($year) . ",
-        size = '" . $size . "',
-        parent_id = " . $admin_id . ", 
-        reg_date = now()";
+// check if there's already a child registered (but possibly identified as deleted)
+$sql = "select * from th_chidon where user_id = " . intval($user) . " and year = " . intval($year);
+$result = mysql_query( $sql );
+if (mysql_num_rows( $result ) > 0) {
+    $sql = "update th_chidon 
+            set school_id = " . intval($school) . ",
+            size = '" . $size . "',
+            parent_id = " . $admin_id . ", 
+            reg_date = now(),
+            deleted = 0 
+            where user_id = " . intval($user) . "
+            and year = " . intval($year);
+} else {
+    $sql = "insert into th_chidon 
+            set user_id = " . intval($user) . ",
+            school_id = " . intval($school) . ",
+            year = " . intval($year) . ",
+            size = '" . $size . "',
+            parent_id = " . $admin_id . ", 
+            reg_date = now()";
+}
 
 if (mysql_query($sql)) {
     echo 0;
