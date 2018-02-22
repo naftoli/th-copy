17ea5c50b73482674ea1c16209daee76c84ecd55
Primary Server <mashpia@host1.mashpia.com>
2018-02-22 11:42:47 -0500

    first draft of printing letters report for missing medals
    Former-commit-id: 5ac850706e609a174dbb0b4a7b9ecc992f2f34a4

---
+++ public_html/reports/ranks/missing_medals.php
@@ -0,0 +1,91 @@
+<?php $debug = false;
+// enable debuging
+if ($_GET['debug']) {
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+    $debug = true; // set debug to true
+}
+
+/***************** AUTHENTICATION **********************/
+$admin_auth = array('school'); 
+require_once($_SERVER["DOCUMENT_ROOT"].'/header.php');
+
+// only superusers can use this page. Non superusers get redirected to the page that they can use
+if ($admin_user['auth'] != 'super') {
+    header("Location: /reports/users/");
+}
+
+$subjects_list = [];
+$subjects_list_query = mysql_query(
+     " SELECT subject_id, subject_name FROM subjects "
+    ." WHERE subject_id IN (45, 27, 4, 90, 21, 16, 13, 40, 12, 41, 100, 1)"
+    ." ORDER BY subject_name"
+);
+while($subject = mysql_fetch_assoc($subjects_list_query)){
+    $subjects_list[] = $subject;
+}
+
+$medals = [];
+$medals_query = mysql_query("SELECT medal_ord, medal_name FROM medals ORDER BY medal_ord");
+while($medal = mysql_fetch_assoc($medals_query)){
+    $medals[] = $medal;
+}
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Tzivos Hashem | Rank Reports Home Page</title>
+        <link href="/admin_styles.css" rel="stylesheet" type="text/css">
+        <link href="/raffles/shared/styles/action-links.css" rel="stylesheet" type="text/css">
+        <link href="/styles/admin/grey_select.css" rel="stylesheet" type="text/css"/>
+        <style>
+            .half-width {width: 49.6%;display: inline-block;}
+            .submit-button{text-align: center;    margin-top: 15px;}
+            input[type="submit"]{padding: 6px 10px;}
+            .missing_medal {margin: 10px;}
+        </style>
+    </head>
+    <body>
+        <? // load the admin UI and JQuery 1.4
+            include($_SERVER["DOCUMENT_ROOT"].'/admin_header.php');
+        ?>
+        <h1>Missing Medals Printout Report</h1>
+        
+        <p>Please enter the missing medals.</p>
+        
+        <form action="missing_medals_printout.php" method="POST" target="_blank">
+            <div class="missing-medals">
+                <div class="template missing_medal">
+                    <div class="half-width">
+                        <label>Subject:</label>
+                        <select name="subjects[]">
+                            <? foreach ($subjects_list as $subject) {?>
+                                <option value="<?=$subject['subject_id']?>"><?=$subject['subject_name']?></option>
+                            <? } ?>
+                        </select>
+                    </div>
+                    <div class="half-width">
+                        <label>Medal:</label>
+                        <select name="medals[]">
+                            <? foreach ($medals as $medal) {?>
+                                <option value="<?=$medal['medal_ord']?>"><?=$medal['medal_name']?></option>
+                            <? } ?>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="submit-button">
+                <a class="button" id="add_medal">Add Missing Medal.</a>
+                <input type="submit" value="Print Letters"/>
+            </div>
+        </form>
+        <script>
+            $("#add_medal").click(function(){
+                $(".missing-medals").append("<div class='missing_medal'>" + $(".template").html() + "</div>");
+            });
+        </script>
+    </body>
+</html>
---
+++ public_html/reports/ranks/missing_medals_printout.php
@@ -0,0 +1,107 @@
+<?php $debug = false;
+// enable debuging
+if ($_GET['debug']) {
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+    $debug = true; // set debug to true
+}
+
+/***************** AUTHENTICATION **********************/
+$admin_auth = array('school'); 
+require_once($_SERVER["DOCUMENT_ROOT"].'/header.php');
+
+// only superusers can use this page. Non superusers get redirected to the page that they can use
+if ($admin_user['auth'] != 'super') {
+    header("Location: /reports/users/");
+}
+
+$missing_medals = [];
+
+foreach($_POST['subjects'] as $index => $subject) {
+    $missing_medals[] = ["subject_id" => $subject, "medal_ord" => $_POST['medals'][$index]];
+}
+
+// get this from the post data...
+
+
+$missing_medals_info = [];
+$missing_medals_sql = [];
+// add all the missing medals to the array///
+foreach($missing_medals as $medal) {
+    $missing_medals_sql[] = "(mm.subject_id = '".$medal['subject_id']."' AND mm.medal_ord = '".$medal['medal_ord']."')";
+    
+    $subject_name_query = mysql_query("SELECT subject_name FROM subjects WHERE subject_id = '". $medal['subject_id'] ."'");
+    $medal_name_query   = mysql_query("SELECT medal_name   FROM medals   WHERE medal_ord  = '". $medal['medal_ord']  ."'");
+    
+    $missing_medals_info[] = ["subject" => mysql_result($subject_name_query, 0), "medal" => mysql_result($medal_name_query, 0)];
+}
+
+require_once($_SERVER["DOCUMENT_ROOT"]."/class.report.php");
+$report = new Report(); // set the previous dates to true if the toggle is pointing to "previous"
+$greg_start = $report->getReportDates()['start'];
+$greg_end = $report->getReportDates()['end'];
+
+$user_query = mysql_query(
+    " SELECT u.first, u.last, s.school_name, c.class_grade, c.class_sub "
+    ." FROM medal_marks mm "
+    ." JOIN users u USING (user_id) "
+    ." JOIN schools s USING (school_id) "
+    ." JOIN classes c USING (class_id) "
+    ." WHERE (".implode(" OR ", $missing_medals_sql). ") "
+    ." AND date_awarded >= $greg_start AND date_awarded <= $greg_end "
+    ." GROUP BY user_id "
+    ." ORDER BY s.school_name, c.class_grade, c.class_sub, u.first, u.last "
+);
+    
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Tzivos Hashem | Rank Reports Home Page</title>
+        <style>
+            .page-break {page-break-after: always;}
+            p.large{font-size: 1.3em;line-height: 1.4;}
+            p.school-name{margin-bottom: 50px; text-align: center; font-size: .9em;display: none;}
+            @media print {
+                p.school-name{display: block;}
+            }
+            div.letter{font-size: 1.5em;}
+        </style>
+    </head>
+    <body>
+        <h1>Tzivos Hashem Missing Medals Report</h1>
+        <p class="large">
+            <strong>Report Dates: </strong>
+            From <?=$report->getHeReportDates()['start_he']?> To <?=$report->getHeReportDates()['end_he']?>
+        </p>
+        <p class="large">
+            <strong>Medals Missing: </strong><br/>
+            <? foreach ($missing_medals_info as $index => $info) { ?>
+                <?=($index + 1).". ".$info['medal']." ".$info['subject']?> Medal</br>
+            <? } ?>
+        </p>
+        <div class="page-break"></div>
+        
+        <? while($user = mysql_fetch_assoc($user_query)) { ?>
+            <p class="school-name">
+                <?=$user['school_name']?> - <?=$user['class_grade'] . " " .$user['class_sub']?>
+            </p>
+            
+            <div class="letter">
+                <p>Dear <?=$user['first']?> <?=$user['last']?>,</p>
+                
+                <p>
+                    This year, chayolim have earned more medals than ever before--324,090,459!
+                    So many medals have been earned that Tzivos Hashem Headquarters can't keep up--we've run out of medals!
+                    New ones are being created right now at our factories in China, and then they'll travel to Headquarters on a huge freight ship.
+                    Once they arrive, they'll be mailed to your school right away. Thanks for your patience, and keep on bringing Moshiach with all of your missions.
+                </p>
+                
+                <p>- Rabbi Shimmy Weinbaum</p>
+            </div>
+            
+            <div class="page-break"></div>
+        <? } ?>
+    </body>
+</html>
