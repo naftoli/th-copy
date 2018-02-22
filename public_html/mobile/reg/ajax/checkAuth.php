cb4555a5c05046a0f9c5ea63a6d4842e3d9e0a9f
Primary Server <mashpia@host1.mashpia.com>
2018-02-22 11:38:28 -0500

    change sql to be upper case
    Former-commit-id: 5a0b20ce532ee242aab5f1e7f744a4f434ecb54d

--- public_html/mobile/reg/ajax/checkAuth.php
+++ public_html/mobile/reg/ajax/checkAuth.php
@@ -6,7 +6,7 @@ $user_id = mysql_real_escape_string( $_POST['user_id'] );
 require 'encrypt.php';
 $admin_id = encrypt_decrypt('decrypt', $admin_id);
 
-$sql = "select * from admin_auths where admin_id = " . $admin_id . " and id = " . $user_id;
+$sql = "SELECT * FROM admin_auths WHERE admin_id = " . $admin_id . " AND id = " . $user_id;
 $result = mysql_query( $sql );
 if (mysql_num_rows($result) > 0) {
 	echo 1;
