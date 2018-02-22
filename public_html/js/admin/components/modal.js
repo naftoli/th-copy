8e7f811870a7bfc530cbba41e2104f44d93625b7
Primary Server <mashpia@host1.mashpia.com>
2018-02-22 11:43:11 -0500

    general perpose modal controller
    Former-commit-id: 7ed887ebd8b4e92faf0c6ab87b33f8d995f65992

---
+++ public_html/js/admin/components/modal.js
@@ -0,0 +1,27 @@
+// simple wrapper to show/hide custom modals...
+
+var modal = function(element) {
+    
+    var modal_element = element;
+    
+    var show = function(){
+        // attempts to pull data from hidden input fields and set them to the fields in the modal
+        modal_element.css({"visibility": "visible"});
+        modal_element.css({"opacity": "1"});
+        
+        modal_element.find(".close").click(hide);
+    };
+    
+    var hide = function() {
+        modal_element.css({"opacity": "0"});
+        setTimeout(function() {
+            modal_element.css({"visibility": "hidden"});
+        }, 100);
+    };
+    
+    // public variables returned after the encapslated function call
+    return {
+        show: show,
+        hide: hide
+    };
+};
