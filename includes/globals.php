4690b406a7327225a5168fe4e54c75ad08599d55
Menachem Hornbacher <mendelh1537@gmail.com>
2018-02-26 13:16:13 -0500

    add 'db_host' to global params for local development
    Former-commit-id: 8193d274a3bf7918d887f8b33e0f2b77c599b5a3

--- includes/globals.php
+++ includes/globals.php
@@ -1,3 +1,8 @@
 <?php
+// detect if the server is running on 192.168., if so connect to the remote server, else use localhost.
+$global_db_host = strpos($_SERVER['HTTP_HOST'], "192.168.") === false ? // use triple equals since it should return 0 which is falsy
+    "localhost" : 
+    "mashpia.com";
+// DBS credentials
 $global_db_user = 'mashpia_cth';
 $global_db_pass = 'UlqKsfnTUq2A';
