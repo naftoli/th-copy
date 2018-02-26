eef596b5bb301c02a99fc0ad91a4d6d7ccfa193c
Menachem Hornbacher <mendelh1537@gmail.com>
2018-02-26 13:23:29 -0500

    change to new db host variable
    Former-commit-id: 3e82f2450bbe7c50493b8cccb1cd30d423c9ea9c

--- public_html/achos/db.php
+++ public_html/achos/db.php
@@ -3,8 +3,8 @@
 //db
 //global $link = null;
 // old pass = eZauPhy9CEqEdYDT
-require '/home/mashpia/includes/globals.php';
-$link = mysql_connect('localhost', $global_db_user, $global_db_pass) or trigger_error_server('Failed to connect to mysql', E_USER_ERROR);
+require_once($_SERVER['DOCUMENT_ROOT'] . '/../includes/globals.php'); // import the global files...
+$link = mysql_connect($global_db_host.":3306", $global_db_user, $global_db_pass) or trigger_error_server('Failed to connect to mysql', E_USER_ERROR);
 mysql_query('SET NAMES utf8');
 mysql_query('SET CHARACTER_SET utf8');
 mysql_select_db('achos') or trigger_error_server('Failed to select db', E_USER_ERROR);
