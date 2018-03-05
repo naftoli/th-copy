96ec1a4e41ecb02107a71fdbc3bd19b5f7a89ef1
Primary Server <mashpia@host1.mashpia.com>
2018-03-05 10:31:22 -0500

    add saveFile function
    Former-commit-id: 2847e9ad6cbe4d0e8e253952fb5d8bfaace7ebb0

--- public_html/file_save.php
+++ public_html/file_save.php
@@ -77,6 +77,76 @@ function addFile($file, $missing = NULL) {
 	}
 }
 
+function saveFile( $file, $target, $file_name, $missing = false ) {
+	switch($file['error']) {
+		
+		case UPLOAD_ERR_INI_SIZE:
+		case UPLOAD_ERR_FORM_SIZE:
+			echo T_('File is too large. Please go back and try again.');
+			exit;
+		break;
+	
+		case UPLOAD_ERR_PARTIAL:
+			echo T_('File was only partially uploaded. Please go back and try again.');
+			exit;
+		break;
+	
+		case UPLOAD_ERR_NO_FILE:
+			return $missing;
+		break;
+	
+	}
+	// check for any other errors...
+	if($file['error'] !== UPLOAD_ERR_OK) {
+		return false;
+	}
+	// make sure it is uploaded..
+	if (!is_uploaded_file($file['tmp_name'])) {
+		return false;
+	}
+	// combine the target with the file_name
+	$ext = pathinfo($file['name'], PATHINFO_EXTENSION);
+	$file_name = $file_name . "." . $ext; // add the extension to the image
+	$target_file = $target . $file_name;
+	
+	// make sure that the file does not already exist....
+	$i = 1;
+	while (file_exists($target_file)) {
+		$file_name_tmp = "v" . $i++ . "." . $file_name;
+		$target_file = $target . $file_name_tmp;
+	}
+	
+	if (!move_uploaded_file($file['tmp_name'], $target_file)) { // actually move it
+		return false;
+	}
+	
+	try { // the image was uploaded. so lets try to scale it down
+		$image = new Imagick( $target_file );
+		$image->thumbnailImage( 250, 0, false, true ); // compress the image down to a smaller size...
+		// attempt to fix the rotation
+		try {
+			if ( in_array( exif_imagetype( $target_file ), [ IMAGETYPE_JPEG, IMAGETYPE_TIFF_II, IMAGETYPE_TIFF_MM ] ) && exif_read_data( $target_file )) {
+				$exif = @exif_read_data( $target_file ); // read the data
+				$orientation = $exif['Orientation']; // get the orientation
+				if($orientation){ // this will only run if orientation is set which can only happen if $_POST['action'] is set to fix
+					switch($orientation) {  
+						case 3: $image->rotateimage("#FFF", 180); $thumb->rotateimage("#FFF", 180); break; // upside down
+						case 6: $image->rotateimage("#FFF", 90); $thumb->rotateimage("#FFF", 90); break; // rotate 90 degrees CW
+						case 8: $image->rotateimage("#FFF", -90); $thumb->rotateimage("#FFF", -90);break; // rotate 90 degrees CCW
+					}
+				}
+			}
+		} catch (ImagickException $e) {} // rotation not fixed. do nothing about it.
+		
+		$image->writeImage( $target_file ); // write the file...
+		
+	} catch (ImagickException $e) {
+		return false;
+	} // the image was not uploaded/cropped correctly.
+	
+	return $target_file;
+}
+
 function addFileNew($file, $missing = NULL) {
 
 	switch($file['error']) {
