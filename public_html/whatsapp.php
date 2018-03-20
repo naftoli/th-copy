fad7125ccc24bc85d79e08a3f3ef9968a3fdc302
Menachem Hornbacher <mendelh1537@gmail.com>
2018-03-20 13:05:41 -0400

    use it to determine if we should run the report
    Former-commit-id: b2f971e3368368ed298f6ed99908e9901236a1a8

--- public_html/whatsapp.php
+++ public_html/whatsapp.php
@@ -3,10 +3,19 @@ require_once( $_SERVER['DOCUMENT_ROOT'] . '/db.php' );
 require_once( $_SERVER['DOCUMENT_ROOT'] . '/class.shabbosMevorchim.php' );
 $sm = new ShabbosMevorchim();
 
-if (isset($_POST['submit'])) {
+$date = false;
+if ( isset( $_POST[ 'date' ] ) ) {
     $sm->setReportDates( $_POST['date'] );
     $reportDates = $sm->getReportDates();
     $date = end( $reportDates ); // the last date of the report dates is the date we want to generate
+}
+
+if ( $date && !$sm->getBackupRan( $date ) ) {
+    echo "Sorry but it appears that amounts are still being entered for the current month. Please try again after the deadline has passed.<br/><br/>";
+    unset($_POST['submit']);
+}
+
+if (isset($_POST['submit'])) {
     // get the gender that we want to generate the report for
     $gender = mysql_real_escape_string( $_POST['gender'] );
     // fetch all the classes and sort them by grade
