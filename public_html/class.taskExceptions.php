de11262dd010b9e482e44def6633d42a66cc805c
Menachem Hornbacher <mendelh1537@gmail.com>
2018-03-21 16:45:26 -0400

    make SQL uppercase
    Former-commit-id: c65b5ea43978be185eaff74f3b6bd39abd87de50

--- public_html/class.taskExceptions.php
+++ public_html/class.taskExceptions.php
@@ -7,7 +7,7 @@ class TaskExceptions {
 	
     public function isException( $taskID, $userID ) {
         //find out school_id and class_id for user
-        $sql = "select school_id, class_id from users where user_id = " . $userID;
+        $sql = "SELECT school_id, class_id FROM users WHERE user_id = " . $userID;
         $result = mysql_query( $sql );
         $row = mysql_fetch_assoc( $result );
         $schoolID = $row['school_id'];
@@ -16,18 +16,19 @@ class TaskExceptions {
         //find out if school has exception
         if ( isset( $schoolID ) ) {
             //find out if school is unenrolled from subject
-            $sql = "select * from school_subjects 
-                    where school_id = $schoolID 
-                    and subject_id = (
-                    select subject_id from date_tasks_missions dtm 
-                    join date_tasks dt using (date_tasks_mission_id) 
-                    where date_task_id = $taskID)";
+            $sql = "SELECT * FROM school_subjects 
+                    WHERE school_id = $schoolID 
+                    AND subject_id = (
+                    SELECT subject_id FROM date_tasks_missions dtm 
+                    JOIN date_tasks dt USING (date_tasks_mission_id) 
+                    WHERE date_task_id = $taskID)";
             $result = mysql_query($sql);
             if (mysql_num_rows($result) == 0) {
                 return true;
             }
-            $sql = "select * from school_task_exceptions where school_id = " . $schoolID . 
-            " and date_task_id = " . $taskID; 
+            
+            $sql = "SELECT * FROM school_task_exceptions WHERE school_id = " . $schoolID . 
+            " AND date_task_id = " . $taskID; 
             $result = mysql_query( $sql );
             $numRows = mysql_num_rows( $result );
             if ( $numRows > 0 ) {
@@ -37,8 +38,8 @@ class TaskExceptions {
 
         //find out if class has exception
         if ( isset( $classID ) ) {
-            $sql = "select * from class_task_exceptions where class_id = " . $classID . 
-            " and date_task_id = " . $taskID;
+            $sql = "SELECT * FROM class_task_exceptions WHERE class_id = " . $classID . 
+            " AND date_task_id = " . $taskID;
             $result = mysql_query( $sql );
             $numRows = mysql_num_rows( $result );
             if ( $numRows > 0 ) {
