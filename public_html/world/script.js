d8f332fb989c40674630d49084ff2a94ccd17287
Menachem Hornbacher <mendelh1537@gmail.com>
2018-03-28 14:52:00 -0400

    shamai got my laptop
    Former-commit-id: bbc6e2a86a67c14a7730bff1bd56bf48049ebb1d

--- public_html/world/script.js
+++ public_html/world/script.js
@@ -76,22 +76,19 @@ $( document ).ready( function(){
                 var nextLevel =  postData.grade ? 3 : parseInt( postData.level ) + 1;
 
                 html += "<tr>";
-                html +=     '<th></td><td>';
+                html +=     '<td class="first">';
+                if ( postData.level == 1 ){
+                    html += "<img class='school_logo' alt='school_logo' src='//mashpia.com/schoolLogos/" + 
+                        ( row.logo ? row.logo : "_logo.png" ) + "'/>";
+                }
+                html += '<span class="number"></span>';
+                html += '</td><td>';
 
                 if ( postData.level < 3 && !row.lastLevel ) {
                     html +=         '<a class="id_link" href="#' + nextLevel + '-' + row.id  + '" >';
-                    // add the logo for the first row
-                    if ( postData.level == 1 ){
-                        html += "<img class='school_logo' alt='school_logo' src='//mashpia.com/schoolLogos/" + 
-                            ( row.logo ? row.logo : "_logo.png" ) + "'/>";
-                    }
                     html +=             '<span class="school_name">' + row.name + '</span>';
                     html +=         '</a>';
                 } else {
-                    if ( postData.level == 1 ){
-                        html += "<img class='school_logo' alt='school_logo' src='//mashpia.com/schoolLogos/" + 
-                            ( row.logo ? row.logo : "_logo.png" ) + "'/>";
-                    }
                     html += '<span class="school_name">' + row.name + '</span>';
                 }
                 // get the averages
@@ -99,13 +96,13 @@ $( document ).ready( function(){
                 var mishna_avg = row.campaigns.mishna.avg;
 
                 html +=     '</td>';
-                html +=     '<td>' + row.campaigns.tanya.learned + '</td>';
-                html +=     tanya_avg !== undefined ? '<td class="' + getColor( tanya_avg ) + '">' + tanya_avg + '</td>' : "";
-                html +=     '<td>' + row.campaigns.mishna.learned + '</td>';
-                html +=     mishna_avg !== undefined ? '<td class="' + getColor( mishna_avg ) + '">' + mishna_avg + '</td>' : "";
+                html +=     '<td class="total">' + row.campaigns.tanya.learned + '</td>';
+                html +=     tanya_avg !== undefined ? '<td class="total ' + getColor( tanya_avg ) + '">' + tanya_avg + '</td>' : "";
+                html +=     '<td class="total">' + row.campaigns.mishna.learned + '</td>';
+                html +=     mishna_avg !== undefined ? '<td class="total ' + getColor( mishna_avg ) + '">' + mishna_avg + '</td>' : "";
                 // if the level is not 3, calculate and render the total average for both tanya and mishna combined
                 if ( postData.level !== 3 ) {
-                    html += '<td>';
+                    html += '<td class="total">';
                     html += Math.floor( 
                         ( row.campaigns.tanya.learned + row.campaigns.mishna.learned ) / row.child_count 
                     );
@@ -134,7 +131,7 @@ $( document ).ready( function(){
             // show index column as per docs here: https://datatables.net/examples/api/counter_columns.html
             reportTable.on( 'order.dt search.dt', function () {
                 reportTable.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
-                    cell.innerHTML = i + 1;
+                    $( cell ).find( ".number" ).html( i + 1 );
                 } );
             } ).draw();
         });
