adce47d7e6b37ce3fd8a2bba835f73dbd30d937b
Menachem Hornbacher <mendelh1537@gmail.com>
2018-03-29 17:48:39 -0400

    only show address for BC's and HQ
    Former-commit-id: 02d0e3a44f799647edf7ae97361c20931e3aaff9

--- public_html/raffles/shared/ajax/get_raffle_winners.php
+++ public_html/raffles/shared/ajax/get_raffle_winners.php
@@ -2,10 +2,18 @@
 //error_reporting(E_ALL);
 //ini_set("display_errors", 1);
 /***************** IMPORTS **********************/
-require_once($_SERVER["DOCUMENT_ROOT"].'/db.php'); // load the db so that the raffle can do its thing
-require_once(dirname(__FILE__).'/../classes/Raffle.php');
+require_once( $_SERVER["DOCUMENT_ROOT"].'/db.php' ); // load the db so that the raffle can do its thing
+require_once( dirname(__FILE__).'/../classes/Raffle.php' );
 // namespace fixing
 use raffles\weekly\Raffle as Raffle; // use the raffle from its namespace
+
+$shipping = false;
+if ( isset( $_COOKIE['admin_id'] ) ){
+    $is_parent_query = mysql_query(
+        "SELECT auth FROM admin_auths WHERE auth != 'user' AND admin_id = " . mysql_escape_string( $_COOKIE['admin_id'] ) . ";"
+    );
+    $shipping = mysql_num_rows( $is_parent_query ) > 0;
+}
 
 $school_id = isset($_POST['school_id']) ? $_POST['school_id'] : false;
 $raffle_id = isset($_POST['raffle_id'])? $_POST['raffle_id'] : false;
@@ -25,7 +33,7 @@ if(!$raffle_id && $_GET['v'] == 2){
     }
 } elseif(!$raffle_id) {
     $raffles = Raffle::loadAll("WHERE show_on_mobile = 1 ORDER BY date_ran desc, type"); // show the most recent raffles with weekly having a higher priority then monthly to maintain order
-}else {
+} else {
     $raffles = []; // create a raffles array
     $raffles[] = Raffle::load($raffle_id); // and add the raffle they asked for
 }
@@ -34,7 +42,7 @@ $return_array = [];
 $sorting = isset($_POST['sorting']) ? $_POST['sorting'] : "name";
 // for each raffle
 foreach($raffles as $raffle){
-    $winners_info = $raffle->get_winner_info($school_id, $seperate_genders, $sorting); // get the winners for the school (if given, will be false and all the schools will be returned otherwise)
+    $winners_info = $raffle->get_winner_info($school_id, $seperate_genders, $sorting, $shipping); // get the winners for the school (if given, will be false and all the schools will be returned otherwise)
     $raffle_from = explode(' ', iconv('WINDOWS-1255', 'UTF-8', jdtojewish($raffle->start_date, true, CAL_JEWISH_ADD_GERESHAYIM)));
     $raffle_from = $raffle_from[0] . ' ' . $raffle_from[1];
     
