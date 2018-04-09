45af19968d5efe5f85dea8bf94d1dc6672023617
Menachem Hornbacher <mendelh1537@gmail.com>
2018-04-09 11:00:55 -0400

    minor cleanup
    Former-commit-id: 2b2959868f0dbda3374e3d5a78773cc7fd0436a2

--- public_html/mobile/reg/ajax/getMedalInfo.php
+++ public_html/mobile/reg/ajax/getMedalInfo.php
@@ -1,9 +1,9 @@
 <?
-require '../../../db.php';
+require_once( dirname(__FILE__) . '/../../../db.php' );
 $subject = mysql_real_escape_string( $_POST['subject'] );
 
 $info = array();
-$sql = "select medal_ord, missions_required from medals_subjects where subject_id = " . $subject;
+$sql = "SELECT medal_ord, missions_required from medals_subjects where subject_id = " . $subject;
 $result = mysql_query($sql);
 while ($row = mysql_fetch_assoc($result)) {
 	$info[$row['medal_ord']] = (int)$row['missions_required'];
