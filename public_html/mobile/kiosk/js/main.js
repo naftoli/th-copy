b51ceaed37c37c0a0e8142bc249e2aa6a12f74f3
Menachem Hornbacher <mendelh1537@gmail.com>
2018-04-11 15:48:50 -0400

    redo kiosk design
    Former-commit-id: 4df68ae889221ace9d4db64474d352b5ee11414d

---
+++ public_html/mobile/kiosk/js/main.js
@@ -0,0 +1,78 @@
+// check the number as a user posts it
+function checkNumber( cardNumber ) {
+    if ( cardNumber.length !== 20 ) return false;
+
+    function login( response ) {
+        response = $.parseJSON( response );
+        if ( !response.success ) showError( response.body );
+        // log the user in
+        localStorage.setItem( "login", "user" );
+        localStorage.setItem( "id", response.user_id );
+        localStorage.setItem( "kiosk", true );
+        // and redirect to their profile page
+        location.href = "/mobile/reg/medals/index.html?id=" + response.user_id;
+    }
+    // make the post request
+    $.post( 'api/checkID.php', { card : card }, login );
+}
+
+// show errors to the user in a nice, async way
+function showError( error ) {
+    console.error( error );
+};
+
+// show a box when the user scans the code
+// function showScanningBox( result ) {
+//     var drawingCtx = Quagga.canvas.ctx.overlay,
+//         drawingCanvas = Quagga.canvas.dom.overlay;
+    
+//     if ( result && result.box ) {
+//         Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
+//     }
+
+//     if ( result && result.codeResult && result.codeResult.code) {
+//         Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
+//     }
+// }
+
+// initialize Quagga JS with the follwing settings and setup the event listeners
+Quagga.init({
+    inputStream: {
+        name : "Live",
+        type : "LiveStream",
+        target: "#barcode_scanner",
+        constraints: {
+            width: {min: 640, ideal: 1280, max: 1920},
+            height: {min: 480, ideal: 720, max: 1280},
+            aspectRatio: {min: 1, max: 100},
+            facingMode: "environment" // or user
+        }
+    },
+    locator: {
+        patchSize: "medium",
+        halfSample: true
+    },
+    numOfWorkers: 2,
+    decoder: {
+        readers : ["code_128_reader"]
+    },
+    locate: true,
+    multiple: true
+}, function ( error ) {
+    if ( error ) {
+        showError( "Sorry, it seems we cannot scan cards on your device. Please enter in the card number by hand." );
+        $("#barcode_scanner").hide();
+        $("#manual_scanner").show(); // show the manual scanner
+    } else {
+        console.log( "Quagga JS initialized. Ready to start Scanning Cards" );
+        Quagga.start();
+    }
+});
+
+// Quagga.onDetected( function( data ) {
+//     if ( !checkNumber( data.codeResult.code ) ) {
+//         showError( "Sorry, it seems we could not read the card properly. Please try another angle." )
+//     }
+// });
+
+// Quagga.onProcessed( showScanningBox );
