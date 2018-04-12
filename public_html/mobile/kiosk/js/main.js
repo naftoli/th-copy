a20debfddee7ec7fd24c0da758ce6906724d0f2d
Menachem Hornbacher <mendelh1537@gmail.com>
2018-04-12 11:06:48 -0400

    eliminate the green seaking boxes
    Former-commit-id: fa51dcd6d750648f020dacbdaf11714c5b66b761

--- public_html/mobile/kiosk/js/main.js
+++ public_html/mobile/kiosk/js/main.js
@@ -28,15 +28,6 @@ function showError( error ) {
 function showScanningBox( result ) {
     var drawingCtx = Quagga.canvas.ctx.overlay,
         drawingCanvas = Quagga.canvas.dom.overlay;
-    
-    if (result && result.boxes) {
-        drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-        result.boxes.filter(function (box) {
-            return box !== result.box;
-        }).forEach(function (box) {
-            Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
-        });
-    }
 
     if ( result && result.box ) {
         Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
