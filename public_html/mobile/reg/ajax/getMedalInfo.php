cec71f241df624ba419f3848afb67e6c6c5dc48b
Menachem Hornbacher <mendelh1537@gmail.com>
2018-04-16 13:44:39 -0400

    Revert "Merge branch 'menachemhornbacher/ch592/yearly-raffle-report'"
    This reverts commit 64cac8bacda0ef389809e533de6f18adf1dac0f3 [formerly 59c4b61e6cec043e6612bf39260995ed10a974e3], reversing
    changes made to d969ab42ada5d26b43b6cf8941f415903098f76a [formerly d506f35b47db8d336cb46330b2f07f8b773fe291].
    
    
    Former-commit-id: a03f0bc5ff3527352657682bb3ffe39c7e821249

--- public_html/mobile/reg/ajax/getMedalInfo.php
+++ public_html/mobile/reg/ajax/getMedalInfo.php
@@ -1,9 +1,9 @@
 <?
-require_once( dirname(__FILE__) . '/../../../db.php' );
+require '../../../db.php';
 $subject = mysql_real_escape_string( $_POST['subject'] );
 
 $info = array();
-$sql = "SELECT medal_ord, missions_required from medals_subjects where subject_id = " . $subject;
+$sql = "select medal_ord, missions_required from medals_subjects where subject_id = " . $subject;
 $result = mysql_query($sql);
 while ($row = mysql_fetch_assoc($result)) {
 	$info[$row['medal_ord']] = (int)$row['missions_required'];
--- public_html/mobile/reg/medals/ajax/getMedalInfo.php
+++ public_html/mobile/reg/medals/ajax/getMedalInfo.php
@@ -1,6 +1,6 @@
 <?
 // load up the database
-require_once( dirname(__FILE__) . '/../../../../db.php' );
+require '../../../../db.php';
 $user = mysql_real_escape_string( $_POST['user_id'] );
 
 // figure out which subjects we are showing
@@ -10,7 +10,7 @@ $c->setType();
 $subjects = $c->getCampaigns();
 
 $subjectNames = array();
-$sql = "SELECT subject_id, subject_name FROM subjects WHERE subject_id IN (" . implode( ',', $subjects ) . ")";
+$sql = "select subject_id, subject_name from subjects where subject_id in (" . implode(',', $subjects) . ")";
 $result = mysql_query( $sql );
 while ($row = mysql_fetch_assoc( $result )) {
 	$name = $row['subject_name'];
@@ -20,7 +20,7 @@ while ($row = mysql_fetch_assoc( $result )) {
 	} else if ($row['subject_id'] == 27) {
 		$name = "תניא";
 	}
-	$subjectNames[$row['subject_id']] = $name;
+	$subjectNames[$row[subject_id]] = $name;
 }
 // get the missions
 $missions = array();
@@ -48,14 +48,13 @@ while ( $row = mysql_fetch_assoc( $result ) ) {
 		    $total += $newTotal[$barcode];		
 	}
 	*/
-    $sql2 = "SELECT medal_name, missions_required, profile_photo_id FROM medals_subjects 
-             JOIN medals USING (medal_ord)    
-             WHERE subject_id = " . $subject . " 
-             ORDER BY medal_ord"; 
+    $sql2 = "select medal_name, missions_required, profile_photo_id from medals_subjects 
+             join medals using (medal_ord)    
+             where subject_id = " . $subject . " 
+             order by medal_ord"; 
     //echo $sql2 . "<br />"; 
     $result2 = mysql_query( $sql2 );
     $needed = 0;
-    $base_amount = 0;
 	$tempMedals = array();
 	$tempMedalPics = array();
 	$ctr = 0;
@@ -63,7 +62,6 @@ while ( $row = mysql_fetch_assoc( $result ) ) {
         $tempMedals[$ctr] = $row2['medal_name'];
 		$tempMedalPics[$ctr] = $row2['profile_photo_id'];
         $required = (int)$row2['missions_required'];
-        $base_amount = $needed;
         $needed += $required;
 		//echo $medal . " needs " . $needed . " missions.<br />";
         if ( $needed > $total ) {
@@ -71,19 +69,13 @@ while ( $row = mysql_fetch_assoc( $result ) ) {
         		$missions[$subject] = array(
 	            	'medal'	=>	"None", 
 	            	'photo'	=>	"", 
-                    'left'	=>	$needed - $total,
-                    'total' =>  $total,
-                    'needed' => $needed,
-                    'base_amount' => $base_amount
+	            	'left'	=>	$needed - $total
 				);
         	} else {
 	            $missions[$subject] = array(
 	            	'medal'	=>	$tempMedals[$ctr-1], 
 	            	'photo'	=>	$tempMedalPics[$ctr-1], 
-                    'left'	=>	$needed - $total,
-                    'total' =>  $total,
-                    'needed' => $needed,
-                    'base_amount' => $base_amount
+	            	'left'	=>	$needed - $total
 				);
 			}
             break;
@@ -126,10 +118,7 @@ foreach ( $subjects as $subject ) {
 			'name'	=>	$subjectNames[$subject], 
 			'medal'	=>	$medal, 
 			'photo'	=>	$photo, 
-            'left'	=>	$left,
-            'total'	=>	intval( $mission['total'] ), 
-            'needed'=>	$mission['needed'], 
-            'base_amount'=>	$mission['base_amount'], 
+			'left'	=>	$left, 
 			'next'	=>	($key === false ? 1 : ++$key)
 		);
     } else {
@@ -138,9 +127,7 @@ foreach ( $subjects as $subject ) {
 			'name'	=>	$subjectNames[$subject], 
 			'medal'	=>	"None", 
 			'photo'	=>	"", 
-            'left'	=>	(int)$medal_subjects[$subject]['left'],
-            'total'	=>	0, 
-            'needed'=>	(int)$medal_subjects[$subject]['left'],
+			'left'	=>	(int)$medal_subjects[$subject]['left'],
 			'next'	=>	1
 		);
     }
