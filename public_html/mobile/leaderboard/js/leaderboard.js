546f2be58ea56db9dbdd4eb7d2fe3f9b86fedb40
Menachem Hornbacher <mendelh1537@gmail.com>
2018-04-18 11:13:59 -0400

    have the items animate in all at once
    Former-commit-id: bc7083e9d8c403469f33f9eb6efb89c732a76c24

--- public_html/mobile/leaderboard/js/leaderboard.js
+++ public_html/mobile/leaderboard/js/leaderboard.js
@@ -18,14 +18,15 @@ $( document ).ready(function() {
     }
     // render the leaderboard onto the page
     function renderLeaderBoard( response ) {
-        $("#leaderboard").html(""); // clear the leaderboard
+        var html = "";
         response.data.forEach( function( user, index ) {
-            $("#leaderboard").append( renderUser( user, index + 1 ) );
+            html += renderUser( user, index + 1 );
         });
+        $("#leaderboard").html( html );
     }
 
     function renderUser ( user, position ) {
-        var html = '<div class="user">';
+        var html = '<div class="user animated jackInTheBox">';
         html +=     '<img src="/mobile/img_new/ranks/' + user.rank + '.svg" alt="' + user.rank + '" />';
         html +=     '<h1>#' + position + ': ' + user.first + ' ' + user.last + '</h1>'
         html +=     '<div class="medal_count">' + user.medal_count + ' Medals</div>'
