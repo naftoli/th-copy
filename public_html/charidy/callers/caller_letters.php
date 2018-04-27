4747b09252268ecb957ac3e24a96a1e6c650e71d
Menachem Hornbacher <mendelh1537@gmail.com>
2018-04-27 18:53:03 -0400

    change title
    Former-commit-id: 908f05fca84c9837f88588aa104247a7977caadd

--- public_html/charidy/callers/caller_letters.php
+++ public_html/charidy/callers/caller_letters.php
@@ -19,7 +19,7 @@ $callers = Caller::LoadAll();
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Donor Letters</title>
+    <title>Caller Printouts</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="letters.css" />
