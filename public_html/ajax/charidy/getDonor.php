7c1cbc54940fc27af15eb53c8d1622088ae09e71
Menachem Hornbacher <mendelh1537@gmail.com>
2018-04-30 10:29:22 -0400

    set the access control headers in .htaccess
    Former-commit-id: ec1cdbccc1e13aba7e5458c287a8ff7fc49e653f

--- public_html/ajax/charidy/getDonor.php
+++ public_html/ajax/charidy/getDonor.php
@@ -19,62 +19,67 @@ Last Name
 Picture (url link to picture)
 School 
 */
-header("Access-Control-Allow-Origin: *");
-require '../../db.php';
+header('Content-Type: application/json');
+require_once( dirname(__FILE__) . '/../../db.php' );
 
-$key = mysql_real_escape_string($_POST['key']);
-if ($key == 'cth5778!') {
+$key = mysql_real_escape_string( $_POST['key'] );
+
+if ( $key != 'cth5778!' ) {
+    die(); // send an error message?
+}
+
+// blank response
+$response = [];
+$email = mysql_real_escape_string( $_POST['email'] );
+
+$donor_query = mysql_query(
+    "SELECT * FROM charidy_donors WHERE email = '" . trim($email) . "'"
+);
+// if we have a result...
+if ( $row = mysql_fetch_assoc($donor_query) ) {
+    $donor_id = $row['donor_id'];
+    $parent_id = $row['parent_admin_id'];
+    $phone = $row['phone'];
+    $address = $row['address'];
     
-    $info = array();
-    $email = mysql_real_escape_string($_POST['email']);
-    
-    $sql = "select * from charidy_donors where email = '" . trim($email) . "'";
-    $result = mysql_query($sql);
-    if ($row = mysql_fetch_assoc($result)) {
-        $donor_id = $row['donor_id'];
-        $parent_id = $row['parent_admin_id'];
-        $phone = $row['phone'];
-        $address = $row['address'];
-        
-        $info['children'] = array();
-        if ($parent_id) {
-            $sql2 = "select u.user_id, u.first, u.last, u.school_id, s.school_name, u.mobile_pic 
-                    from users u
-                    join admin_auths aa on aa.id = u.user_id
-                    join admins a using (admin_id)
-                    where a.admin_id = " . $parent_id;
-            $result2 = mysql_query($sql2);
-            while ($row2 = mysql_fetch_assoc($result2)) {
-                $info['children'][] = array(
-                    'user_id'       =>  $result2['user_id'],
-                    'name'          =>  $result2['first'] . ' ' . $result2['last'],
-                    'school'        =>  $result2['school_name'],
-                    'school_id'     =>  $result2['school_id'], 
-                    'picture'       =>  'https://mashpia.com/' . $result2['mobile_pic']
-                );
-            }
+    $response['children'] = array();
+    if ($parent_id) {
+        $sql2 = "select u.user_id, u.first, u.last, u.school_id, s.school_name, u.mobile_pic 
+                from users u
+                join admin_auths aa on aa.id = u.user_id
+                join admins a using (admin_id)
+                where a.admin_id = " . $parent_id;
+        $result2 = mysql_query($sql2);
+        while ($row2 = mysql_fetch_assoc($result2)) {
+            $response['children'][] = array(
+                'user_id'       =>  $result2['user_id'],
+                'name'          =>  $result2['first'] . ' ' . $result2['last'],
+                'school'        =>  $result2['school_name'],
+                'school_id'     =>  $result2['school_id'], 
+                'picture'       =>  'https://mashpia.com/' . $result2['mobile_pic']
+            );
         }
-        
-        $info['donor_id'] = $donor_id;
-        $info['parent_id'] = $parent_id;
-        $info['phone_number'] = $phone;
-        $info['address'] = $address;
-        $info['donation_info'] = array(
-            'last_yr'   =>  0,
-            'this_yr'   =>  0
-        );
-    } else {
-        $info  =  array(
-            'donor_id'  =>  0,
-            'parent_id' =>  0,
-            'phone'     =>  '',
-            'address'   =>  '',
-            'donation_last_yr'   =>  0,
-            'donation_this_yr'   =>  0,
-            'children'  =>  array()
-        );
     }
     
-    echo json_encode( $info );
+    $response['donor_id'] = $donor_id;
+    $response['parent_id'] = $parent_id;
+    $response['phone_number'] = $phone;
+    $response['address'] = $address;
+    $response['donation_info'] = array(
+        'last_yr'   =>  0,
+        'this_yr'   =>  0
+    );
+} else {
+    $response  =  array(
+        'donor_id'  =>  0,
+        'parent_id' =>  0,
+        'phone'     =>  '',
+        'address'   =>  '',
+        'donation_last_yr'   =>  0,
+        'donation_this_yr'   =>  0,
+        'children'  =>  array()
+    );
 }
+
+echo json_encode( $response );
 ?>
