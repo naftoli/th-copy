49695dcca9f1202f14fbae8597d9f86202e40e25
Primary Server <mashpia@host1.mashpia.com>
2018-05-02 15:53:52 -0400

    changed current_amount param to amount
    Former-commit-id: 865a8f291f2c09e89219c6b555dbbedd767c90d9

--- public_html/ajax/charidy/newDonation.php
+++ public_html/ajax/charidy/newDonation.php
@@ -7,7 +7,7 @@ $year = GlobalSettings::getCurrentYear();
 //echo json_encode( $_POST );
 
 $donor_id = mysql_real_escape_string( $_POST['donor_id'] );
-$donation_amount = mysql_real_escape_string( $_POST['current_amount'] );
+$donation_amount = mysql_real_escape_string( $_POST['amount'] );
 $date = mysql_real_escape_string( $_POST['date_time'] );
 $user_serial = isset($_POST['dedication_user_id']) && $_POST['dedication_user_id'] > 0 ?  mysql_real_escape_string( $_POST['dedication_user_id'] ) : 0;
 
