f11743a933faf217e088b207bfcbe8251a898ae6
Menachem Hornbacher <mendelh1537@gmail.com>
2018-05-03 10:41:24 -0400

    load the schools
    Former-commit-id: 90a185d59c7c755bc51e093a86f65d40c2a4daa9

--- public_html/mobile/registration/js/child.js
+++ public_html/mobile/registration/js/child.js
@@ -10,4 +10,31 @@ var childApp = function(){
         $("#pages > section#" + id).show();
     }
 
+    function loadSchools() {
+        $.get( "api/schools.php", function( response ) {
+            renderDropdown( "school_id", response.data );
+        });
+    };
+    loadSchools();
+
+    function loadClasses( school_id ){
+        $.get( "api/classes.php", { 'school_id': school_id }, function( response ) {
+            renderDropdown( "class_id", response.data );
+        });
+    }
+
+    function renderDropdown( id, options ) {
+        // keep the disabled top option
+        var html = $("select#" + id + " option:disabled").prop('outerHTML');
+        
+        options.forEach( function( option ) {
+            html += "<option value='" + option.id + "'>" + option.name + "</option>";
+        });
+        // only update selects
+        $( "select#" + id ).html( html );
+    }
+
+    return {
+        loadClasses: loadClasses
+    }
 }();
