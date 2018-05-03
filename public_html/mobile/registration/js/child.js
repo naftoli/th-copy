cf89441f835fa8e5dac782b795d74faa5bdc73ee
Menachem Hornbacher <mendelh1537@gmail.com>
2018-05-03 16:17:10 -0400

    wire it up
    Former-commit-id: fc4b9c80e6996621a8e2b9afe12d21372cac9cd4

--- public_html/mobile/registration/js/child.js
+++ public_html/mobile/registration/js/child.js
@@ -5,68 +5,123 @@ var childApp = function(){
     
     // define the state
     var state = {
-        current_page_id: "#"
+        preface: "th-"
     };
 
     // event listeners
     $( ".navigation-button" ).click( function( event ) {
         showPage( event.target.dataset.id );
     });
-
-    $( "#school_id" ).change( function( event ) {
+    // load the classes regardless of if they are in a base
+    $( "#th-school_id, #no-th-school_id" ).change( function( event ) {
         loadClasses( event.target.value );
     });
-
-    $( "#class_id" ).change( function( event ) {
+    // load the last names if they are in a base
+    $( "#th-class_id" ).change( function( event ) {
         loadUsers( event.target.value );
     });
 
+    $( "#th-base-child" ).submit( registerChild );
+    $( "#no-th-base-child" ).submit( createChild );
+
     // internal functions ( re-used or seperated code called above )
+    // change the page (section)
     function showPage( id ){
+        state.preface = id.replace("base", "");
         $("#pages > section").hide();
         $("#pages > section#" + id).show();
-    }
 
+        if ( state.preface == "no-th-" && $("select#no-th-class_id option").length == 1 ){
+            loadClasses( $("select#no-th-school_id").val() );
+        }
+    }
+    // load list of schools
     function loadSchools() {
         $.get( "api/schools.php", function( response ) {
             renderDropdown( "school_id", response.data );
         });
     };
-
+    // load the classes
     function loadClasses( school_id ){
         $.get( "api/classes.php", { 'school_id': school_id }, function( response ) {
             renderDropdown( "class_id", response.data );
         });
     }
-
+    // load all last names
     function loadUsers( class_id ){
         $.get( "api/classes.php", { 'class_id': class_id }, function( response ) {
             // clean the data coming over the wire
             var names = [];
             response.data.users.forEach( function( user ){
-                names.push({
-                    "id": user.last,
-                    "name": user.last
-                });
+                if ( names[ names.length - 1 ] != user.last ){
+                    names.push( user.last );
+                }
             });
             
-            renderDropdown( "last", names );
+            renderDropdown( "last", names.map( function( name ) {
+                return { id: name, name: name }
+            }));
         });
     }
 
+    // update the information in the dropdowns based on a list
     function renderDropdown( id, options ) {
         // keep the disabled top option
-        var html = $("select#" + id + " option:disabled").prop('outerHTML');
+        var html = $(" select#" + state.preface + id + " option:disabled").prop('outerHTML');
         
         options.forEach( function( option ) {
             html += "<option value='" + option.id + "'>" + option.name + "</option>";
         });
         // only update selects
-        $( "select#" + id ).html( html );
+        $( "select#" + state.preface + id ).html( html );
+    }
+    // convert the form to a JSON object
+    function formToJSON( form ){
+        var json = {};
+        $(event.target).serializeArray().forEach( function( input ) {
+            json[ input.name ] = input.value;
+        })
+        return json;
     }
 
-    // expose the following
-    return {
-        loadClasses: loadClasses
+    // show an error message
+    function showError( message ){
+        $("#errorModal .modal-body").text( message );
+        $("#errorModal").modal('show');
+        return false;
+    }
+
+    // register a existing child
+    function registerChild( event ) {
+        event.preventDefault();
+
+        var postData = formToJSON( event.target );
+        // validate the DOB is valid input
+        if ( !postData.dob.match(/^\d{4}-(0[1-9]|1[0-2])-([0-3][0-9])$/) ){
+            return showError( "Please enter a valid Date of Birth (YYYY-MM-DD)" );
+        }
+
+        $.post( "api/tasks/addChild.php", postData, function( response ){
+            // show any API errors...
+            if( !response.success ) return showError( response.error );
+            // show the user if the fee is paid for
+            if ( response.data.tuition ){
+                $( "#tuition-paid" ).show();
+            } else {
+                $( "#tuition-paid" ).hide();
+            }
+
+            $('#successModal').modal('show');
+        });
+    }
+    // create a new child
+    function createChild( event ) {
+        event.preventDefault();
+
+        var postData = formToJSON( event.target );
+        
+        if ( !postData.dob.match(/^\d{4}-(0[1-9]|1[0-2])-([0-3][0-9])$/) ){
+            return showError( "Please enter a valid Date of Birth (YYYY-MM-DD)" );
+        }
     }
 }();
