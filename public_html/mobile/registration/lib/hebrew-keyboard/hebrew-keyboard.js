f7466460ce70d6a49b2f3a3bf5907af6c0ea5af2
Menachem Hornbacher <mendelh1537@gmail.com>
2018-05-04 11:41:03 -0400

    hebrew typing script
    Former-commit-id: fda73f8b8ecdc9ffab4cd2f3e0d2b0e584d6bd8f

---
+++ public_html/mobile/registration/lib/hebrew-keyboard/hebrew-keyboard.js
@@ -0,0 +1,29 @@
+var hebrew_keyboard = function(){
+    // all the keys we want to replace
+    var keys = {
+        'q': '/',   'w': '\'', 'e': 'ק',   'r': 'ר',  't': 'א',
+        'y': 'ט',   'u': 'ו',  'i': 'ן',   'o': 'ם',  'p': 'פ',
+        'a': 'ש',   's': 'ד',  'd': 'ג',   'f': 'כ',  'g': 'ע',
+        'h': 'י',   'j': 'ח',  'k': 'ל',   'l': 'ך',  ';': 'ף',
+        '\'': ',',  'z': 'ז',  'x': 'ס',   'c': 'ב',  'v': 'ה',
+        'b': 'נ',   'n': 'מ',  'm': 'צ',   ',': 'ת',  '.': 'ץ',
+    }
+    // function to replace keys when typing
+    function onKeyPressed( event ){
+        // get the character
+        var character = String.fromCharCode(event.keyCode).toLowerCase(); // get the typed character
+        // if we have a letter to replace it...
+        if( keys[ character ] ){
+            event.target.value = event.target.value + keys[ character ];
+            event.preventDefault(); // do not add the typed letter
+        };
+    }
+    // funciton to attach to ids
+    function attachInput( id ){
+        $( id ).keydown( onKeyPressed );
+    }
+    // public functions
+    return {
+        attach: attachInput
+    }
+}();
