e0f7bda4e15c91df25cf759be67bfea3c6a4bcb3
Menachem Hornbacher <mendelh1537@gmail.com>
2018-05-09 15:12:09 -0400

    add deadline
    Former-commit-id: 1c981a85a83132ebdca2ed31fb9be519ae8f241c

--- public_html/raffles/yearly/classes/YearlyRaffle.php
+++ public_html/raffles/yearly/classes/YearlyRaffle.php
@@ -15,6 +15,7 @@ class YearlyRaffle {
     private $DAY_COUNT = 160;
     private $db_conn;
     private $dates;
+    private $deadline = 2458243; // May 4 2018
     
     public $eligibility;
 
@@ -30,7 +31,7 @@ class YearlyRaffle {
              " SELECT user_id, COUNT( DISTINCT(mark_date) ) as days "
             ." FROM date_tasks_marks JOIN users USING (user_id) "
             ." WHERE mark_date > " . $this->dates['start'] . " "
-            ." AND mark_date < " . $this->dates['end'] . " "
+            ." AND mark_date < " . $this->deadline . " "
             ." AND school_id = '$school_id' "
             ." GROUP BY user_id;"
         );
@@ -49,7 +50,7 @@ class YearlyRaffle {
              " SELECT user_id, COUNT( DISTINCT(mark_date) ) as days "
             ." FROM date_tasks_marks JOIN users USING (user_id) "
             ." WHERE mark_date > " . $this->dates['start'] . " "
-            ." AND mark_date < " . $this->dates['end'] . " "
+            ." AND mark_date < " . $this->deadline . " "
             ." AND user_id = '$user_id' "
         );
         // load all the eligible users
@@ -68,7 +69,7 @@ class YearlyRaffle {
             $users_sql = "SELECT user_id, user_serial, school_name, first, last, COUNT(DISTINCT (mark_date)) AS days "
                 ." FROM date_tasks_marks JOIN users USING (user_id) JOIN schools USING (school_id) "
                 ." WHERE mark_date > " . $this->dates['start'] . " "
-                ." AND mark_date < " . $this->dates['end'] . " "
+                ." AND mark_date < " . $this->deadline . " "
                 ." GROUP BY user_id "
                 ." HAVING days >= " . $this->DAY_COUNT . " "
                 ." ORDER BY school_name, last, first ";
@@ -101,6 +102,6 @@ class YearlyRaffle {
     }
 
     public function getEnd() {
-        return $this->dates['end'];
+        return $this->deadline;
     }
 }
