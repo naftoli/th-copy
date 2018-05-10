d1b8bfce1f7ccc40a2d1a610af45bf0919ab591e
Menachem Hornbacher <mendelh1537@gmail.com>
2018-05-10 10:07:46 -0400

    re-arrange links
    Former-commit-id: d475d3a6a0edd153cb32e6ac4a6562003461a9d1

--- public_html/raffles/shared/forms/index.php
+++ public_html/raffles/shared/forms/index.php
@@ -33,24 +33,39 @@ require_once($_SERVER["DOCUMENT_ROOT"].'/header.php');
             Please click <a href="https://docs.google.com/document/d/1FKrfoxTUTORwW9lO3Kx0mUY9wVeCBFCn0noQzVoj4q8/edit?usp=sharing">here</a> for the Rewards Manual
         </p>
         
-        <h2>Forms and Reports</h2>
+        <h2>Weekly / Monthly Raffles</h2>
         <div id="action-links">
             <a href="eligible_form.php">
                 <div class="button">
                     <img src="/images/icon_profile.png" height="32" alt="tickets"/>
-                    <span class="link-text">Eligible Students (Weekly/Monthly)</span>
-                </div>
-            </a>
-            <a href="../../yearly/eligibility_report.php">
-                <div class="button">
-                    <img src="/images/icon_profile.png" height="32" alt="tickets"/>
-                    <span class="link-text">Eligible Students (Yearly)</span>
+                    <span class="link-text">Eligible Students</span>
                 </div>
             </a>
             <a href="winners_form.php">
                 <div class="button">
                     <img src="/images/icon_admin_medal.png" height="32" alt="medal"/>
                     <span class="link-text">Winners</span>
+                </div>
+            </a>
+        </div>
+        <h2>Yearly Raffle</h2>
+        <div id="action-links">
+            <a href="../../yearly/eligibility_report.php">
+                <div class="button">
+                    <img src="/images/icon_profile.png" height="32" alt="tickets"/>
+                    <span class="link-text">Student Eligibility Report</span>
+                </div>
+            </a>
+            <a href="../../yearly/eligibility_report_hq.php">
+                <div class="button">
+                    <img src="/images/icon_auction.png" height="32" alt="tickets"/>
+                    <span class="link-text">Eligible Students only</span>
+                </div>
+            </a>
+            <a href="../../yearly/printout/">
+                <div class="button">
+                    <img src="/images/parentIcons/Printer.gif" height="32" alt="tickets"/>
+                    <span class="link-text">Print Posters</span>
                 </div>
             </a>
         </div>
@@ -89,12 +104,6 @@ require_once($_SERVER["DOCUMENT_ROOT"].'/header.php');
                     <div class="button">
                         <img src="/images/icon_admin_medal.png" height="32" alt="medal"/>
                         <span class="link-text">Video Winners</span>
-                    </div>
-                </a>
-                <a href="../../yearly/eligibility_report_hq.php">
-                    <div class="button">
-                        <img src="/images/icon_profile.png" height="32" alt="tickets"/>
-                        <span class="link-text">Eligible Students HQ (Yearly)</span>
                     </div>
                 </a>
             </div>
