cf1bdf6fe298c55852ed27567fc6378917a84685
Menachem Hornbacher <mendelh1537@gmail.com>
2018-05-16 10:22:42 -0400

    mobile API header
    Former-commit-id: ce9cb9a1799c46f247a7999bcd6b6ce5e13b0709

---
+++ public_html/mobile/api/header.php
@@ -0,0 +1,12 @@
+<?php
+require_once( dirname(__FILE__) . "/../../db.php" ); // database
+include_once( dirname(__FILE__) . "/functions/primary_functions.php" ); // functions for all pages
+require_once( dirname(__FILE__) . '/../reg/ajax/encrypt.php'); // encryption scheme
+
+/** AUTHENTICATION */
+if( !isset( $_COOKIE['admin'] ) )
+    render_json_error( "Invalid Authentication" );
+// decrypt the token
+$admin_id = encrypt_decrypt('decrypt', $_COOKIE['admin']);
+if ( !$admin_id )
+    render_json_error( "Invalid Authentication" );
