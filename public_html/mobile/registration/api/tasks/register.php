8dec8017a76cdc0fe2e60b433c095bda7f9c803f
Menachem Hornbacher <mendelh1537@gmail.com>
2018-05-17 15:25:43 -0400

    support payment profiles
    Former-commit-id: c343ffe0f5a6408f3a51d752893c6ebec8daa121

--- public_html/mobile/registration/api/tasks/register.php
+++ public_html/mobile/registration/api/tasks/register.php
@@ -1,5 +1,4 @@
 <?php
-define("AUTHORIZE_NET_SANDBOX", true);
 // info
 include_once( dirname(__FILE__) . "/../header.php" );
 require_once( dirname(__FILE__) . "/../../../../class.globalSettings.php" );
@@ -25,14 +24,19 @@ $description = "User Registration For $year: " . implode(", ", array_map( functi
 }, $_POST['users']));
 $amount = intval( $_POST['total'] );
 $shipping_charges = intval( $_POST[ 'shipping_charges' ] );
+$shipping_type = intval( $_POST[ 'shipping_type' ] );
 $zip = mysql_real_escape_string( $_POST[ 'zip' ] );
+
+$payment_profile_id = isset( $_POST[ 'payment_profile' ] ) ? 
+        mysql_real_escape_string( $_POST[ 'payment_profile' ] ) : false;
 /******************************** ADMIN INFO ********************************/
 // get the email for the account
 $admin_info_query = mysql_query(
-    "SELECT admin_email, authorize_customer_profile_id FROM admins WHERE admin_id = '$admin_id'"
+    "SELECT admin_email, admin_country, authorize_customer_profile_id FROM admins WHERE admin_id = '$admin_id'"
 );
 $admin_info = mysql_fetch_assoc( $admin_info_query );
 $admin_email = $admin_info[ 'admin_email' ];
+$shipping_destination = $admin_info[ 'admin_country' ];
 $customer_profile_id = isset( $admin_info['authorize_customer_profile_id'] ) ? $admin_info['authorize_customer_profile_id'] : false;
 
 if ( !$admin_email )
@@ -43,6 +47,7 @@ if ( !$admin_email )
     );
 
 /******************************** PAYMENT ********************************/
+// if we do not have an account for them on file
 if ( !$customer_profile_id ){
     $payment_profile = PaymentProfile::createBasicArray(
         $_POST['cc-number'], $_POST['cc-exp'], $_POST['x_card_code']
@@ -57,17 +62,28 @@ if ( !$customer_profile_id ){
         ." WHERE admin_id = '$admin_id'; "
     );
     $payment_profile_id = $customer_profile->paymentProfiles[0]['customerPaymentProfileId'];
+// if they are adding a new card
+} else if ( !$payment_profile_id && $_POST['cc-number'] ) {
+    $payment_profile = PaymentProfile::create(
+        $_POST['cc-number'], $_POST['cc-exp'], $_POST['x_card_code'], $customer_profile_id
+    );
+
+    if ( !($payment_profile instanceof PaymentProfile) )
+        render_json_error( $payment_profile['messages']['message'][0]['text'] );
+
+    $customer_profile = new CustomerProfile( $customer_profile_id );
+    $payment_profile_id = $payment_profile->customerPaymentProfileId;
+
 } else {
     $customer_profile = new CustomerProfile( $customer_profile_id );
-    // if it is invalid, delete the record from the table
-    if ( !($customer_profile instanceof CustomerProfile) ) {
-        mysql_query( "UPDATE admins SET authorize_customer_profile_id = NULL WHERE admin_id = '$admin_id';" );
-        render_json_error( "Could not load User Payment Info. Please try again." );
-    }
-    if ( count( $customer_profile->paymentProfiles ) == 0 ) 
-        render_json_error( "Invalid Payment Method" );
-    $payment_profile_id = $customer_profile->paymentProfiles[0]['customerPaymentProfileId'];
 }
+
+// if it is invalid, delete the record from the table
+if ( !($customer_profile instanceof CustomerProfile) ) {
+    mysql_query( "UPDATE admins SET authorize_customer_profile_id = NULL WHERE admin_id = '$admin_id';" );
+    render_json_error( "Could not load User Payment Info. Please try again." );
+}
+
 // charge the card
 $response = $customer_profile->chargeCard(
     $amount, $payment_profile_id, null, null, $description
@@ -92,6 +108,7 @@ $transactions_query = mysql_query(
 
 // save the registration
 $errors = [];
+$special_registration_ids = [];
 foreach ( $_POST['users'] as $user ){
     $user_id = $user['user_id'];
     $user_amount = $user[ 'registration_fee' ];
@@ -125,6 +142,10 @@ foreach ( $_POST['users'] as $user ){
         );
     }
 
+    if ( in_array( $school_id, [ '269', '61' ] ) ){
+        $special_registration_ids[] = $user_id;
+    }
+
     try {
         $c = new CampaignEnrollment($user_id);
         $c->enroll();
@@ -137,6 +158,23 @@ foreach ( $_POST['users'] as $user ){
     $bi = new BirthdayYi( $user_id );   $bi->setBirthday();
     $hdob = new HeDob( $user_id );      $hdob->setHeDob();
 }
+
+if ( count( $special_registration_ids ) > 0 ) {
+    $special_registration_sql = 
+        " INSERT INTO registration SET "
+        ." description = '$description' "
+        ." approval = '$responseString' "
+        ." year = '$year' "
+        ." school_id = '$school_id' "
+        ." admin_id = '$admin_id' "
+        ." ship_option = '$shipping_type' "
+        ." ship_dest = '$shipping_destination' "
+        ." users = '" . implode( ",", $special_registration_ids ) . "' ";
+    $special_registration_query = mysql_query( $special_registration_sql );
+
+    if ( !$special_registration_query ) $errors[] = "Could not sync with registration table\nSQL: " . $special_registration_sql;
+}
+
 
 if ( count( $errors ) > 0 ){
     $to = "bugs@tzivoshashem.org";
