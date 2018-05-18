f9b872e0fcb0476ea7063120aeb803b4adbe8b8e
Menachem Hornbacher <mendelh1537@gmail.com>
2018-05-18 09:57:50 -0400

    use test account for payment processor
    Former-commit-id: ae35bef4880316122cedd1ac10b60041a75e467e

--- public_html/mobile/registration/api/header.php
+++ public_html/mobile/registration/api/header.php
@@ -1,4 +1,6 @@
 <?php
+define("AUTHORIZE_NET_SANDBOX", true);
+
 require_once( dirname(__FILE__) . "/../../../db.php" ); // database
 include_once( dirname(__FILE__) . "/functions/primary_functions.php" ); // functions for all pages
 require_once( dirname(__FILE__) . '/../../reg/ajax/encrypt.php'); // encryption scheme
