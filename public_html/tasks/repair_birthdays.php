bed87fb049e919c408149da48b9331dcb3914266
Menachem Hornbacher <mendelh1537@gmail.com>
2018-05-29 11:56:44 -0400

    update query
    Former-commit-id: a4569e0015ad870fe7b8717a9a334277712fd7a4

--- public_html/tasks/repair_birthdays.php
+++ public_html/tasks/repair_birthdays.php
@@ -17,15 +17,23 @@ require_once( dirname(__FILE__) . '/../class.birthdayYi.php' );
 $cutoff = "2458589"; // all birthdays after this date will be deleted and remade.
 
 $query = mysql_query(
-     " SELECT DISTINCT(user_id), dob "
-    ." FROM birthdays "
-    ." JOIN date_tasks_missions USING (date_tasks_mission_id) "
-    ." JOIN users USING (user_id) "
-    ." WHERE start_date >= $cutoff "
-    ." AND user_registered IS NOT NULL;"
+     "SELECT DISTINCT(user_id), dob, mission_name, start_date, end_date "
+    ."FROM date_tasks_missions JOIN birthdays USING (date_tasks_mission_id) "
+    ."JOIN users USING (user_id) WHERE start_date > 2458689 "
+    ."AND subject_id = 40 AND dob <= '2013-01-01 00:00:00' AND user_registered IS NOT NULL "
+    ."GROUP BY user_id "
+    ."ORDER BY start_date "
 );
+/*
+" SELECT DISTINCT(user_id), dob "
+." FROM birthdays "
+." JOIN date_tasks_missions USING (date_tasks_mission_id) "
+." JOIN users USING (user_id) "
+." WHERE start_date >= $cutoff "
+." AND user_registered IS NOT NULL;"
+*/
 
-echo mysql_num_rows( $query ) . " Children with birthdays after April 15 2019<br/><br/>";
+echo mysql_num_rows( $query ) . " Children with birthdays that are insane<br/><br/>";
 // go through each user and update them
 while( $row = mysql_fetch_assoc( $query ) ){
     $user_id = $row['user_id'];
