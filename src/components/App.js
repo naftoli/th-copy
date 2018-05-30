7d79a50de476ead3c555b9d57e7ee0bccbba92fb
Menachem Hornbacher <mendelh1537@gmail.com>
2018-05-29 22:42:19 -0400

    introduce the sidebar

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
+import Sidebar from 'components/navigation/Sidebar';
 import logo from 'img/logo.svg';
 import 'styles/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id="App">
+        <Sidebar />
+        <div id="content">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
       </div>
     );
   }
