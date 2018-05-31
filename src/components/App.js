b727cc2cb93e94891a0cbfb319ec848de17e084b
Menachem Hornbacher <mendelh1537@gmail.com>
2018-05-31 18:34:42 -0400

    Add code to toggle sidebar

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
-import Sidebar from 'components/navigation/Sidebar';
+import Sidebar, { getMenu } from 'components/navigation/Sidebar';
 import logo from 'img/logo.svg';
 import 'styles/App.css';
 
 class App extends Component {
+  state = { active: false }
+
+  toggle = () => {
+    this.setState({
+      active: !this.state.active
+    });
+  }
+
+  componentDidMount() {
+    if ( window.innerWidth > 768 ) {
+      this.setState({ active: true });
+    }
+  }
+
   render() {
     return (
       <div id="App">
-        <Sidebar />
+        <Sidebar menu={ getMenu() } active={ this.state.active } />
         <div id="content">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
@@ -16,6 +30,7 @@ class App extends Component {
           <p className="App-intro">
             To get started, edit <code>src/App.js</code> and save to reload.
           </p>
+          <button style={ { float: "right" } } onClick={ this.toggle }>Toggle</button>
         </div>
       </div>
     );
