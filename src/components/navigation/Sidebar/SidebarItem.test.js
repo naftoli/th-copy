1618f897191831989a1f8814d4eb9a1c498f123b
Menachem Hornbacher <mendelh1537@gmail.com>
2018-06-03 14:19:50 -0400

    add test suite for SidebarItem

---
+++ src/components/navigation/Sidebar/SidebarItem.test.js
@@ -0,0 +1,124 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import SidebarItem from './SidebarItem';
+import SidebarDropdown from './SidebarDropdown';
+
+describe("SidebarItem", () => {
+  // BOILERPLATE
+  let props, mountedComponent;
+  // Component singleton
+  const sidebarItem = () => {
+    return mountedComponent ? mountedComponent : mountedComponent = mount(
+      <SidebarItem {...props} />
+    );
+  }
+  // clear global variables before each test
+  beforeEach(() => {
+    props = {};
+    mountedComponent = undefined;
+  });
+
+  // TESTS
+  describe('renders - children', () => {
+
+    it('renders a SidebarDropdown', () => {
+      props.children = [];
+      expect( sidebarItem().find( SidebarDropdown ).length ).toBe( 1 );
+    })
+
+  })
+
+  describe('renders - legacy link', () => {
+    
+    beforeEach(() => {
+      props = {
+        label: 'Item 1',
+        legacy: true,
+        path: '/test'
+      }
+    });
+
+    it('an LI tag', () => {
+      expect( sidebarItem().find('li').length ).toBe( 1 );
+    });
+
+    it('an A tag', () => {
+      expect( sidebarItem().find('a').length ).toBe( 1 );
+    });
+
+  })
+
+  // TODO when react router is setup
+  describe('renders - internal (react) link', () => {
+
+    it('an LI tag')
+
+    it('a NavLink element')
+
+  })
+
+  describe('props', () => {
+
+    describe('.label', () => {
+
+      it('has a default value (\'\')', () => {
+        expect( sidebarItem().props().label ).toBe( '' );
+      })
+
+      it('renders as the elements text', () => {
+        props.label = 'Item 1';
+        expect( sidebarItem().text() ).toBe( props.label );
+      })
+    
+    })
+
+    describe('.icon', () => {
+      
+      it('has a default value (false)', () => {
+        expect( sidebarItem().props().icon ).toBe( false );
+      })
+
+      it('renders the provided element in the A tag', () => {
+        props.icon = <i id='test-icon' />;
+        expect( sidebarItem().find("a > #test-icon").length ).toBe( 1 );
+      })
+
+    })
+
+    describe('.children', () => {
+
+      it('has a default value (false)', () => {
+        expect( sidebarItem().props().children ).toBe( false );
+      })
+
+      it('renders a SidebarDropdown with all it\'s props when this prop is valid', () => {
+        props.children = [ { label: "Item 2.1" } ];
+        props.test = "fake prop";
+
+        expect( sidebarItem().find( SidebarDropdown ).length ).toBe( 1 );
+        expect( sidebarItem().find( SidebarDropdown ).props().children ).toBe( props.children );
+        expect( sidebarItem().find( SidebarDropdown ).props().test ).toBe( props.test );
+      })
+
+      it('does not render a SidebarDropdown when this prop is not valid', () => {
+        expect( sidebarItem().find( SidebarDropdown ).length ).toBe( 0 );
+      })
+
+    })
+
+    describe('.legacy', () => {
+      it('has a default value(false)', () => {
+        expect( sidebarItem().props().legacy ).toBe( false );
+      })
+
+      it('renders a NavLink tag when set to false')
+      it('renders an A tag when set to true')
+    })
+
+    describe('.path', () => {
+      it('has a default value (#)', () => {
+        expect( sidebarItem().props().path ).toBe( '#' );
+      })
+    })
+  })
+})
