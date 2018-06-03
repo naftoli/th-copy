168577fcef5568561766f0761bf6fee834c8d613
Menachem Hornbacher <mendelh1537@gmail.com>
2018-06-03 15:48:55 -0400

    Add profile picture and title

--- src/components/navigation/Navbar/Navbar.jsx
+++ src/components/navigation/Navbar/Navbar.jsx
@@ -4,8 +4,15 @@ import {
   NavbarBrand
 } from 'reactstrap';
 import './Navbar.scss';
+import { LEGACY_URL } from 'components/constants';
+import user from 'img/user.svg';
 
 class Navbar extends Component {
+  static defaultProps = {
+    profile_picture: user,
+    title: "Tzivos Hashem"
+  }
+
   render(){
     return (
       <BoostrapNavbar id="mashpia-navbar">
@@ -13,6 +20,15 @@ class Navbar extends Component {
           <img src="//mashpia.com/mobile/img_new/TH%20Logo-colorful-svg.svg" alt="logo" />
           <span>Menu</span>
         </NavbarBrand>
+        <div id="navbar-title" className="mx-auto">
+          { this.props.title }
+        </div>
+        <div id="navbar-menu">
+          <a href={`${LEGACY_URL}/admin_profile.php`}>
+            <img id="profile-picture" src={ this.props.profile_picture } alt="profile_picture"/>
+            <span>My Account</span>
+          </a>
+        </div>
       </BoostrapNavbar>
     )
   }
