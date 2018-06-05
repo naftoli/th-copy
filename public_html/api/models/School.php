483b29a522b8c7dd0096d2164cdfd915da9a82f5
Menachem Hornbacher <mendelh1537@gmail.com>
2018-06-05 11:49:41 -0400

    Add serializers
    Former-commit-id: fc644f44bf2436f9cf12e903d2fe31f78ef42606

--- public_html/api/models/School.php
+++ public_html/api/models/School.php
@@ -1,5 +1,36 @@
 <?php
-class School extends ActiveRecord\Model {
+class School extends ActiveRecord\Model implements JsonSerializable {
     // relationships
     static $has_many = [ ['school_reg_infos'] ];
+
+
+    // ******************************* SERIALIZERS *******************************
+    /**
+     * jsonSerialize
+     * 
+     * serialize object to array
+     * 
+     * @return array
+     */
+    public function jsonSerialize(){
+        return $this->to_array([
+            'except' => [
+                'cc_first', 'cc_last', 'cc_address', 'cc_state', 'cc_zip', 'cc_number', 'cc_exp', 'cc_cvv',
+                'cc_approval_number',  'authorize_customer_profile_id', 'authorize_payment_profile_id'
+            ]
+        ]);
+    }
+
+    /**
+     * publicSerialize
+     * 
+     * serialze school for public endpoints
+     *
+     * @return array
+     */
+    public function publicSerialize(){
+        return $this->to_array([
+            'only' => [ 'school_id', 'school_name', 'hachayol_name' ]
+        ]);
+    }
 }
