a7b37843426ce537a2be7f1992b6465fc4f98183
Menachem Hornbacher <mendelh1537@gmail.com>
2018-06-05 10:18:37 -0400

    Add validations and serializations
    Former-commit-id: 14206c2b5e68959e9070b3ad09ad4f9440ef0bd2

--- public_html/api/models/SchoolRegInfo.php
+++ public_html/api/models/SchoolRegInfo.php
@@ -1,5 +1,18 @@
 <?php
-class SchoolRegInfo extends ActiveRecord\Model {
+class SchoolRegInfo extends ActiveRecord\Model implements JsonSerializable {
     // relationships
     static $belongs_to = [ ['school'] ];
+    // validations
+    static $validates_uniqueness_of = [
+        [ ['school_id', 'year'], 'message' => '- duplicate record' ]
+    ];
+    
+    public function validate() {
+        if ( $this->type == 2 && !$this->reg_deadline )
+            $this->errors->add('registration_deadline', 'must be present on guaranteed bases');
+    }
+
+    public function jsonSerialize(){
+        return $this->to_array();
+    }
 }
