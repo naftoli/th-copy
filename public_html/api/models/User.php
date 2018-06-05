c0426244003ea847fa1d49b30af7e3ba0a2c05fc
Menachem Hornbacher <mendelh1537@gmail.com>
2018-06-05 12:11:54 -0400

    Create User model
    Former-commit-id: 415b8f41ce24e64cbecf2c390091f4d512901ba1

---
+++ public_html/api/models/User.php
@@ -0,0 +1,36 @@
+<?php
+class User extends ActiveRecord\Model implements JsonSerializable {
+    // relationships
+    static $belongs_to = [
+        [ 'school' ], [ 'platton' ]
+    ];
+
+    // ******************************* SERIALIZERS *******************************
+    /**
+     * jsonSerialize
+     * 
+     * serialize object to array
+     * 
+     * @return array
+     */
+    public function jsonSerialize(){
+        return $this->to_array();
+    }
+
+    /**
+     * publicSerialize
+     * 
+     * serialze school for public endpoints
+     *
+     * @return array
+     */
+    public function publicSerialize(){
+        return $this->to_array([
+            'only' => [ 'user_serial', 'first', 'last' ],
+            'include' => [ 
+                'school' => [ 'only' => [ 'school_id', 'school_name' ] ],
+                'platton' => [ 'only' => [ 'class_id', 'class_grade', 'class_sub' ], 'methods' => [ 'name' ] ]
+            ]
+        ]);
+    }
+}
