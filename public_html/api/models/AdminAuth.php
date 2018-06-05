978d0a8e891b6ceab11b207fe67aa50046c4b226
Menachem Hornbacher <mendelh1537@gmail.com>
2018-06-05 11:49:30 -0400

    hide untill User model is created
    Former-commit-id: 4f658d1f413fd268a12342742b65cfa702d6aafa

--- public_html/api/models/AdminAuth.php
+++ public_html/api/models/AdminAuth.php
@@ -2,9 +2,9 @@
 class AdminAuth extends ActiveRecord\Model {
     static $belongs_to = [
         [ 'admin' ],
-        [ 'user',
-            'foreign_key' => 'id', # key in linked (this) table
-            'primary_key' => 'user_id',  # key in "parent" table
-        ]
+        // [ 'user',
+        //     'foreign_key' => 'id', # key in linked (this) table
+        //     'primary_key' => 'user_id',  # key in "parent" table
+        // ]
     ];
 }
