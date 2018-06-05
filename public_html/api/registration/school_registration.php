6111cbd11a33ae92b11db12aa8c89cd1754aa23c
Menachem Hornbacher <mendelh1537@gmail.com>
2018-06-05 10:19:41 -0400

    implament RestRouter interface now that all routes are compleate
    Former-commit-id: 305ce84428e6ea515fc5be854b4eacfa01b5a687

--- public_html/api/registration/school_registration.php
+++ public_html/api/registration/school_registration.php
@@ -2,7 +2,7 @@
 define( "MASHPIA_AUTH_REQUIRED", true );
 include_once( __DIR__ . "/../header/header.php" );
 
-class SchoolRegistrationRouter {
+class SchoolRegistrationRouter implements RestRouter {
     // limit controller to HQ
     function authenticate(){
         global $current_user;
