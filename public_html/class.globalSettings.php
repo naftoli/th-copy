6722cfbd92e129d56d80584f4006086cc6ff99b9
Menachem Hornbacher <mendelh1537@gmail.com>
2018-06-05 17:57:02 -0400

    add logic for registration rates
    Former-commit-id: 5442531ce1341d3483ae90864f704e847b00bb53

--- public_html/class.globalSettings.php
+++ public_html/class.globalSettings.php
@@ -47,5 +47,38 @@ class GlobalSettings {
         $row = mysql_fetch_assoc($result);
         return $row['val'];
     }
+
+    /**
+     * GlobalSettings::getRegCost
+     * 
+     * returns current registration costs for the given school type, and early bird specials.
+     * 
+     * Accepts optional school paramater to return the rate that the school pays.
+     *
+     * @param int $type
+     * @param boolean $early_bird
+     * @param boolean $school
+     * @return integer
+     */
+    public static function getRegCost( $type, $early_bird, $school = false ) {
+        if ( $type == 1 ) { // In Tuition
+            return $school ? 50 : 0; // schools get billed $50 for each kid
+        } else if ( $type == 2 ) { // Guarranteed, they get a bit of a discount
+            return $early_bird ? 50 : 55;
+        }
+        // everyone else / default return
+        return $early_bird ? 55 : 60;
+    }
+
+    /**
+     * getChidonCost
+     * 
+     * return the current price for chidon registration
+     *
+     * @return int
+     */
+    public static function getChidonCost(){
+        return 5;
+    }
 }
 ?>
