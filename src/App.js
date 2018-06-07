c42a36c3a0b015e4b272a09935cbc19b88b8f4ec
Menachem Hornbacher <mendelh1537@gmail.com>
2018-06-07 18:39:17 -0400

    move and clean up App.js component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Dashboard from 'pages/Dashboard/Dashboard';
+import 'styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Dashboard>
+        Hello World!
+      </Dashboard>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import 'styles/index.css';
-import App from 'components/App';
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
