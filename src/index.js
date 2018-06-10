8a226f86bf426605f8bc9bbce8214d9ad88a3510
Menachem Hornbacher <mendelh1537@gmail.com>
2018-06-10 19:15:11 -0400

    wire up the app with the state

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 // Global Styles
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { Provider } from 'react-redux';
+import store from './store';
+// Authentication
+import checkLogin from 'store/login/checkLogin';
+// App
 import 'styles/index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+checkLogin( store.dispatch );
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
