d151e10d788a4be287f483fd0f19d5a518f9f001
Menachem Hornbacher <mendelh1537@gmail.com>
2018-06-10 18:52:49 -0400

    function to check current users login status

---
+++ src/store/login/checkLogin.js
@@ -0,0 +1,14 @@
+import { actions } from './actions';
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
+
+export default dispatch => {
+  const tokens = {
+    legacy: cookies.get('admin_auth'),
+    mobile: cookies.get('admin')
+  }
+
+  if ( tokens.legacy )
+    return dispatch( actions.tokens( tokens ) );
+}
