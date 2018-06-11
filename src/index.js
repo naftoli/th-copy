e536c0e99e634411288467cadde478565513f11c
Menachem Hornbacher <mendelh1537@gmail.com>
2018-06-11 08:45:36 -0400

    import blueprint css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // Global Styles
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
+
 import { Provider } from 'react-redux';
 import store from './store';
 // Authentication
