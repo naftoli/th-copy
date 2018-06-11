0eeeb27321910aaa31971f802f8ca8f3735cecb1
Menachem Hornbacher <mendelh1537@gmail.com>
2018-06-11 16:51:21 -0400

    add the router

--- src/App.js
+++ src/App.js
@@ -10,26 +10,26 @@ export class App extends Component {
   render() {
     if ( this.props.logged_in ) {
       return (
-        <Router>
+        <Router basename={ process.env.PUBLIC_URL } >
           <Dashboard>
             <Switch>
-              <Route path="/" exact render={props => <h1>HomePage</h1>}/>
-              <Route path="/users" exact render={props => <h1>View / Edit Users</h1>}/>
-              <Route path="/users/registration" render={props => <h1>Bulk User Registration</h1>}/>
-              <Route path="/users/cards" render={props => <h1>User Rank Cards</h1>}/>
-              <Route path="/users/:id" render={props => <h1>View / Edit Single User</h1>}/>
+              <Route path={`/`} exact render={props => <h1>HomePage</h1>}/>
+              <Route path={`/users`} exact render={props => <h1>View / Edit Users</h1>}/>
+              <Route path={`/users/registration`} render={props => <h1>Bulk User Registration</h1>}/>
+              <Route path={`/users/cards`} render={props => <h1>User Rank Cards</h1>}/>
+              <Route path={`/users/:id`} render={props => <h1>View / Edit Single User</h1>}/>
 
-              <Route path="/plattons" exact render={props => <h1>Plattons</h1>}/>
-              <Route path="/parents" exact render={props => <h1>Parents</h1>}/>
-              <Route path="/staff" exact render={props => <h1>Staff</h1>}/>
+              <Route path={`/plattons`} exact render={props => <h1>Plattons</h1>}/>
+              <Route path={`/parents`} exact render={props => <h1>Parents</h1>}/>
+              <Route path={`/staff`} exact render={props => <h1>Staff</h1>}/>
 
-              <Route path="/base" exact render={props => <h1>View / Edit Base</h1>}/>
-              <Route path="/base/settings" exact render={props => <h1>Base Settings</h1>}/>
-              <Route path="/base/transactions" exact render={props => <h1>Base Transactions</h1>}/>
+              <Route path={`/base`} exact render={props => <h1>View / Edit Base</h1>}/>
+              <Route path={`/base/settings`} exact render={props => <h1>Base Settings</h1>}/>
+              <Route path={`/base/transactions`} exact render={props => <h1>Base Transactions</h1>}/>
 
-              <Route path="/logout" component={Logout}/>
+              <Route path={`/logout`} component={Logout}/>
 
-              <Route render={ props => <h1>404</h1> } />
+              <Route render={ props => <h1>404 - {process.env.PUBLIC_URL}</h1> } />
             </Switch>
           </Dashboard>
         </Router>
