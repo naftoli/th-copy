320a3e30bed93a4a861dde080e050a9dbac880bf
Menachem Hornbacher <mendelh1537@gmail.com>
2018-06-14 15:58:39 -0400

    shimmy updates
    Former-commit-id: f6b5ea61b75bf3988ffee6f0bb341c7467de5251

--- public_html/mobile/reg/js/sticker-board.js
+++ public_html/mobile/reg/js/sticker-board.js
@@ -116,20 +116,21 @@ var sticker_board = function() {
                 status_text = ( medal_info.missions_required - ( total - last_total ) ) + ' to ' + medal_info.medal_name;
             // future
             } else {
-                compleation_status = ( 1 - ( ( medal_info.running_total - total ) / medal_info.running_total ) ) * 100;
-                status_text = ( medal_info.running_total - total ) + ' to ' + medal_info.medal_name;
+                compleation_status = 0; status_text = '';
             }
             
             // render the medal icon
             html += '<hr><div class="row">' +
                 '<div class="col-4 col-sm-3 medal-level">' +
                     '<span class="levelText">Level ' + medal_info.medal_ord + '</span>' + 
-                    '<img class="medal-img ' + medal_classes + '" src="' + medal_info.photo + '" />' + 
-                    '<div class="medal-status progress">' + 
+                    '<img class="medal-img ' + medal_classes + '" src="' + medal_info.photo + '" />';
+            if ( status_text !== '' ) {
+            html += '<div class="medal-status progress">' + 
                         '<div class="progress-bar ' + medal_color + '" role="progressbar" style="width: ' + compleation_status + '%;"></div>' + 
                         '<span>' + status_text + '</span>' +
-                    '</div>' + 
-                '</div>' +
+                    '</div>';
+            }
+            html += '</div>' +
                 '<div class="col-8 col-sm-9 medal-level-stickers">';
             // render all the stickers
             for( var i = 1; i <= medal_info.missions_required; i++ ) {
@@ -197,7 +198,7 @@ var sticker_board = function() {
             // fix issue where height is not calculated correctly on first load
             setTimeout( function() {
                 var height = $('#sticker-board-' + index)[0].scrollHeight;
-                $('#medal-stickers .slick-list.draggable').height( height );
+                $('#medal-stickers .slick-list.draggable').height( height + 20 );
                 
             }, 750 ); // 750ms should be enough time to come in and fix the issue then.
         }
