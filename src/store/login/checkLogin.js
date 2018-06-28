fe2c7079d04030dc166726502f5237a81e1be030
Menachem Hornbacher <mendelh1537@gmail.com>
2018-06-28 12:30:20 -0400

    update login when pulling login from cache

--- src/store/login/checkLogin.js
+++ src/store/login/checkLogin.js
@@ -1,4 +1,4 @@
-import { setTokens } from './actions';
+import { setTokens, changeLogin } from './actions';
 import { getCurrentUser } from './operations';
 import Cookies from 'universal-cookie';
 
@@ -10,10 +10,16 @@ export default dispatch => {
     mobile: cookies.get('admin'),
     id:     cookies.get('admin_id'),
   }
-
+  // if we have tokens
   if ( tokens.legacy ) {
     dispatch( setTokens( tokens.legacy, tokens.mobile, tokens.id ) );
-    getCurrentUser()( dispatch );
+    getCurrentUser()( dispatch ).then( () => {
+      // change to the selected login if we have one...
+      if ( cookies.get( 'login' ) ) {
+        const [ type, id ] = cookies.get( 'login' ).split('-');
+        dispatch( changeLogin( type, parseInt(id, 10) ) );
+      }
+    });
   }
 
   return false;
