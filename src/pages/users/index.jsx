4ff18edd014265238da243fdf7cb391e5d768d35
Menachem Hornbacher <mendelh1537@gmail.com>
2018-06-29 14:38:41 -0400

    Add /new route

--- src/pages/users/index.jsx
+++ src/pages/users/index.jsx
@@ -8,15 +8,15 @@ export class UsersPage extends Component {
   render() {
     const { path } = this.props.match;
     return (
-        <Switch>
-          <Route path={ path } exact component={ AllUsers } />
-          <Route path={`${path}/registration`} render={props => <h1>Bulk User Registration</h1>}/>
-          <Route path={`${path}/cards`} render={props => <h1>User Rank Cards</h1>}/>
-          <Route path={`${path}/:id`} render={props => <h1>View / Edit Single User</h1>}/>
-        </Switch>
+      <Switch>
+        <Route path={ path } exact component={ AllUsers } />
+        <Route path={`${path}/registration`} render={props => <h1>Bulk User Registration</h1>}/>
+        <Route path={`${path}/cards`} render={props => <h1>User Rank Cards</h1>}/>
+        <Route path={`${path}/new`} render={props => <h1>Create New User</h1>}/>
+        <Route path={`${path}/:id`} render={props => <h1>View / Edit Single User: { props.match.params.id }</h1>}/>
+      </Switch>
     )
   }
-
 }
 
 export default UsersPage;
