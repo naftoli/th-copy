07c5cc8e10fd8023eefbd985fc272031ef961fb6
Menachem Hornbacher <mendelh1537@gmail.com>
2018-07-04 10:55:49 -0400

    remove unused code and direct them to pre-register
    Former-commit-id: 4dd4d43e7558a154ff663106bcbd62176180a8d5

--- public_html/main_home.php
+++ public_html/main_home.php
@@ -1,35 +1,9 @@
 <?
-/*
-$qry = "select count(*) as total from users 
-		join schools using (school_id) 
-		where school_id is not null 
-		and school_era is null";
-$res = mysql_query($qry);
-$r = mysql_fetch_assoc($res);
-$total = $r['total'];
-
-$qry = "select count(*) as total from users 
-		join schools using (school_id) 
-		where user_registered > 0";
-$res = mysql_query($qry);
-$r = mysql_fetch_assoc($res);
-$totalReg = $r['total'];
-
-$qry = "select count(*) as total from users where school_id = " . $school_id;
-$res = mysql_query($qry);
-$r = mysql_fetch_assoc($res);
-$totalSchool = $r['total'];
-
-$qry = "select count(*) as total from users where school_id = " . $school_id . " and user_registered > 0";
-$res = mysql_query($qry);
-$r = mysql_fetch_assoc($res);
-$totalSchoolReg = $r['total'];
-*/
 require_once 'class.globalSettings.php';
-$year = GlobalSettings::getChidonYear();
+$chidonYear = GlobalSettings::getChidonYear();
 
 // get campaigns for current year
-$campaign_query = mysql_query( "SELECT * FROM line_campaigns WHERE year = " . $year );
+$campaign_query = mysql_query( "SELECT * FROM line_campaigns WHERE year = " . $chidonYear );
 while ($campaign = mysql_fetch_assoc( $campaign_query )) {
 	if (strtolower($campaign['type']) == 'tanya') $tanyaCampaign = $campaign['id'];
 	else if (strtolower($campaign['type']) == 'mishna') $mishnaCampaign = $campaign['id'];
@@ -56,9 +30,6 @@ while ($campaign = mysql_fetch_assoc( $campaign_query )) {
                     "<input type='submit' name='submit' value='submit' />" + 
                 "</form>" );
         });
-         
-        //check if user has been created in wordpress
-        //$.get('syc.php', {user : user, pass : pass});
         
         var del = <?=isset($_GET['deletedParents']) ? $_GET['deletedParents'] : 0?>;
         if (del) {
@@ -161,113 +132,8 @@ while ($campaign = mysql_fetch_assoc( $campaign_query )) {
         			alert("Error updating.");
         		}
         	});
-        });       
-        /*
-        $("#learned").blur( function() {
-        	var num = $(this).val().trim();
-			if (num == '') num = 0;
-        	var school = <?=$school_id?>;
-        	if (isNaN(num)) {
-        		alert("You must enter a number.");
-        		return;
-        	}
-        	$.post('ajax/updateBalPehCampaign.php', {
-        		id : tanyaCampaign, 
-        		val : num, 
-        		school : school, 
-        		table : 'lines_learned'
-        	}, function( data ) {
-        		if (data == 1) {
-        			//alert("Updated.");
-					
-//         			$.post('ajax/updateBpSummary.php', {
-//         				campaign : tanyaCampaign, 
-//         				school : school 
-//         			});
-        			
-        		} else {
-        			alert("Error updating.");
-        		}
-        	});
         });
-        
-        $("#mLearned").blur( function() {
-        	var num = $(this).val().trim();
-			if (num == '') num = 0;
-        	var school = <?=$school_id?>;
-        	if (isNaN(num)) {
-        		alert("You must enter a number.");
-        		return;
-        	}
-        	$.post('ajax/updateBalPehCampaign.php', {
-        		id : mishnaCampaign, 
-        		val : num, 
-        		school : school, 
-        		table : 'lines_learned'
-        	}, function( data ) {
-        		if (data == 1) {
-        			//alert("Updated.");
-					
-//         			$.post('ajax/updateBpSummary.php', {
-//         				campaign : mishnaCampaign, 
-//         				school : school 
-//         			});
-        			
-        		} else {
-        			alert("Error updating.");
-        		}
-        	});
-        });
-		
-		$("#syncTanya").click( function() {
-			$.post('ajax/updateBpSummary.php', {
-				campaign : tanyaCampaign, 
-				school : school 
-			});
-		});
-		
-		$("#syncMishna").click( function() {
-			$.post('ajax/updateBpSummary.php', {
-				campaign : mishnaCampaign, 
-				school : school 
-			});
-		});
-		/*
-        function checkLines() {
-        	<? if (!$admin->is_parent == true) : ?>
-        	var v = prompt("Please enter how many lines your school commits to learn Baal Peh by Yud Alef Nissan", " ");
-        	if (v > 0 && !isNaN(v)) {
-        		$("#lines").val(v);
-        		$("#lines").trigger('blur');
-        	} else {
-        		checkLines();
-        	}
-        	<? endif; ?>
-        }
-        
-        $.post('ajax/maosChittim.php', {
-        	school : <?=$schl_id;?>,
-        	type : 'pledged',
-        	action : 'get'
-        }, function(data) {
-        	if (data) {
-        		$("#maos").val(data);
-        	}
-        });
-        
-        $("#maos").blur(function() {
-        	var val = $(this).val();
-        	$.post('ajax/maosChittim.php', {
-        		school : <?=$schl_id;?>, 
-        		val : val, 
-	        	type : 'pledged',
-	        	action : 'set'
-        	});
-        });
-        */
-        //var total = <?//=$total?>;
-        //var totalReg = <?//=$totalReg?>;
-	    });
+	});
 </script>
 
 <style>
@@ -316,7 +182,7 @@ while ($campaign = mysql_fetch_assoc( $campaign_query )) {
 	.updates div .inner {
 		clear: none;
 		overflow: hidden;
-		padding: 0;
+		padding: 10px;
 	}
 	.updates div.resources {
 		background-color: #D5D8DE;
@@ -435,39 +301,6 @@ while ($campaign = mysql_fetch_assoc( $campaign_query )) {
 	</p>
 	<div style='clear: both'></div>
 </div>
-<!--
-<div align="center">
-	<br />
-	<p>
-		Click <a href="admin_user_auction.php">here</a> to assign prizes to your students for the End of Year Auction!
-	</p>
-</div>
-
-<div align="center">
-	<video width="480" height="320" controls>
-	  <source src="downloads/Auction3.mp4" type="video/mp4">
-	Your browser does not support the video tag. 
-	Click <a href="downloads/Auction3.mp4">here</a> to view end of year auction video clip.
-	</video>
-</div>
-
-<div align="center">
-	<video width="480" height="320" controls>
-	  <source src="downloads/chidon2.mp4" type="video/mp4">
-	Your browser does not support the video tag. 
-	Click <a href="downloads/chidon2.mp4">here</a> to view end of year auction video clip.
-	</video>
-</div>
-
-<div align="center">
-	<br />
-	<video width="480" height="320" controls>
-	  <source src="downloads/Auction 5775 HD.mp4" type="video/mp4">
-	Your browser does not support the video tag. 
-	Click <a href="downloads/Auction 5775 HD.mp4">here</a> to view end of year auction video clip.
-	</video><br />
-</div>
--->
 <div class="middle">
 	<div class="line"></div>
 	<div class="buttons"> 
@@ -484,21 +317,6 @@ while ($campaign = mysql_fetch_assoc( $campaign_query )) {
 				<br />Mark Missions
 			</a>
 		</div>
-		<!--
-		<div class="button">
-			<a href="stickers_report.php">
-				<img src="images/parentIcons/StickerHiskashrus.jpg" />
-				<br />Sticker Report
-			</a>
-		</div>
-		
-		<div class="button">
-			<a href="charge_card_report.php">
-				<img src="images/parentIcons/Charge it Report.gif" />
-				<br />Charge It Report
-			</a>
-		</div>
-		-->	
 	</div>
 	<div style='clear: both'></div>
 </div>
@@ -507,31 +325,42 @@ while ($campaign = mysql_fetch_assoc( $campaign_query )) {
 	<h2>Tzivos Hashem Updates</h2>
 	
 	<?
-	$qry = "select count(user_id) as total from users where school_id = $school_id and user_registered > 0";
+    $regYear = GlobalSettings::getRegistrationYear();
+
+    $qry = "SELECT COUNT(user_id) AS total FROM user_registration "
+        ."WHERE school_id = $school_id AND year = $regYear;";
 	$resultQ = mysql_query($qry);
 	$rowQ = mysql_fetch_assoc($resultQ);
 	$registered = $rowQ['total'];
 	
-	$qry2 = "select count(user_id) as total from users where school_id = $school_id and user_registered is null";
+    $qry2 = "SELECT COUNT(user_id) AS total FROM users u "
+        ."LEFT JOIN user_registration USING (user_id) "
+        ."WHERE u.school_id = $school_id AND ( year = $regYear OR year is null );";
 	$resultQ2 = mysql_query($qry2);
 	$rowQ2 = mysql_fetch_assoc($resultQ2);
-	$notRegistered = $rowQ2['total'];
-	
-	require_once 'class.globalSettings.php';
-	$year = GlobalSettings::getRegistrationYear();
+    $notRegistered = $rowQ2['total'];
+    
+    $school_registered_query = mysql_query(
+        "SELECT date_paid FROM school_registrations WHERE school_id = $school_id AND year = $regYear;"
+    );
+    $school_registered = mysql_fetch_assoc( $school_registered_query )['date_paid'];
 	?>	
 	<div>
 		<div class="inner">
-			<h3>Registration <?=$year?></h3>
-			<p>
-				You have <?=$registered?> chayolim registered in the program.<br />
-				<? if ($notRegistered > 0) : ?>
-				<span style="color: red; font-weight: bold;">
-					You still have <?=$notRegistered?> chayolim that are not yet registered!<br />
-					Click <a href="admin_users_register_new.php?school_id=<?=$school_id?>&registered=1">here</a> to register them!</span>
-				</span>
-				<? endif; ?>
-			</p>
+			<h3>Registration <?=$regYear?></h3>
+            <? if ( $school_registered ) { ?>
+                <p>
+                    You have <?=$registered?> chayolim registered in the program for <?=$regYear?>.<br />
+                    <? if ($notRegistered > 0) : ?>
+                    <span style="color: red; font-weight: bold;">
+                        You still have <?=$notRegistered?> chayolim that are not yet registered for <?=$regYear?>!<br />
+                        <!-- Click <a href="admin_users_register_new.php?school_id=<?=$school_id?>&registered=1">here</a> to register them!</span> -->
+                    </span>
+                    <? endif; ?>
+                </p>
+            <? } else { ?>
+                <h4>Pre-register your base for <?=$regYear?> <a href='registration.php'>here</a>.</h4>
+            <? } ?>
 			<!--Click <a href="child_list.php">here</a> for the list of parent accounts with linked children.-->
 		</div>
 	</div>
