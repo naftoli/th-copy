c2d2603816fd8e4238f48cdf626e3b20e745d367
Menachem Hornbacher <mendelh1537@gmail.com>
2018-07-09 09:05:22 -0400

    show name of file in fileinput

--- src/components/ui/FileInput.jsx
+++ src/components/ui/FileInput.jsx
@@ -3,13 +3,25 @@ import './styles/FileInput.scss';
 
 export class FileInput extends Component {
 
+  state = {
+    text: 'Choose a file'
+  }
+
+  fileSelected = () => {
+    this.setState({
+      text: this.props.inputRef.current.files[0].name
+    });
+  }
+
   render(){
+    const { inputRef, className } = this.props;
     return (
       <div className='FileInput'>
-        <input type="file" id="file" accept=".xls"/>
+        <input type="file" id="file" accept=".xls" ref={ inputRef } 
+          className={ className } onChange={ this.fileSelected }/>
         <label htmlFor="file">
           <i className="fas fa-upload"></i>&nbsp;
-          <strong>Choose a file</strong>
+          <strong>{this.state.text}</strong>
           {/* <span className="box__dragndrop"> or drag it here</span>. */}
         </label>
       </div>
