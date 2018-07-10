7f62020a4fd3b8cb43c4e922d0e573f2bae048bd
Menachem Hornbacher <mendelh1537@gmail.com>
2018-07-10 11:00:04 -0400

    booklet report for chidon
    Former-commit-id: fb9f61c8caef032d4054d41ca32fa2c9aa0f3797

--- public_html/reports/chidon/index.php
+++ public_html/reports/chidon/index.php
@@ -111,6 +111,15 @@ require_once($_SERVER["DOCUMENT_ROOT"].'/header.php');
                 </div>
             </a>
         </div>
+        <h2>Booklets</h2>
+        <div id="action-links">
+            <a href="booklet_report.php<?=$debug ? "?debug=true": "";?>">
+                <div class="button">
+                    <img src="/v2/images/back-end/admin/icon_install.png" height="32" alt="tickets"/>
+                    <span class="link-text">Chidon Booklet Report</span>
+                </div>
+            </a>
+        </div>
         <?} // end admin only links ?>
         </div>
         
