ce5db15013946c69cd3febc4b081e3b80a1ea078
Menachem Hornbacher <mendelh1537@gmail.com>
2018-07-11 15:21:05 -0400

    load the user from state (move work to home)

--- src/pages/users/UserPage/UserPage.jsx
+++ src/pages/users/UserPage/UserPage.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class UserPage extends Component {
+
   render(){
-    return <p>{ this.props.match.params.id }</p>
+    const { soldiers, match } = this.props;
+    const user = soldiers.find( soldier => soldier.user_id == match.params.id )
+    return <pre>{ JSON.stringify( user, null, 2 ) }</pre>
   }
 }
 
-export default UserPage;
+const mapStateToProps = ( state ) => {
+  return {
+    ...state.soldiers,
+    current_login: state.login.current_login
+  };
+}
+
+export default connect( mapStateToProps )( UserPage );
