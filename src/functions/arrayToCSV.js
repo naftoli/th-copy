febfe1c37d01b3a90b698463dc5e93d1e67d374b
Menachem Hornbacher <mendelh1537@gmail.com>
2018-07-22 18:40:12 -0400

    add spacing and comments

--- src/functions/arrayToCSV.js
+++ src/functions/arrayToCSV.js
@@ -14,10 +14,11 @@
 const arrayToCSV = ( headers, rows, filename ) => {
   // generate the csv content
   const universalBOM = "\uFEFF";
-  let csvContent = `${headers.join(',')}\r\n`;
-
-  rows.forEach( row => { csvContent += `${row.join(',')}\n`});
+  let csvContent = `${ headers.join(',') }\n`;
+  // Add each row to the CSV content and encode it for unicode in excel
+  rows.forEach( row => { csvContent += `${row.join(',')}\n` } );
   csvContent = encodeURIComponent( universalBOM + csvContent );
+  // create and click the download link
   let link = document.createElement('a');
   link.href = `data:text/csv;charset=utf-8,${csvContent}`;
   link.target = '_blank';
