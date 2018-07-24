4eae747e27920ca43a2a37ae3eb4ac8aadabe3c0
Menachem Hornbacher <mendelh1537@gmail.com>
2018-07-24 16:22:43 -0400

    clean up as shamai requested

--- src/pages/users/UserPage/PersonalTab.jsx
+++ src/pages/users/UserPage/PersonalTab.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { LEGACY_URL } from 'components/constants';
 // components
-import { Row, Col, Input, Label } from 'reactstrap';
+import { Row, Col, Input } from 'reactstrap';
 import DatePicker from 'react-datepicker';
 import ProfilePicture from 'components/ui/ProfilePicture';
 // functions
@@ -27,8 +27,9 @@ class PersonalTab extends Component {
 
   render(){
     let { 
-      user_serial, profilePicture, barcode, gender,
-      first, last, first_he, last_he, dob, dob_he
+      user_serial, profilePicture, barcode, 
+      first, last, first_he, last_he, 
+      dob, dob_he, currentRank, gender
     } = this.props.soldier;
     dob = moment( dob );
 
@@ -36,12 +37,32 @@ class PersonalTab extends Component {
     return (
       <div id='PersonalTab'>
         <Row id='image-row'>
-          <Col xs={{ size: 4, order: 12 }}>
-            <ProfilePicture src={`${LEGACY_URL}${profilePicture}`} className='inline-profile' />
+          <Col xs='12' sm={{ size: 4, order: 12 }} lg='3'>
+            <Row>
+              <Col xs='3' sm='12'>
+                <ProfilePicture src={`${LEGACY_URL}${profilePicture}`} className='inline-profile' 
+                  rank={ currentRank.rank_ord} />
+              </Col>
+              <Col xs='9' sm='12'>
+                <label>Gender</label>
+                <div id='gender-row'>
+                  <label>
+                    <Input type='radio' name='gender' id='gender' value='M' 
+                      checked={ gender === 'M' } onChange={ this.handleChange }/>{' '}
+                    Male <i className='fas fa-male'></i>
+                  </label>
+                  <label>
+                    <Input type='radio' name='gender' id='gender' value='F'
+                      checked={ gender === 'F' } onChange={ this.handleChange }/>{' '}
+                    Female <i className='fas fa-female'></i>
+                  </label>
+                </div>
+              </Col>
+            </Row>
           </Col>
-          <Col xs='8'>
-            <h3>Serial #: {user_serial}</h3>
-            <h3>Barcode: {barcode}</h3>
+          <Col xs='12' sm='8' lg='9'>
+            <h4>Serial #: {user_serial}</h4>
+            <h4>Barcode: {barcode}</h4>
             <Row>
               <Col xs='6'>
                 <label>First Name</label>
@@ -62,37 +83,24 @@ class PersonalTab extends Component {
                 <Input id='last_he' value={last_he} onChange={this.hebrewChange}/>
               </Col>
             </Row>
+            <Row>
+              <Col xs='6'>
+                <label>Date of Birth</label>
+                <DatePicker className='form-control' dropdownMode="select" showMonthDropdown
+                  selected={moment( dob )} onChange={this.dateChange('dob')} showYearDropdown
+                  dateFormat="LL"
+                  minDate={moment().subtract( 20, 'years' )} maxDate={moment().subtract( 5, "years" )}
+                />
+              </Col>
+              <Col xs='6'dir='rtl'>
+                <label>יום הולדת</label>
+                <Input disabled value={ dob_he }/>
+              </Col>
+            </Row>
           </Col>
         </Row>
-        <Row>
-          <Col xs='6' sm='4'>
-            <label>Date of Birth</label>
-            <DatePicker className='form-control' dropdownMode="select" showMonthDropdown
-              selected={moment( dob )} onChange={this.dateChange('dob')} showYearDropdown
-              dateFormat="LL"
-              minDate={moment().subtract( 20, 'years' )} maxDate={moment().subtract( 5, "years" )}
-            />
-          </Col>
-          <Col xs='6' sm='4' dir='rtl'>
-            <label>יום הולדת</label>
-            <Input disabled value={ dob_he }/>
-          </Col>
-          <Col xs='12' sm='4'>
-            <label>Gender</label>
-            <div id='gender-row'>
-              <label>
-                <Input type='radio' name='gender' id='gender' value='M' 
-                  checked={ gender === 'M' } onChange={ this.handleChange }/>{' '}
-                Male <i className='fas fa-male'></i>
-              </label>
-              <label>
-                <Input type='radio' name='gender' id='gender' value='F'
-                  checked={ gender === 'F' } onChange={ this.handleChange }/>{' '}
-                Female <i className='fas fa-female'></i>
-              </label>
-            </div>
-          </Col>
-        </Row>
+        Address
+        Registration Info
         {/* <pre>{ JSON.stringify( this.props, null, 2 ) }</pre> */}
       </div>
     );
