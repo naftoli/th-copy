5896c82653234150e0a0a48c4979b763ee050f87
Menachem Hornbacher <mendelh1537@gmail.com>
2018-07-24 16:22:10 -0400

    add rank if passed to profile picture

--- src/components/ui/ProfilePicture.jsx
+++ src/components/ui/ProfilePicture.jsx
@@ -1,4 +1,14 @@
 import React from 'react';
+import { LEGACY_URL } from 'components/constants';
+
+const styles = { 
+  profilePicture: { position: 'relative' },
+  rankPicture: {
+    position: 'absolute',
+    right: '-8%', bottom: '-10%',
+    height: '50%'
+  }
+}
 
 const handleError = ( props ) => ( e ) => {
   e.target.src='//mashpia.com/mobile/reg/images/profile-photo-default.jpg';
@@ -8,8 +18,11 @@ const handleError = ( props ) => ( e ) => {
 }
 
 const ProfilePicture = ( props ) => (
-  <div className='profile-picture'>
-    <img { ... props } onError={ handleError( props ) } alt='profile' />
+  <div className='profile-picture' style={styles.profilePicture}>
+    <img { ...props } onError={ handleError( props ) } alt='profile' />
+    { props.rank && 
+      <img className='rank' src={`${LEGACY_URL}/mobile/img_new/ranks/${props.rank}.svg`} style={styles.rankPicture} /> 
+    }
   </div>
 );
 
