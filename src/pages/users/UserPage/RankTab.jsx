51cf9c336c48405d7fa361410c1c8203562811a1
Menachem Hornbacher <mendelh1537@gmail.com>
2018-07-26 14:48:30 -0400

    Add rank tab

---
+++ src/pages/users/UserPage/RankTab.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { LEGACY_URL } from 'components/constants';
+import { Row, Col } from 'reactstrap';
+import moment from 'moment';
+
+class RankTab extends Component {
+  render() {
+    let { currentRank } = this.props.soldier;
+    return (
+      <div id='RankTab'>
+        <h4>Rank: {currentRank.name}</h4>
+        { currentRank.ranks.map( (rank, index) => <RankRow rank={rank} key={index} />) }
+      </div>
+    )
+  }
+}
+
+const RankRow = ( { rank } ) => {
+  // create an array of empty slots
+  const medals_required = parseInt(rank.medals_required, 10);
+  const unfilled = rank.total_medals - ( rank.medals.length + parseInt(rank.medals_required, 10) );
+  const unfilled_slots = new Array( unfilled ).fill('').map( (x, index) =>
+    <div key={index}>
+      <img src={ `${LEGACY_URL}/kiosk/images/medals/holder.png` } alt='medal' style={{opacity: '.3'}}/>
+      <p>(#{ medals_required + rank.medals.length + index + 1 })</p>
+    </div>
+  );
+
+  return (
+    <Row className='RankRow'>
+      <Col xs='3' sm='2' className='RankRow-rank'>
+        <p>{ rank.rank_name }</p>
+        <p>({ moment(rank.date_promoted).format('l') })</p>
+        <img src={`${LEGACY_URL}/mobile/img_new/ranks/${rank.rank_ord}.svg`} alt='rank' />
+      </Col>
+      <Col xs='9' sm='10' className='RankRow-medals'>
+        { rank.medals && rank.medals.map( (medal, index) => 
+          <div key={index}>
+            <img src={ `${LEGACY_URL}${medal.photo}` } alt='medal'/>
+            <p>{ moment(medal.date_awarded).format('l') }</p>
+            <p>{medal.date_awarded_he}</p>
+          </div>
+        ) }
+        { unfilled_slots }
+      </Col>
+    </Row>
+  )
+}
+
+export default RankTab
