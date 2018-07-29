a8c01a7090654418c84936679f4d8826035289de
Menachem Hornbacher <mendelh1537@gmail.com>
2018-07-29 11:52:03 -0400

    move styles to stylesheet to support hover and before styles

--- src/components/ui/ProfilePicture.jsx
+++ src/components/ui/ProfilePicture.jsx
@@ -1,28 +1,20 @@
 import React from 'react';
 import { LEGACY_URL } from 'components/constants';
-
-const styles = { 
-  profilePicture: { position: 'relative' },
-  rankPicture: {
-    position: 'absolute',
-    right: '-8%', bottom: '-10%',
-    height: '50%'
-  }
-}
+import classnames from 'classnames';
+import './styles/ProfilePicture.scss'
 
 const handleError = ( props ) => ( e ) => {
   e.target.src='//mashpia.com/mobile/reg/images/profile-photo-default.jpg';
-  if ( props.onError ) {
-    props.onError( e );
-  }
+  if ( props.onError ) props.onError( e );
 }
 
 const ProfilePicture = ( props ) => (
-  <div className='profile-picture' style={styles.profilePicture}>
-    <img { ...props } onError={ handleError( props ) } alt='profile' />
+  <div className={classnames( `profile-picture`, { editable: !!props.onClick } )}>
+    <img { ...props } className={ classnames( props.className, 'profile-img' ) } 
+      onError={ handleError( props ) } alt='profile' />
     { props.rank && 
-      <img className='rank' src={`${LEGACY_URL}/mobile/img_new/ranks/${props.rank}.svg`} 
-        style={styles.rankPicture} alt='rank' /> 
+      <img src={`${LEGACY_URL}/mobile/img_new/ranks/${props.rank}.svg`} 
+        className='rank' alt='rank' />
     }
   </div>
 );
