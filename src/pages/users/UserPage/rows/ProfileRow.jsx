fed262a0c20ec57aa55768ba14c503115713551a
Menachem Hornbacher <mendelh1537@gmail.com>
2018-07-30 15:14:43 -0400

    move profile picture row to new component

---
+++ src/pages/users/UserPage/rows/ProfileRow.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { LEGACY_URL } from 'components/constants';
+// components
+import { Row, Col } from 'reactstrap';
+import ProfilePicture from 'components/ui/ProfilePicture';
+import Radio from 'components/ui/Radio';
+
+const ProfileRow = ({ soldier, onChange, toggle }) => {
+
+  const genderChange = ( event ) => {
+    let { school_type_id } = soldier;
+    // format the data
+    if ( ![2, 12, 3, 13].includes(school_type_id) ) {
+      school_type_id = event.target.value === 'M' ? 2 : 3
+    } else {
+      school_type_id = school_type_id + ( event.target.value === 'M' ? -1 : 1 )
+    }
+    // format the data
+    onChange({
+      [event.target.id]: event.target.value, school_type_id
+    });
+  }
+
+  const { profilePicture, currentRank, gender  } = soldier;
+  const profile_picture = profilePicture ? `${LEGACY_URL}${profilePicture}` : '';
+  const rank = currentRank ? currentRank.rank : undefined;
+
+  return (
+    <Row>
+      <Col xs='3' sm='12'>
+        <ProfilePicture src={ profile_picture } className='inline-profile' 
+          rank={ rank } onClick={ toggle } />
+      </Col>
+      <Col xs='9' sm='12'>
+        <label>Gender</label>
+        <div id='gender-row'>
+          <Radio type='radio' name='gender' id='gender' value='M' 
+              checked={ gender === 'M' } onChange={ genderChange }>
+            Male <i className='fas fa-male'></i>
+          </Radio>
+          <Radio type='radio' name='gender' id='gender' value='F'
+              checked={ gender === 'F' } onChange={ genderChange }>
+            Female <i className='fas fa-female'></i>
+          </Radio>
+        </div>
+      </Col>
+    </Row>
+  );
+}
+
+export default ProfileRow;
--- src/pages/users/UserPage/tabs/PersonalTab.jsx
+++ src/pages/users/UserPage/tabs/PersonalTab.jsx
@@ -2,10 +2,8 @@ import React, { Component } from 'react';
 import { LEGACY_URL } from 'components/constants';
 // components
 import { Row, Col } from 'reactstrap';
-import ProfilePicture from 'components/ui/ProfilePicture';
-import Radio from 'components/ui/Radio';
 import CropperModal from 'components/modals/CropperModal';
-import { AddressRow, RegistrationRow, NameRow, DobRow } from '../rows';
+import { ProfileRow, NameRow, DobRow, AddressRow, RegistrationRow } from '../rows';
 
 class PersonalTab extends Component {
   // initial state
@@ -13,14 +11,6 @@ class PersonalTab extends Component {
   // handle updates from events
   handleChange = ( event ) => {
     this.props.handleChange( { [event.target.id]: event.target.value } );
-  }
-  // handle the gender change
-  genderChange = ( event ) => {
-    const { school_type_id } = this.props.soldier;
-    this.props.handleChange({
-      [event.target.id]: event.target.value,
-      school_type_id: school_type_id + ( event.target.value === 'M' ? -1 : 1 )
-    });
   }
   // format dates
   dateChange = ( name ) => ( date ) => {
@@ -32,6 +22,7 @@ class PersonalTab extends Component {
   toggle = () => {
     this.setState({ cropperModalShow: !this.state.cropperModalShow });
   }
+  // close the modal and update the profile picture
   updateProfile = ( formData ) => {
     this.toggle();
     this.props.updateProfile( formData );
@@ -39,40 +30,24 @@ class PersonalTab extends Component {
 
   render(){
     const soldier = this.props.soldier;
-    let { user_serial, barcode, profilePicture, currentRank, gender  } = soldier;
+    let { user_serial, barcode, profilePicture  } = soldier;
     const profile_picture = profilePicture ? `${LEGACY_URL}${profilePicture}` : '';
-    const rank = currentRank ? currentRank.rank : undefined;
     // render form
     return (
       <div id='PersonalTab'>
         <Row id='image-row'>
-          <Col xs='12' sm={{ size: 4, order: 12 }} lg='3' xl='2'>
-            <Row>
-              <Col xs='3' sm='12'>
-                <ProfilePicture src={ profile_picture } className='inline-profile' 
-                  rank={ rank } onClick={ this.toggle } />
-              </Col>
-              <Col xs='9' sm='12'>
-                <label>Gender</label>
-                <div id='gender-row'>
-                  <Radio type='radio' name='gender' id='gender' value='M' 
-                      checked={ gender === 'M' } onChange={ this.genderChange }>
-                    Male <i className='fas fa-male'></i>
-                  </Radio>
-                  <Radio type='radio' name='gender' id='gender' value='F'
-                      checked={ gender === 'F' } onChange={ this.genderChange }>
-                    Female <i className='fas fa-female'></i>
-                  </Radio>
-                </div>
-              </Col>
-            </Row>
-          </Col>
-          <Col xs='12' sm='8' lg='9' xl='10'>
+          <Col xs={{ size: 12, order: 12 }} sm='8' lg='9' xl='10'>
             { user_serial && <h4>Serial #: {user_serial}</h4> }
             { barcode && <h4>Barcode: {barcode}</h4> }
 
             <NameRow soldier={ soldier } onChange={ this.handleChange } />
-            <DobRow soldier={ soldier } onChange={ this.dateChange('dob') } show_he />
+
+            <DobRow soldier={ soldier } show_he onChange={ this.dateChange('dob') } />
+
+          </Col>
+          <Col xs='12' sm={{ size: 4, order: 12 }} lg='3' xl='2'>
+            <ProfileRow soldier={ soldier } toggle={ this.toggle } 
+              onChange={ this.props.handleChange } />
           </Col>
         </Row>
 
@@ -82,6 +57,7 @@ class PersonalTab extends Component {
 
         <CropperModal isOpen={ this.state.cropperModalShow } src={ profile_picture } 
           toggle={ this.toggle } uploadImage={ this.updateProfile } />
+
       </div>
     );
   }
