b8732d21f6cace75949e4adeb67aa757e22fe2d5
Menachem Hornbacher <mendelh1537@gmail.com>
2018-07-30 15:14:15 -0400

    support `required`

--- src/pages/users/UserPage/rows/NameRow.jsx
+++ src/pages/users/UserPage/rows/NameRow.jsx
@@ -4,7 +4,7 @@ import { Row, Col, Input } from 'reactstrap';
 // functions
 import { toHebrew } from 'functions/utils';
 
-const RegistrationRow = ( { soldier, onChange } ) => {
+const RegistrationRow = ( { soldier, onChange, required } ) => {
   const { first, last, first_he, last_he } = soldier;
   // change the hebrew text
   const hebrewChange = ( event ) => {
@@ -16,19 +16,19 @@ const RegistrationRow = ( { soldier, onChange } ) => {
     <Row>
       <Col xs='6'>
         <label>First Name</label>
-        <Input id='first' value={ first } onChange={ onChange } />
+        <Input id='first' value={ first } onChange={ onChange } required={ required }/>
       </Col>
       <Col xs='6'>
         <label>Last Name</label>
-        <Input id='last' value={ last } onChange={ onChange } />
+        <Input id='last' value={ last } onChange={ onChange } required={ required }/>
       </Col>
       <Col xs='6' dir='rtl'>
         <label>שם פרטי (First Name)</label>
-        <Input id='first_he' value={ first_he } onChange={ hebrewChange }/>
+        <Input id='first_he' value={ first_he } onChange={ hebrewChange } required={ required }/>
       </Col>
       <Col xs='6' dir='rtl'>
         <label>שם משפחה (Last Name)</label>
-        <Input id='last_he' value={ last_he } onChange={ hebrewChange }/>
+        <Input id='last_he' value={ last_he } onChange={ hebrewChange } required={ required }/>
       </Col>
     </Row>
   );
