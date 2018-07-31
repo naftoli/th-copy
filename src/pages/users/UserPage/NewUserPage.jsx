68ad582b6fbeb2a02c5741cfa9bbbdb70ff3fba5
Menachem Hornbacher <mendelh1537@gmail.com>
2018-07-31 18:05:59 -0400

    Add platoon and base boxes to user page

--- src/pages/users/UserPage/NewUserPage.jsx
+++ src/pages/users/UserPage/NewUserPage.jsx
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 // components
 import { Row, Col, Button } from 'reactstrap';
 import { ProfileRow, NameRow, DobRow, AddressRow } from './rows';
+import PlatoonSelect from 'components/selects/PlatoonSelect';
 import CropperModal from 'components/modals/CropperModal';
+import BaseSelect from 'components/selects/BaseSelect';
 import Select from 'react-select';
 // functions
-import { connect } from 'react-redux';
-import { missionTypeOptions, findOption } from './tabs/SettingsTab';
-import { setTitle } from 'functions/utils';
+import { missionTypeOptions, findOption } from 'functions/selects';
 import { uploadProfile } from 'store/soldiers/operations';
+import { loginChanged } from 'functions/login';
+import { setTitle } from 'functions/utils';
+import { connect } from 'react-redux';
 
 class NewUserPage extends Component {
   // initial state
@@ -16,13 +19,27 @@ class NewUserPage extends Component {
     cropperModalShow: false, 
     soldier: {
       first: '', last: '', first_he: '', last_he: '', 
-      user_address1: '', user_city: '', user_state: '',
-      user_phone: '', user_postal: '', user_country: ''
+      user_city: '', user_state: '', user_phone: '', 
+      user_postal: '', user_country: ''
     }
   }
   // lifecylcle - initial component mount
   componentDidMount() { 
     setTitle( 'Create New Soldier' );
+    this.setupSoldier();
+  }
+  componentDidUpdate( prevProps ) {
+    if ( loginChanged( this.props.current_login, prevProps.current_login ) ) {
+      this.setupSoldier();
+    }
+  }
+  // setup the school_id/class_id on the soldier
+  setupSoldier = () => {
+    const { code, id } = this.props.current_login;
+    if ( code === 'BC' )
+      return this.setState({ soldier: { ...this.state.soldier, school_id: id } });
+    if ( code === 'TEACHER' )
+      return this.setState({ soldier: { ...this.state.soldier, class_id: id } });
   }
   // edit profile
   toggle = () => {
@@ -62,11 +79,31 @@ class NewUserPage extends Component {
   }
 
   render() {
-    const { current_login } = this.props;
+    const { code } = this.props.current_login;
     const { soldier, cropperModalShow } = this.state;
-    const { gender, school_type_id } = soldier;
+    const { gender, school_type_id, school_id, class_id } = soldier;
     // generate the mission_type options
     let mission_type_options = missionTypeOptions( gender );
+
+    let baseSelect, platoonSelect;
+    
+    if ( ['HQ', 'CKIDS-ADMIN'].includes( code ) ) {
+      baseSelect = (
+        <Col xs='6'>
+          <label>Base</label>
+          <BaseSelect value={ school_id } onChange={this.handleSelectChange('school_id')} />
+        </Col>
+      );
+    }
+
+    if ( ['HQ', 'CKIDS-ADMIN', 'BC' ].includes( code ) ) {
+      platoonSelect = (
+        <Col xs={ code === 'BC' ? 12 : 6 }>
+          <label>Platoon</label>
+          <PlatoonSelect school_id={ school_id } value={ class_id } />
+        </Col>
+      );
+    }
 
     return (
       <form id='NewUserPage' onSubmit={this.submit}>
@@ -80,10 +117,11 @@ class NewUserPage extends Component {
               <Col xs='6'>
                 <label>Mission Type</label>
                 <Select options={mission_type_options} onChange={this.handleSelectChange('school_type_id')}
-                  value={findOption( mission_type_options, school_type_id )}/>
+                  value={findOption( mission_type_options, school_type_id )} openMenuOnFocus />
               </Col>
+              { baseSelect }
+              { platoonSelect }
             </DobRow>
-
           </Col>
           <Col xs='12' sm={{ size: 4, order: 12 }} lg='3' xl='2'>
             <ProfileRow soldier={ soldier } toggle={ this.toggle } 
