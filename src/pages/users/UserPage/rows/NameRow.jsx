97484af60b86d4150a446df88acd7ba841d8ac61
Menachem Hornbacher <mendelh1537@gmail.com>
2018-07-31 19:02:48 -0400

    add input validation

--- src/pages/users/UserPage/rows/NameRow.jsx
+++ src/pages/users/UserPage/rows/NameRow.jsx
@@ -16,19 +16,27 @@ const RegistrationRow = ( { soldier, onChange, required } ) => {
     <Row>
       <Col xs='6'>
         <label>First Name</label>
-        <Input id='first' value={ first } onChange={ onChange } required={ required }/>
+        <Input id='first' value={ first } onChange={ onChange } required={ required } 
+          pattern='^[a-zA-Z\s]{3,}$' title="Three or more letters"/>
+        <div className='invalid-message'>Please enter 3 or more letters</div>
       </Col>
       <Col xs='6'>
         <label>Last Name</label>
-        <Input id='last' value={ last } onChange={ onChange } required={ required }/>
+        <Input id='last' value={ last } onChange={ onChange } required={ required }
+          pattern='^[a-zA-Z\s]{3,}$' title="Three or more letters"/>
+        <div className='invalid-message'>Please enter 3 or more letters</div>
       </Col>
       <Col xs='6' dir='rtl'>
         <label>שם פרטי (First Name)</label>
-        <Input id='first_he' value={ first_he } onChange={ hebrewChange } required={ required }/>
+        <Input id='first_he' value={ first_he } onChange={ hebrewChange } required={ required }
+          pattern='^[א-ת\s]{3,}$' title="Three or more Hebrew letters"/>
+        <div className='invalid-message'>Please enter 3 or more <em>Hebrew</em> letters</div>
       </Col>
       <Col xs='6' dir='rtl'>
         <label>שם משפחה (Last Name)</label>
-        <Input id='last_he' value={ last_he } onChange={ hebrewChange } required={ required }/>
+        <Input id='last_he' value={ last_he } onChange={ hebrewChange } required={ required }
+          pattern='^[א-ת\s]{3,}$' title="Three or more Hebrew letters"/>
+        <div className='invalid-message'>Please enter 3 or more <em>Hebrew</em> letters</div>
       </Col>
     </Row>
   );
