d57b45da95109f70ae8871580ad5452884c930c1
unknown <naftoli@tzivoshashem.org>
2018-08-01 15:57:57 -0400

    more testing
    Former-commit-id: 0b221cdc06c16328689bf819f2955c70a1335d76

--- public_html/chabad_org/auth.php
+++ public_html/chabad_org/auth.php
@@ -8,7 +8,7 @@ $shliach = new ChabadShliach( $_POST['key'] );
 $success = false;
 if ( $shliach->authenticate() ) {
       if ( $shliach->setPersonalInfo() ) {
-            $shliach->setCenters( array( 117539 ) );
+            $shliach->setCenters( array( 117552 ) );
             if ( $shliach->setMosdos() ) {
                   $success = true;
                   // do something with info
