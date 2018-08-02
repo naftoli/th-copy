e0a250ee0288661c61252bae33cbf05dae922c00
unknown <naftoli@tzivoshashem.org>
2018-08-01 22:27:51 -0400

    more debugging
    Former-commit-id: 501d145248f96ee8322eef58e0e1923e2c23b77c

--- public_html/chabad_org/testAPI.php
+++ public_html/chabad_org/testAPI.php
@@ -3,13 +3,14 @@ ini_set('display_errors',1);
 header('Content-type: application/json');
 require 'classes/ChabadShliach.php';
 $shliach = new ChabadShliach( $_POST['key'] );
+$mosadID = $_POST['mosad'];
 
 // make sure we get all needed info
 $success = false;
 if ( $shliach->authenticate() ) {
     $shliach->setDebug( true );
     if ( $shliach->setPersonalInfo() ) {
-        $shliach->setCenters( array( 117552 ) );
+        $shliach->setCenters( array( $mosadID ) );
         if ( $shliach->setMosdos() ) {
                 $success = true;
                 // do something with info
