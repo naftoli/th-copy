0bf096b3f241d274652548b80abb006daed8d849
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-02 17:36:04 -0400

    updTes to our callout

--- src/components/ui/Callout.jsx
+++ src/components/ui/Callout.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Alert } from 'reactstrap';
+import classnames from 'classnames';
 import './styles/Callout.scss';
 
-const Callout = ( { icon, title, children, color='primary' } ) => {
+const Callout = ( { icon = 'fas fa-info-circle', title, children, color='primary' } ) => {
+  const classNames = classnames('th-callout', {
+    'th-callout-icon': !!icon
+  });
   return (
-    <Alert className='th-callout' color={color}>
+    <Alert className={classNames} color={color} transition={{ baseClass: '', timeout: 0 }}>
       { icon && <i className={ icon }></i> }
       { title && <h4>{ title }</h4> }
       { children }
