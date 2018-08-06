24e4761dbfb08624e5d351a5d8967a0e91b43be4
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-06 13:48:12 -0400

    basic registerSoldiers function

--- src/store/soldiers/registration/operations.js
+++ src/store/soldiers/registration/operations.js
@@ -1,13 +1,15 @@
+import API from 'api/api';
+import { setRegistrationSoldiers, setLoading } from '../actions';
+import { createNotifcation, updateNotifcation } from 'functions/notifications';
+
 /**
  * This file updates the soldiers store sections related to soldier registration
  */
-import API from 'api/api';
-import { setRegistrationSoldiers, setLoading } from '../actions';
 
 // get all soldiers
 export const getSoldiers = () => dispatch => {
   dispatch( setLoading( true ) );
-  return API.get( '/registration/users.php' )
+  return API.get( '/registration/users' )
   .then( response => {
     dispatch( setLoading( false ) );
     // go to catch if request has an issue
@@ -20,4 +22,19 @@ export const getSoldiers = () => dispatch => {
     dispatch( setLoading( false ) );
     return Promise.reject( error );
   });
+}
+
+export const registerSoldiers = ( user_ids, payment, total ) => dispatch => {
+  const toast_id = createNotifcation(`Registering ${user_ids.length} Soldiers..`);
+  const postData = { user_ids, payment, total };
+  return API.post( '/registration/users', postData )
+  .then( response => {
+    updateNotifcation(
+      toast_id, `${user_ids.length} Soldiers Registered for $${total}!`, 
+      response.message, response.success 
+    );
+    debugger;
+  }).catch( error => {
+    updateNotifcation( toast_id, '', error.message, false );
+  })
 }
