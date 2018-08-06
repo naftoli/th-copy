1718fe927aa72a9c71a2dac13622c1087152f396
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-06 14:44:40 -0400

    add customer profiles to schools
    Former-commit-id: 95b9a5fbad0955f3f9b3e061267f53377cb4b45f

--- public_html/api/models/School.php
+++ public_html/api/models/School.php
@@ -3,9 +3,9 @@ include_once( __DIR__ . '/traits/BuildModel.php' );
 
 class School extends ActiveRecord\Model implements JsonSerializable {
     use traits\BuildModel;
-    
+    private $customer_profile;
     // relationships
-    static $has_many = [ 
+    static $has_many = [
         [ 'school_registrations' ], 
         [ 'platoons', 'order' => 'class_grade, class_sub', ], 
         [ 'users' ] 
@@ -35,6 +35,58 @@ class School extends ActiveRecord\Model implements JsonSerializable {
 
     public function logoPath(){
         return "/schoolLogos/$this->logo";
+    }
+
+    //********************************** PAYMENTS **********************************/
+    /**
+     * customerProfile
+     * 
+     * Attmpts to return customer profile from API, if not found returns false
+     * If optional $payment_profile array provided it will attempt to create a payment profile and return it.
+     *  If it encounters an error while preforming creation it will return the array from the API
+     *
+     * @param array $payment_profile
+     * @return CustomerProfile/boolean/array
+     */
+    public function customerProfile(){
+        if ( $this->authorize_customer_profile_id && !$this->customer_profile ) {
+            $this->customer_profile = new classes\authorize\CustomerProfile(
+                $this->authorize_customer_profile_id
+            );
+        }
+        return $this->customer_profile;
+    }
+    public function createPaymentProfile( $payment_info, $email='example@example.com' ) { 
+        // if we do not have a customer profile
+        if ( !$this->customerProfile() instanceof classes\authorize\CustomerProfile ) {
+            // create the account
+            $payment_profile = classes\authorize\PaymentProfile::createBasicArray(
+                $payment_info['cc-number'], $payment_info['cc-exp'], $payment_info['x_card_code']
+            );
+            $this->customer_profile = classes\authorize\CustomerProfile::create(
+                "CTH_".$this->school_id, $email, false, $payment_profile
+            );
+            // handle errors
+            if ( !$this->customer_profile instanceof classes\authorize\CustomerProfile )
+                return $this->customer_profile["message"];
+            // save the valid information
+            $this->authorize_customer_profile_id = $this->customer_profile->customerProfileId;
+            $this->save();
+            // return a new PaymentProfile instance
+            return new classes\authorize\PaymentProfile(
+                $this->customer_profile->paymentProfiles[0]['customerPaymentProfileId'],
+                $this->customer_profile->customerProfileId
+            );
+        // if we do have a customer profile
+        } else {
+            $payment_profile = classes\authorize\PaymentProfile::create(
+                $payment_info['cc-number'], $payment_info['cc-exp'], $payment_info['x_card_code'],
+                $this->authorize_customer_profile_id
+            );
+            if ( !($payment_profile instanceof classes\authorize\PaymentProfile) )
+                return $payment_profile['messages']['message'][0]['text'];
+            return $payment_profile;
+        }
     }
 
     // ******************************* SERIALIZERS *******************************
