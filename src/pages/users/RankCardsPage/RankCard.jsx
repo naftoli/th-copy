52ecd2a6155f795219ff5105c402041a68c8dfb9
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-07 12:57:17 -0400

    split rank card files

--- src/pages/users/RankCardsPage/RankCard.jsx
+++ src/pages/users/RankCardsPage/RankCard.jsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { LEGACY_URL } from 'components/constants';
 import PropTypes from 'prop-types';
 // import classnames from 'classnames';
+import './RankCard.scss';
 
 class RankCard extends Component {
   
   static propTypes = {
     user: PropTypes.shape({
       rank: PropTypes.string,
+      rank_ord: PropTypes.number,
       user_serial: PropTypes.string, 
       first: PropTypes.string,
       last: PropTypes.string,
@@ -20,39 +22,41 @@ class RankCard extends Component {
   render() {
     const { 
       rank, user_serial, first, last, school_logo, 
-      profilePicture, barcode,
+      profilePicture, barcode, rank_ord
     } = this.props.user
     return (
-      <div className={`print RankCard ${rank}`}>
-        <div className='top'>
-          This card entitles the cardholder to TH privileges with the { rank } rank
-        </div>
-        <div className='sig'>
-          <p>Authorized Signature</p>
-          <h1 className='rank-row'>
-            <span className='rank'>{rank} </span>{first} {last}
-          </h1>
-          <p>Serial #: <strong>{ user_serial }</strong></p>
-        </div>
-        <div className='info'>
-          <div className='logo'>
-            <img src={ LEGACY_URL + school_logo } alt='school_logo' />
+      <div className='print RankCard'>
+        <div className={`rank-card rank-${rank_ord}`}>
+          <div className='top'>
+            This card entitles the cardholder to TH privileges with the { rank } rank
           </div>
-          <div className='base'>
-            <p>BASE #614199</p>
-            <p><strong>Avrohom Academy</strong></p>
-            <p>Brooklyn, NY <strong>Platoon:</strong></p>
+          <div className='sig'>
+            <p>Authorized Signature</p>
+            <h1 className='rank-row'>
+              <span className='rank'>{rank} </span>{first} {last}
+            </h1>
+            <p>Serial #: <strong>{ user_serial }</strong></p>
+          </div>
+          <div className='info'>
+            <div className='logo'>
+              <img src={ LEGACY_URL + school_logo } alt='school_logo' />
             </div>
-          <div className='photo'>
-            <img src={ LEGACY_URL + profilePicture }/>
+            <div className='base'>
+              <p>BASE #614199</p>
+              <p><strong>Avrohom Academy</strong></p>
+              <p>Brooklyn, NY <strong>Platoon:</strong></p>
+              </div>
+            <div className='photo'>
+              <img src={ LEGACY_URL + profilePicture } alt='profile'/>
+            </div>
           </div>
-        </div>
-        <div className='barcode'>
-          <div className='code'>
-            <img src={`${LEGACY_URL}/barcode.php/${barcode}`}/>
-            { barcode }
+          <div className='barcode'>
+            <div className='code'>
+              <img src={`${LEGACY_URL}/barcode.php/${barcode}`} alt='barcode'/>
+              { barcode }
+            </div>
+            <div className='stats'></div>
           </div>
-          <div className='stats'></div>
         </div>
       </div>
     )
--- src/pages/users/RankCardsPage/RankCardsPage.jsx
+++ src/pages/users/RankCardsPage/RankCardsPage.jsx
@@ -25,14 +25,41 @@ export class RegistrationPage extends Component {
 
   render() {
     const users = [
-      { rank: 'Private', user_serial: '7761797', first: 'No', last: 'Name', barcode: 37170565993180946000,
-        school_logo: '/file_view.php?id=2275195517', profilePicture: '/mobile/reg/img/20180525193812.png' }
+      { rank: 'Private', user_serial: '7761797', first: 'No', last: 'Name', barcode: 37170565993180946000, rank_ord: 1,
+        school_logo: '/file_view.php?id=2275195517', profilePicture: '/mobile/reg/img/20180525193812.png' },
+      { rank: 'Sergeant', user_serial: '7761797', first: 'No', last: 'Name', barcode: 37170565993180946000, rank_ord: 2,
+        school_logo: '/file_view.php?id=2275195517', profilePicture: '/mobile/reg/img/20180525193812.png' },
+      { rank: 'Sergeant Major', user_serial: '7761797', first: 'No', last: 'Name', barcode: 37170565993180946000, rank_ord: 3,
+        school_logo: '/file_view.php?id=2275195517', profilePicture: '/mobile/reg/img/20180525193812.png' },
+      { rank: 'Second Lieutenant', user_serial: '7761797', first: 'No', last: 'Name', barcode: 37170565993180946000, rank_ord: 4,
+        school_logo: '/file_view.php?id=2275195517', profilePicture: '/mobile/reg/img/20180525193812.png' },
+      { rank: 'First Lieutenant', user_serial: '7761797', first: 'No', last: 'Name', barcode: 37170565993180946000, rank_ord: 5,
+        school_logo: '/file_view.php?id=2275195517', profilePicture: '/mobile/reg/img/20180525193812.png' },
+      { rank: 'Captain', user_serial: '7761797', first: 'No', last: 'Name', barcode: 37170565993180946000, rank_ord: 6,
+        school_logo: '/file_view.php?id=2275195517', profilePicture: '/mobile/reg/img/20180525193812.png' },
+      { rank: 'Major', user_serial: '7761797', first: 'No', last: 'Name', barcode: 37170565993180946000, rank_ord: 7,
+        school_logo: '/file_view.php?id=2275195517', profilePicture: '/mobile/reg/img/20180525193812.png' },
+      { rank: 'Colonel', user_serial: '7761797', first: 'No', last: 'Name', barcode: 37170565993180946000, rank_ord: 8,
+        school_logo: '/file_view.php?id=2275195517', profilePicture: '/mobile/reg/img/20180525193812.png' },
+      { rank: 'General', user_serial: '7761797', first: 'No', last: 'Name', barcode: 37170565993180946000, rank_ord: 9,
+        school_logo: '/file_view.php?id=2275195517', profilePicture: '/mobile/reg/img/20180525193812.png' },
+      { rank: '1* General', user_serial: '7761797', first: 'No', last: 'Name', barcode: 37170565993180946000, rank_ord: 10,
+        school_logo: '/file_view.php?id=2275195517', profilePicture: '/mobile/reg/img/20180525193812.png' },
+      { rank: '2* General', user_serial: '7761797', first: 'No', last: 'Name', barcode: 37170565993180946000, rank_ord: 11,
+        school_logo: '/file_view.php?id=2275195517', profilePicture: '/mobile/reg/img/20180525193812.png' },
+      { rank: '3* General', user_serial: '7761797', first: 'No', last: 'Name', barcode: 37170565993180946000, rank_ord: 12,
+        school_logo: '/file_view.php?id=2275195517', profilePicture: '/mobile/reg/img/20180525193812.png' },
+      { rank: '4* General', user_serial: '7761797', first: 'No', last: 'Name', barcode: 37170565993180946000, rank_ord: 13,
+        school_logo: '/file_view.php?id=2275195517', profilePicture: '/mobile/reg/img/20180525193812.png' },
+      { rank: '5* General', user_serial: '7761797', first: 'No', last: 'Name', barcode: 37170565993180946000, rank_ord: 14,
+        school_logo: '/file_view.php?id=2275195517', profilePicture: '/mobile/reg/img/20180525193812.png' },
+
     ]
     return (
       <div id='RankCardsPage'>
         <Callout title='Soldier Rank Cards' className='no-print'>
           Please note that Headquarters will likely print and ship permenent ID cards to you.<br/>
-          If you wish to print your own please make sure you can print 3.5in x 2in ( 8.89cm x 5.08cm)
+          If you wish to print your own please make sure you can print 3.37in x 2.13in ( 8.56cm x 5.41cm )
         </Callout>
         <Row className='no-print'>
           <Col xs='12'>
