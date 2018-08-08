0f344b7a8402c44dd35a6b90621d803979e21367
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-08 13:16:14 -0400

    finish the rank card

--- src/pages/users/RankCardsPage/RankCard.jsx
+++ src/pages/users/RankCardsPage/RankCard.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
-import { LEGACY_URL } from 'components/constants';
 import PropTypes from 'prop-types';
+// components
+import { ProfilePicture, Checkbox } from 'components/ui';
+// constants and functions
+import { LEGACY_URL } from 'components/constants';
 import classnames from 'classnames';
-import { ProfilePicture } from 'components/ui';
+// styles
 import './RankCard.scss';
 
 class RankCard extends Component {
   
   static propTypes = {
     permanent: PropTypes.bool,
+    onChange: PropTypes.func,
     user: PropTypes.shape({
       rank: PropTypes.string,
       rank_ord: PropTypes.number,
@@ -24,13 +28,19 @@ class RankCard extends Component {
   }
 
   static defaultProps = {
-    permanent: true
+    permanent: true,
+    showPrinted: false,
   }
 
+  onChange = (user_id, rank_ord) => ({ target }) => {
+    this.props.onChange( user_id, rank_ord, target.checked );
+  };
+
   render() {
+    const { showPrinted } = this.props;
     const { 
-      rank, user_serial, first, last, first_he, last_he,
-      profilePicture, barcode, rank_ord, platoon,
+      rank, user_id, user_serial, first, last, first_he, last_he,
+      profilePicture, barcode, rank_ord, platoon, printed,
       school_logo, school_name, member_since, valid_utill
     } = this.props.user;
     // classnames for cards. See SCSS for what they do
@@ -38,9 +48,16 @@ class RankCard extends Component {
 
     return (
       <div className={classNames}>
+        { showPrinted && 
+          <div className='no-print'>
+            <Checkbox checked={!!printed} onChange={ this.onChange(user_id, rank_ord) }>
+              Printed
+            </Checkbox>
+          </div>
+        }
         <div className={`rank-card rank-${rank_ord}`}>
           <div className='top'>
-            This card entitles the cardholder to TH privileges with the { rank } rank
+            <p>This card entitles the cardholder to TH privileges with the { rank } rank</p>
           </div>
           <div className='sig'>
             <p>Authorized Signature</p>
