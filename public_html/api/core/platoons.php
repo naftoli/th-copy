bde4fa4008399e4c618ad5eec3162c1065058766
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-08 17:36:38 -0400

    add show route and update response
    Former-commit-id: 28ad34a0c1c180ed85197db12fa6575d5cd43ab0

--- public_html/api/core/platoons.php
+++ public_html/api/core/platoons.php
@@ -11,21 +11,24 @@ class PlatoonRouter {
         // limit based on admin type
         $login = $current_user->login;
         if ( $login['code'] === 'HQ' && !isset( $_GET['school_id'] ) ) {
-            $filters[] = 'schools.test_school = 0';
+            $filters[] = 's.test_school = 0';
         } else if ( $login['code'] === 'CKIDS-ADMIN' && !isset( $_GET['school_id'] ) ) {
-            $filters[] = 'schools.ckids = 1';
+            $filters[] = 's.ckids = 1';
         } else if ( $login['code'] === 'BC' ) {
-            $filters[] = 'classes.school_id = ?';   $params[] = $login['id'];
+            $filters[] = 'c.school_id = ?';   $params[] = $login['id'];
         } else if ( $login['code'] === 'TEACHER' ) {
-            $filters[] = 'classes.class_id = ?';   $params[] = $login['id'];
+            $filters[] = 'c.class_id = ?';   $params[] = $login['id'];
         } else if ( isset( $_GET['school_id'] ) ) {
-            $filters[] = 'classes.school_id = ?';   $params[] = $_GET['school_id'];
+            $filters[] = 'c.school_id = ?';   $params[] = $_GET['school_id'];
         } else { json_error( 'Invalid request. Please select a Base.'); }
         // combine the filters
         $filters = 'WHERE ' . implode( ' AND ', $filters );
         // generate the SQL
-        $sql = "SELECT class_id, class_grade, class_sub, school_id, school_name FROM classes JOIN schools USING (school_id) "
-            ." $filters ORDER BY school_name, class_grade, class_sub ";
+        $sql = "SELECT class_id, class_grade, class_sub, s.school_id, school_name, "
+            ."class_teacher as teacher, c.cell, c.email, COUNT(*) as soldier_count, staff_count "
+            ."FROM classes c JOIN schools s USING (school_id) JOIN users u USING ( class_id ) "
+            ."LEFT JOIN ( SELECT count(*) as staff_count, id FROM admin_auths WHERE auth='class' GROUP BY id ) s ON s.id = c.class_id "
+            ." $filters GROUP BY class_id ORDER BY school_name, class_grade, class_sub ";
         $query = $pdo->prepare( $sql );
         $query->execute( $params );
 
@@ -37,9 +40,16 @@ class PlatoonRouter {
             ]) )->name();
             $platoon['class_id']  = intval( $platoon['class_id' ] );
             $platoon['school_id'] = intval( $platoon['school_id'] );
+            $platoon['soldier_count'] = intval( $platoon['soldier_count'] );
+            $platoon['staff_count'] = intval( $platoon['staff_count'] );
             $platoons[] = $platoon;
         }
         json_response( $platoons );
+    }
+
+    public function show( $id ) {
+        $platoon = Platoon::find( $id );
+        json_response( $platoon );
     }
 }
 
