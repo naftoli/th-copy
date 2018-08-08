001af9851461314d02e7032f1eed5dc76b2fd9f3
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-07 20:10:11 -0400

    finally finish Rank Card for now

--- src/pages/users/RankCardsPage/RankCard.jsx
+++ src/pages/users/RankCardsPage/RankCard.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { LEGACY_URL } from 'components/constants';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import { ProfilePicture } from 'components/ui';
 import './RankCard.scss';
 
 class RankCard extends Component {
@@ -14,9 +15,10 @@ class RankCard extends Component {
       user_serial: PropTypes.string, 
       first: PropTypes.string,
       last: PropTypes.string,
+      school_name: PropTypes.string,
       school_logo: PropTypes.string,
       profilePicture: PropTypes.string,
-      barcode: PropTypes.number,
+      barcode: PropTypes.string,
       platoon: PropTypes.string
     })
   }
@@ -27,8 +29,9 @@ class RankCard extends Component {
 
   render() {
     const { 
-      rank, user_serial, first, last, school_logo, 
-      profilePicture, barcode, rank_ord, platoon
+      rank, user_serial, first, last, first_he, last_he,
+      profilePicture, barcode, rank_ord, platoon,
+      school_logo, school_name, member_since, valid_utill
     } = this.props.user;
     // classnames for cards. See SCSS for what they do
     const classNames = classnames('print RankCard', { 'permanent': this.props.permanent });
@@ -42,7 +45,7 @@ class RankCard extends Component {
           <div className='sig'>
             <p>Authorized Signature</p>
             <h1 className='rank-row'>
-              <span className='rank'>{rank} </span>{first} {last}
+              <span className='rank'>{rank} </span>{first_he || first} {last_he || last}
             </h1>
             <p>Serial #: <strong>{ user_serial }</strong></p>
           </div>
@@ -52,13 +55,11 @@ class RankCard extends Component {
             </div>
             <div className='base'>
               <p>BASE #614199</p>
-              <p><strong>Avrohom Academy</strong></p>
+              <p><strong>{school_name}</strong></p>
               <p>Brooklyn, NY</p>
-              <p><strong>Platoon: { platoon }</strong></p>
-              </div>
-            <div className='photo'>
-              <img src={ LEGACY_URL + profilePicture } alt='profile'/>
+              <p><strong>Platoon: { platoon || 'N/A' }</strong></p>
             </div>
+            <ProfilePicture src={ LEGACY_URL + profilePicture } />
           </div>
           <div className='barcode'>
             <div className='code'>
@@ -66,8 +67,8 @@ class RankCard extends Component {
               { barcode }
             </div>
             <div className='stats'>
-              <p>Member since<br/> כ"ד אלול התשע"ה</p>
-              <p>Valid until<br/> כ"ט אלול התשפ"ג</p>
+              <p>Member since<br/>{member_since}</p>
+              { valid_utill && <p>Valid until<br/>{valid_utill}</p> }
             </div>
           </div>
         </div>
