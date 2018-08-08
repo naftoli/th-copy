06366a04f8df07dbf0a80f44c0820f79a14fb9d3
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-08 17:40:10 -0400

    update the platoon operations

--- src/store/platoons/operations.js
+++ src/store/platoons/operations.js
@@ -1,42 +1,21 @@
-import API from 'api/api';
+import API, { handleAPIResponse } from 'api/api';
 // import { toast } from 'react-toastify';
 import * as actions from './actions';
 
 // get all soldiers
-export const getPlatoons = ( id ) => dispatch => {
+export const getPlatoons = ( school_id = false ) => dispatch => {
   dispatch( actions.setLoading( true ) );
-  const query_string = id ? `?school_id=${id}` : '';
-  return API.get( `/core/platoons.php${query_string}` )
+  const query_string = school_id ? `?school_id=${school_id}` : '';
+  return API.get( `/core/platoons${query_string}` )
     .then( response => {
       dispatch( actions.setPlatoons( response.data ) );
       return dispatch( actions.setLoading( false ) );
-    }).catch( () => {
+    }).catch( ( error ) => {
       dispatch( actions.setLoading( false ) );
+      return Promise.reject( error );
     });
 }
-// update a single soldier
-export const updatePlatoon = ( id, data ) => dispatch => {
-  // const toast_id = toast.info( "Updating Platoon...", { autoClose: false } );
-  // return API.post( `/core/platoons.php?id=${id}`, data )
-  //   .then( response => {
-  //     if ( !response.success ) {
-  //       toast.update( toast_id, { type: toast.TYPE.ERROR, render: response.error }); 
-  //     } else { 
-  //       dispatch( actions.updateSoldier( id, response.data ) );
-  //       toast.update( toast_id, { type: toast.TYPE.SUCCESS, render: "Soldier Updated!", autoClose: null });
-  //     };
-  //     return response;
-  //   }).catch( error => {
-  //     toast.update( toast_id, {type: toast.TYPE.ERROR, render: error.message });
-  //     return Promise.reject( error );
-  //   });
-}
 
-/********************** NON STORE API OPERATIONS **********************/
-// load a single soldier - not added to state
 export const getPlatoon = ( id ) => dispatch => {
-  // return API.get( `/core/users.php?id=${id}` )
-  //   .then( response => {
-  //     return response.data;
-  //   })
+  return API.get( `/core/platoons?id=${id}` ).then( handleAPIResponse );
 }
