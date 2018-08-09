a75b7184a4134df8312f99aeb15b26edb918d4a0
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-09 17:23:54 -0400

    do not auto-reload on login change. only when school_id is changed

--- src/components/selects/PlatoonSelect.jsx
+++ src/components/selects/PlatoonSelect.jsx
@@ -6,7 +6,6 @@ import Select from './Select';
 // functions
 import { getPlatoons } from 'store/platoons/operations';
 import { setPlatoons } from 'store/platoons/actions';
-import { loginChanged } from 'functions/login';
 import { findOption } from 'functions/selects';
 
 export class PlatoonSelect extends Component {
@@ -25,28 +24,39 @@ export class PlatoonSelect extends Component {
   }
 
   // update if the login changed or the school_id prop changed
-  componentDidUpdate( { school_id: prevId, login: prevLogin } ) {
-    const { login, school_id } = this.props;
-    if ( loginChanged( login, prevLogin ) || prevId !== school_id )
-      this.loadPlatoons();
+  componentDidUpdate( { school_id: prevId } ) {
+    // if the school ID changed, get the new platoons into redux
+    const { school_id, onChange, value, isClearable } = this.props;
+    if ( prevId !== school_id ) { this.loadPlatoons(); }
+
+    // if we have a value and it is not selected, select it
+    const platoon_options = this.getPlatoonOptions();
+    const selected = findOption( platoon_options, value );
+    if ( !selected && platoon_options.length > 0 && !isClearable ) { 
+      onChange( platoon_options[0] ); 
+    }
   }
   // load the platoons
   loadPlatoons = () => {
-    const { school_id, login } = this.props;
-    const haveSchool = school_id || login.code === 'BC';
-    if ( haveSchool ) this.props.getPlatoons( school_id );
+    const { school_id } = this.props;
+    if ( school_id ) this.props.getPlatoons( school_id );
     else this.props.setPlatoons( [] );
   }
 
-  render() {
-    const { platoons, value, onChange, showAllOption, loading } = this.props;
-    let platoon_options = platoons.map( 
+  getPlatoonOptions = () => {
+    const { platoons, showAllOption } = this.props;
+    const platoon_options = platoons.map( 
       ({ class_id, name, school_id }) => ({ value: class_id, label: name, school_id, class_id })
     );
     if ( showAllOption ) platoon_options.unshift({ value: false, label: 'All Platoons' });
+    return platoon_options;
+  }
 
-    let selected = findOption( platoon_options, value );
-    if ( !selected && platoon_options.length > 0 ) onChange( platoon_options[0] );
+  render() {
+    const { value, onChange, loading } = this.props;
+
+    let platoon_options = this.getPlatoonOptions();
+    const selected = findOption( platoon_options, value );
     platoon_options = loading ? [] : platoon_options;
 
     return (
@@ -56,9 +66,8 @@ export class PlatoonSelect extends Component {
   }
 }
 
-const mapStateToProps = ( { platoons, login } ) => ({
-  ...platoons,
-  login: login.current_login
+const mapStateToProps = ( { platoons } ) => ({
+  ...platoons
 })
 
 export default connect( mapStateToProps, { getPlatoons, setPlatoons } )( PlatoonSelect );
