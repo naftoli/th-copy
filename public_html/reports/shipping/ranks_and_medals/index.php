e5ff2d6211341c52da23b360306f512f2dccff88
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-09 11:48:35 -0400

    add back all schools option
    Former-commit-id: 88fa7409d7e783ea0f5f34756dc9b809c482c222

--- public_html/reports/shipping/ranks_and_medals/index.php
+++ public_html/reports/shipping/ranks_and_medals/index.php
@@ -58,6 +58,7 @@ if($debug) echo "</pre>";?>
                 <div class="row">
                     <i class="fa fa-university" aria-hidden="true"></i> School: 
                     <select id="school_id" name="school_id">
+                        <option value="">All Schools</option>
                         <? foreach($schools as $school_id => $school_name){?>
                             <option value="<?=$school_id?>"><?=$school_name?></option>
                         <?}?>
