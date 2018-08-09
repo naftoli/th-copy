aac4a2c1774f50b52c182123deeae58d01143699
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-09 18:45:44 -0400

    extend the AdminAuth class
    Former-commit-id: 889d9637246581fd7fdf2aab7832a448fb1a8e1d

--- public_html/api/models/AdminAuth.php
+++ public_html/api/models/AdminAuth.php
@@ -1,6 +1,19 @@
 <?php
-class AdminAuth extends ActiveRecord\Model {
+
+class AdminAuth extends ActiveRecord\Model implements JsonSerializable {
     static $belongs_to = [
         [ 'admin' ]
     ];
+
+    static $validates_uniqueness_of = [
+        [['admin_id','auth','id'], 'message' => 'Auth already Exists']
+    ];
+
+    public static function findAuth( $admin_id, $auth, $auth_id ) {
+        return self::find_by_admin_id_and_auth_and_id( $admin_id, $auth, $auth_id );
+    }
+
+    public function jsonSerialize() {
+        return $this->to_array();
+    }
 }
