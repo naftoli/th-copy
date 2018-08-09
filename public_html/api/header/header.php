7b00f1e27b42567e975b53c71b01fbd021cdc85a
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-09 17:44:00 -0400

    support delete command via CORS
    Former-commit-id: 79296843b15766befa27682ac2cbbd75af5c2a70

--- public_html/api/header/header.php
+++ public_html/api/header/header.php
@@ -22,6 +22,7 @@ include_once( __DIR__ . '/../../class.globalSettings.php');
 // set headers
 header('Access-Control-Allow-Origin: '. ( isset( $_SERVER['HTTP_ORIGIN'] ) ? $_SERVER['HTTP_ORIGIN'] : "*" ) ); // CORS
 header('Access-Control-Allow-Credentials: true');
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE");
 header('Access-Control-Allow-Headers: mobile, Content-Type, Authorization, login');
 header("Content-Type: text/html; charset=utf-8;");
 
