73c4493543778cdddaa2cf9e30a0785508927b41
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-10 11:44:02 -0400

    set defaults onCreate
    Former-commit-id: efe7e307219a854152fb18719e1e9428dcc05f19

--- public_html/api/models/AdminAuth.php
+++ public_html/api/models/AdminAuth.php
@@ -1,6 +1,8 @@
 <?php
 
 class AdminAuth extends ActiveRecord\Model implements JsonSerializable {
+    static $before_create = ['setDefaultRole', 'setDefaultPosition'];
+    
     static $belongs_to = [
         [ 'admin' ]
     ];
@@ -11,6 +13,28 @@ class AdminAuth extends ActiveRecord\Model implements JsonSerializable {
 
     public static function findAuth( $admin_id, $auth, $auth_id ) {
         return self::find_by_admin_id_and_auth_and_id( $admin_id, $auth, $auth_id );
+    }
+    // set the default role for the connetion
+    public function setDefaultRole() {
+        if ( $this->role_id ) return true;
+        if ( $this->auth === 'user' ) {
+            $this->role_id = 1;
+        } else if ( $this->auth === 'class' ) {
+            $this->role_id = 13;
+        } else if ( $this->auth === 'school' ) {
+            $this->role_id = 18;
+        }
+    }
+    // set the default position for the auth
+    public function setDefaultPosition() {
+        if ( $this->position ) return true;
+        if ( $this->auth === 'user' ) {
+            $this->position = 'Parent';
+        } else if ( $this->auth === 'class' ) {
+            $this->position = 'Teacher';
+        } else if ( $this->auth === 'school' ) {
+            $this->position = 'Base Commander';
+        }
     }
 
     public function jsonSerialize() {
