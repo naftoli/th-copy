e85e14afadfee2608de1c71f6af362d9837b7588
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-10 15:33:27 -0400

    bring this support to the selectors as well

--- src/components/selects/BaseSelect.jsx
+++ src/components/selects/BaseSelect.jsx
@@ -11,41 +11,56 @@ import { findOption } from 'functions/selects';
 export class BaseSelect extends Component {
 
   static propTypes = {
-    showAllOption: PropTypes.bool
+    showAllOption: PropTypes.bool,
+    fetchAll: PropTypes.bool,
+    onChange: PropTypes.func,
+    value: PropTypes.any
   }
 
   static defaultProps = {
-    showAllOption: false
+    showAllOption: false,
+    fetchAll: false
   }
 
 
   componentDidMount(){
-    if ( this.props.bases.length === 0 )
-      this.loadBases();
+    this.props.getBases( this.props.fetchAll );
   }
 
-  // update if the login changed or the school_id prop changed
+  // update if the login changed and we did not fetch all bases for this BC
   componentDidUpdate( prevProps ) {
-    if ( loginChanged( this.props.login, prevProps.login ) )
-      this.loadBases();
+    const { login, fetchAll, getBases, onChange, value, isClearable } = this.props;
+    // fetch new bases if we need to
+    if ( loginChanged( login, prevProps.login ) ) {
+      getBases( fetchAll );
+    }
+    // actually select the first option if is not clearable
+    const options = this.getOptions();
+    const selected = findOption( options, value );
+    if ( !selected && options.length > 0 && !isClearable ) { 
+      onChange( options[0] ); 
+    }
   }
-  // load the platoons
-  loadBases = () => {
-    this.props.getBases();
+
+  getOptions = () => {
+    const { bases, showAllOption } = this.props;
+    const options = bases.map( 
+      ({ school_name, school_id }) => ({ value: school_id, label: school_name })
+    );
+    if ( showAllOption ) options.unshift({ value: false, label: 'All Platoons' });
+    return options;
   }
+
   // render
   render() {
-    const { bases, onChange, value, showAllOption, loading } = this.props;
-    const base_options = bases.map( 
-      ({ school_id, school_name }) => ({ value: school_id, label: school_name })
-    );
-    if ( showAllOption ) base_options.unshift({ value: false, label: 'All Bases' });
-
-    let selected = findOption( base_options, value );
-    if ( !selected && base_options.length > 0 ) onChange( base_options[0] );
+    const { onChange, value, loading } = this.props;
+    
+    let options = this.getOptions();
+    const selected = findOption( options, value );
+    options = loading ? [] : options;
 
     return (
-      <Select { ...this.props } options={base_options} value={ selected }
+      <Select { ...this.props } options={ options } value={ selected }
         isLoading={ loading } onChange={ onChange } />
     )
   }
--- src/components/selects/PlatoonSelect.jsx
+++ src/components/selects/PlatoonSelect.jsx
@@ -11,11 +11,16 @@ import { findOption } from 'functions/selects';
 export class PlatoonSelect extends Component {
 
   static propTypes = {
-    showAllOption: PropTypes.bool
+    showAllOption: PropTypes.bool,
+    fetchAll: PropTypes.bool,
+    onChange: PropTypes.func,
+    value: PropTypes.any,
+    school_id: PropTypes.any.isRequired
   }
 
   static defaultProps = {
-    showAllOption: false
+    showAllOption: false,
+    fetchAll: false
   }
 
   componentDidMount(){
@@ -26,43 +31,50 @@ export class PlatoonSelect extends Component {
   // update if the login changed or the school_id prop changed
   componentDidUpdate( { school_id: prevId } ) {
     // if the school ID changed, get the new platoons into redux
-    const { school_id, onChange, value, isClearable } = this.props;
-    if ( prevId !== school_id ) { this.loadPlatoons(); }
+    const { school_id, onChange, value, isClearable, fetchAll } = this.props;
+    if ( prevId !== school_id && !fetchAll ) { this.loadPlatoons(); }
 
     // if we have a value and it is not selected, select it
-    const platoon_options = this.getPlatoonOptions();
-    const selected = findOption( platoon_options, value );
-    if ( !selected && platoon_options.length > 0 && !isClearable ) { 
-      onChange( platoon_options[0] ); 
+    const options = this.getOptions();
+    const selected = findOption( options, value );
+    if ( !selected && options.length > 0 && !isClearable ) { 
+      onChange( options[0] ); 
     }
   }
   // load the platoons
   loadPlatoons = () => {
-    const { school_id } = this.props;
-    if ( school_id ) this.props.getPlatoons( school_id );
-    else this.props.setPlatoons( [] );
+    const { school_id, fetchAll, loading } = this.props;
+    
+    if ( fetchAll ) { 
+      return this.props.getPlatoons( false, true ); // fetch all platoons
+    } else if ( school_id ) {
+      return this.props.getPlatoons( school_id );
+    } else {
+      return this.props.setPlatoons( [] );
+    }
   }
 
-  getPlatoonOptions = () => {
+  getOptions = () => {
     const { platoons, showAllOption, school_id } = this.props;
-    const platoon_options = platoons.filter(
-      platoon => platoon.school_id === school_id
-    ).map( 
-      ({ class_id, name, school_id }) => ({ value: class_id, label: name, school_id, class_id })
-    );
-    if ( showAllOption ) platoon_options.unshift({ value: false, label: 'All Platoons' });
-    return platoon_options;
+    
+    const options = platoons
+    // only platoons in the school_id from props
+    .filter( platoon => platoon.school_id === school_id )
+    // map them to what react-select expects
+    .map( ({ class_id, name, school_id }) => ({ value: class_id, label: name }) );
+    if ( showAllOption ) options.unshift({ value: false, label: 'All Platoons' });
+    return options;
   }
 
   render() {
     const { value, onChange, loading } = this.props;
 
-    let platoon_options = this.getPlatoonOptions();
-    const selected = findOption( platoon_options, value );
-    platoon_options = loading ? [] : platoon_options;
+    let options = this.getOptions();
+    const selected = findOption( options, value );
+    options = loading ? [] : options;
 
     return (
-      <Select {...this.props} options={ platoon_options } value={ selected }
+      <Select {...this.props} options={ options } value={ selected }
         isLoading={ loading } onChange={ onChange }/>
     )
   }
