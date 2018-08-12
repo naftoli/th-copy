3dd865b4d1b9a13c82a982539fcb150b20d2faf3
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-12 15:14:56 -0400

    big whoops

--- src/store/bases/operations.js
+++ src/store/bases/operations.js
@@ -5,7 +5,8 @@ import * as actions from './actions';
 // get all soldiers
 export const getBases = ( all = false ) => dispatch => {
   dispatch( actions.setLoading( true ) );
-  return API.get( `/core/bases.php${ all && '?all=true' }` )
+  all = all ? '?all=true' : '';
+  return API.get( `/core/bases.php${all}` )
     .then( response => {
       dispatch( actions.setBases( response.data ) );
       return dispatch( actions.setLoading( false ) );
