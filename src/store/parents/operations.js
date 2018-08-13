5d6967e228188655d65ed74fba499462aa1e199f
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-13 19:13:35 -0400

    set the loading state

--- src/store/parents/operations.js
+++ src/store/parents/operations.js
@@ -2,12 +2,17 @@ import API, { handleAPIResponse } from 'api/api';
 import * as actions from './actions';
 
 export const getParents = () => dispatch => {
+  dispatch( actions.setLoading( true ) );
   return API.get(`/core/parents`)
   .then( handleAPIResponse )
   .then( ({ parents, children }) => {
     dispatch( actions.setParents( parents ) )
     dispatch( actions.setChildren( children ) )
-  });
+  })
+  .catch( error => {
+    dispatch( actions.setLoading( true ) );
+    return Promise.reject( error );
+  })
 }
 
 /***************************** DOES NOT MODIFY STATE. JUST HITS API *****************************/
