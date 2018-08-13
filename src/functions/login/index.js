733e6322a3bd02283fa12dd51b076f50cc5b9c23
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-13 12:03:45 -0400

    shortcut function to practice DRY

--- src/functions/login/index.js
+++ src/functions/login/index.js
@@ -1,7 +1,14 @@
+import store from 'store/index';
+
 // detect login changes
 export const loginChanged = ( new_login, old_login ) => {
   const { type, id } = new_login;
   const { type: prevType, id: prevId } = old_login;
   // return if true if there are any differences between the objects
   return type !== prevType || prevId !== id;
+}
+
+export const loginStoreChanged = ( old_login ) => {
+  const new_login = store.getState().login.current_login
+  return loginChanged( new_login, old_login );
 }
