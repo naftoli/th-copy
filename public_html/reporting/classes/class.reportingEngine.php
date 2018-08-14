1ec94aa26c5a1a71983e320513c7d2261a4a081d
unknown <naftoli@tzivoshashem.org>
2018-08-14 01:51:08 -0400

    reporting engine
    Former-commit-id: 3cb69b06b42dab9b34a58b2469351fb3742ad23f

--- public_html/reporting/classes/class.reportingEngine.php
+++ public_html/reporting/classes/class.reportingEngine.php
@@ -2,16 +2,20 @@
 require '../class.points.php';
 
 class ReportingEngine {
-    private $data = array();
-    private $aliases = array();
-    private $result = array();
-    private $qry = '';
-    private $error = '';
-    private $checkPoints = false;
+    private $data;
+    private $aliases;
+    private $result;
+    private $qry;
+    private $error;
+    private $checkPoints;
     
     public function __construct( $info ) {
         $this->createAliases();
         $this->data = $info;
+        $this->result = array();
+        $this->qry = '';
+        $this->error = '';
+        $this->checkPoints = false;
     }
     
     private function createAliases() {
@@ -35,10 +39,14 @@ class ReportingEngine {
     private function generateSelect() {
         $sql = 'SELECT ';
         foreach ($this->data as $table => $columns) {
+            // skip calc info
+            if ( $table == 'calc' ) continue;
+
             // make sure to always have user_id when pulling from users table
             if ( $table == 'users' ) {
                 $sql .= "u.user_id, ";
             }
+
             foreach ($columns as $column) {
                 // add alias with underscore to each column name to ensure there's no conflicting columnn names
                 $sql .= $this->aliases[$table] . "." . $column . " as " . $this->aliases[$table] . "_" . $column . ", ";
@@ -52,9 +60,13 @@ class ReportingEngine {
     private function generateFrom() {
         // extract tables from data array
         $tables = array();
-        foreach ($this->data as $table => $columns) {
-            if ( $table != 'miles' ) $tables[] = $table;
-            else $this->checkPoints = true;
+        foreach ( $this->data as $table => $columns ) {
+            if ( $table != 'calc' ) { // calc is not a table but a flag that we need to do points calculations
+                $tables[] = $table;
+            } else {
+                $this->checkPoints = true;
+                continue;
+            }
         }
         
         // figure out root table for joins
@@ -95,25 +107,31 @@ class ReportingEngine {
         if (array_key_exists('users', $this->data)) {
             $sql = " WHERE user_registered > 0";
         }
+        $sql .= " limit 10";
         return $sql;
     }
     
     public function runQry() {
-        echo $this->qry . "<br />";
-        if (empty( $this->qry )) return false;
-        if ($res = mysql_query( $this->qry )) {
-            while ($row = mysql_fetch_assoc( $res )) {
+        //echo $this->qry . "<br />";
+        if ( empty( $this->qry ) ) return false;
+        if ( $res = mysql_query( $this->qry ) ) {
+            while ( $row = mysql_fetch_assoc( $res ) ) {
                 // check if we need to add points info to result
                 if ( $this->checkPoints ) {
-                    echo "Checking Points<br />";
                     $p = new Points( $row['user_id'] );
                     // find out which points to get 
-                    $pointsToGet = $this->data['miles'];
+                    $pointsToGet = $this->data['calc'];
                     foreach ( $pointsToGet as $type ) {
-                        if ( $type == 'store_points' ) {
-                            $row['store_miles'] = $p->getStorePoints();
-                        } else if ( $type == 'auction_points' ) {
-                            $row['auction_miles'] = $p->getAuctionPoints();
+                        switch ( $type ) {
+                            case 'store_points':
+                                $row['store_miles'] = $p->getStorePoints();
+                                break;
+                            case 'total_points':
+                                $row['total_miles'] = $p->getTotalPoints();
+                                break;
+                            case 'total_this_yr':
+                                $row['total_this_yr_miles'] = $p->getTotalThisYear();
+                                break;
                         }
                     }
                 }
--- public_html/reporting/createReport.php
+++ public_html/reporting/createReport.php
@@ -1,5 +1,5 @@
 <?php
-echo "<pre>"; print_r( $_POST ); echo "</pre>";
+//echo "<pre>"; print_r( $_POST ); echo "</pre>";
 require '../db.php';
 require 'classes/class.reportingEngine.php';
 
@@ -14,11 +14,12 @@ foreach ( $_POST as $k => $v ) {
             $field = substr($k, $pos);
             if ( $table != 'calc' ) {
                 $info[$table][] = $field;
-            } else {
-                switch ($v) {
+            } else { // it's not info from a table but it's a calculated value
+                switch ( $field ) {
                     case 'store_points':
-                    case 'auction_points':
-                        $info['miles'][] = $v;
+                    case 'total_points':
+                    case 'total_this_yr':
+                        $info['calc'][] = $field;
                         break;
                 }
             }
@@ -30,7 +31,70 @@ $engine = new ReportingEngine( $info );
 $engine->createQry();
 if ( $engine->runQry() ) {
     $result = $engine->getResult();
-    echo "<pre>"; print_r( $result ); echo "</pre>";
+    //echo "<pre>"; print_r( $result ); echo "</pre>";
 } else {
     echo $engine->getError();
+    exit;
 }
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Reports</title>
+		<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs-3.3.7/jqc-1.12.4/dt-1.10.13/cr-1.3.2/fc-3.2.2/fh-3.1.2/r-2.1.1/sc-1.4.2/se-1.2.0/datatables.min.css"/>
+		<style>
+			body {
+				font-family: sans-serif;
+				font-size: 12px;
+				padding-left: 3%;
+				padding-right: 3%;
+			}
+			fieldset {
+				float: left;
+				width: 40%;
+				padding-right: 20px;
+				padding-left: 20px;
+				padding-bottom: 20px;
+			}
+		</style>
+	</head>
+	
+	<body>
+        <table id="table" class="table table-striped table-condensed">
+            <thead>
+                <tr>
+                    <?php
+                    for ( $i = 0; $i < 1; $i++ ) { // all rows have same keys / fields so only need to get it from the first one
+                        echo "<tr>";	
+                        foreach ( $result[$i] as $field => $value ) {
+                            echo "<th>" . $field . "</th>";
+                        }
+                        echo "</tr>";
+                    }
+                    ?>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    foreach ( $result as $info ) {
+                        echo "<tr>";	
+                        foreach ( $info as $value ) {
+                            echo "<td>" . $value . "</td>";
+                        }
+                        echo "</tr>";
+                    }
+                ?>
+            </tbody>
+        </table>
+    </body>
+    
+    <script type="text/javascript" src="https://cdn.datatables.net/v/bs-3.3.7/jqc-1.12.4/dt-1.10.13/cr-1.3.2/fc-3.2.2/fh-3.1.2/r-2.1.1/sc-1.4.2/se-1.2.0/datatables.min.js"></script>
+	<script>
+		$('#table').DataTable({
+			paging : false
+		});
+    </script>
+</html>
