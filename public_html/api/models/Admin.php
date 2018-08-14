fe6e5d08e94c61363e7136aaf48d3cf41b10232e
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-14 17:56:10 -0400

    send emails to parents
    Former-commit-id: 7117672d583514b83fdd2a2e89b712cfdfae2d94

--- public_html/api/models/Admin.php
+++ public_html/api/models/Admin.php
@@ -193,6 +193,32 @@ class Admin extends ActiveRecord\Model implements JsonSerializable {
         ]) );
     }
 
+    public function sendParentEmail() {
+		$headers  = 'MIME-Version: 1.0' . "\r\n";
+		$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+		$headers .= 'From: Tzivos Hashem <cth@tzivoshashem.org>' . "\r\n";
+		$headers .= 'Reply-to: cth@tzivoshashem.org' . "\r\n";
+		$to = $this->admin_email;
+		$subject = "Your new account with Chayolei Tzivos Hashem.";
+		$html = "Hi!<br /><br /> "
+            ."A parent account has been created for your child (or children) in Chayolei Tzivos Hashem.<br /><br />"
+            ."With it, you’ll able to mark your children’s missions daily, straight from any smartphone (or computer). You will also be able to check in on their progress reports, personalize their growth, and stay up-to-date on Tzivos Hashem news from bases around the world.  "
+            ."<br /><br />"
+            ."Darchei Hachassidus will come alive in your home as managing your kids’ Chayolei Tzivos Hashem accounts becomes easier than ever. Help your young soldier reach the greatest heights in Hashem’s army. "
+            ."<br /><br />"
+            ."Your Username is: " . $this->username . " <br />"
+            ."Your Default Password is: " . $this->password . " <br />"
+            ."<br />"
+            ."To change your username/password simply log into your account on tzivoshashem.com/mobile and click 'edit profile' on the top right hand corner. "
+            ."<br /><br />"
+            ."For any questions, help, or feedback, contact your school's Base Commander."
+            ."<br /><br />"
+            ."Wishing you much Yiddishe and Chassidishe Nachas,"
+            ."<br /><br />"
+            ."CTH Headquarters";
+		return @mail($to, $subject, $html, $headers);
+	}
+
     // SERIALIZERS
     public function jsonSerialize() {
         return $this->to_array([
