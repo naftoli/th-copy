d5b843d665d258701d53c24b50f2fcde69a81838
naftolir <naftolir@gmail.com>
2018-08-14 16:30:25 -0400

    reporting engine
    Former-commit-id: c5fbe68d618f940c6a0f10637d99ca535e55708f

--- public_html/reporting/classes/class.reportingEngine.php
+++ public_html/reporting/classes/class.reportingEngine.php
@@ -7,6 +7,8 @@ class ReportingEngine {
     private $result;
     private $qry;
     private $error;
+    private $school_id;
+    private $class_id;
     private $checkPoints;
     
     public function __construct( $info ) {
@@ -15,6 +17,8 @@ class ReportingEngine {
         $this->result = array();
         $this->qry = '';
         $this->error = '';
+        $this->school_id = 0;
+        $this->class_id = 0;
         $this->checkPoints = false;
     }
     
@@ -28,6 +32,14 @@ class ReportingEngine {
             'ranks'         =>  'r'
         );
     }
+
+    public function setSchool( $id ) {
+        if ( $id > 0 ) $this->school_id = $id;
+    }
+
+    public function setClass( $id ) {
+        if ( $id > 0 ) $this->class_id = $id;
+    }
     
     public function createQry() {
         $sql = $this->generateSelect();
@@ -37,7 +49,7 @@ class ReportingEngine {
     }
     
     private function generateSelect() {
-        $sql = 'SELECT ';
+        $sql = "SELECT ";
         foreach ($this->data as $table => $columns) {
             // skip calc info
             if ( $table == 'calc' ) continue;
@@ -58,28 +70,20 @@ class ReportingEngine {
     }
     
     private function generateFrom() {
-        // extract tables from data array
-        $tables = array();
-        foreach ( $this->data as $table => $columns ) {
-            if ( $table != 'calc' ) { // calc is not a table but a flag that we need to do points calculations
-                $tables[] = $table;
-            } else {
-                $this->checkPoints = true;
-                continue;
-            }
-        }
+        // extract tables from data array 
+        $tables = array_keys( $this->data );
         
         // figure out root table for joins
         $root = 'users';
-        if (!in_array('users', $tables) && in_array('admins', $tables)) {
+        if ( !in_array('users', $tables) && in_array('admins', $tables) ) {
             $root = 'admins';
         }
         
         $sql = " FROM ";
-        if ($root == 'users') {
+        if ( $root == 'users' ) {
             $sql .= "users u ";
-            foreach ($tables as $table) {
-                switch ($table) {
+            foreach ( $tables as $table ) {
+                switch ( $table ) {
                     case 'admins':
                         $sql .= "JOIN admin_auths aa ON aa.id = u.user_id
                                 JOIN admins a USING (admin_id) ";
@@ -94,20 +98,33 @@ class ReportingEngine {
                     case 'classes':
                         $sql .= "JOIN classes c ON c.class_id = u.class_id ";
                         break;
+                    case 'calc':
+                        $this->checkPoints = true;
+                        break;
                 }
             }
-        } else if ($root == 'admins') {
+        } else if ( $root == 'admins' ) {
             $sql .= "admins a ";
+            // if we are limiting to school / grade and we only are looking for admin info, we need some joins
+            if ( $this->school_id > 0 ) {
+                $sql .= "JOIN admin_auths aa USING (admin_id) 
+                        JOIN users u ON u.user_id = aa.id";
+            }
+            
         }
         return $sql;
     }
     
     private function generateWhere() {
-        $sql = '';
-        if (array_key_exists('users', $this->data)) {
-            $sql = " WHERE user_registered > 0";
+        $sql = " WHERE 1";
+        // limit to school / class if there's user info
+        if ( $this->school_id > 0 ) {
+            $sql .= " AND u.school_id = " . $this->school_id;
+            if ( $this->class_id > 0 ) {
+                $sql .= " AND u.class_id = " . $this->class_id;
+            }
         }
-        $sql .= " limit 10";
+
         return $sql;
     }
     
@@ -124,18 +141,26 @@ class ReportingEngine {
                     foreach ( $pointsToGet as $type ) {
                         switch ( $type ) {
                             case 'store_points':
-                                $row['store_miles'] = $p->getStorePoints();
+                                $row['store_points'] = $p->getStorePoints();
                                 break;
                             case 'total_points':
-                                $row['total_miles'] = $p->getTotalPoints();
+                                $row['total_points'] = $p->getTotalPoints();
                                 break;
                             case 'total_this_yr':
-                                $row['total_this_yr_miles'] = $p->getTotalThisYear();
+                                $row['total_this_yr'] = $p->getTotalThisYear();
                                 break;
                         }
                     }
                 }
-                $this->result[] = $row;
+                // make sure there's at least one field that has info that's not blank
+                $addToResult = false;
+                foreach ( $row as $key => $value ) {
+                    if ( !empty( $value ) ) {
+                        $addToResult = true;
+                        break;
+                    }
+                }
+                if ( $addToResult ) $this->result[] = $row;
             }
         } else {
             $this->error = $this->qry . "<br />" . mysql_error() . "<br />";
