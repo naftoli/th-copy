3ab31c67bfd6e34c0538b98068a8bcf3f32a79ab
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-14 21:58:37 -0400

    setup StaffDetailPage

--- src/pages/base-managment/staff/StaffDetailPage.jsx
+++ src/pages/base-managment/staff/StaffDetailPage.jsx
@@ -1,46 +1,81 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 // components
-import { Spinner, ProfilePicture, FontAwesome } from 'components/ui';
-import { PhoneNumber } from 'components/inputs';
+import { Page404 } from 'pages/errors';
+import { Spinner, FontAwesome } from 'components/ui';
 import { Row, Col, Input, Button, InputGroup, InputGroupAddon } from 'reactstrap';
+// rows
+import { EditStaffRow } from './rows/EditStaffRow';
 // functions
+import memoize from 'memoize-one';
 import { toast } from 'react-toastify';
 import { loginStoreChanged } from 'functions/login';
+// state
+import { getStaff } from 'store/staff/operations';
 
 class StaffDetailPage extends Component {
 
-  state = { selectedUserId: false }
+  state = {
+    updates: {}
+  }
 
   componentDidMount() {
-    if ( this.props.parents.length === 0 ) {
-      // get the staff member
+    if ( this.props.staff.length === 0 ) {
+      this.loadStaff();
     }
   }
 
-  componentDidUpdate({ login }) {
-    if ( loginStoreChanged( login ) ) {
-      // get the staff member
-    };
+  componentDidUpdate({ login, staff }) {
+    if ( loginStoreChanged( login ) )
+      this.loadStaff();
+    if ( staff !== this.props.staff ) 
+      this.setState({ updates: {} });
   }
+  // wrapper / alias for prop
+  loadStaff = () => { this.props.getStaff(); };
+  
+  // cache the result for performance
+  findStaff = memoize( ( staff, admin_id ) => staff.find( staff => staff.admin_id === admin_id ) );
+  getStaff = () => this.findStaff( this.props.staff, parseInt( this.props.match.params.id, 10 ) );
+  
+  // event handlers
+  handleUpdates = ( updates ) => { this.setState({ updates: { ...this.state.updates, ...updates } }) };
+  onChange = ({ target }) => { this.handleUpdates({ [target.name]: target.value }) };
 
   render() {
+    const { loading } = this.props;
+    let staff = this.getStaff();
+
+    if ( loading && !staff ) return <Spinner size='10' />;
+    if ( !staff ) return <Page404 />;
+
+    // get the things we have changed;
+    staff = { ...staff, ...this.state.updates };
 
     // and render the page
     return (
       <div id='StaffDetailPage'>
+        
         <p className='title'>Account Information</p>
-        <pre>{ JSON.stringify( this.props, null, 2 ) }</pre>
+        <EditStaffRow 
+          { ...staff } 
+          onChange={ this.onChange }
+          />
+        {/* <p className='title'>Positions</p>
+        <pre>{ JSON.stringify( staff.positions, null, 2 ) }</pre> */}
+        <p className='title'>Debug</p>
+        <pre>{ JSON.stringify( { state: this.state }, null, 2 ) }</pre>
       </div>
     );
   }
 }
 
-const mapStateToProps = ( { parents, login } ) => ({
+const mapStateToProps = ( { staff, login } ) => ({
+  ...staff,
   login: login.current_login
 })
 
-const mapDispatchToProps = {}
+const mapDispatchToProps = { getStaff }
 
 export default connect( 
   mapStateToProps, mapDispatchToProps 
---
+++ src/pages/base-managment/staff/rows/EditStaffRow.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { PhoneNumber, Password } from 'components/inputs';
+import { Row, Col, Input } from 'reactstrap';
+
+export const EditStaffRow = ( props ) => {
+  const inputProps = { onChange: props.onChange };
+  const { username, password, title, first, last, email, work, cell } = props;
+  return (
+    <Row>
+      <Col xs={6}>
+        <label>Username</label>
+        <Input name='username' value={ username } {...inputProps} />
+      </Col>
+      <Col xs={6}>
+        <label>Password</label>
+        <Password value={ password } {...inputProps} tabToggle />
+      </Col>
+      <Col xs={4}>
+        <label>Title</label>
+        <Input name='title' value={ title } {...inputProps} />
+      </Col>
+      <Col xs={8} sm={4}>
+        <label>First Name</label>
+        <Input name='first' value={ first } {...inputProps} />
+      </Col>
+      <Col xs={12} sm={4}>
+        <label>Last Name</label>
+        <Input name='last' value={ last } {...inputProps} />
+      </Col>
+      <Col xs={12}>
+        <label>E-Mail</label>
+        <Input name='email' value={ email } {...inputProps} />
+      </Col>
+      <Col xs={12} sm={6}>
+        <label>Work Phone</label>
+        <PhoneNumber name='work' value={ work } {...inputProps} />
+        <div className='invalid-message'>Please enter a valid phone number</div>
+      </Col>
+      <Col xs={12} sm={6}>
+        <label>Cell Phone</label>
+        <PhoneNumber name='cell' value={ cell } {...inputProps} />
+        <div className='invalid-message'>Please enter a valid phone number</div>
+      </Col>
+    </Row>
+  );
+}
