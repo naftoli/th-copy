55f1444027db533a0836be3b1a0e8035d5aff3dc
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-15 11:46:19 -0400

    fix bad first names
    Former-commit-id: 575a5dd76a137140a14c05b4e80133979c73942f

--- public_html/api/functions/format/parents.php
+++ public_html/api/functions/format/parents.php
@@ -1,10 +1,11 @@
 <?php
 
-function formatParentName( $father, $mother ) {
+function formatParentName( $father, $mother, $first = 'Mr. and Mrs.' ) {
     if ( $father && $mother )
         return trim("$father and $mother");
     else if ( $father )
         return trim($father);
     else if ( $mother )
         return trim($mother);
+    else return $first;
 }
