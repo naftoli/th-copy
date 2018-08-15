6782e16bc5d86bc2f4d18c0ae2eb95b088a48b5e
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-15 16:25:21 -0400

    test emails
    Former-commit-id: 9365c9fbff1b4014cf2ef5428934b3d66855f50d

---
+++ public_html/api/emails/index.php
@@ -0,0 +1,49 @@
+<?php
+
+class MashpiaEmails {
+
+    public static function passwordChanged( $to, $name ) {
+        $subject = 'Your Password has been changed.';
+        ob_start();
+        include("templates/passwordChanged.html");
+        $email = ob_get_clean();
+
+        return self::sendEmail(
+            'noreply@tzivoshashem.com', $to, 
+            $subject, $email
+        );
+    }
+
+    public static function sendParentEmail( $to, $username, $password ) {
+        $subject = "Your new account with Chayolei Tzivos Hashem.";
+        $message = "Hi!<br /><br /> "
+            ."A parent account has been created for your child (or children) in Chayolei Tzivos Hashem.<br /><br />"
+            ."With it, you’ll able to mark your children’s missions daily, straight from any smartphone (or computer). You will also be able to check in on their progress reports, personalize their growth, and stay up-to-date on Tzivos Hashem news from bases around the world.  "
+            ."<br /><br />"
+            ."Darchei Hachassidus will come alive in your home as managing your kids’ Chayolei Tzivos Hashem accounts becomes easier than ever. Help your young soldier reach the greatest heights in Hashem’s army. "
+            ."<br /><br />"
+            ."Your Username is: $username <br />"
+            ."Your Default Password is: $password <br />"
+            ."<br />"
+            ."To change your username/password simply log into your account on https://TzivosHashem.com/mobile and click 'edit profile' on the top right hand corner. "
+            ."<br /><br />"
+            ."For any questions, help, or feedback, contact your school's Base Commander."
+            ."<br /><br />"
+            ."Wishing you much Yiddishe and Chassidishe Nachas,"
+            ."<br /><br />"
+            ."CTH Headquarters";
+        return self::sendEmail( 
+            'cth@tzivoshashem.org', $to,
+            $subject, $message
+        );
+    }
+
+    public static function sendEmail( $from, $to, $subject, $msg ) {
+        $headers  = "MIME-Version: 1.0\r\n";
+        $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+        $headers .= "From: Tzivos Hashem <$from>\r\n";
+        $headers .= "Reply-to: $from\r\n";
+    
+        return @mail( $to, $subject, $msg, $headers );
+    }
+}
