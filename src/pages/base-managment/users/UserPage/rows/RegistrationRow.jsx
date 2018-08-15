95f54157faf6dce55d9020d92c198ad4dbb692e2
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-15 10:53:44 -0400

    move registration info to server

--- src/pages/base-managment/users/UserPage/rows/RegistrationRow.jsx
+++ src/pages/base-managment/users/UserPage/rows/RegistrationRow.jsx
@@ -1,24 +1,17 @@
 import React from 'react';
 // components
 import { Row, Col } from 'reactstrap';
-// functions
-import moment from 'moment';
-import julian from 'julian';
 
-const RegistrationRow = ( soldier ) => {
-  const { user_start_date, user_registered } = soldier;
+const RegistrationRow = ({ soldier }) => {
+  const { start_date, registered_at } = soldier;
   return (
     <Row id='registration-row'>
       <Col xs='12'>
         <p className='title'>Registration Information</p>
         <label>Member Since:</label>
-        <h4>{
-          user_start_date ? 
-          moment( julian.toDate( user_start_date ) ).format("LLLL") :
-          'N/A'
-        }</h4>
+        <h4>{ start_date || 'N/A' }</h4>
         <label>Registered:</label>
-        <h4>{ user_registered ? moment( user_registered ).format("LLLL") : 'Not Registered.'}</h4>
+        <h4>{ registered_at || 'Not Registered.'}</h4>
       </Col>
     </Row>
   );
