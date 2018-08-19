6a278fa13a061ef0450ba41f499bb7af6024146b
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-19 12:25:59 -0400

    functions to handle events

---
+++ src/functions/events.js
@@ -0,0 +1,20 @@
+import { toHebrew } from 'functions/utils';
+
+// handle hebrew text inputs
+export const hebrewChange = onChange => event => {
+  event.target.value = toHebrew( event.target.value );
+  onChange( event )
+}
+// convert events to { target.key: target.value };
+export const eventToUpdate = ( target, key = 'name' ) => {
+  return { [target[key]]: target.value }
+}
+
+// filter updates to only ones which are different from the source object. should be Object.filter( key, value => res ) but hey..
+export const filterUpdates = ( source, updates ) => {
+  return Object.entries( updates ) // convert to array ( for all the cool functions )
+    // filter to entries who's value does not match that of the same key in the source
+    .filter( entry => source[entry[0]] !== entry[1] )
+    // and convert that back to an object using reduce
+    .reduce( ( obj, item ) => ({ [item[0]]: item[1] }), {} )
+}
