4f0267d3e227372c0c042a1c2489b9e304474a78
naftolir <naftolir@gmail.com>
2018-08-22 17:26:03 -0400

    created yahadus book report
    Former-commit-id: 024661047e3b1e469dc18be8f9b941b19135decd

---
+++ public_html/reports/registration/yahadus_books.php
@@ -0,0 +1,147 @@
+<?php
+$admin_auth = array(); 	
+require_once ( __DIR__ . '/../../header.php' ); 
+
+require_once ( __DIR__ . '/../../class.globalSettings.php' ); 
+$year = GlobalSettings::getRegistrationYear();
+
+$main_query = mysql_query(
+    "SELECT rc.date, u.first, u.last, s.school_id, s.school_name, c.class_grade 
+    FROM registration_charges rc 
+    JOIN users u USING (user_id) 
+    JOIN schools s ON s.school_id = u.school_id 
+    JOIN classes c ON c.class_id = u.class_id 
+    WHERE type = 'yahadus' 
+    ORDER BY school_name"
+);
+
+$data = [];
+$schools = [];
+$schoolTransitioned = [];
+
+while( $row = mysql_fetch_assoc( $main_query ) ) {
+    $data[$row['school_id']][] = $row;
+    $schools[$row['school_id']] = $row['school_name'];
+}
+
+// find out transition info for each school
+foreach ( $schools as $id => $name ) {
+    $transition_qry = "SELECT * FROM classes WHERE class_era = 0 AND confirmed = 0 AND school_id = " . $id;
+    $transition_res = mysql_query( $transition_qry );
+    if ( mysql_num_rows( $transition_res ) > 0 ) {
+        $schoolTransitioned[$id] = false;
+    } else {
+        $schoolTransitioned[$id] = true;
+    }
+}
+
+$books = [
+    4   => 1,
+    5   => 2,
+    6   => 3,
+    7   => 4,
+    8   => 5
+];
+
+$grand_totals = [
+    1   => 0,
+    2   => 0,
+    3   => 0,
+    4   => 0,
+    5   => 0
+];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?=$year?> Yahadus Book Purchases</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/admin_styles.css" rel="stylesheet" type="text/css" />
+    <style>
+        table { width: 100%; }
+        th, td { border: 1px solid #888; padding: 4px 8px; }
+    </style>
+</head>
+<body>
+    <?php include( __DIR__ . '/../../admin_header.php'); ?>
+    <h1><?=$year?> Yahadus Book Purchases</h1>
+    <h2>Details</h2>
+    <table>
+        <thead>
+            <th>School</th>
+            <th>Student</th>
+            <th>Grade</th>
+            <th>Book #</th>
+            <th>Transitioned Grades</th>
+        </thead>
+        <tbody>
+            <?php
+                foreach( $data as $school_id => $info ) {
+                    $totals[$school_id] = [
+                        1   => 0,
+                        2   => 0,
+                        3   => 0,
+                        4   => 0,
+                        5   => 0
+                    ];
+                    foreach ( $info as $base ) { 
+                        $grade = $base['class_grade'];
+                        $book = $books[$base['class_grade']];
+                        // if platoon transition was not done, grade and book should be moved one higher
+                        if ( !$schoolTransitioned[$school_id] ) {
+                            $grade++;
+                            $book++;
+                        }
+                        echo "<tr><td>" . $base['school_name'] . "</td><td>" . $base['first'] . ' ' . $base['last'] . "</td><td>" . $grade . "</td><td>" . $book . "</td><td>";
+                        if ( $schoolTransitioned[$school_id] ) echo "yes";
+                        else echo "no";
+                        echo "</td></tr>";
+
+                        // add to base and army totals
+                        $totals[$school_id][$book]++;
+                        $grand_totals[$book]++;
+                    }       
+                } 
+            ?>
+        </tbody>
+    </table>
+    <h2>Grand Totals</h2>
+    <table>
+        <thead>
+            <th>Book #</th>
+            <th>Total</th>
+        </thead>
+        <tbody>
+            <?php
+                foreach ( $grand_totals as $book => $total ) {
+                    echo "<tr><td>" . $book . "</td><td>" . $total . "</td></tr>";
+                }
+            ?>
+        </tbody>
+    </table>
+
+    <h2>Totals Per School</h2>
+    <?php
+        foreach ( $totals as $school => $info ) {
+            echo "<h2>" . $schools[$school] . "</h2>";
+            ?>
+            <table>
+                <thead>
+                    <th>Book #</th>
+                    <th>Total</th>
+                </thead>
+                <tbody>
+                    <?php
+                        foreach ( $info as $book => $total ) {
+                            echo "<tr><td>" . $book . "</td><td>" . $total . "</td></tr>";
+                        }
+                    ?>
+                </tbody>
+            </table>
+            <?php
+        }
+    ?>
+</body>
+</html>
