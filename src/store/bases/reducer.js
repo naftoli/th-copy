ca98bf51577682af5f282a44e0394568e6a1cc17
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-22 15:33:45 -0400

    same fix as soldiers

--- src/store/bases/reducer.js
+++ src/store/bases/reducer.js
@@ -7,30 +7,24 @@ export const initialState = {
 
 export default ( state = initialState, action ) => {
   switch ( action.type ) {
+    
     case types.SET_LOADING:
       return Object.assign({}, state, {
         loading: action.payload
       });
+
     case types.SET_BASES:
       return Object.assign({}, state, {
         bases: action.payload
       });
+
     case types.UPDATE_BASE:
-      let updated_bases = state.bases.concat( action.payload.updates );
-      action.payload.id = parseInt( action.payload.id, 10 );
-      // if we have the base in our bases array, update that base.
-      if ( state.bases.find( base => base.class_id === action.payload.id ) ) {
-        updated_bases = state.bases.map( 
-          base => ( base.class_id === action.payload.id ?
-            Object.assign( {}, base, action.payload.updates ) :
-            base
-          )
-        );
-      }
-      // and update the state
-      return Object.assign({}, state, {
-        bases: updated_bases
-      });
+      return {
+        bases: state.bases.map( 
+          base => ( base.school_id === action.payload.id ? { ...base, ...action.payload.updates } : base )
+        )
+      };
+
     default:
       return state; 
   }
