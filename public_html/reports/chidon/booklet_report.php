377530681d9332af76295b8768083c1e40c50567
naftolir <naftolir@gmail.com>
2018-08-22 12:14:47 -0400

    added choosing dates
    Former-commit-id: 1e014684ba20570c812b0fe413001c75ef77f12a

--- public_html/reports/chidon/booklet_report.php
+++ public_html/reports/chidon/booklet_report.php
@@ -7,14 +7,20 @@ require_once ( __DIR__ . '/../../class.globalSettings.php' );
 $year = GlobalSettings::getRegistrationYear();
 
 $booklet_users = [];
-$booklet_users_query = mysql_query(
-    "SELECT amount, date, year, schools.school_id, school_name, logo, first, last, c.class_grade, c.class_sub "
-    ."FROM registration_charges JOIN schools USING (school_id) "
+$qry = "SELECT amount, date, year, schools.school_id, school_name, logo, first, last, c.class_grade, c.class_sub "
+    ."FROM registration_charges rc JOIN schools USING (school_id) "
     ."JOIN users USING (user_id) " 
     ."JOIN classes c ON c.class_id = users.class_id " 
     ."WHERE type = 'chidon' " 
-    ."AND year = $year ORDER BY school_name, first, last, date;"
-);
+    ."AND year = $year ";
+if (isset($_POST['fromDate']) && $_POST['fromDate'] && isset($_POST['toDate']) && $_POST['toDate']) {
+    $from = mysql_real_escape_string( $_POST['fromDate'] );
+    $to = mysql_real_escape_string( $_POST['toDate'] );
+    $qry .= "AND rc.date >= '" . $from . " 00:00:00' AND rc.date <= '" . $to . " 23:59:59' ";
+}
+$qry .= "ORDER BY school_name, first, last, date";
+//echo $qry;
+$booklet_users_query = mysql_query( $qry );
 while ( $row = mysql_fetch_assoc( $booklet_users_query ) ) {
     $booklet_users[$row['school_id']][] = $row;
 }
@@ -43,6 +49,16 @@ $booklets = array(
 <body>
     <?php include( __DIR__ . '/../../admin_header.php'); ?>
     <h1>Chidon Booklet Report</h1>
+    <form action="booklet_report.php" method="post">
+        <p>
+            To have report based on dates, choose starting and ending dates and then click "Refresh Report"
+        </p>  
+        <p>  
+            From Date: <input type="date" name="fromDate" /> 
+            To Date: <input type="date" name="toDate" />
+        </p>
+        <input type="submit" name="submit" value="Refresh Report" />
+    </form>
     <?php
         $booklet_grand_totals = array();
         foreach( $booklet_users as $school_id => $users ) {
