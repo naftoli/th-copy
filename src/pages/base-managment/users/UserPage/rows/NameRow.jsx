7ea065769c3220c8096844450be15e28c0eca8eb
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-22 15:30:28 -0400

    remove auto hebrew due to cursor bug

--- src/pages/base-managment/users/UserPage/rows/NameRow.jsx
+++ src/pages/base-managment/users/UserPage/rows/NameRow.jsx
@@ -1,41 +1,36 @@
 import React from 'react';
 // components
 import { Row, Col, Input } from 'reactstrap';
-// functions
-import { toHebrew } from 'functions/utils';
 
 const NameRow = ( { soldier, onChange, required } ) => {
   const { first, last, first_he, last_he } = soldier;
   // change the hebrew text
-  const hebrewChange = ( event ) => {
-    event.target.value = toHebrew( event.target.value );
-    onChange( event );
-  }
+  const inputProps = { onChange, required };
 
   return (
     <Row>
       <Col xs='6'>
         <label>First Name</label>
-        <Input id='first' value={ first } onChange={ onChange } required={ required } 
+        <Input id='first' value={ first } { ...inputProps }
           pattern='^[a-zA-Z\s]{3,}$' title="Three or more letters"/>
         <div className='invalid-message'>Please enter 3 or more letters</div>
       </Col>
       <Col xs='6'>
         <label>Last Name</label>
-        <Input id='last' value={ last } onChange={ onChange } required={ required }
+        <Input id='last' value={ last } { ...inputProps }
           pattern='^[a-zA-Z\s]{3,}$' title="Three or more letters"/>
         <div className='invalid-message'>Please enter 3 or more letters</div>
       </Col>
       <Col xs='6' dir='rtl'>
         <label>שם פרטי (First Name)</label>
-        <Input id='first_he' value={ first_he } onChange={ hebrewChange } required={ required }
-          pattern='^[א-ת\s]{3,}$' title="Three or more Hebrew letters"/>
+        <Input id='first_he' value={ first_he } { ...inputProps }
+          pattern='^[^a-zA-Z]{3,}$' title="Three or more Hebrew letters"/>
         <div className='invalid-message'>Please enter 3 or more <em>Hebrew</em> letters</div>
       </Col>
       <Col xs='6' dir='rtl'>
         <label>שם משפחה (Last Name)</label>
-        <Input id='last_he' value={ last_he } onChange={ hebrewChange } required={ required }
-          pattern='^[א-ת\s]{3,}$' title="Three or more Hebrew letters"/>
+        <Input id='last_he' value={ last_he } { ...inputProps }
+          pattern='^[^a-zA-Z]{3,}$' title="Three or more Hebrew letters"/>
         <div className='invalid-message'>Please enter 3 or more <em>Hebrew</em> letters</div>
       </Col>
     </Row>
