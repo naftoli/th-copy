fc99de92b25f6cae9a2cef8f5ef7f4f0ad9d09eb
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-22 15:34:04 -0400

    update a base ( with notifications? )

--- src/store/bases/operations.js
+++ src/store/bases/operations.js
@@ -1,6 +1,7 @@
 import API, { handleAPIResponse } from 'api/api';
-// import { toast } from 'react-toastify';
 import * as actions from './actions';
+// functions
+import { createNotifcation, updateNotifcation } from 'functions/notifications';
 
 // get all bases
 export const getBases = () => dispatch => {
@@ -14,13 +15,29 @@ export const getBases = () => dispatch => {
     });
 }
 
-
-//********************** DOES NOT CONNECT TO REDUX **********************/
 // get a single base
-export const getBase = id => {
+export const getBase = id => dispatch => {
   return API.get( `/core/bases?id=${id}` )
   .then( handleAPIResponse );
 }
+
+// update a single base
+export const updateBase = ( id, data ) => dispatch => {
+  const toast_id = createNotifcation( 'Updating Base' );
+  return API.post( `/core/bases?id=${id}`, data )
+  .then( response => { 
+    updateNotifcation( toast_id, 'Base Updated!', response.message, response.success );
+    dispatch( actions.updateBase( id, response.data ) ); 
+    return response.data;
+  })
+  .catch( error => {
+    updateNotifcation( toast_id, '', error.message, false );
+    return Promise.reject( error );
+  });
+}
+
+//********************** DOES NOT CONNECT TO REDUX **********************/
+
 // this function is used in platoonSelect only
 export const getBaseList = ( all = false ) => {
   return API.post( `/core/bases?action=small`, { all } )
