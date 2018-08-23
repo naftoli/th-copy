e4fd7109d0829b41c24b2e2cd97e3c4b9eb67a2a
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-22 20:56:18 -0400

    update school logo from index page

--- src/pages/base-managment/base/BasesPage.jsx
+++ src/pages/base-managment/base/BasesPage.jsx
@@ -4,16 +4,21 @@ import { connect } from 'react-redux';
 // components
 import { Link } from 'react-router-dom';
 import { Button, ButtonGroup } from 'reactstrap';
-import { Table, InlineSync, FontAwesome, Number } from 'components/ui';
+import CropperModal from 'components/modals/CropperModal';
+import { Table, InlineSync, FontAwesome, Number, ProfilePicture } from 'components/ui';
 // functions
 import { loginStoreChanged, isAdmin } from 'functions/login';
 import { arrayToCSV, setTitle, canDownload } from 'functions/utils';
 // state
-import { getBases } from 'store/bases/operations';
+import { getBases, updateBase } from 'store/bases/operations';
 
 class BasesPage extends Component {
 
   static propTypes = {};
+
+  state = { 
+    modal: { show: false, id: false, src: false } 
+  };
 
   componentDidMount() { 
     setTitle( 'View / Edit Bases' );
@@ -32,11 +37,33 @@ class BasesPage extends Component {
     }
     this.props.getBases();
   }
+  // update base logos from master page
+  toggle = () => this.setState({ modal: { ...this.state.modal, show: !this.state.modal.show } });
+  editPicture = id => ({ target }) => this.setState({ modal: { show: true, id, src: target.src } });
+  upload = formData => this.props.updateBase( this.state.modal.id, formData );
+
+  toCSV = () => {
+    const headers = [
+      'Base Number', 'Base Name', 'Base City', 'Base State', 'Base Country', 'Soldiers', 
+    ];
+    // get the data
+    const rows = this.props.bases.map( base => [
+      base.school_number, base.school_name, base.school_city, base.school_state, base.school_country, base.soldier_count
+    ]);
+    arrayToCSV( headers, rows, 'bases' );
+  }
 
   render() {
     const { bases, loading, match } = this.props;
+    const { modal } = this.state;
 
     let columns = [
+      {
+        Header: 'Logo',  accessor: 'logo',
+        Cell: props => <ProfilePicture src={ `/schoolLogos/${props.value}` } className='inline-profile' 
+                          onClick={ this.editPicture( props.original.school_id ) }/>,
+        className: 'profile-picture', width: 85, sortable: false, filterable: false
+      },
       { Header: 'Base Number', accessor: 'school_number',
         Cell: props => <Link to={`${match.path}/${props.original.school_id}`}>{props.value}</Link> },
       { Header: 'Base Name', accessor: 'school_name',
@@ -70,6 +97,11 @@ class BasesPage extends Component {
           loading={ loading && !bases.length } 
           pageId='BasesPage' />
 
+        <CropperModal 
+          fileName='logo' viewMode={0}
+          isOpen={ modal.show } src={ modal.src } 
+          toggle={ this.toggle } uploadImage={ this.upload } />
+
       </div>
     );
   }
@@ -80,7 +112,7 @@ const mapStateToProps = ({ bases, login }) => ({
   login: login.current_login
 });
 
-const mapDispatchToProps = { getBases };
+const mapDispatchToProps = { getBases, updateBase };
 
 export default connect(
   mapStateToProps, mapDispatchToProps
