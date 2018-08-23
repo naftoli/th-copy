1e4fdf93876db6bb4ce2d0071b651e0a5a052e53
naftolir <naftolir@gmail.com>
2018-08-23 12:59:17 -0400

    fixed up reports and added dates to yahadus book report
    Former-commit-id: 7080d15cc6398ea85c096edd8bb551daa7acef60

--- public_html/reports/chidon/yahadus_books.php
+++ public_html/reports/chidon/yahadus_books.php
@@ -5,19 +5,26 @@ require_once ( __DIR__ . '/../../header.php' );
 require_once ( __DIR__ . '/../../class.globalSettings.php' ); 
 $year = GlobalSettings::getRegistrationYear();
 
-$main_query = mysql_query(
-    "SELECT rc.date, u.first, u.last, s.school_id, s.school_name, c.class_grade 
-    FROM registration_charges rc 
-    JOIN users u USING (user_id) 
-    JOIN schools s ON s.school_id = u.school_id 
-    JOIN classes c ON c.class_id = u.class_id 
-    WHERE type = 'yahadus' 
-    ORDER BY school_name"
-);
+$qry = "SELECT rc.date, u.first, u.last, s.school_id, s.school_name, c.class_grade 
+        FROM registration_charges rc 
+        JOIN users u USING (user_id) 
+        JOIN schools s ON s.school_id = u.school_id 
+        JOIN classes c ON c.class_id = u.class_id 
+        WHERE type = 'yahadus' ";
+// limit to dates if limit exists
+if (isset($_POST['fromDate']) && $_POST['fromDate'] && isset($_POST['toDate']) && $_POST['toDate']) {
+    $from = mysql_real_escape_string( $_POST['fromDate'] );
+    $to = mysql_real_escape_string( $_POST['toDate'] );
+    $qry .= "AND rc.date >= '" . $from . " 00:00:00' AND rc.date <= '" . $to . " 23:59:59' ";
+}
+$qry .= "ORDER BY school_name";
+$main_query = mysql_query( $qry );
 
 $data = [];
+$schools = [];
 while( $row = mysql_fetch_assoc( $main_query ) ) {
     $data[$row['school_id']][] = $row;
+    $schools[$row['school_id']] = $row['school_name'];
 }
 
 $books = [
@@ -52,7 +59,19 @@ $grand_totals = [
 <body>
     <?php include( __DIR__ . '/../../admin_header.php'); ?>
     <h1><?=$year?> Yahadus Book Purchases</h1>
-    <h2>Details</h2>
+    <form action="yahadus_books.php" method="post">
+        <p>
+            To have report based on dates, choose starting and ending dates and then click "Refresh Report"
+        </p>  
+        <p>  
+            From Date: <input type="date" name="fromDate" /> 
+            To Date: <input type="date" name="toDate" />
+        </p>
+        <input type="submit" name="submit" value="Refresh Report" />
+    </form>
+    <div style="page-break-after: always;"></div>
+
+    <h2>Yahadus Book Purchase Details</h2>
     <table>
         <thead>
             <th>School</th>
@@ -96,8 +115,8 @@ $grand_totals = [
             ?>
         </tbody>
     </table>
-
-    <h2>Totals Per School</h2>
+    <div style="page-break-after: always;"></div>
+    
     <?php
         foreach ( $totals as $school => $info ) {
             echo "<h2>" . $schools[$school] . "</h2>";
@@ -115,6 +134,7 @@ $grand_totals = [
                     ?>
                 </tbody>
             </table>
+            <div style="page-break-after: always;"></div>
             <?php
         }
     ?>
