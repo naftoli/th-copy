1aedab1ed37707e4fe8e70c3d3dc299002049e62
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-24 18:23:51 -0400

    refactor settings tab

---
+++ src/pages/base-managment/base/rows/SettingsRow.jsx
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+// components
+import DatePicker from 'react-datepicker';
+import { Row, Col } from 'reactstrap';
+import { Radio, Checkbox } from 'components/inputs';
+// functions
+import julian from 'julian';
+import moment from 'moment';
+import { toJulian } from 'functions/utils';
+import { eventToUpdate, handleCheckbox } from 'functions/events';
+
+export class SettingsRow extends Component {
+
+  onChange = ({ target }) => {
+    this.props.onUpdate( eventToUpdate( target, 'name' ) );
+  }
+
+  disableSchoolReset = () => {
+    const store_reset = this.props.base.store_reset > 0 ? 0 : toJulian( moment() );
+    this.props.onUpdate({ store_reset });
+  }
+
+  handleCheckbox = handleCheckbox( this.props.onUpdate );
+
+  render () {
+    let { 
+      store_reset, school_gender, allow_parent_tasks, print_parent_tasks, 
+      chayolei, chidon, tanya, tehillim, ckids
+    } = this.props.base;
+
+    store_reset = store_reset > 0 ? moment( julian.toDate( store_reset ) ) : undefined;
+    // props for all inputs
+    const checkboxProps = { onChange: this.handleCheckbox };
+    
+    return (
+      <Row id='SettingsRow'>
+        <Col xs={12} sm={4} xl={3}>
+          <label>Base Gender</label>
+          <div id='gender-row'>
+            <Radio type='radio' name='school_gender' id='school_gender' value='M' 
+                checked={ school_gender === 'M' } onChange={ this.onChange }>
+              Boys
+            </Radio>
+            <Radio type='radio' name='school_gender' id='school_gender' value='F'
+                checked={ school_gender === 'F' } onChange={ this.onChange }>
+              Girls
+            </Radio>
+            <Radio type='radio' name='school_gender' id='school_gender' value='B'
+                checked={ school_gender === 'B' } onChange={ this.onChange }>
+              Both
+            </Radio>
+          </div>
+        </Col>
+        <Col xs={12} sm={8} xl={5}>
+          <label>Base Enrolled in:</label><br/>
+          <Checkbox checked={!!chayolei} name='chayolei' { ...checkboxProps} >
+            Chayolei
+          </Checkbox>
+          <Checkbox checked={!!chidon} name='chidon' { ...checkboxProps} >
+            Chidon
+          </Checkbox>
+          <Checkbox checked={!!tanya} name='tanya' { ...checkboxProps} >
+            Tanya
+          </Checkbox>
+          <Checkbox checked={!!tehillim} name='tehillim' { ...checkboxProps} >
+            WWTC
+          </Checkbox>
+          <Checkbox checked={!!ckids} name='ckids' { ...checkboxProps} >
+            C-Kids
+          </Checkbox>
+        </Col>
+        <Col xs={12} sm={6} xl={4}>
+          <label>Custom Parent Tasks</label><br/>
+          <Checkbox checked={!!allow_parent_tasks} name='allow_parent_tasks' { ...checkboxProps} >
+            Allow
+          </Checkbox>
+          <Checkbox checked={!!print_parent_tasks} name='print_parent_tasks' { ...checkboxProps} >
+            Print on Mission Sheets
+          </Checkbox>
+        </Col>
+        <Col xs={12} lg={6} id='store-miles'>
+          <label>Store Miles Start From:</label>
+          <DatePicker className='form-control' disabled 
+            // display formats
+            dateFormat='LL' readOnly showMonthDropdown showYearDropdown dropdownMode='select'
+            // current date and handle change
+            selected={ store_reset } onChange={ this.onDateChage } 
+          />
+          <Checkbox checked={ !store_reset } onChange={ this.disableSchoolReset }>
+            Allow soldiers to spend all their miles.
+          </Checkbox>
+        </Col>
+      </Row>
+);
+  }
+}
--- src/pages/base-managment/base/tabs/SettingsTab.jsx
+++ src/pages/base-managment/base/tabs/SettingsTab.jsx
@@ -1,100 +1,23 @@
 import React, { Component } from 'react';
 // components
-import DatePicker from 'react-datepicker';
-import { Row, Col, TabPane } from 'reactstrap';
+import { TabPane } from 'reactstrap';
 import { SaveButton } from 'components/buttons';
-import { Radio, Checkbox, Form } from 'components/inputs';
-// functions
-import julian from 'julian';
-import moment from 'moment';
-import { toJulian } from 'functions/utils';
-import { eventToUpdate, handleCheckbox } from 'functions/events';
-
+import { Form } from 'components/inputs';
+import { SettingsRow } from '../rows';
 export class SettingsTab extends Component {
 
-  onChange = ({ target }) => {
-    this.props.onUpdate( eventToUpdate( target, 'name' ) );
-  }
-
-  handleCheckbox = handleCheckbox( this.props.onUpdate );
-
-  disableSchoolReset = () => {
-    const store_reset = this.props.base.store_reset > 0 ? 0 : toJulian( moment() );
-    this.props.onUpdate({ store_reset });
-  }
-
-
   render(){
-    const { updated, tabId, base, onSubmit, onValidChange } = this.props;
-
-    let { 
-      store_reset, school_gender, allow_parent_tasks, print_parent_tasks, 
-      chayolei, chidon, tanya, tehillim, ckids
-    } = base;
-    
-    store_reset = store_reset > 0 ? moment( julian.toDate( store_reset ) ) : undefined;
+    const { 
+      updated, tabId, base, onSubmit, onValidChange, onUpdate
+    } = this.props;
 
     return (
       <TabPane tabId={ tabId }>
         <Form id='SettingsTab' onSubmit={ onSubmit } onValidChange={ onValidChange }>
-          <Row id='image-row'>
-            <Col xs={12} sm={4} xl={2}>
-              <label>Base Gender</label>
-              <div id='gender-row'>
-                <Radio type='radio' name='school_gender' id='school_gender' value='M' 
-                    checked={ school_gender === 'M' } onChange={ this.onChange }>
-                  Boys
-                </Radio>
-                <Radio type='radio' name='school_gender' id='school_gender' value='F'
-                    checked={ school_gender === 'F' } onChange={ this.onChange }>
-                  Girls
-                </Radio>
-                <Radio type='radio' name='school_gender' id='school_gender' value='B'
-                    checked={ school_gender === 'B' } onChange={ this.onChange }>
-                  Both
-                </Radio>
-              </div>
-            </Col>
-            <Col xs={12} sm={8} xl={4}>
-              <label>Base Enrolled in:</label><br/>
-              <Checkbox checked={!!chayolei} name='chayolei' onChange={this.handleCheckbox}>
-                Chayolei
-              </Checkbox>
-              <Checkbox checked={!!chidon} name='chidon' onChange={this.handleCheckbox}>
-                Chidon
-              </Checkbox>
-              <Checkbox checked={!!tanya} name='tanya' onChange={this.handleCheckbox}>
-                Tanya
-              </Checkbox>
-              <Checkbox checked={!!tehillim} name='tehillim' onChange={this.handleCheckbox}>
-                WWTC
-              </Checkbox>
-              <Checkbox checked={!!ckids} name='ckids' onChange={this.handleCheckbox}>
-                C-Kids
-              </Checkbox>
-            </Col>
-            <Col xs={12} sm={6} xl={3}>
-              <label>Custom Parent Tasks</label><br/>
-              <Checkbox checked={!!allow_parent_tasks} name='allow_parent_tasks' onChange={this.handleCheckbox}>
-                Allow
-              </Checkbox>
-              <Checkbox checked={!!print_parent_tasks} name='print_parent_tasks' onChange={this.handleCheckbox}>
-                Print on Mission Sheets
-              </Checkbox>
-            </Col>
-            <Col xs={12} lg={6} id='store-miles'>
-              <label>Store Miles Start From:</label>
-              <DatePicker className='form-control' 
-                // display formats
-                dateFormat='LL' readOnly showMonthDropdown showYearDropdown dropdownMode='select'
-                // current date and handle change
-                selected={ store_reset } onChange={ this.onDateChage } 
-              />
-              <Checkbox checked={ !store_reset } onChange={ this.disableSchoolReset }>
-                Allow soldiers to spend all their miles.
-              </Checkbox>
-            </Col>
-          </Row>
+
+          <SettingsRow
+            base={ base }
+            onUpdate={ onUpdate } />
 
           <SaveButton show={ updated } />
         </Form>
