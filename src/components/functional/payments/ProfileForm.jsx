2848e73a4f8ccf428e4c51ac79f030800488daa3
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-28 10:41:15 -0400

    API returns strings to avoid errors

--- src/components/functional/payments/ProfileForm.jsx
+++ src/components/functional/payments/ProfileForm.jsx
@@ -43,7 +43,7 @@ class ProfileForm extends Component {
   onChange = ( event ) => {
     const { onProfileSelected } = this.props;
     if ( event.target.value ) {
-      return onProfileSelected( parseInt( event.target.value, 10 ) );
+      return onProfileSelected( event.target.value );
     }
     onProfileSelected( false );
   }
