d6982c47c21a4d5dd1f6ff3a13f554b05b8d4446
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-29 11:22:35 -0400

    add pattern.

--- src/components/inputs/Password.jsx
+++ src/components/inputs/Password.jsx
@@ -52,7 +52,8 @@ export class Password extends Component {
           </InputGroupAddon>
         }
         <input className='form-control' placeholder='Password' type={ inputType } required={ required }
-          onChange={ onChange } value={ value } name='password' ref={ this.passwordRef } />
+          onChange={ onChange } value={ value } name='password' ref={ this.passwordRef } 
+          pattern='^[\w\s]{4,}$' title='4 or more characters'/>
         { showToggle &&
           <InputGroupAddon addonType="append">
             <Button color='primary' onClick={ this.togglePassword } id='toggle-password' 
