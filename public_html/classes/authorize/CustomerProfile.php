a17e28b5e71cc8d8c4ab74946030a8f58c55d69b
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-30 18:05:27 -0400

    change to empty array
    Former-commit-id: e220e154318f617a77b00bc0c32454f137b1232c

--- public_html/classes/authorize/CustomerProfile.php
+++ public_html/classes/authorize/CustomerProfile.php
@@ -165,7 +165,7 @@ class CustomerProfile {
             $profile = $api_data['profile'];
             
             // and set all the internal variables to the API data.
-            $this->paymentProfiles = isset( $profile['paymentProfiles'] ) ? $profile['paymentProfiles'] : null;
+            $this->paymentProfiles = isset( $profile['paymentProfiles'] ) ? $profile['paymentProfiles'] : [];
             
             $this->customerProfileId = $profile['customerProfileId'];
             $this->description = isset( $profile['description'] ) ? $profile['description'] : false;
