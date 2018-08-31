6acd9a14307cf50946251ba39693a8ce10f68a1c
Menachem Hornbacher <mendelh1537@gmail.com>
2018-08-31 00:49:53 -0400

    view create and update tasks
    Former-commit-id: b47cc898946c019fb588ec52a8f6de4e44ebf5cf

--- public_html/api/rewards/achievement_tasks.php
+++ public_html/api/rewards/achievement_tasks.php
@@ -6,40 +6,76 @@ class TasksRouter {
 
     public function index() {
         global $current_user;
+        $login = $current_user->login;
 
         $inst_id = 2; // default institution id.
 
-        $base_filter = 'base = 1';
-        $platoon_filter = 'platoon = 1';
+        $base_filter = false;
+        $platoon_filter = false;
         try {
-            if ( $current_user->login['code'] == 'INST' ) {
-                $inst_id = $current_user->login['id'];
-                $base_filter .= " OR base IN ( SELECT school_id FROM schools WHERE inst_id = $inst_id ) ";
-            } else if ( $current_user->login['code'] == 'BC' ) {
-                $inst_id = School::find( $current_user->login['id'] )->inst_id;
-                $base_filter .= ' OR base = ' . $current_user->login['id'];
-            } else if ( $current_user->login['code'] == 'TEACHER' ) {
-                $platoon = Platoon::find( $current_user->login['id'] );
+            if ( $login['code'] == 'HQ' ) {
+                $base_filter = 'base IN ( SELECT school_id FROM schools WHERE test_school = 0 )';
+            } else if ( $login['code'] == 'INST' ) {
+                $inst_id = $login['id'];
+                $base_filter .= "base IN ( SELECT school_id FROM schools WHERE inst_id = $inst_id ) ";
+            } else if ( $login['code'] == 'BC' ) {
+                $inst_id = School::find( $login['id'] )->inst_id;
+                $base_filter .= 'base = ' . $login['id'];
+                $platoon_filter .= 'platoon IN ( SELECT class_id FROM classes WHERE school_id = "' . $login['id'] . '") ';
+            } else if ( $login['code'] == 'TEACHER' ) {
+                $platoon = Platoon::find( $login['id'] );
                 $inst_id = $platoon->school->inst_id;
-                $base_filter .= ' OR base = ' . $school->school_id;
-                $platoon_filter .= ' OR platoon = ' . $platoon->class_id;
+                $base_filter .= 'base = "' . $platoon->school->school_id . '"';
+                $platoon_filter .= 'platoon = "' . $platoon->class_id . '"';
             } 
-        } catch ( Exception $e ) {};
+        } catch ( Exception $e ) { return json_error( 'Could not get Achivement Filters.' ); };
 
         $tasks = AchievementTask::all([
             'joins' => 'JOIN subjects s USING ( subject_id )',
             'conditions' => 
                 "inst_id IN (0, $inst_id) "
                 ." AND s.subject_type IN ('', 'WWTC', 'Tanya', 'achievement')"
-                ." AND ( $base_filter ) AND ( $platoon_filter )",
-            'include' => [ 'subject' ],
+                .( $base_filter ? " AND ( base = 1 OR $base_filter ) " : '' )
+                .( $platoon_filter ? " AND ( platoon = 1 OR $platoon_filter ) " : '' ),
+            'include' => [ 'subject', 'school', 'class' ],
+            'order' => 'base, platoon'
         ]);
 
         json_response( $tasks );
     }
 
     public function create() {
-        json_response( $_POST );
+        global $current_user;
+        $login = $current_user->login;
+
+        try {
+            $task = new AchievementTask( $_POST );
+        } catch ( Exception $e ) { json_error( 'Invalid Request' ); }
+
+        if ( $login['code'] == 'BC' ) {
+            $task->base = $login['id'];
+        } else if ( $login['code'] == 'TEACHER') {
+            $task->base = Platoon::find( $login['id'] )->school->school_id;
+            $task->platoon = $login['id'];
+        }
+
+        if ( !$task->save() )
+            json_error( 'Could not create Task.' );
+        json_response( $task );
+    }
+
+    public function update( $id ) {
+        $task = AchievementTask::find( $id );
+
+        $keys = ['subject_id', 'task', 'points'];
+        foreach( $keys as $key ){
+            if ( isset( $_POST[$key] ) )
+                $task->{$key} = $_POST[$key];
+        }
+        
+        if ( !$task->save() )
+            json_error( 'Could not update Task.' );
+        json_response( $task );
     }
 }
 
