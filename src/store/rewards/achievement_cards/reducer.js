4c515701aeddc6976b26cdafdcce757464a09dd4
Menachem Hornbacher <mendelh1537@gmail.com>
2018-09-02 14:34:17 -0400

    whoops

--- src/store/rewards/achievement_cards/reducer.js
+++ src/store/rewards/achievement_cards/reducer.js
@@ -25,7 +25,7 @@ export default ( state = initialState, action ) => {
     case types.SET_MILES:
       return { 
         ...state, 
-        miles: action.miles
+        miles: action.payload
       };
 
     default:
