10fbdd4212ba0c2ed1bd4a701f8289011fbc64ea
naftolir <naftolir@gmail.com>
2018-09-06 15:15:37 -0400

    validating amount
    Former-commit-id: fbf26118b696164db29def960f586fd2eff067ad

--- public_html/mobile/reg/ajax/lulavPayment.php
+++ public_html/mobile/reg/ajax/lulavPayment.php
@@ -19,53 +19,60 @@ $state = "";
 
 $description = "Mivtza Lulav 5779 purchase - Admin ID: " . $admin_id . "; Users: " . implode(',', $info['users']);
 
-chdir('../../../');
-require_once 'authorize.php';
-chdir('mobile/reg/ajax/');
+if ( $amount > 0 ) {
+    chdir('../../../');
+    require_once 'authorize.php';
+    chdir('mobile/reg/ajax/');
 
-if ($response_array[0] == 1) { // success
-    $strResponse =  $response_array[3] . ':' . 
-					$response_array[4] . ':' . 
-					$response_array[6] . ':' . 
-                    $response_array[9];	
+    if ($response_array[0] == 1) { // success
+        $strResponse =  $response_array[3] . ':' . 
+                        $response_array[4] . ':' . 
+                        $response_array[6] . ':' . 
+                        $response_array[9];	
 
-    $qry = $MASHPIA_DB->prepare(
-        "INSERT into lulav_purchases 
-        SET admin_id = :admin, 
-        amount_paid = :amount, 
-        authorization = :auth, 
-        users = :users, 
-        year = :year"
-    );
-    $qry->execute([
-        ':admin'    => $admin_id, 
-        ':amount'   => $amount, 
-        ':auth'     => $strResponse, 
-        ':users'    => implode(',', $info['users']), 
-        ':year'     => $year
-    ]);
-    
-    $qry = $MASHPIA_DB->prepare(
-        "INSERT INTO transactions 
-        SET trans_date = now(),
-        admin_id = :admin, 
-        description = :desc,
-        amount = :amount,
-        response = :response"
-    );
-    $qry->execute([ 
-        ':admin'    => $admin_id, 
-        ':desc'     => $description, 
-        ':amount'   => $amount, 
-        ':response' => $strResponse
-    ]);
-    
-    echo json_encode([
-        'success' => true
-    ]);
+        $qry = $MASHPIA_DB->prepare(
+            "INSERT into lulav_purchases 
+            SET admin_id = :admin, 
+            amount_paid = :amount, 
+            authorization = :auth, 
+            users = :users, 
+            year = :year"
+        );
+        $qry->execute([
+            ':admin'    => $admin_id, 
+            ':amount'   => $amount, 
+            ':auth'     => $strResponse, 
+            ':users'    => implode(',', $info['users']), 
+            ':year'     => $year
+        ]);
+        
+        $qry = $MASHPIA_DB->prepare(
+            "INSERT INTO transactions 
+            SET trans_date = now(),
+            admin_id = :admin, 
+            description = :desc,
+            amount = :amount,
+            response = :response"
+        );
+        $qry->execute([ 
+            ':admin'    => $admin_id, 
+            ':desc'     => $description, 
+            ':amount'   => $amount, 
+            ':response' => $strResponse
+        ]);
+        
+        echo json_encode([
+            'success' => true
+        ]);
+    } else {
+        echo json_encode([
+            'success' => false,
+            'error' => $response_array[3]
+        ]);
+    }
 } else {
     echo json_encode([
-        'success' => false,
-        'error' => $response_array[3]
-    ]);
+        'success'   => false,
+        'error'     => "You have not selected anything to purchase.";
+    ])
 }
