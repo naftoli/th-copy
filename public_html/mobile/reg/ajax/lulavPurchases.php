03b1902d3f9b5db813f972f17e208407908b4c43
naftolir <naftolir@gmail.com>
2018-09-06 13:37:06 -0400

    mivtzah lulav finalizing
    Former-commit-id: fef0552e2b6f8fafc242a21cb3b6e832b1b6cefe

---
+++ public_html/mobile/reg/ajax/lulavPurchases.php
@@ -0,0 +1,8 @@
+<?php
+require '../../../db.php';
+
+$sql = "select count(*) as sold from lulav_purchases";
+$result = mysql_query( $sql );
+$row = mysql_fetch_assoc( $result );
+
+echo $row['sold'];
