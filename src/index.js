9f5842b1485254519713e13753929dd445ebb516
Menachem Hornbacher <mendelh1537@gmail.com>
2018-09-12 19:06:45 -0400

    add language support again

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 // Global Styles
 import 'styles/styles.scss';
 // supported languages;
+import moment from 'moment';
+import 'moment/locale/en-ca.js';
+import 'moment/locale/en-il.js';
+import 'moment/locale/he.js';
+import 'moment/locale/fr.js';
 // React-Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -13,6 +18,7 @@ import App from './App';
 
 checkLogin( store.dispatch );
 // set the langauge to match the browser;
+moment.locale( navigator.language );
 
 ReactDOM.render(
   <Provider store={store}>
