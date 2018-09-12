9bafadf38485eb46cffa593fa8a87dc48a916000
Menachem Hornbacher <mendelh1537@gmail.com>
2018-09-12 16:19:57 -0400

    shamai updates

--- src/pages/home/widgets/QuickLinks.jsx
+++ src/pages/home/widgets/QuickLinks.jsx
@@ -42,19 +42,19 @@ export class Resources extends Component {
 
           <div id='links'>
             <a href='//dropbox.com/sh/c2g76cp76it1bf6/AABw7AHHEKWfahv-yIFXV8Qsa?dl=0' target='_blank' rel="noopener noreferrer">
-              <img src={`${LEGACY_URL}/homeIcons/Hachayol Header.gif`} alt='hachayol'/>
+              <img src={`${LEGACY_URL}/homeIcons/Hachayol.png`} alt='hachayol'/>
               Hachayol
             </a>
-            <a href='//dropbox.com/s/9h5k3bqrvm1qrjr/CTH%20-%20BC%20Calendar%205779%20with%20bleed%20HR.pdf?dl=0' target='_blank' rel="noopener noreferrer">
-              <img src={`${LEGACY_URL}/homeIcons/Calendar.gif`} alt='calendar'/>
-              Calendar
-            </a>
             <a href='//dropbox.com/sh/41u2regs73kfp9h/AACJV58J9KD6elXXZisYz74Ia?dl=0' target='_blank' rel="noopener noreferrer">
-              <img src={`${LEGACY_URL}/homeIcons/Tanya CD.gif`} alt='tanya'/>
+              <img src={`${LEGACY_URL}/homeIcons/tanya.png`} alt='tanya'/>
               Tanya
             </a>
+            <a href='//dropbox.com/s/9h5k3bqrvm1qrjr/CTH%20-%20BC%20Calendar%205779%20with%20bleed%20HR.pdf?dl=0' target='_blank' rel="noopener noreferrer">
+              <img src={`${LEGACY_URL}/homeIcons/Calendar.png`} alt='calendar'/>
+              Calendar
+            </a>
             <a href='//dropbox.com/sh/ztiltfbvpo4te9p/AABZjQmM71L5YESXllu1xjrIa?dl=0' target='_blank' rel="noopener noreferrer">
-              <img src={`${LEGACY_URL}/homeIcons/Chidon.gif`} alt='chidon'/>
+              <img src={`${LEGACY_URL}/homeIcons/Chidon.png`} alt='chidon'/>
               Yahadus
             </a>
           </div>
