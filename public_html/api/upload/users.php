f939cbaa780a3d1c9ae3394e9541b4b67626dc98
Menachem Hornbacher <mendelh1537@gmail.com>
2018-09-13 10:27:55 -0400

    invalid date support
    Former-commit-id: 2c9ac80a5fb3dfe388f2e654fe9515e231124b5a

--- public_html/api/upload/users.php
+++ public_html/api/upload/users.php
@@ -129,13 +129,17 @@ class UsersUploadRouter {
                         } catch ( Exception $e ) {
                             $errors[] ="$errorString Date of Birth must follow the format MM/DD/YYYY.";
                         }
-                        // check that it is in our year range
-                        $year = date('Y');
-                        $startYear = $year - 5; $endYear = $year - 15;
-                        if ( $dob->format('Y') > $startYear || $dob->format('Y') < $endYear ) {
-                            $errors[] ="$errorString Date of Birth must be between $startYear and $endYear. Year is " . $dob->format('Y');
+                        if ( $dob instanceof \DateTime ) {
+                            // check that it is in our year range
+                            $year = date('Y');
+                            $startYear = $year - 5; $endYear = $year - 15;
+                            if ( $dob->format('Y') > $startYear || $dob->format('Y') < $endYear ) {
+                                $errors[] ="$errorString Date of Birth must be between $startYear and $endYear. Year is " . $dob->format('Y');
+                            }
+                            $value = $dob->format('Y-m-d');
+                        } else {
+                            $errors[] ="$errorString Date of Birth must follow the format MM/DD/YYYY.";
                         }
-                        $value = $dob->format('Y-m-d');
                     }
 
                     // validate mission type
