99b98070d5796d287f3ae9cc661b1e808758983d
naftolir <naftolir@gmail.com>
2018-10-05 10:37:06 -0400

    removed echo statement
    Former-commit-id: 7d86b78857710267ca1f3dd589f2e4bdeb4ba38e

--- public_html/quota_cards.php
+++ public_html/quota_cards.php
@@ -24,7 +24,7 @@ $dates = $sm->getReportDates();
 end( $dates );
 $month = key( $dates );
 $date = $dates[$month];
-echo "<pre>"; print_r( $dates ); echo "</pre>"; exit;
+//echo "<pre>"; print_r( $dates ); echo "</pre>"; exit;
 ?>
 <!DOCTYPE html>
 <html>
