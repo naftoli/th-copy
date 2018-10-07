8a87ff77fe78400e3c2bb242452e7a8e1f55bb8b
naftolir <naftolir@gmail.com>
2018-10-07 19:04:07 -0400

    updates
    Former-commit-id: 29a6cc44a2ef0baae4bde20dd7f994f2ebf4f2ee

--- public_html/reports/chidon/class_leaderboard.php
+++ public_html/reports/chidon/class_leaderboard.php
@@ -29,24 +29,27 @@ $sql = "select s.school_name, c.class_grade, c.class_sub, c.class_teacher, u.use
         and u.class_id > 0 
         and s.school_era is null 
         and s.chayolei = 1 
-        and s.test_school != 1 
         and c.class_grade in ('4','5','6','7','8') 
         order by school_name, class_grade, class_sub, u.user_id";
 //echo $sql;
 $result = mysql_query( $sql );
 while ( $row = mysql_fetch_assoc( $result ) ) {
     // initialize vars if not set
-    if ( !isset( $info[$row['school_name']]['reg'] ) ) $info[$row['school_name']]['reg'] = 0;
-    if ( !isset( $info[$row['school_name']]['notReg'] ) ) $info[$row['school_name']]['notReg'] = 0;
+    if ( !isset( $info[$row['school_name']][$row['class_grade']][$row['class_sub']]['reg'] ) ) $info[$row['school_name']][$row['class_grade']][$row['class_sub']]['reg'] = 0;
+    if ( !isset( $info[$row['school_name']][$row['class_grade']][$row['class_sub']]['notReg'] ) ) $info[$row['school_name']][$row['class_grade']][$row['class_sub']]['notReg'] = 0;
     
-    if ($row['th_chidon_id']) $info[$row['school_name']]['reg']++;
-    else $info[$row['school_name']]['notReg']++;
+    if ($row['th_chidon_id']) $info[$row['school_name']][$row['class_grade']][$row['class_sub']]['reg']++;
+    else $info[$row['school_name']][$row['class_grade']][$row['class_sub']]['notReg']++;
 }
 
 $percentages = [];
-foreach ( $info as $school => $data ) {
-    $percent = round( ($data['reg'] / ($data['notReg'] + $data['reg']) * 100), 2 );
-    $percentages[$school] = $percent;
+foreach ( $info as $school => $more ) {
+    foreach ( $more as $grade => $other ) {
+        foreach ( $other as $sub => $data ) {
+            $percent = round( ($data['reg'] / ($data['notReg'] + $data['reg']) * 100), 2 );
+            $percentages[$school][$grade][$sub] = $percent;
+        }
+    }
 }
 arsort( $percentages );
 //echo "<pre>"; print_r( $percentages ); echo "</pre>";
@@ -77,14 +80,20 @@ arsort( $percentages );
         <table>
             <thead>
                 <tr>
-                    <th></th><th>School</th><th>Chayolim in Base</th><th>Chayolim Registered in Chidon</th><th>Percentage Registered</th>
+                    <th></th><th>School</th><th>Grade</th><th>Sub</th><th>Chayolim in Base</th><th>Chayolim Registered in Chidon</th><th>Percentage Registered</th>
                 </tr>
             </thead>
             <tbody>
                 <?php
                 $i = 1;
-                foreach ( $percentages as $school => $percent ) {            
-                    echo "<tr><td>#" . $i++ . "</td><td>" . $school . "</td><td>" . ($info[$school]['notReg'] + $info[$school]['reg']) . "</td><td>" . $info[$school]['reg'] . "</td><td>" . $percent . "</td></tr>";
+                foreach ( $percentages as $school => $more ) {     
+                    foreach ( $more as $grade => $other ) {
+                        foreach ( $other as $sub => $percent ) {
+                            echo "<tr><td>#" . $i++ . "</td><td>" . $school . "</td><td>" . $grade . "</td><td>" . $sub . "</td><td>";
+                            echo ($info[$school][$grade][$sub]['notReg'] + $info[$school][$grade][$sub]['reg']) . "</td><td>";
+                            echo $info[$school][$grade][$sub]['reg'] . "</td><td>" . $percent . "</td></tr>";
+                        }
+                    }       
                 }
                 ?>
             </tbody>
