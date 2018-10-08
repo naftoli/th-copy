105b927fee58b9cd7f21bec99e2f029ae9a35b73
naftolir <naftolir@gmail.com>
2018-10-07 20:40:56 -0400

    class leaderboard
    Former-commit-id: 42068a3872b177aaf1ce67e96cfdfe13743bec58

--- public_html/reports/chidon/class_leaderboard.php
+++ public_html/reports/chidon/class_leaderboard.php
@@ -7,7 +7,6 @@ if ($_POST['debug']) {
     error_reporting(E_ALL);
     ini_set("display_errors", 1);
     $debug = true; // set debug to true
-    echo "<pre>";
 }
 
 /***************** AUTHENTICATION **********************/
@@ -18,41 +17,61 @@ require_once($_SERVER["DOCUMENT_ROOT"].'/header.php');
 require_once($_SERVER['DOCUMENT_ROOT']."/class.globalSettings.php");
 $year = GlobalSettings::getChidonYear();
 
+// get all chidon schools
+require_once($_SERVER['DOCUMENT_ROOT'].'/class.adminSchools.php');       
+$as = new AdminSchools( $admin_user['admin_id'], $admin_user['auth'], true, true ); // add chidon schools
+$schools = $as->getSchools();
+
+$ids = [];
 $info = [];
-$sql = "select s.school_name, c.class_grade, c.class_sub, c.class_teacher, u.user_id, tc.th_chidon_id 
+$sql = "select c.class_id, count(u.user_id) as total   
         from users u 
         join schools s using (school_id) 
         join classes c on c.class_id = u.class_id 
-        left join th_chidon tc on tc.user_id = u.user_id 
-        where u.school_id > 0 
-        and u.school_id != 82 
-        and u.class_id > 0 
-        and s.school_era is null 
-        and s.chayolei = 1 
+        where s.school_id in (" . implode( ',', array_keys( $schools ) ) . ") 
+        and s.chidon = 1 
+        and s.test_school != 1 
         and c.class_grade in ('4','5','6','7','8') 
-        order by school_name, class_grade, class_sub, u.user_id";
+        and c.class_era = 0 
+        group by c.class_id";
 //echo $sql;
 $result = mysql_query( $sql );
 while ( $row = mysql_fetch_assoc( $result ) ) {
     // initialize vars if not set
-    if ( !isset( $info[$row['school_name']][$row['class_grade']][$row['class_sub']]['reg'] ) ) $info[$row['school_name']][$row['class_grade']][$row['class_sub']]['reg'] = 0;
-    if ( !isset( $info[$row['school_name']][$row['class_grade']][$row['class_sub']]['notReg'] ) ) $info[$row['school_name']][$row['class_grade']][$row['class_sub']]['notReg'] = 0;
-    
-    if ($row['th_chidon_id']) $info[$row['school_name']][$row['class_grade']][$row['class_sub']]['reg']++;
-    else $info[$row['school_name']][$row['class_grade']][$row['class_sub']]['notReg']++;
+    $ids[] = $row['class_id'];
+    $info[$row['class_id']]['notReg'] = $row['total'];
+
+    // find out how many users from school are registered for chidon
+    $sqlChidon = "select count(*) as registered from th_chidon 
+                    where user_id in (
+                        select user_id from users where class_id = " . $row['class_id'] . "
+                    )    
+                    and year = " . $year;
+    $resultChidon = mysql_query( $sqlChidon );
+    $rowChidon = mysql_fetch_assoc( $resultChidon );
+    $info[$row['class_id']]['reg'] = $rowChidon['registered'];
 }
 
 $percentages = [];
-foreach ( $info as $school => $more ) {
-    foreach ( $more as $grade => $other ) {
-        foreach ( $other as $sub => $data ) {
-            $percent = round( ($data['reg'] / ($data['notReg'] + $data['reg']) * 100), 2 );
-            $percentages[$school][$grade][$sub] = $percent;
-        }
+foreach ( $info as $class_id => $data ) {
+    if ( $data['reg'] == 0 ) {
+        unset($info[$class_id]);
+        continue;
     }
+    $percent = round( ($data['reg'] / ($data['notReg'] + $data['reg']) * 100), 2 );
+    $percentages[$class_id] = $percent;
 }
 arsort( $percentages );
-//echo "<pre>"; print_r( $percentages ); echo "</pre>";
+
+$classInfo = [];
+$sql = "select s.school_name, c.class_id, c.class_grade, c.class_sub 
+        from schools s 
+        join classes c using (school_id) 
+        where class_id in (" . implode(',', $ids) . ")";
+$result = mysql_query( $sql );
+while ( $row = mysql_fetch_assoc( $result ) ) {
+    $classInfo[$row['class_id']] = $row;
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -86,14 +105,13 @@ arsort( $percentages );
             <tbody>
                 <?php
                 $i = 1;
-                foreach ( $percentages as $school => $more ) {     
-                    foreach ( $more as $grade => $other ) {
-                        foreach ( $other as $sub => $percent ) {
-                            echo "<tr><td>#" . $i++ . "</td><td>" . $school . "</td><td>" . $grade . "</td><td>" . $sub . "</td><td>";
-                            echo ($info[$school][$grade][$sub]['notReg'] + $info[$school][$grade][$sub]['reg']) . "</td><td>";
-                            echo $info[$school][$grade][$sub]['reg'] . "</td><td>" . $percent . "</td></tr>";
-                        }
-                    }       
+                foreach ( $percentages as $class_id => $percent ) { 
+                    $school = $classInfo[$class_id]['school_name'];
+                    $grade = $classInfo[$class_id]['class_grade'];
+                    $sub = $classInfo[$class_id]['class_sub'];
+                    echo "<tr><td>#" . $i++ . "</td><td>" . $school . "</td><td>" . $grade . "</td><td>" . $sub . "</td><td>";
+                    echo ($info[$class_id]['notReg'] + $info[$class_id]['reg']) . "</td><td>";
+                    echo $info[$class_id]['reg'] . "</td><td>" . $percent . "</td></tr>";     
                 }
                 ?>
             </tbody>
