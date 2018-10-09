65ca991bad3b407c9c2fb63996e0169bb16efed6
Menachem Hornbacher <mendelh1537@gmail.com>
2018-10-09 16:29:25 -0400

    animate all selects

--- src/components/inputs/selects/Select.jsx
+++ src/components/inputs/selects/Select.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 // components
 import ReactSelect from 'react-select';
+import makeAnimated from 'react-select/lib/animated';
 import ReactSelectCreatable from 'react-select/lib/Creatable';
 
 function withDefaultProps( Select ){
@@ -9,6 +10,8 @@ function withDefaultProps( Select ){
       return (
         <Select 
           openMenuOnFocus
+          components={makeAnimated()}
+          
           { ...this.props }
           menuPlacement="auto" 
           classNamePrefix="react-select" />
