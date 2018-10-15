d9e8c00ab44390c58b052e422f5d81d83150b97a
Menachem Hornbacher <mendelh1537@gmail.com>
2018-10-15 16:25:09 -0400

    marking page
    Former-commit-id: 73cdf86c22fd1b0aee0c2c49d47596f7b00a73fa

---
+++ public_html/api/print/mark.js
@@ -0,0 +1,274 @@
+/**
+ * mark.js
+ * 
+ * This file is a startup and event handler script for mark.php
+ * 
+ * jQuery and functions.php are pre-loaded.
+ */
+// * SETUP EVENT LISTENERS
+$( '#oneCol' ).hide();
+$( '.checkboxDaily' ).click( onCheckboxClicked( true ) );
+$( '.checkbox' ).not( '.textInput' ).click( onCheckboxClicked( false ) );
+$( '.textInput input' ).keyup( onInputChanged );
+$( '#checkAll' ).click( function(){ toggleAll( true ) } );
+$( '#uncheckAll' ).click( function(){ toggleAll( false ) } );
+$( '.dailyRow' ).click( toggleRow );
+
+/**
+ * Returns an event handler for the checkboxes on the page.
+ * 
+ * @param {boolean} daily 
+ */
+function onCheckboxClicked( daily ) {
+    return function( event ) {
+        var user_id = $('input#user_id').val();
+        var div = event.target;
+        // get the task_id and the date for the task
+        var info = div.id.split(':');
+        var task = info[0];
+        var date = info[1];
+
+        var mark_type = daily ? 'daily_task_mark' : 'task_mark';
+        var url = '/delete_functions.php?function_name=delete_' + mark_type
+        if ( $( div ).hasClass('unmarked') )
+            var url = '/add_functions.php?function_name=add_' + mark_type
+
+        var parameters = [ user_id, task, date ];
+        url += '&parameters=' + parameters.join(',');
+        
+        $.getJSON( url, function( response ) {
+            if ( response == false )
+                return window.alert( 'Task not marked. Please try again' );
+            $.post( '/ajax/updateMedalsRanks.php', { user : user_id } );
+            update( div );
+        });
+    }
+}
+
+/**
+ * Event handler for text inputs
+ * 
+ * @param {event} event 
+ */
+function onInputChanged( event ){
+    var div = event.target;
+    var user_id = $('input#user_id').val();
+
+    var val = div.value || 0;
+    var info = div.id.split( ':' );
+    var task = info[0];
+    var date = info[1];
+
+    var parameters = [user_id, task, date, val];
+    url = "/add_functions.php?function_name=add_mark&parameters=" + parameters.join(',');
+    
+    $.getJSON( url, function( response ) {
+        if (success != 1)
+            return alert("Update not performed. Please try again.");
+
+        $.post('/ajax/updateMedalsRanks.php', { user : user_id });
+        update( div );
+    });
+}
+
+/**
+ * toggle all to be checked or unchecked
+ * 
+ * @param {boolean} event 
+ */
+function toggleAll( checked ){
+    $("#loading").show();
+    var user_id = $('input#user_id').val();
+    var className = checked ? 'unmarked' : 'marked';
+
+    var tasks = [];
+    var dates = [];
+    var boxes = $(".checkboxDaily, .checkbox");
+
+    boxes.each( function( index, element ) {
+        if ( $( element ).hasClass( className ) &&
+            !$( element ).hasClass( 'textInput' )
+        ) {
+            var info = element.id.split(':');
+            tasks.push( info[0] );
+            dates.push( info[1] );
+        }
+    });
+
+    tasks = tasks.join(':');
+    dates = dates.join(':');
+
+    var data = { action: checked ? 'add' : 'delete', data:  [ user_id, tasks, dates ] };
+    $.post('/ajax/updateMarks.php', data, function( success ) {
+        if (success == false) {
+            alert( 'Update not performed.' );
+            $( '#loading' ).hide();
+        } else {
+            $.post( '/ajax/updateMedalsRanks.php', { user : user_id } );
+            boxes.each( function( index, element ) {
+                if ( $(element).hasClass( className ) )
+                    update( element );
+            });
+            $( '.dailyRow' ).attr( 'checked', !!checked );
+            $( '#loading' ).hide();
+        }
+    });
+}
+
+/**
+ * event handler for the checkbox on each daily row
+ * 
+ * @param {event} event 
+ */
+function toggleRow( event ) {
+    var tasks = [];
+    var dates = [];
+
+    var div = event.target;
+    var checked = div.checked;
+    var user_id = $('input#user_id').val();
+    var boxes = $( div ).parent().next( '.dailyBoxes' ).find( '.checkboxDaily' );
+
+    boxes.each( function( index, element ) {
+        if ( $( element ).hasClass( checked ? 'unmarked' : 'marked' ) ) {
+            var info = element.id.split(':');
+            tasks.push( info[0] );
+            dates.push( info[1] );
+        }
+    });
+
+    if ( tasks.length == 0 )
+        return false;
+
+    tasks = tasks.join(':');
+    dates = dates.join(':');
+    var data = { 
+        action: checked ? 'add' : 'delete', 
+        data: [ user_id, tasks, dates ]
+    };
+
+    $.post('/ajax/updateMarks.php', data, function( response ) {
+        if ( response == false )
+            return alert( 'Update not performed.' );
+        $.post( '/ajax/updateMedalsRanks.php', { user : user_id } );
+        boxes.each( function( index, element ) {
+            if ( $( element ).hasClass( checked ? 'unmarked' : 'marked' ) )
+                update( element );
+        });
+    });
+}
+
+/**
+ * function update
+ * 
+ * updates the UI for the checkboxes to show if they are marked or not.
+ * 
+ * @param element div 
+ */
+function update( div ) {
+    div = $( div );
+
+    if ( div.hasClass('marked') ) {
+        div.removeClass( 'marked' );
+        div.addClass( 'unmarked' );
+        if ( !div.hasClass( 'textInput' ) )
+            div.empty();
+    } else if ( div.hasClass('unmarked') ) {
+        div.removeClass( 'unmarked' );
+        div.addClass( 'marked' );
+        if (!div.hasClass('textInput'))
+            div.html( "<span class='checkmark'>&#10004;</span>" );
+    }
+}
+
+// footer is apparently not rendered.
+$(".userMission").each( function() {			
+    var user = $(this).attr('id');	
+    var user_id = user.substring(user.indexOf('-') + 1);
+    var image = 'All';
+    var elem = this;
+
+    $.get( 
+        '/ajax/getMissionInfo.php',
+        {user_id : user_id, type : image}, 
+        function( data ) {
+            data = $.parseJSON(data);
+            
+            var stickers = {
+                1	: 'Shabbos Mevorchim Tehillim.gif', 
+                4	: 'Tefillah.gif',
+                12	: 'Mivtzoim.gif',
+                13	: 'Niggunim.gif',
+                16	: 'Sticker - Hiskashrus outline.png', 
+                21	: 'sefer hamitzvos bw.png',
+                27	: 'Tanya.gif',
+                40	: 'Yomei Dipagra.gif',
+                41	: 'Avos Ubonim.gif',
+                42	: 'Vihalachta Bidrachov.gif',
+                45	: 'Cheshbon Hanefesh.gif',
+                90	: 'Chitas.gif',
+                100	: 'Sticker - Brias Haguf_outline bw.png'
+            }
+            
+            var str = "<div class='finalFooter'>";
+            $.each(data, function(i, val) { 
+                str += "<span class='footer_info'>";
+                var j = 0;
+                var s = stickers;
+                $.each(val, function(indx, value) {
+                    //build footer info
+                    if (j++ == 0) { //first get sticker info
+                        str += "<img src='/mission_report/stickerOutlines/" + s[i] + "' /><br /><b>" + indx + "</b><br />";
+                    } else { //then get medal info
+                        str += "<i>" + value + " to " + indx + "</i>";
+                    }
+                });
+                str += "</span>"; 
+            });
+            str += "</div>";
+            $(elem).find("#" + user_id).append(str);
+        }
+    );
+});
+
+$(".arrow-left").click( function() {
+    var val = $( this ).parent().find( "option:selected" ).prev().val();
+    var type = $(this).parent().find('select')[0].id;
+    if ( val === undefined ) {
+        alert( "There are no previous " + type + "s." );
+        return;
+    }
+    $(this).parent().find('select').val( val );
+    $(this).parent().find('select').change();
+
+    if ( ['soldier', 'parsha'].includes( type ) )
+        $('form#marking').submit();
+});
+
+$(".arrow-right").click( function() {
+    var val = $( this ).parent().find( "option:selected" ).next().val();
+    var type = $(this).parent().find('select')[0].id;
+    if ( val === undefined ) {
+        alert( "There are no more " + type + "s." );
+        return;
+    }
+    $(this).parent().find('select').val( val );
+    $(this).parent().find('select').change();
+    
+    if ( ['soldier', 'parsha'].includes( type ) )
+        $('form#marking').submit();
+});
+
+$("select#platoon").change( function() {
+    $.get('/ajax/getUsers.php', { id: $(this).val() }, function( data ) {
+        if (data) {
+            $("select#soldier").empty();
+            var str = '';
+            var users = $.parseJSON( data );
+            for (key in users) {
+                str += "<option value=" + key + ">" + users[key] + "</option>";
+            }
+            $("select#soldier").append( str );
+        }
+    });
+});
---
+++ public_html/api/print/mark.php
@@ -0,0 +1,138 @@
+<?php
+require_once( '../header/header.php' ); // load header
+
+require_once( $_SERVER['DOCUMENT_ROOT'] . '/mission_report/classes/missions.php' );
+require_once( $_SERVER['DOCUMENT_ROOT'] . '/mission_report/classes/noPicMission.php' );
+require_once( $_SERVER['DOCUMENT_ROOT'] . '/mission_report/classes/picMission.php' );
+
+try {
+    $soldier = Soldier::find( $_POST['user_id'] );
+    $parsha = Parsha::find( $_POST['parsha_id'] );
+} catch ( ActiveRecord\RecordNotFound $e ) {
+    echo '<h1>Error: Soldier or Parsha Invalid</h1>'; die();
+}
+
+// * Get the list of parshos for the dropdown
+$year = GlobalSettings::getCurrentYear();
+$today = unixtojd();
+$parshos = Parsha::all([
+    'conditions' => "year = $year AND end < $today",
+    'order' => 'end DESC'
+]);
+
+// * Generate the missions using the legacy code
+$missions = new Missions( $parsha->start, $parsha->end, $soldier->user_id);
+$missions = $missions->getMissions();
+
+// * Generate the printed sheets using the legacy code
+$objMissions = [];
+foreach ( $missions as $mission ) {
+    $type = $mission->pic_mission_type;
+    $objMissions[] = MissionDisplay::getInstance( $type, $mission );
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Mark Printed Missions</title>
+    <link rel="stylesheet" href="/mission_report/newStyle.css?v=2.3" type="text/css" />
+    <style>
+        span.checkmark { pointer-events: none; }
+        form#marking { padding: 4px 25px; }
+        div#options { display: flex; justify-content: space-evenly; align-items: flex-end; }
+    </style>
+</head>
+
+<body>
+    <input type='hidden' id='user_id' value='<?= $soldier->user_id; ?>'/>
+    <form method='post' id='marking'>
+        <div id='options'>
+            <div>
+                <label for='platoon'>Platoon</label>
+                <div>
+                    <div class="arrow-left"></div>
+                    <select id='platoon'>
+                    <?php foreach( $soldier->school->platoons as $platoon ) { ?>
+                        <option value='<?=$platoon->class_id?>' <?= $platoon->class_id == $soldier->class_id ? 'selected' : ''?>>
+                            <?=$platoon->name()?>
+                        </option>
+                    <?php } ?>
+                    </select>
+                    <div class="arrow-right"></div>
+                </div>
+            </div>
+            <div>
+                <label for='soldier'>Soldier</label>
+                <div>
+                    <div class="arrow-left"></div>
+                    <select id='soldier' name='user_id'>
+                    <?php foreach( $soldier->platoon->soldiers as $student ) { ?>
+                        <?php if ( $student->user_registered ) { ?>
+                            <option value='<?=$student->user_id?>' <?= $student->user_id == $soldier->user_id ? 'selected' : ''?>>
+                                <?=$student->name()?>
+                            </option>
+                        <?php } ?>
+                    <?php } ?>
+                    </select>
+                    <div class="arrow-right"></div>
+                </div>
+            </div>
+            <div>
+                <label for='parsha'>Parsha</label>
+                <div>
+                    <div class="arrow-left"></div>
+                    <select id='parsha' name='parsha_id'>
+                    <?php foreach( $parshos as $option ) { ?>
+                        <option value='<?=$option->id?>' <?= $option->id == $parsha->id ? 'selected' : ''?>>
+                            <?=$option->name?>
+                        </option>
+                    <?php } ?>
+                    </select>
+                    <div class="arrow-right"></div>
+                </div>
+            </div>
+            <button type='submit'>Load Missions</button>
+        </div>
+    </form>
+
+    <!-- <div id='stats'>
+        <p>Soldiers Printed: <?= count( $user_ids ) ?> | Parshos Printed: <?= count( $parsha_ids ) ?></p>
+        <p id='total'>Total Mission Sheets Printed: <?= count( $user_ids ) * count( $parsha_ids ) ?></p>
+    </div> -->
+
+    <?php
+        // * Print the missions just like before
+        foreach ( $objMissions as $obj ) {
+            $id = $obj->user_id;
+            if ($obj->lang_id == 1) {
+                echo "<div class='userMission' id='user-" . $id . "' >";
+            } else if ($obj->lang_id == 2) {
+                echo "<div class='userMission he' id='user-" . $id . "' dir='rtl' >";
+            }
+            $obj->markMissionCol();
+            echo "</div>";
+        }
+    ?>
+    <div style="clear: both"></div>
+    <br /><br />
+    
+    <script src="/scripts/functions.js"></script>
+    <script src="/jquery.js"></script>
+    <script src='mark.js'></script>
+
+    <?php // ! *************************** Debug *************************** ?>
+    <pre>
+    <?php
+        // print_r([
+        //     'post' => $_POST,
+        //     'vars' => [
+        //         'soldier' => $soldier,
+        //         'parsha' => $parsha,
+        //     ]
+        // ]);
+    ?>
+    </pre>
+</body>
+</html>
