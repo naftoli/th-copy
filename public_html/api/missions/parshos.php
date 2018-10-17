1b9edc11d681b821e3767c221ac19823434a2639
Menachem Hornbacher <mendelh1537@gmail.com>
2018-10-17 09:50:13 -0400

    rename router
    Former-commit-id: 4869dedd25062b1ec4740ad4c2955dbd56b255a8

--- public_html/api/missions/parshos.php
+++ public_html/api/missions/parshos.php
@@ -2,7 +2,7 @@
 define( "MASHPIA_AUTH_REQUIRED", true );
 include_once( __DIR__ . "/../header/header.php" );
 
-class PrizesRouter {
+class ParshaRouter {
 
     public function index() {
         $parshos = Parsha::find('all', [
@@ -13,6 +13,6 @@ class PrizesRouter {
     }
 }
 
-rest_router( new PrizesRouter );
+rest_router( new ParshaRouter );
 
 die();
