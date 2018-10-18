da558c71d64a48f47305c384b52cc7e2340c45e8
Menachem Hornbacher <mendelh1537@gmail.com>
2018-10-18 16:22:22 -0400

    simplify props

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,17 +1,42 @@
 import React from 'react';
-import { NavItem, NavLink } from 'reactstrap';
 import classnames from 'classnames';
+import { FontAwesome } from 'components/ui';
+import { NavItem, NavLink } from 'reactstrap';
 
 // navigation tab details
-const NavigationTab = ( props ) => {
-  const className = classnames({
-    'active': props.active,
-    [props.className]: !!props.className
+const NavigationTab = ( initialProps ) => {
+  let { 
+    onClick,  active, className,  activeTab,
+    children, tab,    valid,      icon,     ...props
+  } = initialProps;
+
+  className = classnames({
+    'active': !!active,
+    [className]: !!className
   });
+
+  const toggle = () => onClick( tab );
+
+  if ( activeTab && tab )
+    active = activeTab === tab;
+
+  const click = activeTab && tab ? toggle : onClick;
+
+  const onKeyPress = ( event ) => {
+    if ( event.key === 'Enter' )
+      click();
+  }
+
   return (
     <NavItem>
-      <NavLink { ...props } className={className} >
-        { props.children }
+      <NavLink { ...props } 
+          tabIndex = { 0 }
+          active = { active }
+          onClick = { click } 
+          className = { className } 
+          onKeyPress = { onKeyPress }>
+        { children }{' '}
+        { icon && <FontAwesome icon={ icon } /> }
       </NavLink>
     </NavItem>
   );
