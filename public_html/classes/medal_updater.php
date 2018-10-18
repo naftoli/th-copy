882ba4fdf358d2bdaaef9c4d438181e8d9c3b32e
Menachem Hornbacher <mendelh1537@gmail.com>
2018-10-18 11:17:52 -0400

    remove old unused and commented out code
    Former-commit-id: 112cb78edb978000a6d50ffaa542bb1ee2d2bb0c

--- public_html/classes/medal_updater.php
+++ public_html/classes/medal_updater.php
@@ -1,106 +1,27 @@
 <?
 class medal_updater {
-	
-	function __construct()
-	{
-	}
-	/*
-	function update_medal($user_id) {
-		$updated = false;
-		
+
+	function update_medal_two( $user_id ) {
+		$user_id = intval( $user_id );
 		// ***** Get the number of finished missions ***** //
-		$sql1 = "SELECT *, s.subject_name, count(*) AS finished_missions ";
-		$sql1 = $sql1 . "FROM users AS u ";
-		$sql1 = $sql1 . "JOIN user_tracks AS ut USING (user_id) ";
-		$sql1 = $sql1 . "JOIN date_tasks_mission_marks AS dtmm ON (u.user_id=dtmm.user_id AND ut.subject_id=dtmm.subject_id)  ";
-		$sql1 = $sql1 . "JOIN subjects AS s ON (ut.subject_id=s.subject_id) ";
-		$sql1 = $sql1 . "JOIN date_tasks_missions AS dtm ON ( dtm.date_tasks_mission_id = dtmm.date_tasks_mission_id
-						AND dtm.level = ut.level
-						AND dtm.track_id = ut.track_id ) ";
-		$sql1 = $sql1 . "WHERE u.school_id > 0 ";
-		$sql1 = $sql1 . "AND u.school_type_id > 0 ";
-		$sql1 = $sql1 . "AND ut.enrolled=1 ";
+		$sql1 = "SELECT s.subject_id, u.user_id, s.subject_name, COUNT(*) AS finished_missions ";
+		$sql1 .= "FROM users AS u ";
+		$sql1 .= "JOIN user_tracks AS ut USING (user_id) ";
+		$sql1 .= "JOIN date_tasks_mission_marks AS dtmm ON (u.user_id=dtmm.user_id AND ut.subject_id=dtmm.subject_id)  ";
+		$sql1 .= "JOIN subjects AS s ON (ut.subject_id=s.subject_id) ";
+		$sql1 .= "WHERE u.school_id > 0 ";
+		$sql1 .= "AND u.school_type_id > 0 ";
+		//$sql1 .= "AND ut.enrolled=1 ";
 		if ($user_id > 0)
-			$sql1 = $sql1 . "AND u.user_id=" . $user_id . " ";
-		$sql1 = $sql1 . "GROUP BY u.user_id, ut.subject_id ";
-		//echo $sql1;
-		
+			$sql1 .= "AND u.user_id= $user_id ";
+		$sql1 .= "GROUP BY ut.subject_id ";
+
 		$query1 = mysql_query($sql1);
 		// ***** Get the number of finished missions ***** //
 		
 		//if ($row1) {
 		while ($row1 = mysql_fetch_assoc($query1)) {
 			$finished_missions = $row1['finished_missions'];
-
-			// ***** Get the number of medals required to achieve medal mark ***** //		
-			$missions_required = 0;
-			$sql2 = "SELECT * FROM medals_subjects WHERE subject_id=" . $row1['subject_id'] . " ORDER BY medal_ord ";
-			$query2 = mysql_query($sql2);
-			while ($row2 = mysql_fetch_assoc($query2)) {
-				$missions_required = $missions_required + $row2['missions_required'];			
-				
-				// ***** If the number has been met then add the medal ***** //
-				if ($finished_missions >= $missions_required) {
-					$sql3 = "SELECT count(*) AS number_of_medals FROM medal_marks WHERE medal_ord=" . $row2['medal_ord'] . " AND subject_id=" . $row1['subject_id'] . " AND user_id=" . $row1['user_id'];
-					$query3 = mysql_query($sql3);
-					$row3 = mysql_fetch_assoc($query3);
-						
-					if ($row3['number_of_medals'] == 0) {
-						//$medals_counter++;
-						$today_julian = gregoriantojd(date("m"), date("d"), date("Y"));
-						$insert_sql = "INSERT INTO medal_marks SET medal_ord=" . $row2['medal_ord'] . ", subject_id=" . $row1['subject_id'] . ", user_id=" . $row1['user_id'] . ", date_awarded=" . $today_julian;
-						$insert_query = mysql_query($insert_sql);
-						if ($insert_query)
-							$updated = true;
-					}										
-				}
-				// ***** If the number has NOT been met then delete the medal if it exists ***** //
-				else {
-					$delete_sql = "DELETE FROM medal_marks WHERE medal_ord=" . $row2['medal_ord'] . " AND subject_id=" . $row1['subject_id'] . " AND user_id=" . $row1['user_id'];
-					$delete_query = mysql_query($delete_sql);				
-				}				
-			}	
-		}
-		return $updated;
-	}
-	*/
-
-	function update_medal_two($user_id) {
-		// ***** Get the number of finished missions ***** //
-		$sql1 = "SELECT *, s.subject_name, count(*) AS finished_missions ";
-		$sql1 = $sql1 . "FROM users AS u ";
-		$sql1 = $sql1 . "JOIN user_tracks AS ut USING (user_id) ";
-		$sql1 = $sql1 . "JOIN date_tasks_mission_marks AS dtmm ON (u.user_id=dtmm.user_id AND ut.subject_id=dtmm.subject_id)  ";
-		$sql1 = $sql1 . "JOIN subjects AS s ON (ut.subject_id=s.subject_id) ";
-		$sql1 = $sql1 . "WHERE u.school_id > 0 ";
-		$sql1 = $sql1 . "AND u.school_type_id > 0 ";
-		//$sql1 = $sql1 . "AND ut.enrolled=1 ";
-		if ($user_id > 0)
-			$sql1 = $sql1 . "AND u.user_id=" . $user_id . " ";
-		$sql1 = $sql1 . "GROUP BY ut.subject_id ";
-		//if ($user_id == 51629) echo $sql1 . "<br />";
-		
-		$query1 = mysql_query($sql1);
-		// ***** Get the number of finished missions ***** //
-		
-		//if ($row1) {
-		while ($row1 = mysql_fetch_assoc($query1)) {
-			$finished_missions = $row1['finished_missions'];
-			/*
-			if ($row1['subject_id'] == 27) {
-				//if we are in tanya, get total from new system and add to old system
-				//get tanya missions done
-				//first get user barcode
-				$sqlCode = "select user_code from users where user_id = " . $user_id;
-				$resCode = mysql_query( $sqlCode );
-				$rowCode = mysql_fetch_assoc( $resCode );
-				$barcode = '3' . $rowCode['user_code'];
-				
-				$newTotal = header_v2_missions( array( 'arrUserCodes' => array( $barcode ) ) );
-				if (isset($newTotal[$barcode]) && !empty($newTotal[$barcode])) 
-				    $finished_missions += $newTotal[$barcode];		
-			} 
-			*/
 			// ***** Get the number of medals required to achieve medal mark ***** //		
 			$missions_required = 0;
 			$sql2 = "SELECT * FROM medals_subjects WHERE subject_id=" . $row1['subject_id'] . " ORDER BY medal_ord ";
@@ -140,7 +61,6 @@ class medal_updater {
 		
 		}
 	
-	}	
-		
+	}
 } 
 ?>
--- public_html/classes/rank_updater.php
+++ public_html/classes/rank_updater.php
@@ -1,66 +1,6 @@
 <?
-class rank_updater 
-{	
-	function __construct()
-	{
-		$this->info = array();
-	}
-	/*
-	function update_rank($user_id = 0) {
-		$updated = false;
-		
-		// ***** Get the number of medals aquired ***** //
-		$sql1 = "SELECT u.user_id, count(*) AS medals_aquired ";
-		$sql1 = $sql1 . "FROM users AS u ";
-		$sql1 = $sql1 . "JOIN medal_marks AS mm ON (mm.user_id=u.user_id) ";
-		
-		if ($user_id > 0) 
-			$sql1 = $sql1 . "WHERE u.user_id=" . $user_id . " ";
-			
-		$sql1 = $sql1 . "GROUP BY u.user_id ";	
-		
-		$query1 = mysql_query($sql1);
-		// ***** Get the number of medals aquired ***** //
-		
-		while ($row1 = mysql_fetch_assoc($query1)) {
-			$medals_aquired = $row1['medals_aquired'];
-			//echo 'medals_aquired:' . $medals_aquired  . '<br />';
-			
-			
-			// ***** Get the number of medals required for each rank ***** //
-			$sql2 = "SELECT * FROM ranks ORDER BY rank_ord";
-			$query2 = mysql_query($sql2);
-			while ($row2 = mysql_fetch_assoc($query2)) {
-				$rank_ord = $row2['rank_ord'];
-				$medals_required = $row2['medals_required'];
-				//echo 'medals_required:' . $medals_required . '<br />';
-				
-				// ***** If the number of medals required has been reached the add the rank ***** //
-				if ($medals_aquired >= $medals_required) {
-					$sql3 = "SELECT count(*) AS rank_exists FROM rank_marks WHERE rank_ord=" . $rank_ord . " AND user_id=" . $user_id;
-					$query3 = mysql_query($sql3);
-					$row3 = mysql_fetch_assoc($query3);
-						
-					if ($row3['rank_exists'] == 0) {
-						$today_julian = gregoriantojd(date("m"), date("d"), date("Y"));
-						$insert_sql = "INSERT INTO rank_marks SET rank_ord=" . $rank_ord . ", user_id=" . $user_id . ", date_promoted=" . $today_julian;
-						$insert_query = mysql_query($insert_sql);
-						if ($insert_query) 
-							$updated = true;
-					}
-				}
-				// ***** If the number of medals required has NOT been reached the delete the rank ***** //
-				else {
-					$delete_sql = "DELETE FROM rank_marks WHERE rank_ord=" . $rank_ord . " AND user_id=" . $user_id;
-					$delete_query = mysql_query($delete_sql);	
-				}
-					
-			}
-			// ***** Get the number of medals required for each rank ***** //
-		}
-		return $updated;	
-	}	
-	*/
+class rank_updater {
+	
 	function update_rank_two($user_id, $debug = false) {
 		// ***** Get the number of medals aquired ***** //
 		$sql1 = "SELECT u.user_id, count(*) AS medals_aquired ";
