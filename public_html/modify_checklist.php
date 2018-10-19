22b2ff2ab0c460c4b0b1574b136a7e51871249dc
naftolir <naftolir@gmail.com>
2018-10-19 13:07:34 -0400

    allow summer missions on checklist
    Former-commit-id: 35d5dca4130382dc868d471444841afc91e35ae9

--- public_html/modify_checklist.php
+++ public_html/modify_checklist.php
@@ -10,9 +10,9 @@ $year = GlobalSettings::getCurrentYear();
 //get default dates
 $dates = array();
 $sql = "SELECT * FROM parshos 
-        WHERE year = " . $year;   
-//$sql = "SELECT * FROM parshos 
-//        WHERE year = " . $year;     
+        WHERE start_date >= 2458292 
+        and end_date <= 2458753";        
+//year = " . $year;      
 $result = mysql_query($sql);
 while ($row = mysql_fetch_assoc($result)) {
     $dates[] = $row;
