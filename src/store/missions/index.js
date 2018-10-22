40535add0e2122bf09d6c4774d7a71a4870193be
Menachem Hornbacher <mendelh1537@gmail.com>
2018-10-22 14:57:01 -0400

    export state (whoops)

--- src/store/missions/index.js
+++ src/store/missions/index.js
@@ -2,9 +2,10 @@ import { combineReducers } from 'redux';
 
 import parshos from './parshos/reducer';
 import mark from './mark/reducer';
+import grid from './grid/reducer';
 
 const reducer = combineReducers({
-  parshos,  mark
+  parshos,  mark,   grid
 });
 
 export default reducer;
