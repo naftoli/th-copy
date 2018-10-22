909ade440124d150236a3a7ff9aee64ec333461c
Menachem Hornbacher <mendelh1537@gmail.com>
2018-10-22 14:55:14 -0400

    separate navigationTab

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,49 +1,5 @@
-import React from 'react';
-import classnames from 'classnames';
-import { FontAwesome } from 'components/ui';
-import { NavItem, NavLink } from 'reactstrap';
-
 // navigation tab details
-const NavigationTab = ( initialProps ) => {
-  let { 
-    onClick,  active, className,  activeTab,
-    children, tab,    valid,      icon,     ...props
-  } = initialProps;
-
-  className = classnames({
-    'active': !!active,
-    [className]: !!className
-  });
-
-  const toggle = () => onClick( tab );
-
-  if ( activeTab && tab )
-    active = activeTab === tab;
-
-  const click = activeTab && tab ? toggle : onClick;
-
-  const onKeyPress = ( event ) => {
-    if ( event.key === 'Enter' )
-      click();
-  }
-
-  return (
-    <NavItem>
-      <NavLink { ...props } 
-          tabIndex = { 0 }
-          active = { active }
-          onClick = { click } 
-          className = { className } 
-          onKeyPress = { onKeyPress }>
-        { children }{' '}
-        { icon && <FontAwesome icon={ icon } /> }
-      </NavLink>
-    </NavItem>
-  );
-}
-
-export { NavigationTab }
-
+export * from './NavigationTab';
 export { default as Dashboard } from './dashboard/Dashboard';
 export { default as Navbar } from './Navbar/Navbar';
 export { default as Sidebar } from './Sidebar/Sidebar';
