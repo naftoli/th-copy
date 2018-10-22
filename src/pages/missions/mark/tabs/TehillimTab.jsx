259776384f70d9b892319ed4141cce3f545452b6
Menachem Hornbacher <mendelh1537@gmail.com>
2018-10-22 17:55:24 -0400

    setup the tab for tehillim.

--- src/pages/missions/mark/tabs/TehillimTab.jsx
+++ src/pages/missions/mark/tabs/TehillimTab.jsx
@@ -1,15 +1,65 @@
 import React, { Component } from 'react';
 // components
-import { TabPane } from 'reactstrap';
+import { Grid } from 'components/missions/Grid';
+import { Row, Col, TabPane, Button } from 'reactstrap';
+import { FontAwesome, InlineSync, Spinner } from 'components/ui';
+import { SMSelect } from 'components/inputs/selects';
 
 class TehillimTab extends Component {
 
+  state = { date: false }
+
+  updateDate = option => {
+    this.setState({ date: option ? option.value : option }, this.load );
+  }
+
+  load = () => {
+    if ( this.state.date )
+      this.props.getGrid( 'tehillim', this.state.date );
+  }
+
   render(){
-    const { tabId } = this.props;
+    const { date } = this.state;
+    const { tabId, soldiers, missions, loading } = this.props;
     // render form
     return (
-      <TabPane tabId={ tabId }>
-        <h1>Tehillim Tab</h1>
+      <TabPane tabId={ tabId } id='TehillimTab'>
+
+        <p className='title'>Grid Options</p>
+
+        <Row id='options'>
+          <Col md={4}>
+            <SMSelect
+              value={ date }
+              onChange={ this.updateDate } />
+          </Col>
+          <Col sm={6} md={4}>
+              <Button color='primary'
+                  onClick={ this.load } 
+                  disabled={ !date || loading }>
+                <InlineSync loading={ loading } /> Refresh Grid
+              </Button>
+          </Col>
+          <Col sm={6} md={4}>
+              <Button color='primary'>
+                <FontAwesome icon='wrench' /> Customize Grid
+              </Button>
+          </Col>
+        </Row>
+
+        <hr/>
+
+        { loading && <Spinner /> }
+        
+        { !loading && // not loading
+          missions.length > 0 && // and we have missions
+          soldiers.length > 0 && // and we have soldiers
+          <Grid 
+            type='tehillim'
+            soldiers={ soldiers } 
+            missions={ missions } 
+            markTask={ this.props.markTask } />
+        }
       </TabPane>
     );
   }
