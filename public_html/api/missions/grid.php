56083c55e0769837b7e404f22cabbb4b71506810
Menachem Hornbacher <mendelh1537@gmail.com>
2018-10-23 14:57:08 -0400

    send if disabled to client and sort according to settings.
    Former-commit-id: 6a5ea74365e517743718fe28420129b4c9e4e2e0

--- public_html/api/missions/grid.php
+++ public_html/api/missions/grid.php
@@ -12,60 +12,9 @@ class GridRouter {
     public function get() {
         global $current_user; global $MASHPIA_DB;
         extract( $this->getDates( $_POST['type'], $_POST['date'] ) );
-        // basic params for all the queries
-        $params = [
-            ':class_id'  => $current_user->login->class_id,
-            ':start_date' => $start_date,   ':end_date' => $end_date,
-        ];
-        // * modified filters
-        // by default we expect
-        $dates_filter = ' dtm.start_date <= :start_date '.'AND dtm.end_date >= :end_date ';
-        // by default we load all subjects where at least one soldier is enrolled and it is not a tehillim campaign
-        $campaigns_filter = 'SELECT DISTINCT subject_id FROM user_tracks JOIN users USING (user_id) '
-                            .'WHERE class_id = :class_id AND user_registered > 0 AND subject_type != \'WWTC\'';
-        // weekly missions may be in middle of the week, but they are within the start and end date
-        if ( $_POST['type'] == 'weekly' )
-            $dates_filter = ' dtm.start_date >= :start_date '.'AND dtm.end_date <= :end_date ';
-        // tehillim only shows WWTC campaigns
-        if ( $_POST['type'] == 'tehillim' )
-            $campaigns_filter = 'SELECT subject_id FROM subjects WHERE subject_type = \'WWTC\'';
 
-        // * Final Query
-        // Query used to fetch tasks for grid.
-        $missions_sql = 'SELECT dt.cat, dt.points AS points, dt.grid_id, dt.quantity, '.$mark_date.' AS mark_date, '
-            .' dtm.subject_id, dtm.start_date, dtm.end_date, s.subject_id, s.subject_name, dt.mandatory_qty '
-            .' FROM mashpiadb.date_tasks dt '
-            .' JOIN mashpiadb.date_tasks_missions dtm USING (date_tasks_mission_id) '
-            .' JOIN mashpiadb.subjects s USING (subject_id) '
-            // * Misc limits used for keeping the data clean
-            .' WHERE dtm.personal = 0 '
-            .' AND dt.cat != \'\' AND dt.grid_id IS NOT NULL AND dt.grid_marking = 1 '
-            // * Limits based on :start_date, :end_date and :daily_task
-            .' AND dt.daily_task = :daily_task '
-            .' AND ' . $dates_filter
-            .' AND dtm.subject_id IN (' . $campaigns_filter .') '
-            // * Limits based on :school_id
-            .' AND ( dtm.created_by_school IS NULL OR dtm.created_by_school = :school_id )'
-            .' AND dtm.lang_id = ( SELECT lang_id FROM schools WHERE school_id = :school_id ) '
-            // * Limit the level, subjects and school_type_id based on the :class_id
-            .' AND dtm.school_type_id IN ('
-                .' SELECT DISTINCT school_type_id FROM users '
-                .' WHERE class_id = :class_id AND user_registered > 0 '
-            .' ) '
-            .' AND dtm.level IN ('
-                .' SELECT DISTINCT level FROM user_tracks JOIN users USING (user_id) '
-                .' WHERE class_id = :class_id AND user_registered > 0 '
-            .' ) '
-            .' GROUP BY dtm.subject_id, dt.grid_id';
-        $mission_params = array_merge( $params, [
-            ':school_id'  => $current_user->login->school_id,
-            ':daily_task' => $_POST['type'] == 'daily' ? '1' : '0',
-        ]);
-
-        // get the missions and grid_ids
-        $missions_query = $MASHPIA_DB->prepare( $missions_sql );
-        $missions_query->execute( $mission_params );
-        $missions = $missions_query->fetchAll();
+        // * Load the tasks
+        $missions = $this->getTasks( $_POST['type'], $_POST['date'] );
         $grid_ids = array_map( function($mission) { return $mission['grid_id']; }, $missions );
         
         // * Load all registered soldiers in this platoon
@@ -84,7 +33,10 @@ class GridRouter {
             .' AND dt.grid_id IN ('.implode( ', ', $grid_ids ).') '
             .' ORDER BY user_id , mark_date , grid_id;';
         $marks_query = $MASHPIA_DB->prepare( $marks_sql );
-        $marks_query->execute( $params );
+        $marks_query->execute([
+            ':class_id'  => $current_user->login->class_id,
+            ':start_date' => $start_date,   ':end_date' => $end_date,
+        ]);
         $marks = [];
         while( $mark = $marks_query->fetch() ){
             $user_id = $mark['user_id'];    $grid_id = $mark['grid_id'];
@@ -108,19 +60,127 @@ class GridRouter {
         json_response([
             'missions' => $missions,
             'soldiers' => $formatted_soldiers
-        ]);
+        ], true, true);
     }
 
+    public function customize() {
+        $missions = $this->getTasks( $_POST['type'] );
+        json_response( $missions );
+    }
+
+    private function getTasks( $type, $date = false, $load_disabled = false ){
+        global $current_user; global $MASHPIA_DB;
+
+        // * start_date and end_date filters
+        // by default we expect a date that is between the start date and end date
+        $dates_filter = ' dtm.start_date <= :start_date '.'AND dtm.end_date >= :end_date ';
+        // weekly missions may be in middle of the week, but they are within the start and end date
+        if ( $type == 'weekly' || !$date ) // if no date we use the start and end dates of the year and need to use this option.
+            $dates_filter = ' dtm.start_date >= :start_date '.'AND dtm.end_date <= :end_date ';
+        // * subject_id filters
+        // by default we load all subjects where at least one soldier is enrolled and it is not a tehillim campaign
+        $campaigns_filter = 'SELECT DISTINCT subject_id FROM user_tracks JOIN users USING (user_id) '
+        .'WHERE class_id = :class_id AND user_registered > 0 AND subject_type != \'WWTC\'';
+        // tehillim only shows WWTC campaigns
+        if ( $type == 'tehillim' )
+            $campaigns_filter = 'SELECT subject_id FROM subjects WHERE subject_type = \'WWTC\'';
+
+        // get the $start_date, $end_date and $mark_date variables
+        if ( $date )
+            extract( $this->getDates( $type, $date ) );
+        else {
+            $mark_date = 'start_date';
+            $start_date = GlobalSettings::getCurYearDates()['start'];
+            $end_date = GlobalSettings::getCurYearDates()['end'];
+        }
+
+        // * Final Query
+        // Query used to fetch tasks for grid.
+        $missions_sql = 'SELECT dt.cat, dt.points AS points, dt.grid_id, dt.quantity, '
+            .$mark_date.' AS mark_date, dtm.subject_id, dtm.start_date, dtm.end_date, '
+            .' s.subject_id, s.subject_name, dt.mandatory_qty, IFNULL( disabled, 0 ) as disabled '
+            .' FROM mashpiadb.date_tasks dt '
+            .' JOIN mashpiadb.date_tasks_missions dtm USING (date_tasks_mission_id) '
+            .' JOIN mashpiadb.subjects s USING (subject_id) '
+            .' LEFT JOIN ( '
+                .'SELECT grid_id, 1 as disabled FROM class_date_tasks_disabled where class_id = :class_id '
+            .') AS cdtd USING (grid_id) '
+            // * Misc limits used for keeping the data clean
+            .' WHERE dtm.personal = 0 '
+            .' AND dt.cat != \'\' AND dt.grid_id IS NOT NULL AND dt.grid_marking = 1 '
+            // * Limits based on :start_date, :end_date and :daily_task
+            .' AND dt.daily_task = :daily_task '
+            .' AND ' . $dates_filter // add date filter if we are limiting by dates
+            .' AND dtm.subject_id IN (' . $campaigns_filter .') '
+            // * Limits based on :school_id
+            .' AND ( dtm.created_by_school IS NULL OR dtm.created_by_school = :school_id )'
+            .' AND dtm.lang_id = ( SELECT lang_id FROM schools WHERE school_id = :school_id ) '
+            // * Limit the level, subjects and school_type_id based on the :class_id
+            .' AND dtm.school_type_id IN ('
+                .' SELECT DISTINCT school_type_id FROM users '
+                .' WHERE class_id = :class_id AND user_registered > 0 '
+            .' ) '
+            .' AND dtm.level IN ('
+                .' SELECT DISTINCT level FROM user_tracks JOIN users USING (user_id) '
+                .' WHERE class_id = :class_id AND user_registered > 0 '
+            .' ) '
+            .' GROUP BY dtm.subject_id, dt.grid_id';
+        // params for query
+        $params = [
+            ':class_id'  => $current_user->login->class_id,
+            ':school_id'  => $current_user->login->school_id,
+            ':daily_task' => $type == 'daily' ? '1' : '0',
+            ':start_date' => $start_date, ':end_date' => $end_date
+        ];
+
+        $missions_query = $MASHPIA_DB->prepare( $missions_sql );
+        $missions_query->execute( $params );
+        $missions = $missions_query->fetchAll();
+
+        $missions = $this->sortMissions( $missions, $current_user->login->class_id, $type );
+        
+        return $missions;
+    }
+    // * get start_date, end_date and value based on the date (parsha_id for weekly)
     private function getDates( $type, $value ) {
-        if ( $type === 'daily' )
-            return [ 'start_date' => $value, 'mark_date' => $value, 'end_date' => $value ];
         if ( $type === 'weekly' ) {
             $parsha = Parsha::find( $value );
             return [ 'start_date' => $parsha->start, 'mark_date' => 'start_date', 'end_date' => $parsha->end ];
         }
-        if ( $type === 'tehillim' ) {
-            return [ 'start_date' => $value, 'mark_date' => $value, 'end_date' => $value ];
-        }
+        return [ 'start_date' => $value, 'mark_date' => $value, 'end_date' => $value ];
+    }
+
+    private function sortMissions( $missions, $class_id, $type ){
+        global $MASHPIA_DB;
+        // get the saved sorting settings
+        $sorting_query = $MASHPIA_DB->prepare(
+            'SELECT sorted_grid_ids FROM class_date_task_sorting WHERE class_id = :class_id AND grid_type=:type'
+        );
+        $sorting_query->execute([':class_id' => $class_id, ':type' => $type ]);
+        $sorted_grid_ids = explode( ',', $sorting_query->fetch( PDO::FETCH_COLUMN ) );
+
+        if( count( $sorted_grid_ids ) == 0 )
+            return $missions;
+        // separate the ones we can sort from the ones we cannot
+        $missions = array_reduce( $missions,
+            function( $result, $mission ) use ( $sorted_grid_ids ) {
+                $key = in_array( $mission['grid_id'], $sorted_grid_ids ) ? 'sortable' : 'unknown';
+                $result[$key][] = $mission;
+                return $result;
+            },
+            [ 'sortable' => [], 'unknown' => [] ]
+        );
+        // sort the ones we can based on the users settings
+        usort( $missions['sortable'], function ( $a, $b ) use ( $sorted_grid_ids ) {
+            $a = $a['grid_id']; $b = $b['grid_id'];
+            if ( $a == $b ) { echo "0\n"; return 0; }
+            // return based on the index of the item in the sorted array where it goes
+            return array_search( $a, $sorted_grid_ids ) > array_search( $b, $sorted_grid_ids ) ? 1 : -1;
+        });
+        // put all missions we cannot merge behined the ones we can.
+        $missions = array_merge( $missions['sortable'], $missions['unknown'] );
+
+        return $missions;
     }
 }
 
