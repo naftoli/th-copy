b1ed2299df7e63244a721a76c6da622a8043ac55
Menachem Hornbacher <mendelh1537@gmail.com>
2018-10-28 14:58:51 -0400

    cleanup soldiers index page

--- src/pages/base-managment/soldiers/SoldiersPage/SoldiersPage.jsx
+++ src/pages/base-managment/soldiers/SoldiersPage/SoldiersPage.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 // components
+import { Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import BulkUploadModal from './BulkUploadModal';
-import { Button } from 'reactstrap';
 import CropperModal from 'components/modals/CropperModal';
-import { ButtonBar, Table, InlineSync, Callout, FontAwesome } from 'components/ui';
+import { ButtonBar, Table, InlineSync, FontAwesome } from 'components/ui';
 // functions
-import { toast } from 'react-toastify';
 import is from 'is_js';
 import { arrayToCSV, setTitle, canDownload } from 'functions/utils';
+import { showError } from 'functions/notifications';
 // styles
 import './SoldiersPage.scss';
 // state
@@ -19,56 +19,71 @@ import {
 // data
 import getColumns from './columns';
 
+const defaultCropperModalSettings = {
+  show: false,  src: false, id: false
+}
+
 export class SoldiersPage extends Component {
   // initial state
-  state = { 
-    cropperModalShow: false, cropperModalSrc: false, 
-    cropperModalId: false,  uploadModalShow: false
+  state = {
+    cropper: {
+      ...defaultCropperModalSettings
+    },
+    upload: {
+      show: false
+    },
+    // cropperModalShow: false, cropperModalSrc: false, 
+    // cropperModalId: false,  uploadModalShow: false
   }
   // load the contents if we do not have any
   componentDidMount(){
     setTitle( 'View/Edit Soldiers' );
-    if ( this.props.soldiers.length < 2 ) {
-      this.getSoldiers();
-    }
+    this.getSoldiers();
   }
 
   getSoldiers = () => {
-    this.props.getSoldiers()
-    .catch( e => toast.error( e.message, { autoClose: 10000 } ) );
+    showError( this.props.getSoldiers() );
   }
 
-  // handler for the modals
-  closeCropperModal = () => { this.setState({ cropperModalShow: false }) }
-  toggleUploadModal = () => { this.setState({ uploadModalShow: !this.state.uploadModalShow }) }
+  // close the cropper modal
+  closeCropperModal = () => this.setState({ cropper: {
+    ...defaultCropperModalSettings
+  } } );
+  // toggle the uploadier
+  toggleUploadModal = () => this.setState({
+    upload: { show: !this.state.upload.show }
+  });
 
   // handler for pressing the picture
-  editPicture = ( id ) => ( event ) => {
-    this.setState({
-      cropperModalShow: true, cropperModalId: id,
-      cropperModalSrc: event.target.src
-    });
-  }
+  editPicture = id => ({ target }) => this.setState({ 
+    cropper: { show: true, id, src: target.src } 
+  });
 
   // handler for when images are updated
-  updatePicture = ( formData ) => {
-    this.setState({ cropperModalShow: false });
-    this.props.updateSoldier( this.state.cropperModalId, formData )
+  updatePicture = formData => {
+    const { id } = this.state.cropper;
+    if ( id )
+      this.props.updateSoldier( id, formData );
+    else {
+      debugger;
+    }
+    this.closeCropperModal();
   }
 
   updateToggle = ( key, id ) => e => {
-    return this.props.updateSoldier( id, { [key]: e.target.checked ? 1 : 0 } )
-    .catch( e => toast.error( e.message ) );
+    return showError( this.props.updateSoldier(
+      id, { [key]: e.target.checked ? 1 : 0 }
+    ));
   }
 
   // handler for uploding the excel file
-  uploadSpreadsheet = ( formData ) => {
-    return this.props.uploadSpreadsheet( formData )
+  uploadSpreadsheet = ( formData ) => showError( 
+    this.props.uploadSpreadsheet( formData )
     .then( () => {
-      this.setState({ uploadModalShow: false }); 
+      this.setState({ upload: { show: false } }); 
       return this.getSoldiers();
-    });
-  }
+    })
+  );
 
   // download the content as a CSV
   toCSV = () => {
@@ -88,16 +103,11 @@ export class SoldiersPage extends Component {
   // render the page
   render() {
     const { current_login, soldiers, loading, match } = this.props;
-    const { cropperModalShow, cropperModalSrc, uploadModalShow } = this.state;
+    const { cropper, upload } = this.state;
     const columns = getColumns( current_login, this.editPicture, this.updateToggle );
     // page definition
     return (
       <div id='SoldiersPage' className='full-height'>
-        {/* User Guide */}
-        <Callout title='View Soldiers'>
-          Click a Soldier's name or serial number to view and edit their account.<br/>
-          Click on a Soldier's profile picture to edit or replace it.
-        </Callout>
         {/* Action buttons */}
         <ButtonBar>
           <Link to={`${match.path}/new`} className='btn btn-primary' role='button'>
@@ -123,11 +133,16 @@ export class SoldiersPage extends Component {
           loading={ loading && !soldiers.length } 
           pageId='SoldiersPage' />
         {/* Modal to edit images */}
-        <CropperModal isOpen={ cropperModalShow } src={ cropperModalSrc } 
-          toggle={ this.closeCropperModal } uploadImage={ this.updatePicture }/>
+        <CropperModal
+          src={ cropper.src } 
+          isOpen={ cropper.show }
+          toggle={ this.closeCropperModal }
+          uploadImage={ this.updatePicture } />
         {/* Modal to upload soldiers */ 
           current_login.code === 'BC' &&
-          <BulkUploadModal isOpen={ uploadModalShow } toggle={ this.toggleUploadModal }
+          <BulkUploadModal
+            isOpen={ upload.show }
+            toggle={ this.toggleUploadModal }
             upload={ this.uploadSpreadsheet }/>
         }
       </div>
