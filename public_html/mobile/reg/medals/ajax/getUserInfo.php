951a55fe72240b628d44e92604ceef904ea4fc9e
Menachem Hornbacher <mendelh1537@gmail.com>
2018-10-29 16:16:33 -0400

    handle no platoon soldiers (do not show undefined)
    Former-commit-id: d0f257f1de9704edcef46105bafcaaeed011e0ed

--- public_html/mobile/reg/medals/ajax/getUserInfo.php
+++ public_html/mobile/reg/medals/ajax/getUserInfo.php
@@ -7,7 +7,7 @@ $sql = "SELECT u.first, u.last, u.first_he, u.last_he, u.lang_id, u.user_serial,
     ." c.class_id, c.class_grade, c.class_sub, c.class_teacher, r.rank_ord, r.rank_name, s.logo, s.logo_girls, u.gender "
 	." FROM users u "
 	." JOIN schools s USING (school_id) "
-	." JOIN classes c ON c.class_id = u.class_id "
+	." LEFT JOIN classes c ON c.class_id = u.class_id "
 	." LEFT JOIN rank_marks USING (user_id) "
 	." LEFT JOIN ranks r USING (rank_ord) "
 	." LEFT JOIN thumbs t ON t.file_id = u.user_photo_id "
