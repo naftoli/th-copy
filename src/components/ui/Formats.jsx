9e22d3559e9e4226380fdbadcf93144b4c7d1f09
Menachem Hornbacher <mendelh1537@gmail.com>
2018-10-30 13:47:00 -0400

    revoke UTC expectation and convert numbers with parseInt

--- src/components/ui/Formats.jsx
+++ src/components/ui/Formats.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import moment from 'moment';
 
 // Format number as per the locale
-export const NumberDisplay = ({ value, className, ...opts }) => {
-  if ( typeof value === 'number' ) 
-    value = value.toLocaleString( navigator.language, opts );
+export const NumberDisplay = ({ value, className, opts }) => {
+  if ( !typeof value === 'number' )
+    value = parseInt( value, 10 );
+  // convert to the local data
+  value = value.toLocaleString( navigator.language, opts );
   return <span className={ className }>{value}</span>
 }
 
 export const DateDisplay = ({ value, calendar = false, fromNow = false, format = 'l' }) => {
   // format correctly
   if ( value ){
-    value = moment.utc( value );
+    value = moment( value );
 
     if ( calendar )
       value = value.calendar();
