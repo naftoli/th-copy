950adf0ce4fa82869eb74db81eca9045fab77ce9
Menachem Hornbacher <mendelh1537@gmail.com>
2018-11-11 11:55:22 -0500

    return pic_mission_type
    Former-commit-id: f17e62086507e67589badd2bdd8b6d401fc4be42

--- public_html/api/models/Soldier.php
+++ public_html/api/models/Soldier.php
@@ -513,7 +513,7 @@ class Soldier extends \ActiveRecord\Model implements \JsonSerializable {
                 'school_type_id', 'user_address1', 'user_address2', 'user_city', 'user_state',
                 'user_postal', 'user_country', 'user_phone', 'gender', 'user_registered', 
                 'chayolei', 'yan', 'chidon', 'allow_parent_tasks', 'print_parent_tasks', 'mobile_pic',
-                'school_id', 'class_id'
+                'school_id', 'class_id', 'pic_mission_type'
             ],
             'methods' => [ 
                 'profilePicture', 'barcode', 'miles', 'rank',
