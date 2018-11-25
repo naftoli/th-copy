510b20b0b458d0dadd6c4e1f20894abead805e30
naftolir <naftolir@gmail.com>
2018-11-24 21:28:44 -0500

    added list of admins used for certs
    Former-commit-id: 849c0ce12ca17a3cfa222f7bae708b83e3703012

---
+++ public_html/rally_certs/cert_list.php
@@ -0,0 +1,75 @@
+<?php
+ini_set('display_errors', 1);
+$admin_auth = array('school'); 
+require_once $_SERVER['DOCUMENT_ROOT'] . '/header.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/class.adminSchools.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/class.schoolClasses.php';
+
+$as = new AdminSchools( $admin_user['admin_id'], $admin_user['auth'] );
+$schools = $as->getSchools();
+
+$ids = [];
+foreach ( $schools as $school_id => $school ) {
+    $sc = new SchoolClasses( $school_id );
+    $grades = $sc->getClasses();
+    foreach ( $grades as $grade ) {
+        $ids[$school_id][] = $grade['class_id'];
+    }
+}
+//echo "<pre>"; print_r( $ids ); echo "</pre>";
+$info = [];
+foreach ( $ids as $school_id => $grades ) {
+    $school_name = $schools[$school_id];
+    $sql = "select a.first, a.last  
+            from admin_auths aa 
+            join admins a using (admin_id) 
+            where aa.auth in ('school','staff') 
+            and aa.id = " . $school_id;
+    $result = mysql_query( $sql );
+    while ( $row = mysql_fetch_assoc( $result ) ) {
+        $info[$school_name][] = $row['first'] . ' ' . $row['last'];
+    }
+
+    foreach ( $grades as $class_id ) {
+        $sql = "select a.first, a.last  
+                from admin_auths aa 
+                join admins a using (admin_id) 
+                where aa.auth = 'class' 
+                and aa.id = " . $class_id;
+        $result = mysql_query( $sql );
+        while ( $row = mysql_fetch_assoc( $result ) ) {
+            $info[$school_name][] = $row['first'] . ' ' . $row['last'];
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='utf8' />
+        <style>
+            tr, th, td {
+                padding: 10px;
+                font-size: 14px;
+            }
+        </style>
+    </head>
+
+    <body>
+        <table>
+            <tr>
+                <th>School</th>
+                <th>Name of Staff Member</th>
+            </tr>
+            <?php 
+            foreach ( $info as $school_name => $names ) {
+                foreach ( $names as $name ) {
+                    if ( !empty( $name ) ) {
+                        echo "<tr><td>" . $school_name . "</td><td>" . $name . "</td></tr>";
+                    }
+                }
+            }
+            ?>
+        </table>
+    </body>
+</html>
