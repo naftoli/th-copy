495ea3a20ac727d10763e5b02bca0a9c808fb095
Menachem Hornbacher <mendelh1537@gmail.com>
2018-12-11 11:07:14 -0500

    bring in base commander to the correct folder
    Former-commit-id: 955854fc807ae1d7c3243bfbaeb3b9f610e97550

---
+++ achos/includes/globals.php
@@ -0,0 +1,10 @@
+<?php
+require_once( __DIR__ . '/mysql.php' );
+// detect if the server is running on 192.168., if so connect to the remote server, else use localhost.
+// use triple equals since it should return 0 which is falsy
+$development = isset( $_SERVER['HTTP_HOST'] ) ? strpos($_SERVER['HTTP_HOST'], "mashpia.com") === false : true;
+
+$global_db_host = $development ? "mashpia.com" : "localhost";
+// DBS credentials
+$global_db_user = 'mashpia_cth';
+$global_db_pass = 'UlqKsfnTUq2A';
