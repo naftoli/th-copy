a6b94575a917004cfe8922e52ae54098b725e862
Menachem Hornbacher <mendelh1537@gmail.com>
2018-12-11 15:52:05 -0500

    update deployment script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,19 @@
-echo 'Deploying...'
+echo 'Pull code changes:'
+git pull
 
-pwd
+echo 'Install PHP dependencies:'
 
-tree .
+cd ../mashpia.com
+composer install
+cd ../deploy
+
+echo ''
+echo 'Deleting existing /new/'
+rm -rf ../mashpia.com/public/new/*
+
+echo 'Moving new code to /new/'
+mv build/* ../mashpia.com/public/new
+mv build/.[!.]* ../mashpia.com/public/new
+rm -rf build # delete the build folder
+
+echo 'Changes Deployed'
