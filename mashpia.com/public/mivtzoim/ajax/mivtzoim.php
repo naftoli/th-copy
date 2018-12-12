55efd0dd6ce3902a3051b99066b48ef51a541272
Menachem Hornbacher <mendelh1537@gmail.com>
2018-12-11 22:03:01 -0500

    add naftoli's changes by hand

--- mashpia.com/public/mivtzoim/ajax/mivtzoim.php
+++ mashpia.com/public/mivtzoim/ajax/mivtzoim.php
@@ -3,10 +3,20 @@ $admin_auth = ['school'];
 require_once $_SERVER['DOCUMENT_ROOT'] . '/header.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/api/header/header.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/api/header/db.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/mivtzoim/classes/mivtzoim.php';
+
+$info = [
+    'data'  =>  [], 
+    'error' =>  false
+];
 
 $id = $_POST['id'];
-$sth = $MASHPIA_DB->prepare("select * from mivtzoim where mivtzoim_id = :id");
-$sth->execute( [':id' => $id] );
-$rows = $sth->fetchAll();
-if ( $rows ) echo json_encode( $rows );
-else echo json_encode( $sth->errorInfo() );
+try {
+    $m = new Mivtzoim( $id );
+    $names = $m->getShortNames();
+    $info['data'] = $names;
+} catch ( \Exception $e ) {
+    $info['data'] = $e->getMessage();
+    $info['error'] = true;
+}
+echo json_encode( $info );
--- mashpia.com/public/mivtzoim/mark.php
+++ mashpia.com/public/mivtzoim/mark.php
@@ -4,17 +4,16 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/header.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/api/header/header.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/api/header/db.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/mivtzoim/classes/mivtzoim.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/class.adminSchools.php';
+
+$as = new AdminSchools( $admin_user['admin_id'], $admin_user['auth'] );
+$schools = $as->getSchools();
 
 if ( isset( $_POST['action'] ) && $_POST['action'] == 'mark' ) {
     //echo "<pre>"; print_r( $_POST ); echo "</pre>"; exit;
     $mivtzoim_id = $_POST['mivtzoim'];
-    $parsha = $_POST['parsha'];
 
     $m = new Mivtzoim( $mivtzoim_id );
-    $arrParsha = explode('|', $parsha);
-    $start = $arrParsha[0];
-    $end = $arrParsha[1];
-    $m->setDates( $start, $end );
 
     // build marks array
     $marks = [];
@@ -23,52 +22,70 @@ if ( isset( $_POST['action'] ) && $_POST['action'] == 'mark' ) {
         $info = explode('|', $k);
         $grid_id = $info[0];
         $user_id = $info[1];
+        $start_date = $info[2];
+        $end_date = $info[3];
+
         if ( is_array( $more ) ) {
             // only get first value of $more 
             // if there's 2 values, the second one will be discarded so that it won't overwrite the first one
             $v = $more[0];
-            if ( $v == 'on' ) $marks[$grid_id][$user_id] = 1;
-            else if ( $v != '' ) $marks[$grid_id][$user_id] = $v;
+            if ( $v == 'on' ) $marks[$grid_id][$user_id][$start_date][$end_date] = 1;
+            else if ( $v != '' ) $marks[$grid_id][$user_id][$start_date][$end_date] = $v;
         }
     }
     $m->markTasks( $marks );
 }
 
 //echo "<pre>"; print_r( $_POST ); echo "</pre>"; exit;
+$school = $_POST['school'];
 $grade = $_POST['grade'];
 $mivtzoim_id = $_POST['mivtzoim'];
-$parsha = $_POST['parsha'];
+$task = isset( $_POST['task'] ) ? $_POST['task'] : 0;
 
-if ( $grade && $mivtzoim_id && $parsha ) {
+if ( $school && $grade && $mivtzoim_id ) {
     try {
         $m = new Mivtzoim( $mivtzoim_id );
     } catch ( \Exception $e ) {
         echo $e->getMessage();
         exit;
     }
-    $arrParsha = explode('|', $parsha);
-    $start = $arrParsha[0];
-    $end = $arrParsha[1];
-    $parsha_name = $arrParsha[2];
-    $m->setDates( $start, $end );
-
-    $sth = $MASHPIA_DB->prepare("
+    
+    $qry = "
         SELECT 
-            user_id, first, last 
+            class_grade,
+            class_sub,
+            user_id,
+            first,
+            last
         FROM
-            users 
+            users u
+                JOIN
+            classes c ON c.class_id = u.class_id
         WHERE
-            user_registered > 0 AND class_id = :grade 
-        ORDER BY last, first
-    ");
-    $sth->execute([
-        ':grade'    =>  $grade
-    ]);
-    $users = $sth->fetchAll();
+            user_registered > 0
+                AND u.school_id = :school";
+    if ( $grade > 0 ) $qry .= " AND u.class_id = :grade";
+    $qry .= " ORDER BY class_grade , class_sub , last , first";
+
+    $sth = $MASHPIA_DB->prepare( $qry );
+    if ( $grade > 0 ) {
+        $sth->execute([
+            ':school'   => $school,
+            ':grade'    => $grade
+        ]);
+    } else {
+        $sth->execute([':school' => $school]);
+    }
+    $rows = $sth->fetchAll();
+    foreach ( $rows as $row ) {
+        $class_name = $row['class_grade'] . (empty( $row['class_sub'] ) ? '' : '-' . $row['class_sub']);
+        $users[$class_name][] = $row;
+    }
 } else {
     header("Location: index.php");
     exit;
 }
+//echo "<pre>"; print_r( $users ); echo "</pre>"; exit;
 ?>
 <!DOCTYPE html>
 <html>
@@ -82,17 +99,19 @@ if ( $grade && $mivtzoim_id && $parsha ) {
         <?php require $_SERVER['DOCUMENT_ROOT'] . '/admin_header.php'; ?>
         <h1>Mark Mivtzoim</h1>
 
-        <h2><?= $parsha_name; ?></h2>
-
         <form action="mark.php" method="post">
+            <input type='submit' name='submit' value='Save' />
             <input type="hidden" name="action" value="mark" />
             <input type="hidden" name="mivtzoim" value="<?=$mivtzoim_id?>" />
-            <input type="hidden" name="grade" value=<?=$grade?>" />
-            <input type="hidden" name="parsha" value="<?=$parsha?>" />
+            <input type="hidden" name="school" value="<?=$school?>" />
+            <input type="hidden" name="grade" value="<?=$grade?>" />
+            <input type="hidden" name="task" value="<?=$task?>" />
+            <h2><?=$schools[$school]?></h2>
             <?php
             // find out how many tasks we need to setup
             try {
                 $tasks = $m->getTasks();
+                $names = $m->getShortNames();
             }  catch ( \Exception $e ) {
                 echo $e->getMessage();
                 exit;
@@ -100,46 +119,63 @@ if ( $grade && $mivtzoim_id && $parsha ) {
             // find out what the grid ids are
             $grid_ids = [];
             foreach ( $tasks as $short_name => $details ) {
-                foreach ( $details as $task ) {
-                    $grid_ids[] = $task['grid_id'];
+                if ( $task > 0 && $names[$task] != $short_name ) continue; // if bc has identified a specific task category to mark, only show those tasks relevant to that task category
+                foreach ( $details as $row ) {
+                    $grid_ids[] = $row['grid_id'];
                 }
             }
             $marks = $m->getMarks( $grid_ids );
             //echo "<pre>"; print_r( $marks ); echo "</pre>";
 
-            echo "<table><thead><tr><th>Student</th>";
+            
+            $numTasksWithQty = 0; // only show total if we have more than one task with qty
+            echo "<table><thead><tr><th>Grade</th><th>Student</th>";
             foreach ( $tasks as $short_name => $details ) {
-                foreach ( $details as $task ) {
-                    echo "<th>" . $task['name'] . "</th>";
+                if ( $task > 0 && $names[$task] != $short_name ) continue;
+                foreach ( $details as $row ) {
+                    echo "<th>" . $row['name'] . "</th>";
+                    if ( $row['quantity'] >= 1 ) $numTasksWithQty++;
                 } 
+                if ( $numTasksWithQty > 1 ) echo "<th>Total</th>";
             }
             echo "</tr></thead><tbody>"; 
-            foreach ( $users as $user ) {
-                echo "<tr><td>" . $user['first'] . ' ' . $user['last'] . "</td>";
-                foreach ( $tasks as $short_name => $details ) {
-                    foreach ( $details as $task ) {
-                        $identifier = $task['grid_id'] . '|' . $user['user_id'];
-                        echo "<td>";
-                        // figure out if input is checkbox or value
-                        if ( $task['quantity'] >= 1 ) {
-                            echo "<input type='text' name='" . $identifier . "[]' size='4' ";
-                            if ( isset( $marks[$task['grid_id']][$user['user_id']] ) ) echo "value='" . $marks[$task['grid_id']][$user['user_id']] . "' ";
-                            echo "/>";
-                        } else {
-                            echo "<input type='checkbox' name='" . $identifier . "[]'";
-                            if ( isset( $marks[$task['grid_id']][$user['user_id']] ) ) echo "value='1' checked";
-                            echo " />";
-                            echo "<input type='hidden' name='" . $identifier . "[]' value='0' />";
+            foreach ( $users as $grade => $students ) {
+                foreach ( $students as $user ) {
+                    echo "<tr><td>" . $grade . "</td><td>" . $user['first'] . ' ' . $user['last'] . "</td>";
+                    foreach ( $tasks as $short_name => $details ) {
+                        if ( $task > 0 && $names[$task] != $short_name ) continue;
+                        $totalQty = 0;
+                        foreach ( $details as $row ) {
+                            $mark_id = $row['start_date'] . '|' . $row['end_date']; // for finding corrent mark in marks array
+                            $identifier = $row['grid_id'] . '|' . $user['user_id'] . '|' . $mark_id;
+                            echo "<td>";
+                            // figure out if input is checkbox or value
+                            if ( $row['quantity'] >= 1 ) {
+                                echo "<input type='text' name='" . $identifier . "[]' size='4' ";
+                                if ( isset( $marks[$row['grid_id']][$user['user_id']][$mark_id] ) ) {
+                                    echo "value='" . $marks[$row['grid_id']][$user['user_id']][$mark_id] . "' ";
+                                    $totalQty += $marks[$row['grid_id']][$user['user_id']][$mark_id];
+                                }
+                                echo "/>";
+                            } else {
+                                echo "<input type='checkbox' name='" . $identifier . "[]'";
+                                if ( isset( $marks[$row['grid_id']][$user['user_id']][$mark_id] ) ) echo "value='1' checked";
+                                echo " />";
+                                echo "<input type='hidden' name='" . $identifier . "[]' value='0' />";
+                            }
+                            echo "</td>";
+                        } 
+                        if ( $numTasksWithQty > 1 ) {
+                            echo "<td>" . $totalQty . "</td>";
                         }
-                        echo "</td>";
-                    } 
+                    }
+                    echo "</tr>";
                 }
-                echo "</tr>";
             }
             echo "</tbody></table>";
             ?>
             <br />
-            <input type="submit" name="submit" value="Save" />
+            <input type='submit' name='submit' value='Save' />
         </form>
     </body>
 </html>
