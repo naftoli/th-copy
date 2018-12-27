f53e681bdbe1487296bd0518514bb18df04210b2
naftolir <naftolir@gmail.com>
2018-12-27 13:24:01 -0500

    make sure to show users in alphabetical order

--- mashpia.com/public/api/print/missions.php
+++ mashpia.com/public/api/print/missions.php
@@ -22,16 +22,30 @@ if ( !$class_ids ) {
     $class_ids = array_map( function ($p) { return $p->class_id; }, $school->platoons );
 }
 
+$users = [];
 if ( !$user_ids ) {
-    $users = \Soldier::find_all_by_class_id( $class_ids );
-    $users = array_filter($users, function ($u) { return $u->user_registered; });
-    $user_ids = array_map(function ($u) { return $u->user_id; }, $users);
+    // do each class separately so that we can order the children alphabetically
+    foreach ( $class_ids as $class_id ) {
+        $usersTmp = \Soldier::find_all_by_class_id( $class_id );
+        $usersTmp = array_filter($usersTmp, function ($u) { return $u->user_registered; });
+        $user_idsTmp = array_map(function ($u) { return $u->user_id; }, $users);
+        // order users alphabetically
+        usort( $usersTmp, function( $a, $b ) {
+            return $a->last > $b->last;
+        });
+        array_merge( $usersTmp, $users );
+        array_merge( $user_idsTmp, $user_ids );
+    }
 // make sure the soldiers are in the selected platoons if provided with an array of soldiers.
 } else if ( $user_ids ) {
     $users = \Soldier::find( $user_ids );
     $users = is_array( $users ) ? $users : [ $users ]; // make sure it is an array so we can filter it
     $users = array_filter($users, function ($u) use ($class_ids) { return in_array( $u->class_id, $class_ids ); });
     $user_ids = array_map(function ($u) { return $u->user_id; }, $users);
+    // order users alphabetically
+    usort( $usersTmp, function( $a, $b ) {
+        return $a->last > $b->last;
+    });
 }
 
 if ( !$parsha_ids ) {
