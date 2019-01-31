54d5bf1637b57d426d309889bec5c12fcbf2735b
naftolir <naftolir@gmail.com>
2019-01-31 14:22:46 -0500

    authorize capturing

--- mashpia.com/public/chidon/chidon_drive/ajax/authorize.php
+++ mashpia.com/public/chidon/chidon_drive/ajax/authorize.php
@@ -58,44 +58,5 @@ function chargeCreditCard( $amount, $cc_info )
     //$response = $controller->executeWithApiResponse(\net\authorize\api\constants\ANetEnvironment::PRODUCTION);
     $response = $controller->executeWithApiResponse( Constants::GetApiEndpoint() );
 
-    echo Constants::GetApiEndpoint(); exit;
-
-    if ($response != null) {
-        // Check to see if the API request was successfully received and acted upon
-        if ($response->getMessages()->getResultCode() == "Ok") {
-            // Since the API request was successful, look for a transaction response
-            // and parse it to display the results of authorizing the card
-            $tresponse = $response->getTransactionResponse();
-        
-            if ($tresponse != null && $tresponse->getMessages() != null) {
-                echo " Successfully created transaction with Transaction ID: " . $tresponse->getTransId() . "\n";
-                echo " Transaction Response Code: " . $tresponse->getResponseCode() . "\n";
-                echo " Message Code: " . $tresponse->getMessages()[0]->getCode() . "\n";
-                echo " Auth Code: " . $tresponse->getAuthCode() . "\n";
-                echo " Description: " . $tresponse->getMessages()[0]->getDescription() . "\n";
-            } else {
-                echo "Transaction Failed \n";
-                if ($tresponse->getErrors() != null) {
-                    echo " Error Code  : " . $tresponse->getErrors()[0]->getErrorCode() . "\n";
-                    echo " Error Message : " . $tresponse->getErrors()[0]->getErrorText() . "\n";
-                }
-            }
-            // Or, print errors if the API request wasn't successful
-        } else {
-            echo "Transaction Failed \n";
-            $tresponse = $response->getTransactionResponse();
-        
-            if ($tresponse != null && $tresponse->getErrors() != null) {
-                echo " Error Code  : " . $tresponse->getErrors()[0]->getErrorCode() . "\n";
-                echo " Error Message : " . $tresponse->getErrors()[0]->getErrorText() . "\n";
-            } else {
-                echo " Error Code  : " . $response->getMessages()->getMessage()[0]->getCode() . "\n";
-                echo " Error Message : " . $response->getMessages()->getMessage()[0]->getText() . "\n";
-            }
-        }
-    } else {
-        echo  "No response returned \n";
-    }
-
     return $response;
 }
--- mashpia.com/public/chidon/chidon_drive/ajax/processDonation.php
+++ mashpia.com/public/chidon/chidon_drive/ajax/processDonation.php
@@ -66,62 +66,114 @@ if ( !empty( $msg ) ) {
 // process donation amount through authorize
 require 'authorize.php';
 $response = chargeCreditCard( $donation, $cc_info );
-exit;
 
-// if successful add donation and user donations to db
-$MASHPIA_DB->beginTransaction();
+if ($response != null) {
+  // Check to see if the API request was successfully received and acted upon
+  if ($response->getMessages()->getResultCode() == "Ok") {
+      // Since the API request was successful, look for a transaction response
+      // and parse it to display the results of authorizing the card
+      $tresponse = $response->getTransactionResponse();
+  
+      if ($tresponse != null && $tresponse->getMessages() != null) {
+          echo " Successfully created transaction with Transaction ID: " . $tresponse->getTransId() . "\n";
+          echo " Transaction Response Code: " . $tresponse->getResponseCode() . "\n";
+          echo " Message Code: " . $tresponse->getMessages()[0]->getCode() . "\n";
+          echo " Auth Code: " . $tresponse->getAuthCode() . "\n";
+          echo " Description: " . $tresponse->getMessages()[0]->getDescription() . "\n";
 
-$stmt = $MASHPIA_DB->prepare("
-  INSERT INTO chidon_donations 
-  SET admin_id = :admin, 
-  chidon_year = :year, 
-  donation_amount = :donation,
-  transaction_id = :trans_id, 
-  transaction_info = :trans_info
-");
-$res = $stmt->execute([
-  ':admin'    =>  $admin_user['admin_id'], 
-  ':year'     =>  $year, 
-  ':donation' =>  $donation, 
-  ':trans_id' =>  111, 
-  ':trans_info' => '1:111:succes'
-]);
-if ( !$res ) {
-  $MASHPIA_DB->rollBack();
-  echo json_encode([
-    'success' => false,
-    'message' => 'Error entering user donation into db.'
-  ]);
-  exit;
-}
+          $trans_id = $tresponse->getTransId();
+          $trans_info = $trans_id . ":" . $tresponse->getResponseCode() . ":" . $tresponse->getMessages()[0]->getCode() . ":". $tresponse->getAuthCode() . ":" . $tresponse->getMessages()[0]->getDescription();
 
-$stmt = $MASHPIA_DB->prepare("
-  INSERT INTO chidon_user_subsidies 
-  SET chidon_donation_id = :donation_id, 
-  chidon_year = :year,
-  user_id = :user,
-  subsidy_amount = :amount
-");
-foreach ( $user_donations as $donation ) {
-  $res = $stmt->execute([
-    ':donation_id'  =>  $MASHPIA_DB->lastInsertId(), 
-    ':year'         =>  $year, 
-    ':user'         =>  $donation['user_id'], 
-    ':amount'       =>  $donation['amount']
-  ]);
-  if ( !$res ) {
-    $MASHPIA_DB->rollBack();
-    echo json_encode([
-      'success' => false,
-      'message' => 'Error entering user donation into db.'
-    ]);
-    exit;
+          // save to transactions table.
+          $transaction_query = $MASHPIA_DB->prepare(
+              "INSERT INTO transactions (trans_date, description, amount, response) "
+              ."VALUES (NOW(), ?, ?, ?)"
+          );
+          $statusTransaction = $transaction_query->execute([
+              $trans_info, $donation, json_encode( $response )
+          ]);
+          $trans_db_id = $MASHPIA_DB->lastInsertId();
+
+          // add donation and user donations to db
+          $MASHPIA_DB->beginTransaction();
+
+          $success = true;
+          $stmt = $MASHPIA_DB->prepare("
+            INSERT INTO chidon_donations 
+            SET admin_id = :admin, 
+            chidon_year = :year, 
+            donation_amount = :donation,
+            transaction_id = :trans_id, 
+            transaction_info = :trans_info
+          ");
+          $res = $stmt->execute([
+            ':admin'    =>  $admin_user['admin_id'], 
+            ':year'     =>  $year, 
+            ':donation' =>  $donation, 
+            ':trans_id' =>  $trans_id, 
+            ':trans_info' => $trans_info
+          ]);
+          if ( $res ) {
+            $stmt = $MASHPIA_DB->prepare("
+              INSERT INTO chidon_user_subsidies 
+              SET chidon_donation_id = :donation_id, 
+              chidon_year = :year,
+              user_id = :user,
+              subsidy_amount = :amount
+            ");
+            foreach ( $user_donations as $donation ) {
+              $res2 = $stmt->execute([
+                ':donation_id'  =>  $MASHPIA_DB->lastInsertId(), 
+                ':year'         =>  $year, 
+                ':user'         =>  $donation['user_id'], 
+                ':amount'       =>  $donation['amount']
+              ]);
+              if ( !$res2 ) {
+                $success = false;
+                break;
+              }
+            }
+          } else {
+            $success = false;
+          }
+
+          if ( $success ) {
+            $MASHPIA_DB->commit();
+          } else {
+            $MASHPIA_DB->rollBack();
+            echo "There was an error saving the donation to the database. please contact HQ.";
+
+            // send email to self with problem
+            $to      = 'naftoli@tzivoshashem.org';
+            $subject = 'Chidon Drive Error';
+            $message = 'Error in inserting donations into chidon drive db tables. Ref: ' . $trans_db_id;
+            $headers = array(
+                'From'     => 'cth@mashpia.com',
+                'Reply-To' => 'cth@mashpia.com'
+            );
+            @mail($to, $subject, $message, $headers);
+          }
+      } else {
+          echo "Transaction Failed \n";
+          if ($tresponse->getErrors() != null) {
+              echo " Error Code  : " . $tresponse->getErrors()[0]->getErrorCode() . "\n";
+              echo " Error Message : " . $tresponse->getErrors()[0]->getErrorText() . "\n";
+          }
+      }
+      // Or, print errors if the API request wasn't successful
+  } else {
+      echo "Transaction Failed \n";
+      $tresponse = $response->getTransactionResponse();
+  
+      if ($tresponse != null && $tresponse->getErrors() != null) {
+          echo " Error Code  : " . $tresponse->getErrors()[0]->getErrorCode() . "\n";
+          echo " Error Message : " . $tresponse->getErrors()[0]->getErrorText() . "\n";
+      } else {
+          echo " Error Code  : " . $response->getMessages()->getMessage()[0]->getCode() . "\n";
+          echo " Error Message : " . $response->getMessages()->getMessage()[0]->getText() . "\n";
+      }
   }
+} else {
+  echo  "No response returned \n";
 }
-
-// if we get here all is good
-$MASHPIA_DB->commit();
-echo json_encode([
-  'success' =>  true, 
-  'message' =>  'Your donation has been processed and given out to the users.'
-]);
+?>
