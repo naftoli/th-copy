701e5217e05b515cf7e730299f65868a88dd433a
naftolir <naftolir@gmail.com>
2019-02-11 21:06:36 -0500

    updates

--- mashpia.com/public/chidon/pr/index.php
+++ mashpia.com/public/chidon/pr/index.php
@@ -37,7 +37,7 @@ $rows = $stmt->fetchAll();
       <?php
       $previousSchool = '';
       $previousGrade = '';
-      foreach ( $rows as $row ) {
+      foreach ( $rows as $index => $row ) {
         $school = $row['school_name'];
         $grade = $row['class_grade'] . (empty( $row['class_sub'] ) ? '' : '-' . $row['class_sub']);
         if ( $school != $previousSchool || $grade != $previousGrade ) {
@@ -55,21 +55,23 @@ $rows = $stmt->fetchAll();
         ?>
         <div class="letter">
           <img src='images/Chidon-Header-5779-01.jpg' />
+
+          <div class="content">
           <h2>Thank you for being such a great partner!</h2>
-  
-          Dear <?=$row['Afirst'] . ' ' . $row['last']?>,
-          <br /><br />
-          I would like to thank you. Your child <?=$row['first']?> has gained so much from learning with you and we truly recognize the time, effort, and encouragement you invested. They could not have done this without you, and for this, you deserve a chidon medal.<br /><br />
-          When I helped restart Chidon six years ago, who would have believed the revolution in chinuch it would inspire? That it would so capture the hearts and minds of thousands of children worldwide? Thousands of children today voluntarily sacrifice their time and comfort to toil in Torah to master the 613 mitzvos, and spread that passion to those around them. They know details of the mitzvos that even their parents and teachers don’t know!<br /><br />
-          Indeed, after six years of growing Chidon, the results are in: The participants’ voluntary learning fosters  study skills and a foundation of Torah knowledge they will build on for the rest of their lives, including (but certainly not limited to) the study of Rambam—cultivating greater Hiskashrus to the Rebbe and the development of their Yiras Shomayim. These are the children on the front lines to greet Moshiach and are truly prepared for the day when we will merit the fulfillment of all 613 Mitzvos, bekarov mamosh.
-          <br /><br />
-          Whether they made it to the Shabbaton or not, they already achieved the ultimate success. To me, to you, to all of their mechanchim, they are all truly winners.<br /><br />
-          Wishing you yiddishe and chassidishe nachas, always,<br /><br />
-          Rabbi Shimmy Weinbaum<br />
-          Tzivos Hashem
+            Dear <?=$row['Afirst'] . ' ' . $row['last']?>,
+            <br /><br />
+            I would like to thank you. Your child <?=$row['first']?> has gained so much from learning with you and we truly recognize the time, effort, and encouragement you invested. They could not have done this without you, and for this, you deserve a chidon medal.<br /><br />
+            When I helped restart Chidon six years ago, who would have believed the revolution in chinuch it would inspire? That it would so capture the hearts and minds of thousands of children worldwide? Thousands of children today voluntarily sacrifice their time and comfort to toil in Torah to master the 613 mitzvos, and spread that passion to those around them. They know details of the mitzvos that even their parents and teachers don’t know!<br /><br />
+            Indeed, after six years of growing Chidon, the results are in: The participants’ voluntary learning fosters  study skills and a foundation of Torah knowledge they will build on for the rest of their lives, including (but certainly not limited to) the study of Rambam—cultivating greater Hiskashrus to the Rebbe and the development of their Yiras Shomayim. These are the children on the front lines to greet Moshiach and are truly prepared for the day when we will merit the fulfillment of all 613 Mitzvos, bekarov mamosh.
+            <br /><br />
+            Whether they made it to the Shabbaton or not, they already achieved the ultimate success. To me, to you, to all of their mechanchim, they are all truly winners.<br /><br />
+            Wishing you yiddishe and chassidishe nachas, always,<br /><br />
+            Rabbi Shimmy Weinbaum<br />
+            Tzivos Hashem
+          </div>
         </div>
         <div style='page-break-after: always'></div>
-      <?php } ?>
+      <?php break;} ?>
     </div>
   </body>
 </html>
