021a18a7f42154690a13f763ba054c07ab362ab7
naftolir <naftolir@gmail.com>
2019-02-14 15:40:03 -0500

    more routing

--- mashpia.com/public/chidon/chidon_drive/index.php
+++ mashpia.com/public/chidon/chidon_drive/index.php
@@ -4,5 +4,5 @@ $url = $_SERVER['REQUEST_URI'];
 $pos = strrpos( $url, '/' );
 $url_end = substr( $url, $pos + 1 );
 if ( $url_end ) header("Location: " . $_SERVER['REMOTE_HOST'] . "/chidon/chidon_drive/site/family-single.html?id=" . $url_end);
-else header("Location: /site");
+else header("Location: " . $_SERVER['REMOTE_HOST'] . "/chidon/chidon_drive/site/");
 exit;
