0edaa7ccaa8be03fe60438eac6a3a480609d561e
naftolir <naftolir@gmail.com>
2019-02-14 15:30:48 -0500

    change routing

--- mashpia.com/public/chidon/chidon_drive/index.php
+++ mashpia.com/public/chidon/chidon_drive/index.php
@@ -3,5 +3,6 @@
 $url = $_SERVER['REQUEST_URI'];
 $pos = strrpos( $url, '/' );
 $url_end = substr( $url, $pos + 1 );
-header("Location: " . $_SERVER['REMOTE_HOST'] . "/chidon/chidon_drive/site/family-single.html?id=" . $url_end);
+if ( is_number( $url_end ) ) header("Location: " . $_SERVER['REMOTE_HOST'] . "/chidon/chidon_drive/site/family-single.html?id=" . $url_end);
+else header("Location: " . $_SERVER['REMOTE_HOST'] . "/chidon/chidon_drive/site");
 exit;
