d27bc198df3e803476f19eb3613d5ae3911f513d
naftolir <naftolir@gmail.com>
2019-02-14 23:51:01 -0500

    more changes bc of domain name

--- mashpia.com/public/chidon/chidon_drive/classes/ChidonDrive.php
+++ mashpia.com/public/chidon/chidon_drive/classes/ChidonDrive.php
@@ -1,5 +1,5 @@
  <?php
-require_once $_SERVER['DOCUMENT_ROOT'] . '/api/header/db.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/../../api/header/db.php';
 
 class ChidonDriveGlobal {
   private $year;
