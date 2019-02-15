9ea4b7ac5b72dfc50affc1658e65d0766ee44791
naftolir <naftolir@gmail.com>
2019-02-15 14:05:27 -0500

    route to secure page

--- mashpia.com/public/chidon/chidon_drive/index.php
+++ mashpia.com/public/chidon/chidon_drive/index.php
@@ -3,6 +3,6 @@
 $url = $_SERVER['REQUEST_URI'];
 $pos = strrpos( $url, '/' );
 $url_end = substr( $url, $pos + 1 );
-if ( $url_end ) header("Location: " . $_SERVER['REMOTE_HOST'] . "/site/family-single.html?id=" . $url_end);
-else header("Location: " . $_SERVER['REMOTE_HOST'] . "/site");
+if ( $url_end ) header("Location: https://" . $_SERVER['REMOTE_HOST'] . "/site/family-single.html?id=" . $url_end);
+else header("Location: https://" . $_SERVER['REMOTE_HOST'] . "/site");
 exit;
