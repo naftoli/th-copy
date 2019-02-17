dab95d54df69e469bcd63aef5f521c64a75dbd98
naftolir <naftolir@gmail.com>
2019-02-16 21:38:56 -0500

    more debugging

--- mashpia.com/public/chidon/chidon_drive/index.php
+++ mashpia.com/public/chidon/chidon_drive/index.php
@@ -1,7 +1,7 @@
 <?php
 // routing of family names
 $url = $_SERVER['REQUEST_URI'];
-$host = isset( $_SERVER['REMOTE_HOST'] ) ? $_SERVER['REMOTE_HOST'] : 'tzivos.local';
+$host = isset( $_SERVER['SERVER_NAME'] ) ? $_SERVER['SERVER_NAME'] : 'tzivos.local';
 $pos = strrpos( $url, '/' );
 $url_end = substr( $url, $pos + 1 );
 if ( $url_end ) header("Location: https://" . $host . "/site/family-single.html?id=" . $url_end);
