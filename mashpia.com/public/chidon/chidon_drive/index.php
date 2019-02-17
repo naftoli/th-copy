4681b4031a987540a6c544974fc04e86b2698a9e
naftolir <naftolir@gmail.com>
2019-02-16 21:36:53 -0500

    more routing issues

--- mashpia.com/public/chidon/chidon_drive/index.php
+++ mashpia.com/public/chidon/chidon_drive/index.php
@@ -4,6 +4,6 @@ $url = $_SERVER['REQUEST_URI'];
 $host = isset( $_SERVER['REMOTE_HOST'] ) ? $_SERVER['REMOTE_HOST'] : 'tzivos.local';
 $pos = strrpos( $url, '/' );
 $url_end = substr( $url, $pos + 1 );
-if ( $url_end ) header("Location: https://" . $_SERVER['REMOTE_HOST'] . "/site/family-single.html?id=" . $url_end);
-else header("Location: https://" . $_SERVER['REMOTE_HOST'] . "/site");
+if ( $url_end ) header("Location: https://" . $host . "/site/family-single.html?id=" . $url_end);
+else header("Location: https://" . $host . "/site");
 exit;
