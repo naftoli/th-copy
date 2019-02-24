1025f9c590927b3437f3a7d574327750cf91b25f
naftoli <34518183+naftoli@users.noreply.github.com>
2019-02-24 17:46:19 -0500

    Revert "Staging"

--- mashpia.com/public/review_enrollment.php
+++ mashpia.com/public/review_enrollment.php
@@ -21,7 +21,7 @@ foreach ($schools as $sid => $schoolName) {
             join admins a using (admin_id) 
             where tc.year = " . $year . "
             and tc.can_enroll = 1
-            and (tc.contestant = 1 or tc.school_rep = 1) 
+            and tc.contestant = 1
             and tc.date_paid > 0 
             and aa.auth = 'user'
             and u.school_id = " . $sid;
