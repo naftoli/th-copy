3ea44ed6c45a7c5b6f536115e0fea969f5138a71
naftolir <naftolir@gmail.com>
2019-02-26 13:39:40 -0500

    fix error message coming up

--- mashpia.com/public/registration_7.php
+++ mashpia.com/public/registration_7.php
@@ -142,7 +142,7 @@ function clean_character($string)
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
-<html xmlns="http://www.w3.org/1999/xhtml" dir="<?=$dir?>">
+<html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=8" />
