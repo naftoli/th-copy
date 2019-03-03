7e8effd0091e956118595bad377d3123cdeec35f
naftolir <naftolir@gmail.com>
2019-03-03 15:03:14 -0500

    enable editing of chidon enrollment

---
+++ mashpia.com/public/chidon/chidon_drive/ajax/getChildrenToEdit.php
@@ -0,0 +1,53 @@
+<?php
+require __DIR__ . '/../../../api/header/db.php';
+require __DIR__ . '/../../../class.globalSettings.php';
+require __DIR__ . '/../encrypt.php';
+
+$year = GlobalSettings::getChidonYear();
+$admin = mysql_real_escape_string( $_POST['admin'] );
+$admin_id = encrypt_decrypt('decrypt', $admin);
+
+if ( $admin_id ) {
+  $stmt = $MASHPIA_DB->prepare("
+    SELECT 
+        u.user_id, u.first, u.last, u.first_he, u.last_he, u.mobile_pic, u.user_photo_id, u.gender, tc.* 
+    FROM
+        users u
+            JOIN
+        th_chidon tc USING (user_id)
+    WHERE
+        tc.year = :year AND tc.parent_id = :admin
+            AND (tc.contestant = 1 or tc.school_rep = 1) 
+            AND tc.date_paid > 0 
+            AND tc.allow_edit = 1 
+  ");
+  $res = $stmt->execute([
+    ':year'   =>  $year, 
+    ':admin'  =>  $admin_id
+  ]);
+  //echo $stmt->debugDumpParams(); exit;
+  if ( $res ) {
+    $children = $stmt->fetchAll();
+    if ( $children ) {
+      echo json_encode([
+        'success'   =>  true,
+        'children'  =>  $children
+      ]);
+    } else {
+      echo json_encode([
+        'success'   =>  false,
+        'error'     =>  "Your child(ren)'s school has not enabled editing of your child(ren)'s enrollment."
+      ]);
+    }
+  } else {
+    echo json_encode([
+      'success'   =>  false,
+      'error'     =>  'There was an error retreiving your children information.'
+    ]);
+  }
+} else {
+  echo json_encode([
+    'success'   =>  false,
+    'error'     =>  'Invalid Admin ID.'
+  ]);
+}
--- mashpia.com/public/mobile/reg/ajax/getChildren.php
+++ mashpia.com/public/mobile/reg/ajax/getChildren.php
@@ -123,16 +123,16 @@ if ( !empty( $users ) ) {
 		
 		// chidon regustration
 		// only relevant until 10/16/2018 12:00am then close down chidon reg
-		if ( unixtojd() < 2458409 ) {
-			if ( !$row['reg_chidon'] // if not in chidon
-				&& $row['class_grade'] >= 4 // and in grade 4+
-				&& $row['chidon'] // make sure the kid is in chidon
-				//&& in_array( $row['school_id'], $australia ) // and not in australia..
-			) {
-				$children[ $row['user_id'] ]['needsReg'] = 1;
-				$children[ $row['user_id'] ]['reg_types']['chidon'] = true;
-			}
-		}
+		// if ( unixtojd() < 2458409 ) {
+		// 	if ( !$row['reg_chidon'] // if not in chidon
+		// 		&& $row['class_grade'] >= 4 // and in grade 4+
+		// 		&& $row['chidon'] // make sure the kid is in chidon
+		// 		//&& in_array( $row['school_id'], $australia ) // and not in australia..
+		// 	) {
+		// 		$children[ $row['user_id'] ]['needsReg'] = 1;
+		// 		$children[ $row['user_id'] ]['reg_types']['chidon'] = true;
+		// 	}
+		// }
 		
 		$children[$row['user_id']]['enrollShabbaton'] = 0;
 		$children[$row['user_id']]['shabbatonRegistered'] = 0;
