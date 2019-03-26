29281540b6984cc8954a229d1794d835dea19e82
naftolir <naftolir@gmail.com>
2019-03-26 13:50:16 -0400

    updates to children

---
+++ mashpia.com/public/chidon/upload/updates.php
@@ -0,0 +1,55 @@
+<?php
+ini_set('display_errors',1);
+require __DIR__ . "/../../db.php";
+$qrys = [];
+$headers = ['th_chidon_id', 'walking_group', 'dropoff_number', 'host_street_num', 'host_street_num_suffix', 'host_street', 'host_street_apt', 'between_streets1', 'between_streets2', 'walking'];
+if (($handle = fopen($_FILES['file']['tmp_name'], "r")) !== FALSE) {
+  $row = 0;
+  $numFields = count( $headers );
+  while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+    if ( $row++ < 1 ) continue;
+    $qry = "update th_chidon set ";
+    for ( $i = 1; $i < $numFields; $i++ ) {
+      if ( $i == ($numFields - 1) ) {
+        $qry .= $headers[$i] . " = " . ( strtolower( $data[$i] ) == 'yes' ? 1 : 0 );
+      } else {
+        $qry .= $headers[$i] . " = '" . $data[$i] . "',";
+      }
+    }
+    $qry .= " where th_chidon_id = " . $data[0];
+    $qrys[] = $qry;
+  }
+  fclose($handle);
+}
+//echo "<pre>"; print_r( $qrys ); echo "</pre>"; exit;
+mysql_query('set autocommit=0');
+mysql_query('begin');
+$success = true;
+
+foreach ( $qrys as $qry ) {
+  if ( !mysql_query( $qry ) ) {
+    echo "There was an error - " . $qry . "<br />" . mysql_error();
+    $success = false;
+    break;
+  }
+}
+
+if ( $success ) mysql_query('commit');
+else mysql_query('rollback');
+mysql_query('set autocommit=1');
+echo "done.";
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  </head>
+  <body>
+    <form action="updates.php" method="post" accept-charset="UTF-8" enctype="multipart/form-data">
+      <label>Upload your spreadsheet
+      <br /><input type="file" name="file" class="file"></label>
+      <br /><input type="submit" name="submit" value="upload" />
+    </form>
+  </body>
+</html>
