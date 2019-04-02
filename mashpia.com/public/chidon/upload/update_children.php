9cf4f315bb784cf765b22e081546eea695c736d7
naftolir <naftolir@gmail.com>
2019-04-02 14:32:35 -0400

    fix bug

--- mashpia.com/public/chidon/upload/update_children.php
+++ mashpia.com/public/chidon/upload/update_children.php
@@ -44,7 +44,7 @@ echo "done.";
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   </head>
   <body>
-    <form action="chidon_children.php" method="post" accept-charset="UTF-8" enctype="multipart/form-data">
+    <form action="update_children.php" method="post" accept-charset="UTF-8" enctype="multipart/form-data">
       <label>Upload your spreadsheet
       <br /><input type="file" name="file" class="file"></label>
       <br /><input type="submit" name="submit" value="upload" />
