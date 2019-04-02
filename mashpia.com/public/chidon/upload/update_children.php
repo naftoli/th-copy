44142ea17abff4e2b2e6366be7df05d1015d3b74
naftolir <naftolir@gmail.com>
2019-04-02 14:37:08 -0400

    fix another bug

--- mashpia.com/public/chidon/upload/update_children.php
+++ mashpia.com/public/chidon/upload/update_children.php
@@ -8,6 +8,7 @@ if (($handle = fopen($_FILES['file']['tmp_name'], "r")) !== FALSE) {
   $numFields = count( $headers );
   while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
     if ( $row++ < 1 ) continue;
+    if ( empty( $data[0] ) ) continue;
     $qry = "update th_chidon set ";
     for ( $i = 1; $i < $numFields; $i++ ) {
       if ( empty( $data[$i] ) ) continue;
