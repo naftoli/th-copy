85db9eb87a53259848e208041642a8f511a25145
naftolir <naftolir@gmail.com>
2019-04-03 08:15:25 -0400

    fix bug

--- mashpia.com/public/chidon/upload/staff_updates.php
+++ mashpia.com/public/chidon/upload/staff_updates.php
@@ -43,7 +43,7 @@ echo "done.";
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   </head>
   <body>
-    <form action="staff_buses.php" method="post" accept-charset="UTF-8" enctype="multipart/form-data">
+    <form action="staff_updates.php" method="post" accept-charset="UTF-8" enctype="multipart/form-data">
       <label>Upload your spreadsheet
       <br /><input type="file" name="file" class="file"></label>
       <br /><input type="submit" name="submit" value="upload" />
