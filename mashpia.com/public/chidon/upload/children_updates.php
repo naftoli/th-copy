cab40f65f30e22abc2a448fd5d7a8fac51cf418a
naftolir <naftolir@gmail.com>
2019-04-04 12:08:30 -0400

    set for reprinting

--- mashpia.com/public/chidon/upload/children_updates.php
+++ mashpia.com/public/chidon/upload/children_updates.php
@@ -13,7 +13,7 @@ if (($handle = fopen($_FILES['file']['tmp_name'], "r")) !== FALSE) {
     for ( $i = 1; $i < $numFields; $i++ ) {
       if ( !empty( $data[$i] ) ) $qry .= $headers[$i] . " = '" . $data[$i] . "',";
     }
-    $qry = substr( $qry, 0, strlen( $qry ) - 1 );
+    $qry .= " not_printed = 1";
     $qry .= " where th_chidon_id = " . $data[0];
     $qrys[] = $qry;
   }
