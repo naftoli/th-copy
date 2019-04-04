37e78e3ca3f86bd18cdbba184fbf5887a3f09310
naftolir <naftolir@gmail.com>
2019-04-04 12:55:02 -0400

    fix bug

--- mashpia.com/public/chidon/upload/children_updates.php
+++ mashpia.com/public/chidon/upload/children_updates.php
@@ -2,8 +2,8 @@
 ini_set('display_errors',1);
 require __DIR__ . "/../../db.php";
 $qrys = [];
-$headers = ['th_chidon_id', 'grade', 'bunk_number', 'test_table', 'round_number', 'sunday_pm_bus', 'cert_number', 'bowling_lane', 'walking_group', 'host', 'host_number', 'acc_street_num', 
-  'acc_street', 'between_streets1', 'between_streets2', 'team_id', 'school_bus', 'open_air_bus', 'coach_bus'];
+$headers = ['th_chidon_id', 'grade', 'bunk_number', 'test_table', 'round_number', 'sunday_pm_bus', 'cert_number', 'bowling_lane', 'walking_group', 'host', 'host_number', 'host_street_num', 
+  'host_street', 'between_streets1', 'between_streets2', 'team_id', 'school_bus', 'open_air_bus', 'coach_bus'];
 if (($handle = fopen($_FILES['file']['tmp_name'], "r")) !== FALSE) {
   $row = 0;
   $numFields = count( $headers );
