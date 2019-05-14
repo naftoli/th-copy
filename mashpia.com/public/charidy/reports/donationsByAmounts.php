75665aa99c532ad0a46f83e0e8a60b1c6e6640cb
naftolir <naftolir@gmail.com>
2019-05-14 15:01:33 -0400

    finish report to show by amounts

--- mashpia.com/public/charidy/reports/donationsByAmounts.php
+++ mashpia.com/public/charidy/reports/donationsByAmounts.php
@@ -3,6 +3,7 @@ require_once '../../db.php';
 
 $donors = [];
 $donations = [];
+$donorsAmounts = [];
 $sql = "select * from mashpia_charidy.donors";
 $result = mysql_query( $sql );
 while ( $row = mysql_fetch_assoc( $result ) ) {
@@ -13,6 +14,7 @@ $sql = "select * from mashpia_charidy.donations";
 $result = mysql_query( $sql );
 while ( $row = mysql_fetch_assoc( $result ) ) {
   $donations[$row['amount']][$row['donor_id']][] = $row;
+  $donorsAmounts[$row['donor_id']][$row['year']] = $row['amount'];
 }
 krsort( $donations );
 ?>
@@ -25,6 +27,7 @@ krsort( $donations );
         font-family: Arial;
         font-size: 14px;
         padding: 5px;
+        border-bottom: 1px dashed grey;
       }
     </style>
   </head>
@@ -38,20 +41,27 @@ krsort( $donations );
         <th>Last Name</th>
         <th>Phone</th>
         <th>Email</th>
-        <th>Year</th>
+        <th>Amounts given</th>
       </tr>
       <?php
       $prevAmount = 0;
       foreach ( $donations as $amount => $other ) {
         foreach ( $other as $donor => $more ) {
           foreach ( $more as $donation ) {
-            echo "<tr><td>";
-            if ( $prevAmount != $amount ) {
-              echo $amount;
-              $prevAmount = $amount;
+            if ( isset( $donors[$donor] ) ) {
+              echo "<tr><td>";
+              if ( $prevAmount != $amount ) {
+                echo $amount;
+                $prevAmount = $amount;
+              }
+              echo "</td><td>" . $donor . "</td><td>" . $donors[$donor]['parent_admin_id'] . "</td><td>" . $donors[$donor]['first_name'] . "</td><td>" . $donors[$donor]['last_name'] . "</td><td>" . 
+                $donors[$donor]['phone'] . "</td><td>" . $donors[$donor]['email'] . "</td><td>";
+              foreach ( $donorsAmounts[$donor] as $year => $donationAmount ) {
+                echo $year . " - " . $donationAmount . "<br />";
+              }
+              echo "</td></tr>";
+              unset( $donors[$donor] ); // remove donor so that he won't show up again.
             }
-            echo "</td><td>" . $donor . "</td><td>" . $donors[$donor]['parent_admin_id'] . "</td><td>" . $donors[$donor]['first_name'] . "</td><td>" . $donors[$donor]['last_name'] . "</td><td>" . 
-              $donors[$donor]['phone'] . "</td><td>" . $donors[$donor]['email'] . "</td><td>" . $donation['year'] . "</td></tr>";
           }
         }
       }
