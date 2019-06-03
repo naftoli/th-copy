e913e8f259d5fdc63b2f75bc941bf930b3bd9f19
unknown <naftoli@tzivoshashem.org>
2019-06-03 19:01:01 -0400

    update leaderboard

--- mashpia.com/public/charidy/callers/leaderboard.php
+++ mashpia.com/public/charidy/callers/leaderboard.php
@@ -1,4 +1,5 @@
 <?php
+ini_set('display_errors',1);
 $admin_auth = ['school']; 
 require_once($_SERVER["DOCUMENT_ROOT"].'/header.php');
 if ( $admin_user['auth'] !== "super" ){
@@ -11,16 +12,18 @@ $result = mysql_query( $sql );
 while ( $row = mysql_fetch_assoc( $result ) ) {
   $callers[] = $row;
 }
-
-$info = [];
-foreach ( $callers as $caller ) {
-
-}
 ?>
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf8" />
+    <style>
+      tr, th, td {
+        font-size: 14px;
+        padding: 5px;
+        font-family: Arial;
+      }
+    </style>
   </head>
   <body>
     <table>
@@ -28,41 +31,55 @@ foreach ( $callers as $caller ) {
         <tr>
           <th>Team</th>
           <th># of People to Call</th>
-          <th># of People Gave</th>
+          <th># of People Gave This Year</th>
           <th>Total Gave in the Past</th>
           <th>Total Gave this Year</th>
         </tr>
       </thead>
       <tbody>
         <?php
+        $grandTotal['people'] = 0;
+        $grandTotal['gave'] = 0;
+        $grandTotal['past'] = 0;
+        $grandTotal['current'] = 0;
         foreach ( $callers as $caller ) {
-          $sql = "select count(*) as number from charidy_donors_callers where year = 5779 and charidy_caller_id = " . $caller['caller_id'];
+          $sql = "select count(*) as number from charidy_donors_callers where year = 5779 and charidy_caller_id = " . $caller['charidy_caller_id'];
           $result = mysql_query( $sql );
           $row = mysql_fetch_assoc( $result );
           $numberPeople = $row['number'];
 
           $sql2 = "select count(*) as number from mashpia_charidy.donations where year = 5779 and donor_id in (
-                  select donor_id from charidy_donors_callers where year = 5779 and charidy_caller_id = " . $caller['caller_id'] . ") 
+                  select donor_id from charidy_donors_callers where year = 5779 and charidy_caller_id = " . $caller['charidy_caller_id'] . ") 
                   group by donor_id";
           $result = mysql_query( $sql2 );
           $row = mysql_fetch_assoc( $result );
           $numberGave = $row['number'];
 
-          $sql3 = "select sum(amount) as total from mashpia_charidy.donations where year != 5779 and donor_id in (
-                  select donor_id from charidy_donors_callers where year = 5779 and charidy_caller_id = " . $caller['caller_id'] . ")";
+          $totalPast = 0;
+          $sql3 = "select max(amount) as max from mashpia_charidy.donations where year != 5779 and donor_id in (
+                  select donor_id from charidy_donors_callers where year = 5779 and charidy_caller_id = " . $caller['charidy_caller_id'] . ") 
+                  group by donor_id";
           $result = mysql_query( $sql3 );
-          $row = mysql_fetch_assoc( $result );
-          $totalPast = $row['total'];
+          while ( $row = mysql_fetch_assoc( $result ) ) {
+            $totalPast += $row['max'];
+          }
 
           $sql3 = "select sum(amount) as total from mashpia_charidy.donations where year = 5779 and donor_id in (
-            select donor_id from charidy_donors_callers where year = 5779 and charidy_caller_id = " . $caller['caller_id'] . ")";
+                  select donor_id from charidy_donors_callers where year = 5779 and charidy_caller_id = " . $caller['charidy_caller_id'] . ")";
           $result = mysql_query( $sql3 );
           $row = mysql_fetch_assoc( $result );
           $totalCurrent = $row['total'];
 
           echo "<tr><td>" . $caller['first'] . ' ' . $caller['last'] . "</td><td>" . $numberPeople . "</td><td>" . $numberGave . "</td><td>" . 
-            $totalPast . "</td><td>" . $totalCurrent . "</td></tr>";
+            number_format( $totalPast, 2) . "</td><td>" . number_format( $totalCurrent, 2 ) . "</td></tr>";
+          
+          $grandTotal['people'] += $numberPeople;
+          $grandTotal['gave'] += $numberGave;
+          $grandTotal['past'] += $totalPast;
+          $grandTotal['current'] += $totalCurrent;
         }
+        echo "<tr><td></td><th>" . number_format( $grandTotal['people'] ) . "</th><th>" . number_format( $grandTotal['gave'] ) . "</th><th>" . 
+            number_format( $grandTotal['past'], 2 ) . "</th><th>" . number_format( $grandTotal['current'], 2 ) . "</th></tr>";
         ?>
       </tbody>
     </table>
