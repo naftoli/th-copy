946f2a7f95e8b50bc8b35eb0b12d4f85a20443fe
unknown <naftoli@tzivoshashem.org>
2019-06-04 12:31:35 -0400

    setup email 3 (3,4)

--- mashpia.com/public/charidy/emails/createEmail.php
+++ mashpia.com/public/charidy/emails/createEmail.php
@@ -14,16 +14,20 @@ $testEmails = [
   'chayazirkind@gmail.com'
 ];
 
-$e = new charidyEmails();
-$e->setEmailNum( 3 );
-$e->setRecipients( $testEmails );
-$e->sendEmails();
+$emailNums = [3,4];
+foreach ( $emailNums as $num ) {
+  echo "Sending Email #:" . $num . "<br />";
+  $e = new charidyEmails();
+  $e->setEmailNum( $num );
+  $e->setRecipients( $testEmails );
+  $e->sendEmails();
 
-$errors = $e->getErrors();
-if ( $errors ) {
-  foreach ( $errors as $error ) {
-    echo $error . "<br />";
+  $errors = $e->getErrors();
+  if ( $errors ) {
+    foreach ( $errors as $error ) {
+      echo $error . "<br />";
+    }
+  } else {
+    echo "done.<br />";
   }
-} else {
-  echo "done";
 }
