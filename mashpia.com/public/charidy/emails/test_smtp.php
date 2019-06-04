d305f485dd91013da0b923a55118ae47df56c567
unknown <naftoli@tzivoshashem.org>
2019-06-04 16:26:57 -0400

    test smtp

--- mashpia.com/public/charidy/emails/test_smtp.php
+++ mashpia.com/public/charidy/emails/test_smtp.php
@@ -1,2 +1,19 @@
 <?php
-require_once "../../Mail.php";
+// Create the Transport
+$transport = (new Swift_SmtpTransport('mail.mashpia.com', 25))
+  ->setUsername('_mainaccount@mashpia.com')
+  ->setPassword('Chayolei@Th5778')
+;
+
+// Create the Mailer using your created Transport
+$mailer = new Swift_Mailer($transport);
+
+// Create a message
+$message = (new Swift_Message('Wonderful Subject'))
+  ->setFrom(['john@doe.com' => 'John Doe'])
+  ->setTo(['naftoli@tzivoshashem.org'])
+  ->setBody('Here is the message itself')
+  ;
+
+// Send the message
+$result = $mailer->send($message);
