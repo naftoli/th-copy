e47a459a0a05ea8dcf703d93ea14238c4f04fe45
naftolir <naftolir@gmail.com>
2019-06-06 11:55:36 -0400

    updates

--- mashpia.com/public/charidy/emails/createEmail.php
+++ mashpia.com/public/charidy/emails/createEmail.php
@@ -46,7 +46,6 @@ $testEmails = [
         <input type="radio" name="email" value="reschedule" />Campaign Rescheduled Email<br /><br />
         <input type="radio" name="email" value="wakeup" />Wakeup Call<br /><br />
         <input type="radio" name="email" value="redo" />Emergency Redo<br /><br />
-        <input type="radio" name="email" value="roundTwo">Round Two<br /><br />
       </div>
 
       Please choose if you would like to send a test email (to a few select individuals), or a real actual email to everyone:<br /><br />
@@ -99,10 +98,6 @@ $testEmails = [
           case 'redo':
             echo "Sending Redo Emails...<br />";
             $emailNums = [17];
-            break;
-          case 'roundTwo':
-            echo "Sending Round Two Emails...<br />";
-            $emailNums = [18];
             break;
         }
 
