f9cd5d29b678d5cd512157e4589237ddfa366b30
unknown <naftoli@tzivoshashem.org>
2019-06-30 14:31:23 -0400

    cont. hachayol poll

--- mashpia.com/public/mobile/inc/modals/hachayolPoll.php
+++ mashpia.com/public/mobile/inc/modals/hachayolPoll.php
@@ -7,7 +7,21 @@
                 <h4 class="modal-title">Hachayol Poll</h4>
             </div>
             <div class="modal-body">
-                
+              <h4>Why do you open Hachayol?</h4>
+              <p>
+                Here's your chance to tell the Hachayol editors your favorite part(s) of the magazine. For each page described below, 
+                choose the answer you feel is best. You can also add the reasons for your answers in the "notes" section below.
+              </p>
+              <?php
+              $parts = ['Parshifier', 'Veholachto Bidrochov', 'Roots', 'Comics', 'Shine Back Page & Game', 'Dubbie\'s Diary', 'Yom Tov Poems'];
+              foreach ( $parts as $part ) {
+                echo "<h5>" . $part . "</h5>";
+                $name = str_replace("'", "", $part);
+                echo "<div><input type='radio' name='" . strtolower($name) . "' value='1' /> I love it<br />";
+                echo "<input type='radio' name='" . strtolower($name) . "' value='2' /> Not my Favorite<br />";
+                echo "<input type='radio' name='" . strtolower($name) . "' value='3' /> I never read it<br /></div>";
+              }
+              ?>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-danger" data-dismiss="modal" style="background-color: #5e1c77;border-color:#834999;">Cancel</button>
