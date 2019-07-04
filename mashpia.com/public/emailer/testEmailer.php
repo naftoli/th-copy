65c96380bc2fe8d6993aa7165a8f2b8564db7d8c
naftolir <naftolir@gmail.com>
2019-07-04 18:51:57 -0400

    change port

--- mashpia.com/public/emailer/testEmailer.php
+++ mashpia.com/public/emailer/testEmailer.php
@@ -2,7 +2,7 @@
 ini_set('display_errors',1);
 require_once '../../vendor/autoload.php';
 
-$transport = (new Swift_SmtpTransport('smtp.mashpia.com', 465))
+$transport = (new Swift_SmtpTransport('smtp.mashpia.com', 25))
   ->setUsername('_mainaccount@mashpia.com')
   ->setPassword('9pGftnyx;yvczPx6')
 ;
