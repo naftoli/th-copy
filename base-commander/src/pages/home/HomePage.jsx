9ad0f3ec88612094a050756c087ea09f0ea39d0f
naftoli <naftoli@tzivoshashem.org>
2019-08-04 15:44:10 -0400

    fix bug

--- base-commander/src/pages/home/HomePage.jsx
+++ base-commander/src/pages/home/HomePage.jsx
@@ -34,7 +34,7 @@ class HomePage extends Component {
 
   render() {
     const { name, img, code, active } = this.props.login;
-    const { login, home, getRegistration, current_user } = this.props;
+    const { login, home, getRegistration } = this.props;
 
     if ( isBlank( code ) ) {
       return (
