0c08abfd807f4fc4fc5088bcbc3a461ea951b448
naftoli <naftoli@tzivoshashem.org>
2019-09-06 13:47:18 -0400

    myshliach reg list

--- mashpia.com/public/myshliach_reg_report.php
+++ mashpia.com/public/myshliach_reg_report.php
@@ -4,9 +4,22 @@ require_once 'class.globalSettings.php';
 $year = GlobalSettings::getRegistrationYear();
 
 $reg = array();
-$sql = "select * from registration r 
-		join admins a using (admin_id) 
-		where year = $year";
+$sql = "
+	SELECT 
+		a.*, u.first as u_first, u.last as u_last 
+	FROM
+		registration_charges rc
+			JOIN
+		transactions t USING (trans_id)
+			JOIN
+		admins a USING (admin_id)
+			JOIN
+		users u ON rc.user_id = u.user_id
+	WHERE
+		rc.year = 5780 AND type = 'chayolei'
+			AND rc.school_id IN (61 , 269)
+	ORDER BY last , first , u_first
+";
 $result = mysql_query($sql);
 while ($row = mysql_fetch_assoc($result)) {
 	$reg[$row['school_id']][] = $row;
@@ -23,7 +36,7 @@ $view = array('no', 'yes');
 		<meta charset="UTF-8" />
 		<style>
 			table {
-                font-size: 12px;
+                font-size: 14px;
                 font-family: "sans-serif";
             }
             th, td {
@@ -71,6 +84,7 @@ $view = array('no', 'yes');
 					<th>Chidon (Mishmor)</th>
 					<? } ?>
 					-->
+					<!--
 					<? if ($school == 269) { ?>
 						<th>Shipping Option</th>
 					<? } else { ?>
@@ -78,7 +92,6 @@ $view = array('no', 'yes');
 					<? } ?>
 						<th>Shipping Dest</th>
 					<? //} ?>
-					<!--
 					<? if ($school == 61) { ?>
 						<th>Ship Supplies</th>
 					<? } ?>
@@ -86,53 +99,40 @@ $view = array('no', 'yes');
 				</tr>
 				<?
 				foreach ($info as $row) {
-					//get children info
-					$children = array();
-					$userIDs = array();
-					$sql = "select user_id, first, last from users where user_id in (" . $row['users'] . ")";
-					$result = mysql_query($sql);
-					while ($chRow = mysql_fetch_assoc($result)) {
-						$children[] = $chRow['first'] . ' ' . $chRow['last'];
-						$userIDs[] = $chRow['user_id'];
-					}
-					$numChildren = count($children);
-					
 					$phone = "H: " . $row['admin_phone_home'] . "<br />W: " . $row['admin_phone_work'] . 
 						"<br />C: " . $row['admin_phone_mobile'];
 					
-					$i = 1;
-					do {
-						echo "<tr><td>" . ($children[$i-1] ? $children[$i-1] : '') . "</td><td>" . 
-							$row['first'] . ' ' . $row['last'] . "</td><td>" . $row['admin_address1'] . 
-							(empty($row['admin_address2']) ? '' : "<br />" . $row['admin_address2']) . 
-							"</td><td>" . $row['admin_city'] . "</td><td>" . $row['admin_state'] . 
-							"</td><td>" . $row['admin_postal'] . "</td><td>" . $row['admin_country'] . 
-							"</td><td>" . $row['admin_email'] . "</td><td>" . $phone . "</td><td>";
-						if ($school == 61) {
-							$send = $row['ship_dest'] ? $row['ship_option'] == 1 ? 'yes' : 'no' : 'yes';
-							echo $send . "</td><td>" . ($row['ship_dest'] ? $row['ship_dest'] : 'USA') . "</td></tr>";
-						} else if ($school == 269) {
-							echo $row['ship_option'] . "</td><td>" . $row['ship_dest'] . "</td></tr>";
+					
+					echo "<tr><td>" . $row['u_first'] . ' ' . $row['u_last'] . "</td><td>" . 
+						$row['first'] . ' ' . $row['last'] . "</td><td>" . $row['admin_address1'] . 
+						(empty($row['admin_address2']) ? '' : "<br />" . $row['admin_address2']) . 
+						"</td><td>" . $row['admin_city'] . "</td><td>" . $row['admin_state'] . 
+						"</td><td>" . $row['admin_postal'] . "</td><td>" . $row['admin_country'] . 
+						"</td><td>" . $row['admin_email'] . "</td><td>" . $phone . "</td><td></tr>";
+					// if ($school == 61) {
+					// 	$send = $row['ship_dest'] ? $row['ship_option'] == 1 ? 'yes' : 'no' : 'yes';
+					// 	echo $send . "</td><td>" . ($row['ship_dest'] ? $row['ship_dest'] : 'USA') . "</td></tr>";
+					// } else if ($school == 269) {
+					// 	echo $row['ship_option'] . "</td><td>" . $row['ship_dest'] . "</td></tr>";
+					// }
+					//$info = explode(':', $row['approval']);
+					//$paid = $info[3];
+					/*
+					echo $view[$row['whatsapp']] . "</td><td>" . $view[$row['tutorial']] . "</td><td>";
+					if ($school == 61) {
+						echo $view[$row['chavrusaEn']] . "</td><td>" . $view[$row['chavrusaHe']] . 
+							"</td><td>" . $view[$row['library']] . "</td><td>" . $view[$row['birthday']] . 
+							"</td><td>" . $view[$row['mishmor']] . "</td><td>";
+					}
+					else if ($school == 61) {
+						if (strpos($row['extra_shipping'], $userIDs[$i-1]) !== false) {
+							echo "yes";
+						} else {
+							echo "no";
 						}
-						//$info = explode(':', $row['approval']);
-						//$paid = $info[3];
-						/*
-						echo $view[$row['whatsapp']] . "</td><td>" . $view[$row['tutorial']] . "</td><td>";
-						if ($school == 61) {
-							echo $view[$row['chavrusaEn']] . "</td><td>" . $view[$row['chavrusaHe']] . 
-								"</td><td>" . $view[$row['library']] . "</td><td>" . $view[$row['birthday']] . 
-								"</td><td>" . $view[$row['mishmor']] . "</td><td>";
-						}
-						else if ($school == 61) {
-							if (strpos($row['extra_shipping'], $userIDs[$i-1]) !== false) {
-								echo "yes";
-							} else {
-								echo "no";
-							}
-							echo "</td></tr>";
-						}
-						*/
-					} while ($i++ < $numChildren);
+						echo "</td></tr>";
+					}
+					*/
 				}
 				?>
 			</table>
