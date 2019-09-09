7a01c8fb5b942bf589ab29a458998ffeb10bc8c8
Naftoli Rapoport <naftoli@tzivoshashem.org>
2019-09-09 13:51:03 -0400

    find bug

--- base-commander/src/pages/base-managment/soldiers/SoldierPage/SoldierPage.jsx
+++ base-commander/src/pages/base-managment/soldiers/SoldierPage/SoldierPage.jsx
@@ -129,8 +129,7 @@ class SoldierPage extends Component {
     if ( isInvalid )
       return toast.error( 'Please correct all invalid fields' );
 
-    console.log(this.state)
-    if ( !this.state.soldier.class_id && !this.state.updates.class_id ) {
+    if ( !this.state.updates.class_id ) {
       return toast.error('You must choose a platoon.')
     }
 
