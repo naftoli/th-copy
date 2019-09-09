3dccedfbec5c57865b77de04fc36d32d15cf8f24
Anthony <tonyleeresume@gmail.com>
2019-09-09 14:08:43 -0400

    Fixed mission dates

--- mashpia.com/public/api/missions/parshos.php
+++ mashpia.com/public/api/missions/parshos.php
@@ -4,9 +4,14 @@ include_once( __DIR__ . "/../header/header.php" );
 class ParshaRouter {
 
     public function index() {
+
+        $p1 = Parsha::find_by_sql('SELECT * FROM parshos WHERE year = 5779 ORDER BY id DESC LIMIT 2');
+
         $parshos = Parsha::all([
-            'conditions' => 'year = ' . GlobalSettings::getCurrentYear(),
+            'conditions' => 'year = ' . GlobalSettings::getCurrentYear() + 1,
         ]);
+
+        $parshos = $p1 + $parshos;
 
         json_response( $parshos, true, true );
     }
