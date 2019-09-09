a91a8f702eadf8212f36f5b4dd932f4ff5add2ab
Anthony <tonyleeresume@gmail.com>
2019-09-09 14:19:51 -0400

    Fixed syntax

--- mashpia.com/public/api/missions/parshos.php
+++ mashpia.com/public/api/missions/parshos.php
@@ -8,7 +8,7 @@ class ParshaRouter {
         $p1 = Parsha::find_by_sql('SELECT * FROM parshos WHERE year = 5779 ORDER BY id DESC LIMIT 2');
 
         $parshos = Parsha::all([
-            'conditions' => 'year = ' . GlobalSettings::getCurrentYear() + 1,
+            'conditions' => 'year = ' . [GlobalSettings::getCurrentYear() + 1],
         ]);
 
         $parshos = $p1 + $parshos;
