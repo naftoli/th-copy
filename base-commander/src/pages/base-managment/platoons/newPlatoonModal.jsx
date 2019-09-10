4c2d754eadca8a59ec398c696f1d1defcbe71de6
Anthony <tonyleeresume@gmail.com>
2019-09-10 11:41:25 -0400

    Added endpoints

--- base-commander/src/pages/base-managment/platoons/newPlatoonModal.jsx
+++ base-commander/src/pages/base-managment/platoons/newPlatoonModal.jsx
@@ -19,7 +19,8 @@ const initialState = {
   platoon: {
     class_grade: '', class_sub: '', class_teacher: '',
     email: '', cell: '', miles_balance: 1000,
-    miles_per_soldier: 100,
+    miles_per_soldier: 100, pic_mission_type: 0, allow_parent_tasks: 0,
+    print_parent_tasks: 0, class_gender: null, whatsapp: 0,
   },
   saving: false,
 }
