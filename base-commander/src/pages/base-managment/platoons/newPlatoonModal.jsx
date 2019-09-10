80faaf4f49714018ca8b0bd97bc30caab1b0b160
Anthony <tonyleeresume@gmail.com>
2019-09-10 14:21:59 -0400

    Fixed column layout

--- base-commander/src/pages/base-managment/platoons/newPlatoonModal.jsx
+++ base-commander/src/pages/base-managment/platoons/newPlatoonModal.jsx
@@ -8,11 +8,12 @@ import {
 } from 'reactstrap';
 // rows
 import { PlatoonRow } from './tabs/PlatoonRow';
-// import { SettingsRow } from './tabs/SettingsRow';
+import { SettingsRow } from './tabs/SettingsRow';
 // functions
 import { toast } from 'react-toastify';
 import { isAdmin } from 'functions/login';
 import { onInputChange, onSelectChange, onCheckboxChange, onJSONChange } from 'functions/events';
+
 
 const initialState = {
   platoon: {
@@ -96,77 +97,11 @@ class NewPlatoonModal extends Component {
 
           <p className='title'>Platoon Settings</p>
 
-          <Row>
-            <Col sm={6} xl={3}>
-              <Label>Show on WWTC Reports</Label>
-              <Toggle
-                name='whatsapp'
-                { ...checkProps }
-                checked={ !!whatsapp } />
-            </Col>
-            
-            <Col sm={6} xl={3}>
-              <Label>Class Gender</Label>
-              <Radio
-                required
-                value='m'
-                { ...inputProps }
-                name='class_gender'
-                checked={ class_gender === 'm' }>
-
-                Boys <FontAwesome icon='male' />
-              </Radio>
-
-              <Radio
-                value='f'
-                { ...inputProps }
-                name='class_gender'
-                checked={ class_gender === 'f' }>
-
-                Girls <FontAwesome icon='female' />
-              </Radio>
-            </Col>
-
-            <Col sm={6} xl={3}>
-              <Label id='customize'>Custom Parent Tasks</Label>
-              <UncontrolledTooltip placement="top" target="customize" autohide={ false }>
-                Allow parents to create completely custom tasks for this soldier.
-                Custom tasks are worth 0.5 miles per day/week
-              </UncontrolledTooltip>
-
-              <Checkbox
-                { ...checkProps }
-                name='allow_parent_tasks'
-                checked={!!allow_parent_tasks }>
-
-                Allow
-              </Checkbox>
-
-              <Checkbox { ...checkProps }
-                name='print_parent_tasks'
-                checked={!!print_parent_tasks }>
-
-                Print on Mission Sheets
-              </Checkbox>
-            </Col>
-
-            <Col sm={6} xl={3}>
-              <Label>Mission Sheet Type</Label>
-              <Radio value='1'
-                  name='pic_mission_type'
-                  onChange={ onJSONChange }
-                  checked={ pic_mission_type === 1 } >
-                No Pictures
-              </Radio>
-
-              <Radio value='2'
-                  name='pic_mission_type'
-                  onChange={ onJSONChange }
-                  checked={ pic_mission_type === 2 }>
-                Small Pictures
-              </Radio>
-            </Col>
-          </Row>       
+          <SettingsRow
+            platoon={ platoon } 
+            inputProps={ inputProps }
+            checkProps={ checkProps }
+            onJSONChange={ this.onJSONChange } />            
 
           </ModalBody>
 
