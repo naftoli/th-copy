cc4c0b62676c5b248cca95a8fd89b05cfccc0004
Anthony <tonyleeresume@gmail.com>
2019-09-10 13:34:02 -0400

    Remove additional platoon row

--- base-commander/src/pages/base-managment/platoons/tabs/PlatoonTab.jsx
+++ base-commander/src/pages/base-managment/platoons/tabs/PlatoonTab.jsx
@@ -44,14 +44,6 @@ export class PlatoonTab extends Component {
             inputProps={ inputProps }
             onSelectChange={ this.onSelectChange } />
 
-          <p className='title'>Platoon Settings</p>
-
-          <SettingsRow
-            platoon={ platoon } 
-            inputProps={ inputProps }
-            checkProps={ checkProps }
-            onJSONChange={ this.onJSONChange } />
-
           <SaveButton show={ updated } />
 
         </form>
