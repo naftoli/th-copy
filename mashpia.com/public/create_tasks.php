87482f50db29ac61ddf8e645fc68d1f2f9c7199b
naftoli <naftoli@tzivoshashem.org>
2019-09-11 14:12:21 -0400

    fix tasks for first 2 weeks of year

--- mashpia.com/public/create_tasks.php
+++ mashpia.com/public/create_tasks.php
@@ -147,8 +147,11 @@ if (isset($_POST['submit'])) {
     // $defaultDates = GlobalSettings::getCurYearDates();
 	// $defaultStart = $defaultDates['start'];
     // $defaultEnd = $defaultDates['end'];
-    $defaultStart = 2458754; 
-    $defaultEnd = 2459103;
+    // $defaultStart = 2458754; 
+    // $defaultEnd = 2459103;
+
+    $defaultStart = 2458740; 
+    $defaultEnd = 2458753;
 
     $weeks = array();
     $sql2 = 'select * from parshos where year in(' . ($missionYear-1) . ',' . $missionYear . ')';
