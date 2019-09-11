313aef98a75c5ef153c280d7760775c2df087293
naftoli <naftoli@tzivoshashem.org>
2019-09-11 13:41:46 -0400

    fix missing parent id in chidon table

---
+++ mashpia.com/public/reports/chidon/fix/fix_missing_parent_id.php
@@ -0,0 +1,17 @@
+<?php
+require $_SERVER['DOCUMENT_ROOT'] . '/db.php';
+
+$users = [];
+$sql = "select * from th_chidon where parent_id = 0 and year = 5780";
+$result = mysql_query( $sql );
+while ( $row = mysql_fetcdh_assoc( $result ) ) {
+    $users[] = $row['user_id'];
+}
+
+foreach ( $users as $id ) {
+    $sql = "select admin_id from admin_auths where id = " . $id;
+    $result = mysql_query( $sql );
+    $row = mysql_fetch_assoc( $result );
+    $sql2 = "update th_chidon set parent_id = " . $row['admin_id'] . " where user_id = " . $id . " and year = 5780";
+    echo $sql2 . "<br />";
+}
