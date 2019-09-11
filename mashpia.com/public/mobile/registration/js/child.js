b99bc27696e80cbade7c063a38f43cbd0e69e4c5
Anthony <tonyleeresume@gmail.com>
2019-09-11 16:06:04 -0400

    Debugging paren-child connection

--- mashpia.com/public/mobile/registration/js/child.js
+++ mashpia.com/public/mobile/registration/js/child.js
@@ -135,7 +135,7 @@ var childApp = function(){
         //     return showError( "Please upload a profile picture for your child." );
         // }
 
-        $.post("/api/core/users?action=creation", postData, function( response ){
+        $.post("/api/core/users?action=create", postData, function( response ){
 
             if( response.success ){
                 $( "#tuition-paid" ).hide();
