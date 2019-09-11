0cdc136750fc672675361ec355a067fa02712587
Anthony <tonyleeresume@gmail.com>
2019-09-11 13:20:53 -0400

    Debugging paren-child connection

--- mashpia.com/public/api/core/users.php
+++ mashpia.com/public/api/core/users.php
@@ -119,8 +119,7 @@ class UsersRouter {
         }
 
         // send the full soldier to the client
-        //json_response( $user );
-        echo $current_user->login['code'];
+        json_response( $user );
     }
 
     public function update( $id ) {
