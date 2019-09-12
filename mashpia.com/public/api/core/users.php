93a9c0baa2190b834559f30b1f81034830020d61
Anthony <tonyleeresume@gmail.com>
2019-09-12 15:01:08 -0400

    Rollback

--- mashpia.com/public/api/core/users.php
+++ mashpia.com/public/api/core/users.php
@@ -112,12 +112,12 @@ class UsersRouter {
         if ( !$user->is_valid() || !$user->save() )
             json_error( 'Could not create Soldier. (CODE: CORE-USERS-98)' );
         // parents get auto connected to their kids
-       if ( $current_user->login->code === 'PARENT' ) {
-            $auth = \AdminAuth::create([
+       //if ( $current_user->type === 'PARENT' ) {
+            /*$auth = \AdminAuth::create([
                 'admin_id' => $current_user->admin_id,
                 'id'       => $user->user_id,     
                 'auth'     => 'user'
-            ]);
+            ]);*/
 
             /*$userVal = 'user';
             $roldIdVal = 1;
@@ -133,10 +133,11 @@ class UsersRouter {
             $stmt = $MASHPIA_DB->prepare( $sql );
             $stmt->execute($data);*/
 
-        }
+       // }
 
+       var_dump($current_user->type);
         // send the full soldier to the client
-        json_response( $user );
+        //json_response( $user );
     }
 
     public function update( $id ) {
