096b05b13851ace6502291a16df1bcc62ed4b7d2
Anthony <tonyleeresume@gmail.com>
2019-09-12 15:07:33 -0400

    Debugging parent-child connection

--- mashpia.com/public/api/core/users.php
+++ mashpia.com/public/api/core/users.php
@@ -135,7 +135,7 @@ class UsersRouter {
 
        // }
 
-       var_dump($current_user->type); 
+       var_dump($current_user->login->type); 
         // send the full soldier to the client
         //json_response( $user );
     }
