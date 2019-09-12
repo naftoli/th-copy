dcbbb37673099a6523a57576d6e5b996296cec78
Anthony <tonyleeresume@gmail.com>
2019-09-12 13:21:46 -0400

    Debugging paren-child connection

--- mashpia.com/public/api/core/users.php
+++ mashpia.com/public/api/core/users.php
@@ -86,6 +86,8 @@ class UsersRouter {
         global $MASHPIA_DB;
         $user = Soldier::build( $_POST );
 
+        $admin_user = Admin::find([ $current_user->admin_id ]);
+
         // make sure soldier with this first and last name and date of birth doesn't exist in this school
         $school_id = $_POST['school_id'];
         $first = $_POST['first'];
@@ -134,7 +136,7 @@ class UsersRouter {
         //}
 
         //var_dump($current_user);
-        echo $current_user->admin_id;
+        echo $admin_user;
         // send the full soldier to the client
         //json_response( $user );
     }
