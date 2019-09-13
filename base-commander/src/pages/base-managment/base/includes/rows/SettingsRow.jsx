252e14eb2aab49c96d4dc64496bfeb7ae39ed68d
naftoli <naftoli@tzivoshashem.org>
2019-09-13 13:13:43 -0400

    undo default checking

--- base-commander/src/pages/base-managment/base/includes/rows/SettingsRow.jsx
+++ base-commander/src/pages/base-managment/base/includes/rows/SettingsRow.jsx
@@ -102,15 +102,15 @@ export class SettingsRow extends Component {
           <p className='title'>Store Miles Settings</p>
           <Label>Store Miles Start From:</Label>
 
-          <Radio checked={ store_reset_jd === 245866 } value='2458663' name='store_miles_reset' onChange={ this.enableSchoolReset } required>
+          <Radio value='2458663' name='store_miles_reset' onChange={ this.enableSchoolReset } required>
             Friday, 25 Sivan (June 28) (Chayolim can use the points they earned from summer missions and on)
           </Radio>
 
-          <Radio checked={ store_reset_jd === 2458733 } value='2458733' name='store_miles_reset' onChange={ this.enableSchoolReset }>
+          <Radio value='2458733' name='store_miles_reset' onChange={ this.enableSchoolReset }>
             Friday, 6 Elul (Sep 6) (Chayolim will not be able to use the points they earned from the majority of summer missions)
           </Radio>
 
-          <Radio checked={ store_reset_jd === 0 } name='store_miles_reset' id='store_reset' onChange={ this.disableSchoolReset }>
+          <Radio name='store_miles_reset' id='store_reset' onChange={ this.disableSchoolReset }>
             Never (This includes all miles from previous years)
           </Radio>
           <br />
