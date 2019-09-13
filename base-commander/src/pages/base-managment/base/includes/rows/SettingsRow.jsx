bcf4906ae807d12d1a5f9640b27d3bce1f01ee59
Anthony <tonyleeresume@gmail.com>
2019-09-13 13:44:14 -0400

    Fix school settings for bases

--- base-commander/src/pages/base-managment/base/includes/rows/SettingsRow.jsx
+++ base-commander/src/pages/base-managment/base/includes/rows/SettingsRow.jsx
@@ -49,7 +49,7 @@ export class SettingsRow extends Component {
     const checkboxProps = { onChange: this.handleCheckbox };
     const schoolGenderProps = { name: 'school_gender', onChange: this.onChange }
     const missionTypeProps = { name: 'pic_mission_type', onChange: this.onNumberChange }
-    const storeMilesRestProps = { name: 'store_miles_reset', onChange: this.onNumberChange }
+    const storeMilesRestProps = { name: 'store_reset', onChange: this.onNumberChange }
     
     return (
       <Row id='SettingsRow'>
@@ -103,21 +103,21 @@ export class SettingsRow extends Component {
           <p className='title'>Store Miles Settings</p>
           <Label>Store Miles Start From:</Label>
 
-          <Radio value='2458663' name='store_miles_reset' 
+          <Radio value='2458663' name='store_reset' 
           { ...storeMilesRestProps }
           checked={ storeMilesRestProps === '2458663' }
           onChange={ this.enableSchoolReset } required>
             Friday, 25 Sivan (June 28) (Chayolim can use the points they earned from summer missions and on)
           </Radio>
 
-          <Radio value='2458733' name='store_miles_reset' 
+          <Radio value='2458733' name='store_reset' 
           { ...storeMilesRestProps }
           checked={ storeMilesRestProps === '2458733' }
           onChange={ this.enableSchoolReset }>
             Friday, 6 Elul (Sep 6) (Chayolim will not be able to use the points they earned from the majority of summer missions)
           </Radio>
 
-          <Radio name='store_miles_reset' id='store_reset'  
+          <Radio name='store_reset' id='store_reset'  
           { ...storeMilesRestProps }
           checked={ storeMilesRestProps === '0' }
           onChange={ this.disableSchoolReset }>
@@ -125,7 +125,7 @@ export class SettingsRow extends Component {
           </Radio>
           <br />
 
-          <Radio name='store_miles_reset' 
+          <Radio name='store_reset' 
           { ...storeMilesRestProps }
           checked={ storeMilesRestProps === toJulian( moment() ) }
           onChange={ this.enableSchoolReset } value={ toJulian( moment() ) }>
