5d6878ad6592ce2baf7df4776fb9aca3e2cd4895
Naftoli Rapoport <naftoli@tzivoshashem.org>
2019-09-17 15:46:50 -0400

    update chidon to allow 8th graders and albany school

--- mashpia.com/public/mobile/reg/ajax/getChildren.php
+++ mashpia.com/public/mobile/reg/ajax/getChildren.php
@@ -144,10 +144,10 @@ if ( !empty( $users ) ) {
 		// }
 		
 		// chidon registration
-		$exceptions = [483,482,544,584,583,588,430,577,13,220];
+		$exceptions = [483,482,544,584,583,588,430,577,13];
 		if ( !$row['reg_chidon'] // if not in chidon
 			&& intval( $row['class_grade'] ) > 3 // and in grade 4+ 
-			&& intval( $row['class_grade'] ) < 8 // not in grade 8 
+			&& intval( $row['class_grade'] ) <= 8 // not in grade 8 
 			&& $row['chidon'] // make sure the kid is in chidon
 			&& !in_array( intval( $children[$row['user_id']]['school_id'] ), $exceptions ) // make sure not one of these schools
 			//&& in_array( $row['school_id'], $australia ) // and not in australia..
