001857b4a8035f07f508dd797ade51157e1fbc81
naftoli <naftoli@tzivoshashem.org>
2019-09-24 16:20:15 -0400

    now acutally remove duplicates

--- mashpia.com/public/fix_reg_duplicates.php
+++ mashpia.com/public/fix_reg_duplicates.php
@@ -15,29 +15,30 @@ while ( $row = mysql_fetch_assoc( $result ) ) {
     $info[$row['user_id']][] = $row;
 }
 
-foreach ( $info as $user => $rows ) {
-    if ( count( $rows ) > 1 ) {
-        foreach ( $rows as $idx => $row ) {
-            if ( $idx == 0 ) continue;
-            try {
-                $date = new DateTime( $row['date'] );
-            } catch (Exception $e) {
-                echo $e->getMessage();
-                exit(1);
-            }
-            $date->add( new DateInterval("P1D") );
-            $sql = "update registration_charges set date = '" . $date->format('Y-m-d H:i:s') . "' where registration_charge_id = " . $row['registration_charge_id'];
-            mysql_query( $sql ) or die( mysql_error() . "<br />" . $sql );
-        }
-    }
-}
-echo "done.";
-
 // foreach ( $info as $user => $rows ) {
 //     if ( count( $rows ) > 1 ) {
-//         $sql = "delete from registration_charges where user_id = " . $user . " and year = 5780 and type = 'chayolei' and date != '" . $rows[0]['date'] . "'";
-//         //echo $sql . "<br />";
-//         mysql_query( $sql );
+//         foreach ( $rows as $idx => $row ) {
+//             if ( $idx == 0 ) continue;
+//             try {
+//                 $date = new DateTime( $row['date'] );
+//             } catch (Exception $e) {
+//                 echo $e->getMessage();
+//                 exit(1);
+//             }
+//             $date->add( new DateInterval("P1D") );
+//             $sql = "update registration_charges set date = '" . $date->format('Y-m-d H:i:s') . "' where registration_charge_id = " . $row['registration_charge_id'];
+//             echo $sql . "<br />"; continue;
+//             mysql_query( $sql ) or die( mysql_error() . "<br />" . $sql );
+//         }
 //     }
 // }
 // echo "done.";
+
+foreach ( $info as $user => $rows ) {
+    if ( count( $rows ) > 1 ) {
+        $sql = "delete from registration_charges where user_id = " . $user . " and year = 5780 and type = 'chayolei' and date != '" . $rows[0]['date'] . "'";
+        //echo $sql . "<br />";
+        mysql_query( $sql );
+    }
+}
+echo "done.";
