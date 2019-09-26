936a159f5bf1fdedb4fb7c86aa9eaa245f455023
Tony <tonyleeresume@gmail.com>
2019-09-26 13:10:41 -0400

    Debugging Raffle Form

--- mashpia.com/public/raffles/shared/forms/raffle_form.php
+++ mashpia.com/public/raffles/shared/forms/raffle_form.php
@@ -56,7 +56,7 @@ if($action == "create" || $action == "update"){ // for both create and update, r
         
         $runYear  = $format_run_date[0];
         $runMonth = $format_run_date[1];
-        $runDay   = $format_run_date[2];
+        $runDay   = $format_run_date[2]; 
         
         $run_date_jd = gregoriantojd($runMonth, $runDay, $runYear);
 
