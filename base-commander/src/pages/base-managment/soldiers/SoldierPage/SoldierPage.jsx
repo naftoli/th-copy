4aabe7f6fbc4527733fd550aa5daa644969bdcea
Tony <tonyleeresume@gmail.com>
2019-10-04 14:06:06 -0400

    Fixing Soldier Platoon Bug

--- base-commander/src/pages/base-managment/soldiers/SoldierPage/SoldierPage.jsx
+++ base-commander/src/pages/base-managment/soldiers/SoldierPage/SoldierPage.jsx
@@ -129,9 +129,15 @@ class SoldierPage extends Component {
     if ( isInvalid )
       return toast.error( 'Please correct all invalid fields' );
 
-    if ( !this.state.updates.class_id ) {
-      return toast.error('You must choose a platoon.')
+    if ( !this.state.soldier.class_id ) {
+      console.log(this.state.soldier);
+      return toast.error('You must choose a platoon.');
+    } else if (this.state.updates.school_id !== undefined && this.state.updates.class_id === undefined) {
+      console.log(this.state.updates);
+      return toast.error('You must choose a platoon.');
     }
+
+
 
     // update the soldier
     this.setState({ saving: true });
