ec07d77ee6f970b3b0777a37e334f59eb16261fd
Naftoli Rapoport <naftoli@tzivoshashem.org>
2019-10-30 16:45:34 -0400

    add shipping name to hachayol report

--- mashpia.com/public/hachayol_report.php
+++ mashpia.com/public/hachayol_report.php
@@ -83,6 +83,7 @@ $posters = array(
             <?php $chidonNum = $h->getChidonNumber( $id ); ?>
             <h2>For Pickup</h2>
             <div class='info'>
+                <?=$school['shipping_name']?><br />
                 <?=$school['name']?><br />
                 <?=$school['address']?><br /><br />
                 Type of School: <?=$school['type']?><br />
