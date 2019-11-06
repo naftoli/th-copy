61869ca65ff81b0e987122ebcdf6987fd2828045
Naftoli Rapoport <naftoli@tzivoshashem.org>
2019-11-06 13:02:40 -0500

    update rank card printing to exclude schools and print each card on new page for temp also

--- mashpia.com/public/api/core/id_cards.php
+++ mashpia.com/public/api/core/id_cards.php
@@ -29,6 +29,8 @@ class IdCardsRouter {
         // combine the filters
         $filters[] = 'u.user_registered IS NOT NULL';
         $filters[] = 'u.chayolei = 1 AND s.chayolei = 1';
+        // add schools exceptions
+        $filters[] = 'u.school_id not in (180,588,612)';
         $filters = implode( ' AND ', $filters );
 
         $rank_marks = "(SELECT MAX(rank_ord) max_rank, user_id FROM rank_marks GROUP BY user_id) cr USING (user_id) "
