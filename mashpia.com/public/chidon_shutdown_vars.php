f7b1e0401c610ae31f7ea8ff97727ab22878eead
Naftoli Rapoport <naftoli@tzivoshashem.org>
2019-11-21 09:53:37 -0500

    allow first chidon test marking

--- mashpia.com/public/chidon_shutdown_vars.php
+++ mashpia.com/public/chidon_shutdown_vars.php
@@ -5,14 +5,14 @@ $shutdown2 = false;
 $shutdown3 = false;
 $today = date("Y-m-d H:i:s"); // date internal is 4 or 5 hrs ahead
 //echo $today . "<br />";
+
 /***************************************************
     After the deadline, only HQ can enter in marks.
     
  ***************************************************/
-
 $date1 = "2019-11-26 05:00:00";
-$date2 = "2019-11-26 05:00:00";
-$date3 = "2019-11-26 05:00:00";
+$date2 = "2020-01-26 05:00:00";
+$date3 = "2020-03-26 05:00:00";
 
 if ($today > $date1) {
     $shutdown1 = true;
@@ -24,8 +24,7 @@ if ($today > $date3) {
     $shutdown3 = true;
 }
 
-// close all marking until further notice
-$shutdown1 = true;
+// close other marking until further notice
 $shutdown2 = true;
 $shutdown3 = true;
 
