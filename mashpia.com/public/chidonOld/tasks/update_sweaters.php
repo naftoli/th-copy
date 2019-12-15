ddcb60401ef282c258ba9877304b155d0b069d26
Naftoli Rapoport <naftoli@tzivoshashem.org>
2019-12-15 15:46:02 -0500

    update sweaters file

---
+++ mashpia.com/public/chidonOld/tasks/update_sweaters.php
@@ -0,0 +1,28 @@
+<?php
+$admin_auth = ['school'];
+require_once $_SERVER['DOCUMENT_ROOT'] . '/header.php';
+if ( $admin_user['auth'] != 'super' ) {
+    echo "Invalid Permissions.";
+    exit;
+}
+require_once $_SERVER['DOCUMENT_ROOT'] . '/PHPExcel/IOFactory.php';
+
+//load spreadsheet
+$objPHPExcel = PHPExcel_IOFactory::load( "sweater_sizes.xlsx" );
+$objWorksheet = $objPHPExcel->getActiveSheet();
+
+$info = [];
+foreach ( $objWorksheet->getRowIterator() as $row ) {
+    $cellIterator = $row->getCellIterator();
+    $cellIterator->setIterateOnlyExistingCells(false);
+    $i = 0;
+    foreach ( $cellIterator as $cell ) {
+        $value = trim( $cell->getValue() );
+        $info[$i++] = $value;
+    }
+}
+
+foreach ( $info as $user => $size ) {
+    $sql = "update th_chidon set sweater_size = '" . strtolower( $size ) . "' where th_chidon_id = " . $user;
+    echo $sql . "<br />";
+}
