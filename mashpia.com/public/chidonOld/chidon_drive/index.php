10c6b920717f3ce2723c9b3a812385157a6adc91
Naftoli Rapoport <naftoli@tzivoshashem.org>
2020-01-01 15:44:50 -0500

    fix bug

--- mashpia.com/public/chidonOld/chidon_drive/index.php
+++ mashpia.com/public/chidonOld/chidon_drive/index.php
@@ -4,7 +4,7 @@ $url = $_SERVER['REQUEST_URI'];
 $host = isset( $_SERVER['SERVER_NAME'] ) ? $_SERVER['SERVER_NAME'] : 'tzivos.local';
 $pos = strrpos( $url, '/' );
 $url_end = substr( $url, $pos + 1 );
-if ( is_number( $url_end ) ) {
+if ( is_numeric( $url_end ) ) {
     header("Location: https://" . $host . "/site/family-single.html?id=" . $url_end);
     exit;
 } else {
