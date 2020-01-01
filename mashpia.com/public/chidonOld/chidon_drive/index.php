59f17e58268d80a4f9ffd25447afaf7460deac71
Naftoli Rapoport <naftoli@tzivoshashem.org>
2020-01-01 15:18:27 -0500

    update routing rules

--- mashpia.com/public/chidonOld/chidon_drive/index.php
+++ mashpia.com/public/chidonOld/chidon_drive/index.php
@@ -3,7 +3,19 @@
 $url = $_SERVER['REQUEST_URI'];
 $host = isset( $_SERVER['SERVER_NAME'] ) ? $_SERVER['SERVER_NAME'] : 'tzivos.local';
 $pos = strrpos( $url, '/' );
-$url_end = substr( $url, $pos + 1 );
-if ( $url_end ) header("Location: https://" . $host . "/site/family-single.html?id=" . $url_end);
-else header("Location: https://" . $host . "/site");
+if ( $pos !== false ) {
+    $url_end = substr( $url, $pos + 1 );
+    header("Location: https://" . $host . "/site/family-single.html?id=" . $url_end);
+    exit;
+} else {
+    switch ( $url ) {
+        case '/site/intro.html':
+        case '/site/setup.html':
+            header("Location: https://" . $host . $url );
+            break;
+        default:
+            header("Location: https://" . $host . "/site");
+            break;
+    }
+}
 exit;
