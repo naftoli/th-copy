046a005ff060f4752853111d3331f5292798d84c
Naftoli Rapoport <naftoli@tzivoshashem.org>
2020-01-01 15:40:21 -0500

    fix reouting issue

--- mashpia.com/public/chidonOld/chidon_drive/index.php
+++ mashpia.com/public/chidonOld/chidon_drive/index.php
@@ -3,15 +3,15 @@
 $url = $_SERVER['REQUEST_URI'];
 $host = isset( $_SERVER['SERVER_NAME'] ) ? $_SERVER['SERVER_NAME'] : 'tzivos.local';
 $pos = strrpos( $url, '/' );
-if ( $pos !== false ) {
-    $url_end = substr( $url, $pos + 1 );
+$url_end = substr( $url, $pos + 1 );
+if ( is_number( $url_end ) ) {
     header("Location: https://" . $host . "/site/family-single.html?id=" . $url_end);
     exit;
 } else {
     switch ( $url ) {
         case '/site/intro.html':
         case '/site/setup.html':
-            header("Location: https://" . $host . $url );
+            header("Location: https://" . $host . $url);
             break;
         default:
             header("Location: https://" . $host . "/site");
