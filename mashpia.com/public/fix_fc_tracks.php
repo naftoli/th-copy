d92ac1d5287d5c0f378ee2b53e82d72e2e3d56d7
Naftoli Rapoport <naftoli@tzivoshashem.org>
2020-01-02 21:26:01 -0500

    hide the FC hey teves mission from showing up

---
+++ mashpia.com/public/fix_fc_tracks.php
@@ -0,0 +1,39 @@
+<?php
+require 'db.php';
+
+$grades = [];
+$sql = "select * from classes where school_id = 622";
+$result = mysql_query( $sql );
+while ( $row = mysql_fetch_assoc( $result ) ) {
+    $grades[$row['class_id']] = $row['class_grade'];
+}
+
+$users = [];
+$sql = "select user_id, class_id from users where school_id = 622";
+$result = mysql_query( $sql );
+while ( $row = mysql_fetch_assoc( $result ) ) {
+    $users[$row['user_id']] = $row['class_id'];
+}
+
+$subjects = [ 1, 4, 12, 13, 15, 16, 21, 27, 40, 41, 42, 45, 90, 100 ];
+$levels = [
+    'Pre1a' =>  6, 
+    '1'     =>  7, 
+    '2'     =>  8, 
+    '3'     =>  9, 
+    '4'     =>  10, 
+    '5'     =>  11, 
+    '6'     =>  12, 
+    '7'     =>  13, 
+    '8'     =>  14
+];
+foreach ( $users as $user => $class ) {
+    foreach ( $subjects as $subject ) {
+        $sql = "update user_tracks set 
+                level = " . $levels[$grades[$class]] . "  
+                where user_id = " . $user . " 
+                and subject_id = " . $subject;
+        mysql_query( $sql ) or die( mysql_error() . "<br />" . $sql . "<br />" );       
+    }
+}
+echo "done";
