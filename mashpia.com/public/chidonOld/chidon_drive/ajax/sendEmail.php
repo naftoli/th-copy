1fcb299c8ab112f0cad504a8a7e6065f0fff3caa
Naftoli Rapoport <naftoli@tzivoshashem.org>
2020-01-23 18:59:23 -0500

    add header to email sent after donation on chidondrive

--- mashpia.com/public/chidonOld/chidon_drive/ajax/sendEmail.php
+++ mashpia.com/public/chidonOld/chidon_drive/ajax/sendEmail.php
@@ -1,4 +1,7 @@
 <?php
+$email = "naftoli@tzivoshashem.org";
+$trans_id = 111111111;
+$amount = 50.5;
 // send email confirmation
 $headers[] = 'MIME-Version: 1.0';
 $headers[] = 'Content-type: text/html; charset=iso-8859-1';
@@ -6,7 +9,8 @@ $headers[] = 'From: chidon@tzivoshashem.com';
 $headers[] = 'Reply-To: chidon@tzivoshashem.com';
 
 $subject = "Chidon Drive Donation";
-$message = "<p>Thank you for your generous donation of $" . number_format( $amount, 2 ) . ".</p><p>Your support enables us to show our children how meaningful their learning is, and to drive them to go mechayil el choyil.</p>";
+$message = "<img src='../site/img/Chidon-Drive-5780-email-header.jpg' style='max-width: auto' />";
+$message .= "<p>Thank you for your generous donation of $" . number_format( $amount, 2 ) . ".</p><p>Your support enables us to show our children how meaningful their learning is, and to drive them to go mechayil el choyil.</p>";
 $message .= "<p>Your transaction id is: " . $trans_id . "</p>";
 @mail( $email, $subject, $message, implode("\r\n", $headers) );
 ?>
