62bf5becf5610c7d171f2700f3d3851e0a347f67
Naftoli Rapoport <naftoli@tzivoshashem.org>
2020-01-23 18:29:24 -0500

    updates to chidondrive and rerouting ules

--- mashpia.com/public/chidonOld/chidon_drive/index.php
+++ mashpia.com/public/chidonOld/chidon_drive/index.php
@@ -2,7 +2,6 @@
 // routing of family names
 $url = $_SERVER['REQUEST_URI'];
 $host = isset( $_SERVER['SERVER_NAME'] ) ? $_SERVER['SERVER_NAME'] : 'tzivos.local';
-echo $host; exit;
 $pos = strrpos( $url, '/' );
 $url_end = substr( $url, $pos + 1 );
 if ( is_numeric( $url_end ) ) {
