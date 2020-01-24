5aff640924f3338fd256196055c49842777cfebd
Naftoli Rapoport <naftoli@tzivoshashem.org>
2020-01-23 19:26:13 -0500

    more debugging

--- mashpia.com/public/chidonOld/chidon_drive/ajax/sendEmail.php
+++ mashpia.com/public/chidonOld/chidon_drive/ajax/sendEmail.php
@@ -8,10 +8,9 @@ $headers[] = 'Content-type: text/html; charset=iso-8859-1';
 $headers[] = 'From: chidon@tzivoshashem.com';
 $headers[] = 'Reply-To: chidon@tzivoshashem.com';
 
-$img = '../site/img/Chidon-Drive-5780-email-header.jpg';
-echo $img; exit;
+$img = "../site/img/Chidon-Drive-5780-email-header.jpg";
 $subject = "Chidon Drive Donation";
-$message = "<img src='$img' style='max-width: auto' />";
+$message = "<img src='$img' style='max-width: auto;' />";
 $message .= "<p>Thank you for your generous donation of $" . number_format( $amount, 2 ) . ".</p><p>Your support enables us to show our children how meaningful their learning is, and to drive them to go mechayil el choyil.</p>";
 $message .= "<p>Your transaction id is: " . $trans_id . "</p>";
 @mail( $email, $subject, $message, implode("\r\n", $headers) );
