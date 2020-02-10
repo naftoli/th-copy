1c913b50d1a6254ac74a2fdcb4594adc78c1bcc4
Naftoli Rapoport <naftoli@tzivoshashem.org>
2020-02-10 14:13:02 -0500

    fix checking authorize customer and payment profiles

--- mashpia.com/public/ajax/chidon/getSchoolInfo.php
+++ mashpia.com/public/ajax/chidon/getSchoolInfo.php
@@ -1,11 +1,36 @@
 <?php
+ini_set('display_errors',1);
 $admin_auth = array('school'); 
 require_once($_SERVER["DOCUMENT_ROOT"].'/header.php');
+require_once($_SERVER["DOCUMENT_ROOT"].'/classes/authorize/CustomerProfile.php');
+require_once($_SERVER["DOCUMENT_ROOT"].'/classes/authorize/PaymentProfile.php');
+use classes\authorize\CustomerProfile;
+use classes\authorize\PaymentProfile;
 
 $school_id = mysql_real_escape_string( $_POST['school'] );
 $sql = "select * from schools where school_id = " . $school_id;
 $result = mysql_query( $sql );
 $school = mysql_fetch_assoc( $result );
+
+if ($school['authorize_customer_profile_id']) {
+    $customerProfile = new CustomerProfile($school['authorize_customer_profile_id']);
+    if ($customerProfile->invalid) { 
+        // remove profileId
+        $sql = "delete authorize_customer_profile_id, authorize_payment_profile_id from schools where school_id = " . $school_id;
+        mysql_query( $sql );
+        $school['authorize_customer_profile_id'] = null;
+        $school['authorize_payment_profile_id'] = null;
+    } else {
+        $paymentProfile = new PaymentProfile($school['authorize_payment_profile_id'], $school['authorize_customer_profile_id']);
+        if ($paymentProfile->invalid) {
+            // remove payment profile
+            $payment_profile->delete();
+            $sql = "delete authorize_payment_profile_id from schools where school_id = " . $school_id;
+            mysql_query( $sql );
+            $school['authorize_payment_profile_id'] = null;
+        }
+    }
+}
 
 echo json_encode( $school );
 ?>
