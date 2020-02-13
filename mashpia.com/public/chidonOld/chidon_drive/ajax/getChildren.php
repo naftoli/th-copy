75ad7ef693da0743f33edf331d71debe65568646
Naftoli Rapoport <naftoli@tzivoshashem.org>
2020-02-13 18:48:48 -0500

    update shabbaton enrollment

--- mashpia.com/public/chidonOld/chidon_drive/ajax/getChildren.php
+++ mashpia.com/public/chidonOld/chidon_drive/ajax/getChildren.php
@@ -18,6 +18,7 @@ if ( $admin_id ) {
     WHERE
         tc.year = :year AND tc.parent_id = :admin
             AND tc.can_enroll = 1 
+            AND (tc.khk = 1 or tc.school_rep = 1 or tc.trophy_contestant = 1 or tc.contestant = 1)
   ");
   $res = $stmt->execute([
     ':year'   =>  $year, 
@@ -26,16 +27,85 @@ if ( $admin_id ) {
   //echo $stmt->debugDumpParams(); exit;
   if ( $res ) {
     $children = $stmt->fetchAll();
+    // make each child's school has already registered and has enough chaps / walking supervisors
+    $stmt = $MASHPIA_DB->prepare("
+      SELECT * FROM th_chidon_schools WHERE year = :year AND school_id = :school
+    ");
+    $stmt_chap = $MASHPIA_DB->prepare("
+      SELECT * FROM th_chidon_chaps WHERE year = :year AND school_id = :school AND chap_type = 1
+    ");
+    $stmt_walking = $MASHPIA_DB->prepare("
+      SELECT * FROM th_chidon_chaps WHERE year = :year AND school_id = :school AND is_walking = 1
+    ");
+    $walking_needed = $MASHPIA_DB->prepare("
+      SELECT * FROM th_chidon_chaps_needed WHERE year = :year AND school_id = :school 
+    ");
+    $schoolNotReady = 0;
+    $numChildren = count( $children );
+    foreach ( $children as $idx => $child ) {
+      $school_id = $child['school_id'];
+      $stmt->execute([
+        ':year' => $year, 
+        ':school' => $school_id
+      ]);
+      $rows = $stmt->fetchAll();
+      if ( count( $rows ) > 0 ) {
+        // check chaps 
+        $stmt_chap->execute([
+          ':year' => $year, 
+          ':school' => $school_id
+        ]);
+        $rows_chap = $stmt_chap->fetchAll(); 
+        if ( count( $rows_chap ) == 0 )  {
+          $schoolNotReady++;
+          unset( $children[$idx] );
+        } else {
+          $stmt_walking->execute([
+            ':year' => $year, 
+            ':school' => $school_id
+          ]);
+          $rows_walking = $stmt_walking->fetchAll();
+          $walking_needed->execute([
+            ':year' => $year, 
+            ':school' => $school_id
+          ]);
+          $walking = $walking_needed->fetch();
+          $needed = $walking['needed'];
+          if ( $needed > count( $rows_walking ) ) {
+            $schoolNotReady++;
+            unset( $children[$idx] );
+          }
+        }
+      } else {
+        $schoolNotReady++;
+        unset( $children[$idx] );
+      }
+    }
     if ( $children ) {
-      echo json_encode([
-        'success'   =>  true,
-        'children'  =>  $children
-      ]);
+      if ( $schoolNotReady > 0 ) {
+        echo json_encode([
+          'success'   =>  true,
+          'children'  =>  $children, 
+          'error'     =>  "Not all of your child(ren)'s school(s) have their chaperones and walking supervisors setup yet. Therefore, not all eligible children will show up on enrollment form."
+        ]);
+      } else {
+        echo json_encode([
+          'success'   =>  true,
+          'children'  =>  $children
+        ]);
+      }
     } else {
-      echo json_encode([
-        'success'   =>  false,
-        'error'     =>  "Your child(ren)'s school needs to activate enrollment before you can enroll."
-      ]);
+      if ( $schoolNotReady > 0 ) {
+        echo json_encode([
+          'success'   =>  false,
+          'error'     =>  "Your child(ren)'s school(s) do not have their chaperones and walking supervisors setup yet. You can only enroll once they set it up."
+        ]);
+      } else {
+        echo json_encode([
+          'success'   =>  false,
+          'error'     =>  "Your child(ren)'s school(s) need to activate enrollment before you can enroll."
+        ]);
+      }
     }
   } else {
     echo json_encode([
