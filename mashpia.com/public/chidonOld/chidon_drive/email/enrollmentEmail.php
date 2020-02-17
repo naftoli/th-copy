f92d0b0a275138f5188f9cd6f51c4c82050e9457
Naftoli Rapoport <naftoli@tzivoshashem.org>
2020-02-17 18:20:43 -0500

    update emial email

--- mashpia.com/public/chidonOld/chidon_drive/email/enrollmentEmail.php
+++ mashpia.com/public/chidonOld/chidon_drive/email/enrollmentEmail.php
@@ -8,9 +8,8 @@ function sendEnrollmentEmail( $email ) {
     $headers[] = 'From: chidon@tzivoshashem.com';
     $headers[] = 'Reply-To: chidon@tzivoshashem.com';
 
-    $message = '
-    <img src="Chidon-5780-Header-web.png" style="max-width: 100%" />
-<p>Thank you for completing your Chidon Shabbaton enrollment form.</p>
+    $message = '<img src="http://test.mashpia.com/chidonOld/chidon_drive/email/Chidon-5780-Header-web.png" style="max-width: 100%; height: auto;" />';
+$message .= '<p>Thank you for completing your Chidon Shabbaton enrollment form.</p>
 <p><strong>PLEASE NOTE: In order for your child to be officially registered for Shabbaton, your information must be properly reviewed, signed, and returned to your child\'s Chidon coordinator. </strong></p>
 <p>If any changes need to be made, please indicate it clearly on this page when submitting.</p>
 <p>IMPORTANT NOTE: The Chidon Shabbaton is a major undertaking, and requires extensive work and coordination to ensure everyone’s safety at all times. For this reason, changes to information submitted after enrollment will require much additional work, and therefore we must apply an additional charge. I understand that any changes to this application after registration closes: </p>
