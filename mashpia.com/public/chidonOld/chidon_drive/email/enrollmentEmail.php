4568f4e8c23870b5b2e617bd01d7bbf0b61956ec
Naftoli Rapoport <naftoli@tzivoshashem.org>
2020-02-17 18:26:47 -0500

    testing links on email

--- mashpia.com/public/chidonOld/chidon_drive/email/enrollmentEmail.php
+++ mashpia.com/public/chidonOld/chidon_drive/email/enrollmentEmail.php
@@ -9,6 +9,7 @@ function sendEnrollmentEmail( $email ) {
     $headers[] = 'Reply-To: chidon@tzivoshashem.com';
 
     $server = $_SERVER['DOCUMENT_ROOT'];
+    echo $server; exit;
     if ( $server == 'chidondrive.com' ) $src = "http://chidondrive.com/email/Chidon-5780-Header-web.png";
     else $src = "http://" . $server . "/chidonOld/chidon_drive/email/Chidon-5780-Header-web.png";
     $message = '<img src="' . $src . '" style="max-width: 100%; height: auto;" />';
