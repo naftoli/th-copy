41c6abde780cbae879a4b829a339ec9395a4902d
Naftoli Rapoport <naftoli@tzivoshashem.org>
2020-02-20 11:32:48 -0500

    update text in edit page

--- mashpia.com/public/chidonOld/chidon_drive/walking_zones/upload_zones.php
+++ mashpia.com/public/chidonOld/chidon_drive/walking_zones/upload_zones.php
@@ -2,22 +2,23 @@
 ini_set('display_errors', 1);
 require_once $_SERVER['DOCUMENT_ROOT'] . '/db.php';
 
-if (($handle = fopen("zones.csv", "r")) !== FALSE) {
+if (($handle = fopen("zones2.csv", "r")) !== FALSE) {
   $qrys = [];
-  $fields = ['chidon_walking_zone_id','zone_5780','street','st_label','cross_label','cross1','cross2','even_start','even_end','odd_start','odd_end'];
+  // $fields = ['chidon_walking_zone_id','zone_5780','street','st_label','cross_label','cross1','cross2','even_start','even_end','odd_start','odd_end'];
+  $fields = ['zone_5780','street','st_label','cross_label','cross1','cross2','even_start','even_end','odd_start','odd_end'];
   while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
     $i = 0;
     foreach ($fields as $field) {
       $$field = $data[$i++];
     }
 
-    if ($chidon_walking_zone_id) {
-      $qrys[] = "update chidon_walking_zones 
-                set zone_5780 = '" . $zone_5780 . "', 
-                st_label = '" . $st_label . "', 
-                cross_label = '" . $cross_label . "'  
-                where chidon_walking_zone_id = " . $chidon_walking_zone_id;
-    } else {
+    // if ($chidon_walking_zone_id) {
+    //   $qrys[] = "update chidon_walking_zones 
+    //             set zone_5780 = '" . $zone_5780 . "', 
+    //             st_label = '" . $st_label . "', 
+    //             cross_label = '" . $cross_label . "'  
+    //             where chidon_walking_zone_id = " . $chidon_walking_zone_id;
+    // } else {
       $qrys[] = "insert into chidon_walking_zones 
                 set zone_5780 = '" . $zone_5780 . "', 
                 st_label = '" . $st_label . "', 
@@ -29,7 +30,7 @@ if (($handle = fopen("zones.csv", "r")) !== FALSE) {
                 even_end = " . $even_end . ", 
                 odd_start = " . $odd_start . ", 
                 odd_end = " . $odd_end;
-    }
+    // }
   }
   // echo "<pre>"; print_r( $qrys ); echo "</pre>"; exit;
   foreach ( $qrys as $line => $qry ) mysql_query( $qry ) or die( "Error on line: " . $line . "<br />" . $qry . "<br />" . mysql_error() );
