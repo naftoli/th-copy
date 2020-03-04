319056b34bd19732cc38396c59058026c77f17dc
Naftoli Rapoport <naftoli@tzivoshashem.org>
2020-03-03 22:03:11 -0500

    added staff summary report; modified kid summary report;

--- mashpia.com/public/chidonOld/reports/school_staff_summary.php
+++ mashpia.com/public/chidonOld/reports/school_staff_summary.php
@@ -8,35 +8,32 @@ if ( $admin_user['auth'] != 'super' ) {
 
 if ( isset( $_POST['submit'] ) ) {
     // echo "<pre>"; print_r( $_POST ); echo "</pre>";
-    if ( !(isset( $_POST['grade'] ) && isset( $_POST['cat'] ) && isset( $_POST['gender'] )) ) {
-        echo "You must choose at least one grade, category and gender.";
+    if ( !(isset( $_POST['cat'] ) && isset( $_POST['gender'] )) ) {
+        echo "You must choose at least one category and gender.";
         exit;
     }
 
     // secure values
-    foreach ( $_POST['grade'] as $k => $val ) {
-        $_POST['grade'][$k] = mysql_real_escape_string( $val );
-    }
+    // foreach ( $_POST['grade'] as $k => $val ) {
+    //     $_POST['grade'][$k] = mysql_real_escape_string( $val );
+    // }
 
     require_once $_SERVER['DOCUMENT_ROOT'] . '/api/header/header.php';
     require_once $_SERVER['DOCUMENT_ROOT'] . '/api/header/db.php';
     require_once $_SERVER['DOCUMENT_ROOT'] . '/class.globalSettings.php';
     $year = GlobalSettings::getChidonYear();
 
-    $grades = "'" . implode("','", $_POST['grade']) . "'";
+    // $grades = "'" . implode("','", $_POST['grade']) . "'";
+    $cats = implode(',', $_POST['cat']);
+    $genders = "'" . implode("','", $_POST['gender']) . "'";
     $stmt = $MASHPIA_DB->prepare("
         SELECT 
-            u.gender, tc.*
+            *
         FROM
-            th_chidon tc
-                JOIN
-            users u USING (user_id)
+            th_chidon_chaps
         WHERE
-            grade IN ($grades) AND year = 5780 
-                AND deleted = 0
-                AND date_paid > 0
-        ORDER BY
-            grade
+            year = :year AND chidon_type in ($genders) 
+                AND chap_type in ($cats)
     ");
     $stmt->execute([':year' => $year]);
     $rows = $stmt->fetchAll();
@@ -44,13 +41,22 @@ if ( isset( $_POST['submit'] ) ) {
     // create summary
     $info = [];
     foreach ( $rows as $row ) {
-        foreach ( $_POST['cat'] as $cat ) {
-            if ( intval($row[$cat]) == 1 ) {
-                if ( isset( $info[$row['gender']][$cat][$row['grade']] ) ) $info[$row['gender']][$cat][$row['grade']]++;
-                else $info[$row['gender']][$cat][$row['grade']] = 1;
-                break;
-            }
+        switch ( intval($row['chap_type']) ) {
+            case 1:
+                $type = "Chaperone";
+            break;
+            case 2:
+                $type = "Walking Supervisor";
+            break;
+            case 3:
+                $type = "Principal";
+            break;
+            case 4:
+                $type = "Other";
+            break;
         }
+        if ( isset( $info[$row['chidon_type']][$type] ) ) $info[$row['chidon_type']][$type]++;
+        else $info[$row['chidon_type']][$type] = 1;
     }
     // echo "<pre>"; print_r( $info ); echo "</pre>";
 }
@@ -78,58 +84,49 @@ if ( isset( $_POST['submit'] ) ) {
     </head>
     <body>
         <?php if ( !isset( $_POST['submit'] ) ) : ?>
-            <form action="shabbaton_summary.php" method="post">
-                <fieldset>
+            <form action="school_staff_summary.php" method="post">
+                <!-- <fieldset>
                     <legend>Choose Grades</legend>
                     <?php
                     for ($i = 4; $i < 9; $i++) {
                         echo "<input type='checkbox' name='grade[]' value='" . $i . "' /> Grade: " . $i . "<br />";
                     }
                     ?>  
-                </fieldset>
+                </fieldset> -->
                 <fieldset>
                     <legend>Choose Categories</legend>
                     <?php
-                    $categories = ['khk', 'school_rep', 'trophy_contestant', 'contestant'];
-                    foreach ( $categories as $cat ) {
-                        echo "<input type='checkbox' name='cat[]' value='" . $cat . "' /> " . $cat . "<br />";
+                    $categories = [
+                        1 => 'Chaperone', 
+                        2 => 'Walking Supervisor', 
+                        3 => 'Principal', 
+                        4 => 'Other'
+                    ];
+                    foreach ( $categories as $val => $cat ) {
+                        echo "<input type='checkbox' name='cat[]' value='" . $val . "' /> " . $cat . "<br />";
                     }
                     ?>  
                 </fieldset>
                 <fieldset>
                     <legend>Choose Gender</legend>
-                    <input type="checkbox" name="gender[]" value='m' /> Boys<br />
-                    <input type="checkbox" name="gender[]" value='f' /> Girls<br />
+                    <input type="checkbox" name="gender[]" value='boys' /> Boys<br />
+                    <input type="checkbox" name="gender[]" value='girls' /> Girls<br />
                 </fieldset>
                 <br />
                 <input type="submit" name="submit" value="submit" />
             </form>
         <?php else : ?>
             <?php foreach ( $info as $gender => $more ) : ?>
-                <h1>
-                <?php
-                if ( $gender == 'M' ) echo 'Boys';
-                else if ( $gender == 'F' ) echo 'Girls';
-                ?>
-                </h1>
+                <h1><?= ucwords($gender); ?></h1>
                 <table>
                     <tr>
                         <th>Category</th>
-                        <th>Grade</th>
                         <th>Total</th>
                     </tr>
                     <?php
-                    $grandTotal = 0;
-                    foreach ( $more as $cat => $other ) {
-                        $gTotal = 0;
-                        foreach ( $other as $grade => $total ) {
-                            $gTotal += $total;
-                            $grandTotal += $total;
-                            echo "<tr><td>" . $cat . "</td><td>" . $grade . "</td><td>" . $total . "</td></tr>";
-                        }
-                        echo "<tr><th colspan='2' style='text-align: right;'>Total:</th><th>" . $gTotal . "</th></tr>";
+                    foreach ( $more as $cat => $total ) {
+                        echo "<tr><td>" . $cat . "</td><td>" . $total . "</td></tr>";
                     }
-                    echo "<tr><th colspan='2' style='text-align: right;'>Grand Total:</th><th>" . $grandTotal . "</th></tr>";
                     ?>
                 </table>
             <?php endforeach; ?>
