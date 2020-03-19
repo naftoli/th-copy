bbdb04c3d82899e4fd1326366158750cfb69f149
naftoli <naftoli@tzivoshashem.org>
2020-03-19 17:49:30 -0400

    rollback prev commit

--- mashpia.com/public/blog/wp-config.php
+++ mashpia.com/public/blog/wp-config.php
@@ -72,12 +72,12 @@ $table_prefix  = 'wp_';
 define('WP_DEBUG', true);
 
 define('WP_ALLOW_MULTISITE', true);
-define('MULTISITE', true);
-define('SUBDOMAIN_INSTALL', false);
-define('DOMAIN_CURRENT_SITE', 'mashpia.com');
-define('PATH_CURRENT_SITE', '/blog/');
-define('SITE_ID_CURRENT_SITE', 1);
-define('BLOG_ID_CURRENT_SITE', 1);
+// define('MULTISITE', true);
+// define('SUBDOMAIN_INSTALL', false);
+// define('DOMAIN_CURRENT_SITE', 'mashpia.com');
+// define('PATH_CURRENT_SITE', '/blog/');
+// define('SITE_ID_CURRENT_SITE', 1);
+// define('BLOG_ID_CURRENT_SITE', 1);
 
 // define('FS_METHOD', 'ssh');
 // define('FTP_USER', 'mashpia');
