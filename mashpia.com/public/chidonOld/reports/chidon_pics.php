b779f1c9f52e8b30e20c649239dad3c19f533364
Naftoli Rapoport <naftoli@tzivoshashem.org>
2020-03-27 15:54:25 -0400

    create images in forlders based off chidon pics

--- mashpia.com/public/chidonOld/reports/chidon_pics.php
+++ mashpia.com/public/chidonOld/reports/chidon_pics.php
@@ -13,6 +13,7 @@ $year = GlobalSettings::getChidonYear();
 $info = [];
 $sql = "select * from th_chidon tc 
         join users u using (user_id) 
+        join schools s on tc.school_id = s.school_id 
         left join thumbs t on u.user_photo_id = t.file_id 
         where date_paid > 0 and year = " . $year . " and tc.school_id in (" . implode(',', array_keys( $schools )) . ")";
 $result = mysql_query( $sql );
@@ -47,6 +48,11 @@ while ( $row = mysql_fetch_assoc( $result ) ) {
     <BODY>
         <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/admin_header.php'; ?>
         <h1>Chidon Pictures</h1>
+        <?php 
+        // create images dir
+        if (!is_dir("images")) mkdir("images");
+        chdir("images");
+        ?>
         <?php foreach ( $info as $id => $children ) : ?>
             <h2><?= $schools[$id] ?></h2>
             <table class="pics">
@@ -79,9 +85,21 @@ while ( $row = mysql_fetch_assoc( $result ) ) {
                     } 
                     echo "<tr><td>" . $child['th_chidon_id'] . "</td><td>" . $child['first'] . ' ' . $child['last'] . "</td><td>";
                     echo "<img src='" . $img . "' /></td></tr>";
+                    
+                    // create images and download to correct folder
+                    $school = $child['school_name'];
+                    if (!is_dir($school)) {
+                        mkdir($school);
+                    }
+                    $new_img = imagecreatefromstring(file_get_contents($img));
+                    $new_image = imagepng($new_img, $school . '/' . $child['th_chidon_id'] . '.png');
                 }
                 ?>
             </table>
-        <?php endforeach; ?>  
+        <?php endforeach; ?> 
+        <?php chdir('../'); ?>
+        <p>
+            <a href="images">Images Directory</a>
+        </p> 
     </BODY>
 </HTML>
