7bc4b0363be57086dbf01c8c0486865e45d11c37
Naftoli Rapoport <naftoli@tzivoshashem.org>
2020-05-06 15:55:26 -0400

    more debugging

--- mashpia.com/public/updateMedalsRanks.php
+++ mashpia.com/public/updateMedalsRanks.php
@@ -1,13 +1,13 @@
 <?php
 ini_set('display_errors', 1);
 $admin_auth = ['school'];
+require_once('header.php');
 ?>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 </head>
 <?php
-require_once('header.php');
 $ids = [];
 $sql = "select user_id from users were user_registered > 0";
 $result = mysql_query( $sql ); 
