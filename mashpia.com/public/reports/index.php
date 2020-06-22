def872d6a2243ef03d6a5fc7d6ed1be447799d09
Naftoli Rapoport <naftoli@tzivoshashem.org>
2020-06-22 19:52:23 -0400

    add campaigns button to office reports

--- mashpia.com/public/reports/index.php
+++ mashpia.com/public/reports/index.php
@@ -105,6 +105,12 @@ require_once($_SERVER["DOCUMENT_ROOT"].'/header.php');
                     <span class="link-text">Mivtzoim</span>
                 </div>
             </a>
+            <a href="/new/missions/personalize">
+                <div class="button">
+                    <img src="/images/icon_report.png" height="32" alt="tickets"/>
+                    <span class="link-text">Campaigns</span>
+                </div>
+            </a>
         </div>
         <?if ($admin_user['auth'] == 'super') {?>
         <h2>Managment Links</h2>
