ddb3f81450bfa0095fee11aa3d0da4101f6d52d7
Naftoli Rapoport <naftoli@tzivoshashem.org>
2020-06-21 21:59:11 -0400

    try printing multiple children in different bathes

--- mashpia.com/public/api/print/missions.php
+++ mashpia.com/public/api/print/missions.php
@@ -141,21 +141,28 @@ if ( $dates == 'english' ) $dates_id = 2;
     <script>
         document.addEventListener('DOMContentLoaded', function() {
             // your code here
-            const original = document.body.innerHTML;
+            // const original = document.body.innerHTML;
+            let elems = [];
             $(".userMission").each( function() {
                 const id = $(this).attr('id');
                 const print_div = document.getElementById(id).innerHTML;
-                document.body.innerHtml = '<html><head><meta charset="utf8" /><link rel="stylesheet" href="/mission_report/newStyle.css?v=2.3" type="text/css" /></head><body>' + print_div + "</body></html>";
-                window.print();
-                document.body.innerHTML = original;
-                // const print_area = window.open('', '', 'width=900,height=650');
-                // print_area.document.write('<html><head><meta charset="utf8" /><link rel="stylesheet" href="/mission_report/newStyle.css?v=2.3" type="text/css" /></head><body>');
-                // print_area.document.write(print_div + "</body></html>");
-                // print_area.document.close();
+                // document.body.innerHtml = '<html><head><meta charset="utf8" /><link rel="stylesheet" href="/mission_report/newStyle.css?v=2.3" type="text/css" /></head><body>' + print_div + "</body></html>";
+                // window.print();
+                // document.body.innerHTML = original;
+                const print_area = window.open('', '', 'width=900,height=650');
+                print_area.document.write('<html><head><meta charset="utf8" /><link rel="stylesheet" href="/mission_report/newStyle.css?v=2.3" type="text/css" /></head><body>');
+                print_area.document.write(print_div + "</body></html>");
+                print_area.document.close();
+                elems.push(print_area);
                 // print_area.focus();
                 // print_area.print();
                 // print_area.close();
             });
+            $(elems).each( function() {
+                $(this).focus();
+                $(this).print();
+                $(this).close();
+            })
         }, false);
     </script>
     <?php // ! *************************** Debug *************************** ?>
