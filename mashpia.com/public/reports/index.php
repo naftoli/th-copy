e7ddfd9267cd3809197606abe8a27f2a2c8f2f75
Naftoli Rapoport <naftoli@tzivoshashem.org>
2020-07-02 15:31:40 -0400

    remove campaigns button

--- mashpia.com/public/reports/index.php
+++ mashpia.com/public/reports/index.php
@@ -105,12 +105,6 @@ require_once($_SERVER["DOCUMENT_ROOT"].'/header.php');
                     <span class="link-text">Mivtzoim</span>
                 </div>
             </a>
-            <a href="/new/missions/personalize">
-                <div class="button">
-                    <img src="/images/icon_report.png" height="32" alt="tickets"/>
-                    <span class="link-text">Campaigns</span>
-                </div>
-            </a>
         </div>
         <?if ($admin_user['auth'] == 'super') {?>
         <h2>Managment Links</h2>
