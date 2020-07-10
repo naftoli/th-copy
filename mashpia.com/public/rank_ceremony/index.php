b20766d18c4b0e1da19412ad1ecf21429448e823
Naftoli Rapoport <naftoli@tzivoshashem.org>
2020-07-09 22:53:30 -0400

    more debugging

--- mashpia.com/public/rank_ceremony/index.php
+++ mashpia.com/public/rank_ceremony/index.php
@@ -23,7 +23,7 @@ function createZip($files, $images, $filename) {
     }
     foreach($files as $file) {
         $zip->addFile($file);
-        unlink($file);
+        // unlink($file);
     }
     foreach ($images as $img) {
         $zip->addFile($img);
