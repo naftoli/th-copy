bb6bcf203260327856aeb77a1ab4d148ac44c8ba
Naftoli Rapoport <naftoli@tzivoshashem.org>
2020-07-13 22:30:37 -0400

    fixx bug in rank_cremony page

--- mashpia.com/public/rank_ceremony/index.php
+++ mashpia.com/public/rank_ceremony/index.php
@@ -33,7 +33,7 @@ function createZip($files, $images, $filename) {
     $zip->close();
 }
 
-$ranksNames = [
+$rankNames = [
     'Sergeant'          =>  'sergeant',
     'Sergeant Major'    =>  'sergeant_major',
     'Second Lieutenant' =>  'second_lieutenant',
@@ -67,7 +67,7 @@ foreach ($schools as $id => $school) {
     foreach ($ranks as $school => $other) {
         foreach ($other as $rank => $more) {
             $j = 1;
-            $info[$i++] = [($rankNames[$rank] . '_intro'),'','','','']; // rank intro
+            $info[$i++] = [($rankNames[$rank] . '_intro')]; // rank intro
             foreach ($more as $teacher => $other) {
                 foreach ($other as $grade => $more) {
                     foreach ($more as $user_id) {
@@ -90,7 +90,8 @@ foreach ($schools as $id => $school) {
             }
         }
     }
-    $info[$i] = ['outro','outro','','','','']; // outro
+    echo "<pre>"; print_r($info); echo "</pre>"; exit;
+    $info[$i] = ['outro','outro']; // outro
     if (count($ranks)) {
         $file_name = "TSV_Report_" . $id . ".csv";
         createFile($info, $file_name);
@@ -111,88 +112,4 @@ header('Content-Length: ' . filesize($filename));
 flush(); // Flush system output buffer
 readfile($filename);
 unlink($filename);
-exit;
 ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <title></title>
-        <meta charset="utf8" />
-        <style>
-            tr, th, td {
-                font-family: Arial, Helvetica, sans-serif;
-                padding: 5px;
-                font-size: 14px;
-            }
-        </style>
-    </head>
-    <body>
-        <?php
-        $info = [];
-        foreach ($schools as $school_id => $school_name) {
-            if (count($ranks[$school_id]) > 0) 
-                echo "<table><tr><th>comp</th><th>comp_name</th><th>chayol_name</th><th>chayol_picture</th><th>school_name</th><th>school_logo</th></tr>";
-            foreach ($ranks[$school_id] as $school => $other) {
-                $k = 0; // index into info array
-                foreach ($other as $rank => $more) {
-                    $i = 1; // number the users within rank
-                    $j = 1; // same as above
-                    foreach ($more as $teacher => $other) {
-                        foreach ($other as $grade => $more) {
-                            foreach ($more as $user_id) {
-                                echo "<tr><td>" . $rank . "</td><td>" . ($rank . '_' . $i++) . "</td><td>" . $users[$school_id][$user_id] . "</td><td>" . 
-                                    $pics[$school_id][$user_id] . "</td><td>" . $school . "</td><td>" . $logos[$school_id][$school]['logo_id'] . "</td></tr>";
-                                $info[$school_id][$k]['comp'] = $rank;
-                                $info[$school_id][$k]['comp_name'] = $rank . '_' . $j++; 
-                                $info[$school_id][$k]['chayol_name'] = $users[$school_id][$user_id];
-                                $info[$school_id][$k]['chayol_picture'] = $pics[$school_id][$user_id];
-                                $info[$school_id][$k]['school_name'] = $school;
-                                $info[$school_id][$k]['school_logo'] = $logos[$school_id][$school]['logo_id'];
-                                $k++;
-                            }
-                        }
-                    }
-                }
-            }
-            echo "</table><br />";
-        }
-        ?>
-    </body>
-    <script>
-        function dataToTSV( headers, rows, filename ) {
-            const universalBOM = "\uFEFF";
-            let csvContent = `${ headers.join("\t") }\n`;
-            // Add each row to the TSV content and encode it for unicode in excel
-            rows.forEach( row => { csvContent += `${row.join("\t")}\n` } );
-            csvContent = encodeURIComponent( universalBOM + csvContent );
-            // create and click the download link
-            let link = document.createElement('a');
-            link.href = `data:text/csv;charset=utf-8,${csvContent}`;
-            // link.target = '_blank';
-            link.download = `${filename}.csv`;
-            link.click();
-        }
-
-        window.onload = function() {
-            // set the headers
-            const headers = ['comp','comp_name','chayol_name','chayol_picture','school_name','school_logo']
-
-            // get the data
-            const info = <?= json_encode($info); ?>;
-
-            // create seperate file for each school
-            for (school in info) {
-                let rows = []
-                for (i of info[school]) {
-                    let row = []
-                    for (h of headers) {
-                        row.push(i[h])
-                    }
-                    rows.push(row)
-                }
-                const filename = "TSV_Report_" + school;
-                dataToTSV( headers, rows, filename );
-            }
-        }
-    </script>
-</html>
