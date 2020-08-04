74ea9941b1173d69814c95fef5170ce60649baef
Naftoli Rapoport <naftoli@tzivoshashem.org>
2020-08-03 20:36:07 -0400

    more debugging

--- mashpia.com/public/class.shabbosMevorchim.php
+++ mashpia.com/public/class.shabbosMevorchim.php
@@ -941,7 +941,7 @@ class ShabbosMevorchim {
                             $row2 = $stmt2->fetch( PDO::FETCH_ASSOC );
                             $total = $row2['total'];
                             $this->studentDoneResults[$date][$class][$user['user_id']][$key] = $row2['total'];
-                            if ($row2['total'] > 0) echo $user['user_id'] . $row2['total'] . "<br />";
+                            if ($row2['total'] > 0) echo $user['user_id'] . '-' . $row2['total'] . "<br />";
                         }
 						
 						if ($total > 0) {
