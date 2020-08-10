ea0773c59ea931e2e40af5428fe0702344427ca5
Naftoli Rapoport <naftoli@tzivoshashem.org>
2020-08-09 20:05:57 -0400

    debugging

--- mashpia.com/public/shabbos_mevorchim_summer.php
+++ mashpia.com/public/shabbos_mevorchim_summer.php
@@ -102,6 +102,7 @@ tr, th, td {
     require_once 'class.adminSchools.php';      
     $as = new AdminSchools( $admin_user['admin_id'], $admin_user['auth'] );
     $ids = $as->getSchools();
+    $ids = [54 => 'Beis Rivka'];
 
     $info = [];
     $userInfo = [];
@@ -118,7 +119,7 @@ tr, th, td {
             $sm->setStudentResults();
             $quotas = $sm->getStudentResults();
             $done = $sm->getStudentDoneResults();
-            // echo "<pre>"; print_r($done); echo "</pre>";
+            echo "<pre>"; print_r($quotas); echo "</pre>"; exit;
             foreach ($quotas as $date => $other) {
                 foreach ($other as $grade => $more) {
                     foreach ($more as $user_id => $values) {
@@ -127,10 +128,10 @@ tr, th, td {
                         $userInfo[$user_id] = mysql_fetch_assoc($result);
                         foreach ($values as $task => $quota) {
                             $result = intval($done[$date][$grade][$user_id][$task]);
-                            if ($user_id == 22949) {
-                                echo "Quota: " . $quota . "<br />"; 
-                                echo "Done: " . $result . "<br />";
-                            }
+                            echo "User: " . $user_id . "<br />";
+                            echo "Date: " . $date . "<br />";
+                            echo "Quota: " . $quota . "<br />"; 
+                            echo "Done: " . $result . "<br /><br />";
                             if ($result && $result >= intval($quota)) {
                                 $info[$id][$user_id][$date] = [
                                     'quota' =>  $quota, 
