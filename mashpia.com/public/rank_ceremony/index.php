431e3293497894720d2d4815e4506e5b4a3d4228
Naftoli Rapoport <naftoli@tzivoshashem.org>
2020-08-17 17:46:51 -0400

    only create file for schools that have ranks

--- mashpia.com/public/rank_ceremony/index.php
+++ mashpia.com/public/rank_ceremony/index.php
@@ -62,42 +62,44 @@ foreach ($schools as $id => $school) {
     $users = $r->getUserInfo();
     $pics = $r->getUserPic();
     $logos = $r->getSchoolLogos();
-    
-    $i = 0;
-    $info[$i++] = ['comp','comp_name','chayol_name','chayol_picture','school_name','school_logo'];
-    $info[$i++] = ['promotions_intro','promotions_intro','','',$school,$logos[$school]['logo_id']]; // intro
-    foreach ($ranks as $school => $other) {
-        foreach ($other as $rank => $more) {
-            $j = 1;
-            $info[$i++] = [($rankNames[$rank] . '_intro'), ucwords(str_replace('_', ' ', ($rankNames[$rank] . '_' . $j++)))]; // rank intro
-            foreach ($more as $teacher => $other) {
-                foreach ($other as $grade => $more) {
-                    foreach ($more as $user_id) {
-                        // create pic of child to add to zipArchive
-                        $url = 'http://mashpia.com' . $pics[$user_id];
-                        $img_url = $user_id . '.png';
-                        $new_img = imagecreatefromstring(file_get_contents($url));
-                        $new_image = imagepng($new_img, $img_url);
-                        if ($new_image && !in_array($img_url, $images)) $images[] = $img_url;
 
-                        $info[$i]['comp'] = $rankNames[$rank];
-                        $info[$i]['comp_name'] = ucwords(str_replace('_', ' ', ($rankNames[$rank] . '_' . $j++))); 
-                        $info[$i]['chayol_name'] = $users[$user_id];
-                        $info[$i]['chayol_picture'] = $new_image ? $img_url : '';
-                        $info[$i]['school_name'] = $school;
-                        $info[$i]['school_logo'] = $logos[$school]['logo_id'];
-                        $i++;
+    if (!empty($ranks)) {
+        $i = 0;
+        $info[$i++] = ['comp', 'comp_name', 'chayol_name', 'chayol_picture', 'school_name', 'school_logo'];
+        $info[$i++] = ['promotions_intro', 'promotions_intro', '', '', $school, $logos[$school]['logo_id']]; // intro
+        foreach ($ranks as $school => $other) {
+            foreach ($other as $rank => $more) {
+                $j = 1;
+                $info[$i++] = [($rankNames[$rank] . '_intro'), ucwords(str_replace('_', ' ', ($rankNames[$rank] . '_' . $j++)))]; // rank intro
+                foreach ($more as $teacher => $other) {
+                    foreach ($other as $grade => $more) {
+                        foreach ($more as $user_id) {
+                            // create pic of child to add to zipArchive
+                            $url = 'http://mashpia.com' . $pics[$user_id];
+                            $img_url = $user_id . '.png';
+                            $new_img = imagecreatefromstring(file_get_contents($url));
+                            $new_image = imagepng($new_img, $img_url);
+                            if ($new_image && !in_array($img_url, $images)) $images[] = $img_url;
+
+                            $info[$i]['comp'] = $rankNames[$rank];
+                            $info[$i]['comp_name'] = ucwords(str_replace('_', ' ', ($rankNames[$rank] . '_' . $j++)));
+                            $info[$i]['chayol_name'] = $users[$user_id];
+                            $info[$i]['chayol_picture'] = $new_image ? $img_url : '';
+                            $info[$i]['school_name'] = $school;
+                            $info[$i]['school_logo'] = $logos[$school]['logo_id'];
+                            $i++;
+                        }
                     }
                 }
             }
         }
-    }
-    $info[$i] = ['outro','outro']; // outro
-    if (count($ranks)) {
-        $file_name = str_replace(' ', '_', $school) . ".csv";
-        createFile($info, $file_name);
-        $files[] = $file_name;
-//        break;
+        $info[$i] = ['outro', 'outro']; // outro
+        if (count($ranks)) {
+            $file_name = str_replace(' ', '_', $school) . ".csv";
+            createFile($info, $file_name);
+            $files[] = $file_name;
+            //        break;
+        }
     }
 } 
 $filename = "tsv.zip";
