ab279e94f8e5f4adc72fc2f61d91aefd70fcb128
Naftoli Rapoport <naftoli@tzivoshashem.org>
2020-08-17 23:55:56 -0400

    remove debugging

--- mashpia.com/public/rank_ceremony/index.php
+++ mashpia.com/public/rank_ceremony/index.php
@@ -98,15 +98,14 @@ foreach ($schools as $id => $school) {
             }
         }
         $info[$i] = ['outro', 'outro']; // outro
-//        if (count($ranks)) {
-//            $file_name = str_replace(' ', '_', $school) . ".csv";
-//            createFile($info, $file_name);
-//            $files[] = $file_name;
-//            break;
-//        }
+        if (count($ranks)) {
+            $file_name = str_replace(' ', '_', $school) . ".csv";
+            createFile($info, $file_name);
+            $files[] = $file_name;
+            break;
+        }
     }
 }
-exit;
 $filename = "tsv.zip";
 createZip($files, $images, $filename);
 
