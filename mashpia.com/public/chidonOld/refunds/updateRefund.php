57a9fe347aadc2d734a5895a202ff3a1c6ec9fe8
Naftoli Rapoport <naftoli@tzivoshashem.org>
2020-08-23 18:24:52 -0400

    send email when activating refund page

--- mashpia.com/public/chidonOld/refunds/updateRefund.php
+++ mashpia.com/public/chidonOld/refunds/updateRefund.php
@@ -19,6 +19,54 @@ $res = $stmt->execute([
     ':admin'    =>  $admin_id
 ]);
 
+if ($res) {
+    // get email address
+    $stmt = $MASHPIA_DB->prepare("
+        SELECT first, last, admin_email FROM admins WHERE admin_id = :admin
+    ");
+    $stmt->execute([':admin' => $admin_id]);
+    $row = $stmt->fetch();
+    $to = $row['admin_email'];
+    $name = $row['first'] . ' ' . $row['last'];
+    if (filter_var($to, FILTER_VALIDATE_EMAIL)) {
+        // send email to parent
+        $from = "accounting@tzivoshashem.org";
+        $headers[] = 'MIME-Version: 1.0';
+        $headers[] = 'Content-type: text/html; charset=iso-8859-1';
+        $headers[] = 'From: ' . $from;
+        $headers[] = 'Reply-to: ' . $from;
+        $subject = "Chidon Shabbaton Refund Page";
+
+        $msg = <<<MSG
+Dear $name,
+<br /><br />
+Thank you for requesting a refund for your child(ren).
+<br /><br />
+Please log into your Tzivos Hashem parents account. 
+<br /><br />
+In the top right corner you will see a button that says "request refund". 
+<br /><br />
+Click on that button and fill out the form.
+<br /><br />
+We wish you much continued Nachas from your child(ren).
+<br /><br />
+We look forward to the ultimate chidon with Moshiach Now.
+<br /><br />
+Kesiva Vachasima Tova 
+<br /><br />
+Shimmy Weinbaum <br />
+Chidon Director 
+MSG;
+
+        if (!mail($to, $subject, $msg, implode("\r\n", $headers))) {
+            // send mail to myself
+            $subject = "Error sending chidon refund email";
+            $to = 'naftoli@tzivoshashem.org';
+            @mail($to, $subject, $msg, implode("\r\n", $headers);
+        }
+    }
+}
+
 echo json_encode([
     'success'   =>  $res
 ]);
