fba99dba9746a42583d5489d427e944062493070
Naftoli Rapoport <naftoli@tzivoshashem.org>
2020-09-14 17:14:52 -0400

    update lulav for canadian schools; update data entry dates;

--- mashpia.com/public/mobile/reg/ajax/getChildren.php
+++ mashpia.com/public/mobile/reg/ajax/getChildren.php
@@ -110,7 +110,7 @@ if ( !empty( $users ) ) {
         $sqlLulav = "select ls.* 
                      from lulav_settings ls 
                      join schools s using (school_id) 
-                     where school_country in ('United States','US','USA','U.S.A.') 
+                     where school_country in ('United States','US','USA','U.S.A.','Canada','canada') 
                      and year = " . $reg_year;
         $resLulav = mysql_query( $sqlLulav );
         while ( $rowLulav = mysql_fetch_assoc( $resLulav ) ) {
