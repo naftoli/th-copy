6c43ea8fbf2a0eb3c988d8fcf5dc301407d734c8
Naftoli Rapoport <naftoli@tzivoshashem.org>
2020-10-02 15:07:34 -0400

    try to fix issue

--- mashpia.com/public/mobile/reg/ajax/getChildren.php
+++ mashpia.com/public/mobile/reg/ajax/getChildren.php
@@ -217,7 +217,7 @@ if ( !empty( $users ) ) {
 
         // turn off chidon
         $keepOn = [7772460, 7763324, 7774904];
-        if (time() > 1601611200 && !in_array($row['user_serial'], $keepOn)) $children[ $row['user_id'] ]['reg_types']['chidon'] = false;
+        if (!in_array($row['user_serial'], $keepOn)) $children[ $row['user_id'] ]['reg_types']['chidon'] = false;
 //        if ($row['user_id'] == 13159) $children[$row['user_id']]['reg_types']['chidon'] = true;
 
         // if school hasn't registered, turn off chayolei, chidon registration
