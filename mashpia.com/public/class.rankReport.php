c11a048ef64949525914a2630762b508658ddcc9
Naftoli Rapoport <naftoli@tzivoshashem.org>
2020-10-29 16:10:34 -0400

    try to fix hebrew not showing up correctly

--- mashpia.com/public/class.rankReport.php
+++ mashpia.com/public/class.rankReport.php
@@ -188,8 +188,22 @@ class RankReport extends Report {
     private function checkHe($first, $last, $separator) {
         if (ord($first[0]) > 122) {
             // its hebrew
-            return strrev($last) . $separator . strrev($first);
+            return $this->mb_strrev($last) . $separator . $this->mb_strrev($first);
         }
+    }
+
+    /**
+     * Reverse a miltibyte string.
+     *
+     * @param string $string The string to be reversed.
+     * @param string|null $encoding The character encoding. If it is omitted, the internal character encoding value
+     *     will be used.
+     * @return string The reversed string
+     */
+    private function mb_strrev(string $string, string $encoding = null): string
+    {
+        $chars = mb_str_split($string, 1, $encoding ?: mb_internal_encoding());
+        return implode('', array_reverse($chars));
     }
 }
 ?>
