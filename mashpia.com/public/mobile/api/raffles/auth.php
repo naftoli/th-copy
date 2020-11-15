b915dc5e6525575b5e196cc18621234d4c770c4b
= <naftoli@tzivoshashem.org>
2020-11-15 12:52:13 -0500

    remove auth for sara yiska to test

--- mashpia.com/public/mobile/api/raffles/auth.php
+++ mashpia.com/public/mobile/api/raffles/auth.php
@@ -6,5 +6,5 @@
 //    exit;
 //}
 // make sure we are already logged in
-$admin_auth = ['school', 'user'];
-require $_SERVER['DOCUMENT_ROOT'] . '/header.php';
+//$admin_auth = ['school', 'user'];
+//require $_SERVER['DOCUMENT_ROOT'] . '/header.php';
