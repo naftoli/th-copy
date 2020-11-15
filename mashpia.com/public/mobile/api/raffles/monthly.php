4b74bc5b30b450e54be590533a6d60ef1b88224d
Naftoli Rapoport <naftoli@tzivoshashem.org>
2020-11-14 19:54:19 -0500

    changes to raffles api

--- mashpia.com/public/mobile/api/raffles/monthly.php
+++ mashpia.com/public/mobile/api/raffles/monthly.php
@@ -1,4 +1,15 @@
 <?php
 require 'auth.php';
+require 'functions.php';
 
+$action = mysql_real_escape_string($_GET['action']);
+$user_id = mysql_real_escape_string($_GET['user_id']);
 
+$result = [];
+switch ($action) {
+    case 'raffle-data':
+        break;
+    case 'winner-data':
+        break;
+}
+echo $result;
--- mashpia.com/public/mobile/api/raffles/weekly.php
+++ mashpia.com/public/mobile/api/raffles/weekly.php
@@ -1,32 +1,33 @@
 <?php
 require 'auth.php';
-
-require $_SERVER['DOCUMENT_ROOT'] . '/raffles/shared/classes/Raffle.php';
-use \raffles\weekly\Raffle as Raffle;
 require 'functions.php';
 
 $action = mysql_real_escape_string($_GET['action']);
 $user_id = mysql_real_escape_string($_GET['user_id']);
 
+require $_SERVER['DOCUMENT_ROOT'] . '/raffles/shared/classes/Raffle.php';
+use \raffles\weekly\Raffle as Raffle;
 $weekly = getRaffleInfo('weekly');
 
+$result = [];
 switch ($action) {
     case 'prize-info':
         $prize = getPrizeInfo($weekly['raffle_id']);
-        echo json_encode([
+        $result = json_encode([
             'img'   => $prize['pic'],
             'thumb' => $prize['thumb'],
             'name'  => $prize['name']
         ]);
         break;
     case 'track-records':
-        echo getRaffleHistory('weekly', $user_id);
+        $result = getRaffleHistory('weekly', $user_id);
         break;
     case 'completed':
         $raffle = Raffle::load($weekly['raffle_id']);
         $completed = $raffle->checkWeekly($user_id);
-        echo json_encode([
+        $result = json_encode([
             'days_completed' => $completed
         ]);
         break;
 }
+echo $result;
