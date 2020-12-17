3db9906678bcfbb8e782492c91958b24fbab6704
= <=>
2020-12-17 15:09:17 -0500

    don't allow changing of types for chidon after certain date
    Signed-off-by: = <=>

--- mashpia.com/public/chidonTests/setTypes.php
+++ mashpia.com/public/chidonTests/setTypes.php
@@ -3,6 +3,15 @@
 $admin_auth = ['school'];
 require $_SERVER['DOCUMENT_ROOT'] . '/header.php';
 
+if ($admin_user['auth'] != 'super') {
+    $today = new DateTime();
+    $shutdown = new DateTime('2020-12-18 05:00:00');
+    if ($today >= $shutdown) {
+        echo "This page is now closed.";
+        exit;
+    }
+}
+
 require $_SERVER['DOCUMENT_ROOT'] . '/class.adminSchools.php';
 $as = new AdminSchools( $admin_user['admin_id'], $admin_user['auth'], true, true ); // add chidon schools
 $schools = $as->getSchools();
