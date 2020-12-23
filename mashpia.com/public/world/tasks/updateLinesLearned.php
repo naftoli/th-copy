3e66f9a38098688104ede2a2f8d3947ab49a70b2
= <=>
2020-12-23 17:41:58 -0500

    update lines learned entered by parents in beg of 5781
    Signed-off-by: = <=>

--- mashpia.com/public/world/tasks/updateLinesLearned.php
+++ mashpia.com/public/world/tasks/updateLinesLearned.php
@@ -6,16 +6,16 @@ $sql = "select user_id, MAX(done_qty) as learned, school_id, class_id from date_
         join date_tasks dt using (date_task_id) 
         join date_tasks_missions dtmm using (date_tasks_mission_id) 
         join users u using (user_id) 
-        where dt.grid_id in (21001, 21002) 
-        and dtmm.start_date > 2458754 
+        where dt.grid_id in (21001,21002,21003,21004,21005,21006,21007,21008,21013,21014) 
+        and dtmm.start_date > 2459027 
         group by user_id";
 $result = mysql_query( $sql );
 while ( $row = mysql_fetch_assoc( $result ) ) {
     $marks[] = $row;
 }
 
-$year = 5780;
-$campaign = 15;
+$year = 5781;
+$campaign = 17;
 $qrys = [];
 foreach ( $marks as $mark ) {
     $exists_query = mysql_query("SELECT mission_sheet_amount AS t FROM lines_learned WHERE campaign_id = " . $campaign . " AND user_id = " . $mark['user_id']);
