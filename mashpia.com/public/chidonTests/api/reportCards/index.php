89d7f5f6060e3f1355b47879360f375e6efa8964
naftoli rapoport <naftoli@tzivoshashem.org>
2021-01-07 19:35:20 -0500

    fix marks in api for report cards

--- mashpia.com/public/chidonTests/api/reportCards/index.php
+++ mashpia.com/public/chidonTests/api/reportCards/index.php
@@ -31,8 +31,8 @@ foreach ($info as $school => $users) {
         else $questions = 10;
         $tests = [];
         for ($i = 1; $i <= 4; $i++) {
-            $test['mivtzahMaven'] = number_format(floatval(($marks[$id][$i]['maven'] / $questions) * 100), 2);
-            $test['shabbatonMark'] = number_format(floatval(($marks[$id][$i][$test_type] / $questions) * 100), 2);
+            $test['mivtzahMaven'] = $marks[$id][$i]['maven'];
+            $test['shabbatonMark'] = $marks[$id][$i][$test_type];
             $tests[] = $test;
         }
         $result[] = [
