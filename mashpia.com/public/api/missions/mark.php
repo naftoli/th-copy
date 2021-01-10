a8ee2af9f881b4f6f944c165acf8dd0e5f9fbb49
naftoli rapoport <naftoli@tzivoshashem.org>
2021-01-10 15:07:05 -0500

    fix conflict in teacher marking

--- mashpia.com/public/api/missions/mark.php
+++ mashpia.com/public/api/missions/mark.php
@@ -7,6 +7,7 @@ require_once( API_ROOT . '/../mission_report/classes/missions.php' );
 require_once( API_ROOT . '/../yearly_prize/classes/TotalWeeklyTasks.php' );
 require_once( API_ROOT . '/../classes/medal_updater.php' );
 require_once( API_ROOT . '/../classes/rank_updater.php' );
+require_once( API_ROOT . '/../class.globalSettings.php' );
 
 class MarkRouter {
 
@@ -61,9 +62,9 @@ class MarkRouter {
                 // update the task
                 $this->updateTask( $user_task, $mark, $mark_date );
                 // update the mission
-                $this->updateMission( 
-                    $user_task['user_id'], 
-                    $user_task['date_tasks_mission_id'], 
+                $this->updateMission(
+                    $user_task['user_id'],
+                    $user_task['date_tasks_mission_id'],
                     $mark_date
                 );
                 // update the cache as each mission is marked
@@ -72,9 +73,6 @@ class MarkRouter {
                 // if it's a tanya mark or mishna mark update the yud alef nissan tables
                 $gridIds = [21001,21002,21003,21004,21005,21006,21007,21008,21013,21014];
                 if (in_array($grid_id, $gridIds)) {
-
-                    require_once $_SERVER['DOCUMENT_ROOT'] . '/db.php';
-                    require_once  $_SERVER['DOCUMENT_ROOT'] . 'class.globalSettings.php';
                     $year = GlobalSettings::getChidonYear();
 
                     // get campaigns for current year
@@ -85,10 +83,9 @@ class MarkRouter {
                         else if (strtolower($row['type']) == 'mishna') $mishnaCampaign = $row['id'];
                     }
 
-                    $campaign = $tanyaCampaign;
-                    if ($grid_id % 2 == 0) $campaign = $mishnaCampaign; // mishna campaigns are even numbers
-
-                    $exists_query = mysql_query("SELECT mission_sheet_amount AS t FROM lines_learned WHERE campaign_id = " . $campaign . " AND user_id = " . $user_id);
+                    $campaign = $grid_id % 2 == 0 ? $mishnaCampaign : $tanyaCampaign; // mishna campaigns are even numbers
+                    $campaign_qry = "SELECT mission_sheet_amount AS t FROM lines_learned WHERE campaign_id = " . $campaign . " AND user_id = " . $user_id;
+                    $exists_query = mysql_query($campaign_qry);
                     if (mysql_num_rows($exists_query) > 0) {
                         if ( $mark > 0 ) {
                             $update_sql = "UPDATE lines_learned"
