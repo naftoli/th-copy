f35250d63bc67bd37f7d295bbf6eb4536dee4ff2
naftoli rapoport <naftoli@tzivoshashem.org>
2021-01-25 16:36:28 -0500

    fix rank ceremony by grade code

--- mashpia.com/public/class.rankReport.php
+++ mashpia.com/public/class.rankReport.php
@@ -89,7 +89,7 @@ class RankReport extends Report {
             else if ( $orderType == 'byGradeRank')
                 $this->ranks[$school][$teacher][$grade][$rank][] = $user_id;
             else if ( $orderType == 'byGradeOnlyRank')
-                $this->ranks[$school][$teacher][$row['class_grade']][$rank][] = $user_id;
+                $this->ranks[$school][$row['class_grade']][$rank][] = $user_id;
             else if ( $orderType == 'byRank' )
                 $this->ranks[$school][$rank][$teacher][$grade][] = $user_id;
             else if ( $orderType == 'byRankFirst' )
