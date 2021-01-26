39794f1840fbbce786afb6ab4e44f6264f5438a9
naftoli rapoport <naftoli@tzivoshashem.org>
2021-01-26 16:35:25 -0500

    show hachayol report by sorted by totals

--- mashpia.com/public/hachayol_report.php
+++ mashpia.com/public/hachayol_report.php
@@ -72,46 +72,47 @@ $posters = array(
 //        9   => 400,
 //        265 => 70
 //    );
-
+    // order schools by total
+    $orderedSchools = [];
+    foreach ($schools as $type => $info) {
+        foreach ($info as $id => $school) {
+            if ($id != 162) $total = $school['total'] + $school['teachers'] + get_extra_hachayols($id, $school['total'] + $school['teachers']);
+            else $total = $school['total'] + get_extra_hachayols($id, $school['total']);
+            $orderedSchools[$total][$type][] = $school;
+        }
+    }
+    // sort by total
+    krsort($orderedSchools);
+//    echo "<pre>"; print_r($orderedSchools); echo "</pre>";
 ?>
     <div align='center' class='hide'>
         <input type='button' value='Print' onclick='window.print();' />
     </div>
     <?php
-    if ( isset( $schools['pickup'] ) ) {
-        foreach( $schools['pickup'] as $id => $school ) { ?>
-            <?php $chidonNum = $h->getChidonNumber( $id ); ?>
-            <h2>For Pickup</h2>
-            <div class='info'>
-                <?=$school['shipping_name']?><br />
-                <?=$school['name']?><br />
-                <?=$school['address']?><br /><br />
-                Type of School: <?=$school['type']?><br />
-                
-                Principal: <?=$school['principal']?><br />
-                <?php
+    foreach ($orderedSchools as $total => $more) {
+        $grandTotal += $total;
+        foreach ($more as $type => $other) {
+            $totals[$type] += $total;
+            foreach ($other as $school) {
+                $chidonNum = $h->getChidonNumber( $id );
+                if ($type == 'pickup') echo "<h2>For Pickup</h2>";
+                else if ($type == 'deliver') echo "<h2>For Delivery</h2>";
+                echo "<div class='info'>";
+                echo $school['shipping_name'] . "<br />";
+                echo $school['name'] . "<br />";
+                echo $school['address'] . "<br />";
+                echo "Type of school: " . $school['type'] . "<br />";
+                echo "Principal: " . $school['principal'] . "<br />";
                 foreach( $school['admins'] as $admin ) {
                     $admin = trim($admin);
                     if (!empty($admin)) echo "Admin: " . $admin . "<br />";
-                } ?>
-                Total Teachers: <?=$school['teachers']?><br />
-                Total Registered children: <?=$school['total']?><br />
-                
-                <?php
-                if ($id === 162) { // Bais Chaya Mushka LA does not want chayoleis for teachers....
-                    $extra = get_extra_hachayols($id, $school['total']);
-                    $total = $school['total'] + $extra;
-                } else { // for all other schools add the extras to the total
-                    $extra = get_extra_hachayols($id, $school['teachers'] + $school['total']);
-                    $total = $school['teachers'] + $school['total'] + $extra; 
-                    // find out if total should be overrided
-//                    if ( isset( $exceptions[$id] ) ) $total = $exceptions[$id];
                 }
-
-                if ( $extra !== 0 ) { ?>
-                Extra: <?=$extra?><br />
-                <?php } ?>
-
+                echo "Total Teachers: " . $school['teachers'] . "<br />";
+                echo "Total Registered children: " . $school['total'] . "<br />";
+                if ($id == 162) $extra = get_extra_hachayols($id, $school['total']);
+                else $extra = get_extra_hachayols($id, $school['teachers'] + $school['total']);
+                if ($extra != 0) echo "Extra: " . $extra . "<br />";
+                ?>
                 <span style="font-size: larger; font-weight: bold;">Total: <?=$total?></span><br />
                 Already Registered for Chidon: <?=$school['chidonReg']?><br />
                 Number of posters: <?=$posters[$id] ?? 0?><br />
@@ -119,65 +120,113 @@ $posters = array(
                 Number of girls posters: <?=$school['chidon_posters_girls'] ?? 0?><br />
                 Possible Chidon Children: <?=$chidonNum?><br />
                 Shipping Requests: <?=$school['shipping_requests']?><br /><br />
+                </div>
+                <div class='page-break'></div>
                 <?php
-                $grandTotal += $total;
-                $totals['pickup'] += $total;
-                ?>
-            </div>
-            <div class='page-break'></div>
-        <?php
+            }
         }
     }
-    
-    if ( isset( $schools['deliver'] ) ) {
-        foreach ( $schools['deliver'] as $id => $school ) { ?>
-            <?php $chidonNum = $h->getChidonNumber( $id ); ?>
-            <h2>For Delivery</h2>
-            <div class='info'>
-                <?=$school['shipping_name']?><br />
-                <?=$school['name'];?><br />
-                <?=$school['address'];?><br /><br />
-                Type of School: <?=$school['type']?><br />
-                <?php
-                foreach( $school['admins'] as $admin ) {
-                    $admin = trim($admin);
-                    if (!empty($admin)) echo "Admin: " . $admin . "<br />";
-                } ?>
-                Total Teachers: <?=$school['teachers']?><br />
-                Total Registered children: <?=$school['total']?><br />
-                <?php
-                if ( in_array($id,  [54, 265] ) ) { // Beis Rivkah only wants the total specified in this file.
-                    $extra = $total = get_extra_hachayols($id);
-                } else if ($id != 162) { // for all other schools add the extras to the total
-                    $extra = get_extra_hachayols($id, $school['teachers'] + $school['total']);
-                    $total = $school['teachers'] + $school['total'] + $extra; 
-                    // find out if total should be overrided
-//                    if ( isset( $exceptions[$id] ) ) $total = $exceptions[$id];
-                } else {
-                    $extra = get_extra_hachayols($id, $school['total']);
-                    $total = $school['total'] + $extra;
-                }
-
-                if ( $extra !== 0 ) { ?>
-                    Extra: <?=$extra?><br />
-                <?php } ?>
-
-                <span style="font-size: larger; font-weight: bold;">Total: <?=$total?></span><br />
-                Already Registered for Chidon: <?=$school['chidonReg']?><br />
-                Number of posters: <?=$posters[$id] ?? 0?><br />
-                Number of boys posters: <?=$school['chidon_posters_boys'] ?? 0?><br />
-                Number of girls posters: <?=$school['chidon_posters_girls'] ?? 0?><br />
-                Possible Chidon Children: <?=$chidonNum?><br />
-                Shipping Requests: <?=$school['shipping_requests']?><br /><br />
-                <?php
-                $grandTotal += $total;
-                $totals['deliver'] += $total;
-                ?>
-            </div>
-            <div class='page-break'></div>
-        <?php
-        }
-    } ?>
+    ?>
+//    if ( isset( $schools['pickup'] ) ) {
+//        foreach( $schools['pickup'] as $id => $school ) { ?>
+<!--            --><?php //$chidonNum = $h->getChidonNumber( $id ); ?>
+<!--            <h2>For Pickup</h2>-->
+<!--            <div class='info'>-->
+<!--                --><?//=$school['shipping_name']?><!--<br />-->
+<!--                --><?//=$school['name']?><!--<br />-->
+<!--                --><?//=$school['address']?><!--<br /><br />-->
+<!--                Type of School: --><?//=$school['type']?><!--<br />-->
+<!--                -->
+<!--                Principal: --><?//=$school['principal']?><!--<br />-->
+<!--                --><?php
+//                foreach( $school['admins'] as $admin ) {
+//                    $admin = trim($admin);
+//                    if (!empty($admin)) echo "Admin: " . $admin . "<br />";
+//                } ?>
+<!--                Total Teachers: --><?//=$school['teachers']?><!--<br />-->
+<!--                Total Registered children: --><?//=$school['total']?><!--<br />-->
+<!--                -->
+<!--                --><?php
+//                if ($id === 162) { // Bais Chaya Mushka LA does not want chayoleis for teachers....
+//                    $extra = get_extra_hachayols($id, $school['total']);
+//                    $total = $school['total'] + $extra;
+//                } else { // for all other schools add the extras to the total
+//                    $extra = get_extra_hachayols($id, $school['teachers'] + $school['total']);
+//                    $total = $school['teachers'] + $school['total'] + $extra;
+//                    // find out if total should be overrided
+////                    if ( isset( $exceptions[$id] ) ) $total = $exceptions[$id];
+//                }
+//
+//                if ( $extra !== 0 ) { ?>
+<!--                Extra: --><?//=$extra?><!--<br />-->
+<!--                --><?php //} ?>
+<!---->
+<!--                <span style="font-size: larger; font-weight: bold;">Total: --><?//=$total?><!--</span><br />-->
+<!--                Already Registered for Chidon: --><?//=$school['chidonReg']?><!--<br />-->
+<!--                Number of posters: --><?//=$posters[$id] ?? 0?><!--<br />-->
+<!--                Number of boys posters: --><?//=$school['chidon_posters_boys'] ?? 0?><!--<br />-->
+<!--                Number of girls posters: --><?//=$school['chidon_posters_girls'] ?? 0?><!--<br />-->
+<!--                Possible Chidon Children: --><?//=$chidonNum?><!--<br />-->
+<!--                Shipping Requests: --><?//=$school['shipping_requests']?><!--<br /><br />-->
+<!--                --><?php
+//                $grandTotal += $total;
+//                $totals['pickup'] += $total;
+//                ?>
+<!--            </div>-->
+<!--            <div class='page-break'></div>-->
+<!--        --><?php
+//        }
+//    }
+//
+//    if ( isset( $schools['deliver'] ) ) {
+//        foreach ( $schools['deliver'] as $id => $school ) { ?>
+<!--            --><?php //$chidonNum = $h->getChidonNumber( $id ); ?>
+<!--            <h2>For Delivery</h2>-->
+<!--            <div class='info'>-->
+<!--                --><?//=$school['shipping_name']?><!--<br />-->
+<!--                --><?//=$school['name'];?><!--<br />-->
+<!--                --><?//=$school['address'];?><!--<br /><br />-->
+<!--                Type of School: --><?//=$school['type']?><!--<br />-->
+<!--                --><?php
+//                foreach( $school['admins'] as $admin ) {
+//                    $admin = trim($admin);
+//                    if (!empty($admin)) echo "Admin: " . $admin . "<br />";
+//                } ?>
+<!--                Total Teachers: --><?//=$school['teachers']?><!--<br />-->
+<!--                Total Registered children: --><?//=$school['total']?><!--<br />-->
+<!--                --><?php
+//                if ( in_array($id,  [54, 265] ) ) { // Beis Rivkah only wants the total specified in this file.
+//                    $extra = $total = get_extra_hachayols($id);
+//                } else if ($id != 162) { // for all other schools add the extras to the total
+//                    $extra = get_extra_hachayols($id, $school['teachers'] + $school['total']);
+//                    $total = $school['teachers'] + $school['total'] + $extra;
+//                    // find out if total should be overrided
+////                    if ( isset( $exceptions[$id] ) ) $total = $exceptions[$id];
+//                } else {
+//                    $extra = get_extra_hachayols($id, $school['total']);
+//                    $total = $school['total'] + $extra;
+//                }
+//
+//                if ( $extra !== 0 ) { ?>
+<!--                    Extra: --><?//=$extra?><!--<br />-->
+<!--                --><?php //} ?>
+<!---->
+<!--                <span style="font-size: larger; font-weight: bold;">Total: --><?//=$total?><!--</span><br />-->
+<!--                Already Registered for Chidon: --><?//=$school['chidonReg']?><!--<br />-->
+<!--                Number of posters: --><?//=$posters[$id] ?? 0?><!--<br />-->
+<!--                Number of boys posters: --><?//=$school['chidon_posters_boys'] ?? 0?><!--<br />-->
+<!--                Number of girls posters: --><?//=$school['chidon_posters_girls'] ?? 0?><!--<br />-->
+<!--                Possible Chidon Children: --><?//=$chidonNum?><!--<br />-->
+<!--                Shipping Requests: --><?//=$school['shipping_requests']?><!--<br /><br />-->
+<!--                --><?php
+//                $grandTotal += $total;
+//                $totals['deliver'] += $total;
+//                ?>
+<!--            </div>-->
+<!--            <div class='page-break'></div>-->
+<!--        --><?php
+//        }
+//    } ?>
 
     <h2>For Pickup</h2>
     <div class='info'>
