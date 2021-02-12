50adb987502b14d39c18689bda8c290e12010098
naftoli rapoport <naftoli@tzivoshashem.org>
2021-02-12 13:57:55 -0500

    add backs of medals for day school daily subjects / tasks

--- mashpia.com/public/mobile/reg/ajax/getMedalInfo.php
+++ mashpia.com/public/mobile/reg/ajax/getMedalInfo.php
@@ -136,6 +136,15 @@ if ( isset( $_GET['v'] ) && $_GET['v'] == 2) {
         // 585 total missions
         else if ( in_array( $subject_id, [ 40, 94 ] ) )
             $row['photo'] = 'images/backs/yd/'.$medal_name.'.gif';
+        // day schools subjects
+        else if ( $subject_id > 120 && subject_id <= 135 ) {
+            if ( !in_array( $subject_id, [ 126, 127, 135 ] ) ) {
+                // daily subjects / tasks
+                $row['photo'] = 'images/backs/daySchools/daily' . $medal_name . '.png';
+            } else {
+                $row['photo'] = '';
+            }
+        }
         // 375 missions
         else
             $row['photo'] = 'images/backs/weekly/'.$medal_name.'.gif';
