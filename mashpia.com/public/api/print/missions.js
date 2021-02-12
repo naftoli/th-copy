80cbe11f2cc0fe11bcef222451810e3a92de8586
naftoli rapoport <naftoli@tzivoshashem.org>
2021-02-11 19:03:05 -0500

    add day school stickers to bottom of mission sheet

--- mashpia.com/public/api/print/missions.js
+++ mashpia.com/public/api/print/missions.js
@@ -25,7 +25,20 @@ $(".userMission").each( function() {
                 42	: 'Vihalachta Bidrachov.gif',
                 45	: 'Cheshbon Hanefesh.gif',
                 90	: 'Chitas.gif',
-                100	: 'Sticker - Brias Haguf_outline bw.png'
+                100	: 'Sticker - Brias Haguf_outline bw.png',
+                121 :  "day-school-Jewish Day 250 px.svg",
+                122 :  "day-school-Jewish Uniform 250px.svg",
+                124 :  "day-school-Health 250px.svg",
+                125 :  "day-school_Torah 250px.svg",
+                126 :  "day-school_Shabbat 250px.svg",
+                127 :  "day-school_Special Days 250px.svg",
+                129 :  "day-school-Kosher 250px.svg",
+                130 :  "day-school_Tefilla.svg",
+                131 :  "day-school-ahavat-yisrael.svg",
+                132 :  "day-school-brachot.svg",
+                133 :  "day-school-Tzedaka.svg",
+                134 :  "day-school-honoring parents 250px.svg",
+                135 :  "day-school-Middot-icon.svg"
             }
             
             var str = "<div class='finalFooter'>";
@@ -37,7 +50,10 @@ $(".userMission").each( function() {
                     //build footer info
                     if (j++ == 0) { //first get sticker info
                         // make sure subject has a sticker image
-                        if (i in s) str += "<img src='/mission_report/stickerOutlines/" + s[i] + "' /><br /><b>" + indx + "</b><br />";
+                        if (i in s) {
+                            if (i < 120) str += "<img src='/mission_report/stickerOutlines/" + s[i] + "' /><br /><b>" + indx + "</b><br />";
+                            else str += "<img src='/mission_report/campaignLogos/" + s[i] + "' /><br /><b>" + indx + "</b><br />";
+                        }
                         else str += "<img src='' /><br /><b>" + indx + "</b><br />";
                     } else { //then get medal info
                         str += "<i>" + value + " to " + indx + "</i>";
