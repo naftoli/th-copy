0262a86e7a833350f029be668a297b16fb9ee7f5
naftoli rapoport <naftoli@tzivoshashem.org>
2021-03-02 13:57:07 -0500

    rep report by grade

--- mashpia.com/public/chidonTests/class.chidonTests.php
+++ mashpia.com/public/chidonTests/class.chidonTests.php
@@ -47,6 +47,7 @@ class ChidonTests
         $qry = "
             SELECT 
                 tc.th_chidon_id, tc.user_id, tc.test_type, tc.parent_id, tc.khk_rep, tc.school_rep, tc.school_rep_old, 
+                tc.khk_test_1, tc.khk_test_2, tc.khk_test_3, tc.khk_test_4, 
                 u.first, u.last, u.gender, 
                 c.class_id, c.class_grade, c.class_sub,
                 s.school_id, s.school_name, a.admin_email
