f7d73600b45437c69122d068fb4411dfa6db274a
naftoli rapoport <naftoli@tzivoshashem.org>
2021-03-10 13:44:07 -0500

    fix paying with cc

--- mashpia.com/public/chidonOld/chidon_drive/ajax/processPaymentNew.php
+++ mashpia.com/public/chidonOld/chidon_drive/ajax/processPaymentNew.php
@@ -1,6 +1,6 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('error_reporting', E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('error_reporting', E_ALL);
 
 require $_SERVER['DOCUMENT_ROOT'] . '/db.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/api/header/db.php';
