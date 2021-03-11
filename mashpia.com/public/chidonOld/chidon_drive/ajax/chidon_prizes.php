98e86719d531a83f911594ab276b28b27767b322
naftoli rapoport <naftoli@tzivoshashem.org>
2021-03-10 22:01:29 -0500

    fix issues with quesry for getting prizes

--- mashpia.com/public/chidonOld/chidon_drive/ajax/chidon_prizes.php
+++ mashpia.com/public/chidonOld/chidon_drive/ajax/chidon_prizes.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../../../class.globalSettings.php';
 $year = GlobalSettings::getChidonYear();
 
 $prizes = [];
-$sql = "select * from chidon_prizes where year = " . $year . " and (quantity - purchased > 0) order by prize_name, size, color";
+$sql = "select * from chidon_prizes where year = " . $year . " and quantity > purchased order by prize_name, size, color";
 $result = mysql_query($sql);
 while ($row = mysql_fetch_assoc($result)) {
     $prizes[$row['prize_id']] = $row;
