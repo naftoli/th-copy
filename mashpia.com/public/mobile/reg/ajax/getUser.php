40c3b1ffee762e62dd512d0094e0d6316a0d395a
Arye Eidelman <aryedov.e@gmail.com>
2021-03-11 15:35:43 -0500

    Ensure that the parent is logged in when editing a childs settings

--- mashpia.com/public/mobile/reg/ajax/getUser.php
+++ mashpia.com/public/mobile/reg/ajax/getUser.php
@@ -1,10 +1,16 @@
 <?php
 require '../../../db.php';
+require 'encrypt.php';
 
 $user = mysql_real_escape_string( $_POST['user'] );
+$admin_id = mysql_real_escape_string( $_COOKIE['admin'] );
+$admin_id = encrypt_decrypt('decrypt', $admin_id);
+
 $sql = "select * from users u 
 		left join thumbs t on t.file_id = u.user_photo_id 
-		where u.user_id = " . $user;
+		join admin_auths aa on aa.id = u.user_id && aa.auth = 'user'
+		where u.user_id = " . $user ."
+		and aa.admin_id = " . $admin_id;
 $result = mysql_query( $sql );
 if ( mysql_num_rows($result) > 0 ) {
 	$row = mysql_fetch_assoc($result);
