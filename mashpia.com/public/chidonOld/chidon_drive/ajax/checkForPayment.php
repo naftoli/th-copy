4a82ecddaca23e3f70cd57953a721a0236db928f
naftoli rapoport <naftoli@tzivoshashem.org>
2021-03-11 22:36:05 -0500

    only delete prizes that haven't been sold out for reregistering parents

---
+++ mashpia.com/public/chidonOld/chidon_drive/ajax/checkForPayment.php
@@ -0,0 +1,28 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL);
+
+require __DIR__ . '/../../../db.php';
+require __DIR__ . '/../../../class.globalSettings.php';
+require __DIR__ . '/../encrypt.php';
+
+$year = GlobalSettings::getChidonYear();
+$admin = mysql_real_escape_string( $_POST['admin'] );
+$admin_id = encrypt_decrypt('decrypt', $admin);
+
+$sql = "select * from th_chidon_parent_purchases where authorize_id > 1 and admin_id = " . $admin_id;
+$result = mysql_query($sql);
+if (mysql_num_rows($result) > 0) {
+    $row = mysql_fetch_assoc($result);
+    echo json_encode([
+        'success'   => true,
+        'payment'   => [
+            'id'    => $row['th_chidon_parent_purchase_id'],
+            'amount'    => $row['amount']
+        ]
+    ]);
+    exit;
+}
+echo json_encode([
+    'success' => false
+]);
