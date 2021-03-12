0d63fe9719a17c5c30847fda4981f4db2fcf95ce
naftoli rapoport <naftoli@tzivoshashem.org>
2021-03-11 22:47:59 -0500

    get payment already paid by admin

--- mashpia.com/public/chidonOld/chidon_drive/ajax/checkPayment.php
+++ mashpia.com/public/chidonOld/chidon_drive/ajax/checkPayment.php
@@ -1,6 +1,6 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('error_reporting', E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('error_reporting', E_ALL);
 
 require __DIR__ . '/../../../db.php';
 require __DIR__ . '/../../../class.globalSettings.php';
