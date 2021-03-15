1d230afb08efb12f04836a2c284d616bb5fbcc09
naftoli rapoport <naftoli@tzivoshashem.org>
2021-03-15 13:48:04 -0400

    fix issue coming from check payment

---
+++ mashpia.com/public/chidonOld/chidon_drive/ajax/processHolds.php
@@ -0,0 +1,52 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL);
+
+require __DIR__ . '/../../../db.php';
+
+//***************** LOAD CURRENT YEAR **********************/
+require_once __DIR__ . '/../../../class.globalSettings.php';
+$year = GlobalSettings::getChidonYear();
+
+require 'authorize.php';
+
+// find parents with holds and their trans id numbers
+$parents = [];
+$sql = "SELECT 
+            admin_id, authorize_id, amount
+        FROM
+            th_chidon_parent_purchases
+        WHERE
+            authorize_trans_type = 'hold'
+                AND authorize_id > 1";
+$result = mysql_query($sql);
+while ($row = mysql_fetch_assoc($result)) {
+    $parents[] = $row;
+}
+
+foreach ($parents as $idx => $parent) {
+    $sql = "SELECT 
+                SUM(donation_amount) as total 
+            FROM
+                mashpiadb.chidon_donations
+            WHERE
+                for_family_id = " . $parent['admin_id'] . " AND chidon_year = " . $year;
+    $result = mysql_query($sql);
+    if (mysql_num_rows($result) > 0) {
+        $row = mysql_fetch_assoc($result);
+        $total = $row['total'];
+        $parents[$idx]['raised'] = $total;
+    }
+}
+
+foreach ($parents as $idx => $parent) {
+    echo $idx + 1 . ") ";
+    // figure out how much to charge
+    $charge = floatval($parent['amount']) - floatval($parent['raised']);
+    if ($charge <= 0) {
+        echo "Parent ID: " . $parent['admin_id'] . " raised more than they were charged. 
+            Raised: " . $parent['raised'] . " On Hold: " . $parent['amount'] . "<br />";
+        continue; // don't charge
+    }
+    echo "Parent ID: " . $parent['admin_id'] . " will be charged: " . $charge . "<br />";
+}
