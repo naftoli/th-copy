5923209eadc61f6dc39d2ebcf2c08dc6b0bd5c8c
naftoli rapoport <naftoli@tzivoshashem.org>
2021-04-16 15:00:43 -0400

    sweater summary; confirmation changes

--- mashpia.com/public/mobile/chidon/ajax/confirmation.php
+++ mashpia.com/public/mobile/chidon/ajax/confirmation.php
@@ -3,7 +3,8 @@ require $_SERVER['DOCUMENT_ROOT'] . '/db.php';
 require 'encrypt.php';
 
 $admin_id = encrypt_decrypt('decrypt', $_POST['admin']);
-$sql = "update admins set confirmed_chidon_5781 = 1 where admin_id = " . $admin_id;
+$field = $_POST['field'];
+$sql = "update admins set " . $field . " = 1 where admin_id = " . $admin_id;
 if (mysql_query($sql)) {
     echo json_encode([
         'success'   => true
