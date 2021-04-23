6cc887724d65a57c449049b2d04cbcd60d792cd9
naftoli rapoport <naftoli@tzivoshashem.org>
2021-04-23 14:16:34 -0400

    fix report for myshliach/anash kinder sweaters and celeb boxes

--- mashpia.com/public/chidonOld/chidon_drive/newReports/myshliachSweaters.php
+++ mashpia.com/public/chidonOld/chidon_drive/newReports/myshliachSweaters.php
@@ -80,9 +80,11 @@ foreach ($purchases as $admin => $details) {
         <meta charset="utf8" />
         <title>MyShliach Sweater Report</title>
         <style>
-            font-family: 'Arial, Helvetica, sans-serif';
-            font-size: 14px;
-            padding: 5px;
+            tr, th, td {
+                font-family: 'Arial, Helvetica, sans-serif';
+                font-size: 14px;
+                padding: 5px;
+            }
         </style>
     </head>
     <body>
