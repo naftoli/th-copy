f2524c45bcadbce048904287170d832739f410d6
naftoli rapoport <naftoli@tzivoshashem.org>
2021-04-26 15:00:28 -0400

    upload reg info

---
+++ mashpia.com/public/mobile/chidon/uploadRegInfo.php
@@ -0,0 +1,60 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL);
+
+$admin_auth = ['school'];
+require $_SERVER['DOCUMENT_ROOT'] . '/header.php';
+
+if ($admin_user['auth'] != 'super') {
+    echo "No Permission.";
+    exit;
+}
+
+if (isset($_POST['submit'])) {
+    $file = $_FILES['regInfo']['tmp_name'];
+    if (move_uploaded_file($file, "regInfo.csv")) {
+        if (($handle = fopen('regInfo.csv', "r")) !== FALSE) {
+            $qrys = [];
+            $i = 0;
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                $admin_id = $data[$i++];
+                $chidon_id = $data[$i++];
+                $reg_fee = $data[$i++];
+                $chidon_drive = $data[$i++];
+                $subsidy = $data[$i++];
+                $coupon = $data[$i++];
+                $coupon_reason = $data[$i++];
+                $paid = $data[$i++];
+                $balance = $data[$i++];
+                $qry = "insert into th_chidon_zelda 
+                        set th_chidon_id = $chidon_id, 
+                        admin_id = $admin_id, 
+                        reg_fee = $reg_fee, 
+                        chidon_drive = $chidon_drive, 
+                        subsidy = $subsidy, 
+                        coupon = '$coupon', 
+                        coupon_readon = '$coupon_reason', 
+                        paid = $paid, 
+                        balance = $balance";
+                echo $qry;
+                $qrys[] = $qry;
+            }
+            fclose($handle);
+        }
+    } else {
+        echo "Error saving file.";
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Upload Reg Info</title>
+    </head>
+    <body>
+        <form action="" method="post" enctype="multipart/form-data">
+            Select file to upload: <input type="file" name="regInfo" />
+            <br /><input type="submit" name="submit" value="upload" />
+        </form>
+    </body>
+</html>
