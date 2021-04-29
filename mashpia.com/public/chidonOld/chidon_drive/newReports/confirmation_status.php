5384375db5d1097ade1c6c588f5cf0b743251c31
naftoli rapoport <naftoli@tzivoshashem.org>
2021-04-29 15:35:34 -0400

    update confirmation pages

--- mashpia.com/public/chidonOld/chidon_drive/newReports/confirmation_status.php
+++ mashpia.com/public/chidonOld/chidon_drive/newReports/confirmation_status.php
@@ -9,19 +9,20 @@ require $_SERVER['DOCUMENT_ROOT'] . '/class.adminSchools.php';
 $as = new AdminSchools($admin_user['admin_id'], $admin_user['auth']);
 $schools = $as->getSchools();
 
-$admins = [];
-$sql = "select a.* from admins a 
-        join admin_auths aa using (admin_id) 
-        join users u on aa.id = u.user_id 
-        join th_chidon tc on tc.user_id = u.user_id 
+$info = [];
+$sql = "select * from th_chidon tc 
+        join users u on tc.user_id = u.user_id 
+        join classes c on c.class_id = u.class_id 
+        join schools s on s.school_id = u.school_id 
+        join th_chidon_zelda tcz on tc.parent_id = tcz.admin_id 
         where u.school_id in (
         " . implode(',', array_keys($schools)) . ") 
         and tc.year = 5781 
-        and (tc.shabbaton_maven = 1 or tc.shabbaton_pro = 1 or tc.shabbaton_expert = 1 or tc.shabbaton_trophy = 1)
-        group by a.admin_id";
+        and (tc.shabbaton_maven = 1 or tc.shabbaton_pro = 1 or tc.shabbaton_expert = 1 or tc.shabbaton_trophy = 1) 
+        group by u.user_id";
 $result = mysql_query($sql);
 while ($row = mysql_fetch_assoc($result)) {
-    $admins[] = $row;
+    $info[$row['school_id']][] = $row;
 }
 
 function convertBool($val) {
@@ -31,6 +32,8 @@ function convertBool($val) {
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="utf8" />
+        <link href="../../../admin_styles.css" rel="stylesheet" type="text/css"/>
         <style>
             tr, th, td {
                 font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
@@ -41,27 +44,37 @@ function convertBool($val) {
         </style>
     </head>
     <body>
-        <table>
-            <tr>
-                <th>Admin ID</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Confirmed Prizes</th>
-                <th>Confirmed Sweaters</th>
-                <th>Registration Balance</th>
-            </tr>
-            <?php
-            foreach ($admins as $admin) {
-                // get registration balance
-                $sql = "select SUM(balance) as total from th_chidon_zelda where admin_id = " . $admin['admin_id'];
-                $result = mysql_query($sql);
-                $row = mysql_fetch_assoc($result);
-                echo "<tr><td>" . $admin['admin_id'] . "</td><td>" . $admin['first'] . ' ' . $admin['last'] . "</td><td>" .
-                    $admin['admin_email'] . "</td><td>" . convertBool($admin['confirmed_prizes']) . "</td><td>" .
-                    convertBool($admin['confirmed_sweaters']) . "</td><td>" . $row['total'] . "</td></tr>";
-//                    "</td><td>" . convertBool($admin['confirmed_reg']) .
-            }
+    <?php
+    include($_SERVER["DOCUMENT_ROOT"].'/admin_header.php');
+
+    foreach ($schools as $school_id => $school_name) {
+        if (isset($info[$school_id])) {
+            echo "<h2>" . $school_name . "</h2>";
             ?>
-        </table>
+            <table>
+                <tr>
+                    <th>Grade</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Confirmed Prizes</th>
+                    <th>Confirmed Sweaters</th>
+                    <th>Registration Balance</th>
+                </tr>
+                <?php
+                foreach ($info[$school_id] as $row) {
+                    $sql = "select confirmed_prizes, confirmed_sweaters from admins where admin_id = " . $row['parent_id'];
+                    $result = mysql_query($sql);
+                    $row2 = mysql_fetch_assoc($result);
+                    $grade = $row['class_grade'] . ($row['class_sub'] ? '-' . $row['class_sub'] : '');
+                    echo "<tr><td>" . $grade . "</td><td>" . $row['first'] . "</td><td>" . $row['last'] . "</td><td>" .
+                        convertBool($row2['confirmed_prizes']) . "</td><td>" . convertBool($row2['confirmed_sweaters']) .
+                        "</td><td>" . $row['balance'] . "</td></tr>";
+                }
+                ?>
+            </table>
+            <?php
+        }
+    }
+    ?>
     </body>
 </html>
