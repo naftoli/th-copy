c82868ddc63e6297af422a911dec489fbbab7c43
naftoli rapoport <naftoli@tzivoshashem.org>
2021-05-05 18:57:57 -0400

    add chidon_id to shipping_report.php

--- mashpia.com/public/chidonOld/chidon_drive/newReports/shipping_report.php
+++ mashpia.com/public/chidonOld/chidon_drive/newReports/shipping_report.php
@@ -220,7 +220,7 @@ $fields = ['sweater_child', 'sweater_mother', 'sweater_father', 'sweater_bubby',
                         $award =  $child['award_type'];
                         if ($award == 'Medal') $award = "Plaque + Medal";
                         $grade = $child['class_grade'] . (empty($child['class_sub']) ? '' : '-' . $child['class_sub']);
-                        echo "<tr><td>" $child['th_chidon_id'] . "</td><td>" . $grade . "</td><td>" . $child['first'] .
+                        echo "<tr><td>" . $child['th_chidon_id'] . "</td><td>" . $grade . "</td><td>" . $child['first'] .
                             "</td><td>" . $child['last'] . "</td><td>";
                         // eligibility
                         if (intval($child['shabbaton_maven'])) echo "Sweater";
