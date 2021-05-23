d4b5614911ea34c8ee8236d6140cb8a6f85462b5
= <naftoli@tzivoshashem.org>
2021-05-23 11:42:47 -0400

    enable reg for chidon 5782 on parent accts

--- mashpia.com/public/mobile/reg/ajax/getChildren.php
+++ mashpia.com/public/mobile/reg/ajax/getChildren.php
@@ -2,7 +2,7 @@
 ini_set('display_errors',1);
 require '../../../db.php';
 require_once( __DIR__ . '/../../../class.globalSettings.php' );
-$chidon_year = isset($_POST['year']) ? $_POST['year'] : GlobalSettings::getChidonYear();
+$chidon_year = isset($_POST['year']) ? $_POST['year'] : GlobalSettings::getChidonRegYear();
 $CHIDON_ACTIVE = true; // change to activate chidon
 
 $admin = mysql_real_escape_string( $_POST['admin'] );
