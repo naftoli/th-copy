bdb0d376dc0c5476c72d379d7f75686f27b4a9d8
= <naftoli@tzivoshashem.org>
2021-05-31 12:43:46 -0400

    revert code in slides

--- mashpia.com/public/medal_ceremony/slides.php
+++ mashpia.com/public/medal_ceremony/slides.php
@@ -34,9 +34,8 @@ $m = new Slides( $prevMedals, $prevMedalsLight );
 // figure out which dates to show
 $m->setDateSelection();
 $end = $_POST['date'];
-$dates = $m->getDates();
-$key = array_search( $end, $dates );
-$start = $dates[$key - 1] + 1;
+$key = array_search( $end, $m->dates );
+$start = $m->dates[$key - 1] + 1;
 $m->overrideDates( $start, $end );
 
 if ( isset( $_POST['currentOnly'] ) ) $m->setToCurrentOnly();
