9769a9f7a63e486774a5688f84dcdc44ae8f17fc
= <naftoli@tzivoshashem.org>
2021-06-30 19:17:13 -0400

    clean up checking coupon code

--- mashpia.com/public/ajax/checkCoupon.php
+++ mashpia.com/public/ajax/checkCoupon.php
@@ -1,30 +1,20 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/api/header/db.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/chidonOld/coupons/class.couponCode.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/class.globalSettings.php';
 
-$coupon = mysql_real_escape_string($_POST['coupon']);
+$year = GlobalSettings::getRegistrationYear();
+$couponCode = new CouponCode($MASHPIA_DB, $year, 'chayolei_base_reg');
+$discount = $couponCode->isValidCode($_POST['coupon']);
 
-if (! $coupon ) {
+if ($discount) {
     echo json_encode([
-        'success'   => false,
-        'error'     => 'Error: Coupon code missing.'
+        'success'   => true,
+        'discount'  => $discount
     ]);
 } else {
-    require_once $_SERVER['DOCUMENT_ROOT'] . '/chidonOld/coupons/class.couponCode.php';
-    require_once $_SERVER['DOCUMENT_ROOT'] . '/class.globalSettings.php';
-
-    $year = GlobalSettings::getRegistrationYear();
-    $couponCode = new CouponCode($MASHPIA_DB, $year, 'chayolei_base_reg');
-    $discount = $couponCode->isValidCode($coupon);
-
-    if ($discount) {
-        echo json_encode([
-            'success'   => true,
-            'discount'  => $discount
-        ]);
-    } else {
-        echo json_encode([
-            'success'   => false,
-            'error'     => 'No such coupon found in the system.'
-        ]);
-    }
+    echo json_encode([
+        'success'   => false,
+        'error'     => 'No such coupon found in the system.'
+    ]);
 }
