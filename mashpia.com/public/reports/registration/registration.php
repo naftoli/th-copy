959cee21a6f2ca6f2872cfc350c9e4d6a00a87b4
= <naftoli@tzivoshashem.org>
2021-07-07 18:31:42 -0400

    run datatable script after all info loads

--- mashpia.com/public/reports/registration/registration.php
+++ mashpia.com/public/reports/registration/registration.php
@@ -189,13 +189,15 @@ foreach ($temp as $row) {
 </body>
 <script type="text/javascript" src="https://cdn.datatables.net/v/bs-3.3.7/jqc-1.12.4/dt-1.10.13/cr-1.3.2/fc-3.2.2/fh-3.1.2/r-2.1.1/sc-1.4.2/se-1.2.0/datatables.min.js"></script>
 <script>
-    $('#table').DataTable({
-        paging : false,
-        "order": [[ 0, 'asc' ]]
-    });
-    $('#details').DataTable({
-        paging : false,
-        "order": [[ 2, 'asc' ]]
-    });
+    $(function() {
+        $('#table').DataTable({
+            paging : false,
+            "order": [[ 0, 'asc' ]]
+        });
+        $('#details').DataTable({
+            paging : false,
+            "order": [[ 2, 'asc' ]]
+        });
+    })
 </script>
 </html>
