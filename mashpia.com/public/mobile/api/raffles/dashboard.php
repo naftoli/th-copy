28ed5c869fdaccf80c30cc517e5b3687ad791e39
= <naftoli@tzivoshashem.org>
2021-07-29 17:06:26 -0400

    add expired key to return object for raffle infographic

--- mashpia.com/public/mobile/api/raffles/dashboard.php
+++ mashpia.com/public/mobile/api/raffles/dashboard.php
@@ -14,14 +14,17 @@ $raffle180 = ($yearly['days_of_tasks'] ?? 180) - checkTasks( $user_id, $yearly['
 echo json_encode([
     'raffle5'   => [
         'daysLeft'  => ($raffle5 > 0 ? $raffle5 : 0),
-        'raffleName'=> $weekly['name']
+        'raffleName'=> $weekly['name'],
+        'expired'   => new DateTime($weekly['run_date']) < new DateTime()
     ],
     'raffle60'  => [
         'daysLeft'  => ($raffle60 > 0 ? $raffle60 : 0),
-        'raffleName'=> $monthly['name']
+        'raffleName'=> $monthly['name'],
+        'expired'   => new DateTime($monthly['run_date']) < new DateTime()
     ],
     'raffle180' => [
         'daysLeft'  => ($raffle180 > 0 ? $raffle180 : 0),
-        'raffleName'=> $yearly['name']
+        'raffleName'=> $yearly['name'],
+        'expired'   => new DateTime($yearly['run_date']) < new DateTime()
     ]
 ]);
