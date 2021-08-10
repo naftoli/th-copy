f572f5ab9f8f96070eb19ce35181e8ae88dbd8be
= <naftoli@tzivoshashem.org>
2021-08-10 16:13:15 -0400

    script to copy chidon prizes from 5781 to 5782

---
+++ mashpia.com/public/chidonOld/prizes/copyPrizes.php
@@ -0,0 +1,36 @@
+<?php
+$admin_auth = ['school'];
+require '/header.php';
+
+if ($admin_auth['auth'] != 'super') {
+    echo "No Permission.";
+    exit;
+}
+
+$prizes = [];
+$sql = "select * from chidon_prizes where year = 5781";
+$result = mysql_query($sql);
+while ($row = mysql_fetch_assoc($result)) {
+    $prizes[] = $row;
+}
+
+$qrys = [];
+foreach ($prizes as $row) {
+    $qrys[] = "insert into chidon_prizes
+                set year = 5782, 
+                prize_picture = '" . $row['prize_picture'] . "', 
+                prize_name = '" . $row['prize_name'] . "'. 
+                quantity = 100, 
+                size = '" . $row['size'] . "', 
+                color = '" . $row['color'] . "', 
+                price = " . $row['price'] . ", 
+                our_price = " . $row['our_price'] . ", 
+                made_possible_by = '" . $row['made_possible_by'] . "', 
+                purchased = 0";
+}
+
+foreach ($qrys as $sql) {
+    mysql_query($sql);
+}
+
+echo "done.";
