41be806275e6ed4c7a2765a9944f530a7aa70379
= <naftoli@tzivoshashem.org>
2021-08-10 16:25:16 -0400

    fix script to copy chidon prizes from 5781 to 5782

--- mashpia.com/public/chidonOld/prizes/copyPrizes.php
+++ mashpia.com/public/chidonOld/prizes/copyPrizes.php
@@ -1,6 +1,7 @@
 <?php
+ini_set('display_errors', 1);
 $admin_auth = ['school'];
-require '/header.php';
+require '../../header.php';
 
 if ($admin_auth['auth'] != 'super') {
     echo "No Permission.";
