5f60675a9180a25211576271c0eae94089022f28
= <naftoli@tzivoshashem.org>
2021-08-15 15:04:35 -0400

    redo try to put report missions on the top of the mission sheets

--- mashpia.com/public/classes/user.php
+++ mashpia.com/public/classes/user.php
@@ -538,7 +538,8 @@ class user {
 
 		// Report Missions task should come first instead of last, so we take the last elem and put it to the front; frequency id = 21
         $last = array_pop($this->sorted_daily_labels);
-		$temp[21] = $last;
+		$idx = key(end($this->sorted_daily_labels));
+		$temp[$idx] = $last;
 		$new_arr = $temp + $this->sorted_daily_labels;
 		$this->sorted_daily_labels = $new_arr;
 //		echo "<pre>"; print_r($this->sorted_daily_labels); echo "</pre>";
