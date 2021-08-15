a68bb1e84d7dc9bf0c467187a3a8821964e5cd47
= <naftoli@tzivoshashem.org>
2021-08-15 12:54:48 -0400

    try to put report missions on the top of the mission sheets

--- mashpia.com/public/classes/date_tasks_mission.php
+++ mashpia.com/public/classes/date_tasks_mission.php
@@ -99,7 +99,8 @@ class date_tasks_mission {
 			$daily_task->set_subject_image_id($subject_image_id);
 			$daily_task->set_dates($start_date, $end_date);
 			$daily_task->set_date_tasks_marks($user_id, $start_date, $end_date);
-			array_push($daily_tasks, $daily_task);
+			if ($daily_task->short_name == 'Report Missions') array_unshift($daily_tasks, $daily_task);
+			else array_push($daily_tasks, $daily_task);
 		}
 		// echo "<pre>" . print_r($daily_tasks) . "</pre>";
 		return $daily_tasks;
