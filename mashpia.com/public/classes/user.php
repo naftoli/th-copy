f966cb4d15e33dbb83e246f5049ad5e8ce27df30
= <naftoli@tzivoshashem.org>
2021-08-16 14:06:18 -0400

    fix making report missions on top for hebrew

--- mashpia.com/public/classes/user.php
+++ mashpia.com/public/classes/user.php
@@ -536,9 +536,10 @@ class user {
 			//echo "<input type='hidden' name='SORTED DAILY LABEL INFO' value='" . $daily_label . "'>\n";
 		}
 
-		// Report Missions task should come first instead of last, so we take the last elem and put it to the front; frequency id = 21; 132
+		// Report Missions task should come first instead of last, so we take the last elem and put it to the front; frequency id = 21/132/133
         $idx = key(end($this->sorted_daily_labels));
-		if (in_array($idx, [21, 132])) {
+		reset($this->sorted_daily_labels);
+		if (in_array($idx, [21, 132, 133])) {
             $last = array_pop($this->sorted_daily_labels);
             $temp[$idx] = $last;
             $new_arr = $temp + $this->sorted_daily_labels;
