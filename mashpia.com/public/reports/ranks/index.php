b2c508cb25d5a521d35a279b10aba196d22ddb49
= <naftoli@tzivoshashem.org>
2021-08-23 14:53:57 -0400

    fix and add link to medal/rank report page

--- mashpia.com/public/reports/ranks/index.php
+++ mashpia.com/public/reports/ranks/index.php
@@ -81,7 +81,7 @@ require_once($_SERVER["DOCUMENT_ROOT"].'/header.php');
                     <span class="link-text">Missing Medals Printout Report</span>
                 </div>
             </a>
-            <a href="medals_summary_report.php">
+            <a href="/medals_summary_report.php">
                 <div class="button">
                     <img src="/images/icon_report.png" height="32" alt="tickets"/>
                     <span class="link-text">Medals Summary Report</span>
