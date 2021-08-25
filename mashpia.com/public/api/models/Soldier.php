da7440211abba780fe3a467cb060daa3510d2b24
= <naftoli@tzivoshashem.org>
2021-08-25 17:23:44 -0400

    remove error reporting

--- mashpia.com/public/api/models/Soldier.php
+++ mashpia.com/public/api/models/Soldier.php
@@ -1,6 +1,6 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('error_reporting', E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('error_reporting', E_ALL);
 
 include_once( __DIR__ . "/../tools/functions/format/parents.php" );
 include_once( __DIR__ . '/../tools/functions/files/images.php' );
