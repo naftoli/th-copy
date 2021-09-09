6b2183218e696dc51fdbff0653b92e411d4d69b3
= <naftoli@tzivoshashem.org>
2021-09-09 12:11:27 -0400

    create chidon possible kids report for shimmy

---
+++ mashpia.com/public/chidonOld/newReports/chidon_possible_report.php
@@ -0,0 +1,72 @@
+<?php
+ini_set('display_errors', 1);
+
+$admin_auth = ['school'];
+require $_SERVER['DOCUMENT_ROOT'] . '/header.php';
+
+if ($admin_user['auth'] != 'super') {
+    echo "No permission to be here.";
+    exit;
+}
+
+$schoolsInfo = [];
+$sql = "select * from schools";
+$result = mysql_query($sql);
+while ($row = mysql_fetch_assoc($result)) {
+    $schoolsInfo[$row['school_id']] = $row;
+}
+
+require $_SERVER['DOCUMENT_ROOT'] . '/class.adminSchools.php';
+$as = new AdminSchools( $admin_user['admin_id'], $admin_user['auth'], true, true ); // add chidon schools
+$schools = $as->getSchools();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf8" />
+        <title>Chidon List Report</title>
+        <style>
+            body  {
+                font-family: Arial, Helvetica, sans-serif;
+                font-size: 16px;
+            }
+            tr, th, td {
+                font-family: Arial, Helvetica, sans-serif;
+                font-size: 12px;
+                padding: 5px;
+            }
+        </style>
+    </head>
+    <body>
+    <?php
+    foreach ($schools as $id => $name) {
+        $children = [];
+        $sql = "select first, last, class_grade, class_sub from users u join classes c using (class_id) where class_grade in ('4', '5', '6', '7', '8') and u.school_id = $id";
+        $result = mysql_query($sql);
+        while ($row = mysql_fetch_assoc($result)) {
+            $children[$row['class_grade']][] = $row;
+        }
+
+        $schoolInfo = $schoolsInfo[$id];
+        if ($schoolInfo['test_school'] == 1) continue;
+        echo "School: " . $name . "<br />";
+        echo "Address: " . $schoolInfo['school_address1'] . " " . $schoolInfo['school_address2'] . "<br />";
+        echo $schoolInfo['school_city'] . ", " . $schoolInfo['school_state'] . " " . $schoolInfo['school_postal'] . "<br />";
+        echo $schoolInfo['school_country'] . "<br />";
+        for ($i = 4; $i <= 8; $i++) {
+            echo "Total for Grade $i: " . (isset($children[$i]) ? count($children[$i]) : 0) . "<br />";
+        }
+        ?>
+<!--        <table>-->
+<!--            <tr>-->
+<!--                <th>Class</th>-->
+<!--                <th>Student</th>-->
+<!--            </tr>-->
+<!--        </table>-->
+        <hr />
+        <div style="page-break-after: always"></div>
+    <?php
+    }
+    ?>
+    </body>
+</html>
