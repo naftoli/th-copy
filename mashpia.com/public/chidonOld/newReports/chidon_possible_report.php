056eb2188a770bf63855ffc2b522e4a8d04deea3
= <naftoli@tzivoshashem.org>
2021-09-09 12:40:44 -0400

    modify chidon possible kids report for shimmy

--- mashpia.com/public/chidonOld/newReports/chidon_possible_report.php
+++ mashpia.com/public/chidonOld/newReports/chidon_possible_report.php
@@ -9,11 +9,13 @@ if ($admin_user['auth'] != 'super') {
     exit;
 }
 
+$schoolOrder = [];
 $schoolsInfo = [];
 $sql = "select * from schools";
 $result = mysql_query($sql);
 while ($row = mysql_fetch_assoc($result)) {
     $schoolsInfo[$row['school_id']] = $row;
+    $schoolOrder[$row['shipping_method']][] = $row['school_id'];
 }
 
 require $_SERVER['DOCUMENT_ROOT'] . '/class.adminSchools.php';
@@ -48,6 +50,7 @@ $schools = $as->getSchools();
         }
 
         $schoolInfo = $schoolsInfo[$id];
+        if ($schoolInfo['shipping_method'] == 'pickup') continue;
         if ($schoolInfo['test_school'] == 1) continue;
         echo "School: " . $name . "<br />";
         echo "Shipping: " . $schoolInfo['shipping_method'] . "<br />";
@@ -62,24 +65,55 @@ $schools = $as->getSchools();
         }
         echo "Grand Total: " . $totals . "<br />";
         ?>
-<!--        <table>-->
-<!--            <tr>-->
-<!--                <th>Grade</th>-->
-<!--                <th>Class</th>-->
-<!--                <th>Student</th>-->
-<!--            </tr>-->
-<!--            --><?php
-//            for ($i = 4; $i <= 8; $i++) {
-//                foreach ($children[$i] as $child) {
-//                    $class = $child['class_grade'] . (empty($child['class_sub']) ? '' : '-' . $child['class_sub']);
-//                    echo "<tr><td>" . $i . "</td><td>" . $class . "</td><td>" . $child['first'] . ' ' . $child['last'] . "</td></tr>";
-//                }
-//            }
-//            ?>
-<!--        </table>-->
         <hr />
         <div style="page-break-after: always"></div>
     <?php
+    }
+    foreach ($schools as $id => $name) {
+        $children = [];
+        $sql = "select first, last, class_grade, class_sub from users u join classes c using (class_id) where class_grade in ('4', '5', '6', '7', '8') and u.school_id = $id";
+        $result = mysql_query($sql);
+        while ($row = mysql_fetch_assoc($result)) {
+            $children[$row['class_grade']][] = $row;
+        }
+
+        $schoolInfo = $schoolsInfo[$id];
+        if ($schoolInfo['shipping_method'] == 'deliver') continue;
+        if ($schoolInfo['test_school'] == 1) continue;
+        echo "School: " . $name . "<br />";
+        echo "Shipping: " . $schoolInfo['shipping_method'] . "<br />";
+        echo "Address: " . $schoolInfo['school_address1'] . " " . $schoolInfo['school_address2'] . "<br />";
+        echo $schoolInfo['school_city'] . ", " . $schoolInfo['school_state'] . " " . $schoolInfo['school_postal'] . "<br />";
+        echo $schoolInfo['school_country'] . "<br />";
+        $totals = 0;
+        for ($i = 4; $i <= 8; $i++) {
+            $total = (isset($children[$i]) ? count($children[$i]) : 0);
+            echo "Total for Grade $i: " . $total . "<br />";
+            $totals += $total;
+        }
+        echo "Grand Total: " . $totals . "<br />";
+        ?>
+        <hr />
+        <div style="page-break-after: always"></div>
+        <?php
+    }
+    foreach ($schools as $id => $name) {
+        $children = [];
+        $sql = "select first, last, class_grade, class_sub from users u join classes c using (class_id) where class_grade in ('4', '5', '6', '7', '8') and u.school_id = $id";
+        $result = mysql_query($sql);
+        while ($row = mysql_fetch_assoc($result)) {
+            $children[$row['class_grade']][] = $row;
+        }
+
+        $schoolInfo = $schoolsInfo[$id];
+        if ($schoolInfo['test_school'] == 1) continue;
+        echo "School: " . $name . "<br />";
+        $totals = 0;
+        for ($i = 4; $i <= 8; $i++) {
+            $total = (isset($children[$i]) ? count($children[$i]) : 0);
+            $totals += $total;
+        }
+        echo "Grand Total: " . $totals . "<br />";
     }
     ?>
     </body>
