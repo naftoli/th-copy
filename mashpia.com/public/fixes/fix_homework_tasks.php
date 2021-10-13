5217a0f08fc2993052b237348876b341b4dcd228
= <naftoli@tzivoshashem.org>
2021-10-12 21:32:14 -0400

    script to fix homework tasks issue

---
+++ mashpia.com/public/fixes/fix_homework_tasks.php
@@ -0,0 +1,26 @@
+<?php
+$admin_auth = ['school'];
+require 'db.php';
+
+if ($admin_user['auth'] != 'super') {
+    echo "No Permission.";
+    exit;
+}
+
+$i = 0;
+$start = 2459581;
+$end = $start + 6;
+do {
+    $sql = "update date_tasks dt 
+            join date_tasks_missions dtm using (date_tasks_mission_id) 
+            set dtm.end_date = $end
+            where dtm.start_date = $start 
+            and dtm.end_date = $start
+            and dt.short_name = 'Homework'";
+    $result = mysql_query($sql);
+    $start = $end + 1;
+    $end = $start + 6;
+    $i++;
+} while (mysql_num_rows($result) > 0);
+
+echo "updated $i weeks.";
