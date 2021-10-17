b8d8ad6fac4002f6a3c02e1d6b2843e49dea062e
= <naftoli@tzivoshashem.org>
2021-10-17 15:52:50 -0400

    fix schools not being able to unconfirm

--- mashpia.com/public/chidonOld/newReports/unconfirm.php
+++ mashpia.com/public/chidonOld/newReports/unconfirm.php
@@ -2,12 +2,7 @@
 $admin_auth = ['school'];
 require_once $_SERVER['DOCUMENT_ROOT'] . '/header.php';
 
-if ($admin_user['auth'] != 'super') {
-    echo "No Permission.";
-    exit;
-}
-
-$user_id = $_POST['user'];
+$user_id = mysql_real_escape_string($_POST['user']);
 
 $sql = "update admins set chidon_confirmed_5782 = 0 where admin_id = (
     select admin_id from admin_auths where auth = 'user' and id = $user_id
