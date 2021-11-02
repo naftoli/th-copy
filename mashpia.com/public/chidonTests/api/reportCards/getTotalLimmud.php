b3528b5703341a51d1034525c9bed62b680357db
= <naftoli@tzivoshashem.org>
2021-11-02 19:42:26 -0400

    fix report card show limmud amounts for each report card

--- mashpia.com/public/chidonTests/api/reportCards/getTotalLimmud.php
+++ mashpia.com/public/chidonTests/api/reportCards/getTotalLimmud.php
@@ -2,14 +2,18 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/db.php';
 
 $sql = "SELECT 
-            IFNULL(SUM(done_qty), 0) as total
+            IFNULL(SUM(done_qty), 0) AS total
         FROM
             date_tasks_marks dtm
                 JOIN
             date_tasks dt USING (date_task_id)
+                JOIN
+            date_tasks_missions dtmm USING (date_tasks_mission_id)
         WHERE
             dt.cat = 'chidon limmud'
-                AND dtm.user_id = " . $_GET['id'];
+                AND dtmm.start_date >= 2459469
+                AND dtmm.end_date <= 2459514
+                AND user_id = " . $_GET['id'];
 $result = mysql_query($sql);
 $total = mysql_fetch_assoc($result)['total'];
 echo $total;
