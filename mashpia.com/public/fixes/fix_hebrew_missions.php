bba924da380482636f1c9877f6d9b9883602af9c
= <naftoli@tzivoshashem.org>
2021-11-03 22:00:13 -0400

    modify script to remove wrong tasks added to hebrew tasks in cheshbon hanefesh

--- mashpia.com/public/fixes/fix_hebrew_missions.php
+++ mashpia.com/public/fixes/fix_hebrew_missions.php
@@ -7,35 +7,56 @@ if ($admin_user['auth'] != 'super') {
     exit;
 }
 
+$types = [2, 3];
+
+$levels = [];
+for ($i = 0; $i < 15; $i++) $levels[] = $i;
+
 $grids = [];
 for ($i = 13001; $i <= 13012; $i++) $grids[] = $i;
 
-$duplicates = [];
+$duplicateMissions = [];
+$duplicateTasks = [];
+
 foreach ($grids as $id) {
-    $sql = "SELECT 
-                date_task_id
-            FROM
-                date_tasks_missions dtm
-                    JOIN
-                date_tasks dt USING (date_tasks_mission_id)
-            WHERE
-                dtm.subject_id = 45
-                    AND dtm.created_by_school IS NULL
-                    AND dtm.created_by_parent IS NULL
-                    AND dtm.lang_id = 4
-                    AND label_id < 50
-                    AND dt.grid_id = " . $id;
-    $result = mysql_query($sql);
-    while ($row = mysql_fetch_assoc($result)) {
-        $duplicates[] = $row['date_task_id'];
+    foreach ($types as $type) {
+        foreach ($levels as $level) {
+            $sql = "SELECT 
+                        date_tasks_mission_id, date_task_id
+                    FROM
+                        date_tasks_missions dtm
+                            JOIN
+                        date_tasks dt USING (date_tasks_mission_id)
+                    WHERE
+                        dtm.created_by_school IS NULL
+                            AND dtm.created_by_parent IS NULL
+                            AND dtm.lang_id = 4
+                            AND created_at < '2021-09-01'
+                            AND dt.grid_id = $id
+                            AND school_type_id = $type
+                            AND level = $level";
+            $result = mysql_query($sql);
+            while ($row = mysql_fetch_assoc($result)) {
+                $duplicateTasks[] = $row['date_task_id'];
+                $duplicateMissions = $row['date_tasks_mission_id'];
+            }
+        }
     }
 }
 
-$deleted = 0;
-foreach ($duplicates as $id) {
+$deletedTasks = 0;
+$deletedMissions = 0;
+foreach ($duplicateTasks as $id) {
     $sql = "delete from date_tasks where date_task_id = " . $id;
     if (mysql_query($sql)) {
-        $deleted++;
+        $deletedTasks++;
     }
 }
-echo "Deleted: " . $deleted;
+foreach ($deletedMissions as $id) {
+    $sql = "delete from date_tasks_missions where date_tasks_mission_id = " . $id;
+    if (mysql_query($sql)) {
+        $deletedMissions++;
+    }
+}
+echo "Deleted Missions: " . $deletedMissions . "<br />";
+echo "Deleted Tasks: " . $deletedTasks;
