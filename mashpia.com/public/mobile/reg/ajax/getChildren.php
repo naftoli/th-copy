b7e38f5d860b2d2fdead9e84f1ac1cec40f1f3b2
= <naftoli@tzivoshashem.org>
2021-11-07 17:39:49 -0500

    make sure only children from USA/Canada and not myshliach/anash kinder can get game sets

--- mashpia.com/public/mobile/reg/ajax/getChildren.php
+++ mashpia.com/public/mobile/reg/ajax/getChildren.php
@@ -72,7 +72,7 @@ while ( $row = mysql_fetch_assoc($result) ) {
 if ( !empty( $users ) ) {
     $children = [];
     //need to have multiple result rows to get highest rank
-    $sql = "select s.school_name, s.school_name_he, s.school_city, s.school_era, s.reg_type, s.shipping_method, c.class_grade, "
+    $sql = "select s.school_name, s.school_name_he, s.school_city, s.school_era, s.reg_type, s.shipping_method, s.school_country, c.class_grade, "
         ." u.user_id, u.first, u.last, u.first_he, u.last_he, u.lang_id, u.chayolei, u.chidon, u.user_serial, u.school_type_id, "
         ." u.mobile_pic, u.user_photo_id, u.school_id, u.user_registered "
         ." FROM users u "
@@ -105,6 +105,7 @@ if ( !empty( $users ) ) {
         $children[$row['user_id']]['shipping']      = $row['shipping_method'];
         $children[$row['user_id']]['chayoleiRegistered'] = false;
         $children[$row['user_id']]['new_day_school'] = intval($row['school_type_id']) == 50 ? true : false;
+        $children[$row['user_id']]['school_country'] = $row['school_country'];
 
         // find out highest rank achieved
         $sqlRank = "select r.rank_ord, r.rank_name, r.rank_image_id 
