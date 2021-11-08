71f70b443c220f2ab028456484fb87a0860ea374
= <naftoli@tzivoshashem.org>
2021-11-08 18:21:27 -0500

    add totals to comprehensive.php

--- mashpia.com/public/chidonOld/newReports/comprehensive.php
+++ mashpia.com/public/chidonOld/newReports/comprehensive.php
@@ -88,32 +88,139 @@ $types = $t->getTypes();
                 <th>Total Minutes Studied</th>
             </tr>
             <?php
+            $totals = [];
             foreach ($info as $user) {
                 $grade = $user['class_grade'] . (empty($user['class_sub']) ? '' : '-' . $user['class_sub']);
                 echo "<tr><td>" . $user['th_chidon_id'] . "</td><td>" . $user['user_serial'] . "</td><td>";
                 if ($admin_user['auth'] == 'super') echo $user['school_name'] . "</td><td>";
                 echo $grade . "</td><td>" . $user['first'] . ' ' . $user['last'] . "</td><td>";
                 if ($user['th_chidon_id']) {
+                    if ($totals['reg']) $totals['reg']++;
+                    else $totals['reg'] = 1;
                     echo $user['reg_date'] . "</td><td>";
-                    if ($user['khk_reg']) echo "YES";
+
+                    if ($user['khk_reg']) {
+                        echo "YES";
+                        if ($totals['khk']) $totals['khk']++;
+                        else $totals['khk'] = 1;
+                    }
                     else echo "NO";
                     echo "</td><td>";
-                    if ($user['marks'][$user['th_chidon_id']][1]['maven'] > 0) echo "YES";
-                    else echo "NO";
+
+                    $mark = parseInt($user['marks'][$user['th_chidon_id']][1]['maven'], 10);
+                    if ($mark > 0) echo "YES";
+                    else {
+                        echo "NO";
+                        if ($totals['missedTest']) $totals['missedTest']++;
+                        else $totals['missedTest'] = 1;
+                    }
+
+                    // keep track of how many kids failed first test
+                    if ($mark > 0 && $mark < 70) {
+                        if ($totals['failedTest']) $totals['failedTest']++;
+                        else $totals['failedTest'] = 1;
+                    }
+
                     echo "</td><td>" . $user['marks'][$user['th_chidon_id']][1]['maven'] . "</td><td>" .
                         $user['marks'][$user['th_chidon_id']][1]['pro'] . "</td><td>" .
                         $user['marks'][$user['th_chidon_id']][1]['expert'] . "</td><td>" .
                         $user['marks'][$user['th_chidon_id']][1]['genius'] . "</td><td>";
+
                     echo $types[$user['test_type']] . "</td><td>";
+
+                    // keep track of how many kids passed/failed their track
+                    if ($user['test_type'] != 'genius') $needed = 90;
+                    else $needed = 70;
+                    if (parseInt($user['marks'][$user['th_chidon_id']][1][$user['test_type']], 10) < $needed) {
+                        if ($totals['track']['failed']) $totals['track']['failed']++;
+                        else $totals['track']['failed'] = 1;
+                    } else {
+                        if ($totals['track']['passed']) $totals['track']['passed']++;
+                        else $totals['track']['passed'] = 1;
+                    }
+
+                    // keep track of how many children passed each type
+                    foreach ($types as $type) {
+                        if ($type == 'genius') $needed = 90;
+                        else $needed = 70;
+                        if (parseInt($user['marks'][$user['th_chidon_id']][1][$type], 10) >= $needed) {
+                            if ($totals[$type]) $totals[$type]++;
+                            else $totals[$type] = 1;
+                        }
+                    }
+
+                    //keep track of how many kids did better than their track
+                    if ($user['test_type'] != 'genius') {
+                        $tracks = array_keys($types);
+                        $index = array_search($user['test_type'], $tracks) + 1;
+                        $next = $tracks[$index];
+                        if ($next == 'genius') $needed = 90;
+                        else $needed = 70;
+                        if (parseInt($user['marks'][$user['th_chidon_id']][1][$next]) >= $needed) {
+                            if ($totals['betterThanTrack']) $totals['betterThanTrack']++;
+                            else $totals['betterThanTrack'] = 1;
+                        }
+                    }
+
                     if (in_array($user['reward_type'], array_keys($types))) echo $types[$user['reward_type']];
                     else echo $types[$user['test_type']];
                     echo "</td><td>";
+
                     echo $t->getHighestTrackEligible($user['th_chidon_id'], $user['marks'][$user['th_chidon_id']]) . "</td><td>";
+
                     $t->setLimmudDates(2459469, 2459514);
-                    echo $t->getTotalMinutesLearned( $user['user_id'] ) . "</td></tr>";
+                    $timeLearned = $t->getTotalMinutesLearned($user['user_id']);
+                    echo $timeLearned . "</td></tr>";
+
+                    $timeNeeded = [
+                        'maven' => 480,
+                        'pro' => 960,
+                        'expert' => 1440,
+                        'genius' => 1920
+                    ];
+                    if ($timeLearned >= $timeNeeded[$user['test_type']]) {
+                        if ($totals['time']['passed']) $totals['time']['passed']++;
+                        else $totals['time']['passed'] = 1;
+                    } else {
+                        if ($totals['time']['failed']) $totals['time']['failed']++;
+                        else $totals['time']['failed'] = 1;
+                    }
                 }
-                else echo "NOT REGISTERED" . "</td><td colspan='10'></td></tr>";
+                else {
+                    if ($totals['unreg']) $totals['unreg']++;
+                    else $totals['unreg'] = 1;
+                    echo "NOT REGISTERED" . "</td><td colspan='10'></td></tr>";
+                }
             }
+            ?>
+        </table>
+        <p></p>
+        <table>
+            <caption>Totals</caption>
+            <tr>
+                <th>Registered</th>
+                <th>Not Registered</th>
+                <th>Registered for KHK</th>
+                <th>Missed first Test</th>
+                <th>Failed first Test</th>
+                <th>Failed Own Track</th>
+                <th>Passed Own Track</th>
+                <th>Did better than Track</th>
+                <th>Passed Yesod</th>
+                <th>Passed Yediah</th>
+                <th>Passed Havanah</th>
+                <th>Passed Iyun</th>
+                <th>Children that completed their minutes</th>
+                <th>Children that didn't complete their minutes</th>
+            </tr>
+            <?php
+            echo "<tr><td>" . $totals['reg'] . "</td><td>" . $totals['unreg'] . "</td><td>" . $totals['khk'] . "</td><td>" .
+                $totals['missedTest'] . "</td><td>" . $totals['failedTest'] . "</td><td>" . $totals['track']['failed'] .
+                "</td><td>" . $totals['track']['passed'] . "</td><td>" . $totals['betterThanTrack'] . "</td><td>";
+            foreach ($types as $type => $value) {
+                echo $totals[$type] . "</td><td>";
+            }
+            echo $totals['time']['passed'] . "</td><td>" . $totals['time']['failed'] . "</td></tr>";
             ?>
         </table>
     </body>
