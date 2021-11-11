802a870598cf9f1f642ceff80d26d14c47b25e65
= <naftoli@tzivoshashem.org>
2021-11-11 12:55:22 -0500

    add links to shipping reports page

--- mashpia.com/public/reports/shipping/index.php
+++ mashpia.com/public/reports/shipping/index.php
@@ -56,6 +56,18 @@ require_once($_SERVER["DOCUMENT_ROOT"].'/header.php');
                     <span class="link-text">Anash Kinder Shipping Medals Labels</span>
                 </div>
             </a>
+            <a href="/medals_labels.php<?=$debug ? "?debug=true": "";?>">
+                <div class="button">
+                    <img src="/images/icon_report.png" height="32" alt="tickets"/>
+                    <span class="link-text">Regular Shipping Medals Labels</span>
+                </div>
+            </a>
+            <a href="/myshliachShipLabels.php<?=$debug ? "?debug=true": "";?>">
+                <div class="button">
+                    <img src="/images/icon_report.png" height="32" alt="tickets"/>
+                    <span class="link-text">MyShliach Shipping Medals Labels</span>
+                </div>
+            </a>
             <a href="/raffle_shipping.php<?=$debug ? "?debug=true": "";?>">
                 <div class="button">
                     <img src="/images/icon_auction.png" height="32" alt="tickets"/>
