68acf4469605e880fcdb147f1f2def3ac8a56e89
= <naftoli@tzivoshashem.org>
2021-11-29 14:53:28 -0500

    update report cards to show bold for the highest track passed

--- base-commander/src/pages/reportCards/ReportCard2.jsx
+++ base-commander/src/pages/reportCards/ReportCard2.jsx
@@ -301,23 +301,71 @@ function ReportCard(info) {
                                 <tr>
                                     <td rowSpan={2}>{index}</td>
                                     <td>Correct Questions</td>
+
+                                    {report.highestTrackPassed == 'Yesod' &&
+                                    <td className="bold">{report.scores[index] ? report.scores[index]['maven'] : 0} / {report.questions['maven']}</td>
+                                    }
+                                    {report.highestTrackPassed != 'Yesod' &&
                                     <td>{report.scores[index] ? report.scores[index]['maven'] : 0} / {report.questions['maven']}</td>
+                                    }
+
+                                    {report.highestTrackPassed == 'Yediah' &&
+                                    <td className="bold">{report.scores[index] ? (parseInt(report.scores[index]['pro'], 10) + parseInt(report.scores[index]['maven'], 10)) : 0} /
+                                        {report.questions['pro'] + report.questions['maven']}</td>
+                                    }
+                                    {report.highestTrackPassed != 'Yediah' &&
                                     <td>{report.scores[index] ? (parseInt(report.scores[index]['pro'], 10) + parseInt(report.scores[index]['maven'], 10)) : 0} /
                                         {report.questions['pro'] + report.questions['maven']}</td>
+                                    }
+
+                                    {report.highestTrackPassed == 'Havonah' &&
+                                    <td className="bold">{report.scores[index] ? (parseInt(report.scores[index]['expert'], 10) + parseInt(report.scores[index]['pro'], 10) + parseInt(report.scores[index]['maven'], 10)) : 0} /
+                                        {report.questions['expert'] + report.questions['pro'] + report.questions['maven']}</td>
+                                    }
+                                    {report.highestTrackPassed != 'Havonah' &&
                                     <td>{report.scores[index] ? (parseInt(report.scores[index]['expert'], 10) + parseInt(report.scores[index]['pro'], 10) + parseInt(report.scores[index]['maven'], 10)) : 0} /
                                         {report.questions['expert'] + report.questions['pro'] + report.questions['maven']}</td>
-                                    {showIyun &&
-                                    <td>{report.scores[index] ? (parseInt(report.scores[index]['genius'], 10) + parseInt(report.scores[index]['expert'], 10) +
+                                    }
+
+                                    {showIyun && report.highestTrackPassed == 'Iyun' &&
+                                    <td className="bold">{report.scores[index] ? (parseInt(report.scores[index]['genius'], 10) + parseInt(report.scores[index]['expert'], 10) +
                                     parseInt(report.scores[index]['pro'], 10) + parseInt(report.scores[index]['maven'], 10)) : 0} /
+                                        {report.questions['genius'] + report.questions['expert'] + report.questions['pro'] + report.questions['maven']}</td>
+                                    }
+                                    {showIyun && report.highestTrackPassed != 'Iyun' &&
+                                    <td>{report.scores[index] ? (parseInt(report.scores[index]['genius'], 10) + parseInt(report.scores[index]['expert'], 10) +
+                                        parseInt(report.scores[index]['pro'], 10) + parseInt(report.scores[index]['maven'], 10)) : 0} /
                                         {report.questions['genius'] + report.questions['expert'] + report.questions['pro'] + report.questions['maven']}</td>
                                     }
                                 </tr>
                                 <tr>
                                     <td>Mark</td>
+
+                                    {report.highestTrackPassed == 'Yesod' &&
+                                    <td className="bold">{report.scores[index] ? (report.tests[index]['maven'] % 1 ? report.tests[index]['maven'].toFixed(2) : report.tests[index]['maven']) : 0}%</td>
+                                    }
+                                    {report.highestTrackPassed != 'Yesod' &&
                                     <td>{report.scores[index] ? (report.tests[index]['maven'] % 1 ? report.tests[index]['maven'].toFixed(2) : report.tests[index]['maven']) : 0}%</td>
+                                    }
+
+                                    {report.highestTrackPassed == 'Yediah' &&
+                                    <td className="bold">{report.scores[index] ? (report.tests[index]['pro'] % 1 ? report.tests[index]['pro'].toFixed(2) : report.tests[index]['pro']) : 0}%</td>
+                                    }
+                                    {report.highestTrackPassed != 'Yediah' &&
                                     <td>{report.scores[index] ? (report.tests[index]['pro'] % 1 ? report.tests[index]['pro'].toFixed(2) : report.tests[index]['pro']) : 0}%</td>
+                                    }
+
+                                    {report.highestTrackPassed == 'Havonah' &&
+                                    <td className="bold">{report.scores[index] ? (report.tests[index]['expert'] % 1 ? report.tests[index]['expert'].toFixed(2) : report.tests[index]['expert']) : 0}%</td>
+                                    }
+                                    {report.highestTrackPassed != 'Havonah' &&
                                     <td>{report.scores[index] ? (report.tests[index]['expert'] % 1 ? report.tests[index]['expert'].toFixed(2) : report.tests[index]['expert']) : 0}%</td>
-                                    {showIyun &&
+                                    }
+
+                                    {showIyun && report.highestTrackPassed == 'Iyun' &&
+                                    <td className="bold">{report.scores[index] ? (report.tests[index]['genius'] % 1 ? report.tests[index]['genius'].toFixed(2) : report.tests[index]['genius']) : 0}%</td>
+                                    }
+                                    {showIyun && report.highestTrackPassed != 'Iyun' &&
                                     <td>{report.scores[index] ? (report.tests[index]['genius'] % 1 ? report.tests[index]['genius'].toFixed(2) : report.tests[index]['genius']) : 0}%</td>
                                     }
                                 </tr>
@@ -326,24 +374,72 @@ function ReportCard(info) {
                         <tr>
                             <td rowSpan={2}>Total</td>
                             <td>Correct Questions</td>
+
+                            {report.highestTrackPassed == 'Yesod' &&
+                            <td className="bold">{totals['maven']} / {report.questions['maven'] * numTests}</td>
+                            }
+                            {report.highestTrackPassed != 'Yesod' &&
                             <td>{totals['maven']} / {report.questions['maven'] * numTests}</td>
+                            }
+
+                            {report.highestTrackPassed == 'Yediah' &&
+                            <td className="bold">{totals['maven'] + totals['pro']} / {
+                                (parseInt(report.questions['maven'], 10) + parseInt(report.questions['pro'], 10)) * numTests}
+                            </td>
+                            }
+                            {report.highestTrackPassed != 'Yediah' &&
                             <td>{totals['maven'] + totals['pro']} / {
                                 (parseInt(report.questions['maven'], 10) + parseInt(report.questions['pro'], 10)) * numTests}
                             </td>
+                            }
+
+                            {report.highestTrackPassed == 'Havonah' &&
+                            <td className="bold">{totals['maven'] + totals['pro'] + totals['expert']} / {
+                                (parseInt(report.questions['maven'], 10) + parseInt(report.questions['pro'], 10) + parseInt(report.questions['expert'], 10)) * numTests}</td>
+                            }
+                            {report.highestTrackPassed != 'Havonah' &&
                             <td>{totals['maven'] + totals['pro'] + totals['expert']} / {
                                 (parseInt(report.questions['maven'], 10) + parseInt(report.questions['pro'], 10) + parseInt(report.questions['expert'], 10)) * numTests}</td>
-                            {showIyun &&
-                                <td>{totals['maven'] + totals['pro'] + totals['expert'] + totals['genius']} / {
+                            }
+
+                            {showIyun && report.highestTrackPassed == 'Iyun' &&
+                                <td className="bold">{totals['maven'] + totals['pro'] + totals['expert'] + totals['genius']} / {
                                     (parseInt(report.questions['maven'], 10) + parseInt(report.questions['pro'], 10) + parseInt(report.questions['expert'], 10) + parseInt(report.questions['genius'], 10)) * numTests}</td>
+                            }
+                            {showIyun && report.highestTrackPassed != 'Iyun' &&
+                            <td>{totals['maven'] + totals['pro'] + totals['expert'] + totals['genius']} / {
+                                (parseInt(report.questions['maven'], 10) + parseInt(report.questions['pro'], 10) + parseInt(report.questions['expert'], 10) + parseInt(report.questions['genius'], 10)) * numTests}</td>
                             }
                         </tr>
                         <tr>
                             <td>Mark</td>
+
+                            {report.highestTrackPassed == 'Yesod' &&
+                            <td className="bold">{totalMarks['maven']}%</td>
+                            }
+                            {report.highestTrackPassed != 'Yesod' &&
                             <td>{totalMarks['maven']}%</td>
+                            }
+
+                            {report.highestTrackPassed == 'Yediah' &&
+                            <td className="bold">{totalMarks['pro']}%</td>
+                            }
+                            {report.highestTrackPassed != 'Yediah' &&
                             <td>{totalMarks['pro']}%</td>
+                            }
+
+                            {report.highestTrackPassed == 'Havonah' &&
+                            <td className="bold">{totalMarks['expert']}%</td>
+                            }
+                            {report.highestTrackPassed != 'Havonah' &&
                             <td>{totalMarks['expert']}%</td>
-                            {showIyun &&
-                                <td>{totalMarks['genius']}%</td>
+                            }
+
+                            {showIyun && report.highestTrackPassed == 'Iyun' &&
+                                <td className="bold">{totalMarks['genius']}%</td>
+                            }
+                            {showIyun && report.highestTrackPassed != 'Iyun' &&
+                            <td>{totalMarks['genius']}%</td>
                             }
                         </tr>
                         <tr className={classes.lastRow}>
