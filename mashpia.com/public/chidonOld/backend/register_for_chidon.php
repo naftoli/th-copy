e5ed779fa4c07f43b15e461ae3b880dcfe2d15a4
= <naftoli@tzivoshashem.org>
2021-12-01 20:02:39 -0500

    update registering for chidon from spreadsheet file

--- mashpia.com/public/chidonOld/backend/register_for_chidon.php
+++ mashpia.com/public/chidonOld/backend/register_for_chidon.php
@@ -13,6 +13,20 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/class.globalSettings.php';
 
 $year = GlobalSettings::getChidonYear();
 
+$tracks = [
+    'yesod'     => 'maven',
+    'yediah'    => 'pro',
+    'havanah'   => 'expert',
+    'iyun'      => 'genius'
+];
+
+$chidon_prizes = [];
+$prize_qry = $MASHPIA_DB->prepare("select * from chidon_prizes where year = :year");
+$prize_qry->execute([':year' => $year]);
+foreach ($prize_qry->fetchAll() as $prize) {
+    $chidon_prizes[$prize['prize_id']] = empty($prize['personalization']) ? 0 : 1;
+}
+
 if ( isset( $_FILES['file'] ) ) {
     $objPHPExcel = PHPExcel_IOFactory::load( $_FILES['file']['tmp_name'] );
     $objWorksheet = $objPHPExcel->getActiveSheet();
@@ -39,15 +53,24 @@ if ( isset( $_FILES['file'] ) ) {
     
     // loop through info array and register kids
     // index 0 is the user serial and index 1 is the school id
+    $i = 0;
     foreach ( $info as $values ) {
-        $user_serial = $values[0];
-        $school_id = $values[1];
+        $user_serial = $values[$i++];
+        $yarmulka = empty($values[$i++]) ? $values[1] : 0;
+        $sweater = $values[$i++];
+        $track = $values[$i++];
+        $book = $values[$i++];
+        $learning_method = $values[$i++];
+        $school_name = $values[$i++];
+        $prizes = $values[$i+];
+        $prize_name = $values[$i++];
 
         // find out what the user id is
-        if ( $handle = $MASHPIA_DB->prepare("select user_id from users where user_serial = :serial") ) {
+        if ( $handle = $MASHPIA_DB->prepare("select user_id, school_id from users where user_serial = :serial") ) {
             if ( $handle->execute([':serial' => $user_serial]) ) {
                 $result = $handle->fetch();
                 $user_id = $result['user_id'];
+                $school_id = $result['school_id'];
 
                 if ( $user_id > 0 ) {
                     // first find out if user already is registered in chidon db
@@ -72,22 +95,68 @@ if ( isset( $_FILES['file'] ) ) {
                                                                         set year = :year, 
                                                                         school_id = :school_id, 
                                                                         user_id = :user_id, 
+                                                                        yarmulka = :yarmulka,
                                                                         size = :size, 
-                                                                        parent_id = :parent_id") ) {
+                                                                        parent_id = :parent_id,
+                                                                        test_type = :type, 
+                                                                        reward_type = :type, 
+                                                                        book = :book,
+                                                                        poll = :learning_method
+                                                                        ") ) {
                                             if ( 
                                                 !$handle->execute([
                                                     ':year'         =>  $year, 
                                                     ':school_id'    =>  $school_id, 
-                                                    ':user_id'      =>  $user_id, 
-                                                    ':size'         =>  'children l', 
-                                                    ':parent_id'    =>  $admin_id
+                                                    ':user_id'      =>  $user_id,
+                                                    ':yarmulka'     =>  $yarmulka,
+                                                    ':size'         =>  $sweater,
+                                                    ':parent_id'    =>  $admin_id,
+                                                    ':type'         =>  $tracks[strtolower($track)],
+                                                    ':book'         =>  $book,
+                                                    ':poll'         =>  $learning_method
                                                 ]) 
                                             ) {
                                                 $error = true;
                                                 $success = false;
                                                 break;
                                             } else {
-                                                $updated++;
+                                                $stmt1 = $MASHPIA_DB->prepare("insert into chidon_user_prizes 
+                                                            set user_id = :user, prize_id = :prize, year = :year
+                                                        ");
+                                                $stmt2 = $MASHPIA_DB->prepare("insert into chidon_user_prizes 
+                                                            set user_id = :user, prize_id = :prize, year = :year, he_name = :name
+                                                        ");
+                                                foreach (explode(',', $prizes) as $prize) {
+                                                    // if we need to add the he name
+                                                    if ($chidon_prizes[$prize]) {
+                                                        if (
+                                                            $stmt2->execute([
+                                                                ':user'     => $user_id,
+                                                                ':prize'    => $prize,
+                                                                ':year'     => $year,
+                                                                ':name'     => $prize_name
+                                                            ])
+                                                        )
+                                                            $updated++;
+                                                        else {
+                                                            $error = true;
+                                                            break 2;
+                                                        }
+                                                    } else {
+                                                        if (
+                                                            $stmt1->execute([
+                                                                ':user'     => $user_id,
+                                                                ':prize'    => $prize,
+                                                                ':year'     => $year
+                                                            ])
+                                                        )
+                                                            $updated++;
+                                                        else {
+                                                            $error = true;
+                                                            break 2;
+                                                        }
+                                                    }
+                                                }
                                             }
                                         } else {
                                             $error = true;
@@ -115,6 +184,7 @@ if ( isset( $_FILES['file'] ) ) {
     }
 
     if ( $error ) {
+        $success = false;
         echo "<pre>"; print_r( $MASHPIA_DB->errorInfo() ); echo "</pre>";
     }
 
