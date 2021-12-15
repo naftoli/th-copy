712bf8218af9052f665b211ee9372a6b79d15160
= <naftoli@tzivoshashem.org>
2021-12-15 17:11:29 -0500

    add selecting test numbers when pullling up report on setTypes.php

--- mashpia.com/public/chidonTests/setTypes.php
+++ mashpia.com/public/chidonTests/setTypes.php
@@ -22,25 +22,29 @@ require $_SERVER['DOCUMENT_ROOT'] . '/chidonTests/class.chidonTests.php';
 $ct = new ChidonTests();
 $types = $ct->getTypes();
 
+$info = [];
+$marks = [];
+
 if (isset($_POST['submit'])) {
+    $numTests = $_POST['test'];
+    foreach ($schools as $id => $school) {
+        $ct->setStudents($id);
+        $info[$id] = $ct->getStudents();
+        $ct->setScores();
+        $ct->calculateMarks();
+        $marks[$id] = $ct->getMarks();
+    }
+}
+
+if (isset($_POST['save'])) {
     $ct->setTestTypes($_POST['type']);
     $ct->setRewardTypes($_POST['reward_type']);
     header("Location: enterScores.php");
     exit;
 }
 
-$info = [];
-$marks = [];
-foreach ($schools as $id => $school) {
-    $ct->setStudents($id);
-    $info[$id] = $ct->getStudents();
-    $ct->setScores();
-    $ct->calculateMarks();
-    $marks[$id] = $ct->getMarks();
-}
-
 function markInfo( $child, $school_id ) {
-    global $types, $marks;
+    global $types, $marks, $numTests;
 
     // get child mark info
     $childMarkInfo = $marks[$school_id][$child['th_chidon_id']];
@@ -54,10 +58,8 @@ function markInfo( $child, $school_id ) {
         $avgs[$type] = 0;
     }
 
-    $numTests = 0; // variable to help determine how many test where taken to calculate avg later
     for ($i = 1; $i <= 4; $i++) {
         if (isset($childMarkInfo[$i])) {
-            if ($childMarkInfo[$i]['maven'] > 0) $numTests++;
             foreach ($childMarkInfo[$i] as $type => $mark) {
                 if ($mark > 0) {
                     $marksPerType[$type] += $mark;
@@ -111,44 +113,57 @@ function markInfo( $child, $school_id ) {
         <?php include($_SERVER['DOCUMENT_ROOT'] . '/admin_header.php'); ?>
         <h1>Chidon Test Types</h1>
         <?php
-        echo "<form action='' method='post'>";
-        echo "<div style='float: right'><input type='submit' name='submit' value='Save & go to Test Scoring' style='padding: 12px; font-size: large' /></div>";
-        echo "<div style='clear: both'></div>";
-        foreach ($info as $school => $children) {
-            if (empty($children)) continue;
-            echo "<h2>" . $schools[$school] . "</h2>";
-            echo "<table><tr><th>Serial Number</th><th>Grade</th><th>Student</th><th>Test Type</th><th>Avg Mark</th>
-                <th>Highest Track Passed</th><th>Avg Mark</th><th>Reward Type for Child</th></tr>";
-            foreach ($children as $child) {
-                $markInfo = markInfo($child, $school);
-                $grade = $child['class_grade'] . ($child['class_sub'] ? '' : '-' . $child['class_sub']);
-                $name = $child['first'] . ' ' . $child['last'];
-                echo "<tr><td>" . $child['user_serial'] . "</td><td>" . $grade . "</td><td>" . $name . "</td><td class='type'>";
-                echo "<select name='type[" . $child['th_chidon_id'] . "]'>";
-                foreach ($types as $type => $value) {
-                    echo "<option value='" . $type . "'";
-                    if ($child['test_type'] == $type) echo " selected ";
-                    echo ">" . $value . "</option>";
+        if (isset($_POST['submit'])) {
+            echo "<form action='' method='post'>";
+            echo "<div style='float: right'><input type='submit' name='save' value='Save & go to Test Scoring' style='padding: 12px; font-size: large' /></div>";
+            echo "<div style='clear: both'></div>";
+            foreach ($info as $school => $children) {
+                if (empty($children)) continue;
+                echo "<h2>" . $schools[$school] . "</h2>";
+                echo "<table><tr><th>Serial Number</th><th>Grade</th><th>Student</th><th>Test Type</th><th>Avg Mark</th>
+                    <th>Highest Track Passed</th><th>Avg Mark</th><th>Reward Type for Child</th></tr>";
+                foreach ($children as $child) {
+                    $markInfo = markInfo($child, $school);
+                    $grade = $child['class_grade'] . ($child['class_sub'] ? '' : '-' . $child['class_sub']);
+                    $name = $child['first'] . ' ' . $child['last'];
+                    echo "<tr><td>" . $child['user_serial'] . "</td><td>" . $grade . "</td><td>" . $name . "</td><td class='type'>";
+                    echo "<select name='type[" . $child['th_chidon_id'] . "]'>";
+                    foreach ($types as $type => $value) {
+                        echo "<option value='" . $type . "'";
+                        if ($child['test_type'] == $type) echo " selected ";
+                        echo ">" . $value . "</option>";
+                    }
+                    echo "</select></td><td>" . $markInfo['avg'] . "</td><td>";
+                    if (!empty($markInfo['highest_track'])) echo $types[$markInfo['highest_track']];
+                    echo "</td><td>" . $markInfo['highest_track_avg'] . "</td><td>";
+                    echo "<select name='reward_type[" . $child['th_chidon_id'] . "]'>";
+                    foreach ($types as $type => $value) {
+                        if ($type == 'genius') continue;
+                        echo "<option value='" . $type . "'";
+                        if ($type == $child['reward_type']) echo " selected ";
+                        echo ">" . $value . "</option>";
+                    }
+                    echo "<option value='highest track passed'";
+                    if ($child['reward_type'] == 'highest track passed') echo " selected ";
+                    echo ">Highest Track Passed</option>";
+                    echo "</select></td></tr>";
                 }
-                echo "</select></td><td>" . $markInfo['avg'] . "</td><td>";
-                if (! empty($markInfo['highest_track'])) echo $types[$markInfo['highest_track']];
-                echo "</td><td>" . $markInfo['highest_track_avg'] . "</td><td>";
-                echo "<select name='reward_type[" . $child['th_chidon_id'] . "]'>";
-                foreach ($types as $type => $value) {
-                    if ($type == 'genius') continue;
-                    echo "<option value='" . $type . "'";
-                    if ($type == $child['reward_type']) echo " selected ";
-                    echo ">" . $value . "</option>";
-                }
-                echo "<option value='highest track passed'";
-                if ($child['reward_type'] == 'highest track passed') echo " selected ";
-                echo ">Highest Track Passed</option>";
-                echo "</select></td></tr>";
+                echo "</table>";
             }
-            echo "</table>";
+            echo "<div style='float: right'><input type='submit' name='submit' value='Save & go to Test Scoring' style='padding: 12px; font-size: large' /></div>";
+            echo "</form>";
+        } else {
+            echo "<form action='' method='post'>";
+            echo "<p>Please select based on which tests you would like to see track averages & highest passing track</p>";
+            echo "Choose Test: <select name='test'>";
+            echo "<option value='1'>Test 1</option>";
+            echo "<option value='2'>Combine tests 1 & 2</option>";
+            echo "<option value='3'>Combine tests 1 - 3</option>";
+            echo "<option value='4'>Combine tests 1 - 4</option>";
+            echo "</select><br /><br />";
+            echo "<input type='submit' name='submit' value='Generate Report' />";
+            echo "</form>";
         }
-        echo "<div style='float: right'><input type='submit' name='submit' value='Save & go to Test Scoring' style='padding: 12px; font-size: large' /></div>";
-        echo "</form>";
         ?>
     </body>
     <script>
@@ -164,7 +179,8 @@ function markInfo( $child, $school_id ) {
                 }
             }
             $('body').show();
-            alert('Please make sure to SAVE after making changes.');
+            const show = <?= isset($_POST['submit']) ? 1 : 0 ?>;
+            if (show) alert('Please make sure to SAVE after making changes.');
         })
     </script>
 </html>
