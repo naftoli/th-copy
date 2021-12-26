d133304124195de2cfc1111d6348ac32a747344b
= <naftoli@tzivoshashem.org>
2021-12-25 23:21:51 -0500

    change layout for new birthday certificates

--- mashpia.com/public/birthday_cert.php
+++ mashpia.com/public/birthday_cert.php
@@ -142,7 +142,7 @@ while ( $row = mysql_fetch_assoc( $result ) ) {
                 src: url('fonts/DIRTYEGO.TTF');
             }
             .page {
-                height: 10cm;
+                height: 15cm;
             }
             .name {
                 margin-left: 2.6cm;
