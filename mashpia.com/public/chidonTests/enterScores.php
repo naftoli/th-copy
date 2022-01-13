df9adbd573cf2fb140e62e83434d0311f21d0322
= <naftoli@tzivoshashem.org>
2022-01-13 13:28:17 -0500

    fix date for closing test 3 marking

--- mashpia.com/public/chidonTests/enterScores.php
+++ mashpia.com/public/chidonTests/enterScores.php
@@ -39,12 +39,12 @@ if ($admin_user['auth'] != 'super') {
     $shutdown = [];
     $shutdown[1] = new DateTime('2021-11-20 05:00:00');
     $shutdown[2] = new DateTime('2021-12-26 05:00:00');
-    $shutdown[3] = new DateTime('2022-01-12 05:00:00');
+    $shutdown[3] = new DateTime('2022-01-19 05:00:00');
     // make different shutdown for 180
-    foreach ($schools as $id => $school) {
-        if ($id == 180) $shutdown[3] = new DateTime('2022-01-16 05:00:00');
-        break;
-    }
+//    foreach ($schools as $id => $school) {
+//        if ($id == 180) $shutdown[3] = new DateTime('2022-01-16 05:00:00');
+//        break;
+//    }
 //    $shutdown[4] = new DateTime('2021-02-24 05:00:00');
 
     if ($shutdown[$testNumber] && $today >= $shutdown[$testNumber]) {
