d9e5dc1ccf05e328333f9876d95c0cc7e2a87b2d
= <naftoli@tzivoshashem.org>
2022-02-09 18:47:16 -0500

    FIX RELATIVE LINKS

--- mashpia.com/public/chidonOld/chidon_drive/ajax/getAdminInfo.php
+++ mashpia.com/public/chidonOld/chidon_drive/ajax/getAdminInfo.php
@@ -1,10 +1,13 @@
 <?php
-require_ONCE __DIR__ . '/../../../db.php';
-require_ONCE __DIR__ . '/../encrypt.php';
+ini_set('display_errors', 1);
+ini_set('error_reporting', 1);
+
+require_once __DIR__ . '/../../../db.php';
+require_once __DIR__ . '/../encrypt.php';
 $admin = $_COOKIE['chidon_admin'];
 $admin_id = encrypt_decrypt('decrypt', $admin);
 
-require __DIR__ . '/../classes/authorize/CustomerProfile.php';
+require_once __DIR__ . '/../../../classes/authorize/CustomerProfile.php';
 use \classes\authorize\CustomerProfile as Customer;
 
 $sql = "select * from admins where admin_id = " . $admin_id;
@@ -14,7 +17,6 @@ $info = mysql_fetch_assoc($result);
 $profile_id = $info['authorize_customer_profile_id'];
 if ($profile_id) {
     $customer = new Customer($profile_id);
-//    echo "<pre>"; print_r($customer); echo "</pre>";
     $info['cards'] = $customer->paymentProfiles;
 }
 
