121c1d2f0e11defdf00080fc10f3e07f5929380b
= <naftoli@tzivoshashem.org>
2022-02-10 19:01:49 -0500

    updates to chidon registration pages

--- mashpia.com/public/chidonOld/chidon_drive/ajax/processPayment5782.php
+++ mashpia.com/public/chidonOld/chidon_drive/ajax/processPayment5782.php
@@ -18,12 +18,23 @@ use classes\authorize\PaymentProfile as Payment;
 
 //******************* GLOBAL VARIABLES ***********************/
 $admin_id = $_POST['admin_id'];
+$admin_email = $_POST['admin_email'];
 $payment_id = $_POST['card_id'];
 $to_charge = isset($_POST['cart_total']) ? $_POST['cart_total'] : 0;
 $ccInfo = isset($_POST['cc']) ? $_POST['cc'] : [];
 $shipping = isset($_POST['shipping']) ? $_POST['shipping'] : 0;
 $cart = $_POST['cart'];
+$sweaters = isset($_POST['sweaters']) ? $_POST['sweaters'] : [];
 $addresses = isset($_POST['addresses']) ? $_POST['addresses'] : [];
+$users = [];
+$user_info = [];
+$reg_cost = 0;
+$iyun = false;
+$celebBoxes = 0;
+$celebBoxShipping = 0;
+$sweater_info = [];
+$sweater_shipping = 0;
+$emailMsg = '';
 
 //******************* SQL QUERIES ***********************/
 $sql = "update th_chidon set paid = :paid, date_paid = now(), paid_by = :admin where year = :year and user_id = :user";
@@ -60,6 +71,53 @@ $sql = "insert into purchase_addresses set
 $sqlAddress = $MASHPIA_DB->prepare($sql);
 
 //******************* CUSTOM FUNCTIONS ***********************/
+function processCart() {
+    global $cart, $users, $celebBoxes, $celebBoxShipping, $user_info, $iyun, $reg_cost;
+
+    $sweater_types = ['mother_sweater', 'father_sweater', 'bubby_sweater', 'zaidy_sweater'];
+
+    foreach ($cart as $item) {
+        if (strpos($item['desc'], 'reg') !== false) {
+            $regInfo = explode('_', $item['desc']);
+            $user_id = $regInfo[1];
+            $users[$user_id] = floatval($item['value']);
+            $reg_cost .= $users[$user_id];
+        } else if ($item['desc'] == 'num_celeb_boxes') {
+            $celebBoxes = intval($item['value']);
+        } else if ($item['desc'] == 'celeb_box_ship') {
+            $celebBoxShipping = intval($item['value']);
+        } else if (in_array($item['desc'], $sweater_types)) {
+            $sweaters[$item['desc']] = intval($item['value']);
+        } else if ($item['desc'] == 'names') {
+            $user_info = $item['value'];
+        } else if ($item['desc'] == 'iyun' && intval($item['value'])) {
+            $iyun = true;
+        }
+    }
+}
+
+function setSweaterInfo() {
+    global $cart, $sweaters, $sweater_info, $sweater_shipping;
+
+    // find out size and shipping info for sweaters purchased
+    foreach ($sweaters as $sweater) {
+        $type = $sweater['type'];
+        $num = $sweater['amount'];
+        for ($i = 1; $i <= $num; $i++) {
+            $size = $type . '_' . $i . '_size';
+            $ship = $type . '_' . $i . '_ship';
+            foreach ($cart as $item) {
+                if ($item['desc'] == $size) {
+                    $sweater_info[$type][$i]['size'] = $item['value'];
+                } else if ($item['desc'] == $ship) {
+                    $sweater_info[$type][$i]['ship'] = $item['value'];
+                    $sweater_shipping += intval($item['value']);
+                }
+            }
+        }
+    }
+}
+
 function addNewCard() {
     global $admin_id, $ccInfo;
     $admin = \Admin::find('first', ['admin_id' => $admin_id]);
@@ -106,37 +164,31 @@ function processFee() {
 }
 
 function processReg() {
-    global $admin_id, $year, $cart, $sqlReg;
+    global $admin_id, $year, $sqlReg, $users;
 
-    // get users to register
-    $users = [];
-    foreach ($cart as $item) {
-        if (strpos($item['desc'], 'reg') !== false) {
-            $regInfo = explode('_', $item['desc']);
-            $user_id = $regInfo[1];
-            $users[$user_id] = floatval($item['value']);
+    if (! $users) return true; // nothing to save so all is good
+    else {
+        // register users
+        $success = true;
+        foreach ($users as $user_id => $amount) {
+            $res = $sqlReg->execute([
+                ':paid' => $amount,
+                ':admin' => $admin_id,
+                ':year' => $year,
+                ':user' => $user_id
+            ]);
+            if (!$res) {
+                $success = false;
+                break;
+            }
         }
+        return $success;
     }
-
-    // register users
-    $success = true;
-    foreach ($users as $user_id => $amount) {
-        $res = $sqlReg->execute([
-            ':paid' => $amount,
-            ':admin' => $admin_id,
-            ':year' => $year,
-            ':user' => $user_id
-        ]);
-        if (!$res) {
-            $success = false;
-            break;
-        }
-    }
-    return $success;
 }
 
 function updateShipping() {
-    global $admin_id, $shipping, $sqlShipping;
+    global $admin_id, $sqlShipping, $shipping;
+
     $updated = true;
     if (intval($shipping)) {
         $updated = $sqlShipping->execute([
@@ -148,27 +200,17 @@ function updateShipping() {
 }
 
 function processCelebBoxes() {
-    global $year, $admin_id, $cart, $addresses, $sqlCelebBox, $sqlAddress, $MASHPIA_DB;
+    global $year, $admin_id, $addresses, $sqlCelebBox, $sqlAddress, $MASHPIA_DB, $celebBoxes, $celebBoxShipping;
 
-    $total = 0;
-    $ship = 0;
-    foreach ($cart as $item) {
-        if ($item['desc'] == 'num_celeb_boxes') {
-            $total = intval($item['value']);
-        } else if ($item['desc'] == 'celeb_box_ship') {
-            $ship = intval($item['value']);
-        }
-    }
-
-    if (!$total) return true; // no need to process anything so return true meaning there's no issues
+    if (!$celebBoxes) return true; // no need to process anything so there's no issues
     else {
         $res = $sqlCelebBox->execute([
             ':year'     => $year,
-            ':total'    => $total,
+            ':total'    => $celebBoxes,
             ':admin'    => $admin_id,
-            ':shipping' => $ship
+            ':shipping' => $celebBoxShipping
         ]);
-        if ($res && $ship) {
+        if ($res && $celebBoxShipping) {
             $purchase_id = $MASHPIA_DB->lastInsertId();
             $addressInfo = $addresses['celeb_box'];
             $res2 = $sqlAddress->execute([
@@ -181,49 +223,22 @@ function processCelebBoxes() {
             ]);
             if ($res2) return true;
         }
-        else if ($res && !$ship) return true;
+        else if ($res && !$celebBoxShipping) return true;
         else if (!$res) return false;
     }
     return false;
 }
 
 function processSweaters() {
-    global $admin_id, $year, $cart, $addresses, $sqlSweater, $sqlAddress, $MASHPIA_DB;
-
-    $sweaters = [];
-    $numSweaters = [];
-
-    // find out which sweaters were purchases and how many
-    foreach ($cart as $item) {
-        $types = ['mother', 'father', 'bubby', 'zaidy'];
-        foreach ($types as $type) {
-            $sweater_type = $type . '_sweater';
-            if ($item['desc'] == $sweater_type) {
-                $numSweaters[$sweater_type] = intval($item['value']);
-            }
-        }
-    }
-
-    // find out size and shipping info for sweaters purchased
-    foreach ($numSweaters as $type => $num) {
-        for ($i = 1; $i <= $num; $i++) {
-            $size = $type . '_' . $i . '_size';
-            $ship = $type . '_' . $i . '_ship';
-            foreach ($cart as $item) {
-                if ($item['desc'] == $size) {
-                    $sweaters[$type][$i]['size'] = $item['value'];
-                } else if ($item['desc'] == $ship) {
-                    $sweaters[$type][$i]['ship'] = $item['value'];
-                }
-            }
-        }
-    }
+    global $admin_id, $year, $addresses, $sqlSweater, $sqlAddress, $MASHPIA_DB, $sweaters, $sweater_info;
 
     // update db
     $success = true;
-    foreach ($sweaters as $type => $more) {
+    foreach ($sweaters as $sweater) {
+        $type = $sweater['type'];
+        $num = $sweater['amount'];
         $typeInfo = explode('_', $type);
-        foreach ($more as $num => $details) {
+        foreach ($sweater_info[$type] as $details) {
             $res = $sqlSweater->execute([
                 ':year'     => $year,
                 ':size'     => $details['size'],
@@ -261,12 +276,74 @@ function processSweaters() {
     return $success;
 }
 
-function sendEmail($trans_id) {
-    global $admin_id, $to_charge;
+function getEmailMsg($trans_id) {
+    global $iyun, $users, $user_info, $shipping, $celebBoxes, $celebBoxShipping, $sweaters, $sweater_shipping, $to_charge, $reg_cost;
 
+    define('CELEB_BOX_COST', 20);
+    define('SWEATER_COST', 25);
 
+    $msg = '';
+
+    if ($users) {
+        $msg .= "THANK YOU FOR REGISTERING YOUR CHILD(REN).<br /><br />";
+        $msg .= 'Your registration is complete for ';
+        $numUsers = count($users);
+        $i = 1;
+        foreach ($users as $user_id => $amount) {
+            $msg .= $user_info[$user_id];
+            if ($i++ < $numUsers) $msg .= ' and ';
+        }
+        $msg .= ".<br />";
+        if ($iyun) $msg .= 'The Iyun part of the final will be online on Sunday, 10 Adar 2 (March 13).<br />';
+        $msg .= "<br />";
+    }
+
+    // check if there was payment
+    if ($to_charge) {
+        if (! $users) {
+            $msg .= "THANK YOU FOR YOUR PURCHASES.<br /><br />";
+        }
+        $msg .= "Your payment of $" . $to_charge . " was received. Your transaction ID for your records is: " .
+            $trans_id . ".<br /><br />";
+
+        $msg .= "<b>A Summary of your charges is as follows:</b><br /><br />";
+        if ($users) $msg .= "Total Registration Charges: $" . (floatval($reg_cost) + intval($shipping)) . "<br />";
+        if ($celebBoxes) $msg .= "Total Celebration Box(es) Charge: $" . ($celebBoxes * CELEB_BOX_COST + $celebBoxShipping) . "<br />";
+        if ($sweaters) {
+            $total = 0;
+            foreach ($sweaters as $type => $amount) {
+                $total += intval($amount) * SWEATER_COST;
+            }
+            $msg .= "Total Sweaters Charge: $" . ($total + $sweater_shipping) . "<br />";
+        }
+        $msg .= "<br /><b>Grand Total: $" . $to_charge . "</b>.<br /><br />";
+    }
+
+    $msg .= "Please continue to review for the Chidon Final on Wednesday, 6 Adar 2 (March 9).<br /><br />
+        If you have any questions, please email Chidon@TzivosHashem.org.<br /><br />Much Hatzlocha!<br />Chidon HQ";
+
+    return $msg;
 }
 
+function sendEmail($msg) {
+    global $admin_email;
+
+    if ($admin_email) {
+        $headers[] = 'MIME-Version: 1.0';
+        $headers[] = 'Content-type: text/html; charset=iso-8859-1';
+        $headers[] = 'From: Chidon Headquarters <chidon@tzivoshashem.org>';
+        $headers[] = 'Reply-to: Chidon Headquarters <chidon@tzivoshashem.org>';
+        $headers[] = 'Bcc: chidon@tzivoshashem.org';
+        return mail($admin_email, 'Chidon Confirmation', $msg, implode("\r\n", $headers));
+    }
+    return false;
+}
+
+//******************* PROGRAM STARTS HERE ***********************/
+processCart();
+setSweaterInfo();
+
+// process everything
 $MASHPIA_DB->beginTransaction();
 // first do all the db stuff and only save if payment goes through
 $registered = processReg();
@@ -278,21 +355,23 @@ $info = [];
 $trans_id = 0;
 if ($registered && $shippingUpdated && $celebBoxesProcessed && $sweatersProcessed) {
     if (intval($to_charge) > 0) {
-        $payment = processFee();
-        if (is_array($payment)) {
-            $trans_id = $payment['transactionResponse']['transId'];
+//        $payment = processFee();
+//        if (is_array($payment)) {
+//            $trans_id = $payment['transactionResponse']['transId'];
             // payment went through so commit to db
-            $MASHPIA_DB->commit();
+//            $MASHPIA_DB->commit();
+            $trans_id = 1111;
+            $MASHPIA_DB->rollBack();
             $info['success'] = true;
             $msg = 'Congratulation! You have successfully registered your child(ren) and / or ordered your additional purchase(s).' . "\r\n" .
-                    'Your card has been charged $' . $to_charge . '. Your transaction ID for your record is: ' . $trans_id . '.' . "\r\n" . 'You should receive an email confirmation shortly 
-                    with all the details.' . "\r\n" . 'Thank You!';
+                    'Your card has been charged $' . $to_charge . '. Your transaction ID for your record is: ' . $trans_id . '.' . "\r\n" .
+                    'You should receive an email confirmation shortly with all the details.' . "\r\n" . 'Thank You!';
             $info['msg'] = $msg;
-        } else {
-            $MASHPIA_DB->rollBack();
-            $info['success'] = false;
-            $info['error'] = $payment;
-        }
+//        } else {
+//            $MASHPIA_DB->rollBack();
+//            $info['success'] = false;
+//            $info['error'] = $payment;
+//        }
     } else {
         $MASHPIA_DB->commit();
         $info['success'] = true;
@@ -301,9 +380,16 @@ if ($registered && $shippingUpdated && $celebBoxesProcessed && $sweatersProcesse
 } else {
     $MASHPIA_DB->rollBack();
     $info['success'] = false;
-    $info['error'] = 'There was an error saving your registration(s) and / or your extra purchase(s). Please try again. If this continues, please email us - chidon@tzivoshashem.org';
+    $info['error'] = 'There was an error saving your registration(s) and / or your extra purchase(s). Please try again. If this continues, please send an email to chidon@tzivoshashem.org';
 }
 // send email confirmation
-if ($info['success']) sendEmail($trans_id);
+if ($info['success']) {
+    $msg = getEmailMsg($trans_id);
+//    echo $msg;
+    if (!sendEmail($msg)) {
+        $header = 'From: chidon@tzivoshashem.org';
+        @mail('naftoli@tzivoshashem.org', 'Error Emailing Chidon Confirmation', $msg, $header);
+    }
+}
 
 echo json_encode($info);
