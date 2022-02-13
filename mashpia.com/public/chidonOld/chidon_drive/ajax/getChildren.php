9e0068cdee36446151a44cc74e3f5cae2aa1d5a8
= <naftoli@tzivoshashem.org>
2022-02-13 15:37:11 -0500

    update pages to include subsidy request

--- mashpia.com/public/chidonOld/chidon_drive/ajax/getChildren.php
+++ mashpia.com/public/chidonOld/chidon_drive/ajax/getChildren.php
@@ -16,7 +16,8 @@ function getChildren() {
     $children = [];
     // track, raised, grade, trip location
     $sql = "select u.user_id, u.school_id, u.class_id, u.mobile_pic, u.user_photo_id, u.first, u.last, c.class_grade as grade, 
-                s.chidon_confirmed_5782 as schoolConfirmed, tc.th_chidon_id, tc.reward_type, IFNULL(cus.subsidy_amount, 0) as raised
+                s.chidon_confirmed_5782 as schoolConfirmed, tc.th_chidon_id, tc.reward_type, tc.payment_request, 
+                IFNULL(cus.subsidy_amount, 0) as raised
             from users u 
             join schools s using (school_id)
             join th_chidon tc using (user_id)  
