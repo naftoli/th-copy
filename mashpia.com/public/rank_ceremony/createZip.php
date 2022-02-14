0e4ff1ae971ea89ffae3181ce38fa2267f00336b
= <naftoli@tzivoshashem.org>
2022-02-14 12:41:49 -0500

    debug rank ceremony

--- mashpia.com/public/rank_ceremony/createZip.php
+++ mashpia.com/public/rank_ceremony/createZip.php
@@ -49,13 +49,13 @@ $images = extractFiles($list2);
 $filename = "Data.zip";
 createZip($files, $images, $filename);
 
-header('Content-Description: File Transfer');
-header('Content-Type: application/octet-stream');
-header('Content-Disposition: attachment; filename="' . basename($filename) . '"');
-header('Expires: 0');
-header('Cache-Control: must-revalidate');
-header('Pragma: public');
-header('Content-Length: ' . filesize($filename));
-flush(); // Flush system output buffer
-readfile($filename);
-unlink($filename);
+//header('Content-Description: File Transfer');
+//header('Content-Type: application/octet-stream');
+//header('Content-Disposition: attachment; filename="' . basename($filename) . '"');
+//header('Expires: 0');
+//header('Cache-Control: must-revalidate');
+//header('Pragma: public');
+//header('Content-Length: ' . filesize($filename));
+//flush(); // Flush system output buffer
+//readfile($filename);
+//unlink($filename);
