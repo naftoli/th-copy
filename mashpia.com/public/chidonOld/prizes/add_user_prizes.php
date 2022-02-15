4f8a6e02bd812f45e54d1b4ef364f1172ca31629
= <naftoli@tzivoshashem.org>
2022-02-14 19:29:58 -0500

    script to add to user prizes

---
+++ mashpia.com/public/chidonOld/prizes/add_user_prizes.php
@@ -0,0 +1,64 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL);
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/db.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/class.globalSettings.php';
+$year = GlobalSettings::getChidonYear();
+
+if (isset($_POST['submit'])) {
+    if (isset($_FILES['user_prizes'])) {
+        $qrys = [];
+        $file = $_FILES['user_prizes']['tmp_name'];
+        if (($handle = fopen($file, "r")) !== false) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== false) {
+                $serial_num = $data[0];
+                $prize = $data[1];
+                $he_name = $data[2];
+
+                if (strpos($prize, ',') !== false) {
+                    $prizes = explode(',', $prize);
+                } else {
+                    $prizes = [$prize];
+                }
+
+                foreach ($prizes as $prize_id) {
+                    $sql = "insert into chidon_user_prizes 
+                            set user_id = (select user_id from users where user_serial = $serial_num), 
+                            year = $year, 
+                            prize_id = $prize_id";
+                    if ($he_name) $sql .= ", he_name = '$he_name'";
+                    $qrys[] = $sql;
+                }
+            }
+        }
+        mysql_query('set autocommit=0');
+        mysql_query("begin");
+        $success = true;
+        foreach ($qrys as $qry) {
+            if (! mysql_query($qry)) {
+                $success = false;
+                break;
+            }
+        }
+        if ($success) mysql_query("commit");
+        else mysql_query("rollback");
+        mysql_query("set autocommit=1");
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf8">
+    <title>Add to user prizes</title>
+</head>
+<body>
+    <?php if (! isset($_POST['submit'])) : ?>
+    <form action="" method="post" enctype="multipart/form-data">
+        Select file to upload: <input type="file" name="user_prizes" />
+        <input type="submit" name="submit" value="submit">
+    </form>
+    <?php endif; ?>
+</body>
+</html>
