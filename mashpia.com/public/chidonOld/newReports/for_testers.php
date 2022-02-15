4271c3646cd69de5b5b576b07e7a525bbc40c113
= <naftoli@tzivoshashem.org>
2022-02-15 16:49:52 -0500

    create report for testers

---
+++ mashpia.com/public/chidonOld/newReports/for_testers.php
@@ -0,0 +1,153 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL);
+
+require $_SERVER['DOCUMENT_ROOT'] . '/db.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/class.globalSettings.php';
+$year = GlobalSettings::getChidonRegYear();
+
+function getRegInfo() {
+    global $reg, $year;
+
+    $sqlReg = "select u.user_id, u.first, u.last, tc.paid, tc.date_paid, tc.payment_request, tc.parent_id 
+            from users u 
+            join th_chidon tc using (user_id) 
+            where tc.date_paid > 0 
+            and tc.year = " . $year;
+    $resReg = mysql_query($sqlReg);
+    while ($rowReg = mysql_fetch_assoc($resReg)) {
+        $reg[] = $rowReg;
+    }
+}
+
+function getShippingInfo() {
+    global $shipping;
+
+    $sql = "select admin_id, first, last, chidon_shipping_paid 
+            from admins 
+            where chidon_shipping_paid > 0";
+    $result = mysql_query($result);
+    while ($row = mysql_fetch_assoc($result)) {
+        $shipping[] = $row;
+    }
+}
+
+function getExtraPurchases() {
+    global $extra_purchases, $year;
+
+    $sqlExtra = "select p.*, a.admin_id, a.first, a.last 
+            from extra_purchases p 
+            join admins a using (admin_id) 
+            where year = " . $year;
+    $resExtra = mysql_query($sqlExtra);
+    while ($rowExtra = mysql_fetch_assoc($resExtra)) {
+        $extra_purchases[] = $rowExtra;
+    }
+}
+
+function getAddresses() {
+    global $addresses;
+
+    $sql = "select * from purchase_addresses";
+    $result = mysql_query($sql);
+    while ($row = mysql_fetch_assoc($result)) {
+        $addresses[$row['purchase_id']] = $row;
+    }
+}
+
+$reg = [];
+$shipping = [];
+$extra_purchases = [];
+$addresses = [];
+
+getRegInfo();
+getExtraPurchases();
+getAddresses();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf8" />
+    <title>Chidon Report</title>
+    <style>
+        tr, th, td {
+            font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+            font-size: 12px;
+            padding: 5px;
+            border-bottom: 1px solid grey;
+        }
+    </style>
+</head>
+<body>
+    <h1>Chidon Report</h1>
+    <table>
+        <caption>Registered Report</caption>
+        <tr>
+            <th>Parent ID</th>
+            <th>User ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Subsidy</th>
+            <th>Paid</th>
+            <th>Date Paid</th>
+        </tr>
+        <?php
+        foreach ($reg as $row) {
+            echo "<tr><td>" . $row['parent_id'] . "</td><td>" . $row['user_id'] . "</td><td>" . $row['first'] .
+                "</td><td>" . $row['last'] . "</td><td>" . $row['payment_request'] . "</td><td>" . $row['paid'] .
+                "</td><td>" . $row['date_paid'] . "</td></tr>";
+        }
+        ?>
+    </table>
+    <br /><br />
+    <table>
+        <caption>Shipping Paid</caption>
+        <tr>
+            <th>Parent ID</th>
+            <th>First</th>
+            <th>Last</th>
+            <th>Amount Paid</th>
+        </tr>
+        <?php
+        foreach ($shipping as $row) {
+            echo "<tr><td>" . $row['admin_id'] . "</td><td>" . $row['first'] . "</td><td>" . $row['last'] . "</td><td>" .
+                "</td><td>" . $row['chidon_shipping_paid'] . "</td></tr>";
+        }
+        ?>
+    </table>
+    <br /><br />
+    <table>
+        <caption>Extra Purchases</caption>
+        <tr>
+            <th>Parent ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Item Purchased</th>
+            <th>Amount purchased</th>
+            <th>Sweater Type</th>
+            <th>Sweater Size</th>
+            <th>Needs Shipping</th>
+            <th>Shipping Paid</th>
+            <th>Address</th>
+        </tr>
+        <?php
+        foreach ($extra_purchases as $purchase) {
+            echo "<tr><td>" . $purchase['admin_id'] . "</td><td>" . $purchase['first'] . "</td><td>" . $purchase['last'] .
+                "</td><td>" . $purchase['item'] . "</td><td>" . $purchase['amount'] . "</td><td>" . $purchase['type_of_sweater'] .
+                "</td><td>" . $purchase['size'] . "</td><td>";
+            if (intval($purchase['shipping_amount'])) echo 'yes';
+            else echo 'no';
+            echo "</td><td>" . $purchase['shipping_amount'] . "</td>";
+            if (intval($purchase['shipping_amount'])) {
+                $ship_info = $addresses[$purchase['purchase_id']];
+                echo "<td>" . $ship_info['address'] . "<br />" . $ship_info['city'] . ' ' . $ship_info['state'] . ' ' .
+                    $ship_info['zip'] . "<br />" . $ship_info['country'] . "</td>";
+            } else {
+                echo "<td></td>";
+            }
+            echo "</tr>";
+        }
+        ?>
+    </table>
+</body>
+</html>
