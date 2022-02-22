d9ce9d248f55c2532b8888f81d85a680c4b71162
= <naftoli@tzivoshashem.org>
2022-02-22 13:54:11 -0500

    fix eigible report

--- mashpia.com/public/chidonOld/chidon_drive/newReports/eligibile_unregistered.php
+++ mashpia.com/public/chidonOld/chidon_drive/newReports/eligibile_unregistered.php
@@ -23,8 +23,9 @@ function getRegInfo() {
             join classes c on c.class_id = u.class_id 
             where paid is null  
             and tc.year = " . $year;
-    if ($admin_user['auth'] != 'super') $sqlReg .= " and school_id in (" . implode(',', array_keys($schools)) . ")";
+    if ($admin_user['auth'] != 'super') $sqlReg .= " and u.school_id in (" . implode(',', array_keys($schools)) . ")";
     $sqlReg .= " order by s.school_name, c.class_grade, c.class_sub, last, first";
+    echo $sqlReg;
     $resReg = mysql_query($sqlReg);
     while ($rowReg = mysql_fetch_assoc($resReg)) {
         $reg[] = $rowReg;
