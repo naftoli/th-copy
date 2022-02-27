2409e2f233b16803a582bffb70e83bba3b6c27d5
= <naftoli@tzivoshashem.org>
2022-02-27 13:51:55 -0500

    match qry in eligibility_registered_report.php to awards report

--- mashpia.com/public/chidonOld/newReports/eligibility_registered_report.php
+++ mashpia.com/public/chidonOld/newReports/eligibility_registered_report.php
@@ -18,13 +18,15 @@ $year = GlobalSettings::getChidonYear();
 $info = [];
 foreach ($schools as $school_id => $name) {
     $sql = "select u.user_id, u.school_id, u.class_id, u.user_serial, u.first, u.last, s.school_name, c.class_grade,    
-                c.class_sub, tc.date_paid, tc.khk_trip, tc.th_chidon_id, tc.test_type, tc.reward_type  
+                c.class_sub, tc.date_paid, tc.khk_trip, tc.th_chidon_id, tc.test_type, tc.reward_type, tci.highest_track  
             from users u 
             join schools s using (school_id) 
             join classes c on u.class_id = c.class_id 
             join th_chidon tc using (user_id) 
+            left join th_chidon_info tci on tc.year = tci.year and tc.user_id = tci.user_id 
             where tc.year = " . $year . " and u.school_id = " . $school_id . " 
             order by school_name, date_paid, class_grade, class_sub, last, first";
+//    echo $sql;
     $result = mysql_query($sql);
     while ($row = mysql_fetch_assoc($result)) {
         $info[] = $row;
@@ -62,7 +64,6 @@ foreach ($schools as $school_id => $name) {
         </tr>
         <?php
         $test = new ChidonTests();
-        $tracks = $test->getTypes();
         $rewards = [
             'Yesod'     => 'Sweater, Gifts & Yesod Final',
             'Yediah'    => 'Sweater, Gifts, Prizes, & Yediah Final',
@@ -72,28 +73,17 @@ foreach ($schools as $school_id => $name) {
         ];
         $i = 0;
         foreach ($info as $row) {
-            $highestTrack = $test->getHighestTrackPassed($row)['highest_track'];
-            $rewardType = $row['reward_type'];
-            if ($rewardType != 'highest track passed') {
-                if ($highestTrack == '') $highestTrack = $rewardType;
-                else {
-                    $indexes = array_keys($tracks);
-                    $key1 = array_search($highestTrack, $indexes);
-                    $key2 = array_search($rewardType, $indexes);
-                    if ($key2 > $key1) $highestTrack = $rewardType;
-                }
-            }
-
-            $track = $highestTrack ? $tracks[$highestTrack] : 'none';
-            if (intval($row['class_grade']) === 8 && (in_array(strtolower($track), ['havonah', 'iyun']))) $track = 'Khk Trip';
-            $reward = $track != 'none' ? $rewards[$track] : 'none';
-            if ($track == 'Khk Trip') $reward .= $tracks[$highestTrack] . ' Final';
+            $track = ucwords($row['highest_track']);
+            if (intval($row['class_grade']) === 8 && (in_array(strtolower($highestTrack), ['havonah', 'iyun']))) $highestTrack = 'Khk Trip';
+            else $highestTrack = $track;
+            $reward = empty($highestTrack) ? 'none' : $rewards[$highestTrack];
+            if ($highestTrack == 'Khk Trip') $reward .= ucwords($track) . ' Final';
             $school = $schools[$row['school_id']];
             $class = $row['class_grade'] . (empty($row['class_sub']) ? '' : '-' . $row['class_sub']);
             $paid = $row['date_paid'] > 0 ? 'yes' : 'no';
             $khk = $row['khk_trip'] > 0 ? 'yes' : '';
             echo "<tr><td>" . $school . "</td><td>" . $class . "</td><td>" . $row['user_serial'] .  "</td><td>" .
-                $row['first'] . "</td><td>" . $row['last'] . "</td><td>" . $track . "</td><td>" . $reward . "</td><td>" .
+                $row['first'] . "</td><td>" . $row['last'] . "</td><td>" . $highestTrack . "</td><td>" . $reward . "</td><td>" .
                 $paid . "</td><td>" . $khk . "</td></tr>";
         }
         ?>
