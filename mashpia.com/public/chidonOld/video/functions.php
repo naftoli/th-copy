d412b07b64fe8416948c596d2acfd7a0e742cfa6
= <naftoli@tzivoshashem.org>
2022-03-20 17:30:38 -0400

    update code for spreadsheet

--- mashpia.com/public/chidonOld/video/functions.php
+++ mashpia.com/public/chidonOld/video/functions.php
@@ -183,7 +183,7 @@ function createSpreadSheet($children) {
         if (isset($info[$track])) {
             foreach ($info[$track] as $child) {
                 $school_name = preg_replace('/\s+/', ' ', $child['school_name']);
-                $school_location = preg_replace('/\s+/', ' ', ($child['school_city'] . ', ' . $child['school_state']));
+                $school_location = preg_replace('/\s+/', ' ', ($child['school_city'] . ",<br>" . $child['school_state']));
                 $school_logo = 'School_' . $child['school_id'];
                 if ($child['gender'] == 'M') $school_logo .= '_b';
                 else if ($child['gender'] == 'F') $school_logo .= '_g';
@@ -267,7 +267,7 @@ function addToSheet($child) {
     $school_logo = '';
     if ($child['school_id'] == 269) {
         $school_name = preg_replace('/\s+/', ' ', $child['non_th_school']);
-        $school_location = preg_replace('/\s+/', ' ', ($child['non_th_city'] . ', ' . $child['non_th_state']));
+        $school_location = preg_replace('/\s+/', ' ', ($child['non_th_city'] . ",<br>" . $child['non_th_state']));
         $school_logo = 'School_' . $child['non_th_school_id'];
         if ($child['gender'] == 'M') $school_logo .= '_b';
         else if ($child['gender'] == 'F') $school_logo .= '_g';
