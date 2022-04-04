a592c815e84d1b2b84f796d2b406e19c3f765da6
= <naftoli@tzivoshashem.org>
2022-04-04 17:00:38 -0400

    new report for 5782 that shows all info and bc can choose if it is missing

---
+++ mashpia.com/public/chidonOld/newReports/missingReports/chidon_report_all.php
@@ -0,0 +1,97 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL);
+
+$admin_auth = ['school'];
+require_once $_SERVER['DOCUMENT_ROOT'] . "/header.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/class.adminSchools.php";
+$as = new AdminSchools($admin_user['admin_id'], $admin_user['auth'], true, true);
+$schools = $as->getSchools();
+
+require 'functions.php';
+
+$users = getUsers();
+$chidonUsers = getChidonUsers();
+$usersLastYr = getChidonUsersLastYr();
+$recruitments = getRecruitments();
+$recruitmentPrizes = getRecruitmentPrizes();
+$surpriseGifts = getSurpriseGifts();
+$prizes = getPrizes();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Full Chidon Report</title>
+        <link href="../../../admin_styles.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <?php include('../../../admin_header.php'); ?>
+        <h1>Full Chidon Report</h1>
+        <?php
+        foreach ($users as $school_id => $more) {
+            echo "<h2>" . $schools[$school_id] . "</h2>";
+            foreach ($more as $user) {
+                // find out if we need to show this child
+                $chidon = false;
+                $recruitment = false;
+                $surprise = false;
+
+                // check if child in chidon
+                $key = array_search($user['user_id'], $chidonUsers);
+                if ($key !== false) {
+                    $chidon = true;
+                    $userInfo = $chidonUsers[$key];
+                }
+
+                // check recruitments
+                $ids = array_keys($recruitments);
+                if (array_search($user['user_id'], $ids) !== false) $recruitment = $recruitments[$user['user_id']];
+                else if (array_search($user['user_serial'], $ids) !== false) $recruitment = $recruitments[$user['user_serial']];
+
+                // check surprise gifts
+                if (array_search($user['user_id'], $surpriseGifts) !== false) $surprise = true;
+
+                if ($chidon || $surprise || $recruitment) {
+                    $grade = $user['class_grade'] . ($user['class_sub'] ? '-' . $user['class_sub'] : '');
+                    $name = $user['first'] . ' ' . $user['last'];
+                    $school = $schools[$school_id];
+
+                    echo "<b>Name: " . $name . "</b><br />";
+                    echo "Serial: " . $user['user_serial'] . "<br />";
+                    echo "School: " . $school . "<br />";
+
+                    if ($recruitment) {
+                        $prize = $recruitmentPrizes[$recruitment];
+                        echo "<br />Recruitment Prize: " . $prize;
+                        if ($recruitment == 3) {
+                            if ($user['gender'] == 'M') echo " Navy";
+                            else if ($user['gender'] == 'F') echo "Burgundy";
+                        }
+                        echo "<br />";
+                    }
+
+                    if ($surprise) {
+                        echo "<br />Surprise Gift: Chavat Book<br />";
+                    }
+
+                    if ($chidon) {
+                        echo "<br />Chidon Gift: " . getGift($user) . "<br />";
+                        // prizes
+                        echo "<br />Prizes:<br />";
+                        foreach ($prizes[$user['user_id']] as $prize) {
+                            $pName = $prize['prize_name'];
+                            $pColor = $prize['color'];
+                            $pSize = $prize['size'];
+                            $desc = $pName;
+                            if ($pColor) $desc .= ' ' . $pColor;
+                            if ($pSize) $desc .= ' ' . $pSize;
+                            echo $desc . "<br />";
+                        }
+                    }
+                }
+            }
+        }
+        ?>
+    </body>
+</html>
---
+++ mashpia.com/public/chidonOld/newReports/missingReports/functions.php
@@ -0,0 +1,120 @@
+<?php
+function getUsers() {
+    global $schools;
+
+    $info = [];
+    $sql = "select * from users u 
+            join schools s using (school_id) 
+            join classes c on c.class_id = u.class_id 
+            where c.class_grade in (4,5,6,7,8)";
+    if (! empty($schools)) $sql .= " and s.school_id in (" . implode(',', array_keys($schools)) . ")";
+    $sql .= "order by school_id, class_grade, class_sub, last, first";
+    $result = mysql_query($sql);
+    while ($row = mysql_fetch_assoc($result)) {
+        $info[$row['school_id']][] = $row;
+    }
+    return $info;
+}
+
+function getChidonUsers() {
+    global $year, $schools;
+
+    $users = [];
+    $sql = "select * from th_chidon where year = " . $year . " and date_paid > 0";
+    if (! empty($schools)) $sql .= " and school_id in (" . implode(',', array_keys($schools)) . ")";
+    $result = mysql_query($sql);
+    while ($row = mysql_fetch_assoc($result)) {
+        $users[$row['user_id']] = $row;
+    }
+    return $users;
+}
+
+function getChidonUsersLastYr() {
+    global $year, $schools;
+
+    $lastYr = $year - 1;
+    $users = [];
+    $sql = "select u.first, u.last, u.user_id, u.user_serial, u.gender, tc.* 
+            from users u 
+            join th_chidon tc using (user_id) 
+            where tc.year = " . $lastYr;
+    if (! empty($schools)) $sql .= " and u.school_id in (" . implode(',', array_keys($schools)) . ")";
+    $result = mysql_query($sql);
+    while ($row = mysql_fetch_assoc($result)) {
+        $users[$row['user_id']] = $row;
+    }
+    return $users;
+}
+
+function getRecruitmentPrizes() {
+    $prizes = [];
+    $sql = "select * from chidon_credit_prizes";
+    $result = mysql_query($sql);
+    while ($row = mysql_fetch_assoc($result)) {
+        $prizes[$row['credits']] = $row['prize'];
+    }
+    return $prizes;
+}
+
+function getRecruitments() {
+    global $year;
+
+    $recruits = [];
+    $sql = "select recruited_by, count(*) as total from th_chidon where year = " . $year;
+    $result = mysql_query($sql);
+    while ($row = mysql_fetch_assoc($result)) {
+        $recruits[$row['recruited_by']] = $row['total'];
+    }
+    return $recruits;
+}
+
+function getSurpriseGifts() {
+    global $year;
+
+    $gifts = [];
+    $lastYr = $year - 1;
+    $sql = "select user_id from th_chidon where year = " . $lastYr . " and surprise_gift = 1";
+    $result = mysql_query($sql);
+    while ($row = mysql_fetch_assoc($result)) {
+        $gifts[] = $row['user_id'];
+    }
+    return $gifts;
+}
+
+function getPrizes() {
+    global $year;
+
+    $prizes = [];
+    $sql = "SELECT 
+                *
+            FROM
+                chidon_user_prizes cup
+                    JOIN
+                chidon_prizes cp USING (prize_id)
+            WHERE
+                cup.year = " . $year;
+    $result = mysql_query($sql);
+    while ($row = mysql_fetch_assoc($result)) {
+        $prizes[$row['user_id']][] = $row;
+    }
+    return $prizes;
+}
+
+function getGift($user) {
+    $gift = '';
+    if ($user['date_paid'] > 0) {
+        switch ($user['gender']) {
+            case 'M':
+                $gift = 'Yarmulka - Size: ' . $user['yarmulka'];
+                break;
+            case 'F':
+                $gift = 'Chidon Necklace';
+                break;
+        }
+    }
+    return $gift;
+}
+
+
+
+
