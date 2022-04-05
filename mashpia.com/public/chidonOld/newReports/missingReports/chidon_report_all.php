0feeb44400226b0b2e492c17d5b1939bbff7fb1d
= <naftoli@tzivoshashem.org>
2022-04-04 21:20:50 -0400

    debug new chidon report for all things

--- mashpia.com/public/chidonOld/newReports/missingReports/chidon_report_all.php
+++ mashpia.com/public/chidonOld/newReports/missingReports/chidon_report_all.php
@@ -21,20 +21,13 @@ $recruitmentPrizes = getRecruitmentPrizes();
 $surpriseGifts = getSurpriseGifts();
 $prizes = getPrizes();
 $awards = getAwards();
-echo "<pre>";
-echo "Users:";
-//print_r($users);
-echo "Chidon Users:";
-print_r($chidonUsers);
-//echo "Recruits:";
-//print_r($recruitments);
-//echo "Surprise Gift:";
-//print_r($surpriseGifts);
-//echo "Prizes:";
-//print_r($prizes);
-//echo "Awards:";
-//print_r($awards);
-echo "</pre>";
+
+echo "Users: " . count($users) . "<br />";
+echo "Chidon Users: " . count($chidonUsers) . "<br />";
+echo "Recruits: " . count($recruitments) . "<br />";
+echo "Surprise Gift: " . count($surpriseGifts) . "<br />";
+echo "Prizes: " . count($prizes) . "<br />";
+echo "Awards: " . count($awards) . "<br />";
 exit;
 ?>
 <!DOCTYPE html>
