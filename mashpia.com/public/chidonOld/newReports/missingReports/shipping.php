e700138232b8be08b509655c6c6c5be3d40e8c68
= <naftoli@tzivoshashem.org>
2022-04-06 18:03:41 -0400

    update shipping report for missing items

--- mashpia.com/public/chidonOld/newReports/missingReports/shipping.php
+++ mashpia.com/public/chidonOld/newReports/missingReports/shipping.php
@@ -50,6 +50,8 @@ $itemTypes = [
     <?php
     foreach ($itemsBySchool as $school_id => $schoolItems) {
         echo "<h2>" . $schools[$school_id] . "</h2>";
+        echo "<hr />";
+        echo "<h3>Summary</h3>";
         $info = getItemSummary($schoolItems);
 //        $details = getItemDetails();
         ?>
