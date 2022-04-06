ec410ddfe6ff02624dbcb34e68ae829cbe301bf6
= <naftoli@tzivoshashem.org>
2022-04-06 12:32:06 -0400

    debug missing items hq report

--- mashpia.com/public/chidonOld/newReports/missingReports/functions.php
+++ mashpia.com/public/chidonOld/newReports/missingReports/functions.php
@@ -228,7 +228,7 @@ function getAllMissingItems() {
     $sql = "select * from chidon_missing_items where year = " . $year;
     $result = mysql_query($sql);
     while ($row = mysql_fetch_assoc($result)) {
-        $items[$row['user_id']] = json_decode($row['items']);
+        $items[$row['user_id']] = $row['items'];
     }
     echo "<pre>"; print_r($items); echo "</pre>";
     return $items;
