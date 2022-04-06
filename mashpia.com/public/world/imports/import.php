a4ecade3ee43ad01e5105b26e67e0bb87a8a9acc
= <naftoli@tzivoshashem.org>
2022-04-06 18:28:40 -0400

    work on importing tanya / mishna lines

--- mashpia.com/public/world/imports/import.php
+++ mashpia.com/public/world/imports/import.php
@@ -1,3 +1,3 @@
 <?php
-$info = file_get_contents("https://chabadkid.com/getuser.php?mashpia=mashpia_mbp_all");
+$info = json_decode(file_get_contents("https://chabadkid.com/getuser.php?mashpia=mashpia_mbp_all"));
 echo "<pre>"; print_r($info); echo "</pre>";
