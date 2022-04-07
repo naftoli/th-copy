ace7f19e67c1bd67287b36e722d3ded01640fb4d
= <naftoli@tzivoshashem.org>
2022-04-07 19:12:30 -0400

    debug issues with checking off

--- mashpia.com/public/chidonOld/newReports/missingReports/chidon_report_all2.php
+++ mashpia.com/public/chidonOld/newReports/missingReports/chidon_report_all2.php
@@ -108,7 +108,7 @@ function isMissing($missing, $desc, $value = '') {
             $("#save").click( function (e) {
                 e.preventDefault()
                 console.log(missing)
-                $.post('saveMissing.php', { missing: JSON.stringify(missing) }, function (success) {
+                $.post('saveMissing2.php', { missing: JSON.stringify(missing) }, function (success) {
                     if (parseInt(success)) alert('Saved.')
                     else alert('Error Saving.')
                 })
---
+++ mashpia.com/public/chidonOld/newReports/saveMissing2.php
@@ -0,0 +1,29 @@
+<?php
+$admin_auth = ['school'];
+require $_SERVER['DOCUMENT_ROOT'] . "/header.php";
+require $_SERVER['DOCUMENT_ROOT'] . "/class.globalSettings.php";
+$year = GlobalSettings::getChidonYear();
+
+$qrys = [];
+$missing = json_decode($_POST['missing']);
+foreach ($missing as $user_id => $items) {
+    if (empty($items)) $sql = "delete from chidon_missing_items where user_id = " . $user_id . " and year = " . $year;
+    else $sql = "insert into chidon_missing_items set user_id = " . mysql_real_escape_string($user_id) . ", items = '" .
+        json_encode($items) . "', year = " . $year . " on duplicate key update items = '" . json_encode($items) . "'";
+    $qrys[] = $sql;
+}
+
+$success = true;
+mysql_query('set autocommit=0');
+mysql_query('begin');
+foreach ($qrys as $qry) {
+    if (! mysql_query($qry)) {
+        $success = false;
+        break;
+    }
+}
+if ($success) mysql_query('commit');
+else mysql_query('rollback');
+mysql_query('set autocommit=1');
+
+echo $success ? 1 : 0;
