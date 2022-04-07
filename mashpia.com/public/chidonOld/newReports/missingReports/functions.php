8dde169437982ffe88b1735ec0babcfdf74eda5b
= <naftoli@tzivoshashem.org>
2022-04-07 15:41:21 -0400

    fix up awards issue on form for all

--- mashpia.com/public/chidonOld/newReports/missingReports/functions.php
+++ mashpia.com/public/chidonOld/newReports/missingReports/functions.php
@@ -341,7 +341,7 @@ function getItemDesc($item, $desc, $user_id) {
             break;
         case 'chidon_prize':
             $prize = $chidonPrizes[$item];
-            $extra_info = $userPrizes[$user_id];
+//            $extra_info = $userPrizes[$user_id];
             $item = $prize['prize_name'] . ' ' . $prize['size'] . ' ' . $prize['color'];
 //            if ($extra_info['he_name']) $item .= ' - ' . $extra_info['he_name'];
             break;
