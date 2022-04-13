a4c0c9bb3637114d1452f19eb6063ccbd45d119d
= <naftoli@tzivoshashem.org>
2022-04-13 14:57:52 -0400

    remove addToMissing from comments

--- mashpia.com/public/chidonOld/newReports/missingReports/chidon_report_all2.php
+++ mashpia.com/public/chidonOld/newReports/missingReports/chidon_report_all2.php
@@ -322,6 +322,7 @@ foreach ($users as $school_id => $more) {
                     }
                 }
                 echo "<br />Comments:<br /><textarea cols='50' rows='5' class='comments'>";
+                $comments = isMissing($missing, 'comments');
                 if ($comments) echo $comments;
                 echo "</textarea><br /><br /></div>";
             }
