e8202ab3fc9d6f7d26fa8b884f9320e16bc91204
= <naftoli@tzivoshashem.org>
2022-04-13 16:59:02 -0400

    rollback using addslashes

--- mashpia.com/public/chidonOld/newReports/missingReports/saveMissing2.php
+++ mashpia.com/public/chidonOld/newReports/missingReports/saveMissing2.php
@@ -9,11 +9,9 @@ $missing = json_decode($_POST['missing']);
 foreach ($missing as $user_id => $items) {
     if (empty($items)) $sql = "delete from chidon_missing_items where user_id = " . $user_id . " and year = " . $year;
     else $sql = "insert into chidon_missing_items set user_id = " . mysql_real_escape_string($user_id) . ", items = '" .
-        json_encode(addslashes($items)) . "', year = " . $year . " on duplicate key update items = '" .
-        json_encode(addslashes($items)) . "'";
+        json_encode($items) . "', year = " . $year . " on duplicate key update items = '" . json_encode($items) . "'";
     $qrys[] = $sql;
 }
-echo "<pre>"; print_r($qrys); echo "</pre>"; exit;
 
 $success = true;
 mysql_query('set autocommit=0');
