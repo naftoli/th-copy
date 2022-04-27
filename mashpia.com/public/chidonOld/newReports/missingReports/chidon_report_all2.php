1cf1d591adaa86def35e4db8da5d35fbf3d0594e
= <naftoli@tzivoshashem.org>
2022-04-27 16:34:09 -0400

    debug chidon missing page

--- mashpia.com/public/chidonOld/newReports/missingReports/chidon_report_all2.php
+++ mashpia.com/public/chidonOld/newReports/missingReports/chidon_report_all2.php
@@ -324,6 +324,7 @@ foreach ($users as $school_id => $more) {
                 }
                 echo "<br />Comments:<br /><textarea cols='50' rows='5' class='comments'>";
                 $comments = isMissing($missing, 'comments');
+                if ($comments) echo $comments;
 //                if ($comments) echo str_replace('u0027', "'", $comments);
                 echo "</textarea><br /><br /></div>";
             }
