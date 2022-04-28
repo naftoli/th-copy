b4b6aa0c3a88429a55182c10d880dca2cfc3c238
= <naftoli@tzivoshashem.org>
2022-04-27 23:54:42 -0400

    fix errors

--- mashpia.com/public/chidonOld/newReports/missingReports/shipping.php
+++ mashpia.com/public/chidonOld/newReports/missingReports/shipping.php
@@ -14,7 +14,7 @@ $schools = $as->getSchools();
 require 'functions.php';
 
 $items = getAllMissingItems();
-$users = getMissingUsers($items);
+$users = getMissingUsers();
 $itemsBySchool = getItemsBySchool();
 $recruitmentPrizesById = getRecruitmentPrizesById();
 $chidonPrizes = getChidonPrizes();
