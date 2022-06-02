fc3c1f5a6283c0ddd383bf099ff02d3f81b05475
= <naftoli@tzivoshashem.org>
2022-06-02 10:29:16 -0400

    peronalize slides for OT

--- mashpia.com/public/medal_ceremony/slidesOTmore.php
+++ mashpia.com/public/medal_ceremony/slidesOTmore.php
@@ -5,7 +5,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/header.php';
 $prevMedals = false;
 $prevMedalsLight = false;
 
-$start = 2459649;
+$start = 2459621;
 $end = 2459718;
 
 require_once 'class.slides.php';
