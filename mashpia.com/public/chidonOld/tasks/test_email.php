d8507335fa07011998fe0f3fa83b731ea8f338aa
= <naftoli@tzivoshashem.org>
2022-06-19 18:37:45 -0400

    working on recruits getting emails

--- mashpia.com/public/chidonOld/tasks/test_email.php
+++ mashpia.com/public/chidonOld/tasks/test_email.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL);
+
 require_once $_SERVER['DOCUMENT_ROOT'] . '/chidonOld/classes/recruits.php';
 $r = new Recruits(7745260);
 $error = $r->sendEmail('Michel Rapoport');
