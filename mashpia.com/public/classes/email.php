558af33e45fd4b409dd1359f100137ba2772c7ed
= <naftoli@tzivoshashem.org>
2022-06-20 14:55:15 -0400

    test email class

--- mashpia.com/public/classes/email.php
+++ mashpia.com/public/classes/email.php
@@ -1,9 +1,9 @@
 <?php
 //Import PHPMailer classes into the global namespace
 //These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer as PHPMailer;
+use PHPMailer\PHPMailer\SMTP as SMTP;
+use PHPMailer\PHPMailer\Exception as Exception;
 
 //Load Composer's autoloader
 require_once __DIR__ . '/../../vendor/autoload.php';
@@ -22,7 +22,7 @@ class Email {
 //            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
 //            $mail->Username   = '_mainaccount@mashpia.com';             //SMTP username
 //            $mail->Password   = 'Naftoli8770!';                         //SMTP password
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+//            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
             $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
             //Recipients
