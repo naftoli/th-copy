71a0457fc83c37e6a8519346c29953fca2e52242
= <naftoli@tzivoshashem.org>
2022-06-20 12:15:40 -0400

    fix working on recruits getting emails

--- mashpia.com/public/classes/email.php
+++ mashpia.com/public/classes/email.php
@@ -6,7 +6,7 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader
-require_once 'vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 class Email {
     private $error;
