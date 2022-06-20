d3aa36f0871a8cf7e073a13f21d5b9ebe80b5323
= <naftoli@tzivoshashem.org>
2022-06-20 14:53:20 -0400

    test email class

--- mashpia.com/public/chidonOld/tasks/test_email.php
+++ mashpia.com/public/chidonOld/tasks/test_email.php
@@ -2,7 +2,18 @@
 ini_set('display_errors', 1);
 ini_set('error_reporting', E_ALL);
 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/chidonOld/classes/recruits.php';
-$r = new Recruits(7745260);
-$error = $r->sendEmail('Michel Rapoport');
-if (intval($error) != 1) echo $error;
+//require_once $_SERVER['DOCUMENT_ROOT'] . '/chidonOld/classes/recruits.php';
+//$r = new Recruits(7745260);
+//$error = $r->sendEmail('Michel Rapoport');
+//if (intval($error) != 1) echo $error;
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/classes/email.php';
+
+$email = new Email();
+$params = [
+    'to'    => 'naftolir@gmail.com',
+    'subject'   => 'Testing emailing',
+    'msg'   => 'Hi There',
+    'from'  => 'chidon@tzivoshashem.org'
+];
+$email->sendEmail($params);
--- mashpia.com/public/classes/email.php
+++ mashpia.com/public/classes/email.php
@@ -19,9 +19,9 @@ class Email {
             $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
             $mail->isSMTP();                                            //Send using SMTP
             $mail->Host       = 'smtp.mashpia.com';                     //Set the SMTP server to send through
-            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = '_mainaccount@mashpia.com';             //SMTP username
-            $mail->Password   = 'Naftoli8770!';                         //SMTP password
+//            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+//            $mail->Username   = '_mainaccount@mashpia.com';             //SMTP username
+//            $mail->Password   = 'Naftoli8770!';                         //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
             $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
