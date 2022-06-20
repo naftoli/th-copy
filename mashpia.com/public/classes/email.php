1895f3914733a9c23a851149adbda657b6093c96
= <naftoli@tzivoshashem.org>
2022-06-20 12:56:34 -0400

    fix working on recruits getting emails

--- mashpia.com/public/classes/email.php
+++ mashpia.com/public/classes/email.php
@@ -15,6 +15,34 @@ class Email {
         //Create an instance; passing `true` enables exceptions
         $mail = new PHPMailer(true);
         try {
+            //Server settings
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host       = 'smtp.mashpia.com';                     //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username   = '_mainaccount@mashpia.com';             //SMTP username
+            $mail->Password   = 'Naftoli8770!';                         //SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+            $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+            //Recipients
+//            $mail->setFrom('from@example.com', 'Mailer');
+//            $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
+//            $mail->addAddress('ellen@example.com');               //Name is optional
+//            $mail->addReplyTo('info@example.com', 'Information');
+//            $mail->addCC('cc@example.com');
+//            $mail->addBCC('bcc@example.com');
+
+            //Attachments
+//            $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+//            $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+            //Content
+//            $mail->isHTML(true);                                  //Set email format to HTML
+//            $mail->Subject = 'Here is the subject';
+//            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+//            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
             if (isset($params['fromAlias'])) $mail->setFrom($params['from'], $params['fromAlias']);
             else $mail->setFrom($params['from']);
             $mail->addAddress($params['to']);
