a6686da820f200860f17951d63417e38890e1352
= <naftoli@tzivoshashem.org>
2022-06-24 15:20:21 -0400

    updates to reports to be able to choose year

--- mashpia.com/public/chidonOld/newReports/book_report.php
+++ mashpia.com/public/chidonOld/newReports/book_report.php
@@ -85,6 +85,7 @@ while ($row = mysql_fetch_assoc($result)) {
     ?>
 </table>
 </body>
+<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
 <script>
     $("#year").change( function () {
         let yr = $(this).val()
--- mashpia.com/public/chidonOld/newReports/reg_report.php
+++ mashpia.com/public/chidonOld/newReports/reg_report.php
@@ -189,6 +189,7 @@ $types = [
             ?>
         </table>
     </body>
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
     <script>
         $("#year").change( function () {
             let yr = $(this).val()
