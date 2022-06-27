9c79e6d6ae78a9edca1131660597fa7416d39017
= <naftoli@tzivoshashem.org>
2022-06-27 14:16:24 -0400

    updates to khk eligibility report

--- mashpia.com/public/chidonOld/newReports/khk_eligibility.php
+++ mashpia.com/public/chidonOld/newReports/khk_eligibility.php
@@ -114,7 +114,7 @@ for (; $i > 0; $i--) {
                 $grade = $child['class_grade'] . (empty($child['class_sub']) ? '' : '-' . $child['class_sub']);
                 echo "<tr id='" . $child['user_id'] . "'><td>" . $child['user_serial'] . "</td><td>" . $school .
                     "</td><td>" . $grade . "</td><td>" . $child['first'] . ' ' . $child['last'] . "</td>";
-                foreach ($yrsDetails[$user_id] as $yr => $value) {
+                foreach ($yrsDetails[$user_id] as $value) {
                     echo "<td>" . ($value ? "&#10003;" : "&#10007;") . "</td>";
                 }
                 echo "<td><input type='checkbox' class='eligibility' name='eligibility[" . $child['user_id'] . "]' ";
