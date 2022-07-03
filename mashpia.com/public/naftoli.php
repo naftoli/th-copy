ff7ee48c2f52ceb3d9eb9ee58fa485093fd0b8a3
naftoli rapoport <naftolir@gmail.com>
2022-07-02 23:11:18 -0400

    fix only show phpinfo if naftoli cookie is set

---
+++ mashpia.com/public/naftoli.php
@@ -0,0 +1,3 @@
+<?php
+if (! isset($_COOKIE['naftoli'])) setcookie('naftoli', '1');
+echo "Cookie set.";
--- mashpia.com/public/phpinfo.php
+++ mashpia.com/public/phpinfo.php
@@ -1,8 +1,3 @@
 <?php
-if (setcookie("naftoli", 1)) {
-    echo "cookie set";
-} else {
-    echo "error setting cookie";
-}
-phpinfo();
+if (isset($_COOKIE['naftoli'])) phpinfo();
 ?> 
