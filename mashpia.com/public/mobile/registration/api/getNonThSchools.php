14d315d822039b18e2c076be81d5092428768519
naftoli rapoport <naftolir@gmail.com>
2022-07-10 15:43:53 -0400

    sort non th schools by school name

--- mashpia.com/public/mobile/registration/api/getNonThSchools.php
+++ mashpia.com/public/mobile/registration/api/getNonThSchools.php
@@ -1,6 +1,6 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/db.php';
-$sql = "select * from non_th_schools";
+$sql = "select * from non_th_schools order by school_name";
 $result = mysql_query($sql);
 while ($row = mysql_fetch_assoc($result)) {
     $schools[$row['non_th_school_id']] = $row['school_name'];
