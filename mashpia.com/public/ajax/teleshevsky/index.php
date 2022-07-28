d0a3a0638580934f62147fa9bb30d7fd05dc3e28
naftoli rapoport <naftolir@gmail.com>
2022-07-28 14:45:21 -0400

    api for teleshevsky

---
+++ mashpia.com/public/ajax/teleshevsky/index.php
@@ -0,0 +1,31 @@
+<?php
+// authenticate
+if (isset($_POST['auth']) && $_POST['auth'] === 'JTaMd105nT' && isset($_POST['school'])) {
+    require '../db.php';
+    $school_number = mysql_real_escape_string($_POST['school']);
+    $info = [];
+    $sql = "
+        SELECT 
+            u.first,
+            u.last,
+            u.first_he,
+            u.last_he,
+            u.dob,
+            u.user_serial AS serial_number,
+            u.user_code AS barcode,
+            c.class_grade,
+            c.class_sub
+        FROM
+            users u
+                JOIN
+            schools s USING (school_id)
+                JOIN
+            classes c ON c.class_id = u.class_id
+        WHERE
+            u.school_number = " . $school_number;
+    $result = mysql_query($sql);
+    while ($row = mysql_fetch_assoc($result)) {
+        $info[] = $row;
+    }
+    echo json_encode($info);
+}
