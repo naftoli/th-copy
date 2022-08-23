498be18e8e87efad099729243271828db0a6c09c
naftoli rapoport <naftolir@gmail.com>
2022-08-23 17:49:59 -0400

    fix saving prizes

--- mashpia.com/public/mobile/registration/api/getPrizes.php
+++ mashpia.com/public/mobile/registration/api/getPrizes.php
@@ -5,24 +5,21 @@ $year = GlobalSettings::getChidonRegYear();
 $year = 5783;
 
 $user_id = mysql_real_escape_string($_POST['user_id']);
-$user_prizes = [];
-$sql = "select * from chidon_user_prizes where user_id = " . $user_id . " and year = " . $year;
-$result = mysql_query($sql);
-while ($row = mysql_fetch_assoc($result)) {
-    $user_prizes[] = $row['prize_id'];
-}
 
 $prizes = [];
 $sql = "select * from chidon_prizes  
         where year = " . $year;
 $result = mysql_query($sql);
 while ($row = mysql_fetch_assoc($result)) {
-    if (in_array($row['prize_id'], $user_prizes)) $row['selected'] = 1;
-    else $row['selected'] = 0;
     $prizes[] = $row;
 }
 
 foreach ($prizes as $idx => $prize) {
+    $sql = "select * from chidon_user_prizes where user_id = " . $user_id . " and prize_id = " . $prize['prize_id'];
+    $result = mysql_query($sql);
+    if (mysql_num_rows($result) > 0) $prizes[$idx]['selected'] = 1;
+    else $prizes[$idx]['selected'] = 0;
+
     $sql = "select count(*) as total 
             from chidon_user_prizes 
             where year = " . $year . " 
