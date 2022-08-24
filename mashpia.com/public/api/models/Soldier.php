50f51024272c0c43812cb9d65de86f3d6ce99941
naftoli rapoport <naftolir@gmail.com>
2022-08-24 13:27:02 -0400

    add user for testing

--- mashpia.com/public/api/models/Soldier.php
+++ mashpia.com/public/api/models/Soldier.php
@@ -548,7 +548,7 @@ class Soldier extends \ActiveRecord\Model implements \JsonSerializable {
     public function regYears() {
         $chayolei_year = GlobalSettings::getRegistrationYear( $this->school_id );
         $chidon_year = GlobalSettings::getChidonRegYear();
-        if (in_array($this->user_id, [5455, 19085, 62881])) {
+        if (in_array($this->user_id, [5455, 19085, 62881, 19274])) {
             $chidon_year = 5783;
         }
         return [
