be656539618548d751f78fa4e22b1e8686e9638c
naftoli rapoport <naftolir@gmail.com>
2022-08-24 12:36:11 -0400

    fix up code for myshliach terms

--- mashpia.com/public/mobile/registration/js/register.js
+++ mashpia.com/public/mobile/registration/js/register.js
@@ -667,7 +667,7 @@ var registrationApp = function() {
             // check myshliach term if relevant
             if (selected_user.school_id == myshliach) {
                 if (! $("#myshliach").is(":checked")) {
-                    errors.push("You must indicate your acceptance of all Terms (6th term not checked).")
+                    errors.push("You must indicate your acceptance of all Terms (6th term not checked)")
                 }
             }
             console.log(errors)
