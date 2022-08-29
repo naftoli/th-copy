c72f886aa69f9b0e4655b8f4c8a5324e9d11d513
naftoli rapoport <naftolir@gmail.com>
2022-08-29 17:28:01 -0400

    temp remove two schools from list of australian schools

--- mashpia.com/public/class.globalSettings.php
+++ mashpia.com/public/class.globalSettings.php
@@ -200,6 +200,7 @@ class GlobalSettings {
 
     public static function isAustralian( $school_id ) {
         $australian = [ 55, 66, 110, 112, 180, 256, 643, 709, 713 ];
+        $australian = [ 55, 66, 110, 112, 180, 256, 643 ];
         return in_array( $school_id, $australian );
     }
 
