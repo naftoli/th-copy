0ef842ef65c285ee7fa05db8c4173b8bf4800159
naftoli rapoport <naftolir@gmail.com>
2022-08-30 18:47:30 -0400

    update school ratios for raffle prizes

--- mashpia.com/public/raffles/shared/classes/Constants.php
+++ mashpia.com/public/raffles/shared/classes/Constants.php
@@ -8,7 +8,7 @@ class Constants{
         return 175;
     }
     
-    public static function get_raffle_school_max_winners() { // array of id => max. Sum of all maxes must equal 178
+    public static function get_raffle_school_max_winners() { // array of id => max. Sum of all maxes must equal 175
         return [
             2   =>  3,
             3   =>  2,
