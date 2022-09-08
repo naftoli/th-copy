2ed4715153c7e35a0f7e1c637183f21bf2d76dab
naftoli rapoport <naftolir@gmail.com>
2022-09-08 13:12:40 -0400

    create cron job to delete admin_auths associations that point to missing child

---
+++ mashpia.com/cron/syncDeleted.php
@@ -0,0 +1,10 @@
+<?php
+// load up the database connection
+include_once( __DIR__ . '/../public_html/api/header/db.php' );
+
+// create cron job to delete admin_auths associations that point to missing child
+$sql = "delete aa.* from admin_auths aa 
+        left join users u on u.user_id = aa.id 
+        where aa.auth = 'user' 
+        and u.user_id is null";
+echo mysql_query($sql);
