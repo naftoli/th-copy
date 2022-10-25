a1740a9cc03863844509c2b233bbf245a967cb2c
naftoli rapoport <naftolir@gmail.com>
2022-10-25 16:46:03 -0400

    fix up reviewing certs

---
+++ mashpia.com/public/mobile/reg/ajax/getHeName.php
@@ -0,0 +1,20 @@
+<?php
+$admin_auth = ['user'];
+require_once $_SERVER['DOCUMENT_ROOT'] . "/header.php";
+
+$sql = "select first_he, last_he from users where user_id = " . mysql_real_escape_string($_POST['user_id']);
+$result = mysql_query($sql);
+if ($result) {
+    $row = mysql_fetch_assoc($result);
+    $name = $row['first_he'] . ' ' . $row['last_he'];
+    if (!empty(trim($name))) {
+        echo json_encode([
+            'success' => true,
+            'heName' => $name
+        ]);
+        exit;
+    }
+}
+echo json_encode([
+    'success'   => false
+]);
