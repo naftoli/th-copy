a41ed625c542a192f03d36d830cfb37643d83054
naftoli rapoport <naftolir@gmail.com>
2022-11-05 21:09:19 -0400

    update game purchases qry git push ; ;

--- mashpia.com/public/mobile/reg/ajax/gamePurchases.php
+++ mashpia.com/public/mobile/reg/ajax/gamePurchases.php
@@ -5,20 +5,17 @@
 require '../../../db.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/class.globalSettings.php';
 $year = GlobalSettings::getRegistrationYear();
+$item_id = 4;
 
 $query ="
     SELECT 
-        COUNT(*) as total
+        count(*) as total 
     FROM
         mashpia_purchases.purchase_details
+            JOIN
+        mashpia_purchases.purchases USING (purchase_id)
     WHERE
-        item_id = 4
-            AND purchase_id IN (SELECT 
-                purchase_id
-            FROM
-                mivtzoim_purchases.purchases
-            WHERE
-                year = $year)";
+        year = $year AND item_id = " . $item_id;
 $result = mysql_query($query);
 $row = mysql_fetch_assoc($result);
 echo intval($row['total']);
