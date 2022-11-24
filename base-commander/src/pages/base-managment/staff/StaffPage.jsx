ac0dad8e8afba6c829330c98ffe750faf3b693fa
naftoli rapoport <naftolir@gmail.com>
2022-11-24 14:02:41 -0500

    add staff id to downloadable cvs

--- base-commander/src/pages/base-managment/staff/StaffPage.jsx
+++ base-commander/src/pages/base-managment/staff/StaffPage.jsx
@@ -33,13 +33,13 @@ class StaffPage extends Component {
     if ( isAdmin( this.props.login.code ) ) {
       headers = [
         'Username',   'First Name', 'Last Name', 'E-mail Address',
-        'Cell Phone', 'Position',   'Platoon', 'Base', 'Base ID'
+        'Cell Phone', 'Position',   'Platoon', 'Base', 'Base ID', 'Staff ID'
       ];
       rows = this.props.staff.map( staff => [
         staff.username,   staff.first,  staff.last,
         staff.email,      staff.cell,   staff.position,
         staff.platoon,    staff.school_name,
-        staff.school_id
+        staff.school_id,  staff.admin_id
       ]);
     } else {
       headers = [
