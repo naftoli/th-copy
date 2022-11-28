c02b15703f6bbae845bd47ada1ae6cf9ec2b4f4d
naftoli rapoport <naftolir@gmail.com>
2022-11-28 18:02:48 -0500

    enable marking chidon marks through api

--- mashpia.com/public/ajax/chabadkid/chidon/marks/index.php
+++ mashpia.com/public/ajax/chabadkid/chidon/marks/index.php
@@ -1,1 +1,30 @@
 <?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/db.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/class.globalSettings.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/chidonTests/class.chidonTests.php';
+
+$ct = new ChidonTests();
+$year = GlobalSettings::getChidonYear();
+
+$marks = [];
+$info = json_decode($_POST['info']);
+foreach ($info as $row) {
+    $serial = $row['serial_number'];
+    // find out th_chidon_id
+    $sql = "select th_chidon_id from th_chidon where year = $year and user_id = (
+            select user_id from users where user_serial = $serial)";
+    $result = mysql_query($sql);
+    if (mysql_num_rows($result) > 0) {
+        $id = mysql_fetch_assoc($result)['th_chidon_id'];
+        $details = [
+            'maven'     => $row['yesod'],
+            'pro'       => $row['yediah'],
+            'expert'    => $row['havonah'],
+            'genius'    => $row['iyun']
+        ];
+        foreach ($details as $type => $number) {
+            $marks[$id][$row['test_number']][$type] = $number;
+        }
+    }
+}
+echo $ct->insertScores($marks);
