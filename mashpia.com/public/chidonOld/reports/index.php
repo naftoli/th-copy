2ecaef19b1236b2eb094776a4d79cb98cd10adba
Naftoli Rapoport <naftoli@tzivoshashem.org>
2022-12-01 19:10:44 +0000

    more debugging

--- mashpia.com/public/chidonOld/reports/index.php
+++ mashpia.com/public/chidonOld/reports/index.php
@@ -162,6 +162,7 @@ require_once '../../class.globalSettings.php';
 $year = GlobalSettings::getChidonYear();
 
 if (isset($_POST['submit'])) {	
+	header("Content-type: text/html; charset=utf-8");
     $data = array();
 	$gender = false;
 	$limit = false;
@@ -258,9 +259,9 @@ if (isset($_POST['submit'])) {
 <!DOCTYPE html>
 <html>
 	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<title>Chidon Reports</title>
 		<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs-3.3.7/jqc-1.12.4/dt-1.10.13/cr-1.3.2/fc-3.2.2/fh-3.1.2/r-2.1.1/sc-1.4.2/se-1.2.0/datatables.min.css"/>
 		<style>
