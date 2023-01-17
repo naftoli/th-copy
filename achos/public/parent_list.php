edbf9d1db01cf202afe84ea2d2765c5abbf4ed7c
naftoli rapoport <naftolir@gmail.com>
2023-01-17 13:25:13 -0500

    make parent list easier to see all the info

--- achos/public/parent_list.php
+++ achos/public/parent_list.php
@@ -5,24 +5,24 @@ require('header.php');
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<link href="admin_styles.css" rel="stylesheet" type="text/css">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Student Accounts</title>
 <style>
-table {
-	width: 100%;
-}
-tr, th, td {
-	vertical-align: text-top;
-	border: 1px solid black;
-	padding: 2px;
-}
+  body, table {
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  table {
+    width: 100%;
+  }
+  tr, th, td {
+    vertical-align: text-top;
+    border: 1px solid black;
+    padding: 2px;
+  }
 </style>
 </head>
 
 <body>
-<? include('admin_header.php');?>
-
 <h1>Student Accounts</h1>
 <table border="1" cellspacing="1" style="font-size:12px">
 <tr>
