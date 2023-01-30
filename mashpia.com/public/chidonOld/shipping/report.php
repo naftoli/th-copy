a69aab1d69169e2c063f73bbc909fe17972b96a5
naftoli rapoport <naftolir@gmail.com>
2023-01-29 22:08:00 -0500

    updates to shipping module

--- mashpia.com/public/chidonOld/shipping/report.php
+++ mashpia.com/public/chidonOld/shipping/report.php
@@ -171,7 +171,7 @@ foreach ($results as $row) {
           // now show items
           foreach ($items_chosen as $cat => $more) {
               echo "<td>";
-              if (isset($info[$cat]) && in_array($row['user_id'], $info[$cat])) echo 'yes';
+              if (isset($info[$cat]) && in_array($row['user_id'], array_keys($info[$cat]))) echo 'yes';
               echo "</td>";
           }
           echo "</tr>";
