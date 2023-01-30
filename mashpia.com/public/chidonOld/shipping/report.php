63789be42938392211a3f5431f7606bbb02d1139
naftoli rapoport <naftolir@gmail.com>
2023-01-29 22:30:20 -0500

    updates to shipping module

--- mashpia.com/public/chidonOld/shipping/report.php
+++ mashpia.com/public/chidonOld/shipping/report.php
@@ -61,7 +61,8 @@ foreach ($tables as $table) {
   if ($table == 'u') continue;
   $sql .= $tableAliases[$table] . " ";
 }
-$sql .= " WHERE tc.year = " . $year;
+$sql .= " WHERE 1 ";
+if (in_array('tc', $tables)) $sql .= " AND tc.year = " . $year;
 if ($_POST['school'] > 0) $sql .= " AND u.school_id = " . $_POST['school'];
 if ($_POST['gender'] == 'm') $sql .= " AND u.gender = '" . $_POST['gender'] . "'";
 else if ($_POST['gender'] == 'f') $sql .= " AND u.gender = '" . $_POST['gender'] . "'";
