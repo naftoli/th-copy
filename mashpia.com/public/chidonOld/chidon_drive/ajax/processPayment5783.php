7984769f67e50933def49aa93f9258d18c161dcd
naftoli rapoport <naftolir@gmail.com>
2023-02-01 12:34:37 -0500

    debug processPayment5783.php

--- mashpia.com/public/chidonOld/chidon_drive/ajax/processPayment5783.php
+++ mashpia.com/public/chidonOld/chidon_drive/ajax/processPayment5783.php
@@ -385,6 +385,7 @@ function saveUltimateTripInfo() {
                 'user'  => $user_id,
                 'year'  => $year
             ]);
+            $stmt->debugDumpParams();
             if (!$res) {
                 $success = false;
                 break;
