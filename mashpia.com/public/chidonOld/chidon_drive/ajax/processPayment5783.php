157c4ea78f352e1e791031c2fded56fd9e1d5285
naftoli rapoport <naftolir@gmail.com>
2023-02-01 11:07:33 -0500

    debug processPayment5783.php

--- mashpia.com/public/chidonOld/chidon_drive/ajax/processPayment5783.php
+++ mashpia.com/public/chidonOld/chidon_drive/ajax/processPayment5783.php
@@ -536,7 +536,7 @@ if ($registered && $shippingUpdated && $celebBoxesProcessed && $sweatersProcesse
 // send email confirmation
 if ($info['success']) {
     $msg = getEmailMsg($trans_id);
-//    echo $msg;
+    echo $msg;
     if (!sendEmail($msg)) {
         $headers[] = 'MIME-Version: 1.0';
         $headers[] = 'Content-type: text/html; charset=iso-8859-1';
