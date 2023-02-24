09bc980f94a80b283fe6d6dba5b0f8571922d5f3
Naftoli Rapoport <naftoli@tzivoshashem.org>
2023-02-24 15:46:09 +0000

    debugging shipping for myshliach

--- mashpia.com/public/chidonOld/shipping/data.php
+++ mashpia.com/public/chidonOld/shipping/data.php
@@ -197,18 +197,16 @@ function createCSV($items) {
         'Address Line 1', 'Address Line 2', 'Address Line 3', 'City', 'State', 'Postal Code', 'Country Code', 'CHI Number',
         'Full Item Name', 'Quantity', 'Child Name - Serial #'];
     foreach ($children as $user_id => $admin_id) {
-        foreach ($info[$user_id] as $details) {
-            foreach ($details as $item) {
-                echo "User: " . $user_id . ", Admin: " . $admin_id . ", Item: " . print_r($item) . "<br />";
-                $admin = $admins[$admin_id];
-                $phone = $admin['admin_phone_mobile'] ?? $admin['admin_phone_work'] ?? $admin['admin_phone_home'] ?? '';
-                $user = $users[$user_id];
-                $school = $user['school_id'] == 61 ? 'MyShliach - Shipping' : 'Anash Kinder - Pickup';
-                $csv[$i++] = [$admin_id, ($admin['first'] . ' ' . $admin['last']), $admin['first'], $admin['last'],
-                    $phone, $school, $admin['admin_address1'], $admin['admin_address2'], '', $admin['admin_city'],
-                    $admin['admin_state'], $admin['admin_postal'], $admin['admin_country'], $item['id'], $item['item'],
-                    $item['qty'], ($user['first'] . ' ' . $user['last'] . ' - ' . $user['user_serial'])];
-            }
+        foreach ($info[$user_id] as $item) {
+            echo "User: " . $user_id . ", Admin: " . $admin_id . ", Item: " . print_r($item) . "<br />";
+            $admin = $admins[$admin_id];
+            $phone = $admin['admin_phone_mobile'] ?? $admin['admin_phone_work'] ?? $admin['admin_phone_home'] ?? '';
+            $user = $users[$user_id];
+            $school = $user['school_id'] == 61 ? 'MyShliach - Shipping' : 'Anash Kinder - Pickup';
+            $csv[$i++] = [$admin_id, ($admin['first'] . ' ' . $admin['last']), $admin['first'], $admin['last'],
+                $phone, $school, $admin['admin_address1'], $admin['admin_address2'], '', $admin['admin_city'],
+                $admin['admin_state'], $admin['admin_postal'], $admin['admin_country'], $item['id'], $item['item'],
+                $item['qty'], ($user['first'] . ' ' . $user['last'] . ' - ' . $user['user_serial'])];
         }
     }
     return $csv;
