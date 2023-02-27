5d3b6212ebb3bd629c08902852d0e927e98838e9
naftoli rapoport <naftolir@gmail.com>
2023-02-27 14:51:24 -0500

    updates to teleshevsky marking for final

--- mashpia.com/public/ajax/chabadkid/chidon/marks/index.php
+++ mashpia.com/public/ajax/chabadkid/chidon/marks/index.php
@@ -6,18 +6,57 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/chidonTests/class.chidonTests.php';
 $ct = new ChidonTests();
 $year = GlobalSettings::getChidonYear();
 
-$marks = [];
-$info = $_POST['scores'];
-foreach ($info as $id => $more) {
-    foreach ($more as $test_num => $scores) {
-        foreach ($scores as $type => $mark) {
-            $marks[$id][$test_num]["$type"] = $mark;
+$test_num = intval($_POST['test_num']);
+if ($test_num == 4) {
+    $levels = [
+        'maven'   => 1,
+        'pro'     => 2,
+        'expert'  => 3,
+        'genius'  => 4
+    ];
+
+    $qrys = [];
+    $info = $_POST['scores'];
+    foreach ($info as $id => $more) {
+        $user_id = getUserID($id);
+        foreach ($more as $test_num => $scores) {
+            foreach ($scores as $type => $mark) {
+                $mark = intval($mark);
+                $level = 'level_' . $levels[$type];
+                $sql = "insert into th_chidon_finals 
+                        set year = $year, 
+                        user_id = $user_id, 
+                        $level = $mark
+                        on duplicate key update $level = $mark";
+                $qrys[] = $sql;
+            }
         }
     }
-}
 
-$test_num = $_POST['test_num'];
-$success = $ct->insertScores($marks);
+    $success = true;
+    mysql_query('set autocommit=0');
+    mysql_query('begin');
+    foreach ($qrys as $qry) {
+        if (! mysql_query($qry)) {
+            $success = false;
+            break;
+        }
+    }
+    if ($success) mysql_query('commit');
+    else mysql_query('rollback');
+    mysql_query('set autocommit=1');
+} else {
+    $marks = [];
+    $info = $_POST['scores'];
+    foreach ($info as $id => $more) {
+        foreach ($more as $test_num => $scores) {
+            foreach ($scores as $type => $mark) {
+                $marks[$id][$test_num]["$type"] = $mark;
+            }
+        }
+    }
+    $success = $ct->insertScores($marks);
+}
 ?>
 <!DOCTYPE html>
 <html>
