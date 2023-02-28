a33ba1ec1a751b28260a72c41f713ac09f699bfc
naftoli rapoport <naftolir@gmail.com>
2023-02-28 15:38:09 -0500

    debug reps

--- mashpia.com/public/chidonTests/class.chidonTests.php
+++ mashpia.com/public/chidonTests/class.chidonTests.php
@@ -86,7 +86,7 @@ class ChidonTests
         if ($this->genderOnly) $qry .= " AND u.gender = :gender";
         // order by
         $qry .= " ORDER BY school_name, class_grade, class_sub, last, first";
-        echo $qry;
+        echo $qry . "<br />";
         $stmt = $this->db->prepare($qry);
         if ($this->genderOnly) {
             if ($school_id > 0 && $class_id > 0 && $user_id > 0) {
