5fc288d668df438987ddd2385491b73fa93e6bad
naftoli rapoport <naftolir@gmail.com>
2023-03-01 18:49:04 -0500

    update setting item as not yet shipped

--- mashpia.com/public/chidonOld/shipping/ajax/saveShipping.php
+++ mashpia.com/public/chidonOld/shipping/ajax/saveShipping.php
@@ -24,46 +24,40 @@ $sql = "INSERT IGNORE INTO th_chidon_shipping
             damaged = :damaged";
 $stmt = $MASHPIA_DB->prepare($sql);
 
-$delete = "DELETE FROM th_chidon_shipping WHERE user_id = :id AND year = :year";
-$stmtDel = $MASHPIA_DB->prepare($delete);
-
 $MASHPIA_DB->beginTransaction();
 $success = true;
 foreach ($info as $row) {
     // figure out shipped / missing / damaged
-    $action = intval($row['action']);
-    if ($action == 0) {
-        $res = $stmtDel->execute([
-            'year'  => $year,
-            'id'    => $row['user']
-        ]);
-    } else {
-        switch ($action) {
-            case 1:
-                $shipped = 1;
-                $missing = 0;
-                $damaged = 0;
-                break;
-            case 2:
-                $shipped = 1;
-                $missing = 1;
-                $damaged = 0;
-                break;
-            case 3:
-                $shipped = 1;
-                $missing = 0;
-                $damaged = 1;
-                break;
-        }
-        $res = $stmt->execute([
-            'year' => $year,
-            'user' => $row['user'],
-            'item' => $row['item'],
-            'shipped' => $shipped,
-            'missing' => $missing,
-            'damaged' => $damaged
-        ]);
+    switch (intval($row['action'])) {
+        case 0:
+            $shipped = 0;
+            $missing = 0;
+            $damaged = 0;
+            break;
+        case 1:
+            $shipped = 1;
+            $missing = 0;
+            $damaged = 0;
+            break;
+        case 2:
+            $shipped = 1;
+            $missing = 1;
+            $damaged = 0;
+            break;
+        case 3:
+            $shipped = 1;
+            $missing = 0;
+            $damaged = 1;
+            break;
     }
+    $res = $stmt->execute([
+        'year'      => $year,
+        'user'      => $row['user'],
+        'item'      => $row['item'],
+        'shipped'   => $shipped,
+        'missing'   => $missing,
+        'damaged'   => $damaged
+    ]);
     if (! $res) {
 //        $stmt->debugDumpParams();
         $success = false;
