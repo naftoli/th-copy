b77fd0b1e685a5a1816a90e74098d3c0ebbbec6d
naftoli rapoport <naftolir@gmail.com>
2023-02-28 19:10:20 -0500

    updates to reps

--- mashpia.com/public/chidonTests/setRep.php
+++ mashpia.com/public/chidonTests/setRep.php
@@ -3,7 +3,7 @@ $admin_auth = ['school'];
 require $_SERVER['DOCUMENT_ROOT'] . '/header.php';
 
 $chidon_id = mysql_real_escape_string($_POST['id']);
-$value = mysql_real_escape_string(intval($_POST['val']));
+$value = mysql_real_escape_string($_POST['val']);
 $field = mysql_real_escape_string($_POST['field']);
 
 $teams = ['Sefer Hamitzvos', 'Mishna Torah', 'Moreh Nevuchim', 'Pirush Hamishnayos', 'Igeres Horambam'];
