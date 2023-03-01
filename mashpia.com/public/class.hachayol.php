42984b3bfc9693c67b89ae5d5633389ab35ec0ae
naftoli rapoport <naftolir@gmail.com>
2023-02-28 21:11:57 -0500

    add school exceptions to hachayols

--- mashpia.com/public/class.hachayol.php
+++ mashpia.com/public/class.hachayol.php
@@ -16,7 +16,7 @@ class Hachayol {
         $this->schoolDetails = array();
         $this->chidonYear = GlobalSettings::getChidonYear();
         $this->chidonNumbers = array();
-        $this->schoolExceptions = [55, 66, 110, 112, 180, 256, 584, 585, 588, 612];
+        $this->schoolExceptions = [55, 66, 110, 112, 180, 256, 584, 585, 588, 612, 432, 713, 709, 427, 434, 690, 480];
     }
     
     public function setSchools( $id = null ) {
