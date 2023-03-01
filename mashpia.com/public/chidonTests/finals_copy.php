20a6b5afb31817614e827a509694dac8dad523f3
naftoli rapoport <naftolir@gmail.com>
2023-03-01 11:42:05 -0500

    files for Mendel

---
+++ mashpia.com/public/chidonTests/finals_copy.php
@@ -0,0 +1,282 @@
+<?php
+//ini_set('display_errors', 1);
+$admin_auth = ['school'];
+require $_SERVER['DOCUMENT_ROOT'] . '/header.php';
+
+require $_SERVER['DOCUMENT_ROOT'] . '/class.adminSchools.php';
+$as = new AdminSchools( $admin_user['admin_id'], $admin_user['auth'], true, true ); // add chidon schools
+$schools = $as->getSchools();
+
+require $_SERVER['DOCUMENT_ROOT'] . '/class.globalSettings.php';
+$year = GlobalSettings::getChidonYear();
+
+require $_SERVER['DOCUMENT_ROOT'] . '/chidonTests/class.chidonTests.php';
+$ct = new ChidonTests();
+
+// save marks
+if (isset($_POST['submit'])) {
+    $qrys = [];
+    for ($i = 1; $i <= 4; $i++) {
+        $level = 'level_' . $i;
+        foreach ($_POST[$level] as $id => $mark) {
+            if ($mark != '') {
+                $mark = intval($mark);
+                $qrys[] = "insert into th_chidon_finals 
+                            set year = $year, 
+                            user_id = $id, 
+                            $level = $mark
+                            on duplicate key update $level = $mark";
+            }
+        }
+    }
+    if (isset($_POST['khk'])) {
+        foreach ($_POST['khk'] as $id => $mark) {
+            if ($mark != '') {
+                $mark = intval($mark);
+                $qrys[] = "insert into th_chidon_finals 
+                            set year = $year, 
+                            user_id = $id, 
+                            khk = $mark
+                            on duplicate key update khk = $mark";
+            }
+        }
+    }
+
+    mysql_query('set autocommit=0');
+    mysql_query('begin');
+    $success = true;
+    foreach ($qrys as $qry) {
+        if (! mysql_query($qry)) {
+            $success = false;
+            break;
+        }
+    }
+    if ($success) mysql_query('commit');
+    else {
+        mysql_query('rollback');
+        echo "Not Saved. Error in qry: " . $qry . "<br />";
+    }
+    mysql_query('set autocommit=1');
+}
+
+$marks = [];
+$sql = "select * from th_khk_marks";
+$result = mysql_query($sql);
+while ($row = mysql_fetch_assoc($result)) {
+    $marks[$row['th_chidon_id']][$row['test_number']] = $row['mark'];
+}
+
+$final_marks = [];
+$sql = "select * from th_chidon_finals where year = " . $year;
+$result = mysql_query($sql);
+while ($row = mysql_fetch_assoc($result)) {
+    $final_marks[$row['user_id']] = $row;
+}
+
+function passedKhk($id) {
+    global $marks;
+
+    if (isset($marks[$id])) {
+        $user_marks = $marks[$id];
+        $total = 0;
+        foreach ($user_marks as $mark) $total += intval($mark);
+        $total /= 4;
+        if ($total >= 70) return true;
+    }
+    return false;
+}
+
+function getAward($child) {
+    global $final_marks;
+
+    $tracks = [
+        1   => 'yesod',
+        2   => 'yediah',
+        3   => 'havonah',
+        4   => 'iyun'
+    ];
+    $finals = [
+        'yesod'     => 20,
+        'yediah'    => 40,
+        'havonah'   => 60,
+        'iyun'      => 80
+    ];
+    $needed = [
+        'yesod'     => 60,
+        'yediah'    => 70,
+        'havonah'   => 80,
+        'iyun'      => 90
+    ];
+    $awards = [
+        'yesod'     => 'certificate',
+        'yediah'    => 'plaque',
+        'havonah'   => 'medal / plaque',
+        'iyun'      => 'trophy / medal / plaque'
+    ];
+
+    $highest_track = $child['highest_track'];
+    // find out if award is same as before final or not
+    $award = false;
+    $key = array_search($highest_track, $tracks);
+    if ($key !== false) {
+        // go down from key to find where the child is holding
+        if (isset($final_marks[$child['user_id']])) {
+            $row = $final_marks[$child['user_id']];
+            $score = 0;
+            for ($i = 1; $i <= $key; $i++) {
+                $level = 'level_' . $i;
+                if ($row[$level]) {
+                    $score += $row[$level];
+                }
+            }
+            for ($i = 1; $i <= $key; $i++) {
+                $divide_by = $finals[$tracks[$i]];
+                $final_score = number_format(($score / $divide_by) * 100, 2);
+                if ($final_score >= $needed[$tracks[$i]]) {
+                    $award = $tracks[$i];
+                }
+            }
+        }
+    }
+    if ($award) return $awards[$award];
+    else return 'no award yet';
+}
+
+$info = [];
+foreach ($schools as $id => $school) {
+    $ct->setStudents($id);
+    $info[$id] = $ct->getStudents();
+}
+// initialize all tests to not be disabled
+$tooLate = false;
+// disable marking after certain dates for bc's
+if ($admin_user['auth'] != 'super') {
+    $today = new DateTime();
+    $shutdown = new DateTime('2023-03-06 19:20:00');
+    if ($today >= $shutdown) {
+        $tooLate = true;
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Enter Test Score</title>
+    <link href="../admin_styles.css" rel="stylesheet" type="text/css">
+    <style>
+      tr, th, td {
+        font-size: 14px;
+        padding: 5px;
+      }
+      td:not(.type) {
+        vertical-align: top;
+      }
+      body {
+        display: none;
+      }
+      .mark, .khk {
+        width: 50px;
+      }
+      input:disabled {
+        background: #ccc;
+      }
+    </style>
+</head>
+<body>
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/admin_header.php'); ?>
+<h1>Enter Test Score</h1>
+<div class="infobox">Please enter the <strong>number</strong> of questions scored correctly. The system will calculate the correct mark.</div>
+<?php
+$types = $ct->getTypes();
+$levels = array_values($types);
+echo "<form action='finals.php' method='post' enctype='multipart/form-data'>";
+echo "<div style='float: right'><input type='submit' name='submit' value='Save' style='padding: 12px; font-size: large' /></div><br /><br />";
+foreach ($info as $school => $children) {
+    if (empty($children)) continue;
+    echo "<h2>" . $schools[$school] . "</h2>";
+    echo "<table><tr><th>Serial Number</th><th>Grade</th><th>Student</th><th>Highest Track</th>";
+    foreach ($types as $old => $new) {
+        echo "<th>$new</th>";
+    }
+    echo "<th>KHK Final</th>";
+    echo "<th>Award</th>";
+    echo "</tr>";
+    foreach ($children as $child) {
+        if ($child['date_paid'] > 0) {
+            $grade = $child['class_grade'] . (empty($child['class_sub']) ? '' : '-' . $child['class_sub']);
+            $name = $child['first'] . ' ' . $child['last'];
+            $id = $child['user_id'];
+            echo "<tr><td>" . $child['user_serial'] . "</td><td>" . $grade . "</td><td>" . $name . "</td><td>" .
+                $child['highest_track'] . "</td>";
+            for ($i = 1; $i <= 4; $i++) {
+                // find out which level the child can go up to
+                $key = array_search(ucwords($child['highest_track']), $levels);
+                $key++;
+                // create the proper input box
+                $level = 'level_' . $i;
+                echo "<td>" . $final_marks[$id][$level] . "</td>";
+            }
+            // add khk_final
+            // check if child should be able to take the khk final
+            $disabled = 'disabled';
+            if (intval($child['khk_reg']) && passedKhk($child['th_chidon_id']) && !$tooLate) $disabled = '';
+            echo "<td>" . $final_marks[$id]['khk'] . "</td>";
+            echo "<td>" . getAward($child) . "</td></tr>";
+        }
+    }
+    echo "</table>";
+}
+echo "<div style='float: right'><input type='submit' name='submit' value='Save' style='padding: 12px; font-size: large' /></div>";
+echo "</form>";
+?>
+</body>
+<script>
+  $(function() {
+    // BCM IA wants to have the page only show when entering a password. not secure but makes her believe it's secure.
+    const school_id = <?=$admin_user['auths']['school'][0]?>;
+    if (school_id == 176) {
+      // password protect
+      const password = 'laky';
+      let pass = '';
+      while (pass != password) {
+        pass = prompt('Please enter password.');
+      }
+    }
+    $('body').show();
+      <?php if (! isset($_POST['submit'])) : ?>
+    alert('Please make sure to SAVE after entering scores.');
+      <?php endif; ?>
+  })
+
+  $(".mark").focus( function() {
+    let val = $(this).val()
+    if (parseInt(val) == 0) {
+      $(this).val('')
+    }
+  })
+
+  $(".mark").blur(function () {
+    const amount = $(this).val()
+    if (amount) {
+      const max = 20
+      if (amount > max) {
+        alert('You cannot enter a number greater than ' + max)
+        $(this).val('')
+        $(this).focus()
+      }
+    } else {
+      if (amount == '') $(this).val(0)
+    }
+  })
+  $(".khk").blur(function () {
+    const amount = $(this).val()
+    const max = 200
+    if (amount > max) {
+      alert('You cannot enter a number greater than ' + max)
+      $(this).val('')
+      $(this).focus()
+    }
+  })
+</script>
+</html>
---
+++ mashpia.com/public/chidonTests/khk_tests_copy.php
@@ -0,0 +1,143 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+$admin_auth = ['school'];
+require $_SERVER['DOCUMENT_ROOT'] . '/header.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/class.globalSettings.php';
+$year = GlobalSettings::getChidonYear();
+
+function getMark($num, $id) {
+    $sql = "select mark from th_khk_marks where th_chidon_id = " . $id . " and test_number = " . $num;
+    $result = mysql_query($sql);
+    if (mysql_num_rows($result) > 0) {
+        $row = mysql_fetch_assoc($result);
+        return intval($row['mark']);
+    }
+    return '';
+}
+
+$msg = '';
+if (isset($_POST['submit'])) {
+    $qrys = [];
+    foreach ($_POST['marks'] as $id => $marks) {
+        foreach ($marks as $num => $mark) {
+            if (is_numeric($mark)){
+                $qrys[] = "insert into th_khk_marks 
+                            set th_chidon_id = " . $id . ", 
+                            test_number = " . $num . ", 
+                            mark = " . floatval($mark) . " 
+                            on duplicate key update mark = " . floatval($mark);
+            }
+        }
+    }
+    foreach ($qrys as $qry) mysql_query($qry);
+    $msg = "Marks Saved.";
+}
+
+require $_SERVER['DOCUMENT_ROOT'] . '/class.adminSchools.php';
+$as = new AdminSchools( $admin_user['admin_id'], $admin_user['auth'], true, true ); // add chidon schools
+$schools = $as->getSchools();
+
+$info = [];
+foreach ($schools as $school_id => $school) {
+    $sql = "select * from users u 
+            join classes c on c.class_id = u.class_id 
+            join th_chidon tc using (user_id) 
+            where tc.khk_reg = 1 
+            and u.school_id = " . $school_id . "            
+            and tc.year = " . $year;
+    $result = mysql_query($sql);
+    while ($row = mysql_fetch_assoc($result)) {
+        $info[$school_id][] = $row;
+    }
+}
+//echo "<pre>"; print_r($info); echo "</pre>"; exit;
+
+// initialize all tests to not be disabled
+$disable = [];
+for ($i = 1; $i <= 4; $i++) {
+    $disable[$i] = false;
+}
+
+// disable marking after certain dates for bc's
+if ($admin_user['auth'] != 'super') {
+    $today = new DateTime();
+    $shutdown1 = new DateTime('2022-10-26 05:00:00');
+//    $shutdown2 = new DateTime('2020-12-26 05:00:00');
+//    $shutdown3 = new DateTime('2021-02-02 05:00:00');
+//    $shutdown4 = new DateTime('2021-02-24 05:00:00');
+
+    if ($today >= $shutdown1) {
+        $disable[1] = true;
+    }
+//    if ($today >= $shutdown2) {
+//        $disable[2] = true;
+//    }
+//    if ($today >= $shutdown3) {
+//        $disable[3] = true;
+//    }
+//    if ($today >= $shutdown4) {
+//        $disable[4] = true;
+//    }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Enter KHK Marks</title>
+    <link href="../admin_styles.css" rel="stylesheet" type="text/css">
+    <style>
+      tr, th, td {
+        font-size: 14px;
+        padding: 5px;
+      }
+      td:not(.type) {
+        vertical-align: top;
+      }
+      .red {
+        color: red;
+      }
+    </style>
+</head>
+<body>
+<?php include('../admin_header.php'); ?>
+<h1>Enter KHK Marks</h1>
+<?php
+if (!empty($msg)) {
+    echo "<div style='color: red'>" . $msg . "</div>";
+}
+echo "<form action='' method='post'>";
+echo "<div style='float: right'><input type='submit' name='submit' value='Save Marks' style='padding: 12px; font-size: large' /></div><div style='clear: both;'></div>";
+foreach ($info as $school => $children) {
+    if (empty($children)) continue;
+    echo "<h2>" . $schools[$school] . "</h2>";
+    echo "<table><tr><th>Serial Number</th><th>Grade</th><th>Student</th><th>Test 1</th><th>Test 2</th><th>Test 3</th><th>Test 4</th><th>Avg Mark</th></tr>";
+    foreach ($children as $child) {
+        $grade = $child['class_grade'] . (empty($child['class_sub']) ? '' : '-' . $child['class_sub']);
+        $name = $child['first'] . ' ' . $child['last'];
+        $id = $child['th_chidon_id'];
+        $avg = 0;
+        echo "<tr><td>" . $child['user_serial'] . "</td><td>" . $grade . "</td><td>" . $name . "</td>";
+        $divideBy = 0;
+        for ($i = 1; $i <= 4; $i++) {
+            $mark = getMark($i, $id);
+            if ($mark > 0) {
+                $avg += $mark;
+                $divideBy++;
+            }
+            echo "<td>" . $mark . "</td>";
+        }
+        if ($divideBy) $avg = round($avg / $divideBy, 2);
+        $class = '';
+        if ($avg >= 80) $class="red";
+        echo "<td class=''" . $class . "'>" . $avg . "</td></tr>";
+    }
+    echo "</table>";
+}
+echo "<div style='float: right'><input type='submit' name='submit' value='Save Marks' style='padding: 12px; font-size: large' /></div>";
+echo "</form>";
+?>
+</body>
+</html>
