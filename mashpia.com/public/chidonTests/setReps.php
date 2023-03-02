ba520d1d793ac03211022ec1eacfd1b34e3458e0
naftoli rapoport <naftolir@gmail.com>
2023-03-01 21:23:24 -0500

    updates for reps

--- mashpia.com/public/chidonTests/setReps.php
+++ mashpia.com/public/chidonTests/setReps.php
@@ -111,8 +111,10 @@ foreach ($child_marks as $school => $more) {
     // find out how many videos were chosen
     $school_id = array_search($school, $schools);
     $sqlV = "select num_chidon_videos from schools where school_id = " . $school_id;
+    echo $sqlV . "<br />";
     $resultV = mysql_query($sqlV);
     $num_videos = mysql_fetch_assoc($resultV)['num_chidon_videos'];
+    continue;
     ?>
     <p id="<?=$school_id?>">
       Please choose how many videos you want to have for the school reps.<br />
