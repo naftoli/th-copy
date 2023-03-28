53d804e04df11c965e608717a18604c0e808736c
naftoli rapoport <naftolir@gmail.com>
2023-03-28 14:06:24 -0400

    more tehillim debugging

--- mashpia.com/public/class.shabbosMevorchim.php
+++ mashpia.com/public/class.shabbosMevorchim.php
@@ -1234,6 +1234,11 @@ class ShabbosMevorchim
                 }
             }
         }
+        echo "<pre>";
+        print_r($this->studentResults);
+        print_r($this->studentDoneResults);
+        echo "</pre>";
+        exit;
     }
 
     public function getStudentResults()
