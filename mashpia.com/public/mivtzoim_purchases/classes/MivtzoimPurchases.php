5053eebc60310fe6c8041e275824818e29862b4b
naftoli rapoport <naftolir@gmail.com>
2023-05-01 08:23:18 -0400

    update "from" email in yahadus book sale email

--- mashpia.com/public/mivtzoim_purchases/classes/MivtzoimPurchases.php
+++ mashpia.com/public/mivtzoim_purchases/classes/MivtzoimPurchases.php
@@ -262,7 +262,7 @@ class MivtzoimPurchases {
         $headers[] = 'MIME-Version: 1.0';
         $headers[] = 'Content-type: text/html; charset=iso-8859-1';
         // Additional headers
-        $headers[] = 'From: cth@tzivoshashem.org';
+        $headers[] = 'From: chidon@tzivoshashem.org';
         // Mail it
         $success = mail($to, $subject, $message, implode("\r\n", $headers));
         return $success;
