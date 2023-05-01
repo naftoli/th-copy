2d7f0dc7f594de13d118cf8041a7aceca85d7d2e
naftoli rapoport <naftolir@gmail.com>
2023-05-01 08:57:00 -0400

    fix bug

--- mashpia.com/public/mobile/mivtzoim/ajax/purchaseSeforim.php
+++ mashpia.com/public/mobile/mivtzoim/ajax/purchaseSeforim.php
@@ -65,7 +65,9 @@ function purchaseItems() {
             $last_name = $nameArr[count($nameArr) - 1];
             $zip = $cc_info->zip;
 
-            require_once '/authorize.php';
+            chdir('../../../');
+            require_once 'authorize.php';
+            chdir('/mobile/reg/ajax/');
 
             if ($response_array[0] == 1) { // success
                 $strResponse = $response_array[3] . ':' .
