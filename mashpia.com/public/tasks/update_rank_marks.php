03b4b3eb97338a72be1dde31256ef3b0ae8243f3
naftoli rapoport <naftolir@gmail.com>
2023-05-19 21:58:50 -0400

    script to update rank marks as shipped

--- mashpia.com/public/tasks/update_rank_marks.php
+++ mashpia.com/public/tasks/update_rank_marks.php
@@ -7,12 +7,21 @@ if ($admin_user['auth'] != 'super') {
     exit;
 }
 
+$qrys = [];
 $sql = "select * from medal_marks where date_awarded <= 2460060";
 $result = mysql_query($sql);
 while ($row = mysql_fetch_assoc($result)) {
-    $greg = jdtogregorian($row['date_awarded']);
-    $sql = "update medal_marks set date_shipped = '" . $greg . "' where medal_ord = " . $row['medal_ord'] .
-        " and subject_id = " . $row['subject_id'] . " and user_id = " . $row['user_id'] .
-        " and date_awarded = " . $row['date_awarded'];
-    echo $sql . "<br>";
+    if ($row['medal_ord'] > 0) {
+        $greg = jdtogregorian($row['date_awarded']);
+        $greg = str_replace('/', '-', $greg);
+        $sql = "update medal_marks set date_shipped = '" . $greg . "' where medal_ord = " . $row['medal_ord'] .
+            " and subject_id = " . $row['subject_id'] . " and user_id = " . $row['user_id'] .
+            " and date_awarded = " . $row['date_awarded'];
+        $qrys[] = $sql;
+    }
+}
+
+foreach ($qrys as $qry) {
+//    mysql_query($qry);
+    echo $qry . "<br>";
 }
