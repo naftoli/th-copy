da356a543940e6f1973122203f5290f544e1e9e5
naftoli rapoport <naftolir@gmail.com>
2023-05-19 22:00:35 -0400

    script to update rank marks as shipped

--- mashpia.com/public/tasks/update_rank_marks.php
+++ mashpia.com/public/tasks/update_rank_marks.php
@@ -13,7 +13,8 @@ $result = mysql_query($sql);
 while ($row = mysql_fetch_assoc($result)) {
     if ($row['medal_ord'] > 0) {
         $greg = jdtogregorian($row['date_awarded']);
-        $greg = str_replace('/', '-', $greg);
+        $dateInfo = explode('/', $greg);
+        $greg = $dateInfo[2] . '-' . $dateInfo[0] . '-' . $dateInfo[1];
         $sql = "update medal_marks set date_shipped = '" . $greg . "' where medal_ord = " . $row['medal_ord'] .
             " and subject_id = " . $row['subject_id'] . " and user_id = " . $row['user_id'] .
             " and date_awarded = " . $row['date_awarded'];
