3db4d9483f9be2f94b135ad57b94822179bf5c2a
naftoli rapoport <naftolir@gmail.com>
2023-05-29 00:39:15 -0400

    fix update rank cards qry to include missing rank cards/books only for that rank

--- mashpia.com/public/api/core/id_cards.php
+++ mashpia.com/public/api/core/id_cards.php
@@ -32,11 +32,11 @@ class IdCardsRouter {
         // add schools exceptions
         $filters[] = 'u.school_id not in (180, 585, 588, 612, 709)';
         $filters = implode( ' AND ', $filters );
-        $fix = " OR ((rm.date_book_shipped is null OR rm.date_card_shipped is null)";
+        $fix = " OR ((rm.date_book_shipped is null OR rm.date_card_shipped is null) AND rm.date_printed is not null";
         if ( isset($_POST['rank']) && $_POST['rank'] ) $fix .= " AND rank_ord = " . $_POST['rank'];
         $fix .= ")";
         $filters .= $fix;
-//        echo $filters; exit;
+        echo $filters; exit;
 
         $rank_marks = "(SELECT MAX(rank_ord) max_rank, user_id FROM rank_marks GROUP BY user_id) cr USING (user_id) "
             ." JOIN rank_marks rm ON (rm.rank_ord = cr.max_rank AND rm.user_id = u.user_id) ";
