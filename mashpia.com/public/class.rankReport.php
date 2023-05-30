faafb886f7ef5c67d68f32f3d282a828b15f9cf5
naftoli rapoport <naftolir@gmail.com>
2023-05-29 21:04:16 -0400

    add files for OT for kids not promoted

--- mashpia.com/public/class.rankReport.php
+++ mashpia.com/public/class.rankReport.php
@@ -26,7 +26,7 @@ class RankReport extends Report {
         $this->userPic = [];
     }
 
-    public function setRanks($orderType = 'byGrade', $rankOrd = 0, $nameBreak = ' ', $specificGender = '', $forShipping = false) {
+    public function setRanks($orderType = 'byGrade', $rankOrd = 0, $nameBreak = ' ', $specificGender = '', $forShipping = false, $kidsNotPromoted = false) {
         $this->ranks = array();
         $start = $this->reportDates['start'];
         $end = $this->reportDates['end'];
@@ -36,7 +36,10 @@ class RankReport extends Report {
                 (date_promoted >= $start AND date_promoted <= $end AND date_printed is null) OR ((rm.date_book_shipped is null OR rm.date_card_shipped is null) AND rm.date_printed is not null)
             )";
         }
-        else $filter = " AND (date_promoted >= $start AND date_promoted <= $end)";
+        else {
+            if ($kidsNotPromoted) $filter = " AND date_promoted < $start";
+            else $filter = " AND (date_promoted >= $start AND date_promoted <= $end)";
+        }
         $sql = "
             SELECT s.school_name, s.logo, s.logo_boys, s.logo_girls, s.school_logo_id, c.class_teacher, c.class_grade, c.class_sub, r.rank_name, u.*, rm.* 
             FROM rank_marks rm
---
+++ mashpia.com/public/rank_ceremony/ot_rest.php
@@ -0,0 +1,84 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL);
+ini_set('max_execution_time', 300);
+
+$admin_auth = ['school'];
+require_once $_SERVER['DOCUMENT_ROOT'] . '/header.php';
+
+if ($admin_user['auth'] != 'super') {
+    echo "No Permission to be here.";
+    exit;
+}
+
+if ( isset($_GET['id']) ) {
+    unset($schools);
+    $schools = [
+        $_GET['id'] => 'School Name'
+    ];
+}
+
+$submit = false;
+if (isset($_POST['submit'])) {
+    $submit = true;
+    $from = explode('-', $_POST['fromDate']);
+    $to = explode('-', $_POST['toDate']);
+    $start = gregoriantojd($from[1], $from[2], $from[0]);
+    $end = gregoriantojd($to[1], $to[2], $to[0]);
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf8" />
+    <script
+        src="https://code.jquery.com/jquery-1.12.4.min.js"
+        integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
+        crossorigin="anonymous"></script>
+    <script>
+      // let date = '';
+      // while (date != 'current' && date != 'previous')
+      //     date = prompt("Do you want to generate ranks base on current dates or previous dates? (enter 'current' or 'previous')")
+      // const prev = date === 'previous' ? 1 : 0
+      const submit = <?= $submit ?>;
+
+      function createFile(school) {
+        const start = <?= $start ?>;
+        const end = <?= $end ?>;
+        return new Promise((resolve, reject) => {
+          $.ajax({
+            url: `createFileOTrest.php?start=${start}&end=${end}`,
+            type: 'POST',
+            data: {
+              school: school,
+            },
+            success: function (data) {
+              resolve(data)
+            },
+            error: function (error) {
+              reject(error)
+            },
+          })
+        })
+      }
+
+      if (submit) {
+        let p = createFile(255)
+        p.then(values => {
+          console.log(values)
+          location.href = 'createZip.php'
+        })
+          .catch(error => {
+            console.log(error)
+          })
+      }
+    </script>
+</head>
+<?php if (!isset($_POST['submit'])) : ?>
+    <form action="" method="post">
+        From date: <input type="date" name="fromDate" /><br />
+        To date: <input type="date" name="toDate" />
+        <input type="submit" name="submit" value="submit" />
+    </form>
+<?php endif; ?>
+</html>
