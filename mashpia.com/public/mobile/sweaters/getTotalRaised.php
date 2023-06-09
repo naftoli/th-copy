f07fb5027ce216496d19b2ace5f76fc444d7cc34
naftoli rapoport <naftolir@gmail.com>
2023-06-08 20:44:25 -0400

    fix login issue being caused by the code on mobile site

--- mashpia.com/public/mobile/sweaters/getTotalRaised.php
+++ mashpia.com/public/mobile/sweaters/getTotalRaised.php
@@ -1,17 +1,5 @@
 <?php
-//ini_set('display_errors', 1);
-//ini_set('error_reporting', E_ALL);
-
-$admin_auth = ['school'];
-require_once $_SERVER['DOCUMENT_ROOT'] . '/header.php';
-
-if ($admin_user['auth'] != 'super') {
-    echo json_encode([
-        'success' => false,
-        'message' => 'No access'
-    ]);
-    exit;
-}
+require $_SERVER['DOCUMENT_ROOT'] . '/db.php';
 
 require $_SERVER['DOCUMENT_ROOT'] . '/mobile/reg/ajax/encrypt.php';
 $admin = mysql_real_escape_string( $_POST['family_id'] );
