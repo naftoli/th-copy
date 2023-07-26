d2ce272f49108729bf1bbac2ea7135b0ef604a9f
naftoli rapoport <naftolir@gmail.com>
2023-07-26 10:34:59 -0400

    battlefront reports updates with wwtc

---
+++ mashpia.com/public/battlefront/api/getWWTC.php
@@ -0,0 +1,63 @@
+<?php
+$admin_auth = ['school'];
+require_once $_SERVER["DOCUMENT_ROOT"] . '/header.php';
+
+// make sure only super admins can access
+if ($admin_user['auth'] != 'super') {
+    echo "No permission.";
+    exit;
+}
+
+// imports
+require_once $_SERVER['DOCUMENT_ROOT'] . '/class.shabbosMevorchim.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/class.adminSchools.php';
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/class.globalSettings.php';
+$year = GlobalSettings::GetCurrentYear();
+
+$sm = calculateSM($year);
+$months = array(
+    0 => 'Tishrei',
+    1 => 'Cheshvon',
+    2 => 'Kislev',
+    3 => 'Teves',
+    4 => 'Shvat',
+    5 => 'Adar I',
+    6 => 'Adar II',
+    7 => 'Nissan',
+    8 => 'Iyar',
+    9 => 'Sivan',
+    10 => 'Tamuz',
+    11 => 'Av',
+    12 => 'Elul'
+);
+// if plain yr change adar 1 to adar and remove adar 2
+if ($sm[6] == $sm[7]) {
+    unset($sm[6]);
+    $months[5] = 'Adar';
+}
+
+// get most recent shabbos mevorchim
+$now = unixtojd();
+foreach ($sm as $date) {
+    if ($date > $now) break;
+}
+
+$sm = new ShabbosMevorchim();
+$sm->setReportDates($date);
+$sm->setArmyResults();
+
+$as = new AdminSchools( $admin_user['admin_id'], $admin_user['auth'] );
+$ids = $as->getSchools();
+
+foreach ($ids as $id => $name) {
+    // generate the report for just this school
+    $sm->setSchool( $id );
+    $sm->setSchoolResults( $id );
+    $sm->setClassResults();
+
+    // changes from shabbos_mevorchim.php
+    $sm->setStudentResults(0, $_GET['date']);
+}
+
+echo json_encode($sm->getStudentResults());
