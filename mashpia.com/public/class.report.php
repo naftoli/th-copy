63917da6fcd39990291933e379b51b5894d6940c
naftoli rapoport <naftolir@gmail.com>
2023-08-29 11:50:01 -0400

    update dates for reports

--- mashpia.com/public/class.report.php
+++ mashpia.com/public/class.report.php
@@ -7,8 +7,9 @@ class Report extends ReportBasic {
 	    
     public function __construct($previousStart = false) {
         parent::__construct();
-//        $this->dates = array(2459340,2459361,2459471,2459501,2459529,2459557,2459585,2459620,2459648,2459697,2459718); // each year take last 2 dates from previous yr and add current dates
-        $this->dates = array( 2459697, 2459718, 2459823, 2459851, 2459879, 2459914, 2459942, 2459970, 2459998, 2460026, 2460061, 2460092 );
+        // each year take last 2 dates from previous yr and add current dates
+//        $this->dates = array( 2459697, 2459718, 2459823, 2459851, 2459879, 2459914, 2459942, 2459970, 2459998, 2460026, 2460061, 2460092 ); // each year take last 2 dates from previous yr and add current dates
+        $this->dates = array( 2460061, 2460092, 2460187, 2460230, 2460258, 2460300, 2460321, 2460349, 2460374, 2460405, 2460450 );
         $this->setReportDates($previousStart);
     }
 
