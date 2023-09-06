a5d25a1e13006bb893288d1ddfa5618d5ba2b5ac
naftoli rapoport <naftolir@gmail.com>
2023-09-05 23:14:44 -0400

    fix quota cards

--- mashpia.com/public/quota_cards.php
+++ mashpia.com/public/quota_cards.php
@@ -18,7 +18,7 @@ foreach ( $schools as $id => $school ) {
 require $_SERVER['DOCUMENT_ROOT'] . '/class.globalSettings.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/class.shabbosMevorchim.php';
 
-$year = GlobalSettings::getCurrentYear();
+$year = GlobalSettings::getRegistrationYear();
 $sm = new ShabbosMevorchim( $year );
 $sm->setQuotaCardDates();
 $dates = $sm->getReportDates();
