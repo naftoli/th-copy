09521e2953e08d7d8ccaa6d20871bffaae8bfe91
naftoli rapoport <naftolir@gmail.com>
2023-09-07 12:18:24 -0400

    fix reg bugs

--- mashpia.com/public/api/models/Soldier.php
+++ mashpia.com/public/api/models/Soldier.php
@@ -599,7 +599,7 @@ class Soldier extends \ActiveRecord\Model implements \JsonSerializable {
             }
 //            echo $reg_query->debugDumpParams();
             // save the charge
-            $type = 'chayolei';
+            $type = 'chidon';
             if ($lite) $type = 'chayolei-lite';
             if ($ckids) $type = 'ckids';
             if (!$this->registrationCharge($type, $amount, $trans_id, false, $discount)) {
