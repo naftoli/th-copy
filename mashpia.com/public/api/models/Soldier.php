7ea4a519a6161392cb44f5761aadbf95b6428b4b
naftoli rapoport <naftolir@gmail.com>
2023-09-13 13:57:18 -0400

    take down child

--- mashpia.com/public/api/models/Soldier.php
+++ mashpia.com/public/api/models/Soldier.php
@@ -529,6 +529,9 @@ class Soldier extends \ActiveRecord\Model implements \JsonSerializable {
         // if tuition school, turn off chayolei
         if (!$result['chayolei'] && !$isBC && intval($row['reg_type']) == 1) $result['chayolei'] = true;
 
+        // turn off chidon reg
+        $result['chidon'] = true;
+
         return $result;
     }
 
