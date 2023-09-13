f58615ea2acd3a3aa49cfa577763628a40ca5a5d
naftoli rapoport <naftolir@gmail.com>
2023-09-13 18:46:20 -0400

    trun off chidon for beis rivka

--- mashpia.com/public/mobile/reg/ajax/getChildren.php
+++ mashpia.com/public/mobile/reg/ajax/getChildren.php
@@ -288,6 +288,8 @@ if ( !empty( $users ) ) {
             $children[ $row['user_id'] ]['reg_types'] = [];
         }
 
+        if ($row['school_id'] == 54) $children[ $row['user_id'] ]['reg_types']['chidon'] = false; // turn off chidon for beis rivka
+
         // chidon experience registration
         $children[$row['user_id']]['shabbatonPaid'] = 0;
         $cSql = "SELECT * FROM th_chidon WHERE date_paid > 0 and year = " . $chidon_year . " AND user_id = " . $row['user_id'];
