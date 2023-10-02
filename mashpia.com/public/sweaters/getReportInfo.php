09e5e19f714d255417a1435770522630d7776ee0
naftoli rapoport <naftolir@gmail.com>
2023-10-02 16:28:50 -0400

    fix sweater rporting

--- mashpia.com/public/sweaters/getReportInfo.php
+++ mashpia.com/public/sweaters/getReportInfo.php
@@ -28,26 +28,27 @@ while ($row = mysql_fetch_assoc($result)) {
     $sweaters[$row['admin_id']][] = $row;
 }
 
-$admins = [];
-$sql = "select * from admins where admin_id in (" . implode(',', array_keys($raised)) . ") order by admin_id";
-$result = mysql_query($sql);
-while ($row = mysql_fetch_assoc($result)) {
-    $admins[$row['admin_id']] = $row;
-}
+$admin_ids = array_unique(array_merge(array_keys($raised), array_keys($sweaters)), SORT_NUMERIC);
 
 $children = [];
 $sql = "select * from users u 
         join schools s using (school_id) 
         join classes c on c.class_id = u.class_id 
         join admin_auths aa on aa.id = u.user_id 
-        where aa.admin_id in (" . implode(',', array_keys($admins)) . ") 
+        where aa.admin_id in (" . implode(',', $admin_ids) . ") 
         and aa.auth = 'user' 
-        and u.user_registered > 0 
         and c.class_grade != '8' 
         and u.school_id not in (61, 269, 612)";
 $result = mysql_query($sql);
 while ($row = mysql_fetch_assoc($result)) {
     $children[$row['admin_id']][$row['user_id']] = $row;
+}
+
+$admins = [];
+$sql = "select * from admins where admin_id in (" . implode(',', array_keys($children)) . ") order by admin_id";
+$result = mysql_query($sql);
+while ($row = mysql_fetch_assoc($result)) {
+    $admins[$row['admin_id']] = $row;
 }
 
 echo json_encode([
