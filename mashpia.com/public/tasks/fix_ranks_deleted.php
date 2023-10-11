183f1ffe05b1f9b1c52a222ba5d26dd938423fdb
naftoli rapoport <naftolir@gmail.com>
2023-10-11 19:19:44 -0400

    work on the ranks deleted issue

--- mashpia.com/public/tasks/fix_ranks_deleted.php
+++ mashpia.com/public/tasks/fix_ranks_deleted.php
@@ -16,15 +16,33 @@ while ($row = mysql_fetch_assoc($result)) {
                 set rank_ord = " . $row['rank_ord'] . ", 
                 user_id = " . $row['user_id'] . ", 
                 date_promoted = " . $row['date_promoted'];
-        if ($row['date_printed']) $sql .= ", date_printed = " . $row['date_printed'];
-        if ($row['date_book_shipped']) $sql .= ", date_book_shipped = " . $row['date_book_shipped'];
-        if ($row['date_book_received']) $sql .= ", date_book_received = " . $row['date_book_received'];
-        if ($row['date_card_shipped']) $sql .= ", date_card_shipped = " . $row['date_card_shipped'];
-        if ($row['date_card_received']) $sql .= ", date_card_received = " . $row['date_card_received'];
+        if ($row['date_printed']) $sql .= ", date_printed = '" . $row['date_printed'] . "'";
+        if ($row['date_book_shipped']) $sql .= ", date_book_shipped = '" . $row['date_book_shipped'] . "'";
+        if ($row['date_book_received']) $sql .= ", date_book_received = '" . $row['date_book_received'] . "'";
+        if ($row['date_card_shipped']) $sql .= ", date_card_shipped = '" . $row['date_card_shipped'] . "'";
+        if ($row['date_card_received']) $sql .= ", date_card_received = '" . $row['date_card_received'] . "'";
         if ($row['ranks_updated']) $sql .= ", ranks_updated = " . $row['ranks_updated'];
         $qrys[] = $sql;
     }
 }
 
-foreach ($qrys as $qry) mysql_query($qry);
-echo "done";
+$success = true;
+mysql_query('set autocommit=0');
+mysql_query('start transaction');
+
+foreach ($qrys as $qry) {
+    if (! mysql_query($qry)) {
+        $success = false;
+        echo $qry . "<br />" . mysql_error() . "<br /><br />";
+        break;
+    }
+}
+
+if ($success) {
+    mysql_query('commit');
+} else {
+    mysql_query('rollback');
+}
+mysql_query('set autocommit=1');
+
+echo "Success: " . $success;
