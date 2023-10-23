154fba52210462148d64f8c22891febf332ef688
naftoli rapoport <naftolir@gmail.com>
2023-10-23 09:18:20 -0400

    updates for editing chidon

--- mashpia.com/public/mobile/registration/js/register.js
+++ mashpia.com/public/mobile/registration/js/register.js
@@ -1818,7 +1818,7 @@ var registrationApp = function() {
             APIRequest( 'POST', api_url + '?action=registerUsers', postData, resolve)
         }).then( function( data ) {
             if ( typeof data === 'string' || typeof data === 'object' && data.success ) {
-                if (showClasses) $("#successModal #success").append("<p>" + Msg8 + "<a href='https://merkos302.formstack.com/forms/chidon_shiurim_registration'>" + Msg9 + "</a></p>");
+                if (showClasses) $("#successModal #myshliach-extra").html("<br /><p>" + Msg8 + "<a href='https://merkos302.formstack.com/forms/chidon_shiurim_registration'>" + Msg9 + "</a></p>");
                 $("#successModal").modal('show');
             }
         });
