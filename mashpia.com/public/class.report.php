63214cce7874c180271a2d66c2a4671631a48521
naftoli rapoport <naftolir@gmail.com>
2023-10-24 08:50:09 -0400

    change medal packing date

--- mashpia.com/public/class.report.php
+++ mashpia.com/public/class.report.php
@@ -9,7 +9,7 @@ class Report extends ReportBasic {
         parent::__construct();
         // each year take last 2 dates from previous yr and add current dates
 //        $this->dates = array( 2459697, 2459718, 2459823, 2459851, 2459879, 2459914, 2459942, 2459970, 2459998, 2460026, 2460061, 2460092 ); // each year take last 2 dates from previous yr and add current dates
-        $this->dates = array( 2460061, 2460092, 2460226, 2460258, 2460300, 2460321, 2460349, 2460374, 2460405, 2460450 );
+        $this->dates = array( 2460061, 2460092, 2460226, 2460246, 2460300, 2460321, 2460349, 2460374, 2460405, 2460450 );
         $this->setReportDates($previousStart);
     }
 
