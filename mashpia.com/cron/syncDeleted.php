92ae6b0799fd8664e9037e8cc57a53523c7dffdc
naftoli rapoport <naftolir@gmail.com>
2023-10-23 23:58:10 -0400

    make sure to delete any kids that aren't in the system, from the admin_auths table - cron job

--- mashpia.com/cron/syncDeleted.php
+++ mashpia.com/cron/syncDeleted.php
@@ -2,9 +2,6 @@
 // load up the database connection
 include_once( __DIR__ . '/public/api/header/db.php' );
 
-// create cron job to delete admin_auths associations that point to missing child
-$sql = "delete aa.* from admin_auths aa 
-        left join users u on u.user_id = aa.id 
-        where aa.auth = 'user' 
-        and u.user_id is null";
+// create cron job to delete admin_auth associations that point to missing child
+$sql = "delete from admin_auths where id not in (select user_id from users)";
 echo mysql_query($sql);
