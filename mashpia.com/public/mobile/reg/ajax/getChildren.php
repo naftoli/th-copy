247734e7f41b3dc10953a29ad7ded809d45208ba
naftoli rapoport <naftolir@gmail.com>
2023-10-31 17:51:20 -0400

    enable editing for some schools

--- mashpia.com/public/mobile/reg/ajax/getChildren.php
+++ mashpia.com/public/mobile/reg/ajax/getChildren.php
@@ -297,7 +297,7 @@ if ( !empty( $users ) ) {
          	$children[ $row['user_id'] ]['needsReg'] = 1;
          	$children[ $row['user_id'] ]['reg_types']['chidon'] = true;
          } else if ( $row['reg_chidon'] ) {
-         	if (isset($_COOKIE['naftoli']) || in_array($children[$row['user_id']]['school_id'], [54, 33, 542, 21, 434, 105])) {
+         	if (isset($_COOKIE['naftoli']) || in_array($children[$row['user_id']]['school_id'], [54, 33, 542, 21, 434, 105, 61])) {
                 $children[$row['user_id']]['editChidon'] = true;
             }
          }
