036ee30f1e1862996a6de051b946a70bd0d04d77
naftoli rapoport <naftolir@gmail.com>
2023-11-20 12:34:56 -0500

    fixing / debugging certificate report

--- mashpia.com/public/chidonOld/newReports/awards/awards_initial.php
+++ mashpia.com/public/chidonOld/newReports/awards/awards_initial.php
@@ -9,7 +9,50 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/class.adminSchools.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/class.globalSettings.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/chidonTests/class.chidonTests.php';
 
-require 'afterTest1.php';
+$as = new AdminSchools($admin_user['admin_id'], $admin_user['auth']);
+$schools = $as->getSchools();
+
+$year = GlobalSettings::getChidonYear();
+
+// qry to get all kids that should get the award
+$sql = "
+    SELECT
+        s.school_name, u.user_id, u.class_id, u.school_id, u.user_serial, u.first_he, u.last_he, u.gender, 
+        c.class_grade, c.class_sub, tc.parent_id, tc.th_chidon_id, tc.test_type, tc.reward_type 
+    FROM 
+        users u 
+        join schools s using (school_id) 
+        join classes c on c.class_id = u.class_id 
+        join th_chidon tc using (user_id) 
+        join th_chidon_marks tcm using (th_chidon_id) 
+    WHERE
+        tc.year = $year 
+    GROUP BY 
+        u.user_id 
+    ORDER BY
+        s.school_id, c.class_grade, c.class_sub, u.last, u.first";
+//echo $sql . "<br />"; exit;
+$stmt = $MASHPIA_DB->query($sql);
+$rows = $stmt->fetchAll();
+$info = [];
+foreach ($rows as $row) {
+    $row['award'] = 'cert'; // default to 'cert'
+    $info[$row['school_id']][] = $row;
+}
+
+// find out order of kids for admins
+$admins = [];
+$sql = "select aa.admin_id, aa.id from admin_auths aa 
+        join users u on u.user_id = aa.id 
+        join th_chidon tc using (user_id) 
+        where tc.year = $year 
+        and u.school_id in (61, 269) 
+        group by id 
+        order by u.dob";
+$result = mysql_query($sql);
+while ($row = mysql_fetch_assoc($result)) {
+    $admins[$row['admin_id']][] = $row['id'];
+}
 
 $adminsSorted = [];
 foreach ($info as $school => $children) {
