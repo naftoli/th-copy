56a2f88168dc7c0646f50407aac847c3d74090d2
naftoli rapoport <naftolir@gmail.com>
2023-11-21 22:01:07 -0500

    updates for birthday certificate

--- mashpia.com/public/birthday_cert.php
+++ mashpia.com/public/birthday_cert.php
@@ -5,26 +5,26 @@ $jYear = GlobalSettings::getCurrentYear();
 
 //echo "<pre>"; print_r( $_POST ); echo "</pre>";
 $parshaDates = array();
-if ( isset( $_GET['school'] ) ) {
+if (isset($_GET['school'])) {
     $school_id = $_GET['school'];
     $class_id = $_GET['class'];
     $user_id = $_GET['user'];
     $parsha = $_GET['parsha'];
     $gender = $_GET['gender'];
-    $dates = explode( ":", $parsha );
+    $dates = explode(":", $parsha);
     $start = $dates[0];
     $end = $dates[1];
-	$parshaDates[$start] = $parsha;
-} else if ( isset( $_POST['schools'] ) ) {
-    $schools = explode( ':', $_POST['schools'] );
-    $parshas = explode( ':', $_POST['parshas'] );
-	$gender = $_POST['gender'];
+    $parshaDates[$start] = $parsha;
+} else if (isset($_POST['schools'])) {
+    $schools = explode(':', $_POST['schools']);
+    $parshas = explode(':', $_POST['parshas']);
+    $gender = $_POST['gender'];
     //get dates
     $dates = array();
     $numDates = 0;
-    $sqlReport = "select start, end, name from parshos where end in (" . implode( ',', $parshas ) . ")";
-    $resultReport = mysql_query( $sqlReport );
-    while ( $rowReport = mysql_fetch_assoc( $resultReport ) ) {
+    $sqlReport = "select start, end, name from parshos where end in (" . implode(',', $parshas) . ")";
+    $resultReport = mysql_query($sqlReport);
+    while ($rowReport = mysql_fetch_assoc($resultReport)) {
         $dates['start'][] = $rowReport['start'];
         $dates['end'][] = $rowReport['end'];
         $parshaDates[$rowReport['start']] = $rowReport['name'];
@@ -33,25 +33,25 @@ if ( isset( $_GET['school'] ) ) {
 }
 
 $names = [];
-if ( isset( $school_id ) ) {
+if (isset($school_id)) {
     $sql = "select u.user_id, u.first, u.last, u.first_he, u.last_he, u.he_name, c.class_grade, c.class_sub, s.school_name, u.dob  
             from users u 
             join schools s on (s.school_id = u.school_id) 
             join classes c on (c.class_id = u.class_id)         
             where u.user_registered > 0 
-            and u.dob > 0 "; 
-    if ( $school_id > 0 ) {
+            and u.dob > 0 ";
+    if ($school_id > 0) {
         $sql .= " and u.school_id = $school_id ";
     }
-    if ( $class_id > 0 ) {
+    if ($class_id > 0) {
         $sql .= " and u.class_id = " . $class_id;
     }
-    if ( $user_id > 0 ) {
+    if ($user_id > 0) {
         $sql .= " and u.user_id = " . $user_id;
     }
-    if ( $gender == 'm' ) {
+    if ($gender == 'm') {
         $sql .= " and school_type_id in (2,12) ";
-    } else if ( $gender == 'f' ) {
+    } else if ($gender == 'f') {
         $sql .= " and school_type_id in (3,13) ";
     }
     $sql .= " order by c.class_grade, c.class_sub, u.last, u.first";
@@ -62,163 +62,170 @@ if ( isset( $school_id ) ) {
             join classes c on (c.class_id = u.class_id)         
             where u.user_registered > 0 
             and u.dob > 0 
-            and u.school_id in (" . implode( ',', $schools ) . ")"; 
-     if ( $gender == 'm' ) {
+            and u.school_id in (" . implode(',', $schools) . ")";
+    if ($gender == 'm') {
         $sql .= " and (gender = 'M' or gender = 'm') ";
-     } else if ( $gender == 'f' ) {
+    } else if ($gender == 'f') {
         $sql .= " and (gender = 'F' or gender = 'f') ";
-     }
-	 $sql .= "order by s.school_name, c.class_grade, c.class_sub, u.last, u.first";
-     //echo $sql;
+    }
+    $sql .= "order by s.school_name, c.class_grade, c.class_sub, u.last, u.first";
+    //echo $sql;
 }
 //echo $sql; exit;
 
-$result = mysql_query( $sql );
-while ( $row = mysql_fetch_assoc( $result ) ) {
+$result = mysql_query($sql);
+while ($row = mysql_fetch_assoc($result)) {
     //check users dob to see if within dates range
     //get month and day of hebrew birthday
-    $dob = explode( '-', $row['dob'] );
-    if ( $dob[1] == 0 || $dob[2] == 0 ) continue;
-    $jd = gregoriantojd( $dob[1], $dob[2], $dob[0] );
-    $jewish = jdtojewish( $jd );
-    $j = explode( '/', $jewish );
+    $dob = explode('-', $row['dob']);
+    if ($dob[1] == 0 || $dob[2] == 0) continue;
+    $jd = gregoriantojd($dob[1], $dob[2], $dob[0]);
+    $jewish = jdtojewish($jd);
+    $j = explode('/', $jewish);
     $jMonth = $j[0];
     $jDay = $j[1];
-    
+
     //find this year's jd equivalent of hebrew birthday
-    $jdNow = jewishtojd( $jMonth, $jDay, $jYear );
-    $jewishNow = jdtojewish( $jdNow, true, CAL_JEWISH_ADD_GERESHAYIM + CAL_JEWISH_ADD_ALAFIM_GERESH );
+    $jdNow = jewishtojd($jMonth, $jDay, $jYear);
+    $jewishNow = jdtojewish($jdNow, true, CAL_JEWISH_ADD_GERESHAYIM + CAL_JEWISH_ADD_ALAFIM_GERESH);
     $jNow = iconv('WINDOWS-1255', 'UTF-8', $jewishNow);
-	
-	//find the english year corresponding to hebrew birthday
-	$bd = jdtogregorian($jdNow);
-	$bdArr = explode('/', $bd);
-	$age = ($bdArr[2] - $dob[0]);
-	/*
-	if ( $row['user_id'] == 17100 ) {
-		echo "<pre>"; print_r( $dob ); echo "</pre>";
-		echo $jd . "<br />";
-		echo $jewish . "<br />";
-		echo $jdNow . "<br />";
-		echo $start . ' - ' . $end;
-	}
-	 * 
-	 */ 
-    
-    if ( isset( $school_id ) ) {
+
+    //find the english year corresponding to hebrew birthday
+    $bd = jdtogregorian($jdNow);
+    $bdArr = explode('/', $bd);
+    $age = ($bdArr[2] - $dob[0]);
+    /*
+      if ( $row['user_id'] == 17100 ) {
+          echo "<pre>"; print_r( $dob ); echo "</pre>";
+          echo $jd . "<br />";
+          echo $jewish . "<br />";
+          echo $jdNow . "<br />";
+          echo $start . ' - ' . $end;
+      }
+       *
+       */
+
+    if (isset($school_id)) {
         //if hebrew date within range then we are good and add user to array otherwise don't add to array
-        if ( $jdNow >= $start && $jdNow <= $end ) {
-            $grade = empty( $row['class_sub'] ) ? $row['class_grade'] : $row['class_grade'] . '-' . $row['class_sub'];
-			$heName = empty( $row['he_name'] ) ? 
-				(( empty( $row['first_he'] ) && empty( $row['last_he'] )) ? $row['first'] . ' ' . $row['last'] :
-				$row['first_he'] . '  ' . $row['last_he']) : $row['he_name'];
-			$names[$row['school_name']][$grade][$jNow][$parsha][] = array( 'age' => $age, 'name' => $heName );
+        if ($jdNow >= $start && $jdNow <= $end) {
+            $grade = empty($row['class_sub']) ? $row['class_grade'] : $row['class_grade'] . '-' . $row['class_sub'];
+            $heName = empty($row['he_name']) ?
+                ((empty($row['first_he']) && empty($row['last_he'])) ? $row['first'] . ' ' . $row['last'] :
+                    $row['first_he'] . '  ' . $row['last_he']) : $row['he_name'];
+            $names[$row['school_name']][$grade][$jNow][$parsha][] = array('age' => $age, 'name' => $heName);
         }
     } else {
         //loop through dates array to check if hebrew jd birthday falls within any of them
         $found = false;
-        for ( $i = 0; $i < $numDates; $i++ ) {
-            if ( $jdNow >= $dates['start'][$i] && $jdNow <= $dates['end'][$i] ) {
+        for ($i = 0; $i < $numDates; $i++) {
+            if ($jdNow >= $dates['start'][$i] && $jdNow <= $dates['end'][$i]) {
                 $found = true;
                 break;
             }
         }
-        if ( $found ) {
-            $grade = empty( $row['class_sub'] ) ? $row['class_grade'] : $row['class_grade'] . '-' . $row['class_sub'];
-			$heName = empty( $row['he_name'] ) ? 
-				(( empty( $row['first_he'] ) && empty( $row['last_he'] )) ? $row['first'] . ' ' . $row['last'] :
-				$row['first_he'] . ' ' . $row['last_he']) : $row['he_name'];
-			$names[$row['school_name']][$grade][$jNow][$parshaDates[$dates['start'][$i]]][] = array( 'age' => $age, 'name' => $heName );
+        if ($found) {
+            $grade = empty($row['class_sub']) ? $row['class_grade'] : $row['class_grade'] . '-' . $row['class_sub'];
+            $heName = empty($row['he_name']) ?
+                ((empty($row['first_he']) && empty($row['last_he'])) ? $row['first'] . ' ' . $row['last'] :
+                    $row['first_he'] . ' ' . $row['last_he']) : $row['he_name'];
+            $names[$row['school_name']][$grade][$jNow][$parshaDates[$dates['start'][$i]]][] = array('age' => $age, 'name' => $heName);
         }
     }
 }
 ?>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <style type="text/css">
-            @font-face {
-                font-family: DirtyEgo;
-                src: url('fonts/DIRTYEGO.TTF');
-            }
-            .page {
-                height: 7.5cm;
-            }
-            .name {
-                margin-left: 2.6cm;
-                width: 6cm;
-                font-size: 30px;
-                font-weight: bold;
-                text-align: center;
-                font-family: DirtyEgo;
-                transform: rotate(180deg);
-            }
-            .grade {
-                font-size: 12px;
-                font-weight: normal;
-            }
-            .page-break {
-                page-break-after: always;
-            }
-            @media print {
-                .no-print {
-                    display: none;
-                }
-            }
-            @media screen {
-                .no-print { 
-                    margin-left: 38%;
-                    font-family: Arial, Helvetica, sans-serif;
-                    font-size: 12px;
-                }
-                .print {
-                    margin-left: 16%;
-                }
-            }
-        </style>
-    </head>
-    
-    <body>
-        <?
-        if ( empty( $names ) ) {
-            echo "Sorry there are no names that meet your criteria, please go back and revise the options.";
-            exit;
-        }
-        ?>
-        <div class="no-print">
-            <p>Printing Instructions:<br />
-            Step 1: Set the Orientation to <u>Portrait</u><br />
-            Step 2: Check 'Shrink to fit Page Width'<br />
-            Step 3: In Options check 'Print Background (colors & images)'<br />
-            Step 4: In the second tab set all Margins to 0.0 inches (All Sides)<br />
-            Step 5: Set all Headers & Footers to Blank</p>
-            <p class='print'>
-                <input type="button" value="Print" onclick="window.print()" />
-            </p>
-        </div>
-        <? 
-        //echo "<pre>"; print_r( $names ); echo "</pre>";
-        foreach ( $names as $school => $info ) {
-            foreach ( $info as $grade => $dates ) {
-	        	foreach ( $dates as $date => $other ) {
-	        		foreach ( $other as $parsha => $children ) {
-	                    foreach ( $children as $child ) {
-	                        ?> 
-	                        <div class="page"></div>
-	                        <div class="name">
-	                            <?=$child['name']?><br />
-	                            <span class="grade">
-	                                <?=$school?> Platoon: <?=$grade?> - <?=$child['age']?> yrs. old
-	                                <br /><?=$date?> - <?=$parsha?>
+<head>
+  <meta charset="UTF-8">
+  <style type="text/css">
+    @font-face {
+      font-family: DirtyEgo;
+      src: url('fonts/DIRTYEGO.TTF');
+    }
+
+    .page {
+      height: 7.5cm;
+    }
+
+    .name {
+      margin-left: 2.6cm;
+      width: 6cm;
+      font-size: 30px;
+      font-weight: bold;
+      text-align: center;
+      font-family: DirtyEgo;
+      transform: rotate(180deg);
+    }
+
+    .grade {
+      font-size: 12px;
+      font-weight: normal;
+    }
+
+    .page-break {
+      page-break-after: always;
+    }
+
+    @media print {
+      .no-print {
+        display: none;
+      }
+    }
+
+    @media screen {
+      .no-print {
+        margin-left: 38%;
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 12px;
+      }
+
+      .print {
+        margin-left: 16%;
+      }
+    }
+  </style>
+</head>
+
+<body>
+<?
+if (empty($names)) {
+    echo "Sorry there are no names that meet your criteria, please go back and revise the options.";
+    exit;
+}
+?>
+<div class="no-print">
+  <p>Printing Instructions:<br/>
+    Step 1: Set the Orientation to <u>Portrait</u><br/>
+    Step 2: Check 'Shrink to fit Page Width'<br/>
+    Step 3: In Options check 'Print Background (colors & images)'<br/>
+    Step 4: In the second tab set all Margins to 0.0 inches (All Sides)<br/>
+    Step 5: Set all Headers & Footers to Blank</p>
+  <p class='print'>
+    <input type="button" value="Print" onclick="window.print()"/>
+  </p>
+</div>
+<?
+//echo "<pre>"; print_r( $names ); echo "</pre>";
+foreach ($names as $school => $info) {
+    foreach ($info as $grade => $dates) {
+        foreach ($dates as $date => $other) {
+            foreach ($other as $parsha => $children) {
+                foreach ($children as $child) {
+                    ?>
+                  <div class="page"></div>
+                  <div class="name">
+                      <?= $child['name'] ?><br/>
+                    <span class="grade">
+	                                <?= $school ?> Platoon: <?= $grade ?> - <?= $child['age'] ?> yrs. old
+	                                <br/><?= $date ?> - <?= $parsha ?>
 	                            </span>
-	                        </div>
-	                        <div class="page-break"></div>
-	                        <?
-	                    } 
-	                }
-				}
+                  </div>
+                  <div class="page-break"></div>
+                    <?
+                }
             }
         }
-        ?>
-    </body>
+    }
+}
+?>
+</body>
 </html>
--- mashpia.com/public/school_birthdays.php
+++ mashpia.com/public/school_birthdays.php
@@ -3,7 +3,7 @@ $admin_auth = array('user');
 require('header.php');
 
 require_once 'class.adminSchools.php';
-$as = new AdminSchools( $admin_user['admin_id'], $admin_user['auth'] );
+$as = new AdminSchools($admin_user['admin_id'], $admin_user['auth']);
 $schools = $as->getSchools();
 
 require_once 'class.globalSettings.php';
@@ -22,146 +22,150 @@ $today = unixtojd();
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN""http://www.w3.org/TR/html4/strict.dtd">
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>School Birthdays</title>
-        <link href="/admin_styles.css" rel="stylesheet" type="text/css">
-        <style type="text/css">
-            tr, th, td {
-                padding: 10px;
-                vertical-align: top;
-            }
-            .scrollable {
-                height: 400px;
-                overflow-x: scroll;
-            }
-            .scrollable .mosdos {
-                width: 400px;
-            }
-            .scrollable .periods {
-                width: 200px;
-            }
-        </style>
-        <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
-        <script type="text/javascript">
-            $( function() {
-                $("#checkSchools").click( function() {
-                    $(".school").attr("checked", true);      
-                });
-                
-                $("#uncheckSchools").click( function() {
-                    $(".school").attr("checked", false);
-                });
-                
-                $("#checkParshas").click( function() {
-                    $(".parsha").attr("checked", true);
-                });
-                
-                $("#uncheckParshas").click( function() {
-                    $(".parsha").attr("checked", false);
-                });
-                
-                $('#get_birthdays').click(function(){
-                    var schools = '';
-                    $("#school_id input:checked").each(function(){
-                        schools = schools + $(this).val() + ':';
-                    });
-                    schools = schools.substr(0, schools.length - 1)
-                    $('#schools').val(schools);
-                    
-                    var parshas = '';
-                    $("#parsha_period input:checked").each(function(){
-                        parshas = parshas + $(this).val() + ':'
-                    });
-                    parshas = parshas.substr(0, parshas.length - 1);                
-                    $('#parshas').val(parshas);
-                    /*
-                    var method = $(".method:checked").val();
-                    $("#method").val( method );
-                    */
-                   	var gender = $(".gender");
-                   	$.each(gender, function() {
-                   		if ($(this).is(":checked")) {
-                   			$("#gender").val($(this).val());
-                   		}
-                   	});
-               
-                    if (schools == '' || parshas == '' || $("#gender").val() == '')
-                        alert('You must pick at least one school and one parsha and the gender.');
-                    else
-                        $('#get_birthday_form').submit();
-                });
-            });
-        </script>
-    </head>
-    
-    <body>
-        <? require 'admin_header.php'; ?>        
-        <h1>School Birthdays</h1>
-        
-        <form name="get_birthday_form" id="get_birthday_form" method="post" action="birthday_cert.php">
-            <input type="hidden" name="schools" id="schools" />
-            <input type="hidden" name="parshas" id="parshas" />
-            <input type="hidden" name="gender" id="gender" />
-        </form>
-        
-        <div class="body left marking_missions">
-                    
-            <div class="infobox2 marking_list clearfix">
-        
-                <div align="center">
-                    <table>
-                        <tr>
-                            <th>Select Schools</th>
-                            <th>Select Parshas</th>
-                        </tr>
-                        <tr>
-                            <td id="school_id">
-                                <div class="scrollable mosdos">
-                                    <? 
-                                    foreach ( $schools as $id => $school ) {
-                                        //skip certain schools
-                                        if ( in_array( $id, array(82,65,79,187,198,241) ) ) 
-                                            continue;
-                                        echo "<input type='checkbox' name='schools[]' value='" . $id . "' class='school'>" . $school . "<br />";
-                                    } 
-                                    ?>
-                                </div>
-                            </td>
-                            <td id="parsha_period">
-                                <div class="scrollable periods">
-                                    <? 
-                                    foreach ( $dates as $date ) { 
-                                        echo "<input type='checkbox' name='parshas[]' value='" . 
-                                            $date['end'] . "' class='parsha'>" .
-                                            $date['name'] . "<br />";
-                                    }
-                                    ?>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                        	<td colspan="2" align="center">
-                        		<input type="radio" class="gender" value="m" /> boys 
-                        		<input type="radio" class="gender" value="f" /> girls
-                        	</td>
-                        </tr>
-                        <tr>
-                            <td align="center"><input type='button' id='checkSchools' value='Check All' />
-                                <input type='button' id='uncheckSchools' value='Uncheck All' /></td>
-                            <td align="center"><input type='button' id='checkParshas' value='Check All' />
-                                <input type='button' id='uncheckParshas' value='Uncheck All' /></td>
-                        </tr>
-                        <tr>
-                            <td colspan="2" align="center">
-                                <input type="button" id="get_birthdays" value="Generate Birthday Certs">
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-                
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <title>School Birthdays</title>
+  <link href="/admin_styles.css" rel="stylesheet" type="text/css">
+  <style type="text/css">
+    tr, th, td {
+      padding: 10px;
+      vertical-align: top;
+    }
+
+    .scrollable {
+      height: 400px;
+      overflow-x: scroll;
+    }
+
+    .scrollable .mosdos {
+      width: 400px;
+    }
+
+    .scrollable .periods {
+      width: 200px;
+    }
+  </style>
+  <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+  <script type="text/javascript">
+    $(function () {
+      $("#checkSchools").click(function () {
+        $(".school").attr("checked", true);
+      });
+
+      $("#uncheckSchools").click(function () {
+        $(".school").attr("checked", false);
+      });
+
+      $("#checkParshas").click(function () {
+        $(".parsha").attr("checked", true);
+      });
+
+      $("#uncheckParshas").click(function () {
+        $(".parsha").attr("checked", false);
+      });
+
+      $('#get_birthdays').click(function () {
+        var schools = '';
+        $("#school_id input:checked").each(function () {
+          schools = schools + $(this).val() + ':';
+        });
+        schools = schools.substr(0, schools.length - 1)
+        $('#schools').val(schools);
+
+        var parshas = '';
+        $("#parsha_period input:checked").each(function () {
+          parshas = parshas + $(this).val() + ':'
+        });
+        parshas = parshas.substr(0, parshas.length - 1);
+        $('#parshas').val(parshas);
+        /*
+        var method = $(".method:checked").val();
+        $("#method").val( method );
+        */
+        var gender = $(".gender");
+        $.each(gender, function () {
+          if ($(this).is(":checked")) {
+            $("#gender").val($(this).val());
+          }
+        });
+
+        if (schools == '' || parshas == '' || $("#gender").val() == '')
+          alert('You must pick at least one school and one parsha and the gender.');
+        else
+          $('#get_birthday_form').submit();
+      });
+    });
+  </script>
+</head>
+
+<body>
+<? require 'admin_header.php'; ?>
+<h1>School Birthdays</h1>
+
+<form name="get_birthday_form" id="get_birthday_form" method="post" action="birthday_cert.php">
+  <input type="hidden" name="schools" id="schools"/>
+  <input type="hidden" name="parshas" id="parshas"/>
+  <input type="hidden" name="gender" id="gender"/>
+</form>
+
+<div class="body left marking_missions">
+
+  <div class="infobox2 marking_list clearfix">
+
+    <div align="center">
+      <table>
+        <tr>
+          <th>Select Schools</th>
+          <th>Select Parshas</th>
+        </tr>
+        <tr>
+          <td id="school_id">
+            <div class="scrollable mosdos">
+                <?
+                foreach ($schools as $id => $school) {
+                    //skip certain schools
+                    if (in_array($id, array(82, 65, 79, 187, 198, 241)))
+                        continue;
+                    echo "<input type='checkbox' name='schools[]' value='" . $id . "' class='school'>" . $school . "<br />";
+                }
+                ?>
             </div>
-            
-        </div>
-    </body>    
+          </td>
+          <td id="parsha_period">
+            <div class="scrollable periods">
+                <?
+                foreach ($dates as $date) {
+                    echo "<input type='checkbox' name='parshas[]' value='" .
+                        $date['end'] . "' class='parsha'";
+                    if ($date['start'] == 2460280) echo " checked";
+                    echo ">" . $date['name'] . "<br />";
+                }
+                ?>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td colspan="2" align="center">
+            <input type="radio" class="gender" value="m"/> boys
+            <input type="radio" class="gender" value="f"/> girls
+          </td>
+        </tr>
+        <tr>
+          <td align="center"><input type='button' id='checkSchools' value='Check All'/>
+            <input type='button' id='uncheckSchools' value='Uncheck All'/></td>
+          <td align="center"><input type='button' id='checkParshas' value='Check All'/>
+            <input type='button' id='uncheckParshas' value='Uncheck All'/></td>
+        </tr>
+        <tr>
+          <td colspan="2" align="center">
+            <input type="button" id="get_birthdays" value="Generate Birthday Certs">
+          </td>
+        </tr>
+      </table>
+    </div>
+
+  </div>
+
+</div>
+</body>
 </html>
