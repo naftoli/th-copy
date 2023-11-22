a34e33204ac1ec7758ad45198f526729a8522cd8
naftoli rapoport <naftolir@gmail.com>
2023-11-22 10:11:25 -0500

    make sure prev yrs marks don't get overriden

--- mashpia.com/public/chidonTests/class.chidonTests.php
+++ mashpia.com/public/chidonTests/class.chidonTests.php
@@ -171,7 +171,7 @@ class ChidonTests
         ");
         $stmt->execute([':year' => $yr]);
         $this->children = $stmt->fetchAll();
-        echo "<pre>"; print_r($this->children); echo "</pre>"; exit;
+//        echo "<pre>"; print_r($this->children); echo "</pre>"; exit;
     }
 
     public function setScores() {
