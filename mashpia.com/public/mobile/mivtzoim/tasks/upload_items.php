a386fd35f26597d040806a6091e5280ba2ae0f65
naftoli rapoport <naftolir@gmail.com>
2023-11-22 16:13:28 -0500

    hei teves sale

---
+++ mashpia.com/public/mobile/mivtzoim/tasks/upload_items.php
@@ -0,0 +1,64 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL);
+
+$admin_auth = ['school'];
+require $_SERVER['DOCUMENT_ROOT'] . '/header.php';
+
+if ($admin_user['auth'] != 'super') {
+    echo "You are not authorized to view this page.";
+    exit;
+}
+
+require $_SERVER['DOCUMENT_ROOT'] . '/api/header/db.php';
+
+if (isset($_FILES['items'])) {
+    $stmt = $MASHPIA_DB->prepare("
+        INSERT INTO mivtzoim_items
+        SET
+            item = :item,
+            type = :type,
+            msrp = :msrp,
+            sale = :sale,
+            stock = :stock,
+            thumb = :thumb
+    ");
+    $MASHPIA_DB->startTransaction();
+
+    $file = fopen($_FILES['items']['tmp_name'], 'r');
+    while (($line = fgetcsv($file)) !== FALSE) {
+        $item = $line[0];
+        $type = $line[1];
+        $msrp = $line[2];
+        $sale = $line[3];
+        $stock = $line[4];
+        $image = $line[5];
+        if (! $stmt->execute([
+            ':item' => $item,
+            ':type' => 'Hei Teves',
+            ':msrp' => $msrp,
+            ':sale' => $sale,
+            ':stock' => $stock,
+            ':thumb' => $image
+        ])) {
+            $MASHPIA_DB->rollback();
+            echo "Error: " . $stmt->errorInfo()[2];
+            exit;
+        }
+    }
+    $MASHPIA_DB->commit();
+    echo "done.";
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Upload Items</title>
+</head>
+<body>
+    <form method="post" enctype="multipart/form-data">
+        <input type="file" name="items" />
+        <input type="submit" value="Upload" />
+    </form>
+</body>
+</html>
