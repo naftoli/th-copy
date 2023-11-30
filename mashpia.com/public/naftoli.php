7af1214aa1eb9fc2e239ca9099c1098b046bb6c4
naftoli rapoport <naftolir@gmail.com>
2023-11-30 14:37:10 -0500

    updates for showing report card on parent accounts

--- mashpia.com/public/naftoli.php
+++ mashpia.com/public/naftoli.php
@@ -1,3 +1,6 @@
 <?php
-if (! isset($_COOKIE['naftoli'])) setcookie('naftoli', '1');
+if (! isset($_COOKIE['naftoli'])) {
+    setcookie('naftoli', '1');
+    setcookie('naftoli', '1', 0, "/mobile/");
+}
 echo "Cookie set.";
