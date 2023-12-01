118e2a576c1db05530ba86aac6288e737527a268
naftoli rapoport <naftolir@gmail.com>
2023-11-30 20:18:40 -0500

    turn off chidon editing but allow with cookie

--- mashpia.com/public/api/models/Soldier.php
+++ mashpia.com/public/api/models/Soldier.php
@@ -453,7 +453,7 @@ class Soldier extends \ActiveRecord\Model implements \JsonSerializable {
         }
 
         // turn off chidon edit if no cookie
-        $result['chidonEdit'] = false;
+        if (! isset($_COOKIE['naftoli'])) $result['chidonEdit'] = false;
 
         // turn off chayolei and chidon reg if school has not registered yet
         if (
