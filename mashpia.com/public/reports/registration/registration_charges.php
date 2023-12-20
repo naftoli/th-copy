d02441251d064bfd37f16ef5368e9a3fef740b57
naftoli rapoport <naftolir@gmail.com>
2023-12-20 06:21:27 -0500

    update ability to create refunds

--- mashpia.com/public/reports/registration/registration_charges.php
+++ mashpia.com/public/reports/registration/registration_charges.php
@@ -1,18 +1,21 @@
 <?php
-$admin_auth = array(); 	
-require_once ( __DIR__ . '/../../header.php' ); 
+$admin_auth = array();
+require_once(__DIR__ . '/../../header.php');
 
-require_once ( __DIR__ . '/../../class.globalSettings.php' ); 
+require_once(__DIR__ . '/../../class.globalSettings.php');
 $year = GlobalSettings::getRegistrationYear();
 
 // get the totals
 $totals = [];
 $total_query = mysql_query(
-    "SELECT type, SUM( amount ) AS total FROM registration_charges WHERE year = $year GROUP BY type ORDER BY type;"
+    "SELECT type, SUM( amount ) AS total 
+    FROM registration_charges 
+    WHERE year = $year AND refunded = 0 
+    GROUP BY type ORDER BY type;"
 );
 $grand_total = 0;
-while ( $row = mysql_fetch_assoc( $total_query ) ) {
-    $grand_total += intval( $row['total'] );
+while ($row = mysql_fetch_assoc($total_query)) {
+    $grand_total += intval($row['total']);
     $totals[getDescription($row['type'])] = intval($row['total']);
 }
 ksort($totals);
@@ -20,84 +23,135 @@ ksort($totals);
 // get the details
 $details = [];
 $detail_query = mysql_query(
-    "SELECT s.school_name, s.school_number, u.user_serial, u.first, u.last, rc.type, rc.date, rc.year, rc.amount "
-    ."FROM registration_charges rc LEFT JOIN schools s USING ( school_id ) "
-    ."LEFT JOIN users u USING ( user_id ) LEFT JOIN transactions t USING ( trans_id ) "
-    ."WHERE year = $year ORDER BY rc.date DESC, school_name, u.first, u.last, rc.amount;"
+    "SELECT s.school_name, s.school_number, u.user_serial, u.first, u.last, rc.type, rc.date, rc.year, rc.amount, rc.refunded "
+    . "FROM registration_charges rc LEFT JOIN schools s USING ( school_id ) "
+    . "LEFT JOIN users u USING ( user_id ) LEFT JOIN transactions t USING ( trans_id ) "
+    . "WHERE year = $year ORDER BY rc.date DESC, school_name, u.first, u.last, rc.amount;"
 );
-while ( $row = mysql_fetch_assoc( $detail_query ) ) $details[] = $row;
+while ($row = mysql_fetch_assoc($detail_query)) $details[] = $row;
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <meta charset="utf-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title><?=$year?> Registration Charges</title>
+    <title><?= $year ?> Registration Charges</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="/admin_styles.css" rel="stylesheet" type="text/css" />
+    <link href="/admin_styles.css" rel="stylesheet" type="text/css"/>
     <style>
-        table { width: 100%; }
-        th, td { border: 1px solid #888; padding: 4px 8px; }
-        #details { font-size: 14px; }
+      table {
+        width: 100%;
+      }
+
+      th, td {
+        border: 1px solid #888;
+        padding: 4px 8px;
+      }
+
+      #details {
+        font-size: 14px;
+      }
     </style>
-</head>
-<body>
-    <?php include( __DIR__ . '/../../admin_header.php'); ?>
-    <h1><?=$year?> Soldier Registration Charges</h1>
-    <h2>Totals</h2>
-    <table>
-        <thead>
-            <th>Registration Type</th>
-            <th>Total Received</th>
-        </thead>
-        <tbody>
-            <?php
-                foreach( $totals as $type => $total ) { ?>
-                <tr>
-                    <td><?= $type ?></td>
-                    <td>$<?=number_format( $total ) ?></td>
-                </tr>
-            <?php } ?>
-            <tr>
-                <th>Grand Total</th>
-                <th>$<?=number_format( $grand_total ) ?></th>
-            </tr>
-        </tbody>
-    </table>
-    <h2>Details</h2>
-    <table id='details'>
-        <thead>
-            <th colspan='2'>Base</th>
-            <th>Serial Number</th>
-            <th>Name</th>
-            <th>Registration Type</th>
-            <th>Registration Time</th>
-            <th>Amount Paid</th>
-        </thead>
-        <tbody>
-            <?php
-                foreach( $details as $user ) { ?>
-                <tr>
-                    <td><?= $user['school_number'] ?></td>
-                    <td><?= $user['school_name'] ?></td>
-                    <td><?= $user['user_serial'] ?></td>
-                    <td><?= $user['first'] . " " . $user['last'] ?></td>
-                    <td><?= $user['type'] ?></td>
-                    <td><?= ( new DateTime($user[ 'date' ]) )->format( 'm/d/Y g:i:sa e' ); ?></td>
-                    <td>$<?= $user['amount'] ?></td>
-                </tr>
-            <?php } ?>
-        </tbody>
-    </table>
-</body>
-</html>
+  </head>
+  <body>
+  <?php include(__DIR__ . '/../../admin_header.php'); ?>
+  <h1><?= $year ?> Soldier Registration Charges</h1>
+  <h2>Totals</h2>
+  <table>
+    <thead>
+    <th>Registration Type</th>
+    <th>Total Received</th>
+    </thead>
+    <tbody>
+    <?php
+    foreach ($totals as $type => $total) { ?>
+      <tr>
+        <td><?= $type ?></td>
+        <td>$<?= number_format($total) ?></td>
+      </tr>
+    <?php } ?>
+    <tr>
+      <th>Grand Total</th>
+      <th>$<?= number_format($grand_total) ?></th>
+    </tr>
+    </tbody>
+  </table>
+  <h2>Details</h2>
+  <table id='details'>
+    <thead>
+    <th colspan='2'>Base</th>
+    <th>Serial Number</th>
+    <th>Name</th>
+    <th>Registration Type</th>
+    <th>Registration Type Code</th>
+    <th>Registration Time</th>
+    <th>Amount Paid</th>
+    <th>Refunded</th>
+    <th></th>
+    </thead>
+    <tbody>
+    <?php
+    foreach ($details as $user) { ?>
+      <tr>
+        <td><?= $user['school_number'] ?></td>
+        <td><?= $user['school_name'] ?></td>
+        <td><?= $user['user_serial'] ?></td>
+        <td><?= $user['first'] . " " . $user['last'] ?></td>
+        <td><?= getDescription($user['type']) ?></td>
+        <td><?= $user['type'] ?></td>
+        <td><?= (new DateTime($user['date']))->format('m/d/Y g:i:sa e'); ?></td>
+        <td>$<?= $user['amount'] ?></td>
+        <td><?= intval($user['refunded']) ? 'Yes' : 'No'; ?></td>
+        <td>
+          <button class="refund"
+              <?php if (intval($user['refunded'])) echo " disabled"; ?>
+          >Refund Charge
+          </button>
+        </td>
+      </tr>
+    <?php } ?>
+    </tbody>
+  </table>
+  </body>
+  <script>
+    $(document).ready(function () {
+      $('.refund').click(function () {
+        var row = $(this).closest('tr');
+        var serial = row.find('td:nth-child(3)').text();
+        var name = row.find('td:nth-child(4)').text();
+        var type = row.find('td:nth-child(5)').text();
+        var amount = row.find('td:nth-child(7)').text();
+        var confirmed = confirm('Are you sure you want to refund $' + amount + ' for ' + name + ' (' + serial + ') for ' + type + '?');
+        if (confirmed) {
+          $.ajax({
+            url: 'refund.php',
+            type: 'POST',
+            data: {
+              serial: serial,
+              type: type,
+              amount: amount
+            },
+            success: function (data) {
+              if (data == 'success') {
+                row.find('td:nth-child(8)').text('Yes');
+                row.find('td:nth-child(9) button').attr('disabled', true')
+              } else {
+                alert('Error: ' + data);
+              }
+            }
+          });
+        }
+      });
+    });
+  </script>
+  </html>
 <?php
 // lookup description for registration charges table by codeOnly property
 function getDescription($code)
 {
     $descriptions = [
-        'chayolei'  => 'CTH enrollment',
-        'shipping'  => 'Shipping Fee (before the codes)',
+        'chayolei' => 'CTH enrollment',
+        'shipping' => 'Shipping Fee (before the codes)',
 
         'THE' => 'CTH enrollment',
         'HACH' => 'Hachayol subscription',
