a15e5d87cf9563ca87498480fd3d7f53096bd4f6
naftoli rapoport <naftolir@gmail.com>
2023-12-27 04:21:17 -0500

    start working on limud eval  report

---
+++ mashpia.com/public/chidonOld/newReports/api/limudEval.php
@@ -0,0 +1,32 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL);
+
+$admin_auth = ['school'];
+require_once $_SERVER['DOCUMENT_ROOT'] . '/header.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/class.adminSchools.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/class.globalSettings.php';
+
+$as = new AdminSchools($admin_user['admin_id'], $admin_user['auth'], true, true);
+$schools = $as->getSchools();
+$year = GlobalSettings::getChidonYear();
+
+$grades = [
+    0 => 'All Classes',
+];
+
+if (count($schools) == 1) {
+    $school_id = key($schools);
+    $sql = "select * from classes where class_era = 0 and class_grade in ('4', '5', '6', '7', '8') and school_id = " . $school_id;
+    $result = mysql_query($sql);
+    while ($row = mysql_fetch_assoc($result)) {
+        $grades[$row['class_id']] = $row['class_grade'] . ($row['class_sub'] ? '-' . $row['class_sub'] : '');
+    }
+}
+
+echo json_encode([
+    'success'   => true,
+    'schools'   => $schools,
+    'years'     => [$year - 1, $year],
+    'grades'    => $grades,
+]);
---
+++ mashpia.com/public/chidonOld/newReports/api/limudInfo.php
@@ -0,0 +1,27 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL);
+
+$admin_auth = ['school'];
+require_once $_SERVER['DOCUMENT_ROOT'] . '/header.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/api/header/header.php';
+
+$input = json_decode(file_get_contents('php://input'), true);
+$year = $input['year'];
+$school_id = $input['school'];
+$class_id = $input['grade'];
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/chidonTests/class.chidonTests.php';
+$ct = new ChidonTests($year);
+$ct->setStudents($school_id, $class_id);
+$ct->setScores();
+$ct->calculateMarks();
+
+$students = $ct->getStudents();
+$marks = $ct->getMarks();
+
+echo json_encode([
+    'success'   => true,
+    'info'      => $students,
+    'marks'     => $marks,
+]);
