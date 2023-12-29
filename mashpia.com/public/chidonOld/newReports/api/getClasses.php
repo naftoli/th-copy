4f7424be90b1f36619dc355a3ca7d593a4c6882c
naftoli rapoport <naftolir@gmail.com>
2023-12-29 14:17:17 -0500

    updates to limud eval report

--- mashpia.com/public/chidonOld/newReports/api/getClasses.php
+++ mashpia.com/public/chidonOld/newReports/api/getClasses.php
@@ -5,7 +5,8 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/header.php';
 $input = json_decode(file_get_contents('php://input'), true);
 $school_id = $input['school'];
 
-$sql = "select * from classes where class_era = 0 and school_id = " . $school_id;
+$sql = "select * from classes where class_era = 0 and school_id = " . $school_id . " and 
+        class_grade in ('4', '5', '6', '7', '8') order by class_grade, class_sub";
 $result = mysql_query($sql);
 while ($row = mysql_fetch_assoc($result)) {
     $grades[$row['class_id']] = $row['class_grade'] . ($row['class_sub'] ? '-' . $row['class_sub'] : '');
--- mashpia.com/public/chidonOld/newReports/api/limudInfo.php
+++ mashpia.com/public/chidonOld/newReports/api/limudInfo.php
@@ -8,7 +8,8 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/header.php';
 $input = json_decode(file_get_contents('php://input'), true);
 $year = $input['year'];
 $school_id = $input['school'];
-$class_id = $input['grade'];
+$class_id = intval($input['grade']);
+$test_num = $input['test_num'];
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/chidonTests/class.chidonTests.php';
 $ct = new ChidonTests($year);
@@ -18,9 +19,46 @@ $ct->calculateMarks();
 
 $students = $ct->getStudents();
 $marks = $ct->getMarks();
+$num_questions = $ct->getTestQuestions();
+$dates = $ct->getDates();
+
+$learning_time = [
+    'maven'     => 10,
+    'pro'       => 20,
+    'expert'    => 30,
+    'genius'    => 45,
+];
+$total_days = [32, 37, 37];
+
+$info = [];
+foreach ($students as $student) {
+    $learned = $ct->getTotalMinutesLearned($student['user_id'], $dates[$test_num], true);
+    $track_passed = isset($marks[$student['th_chidon_id']]) ? $ct->getHighestTrack($marks[$student['th_chidon_id']], $student['user_id']) : '';
+    $info[] = [
+        'user_id'   => $student['user_id'],
+        'school_id' => $student['school_id'],
+        'school_name'   => $student['school_name'],
+        'serial'    => $student['user_serial'],
+        'name'      => $student['first'] . ' ' . $student['last'],
+        'grade'     => $student['class_grade'] . ($student['class_sub'] ? '-' . $student['class_sub'] : ''),
+        'track'     => $student['test_type'],
+        'reward'    => $student['reward_type'] === 'highest track passed' ? $track_passed : $student['reward_type'],
+        'passing_avg'   => $ct->getPassingAvgs($student['user_id'])[$student['test_type']],
+        'yesod'     => ($marks[$student['th_chidon_id']][$test_num]['maven'] ?? 0 . '/' . $num_questions['maven']),
+        'yediah'    => ($marks[$student['th_chidon_id']][$test_num]['pro'] ?? 0 . '/' . $num_questions['pro']),
+        'havonah'   => ($marks[$student['th_chidon_id']][$test_num]['expert'] ?? 0 . '/' . $num_questions['expert']),
+        'iyun'      => ($marks[$student['th_chidon_id']][$test_num]['genius'] ?? 0 . '/' . $num_questions['genius']),
+        'non_cumulative_track_passed'   => $track_passed,
+        'cumulative_track_passed'   => '',
+        'time_committed'    => $learning_time[$student['test_type']] * $total_days[$test_num],
+        'time_learned'  => $learned,
+        'dropped_out'   => 0,
+        'reason'        => '',
+    ];
+}
 
 echo json_encode([
     'success'   => true,
-    'info'      => $students,
+    'info'      => $info,
     'marks'     => $marks,
 ]);
