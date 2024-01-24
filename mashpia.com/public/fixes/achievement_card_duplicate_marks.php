16dbf71feaf9330a76301ce7ff0be11f439bf288
naftoli rapoport <naftolir@gmail.com>
2024-01-24 12:04:25 -0500

    work on finding duplicate achievement cards

--- mashpia.com/public/fixes/achievement_card_duplicate_marks.php
+++ mashpia.com/public/fixes/achievement_card_duplicate_marks.php
@@ -3,50 +3,103 @@ require '../db.php';
 
 $rows = [];
 $sql = "SELECT 
+            first,
+            last,
+            class_grade,
+            class_sub,
             user_point_id,
             user_id,
             achievement_card_id,
+            card_points,
             COUNT(*) AS total
         FROM
-            pointsDB.user_points
+            pointsDB.user_points up
+                JOIN
+            pointsDB.achievement_cards USING (achievement_card_id)
+                JOIN
+            users u USING (user_id)
+                JOIN
+            classes c ON c.class_id = u.class_id
         WHERE
-            created > '2023-06-01'
+            up.created > '2023-06-01'
                 AND achievement_card_id > 0
+                AND up.institution_id = 255
         GROUP BY achievement_card_id
-        HAVING total > 1";
+        HAVING total > 1 
+        ORDER BY class_grade, class_sub, last, first";
 $result = mysql_query($sql);
 while ($row = mysql_fetch_assoc($result)) {
     $rows[] = $row;
 }
 
-$toDelete = [];
-foreach ($rows as $row) {
-    $sql = "SELECT 
-                user_point_id
-            FROM
-                pointsDB.user_points
-            WHERE
-                achievement_card_id = " . $row['achievement_card_id'] . " 
-                    AND user_id = " . $row['user_id'];
-    $result = mysql_query($sql);
-    $i = 0;
-    while ($r = mysql_fetch_assoc($result)) {
-        if (++$i > 1) {
-            $toDelete[] = $r['user_point_id'];
-        }
-    }
-}
-
-$qrys = [];
-foreach ($toDelete as $user_point_id) {
-    $qry = "delete from pointsDB.user_points where user_point_id = " . $user_point_id;
-    $qrys[] = $qry;
-}
-
-$deleted = 0;
-foreach ($qrys as $qry) {
-    echo $qry . "<br />";
-//    if (mysql_query($qry)) $deleted++;
-}
-
-echo "Deleted: " . $deleted;
+//$toDelete = [];
+//foreach ($rows as $row) {
+//    $sql = "SELECT
+//                user_point_id
+//            FROM
+//                pointsDB.user_points
+//            WHERE
+//                achievement_card_id = " . $row['achievement_card_id'] . "
+//                    AND user_id = " . $row['user_id'];
+//    $result = mysql_query($sql);
+//    $i = 0;
+//    while ($r = mysql_fetch_assoc($result)) {
+//        if (++$i > 1) {
+//            $toDelete[] = $r['user_point_id'];
+//        }
+//    }
+//}
+//
+//$qrys = [];
+//foreach ($toDelete as $user_point_id) {
+//    $qry = "delete from pointsDB.user_points where user_point_id = " . $user_point_id;
+//    $qrys[] = $qry;
+//}
+//
+//$deleted = 0;
+//foreach ($qrys as $qry) {
+//    echo $qry . "<br />";
+////    if (mysql_query($qry)) $deleted++;
+//}
+//
+//echo "Deleted: " . $deleted;
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Points</title>
+        <style>
+            table {
+                border-collapse: collapse;
+            }
+            td, th {
+                border: 1px solid black;
+                padding: 5px;
+            }
+        </style>
+    </head>
+    <body>
+        <table>
+            <tr>
+                <th>Grade</th>
+                <th>Name</th>
+                <th>Points</th>
+                <th>Card ID</th>
+                <th>User ID</th>
+                <th>Card Count</th>
+                <th>Points</th>
+            </tr>
+            <?php foreach ($rows as $row) { ?>
+                <tr>
+                    <td><?php echo $row['first'] . ' ' . $row['last'] ?></td>
+                    <td><?php echo $row['class_grade'] . ($row['class_sub'] ? '-' . $row['class_sub'] : '') ?></td>
+                    <td><?php echo $row['user_point_id'] ?></td>
+                    <td><?php echo $row['achievement_card_id'] ?></td>
+                    <td><?php echo $row['user_id'] ?></td>
+                    <td><?php echo $row['total'] ?></td>
+                    <td><?php echo (intval($row['card_points']) * intval($row['total'])) ?></td>
+                </tr>
+            <?php } ?>
+        </table>
+    </body>
+</html>
