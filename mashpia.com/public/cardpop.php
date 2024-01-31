c6ebd32b8929218a2e18d00021f82e9d1e0d6f60
naftoli rapoport <naftolir@gmail.com>
2024-01-31 08:41:21 -0500

    try to fix issue with slowness in getting points for cards

--- mashpia.com/public/cardpop.php
+++ mashpia.com/public/cardpop.php
@@ -2,9 +2,9 @@
 require $_SERVER['DOCUMENT_ROOT'] . '/db.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/class.points.php';
 
-$school = $_REQUEST['school_id'];
-$user = $_REQUEST['user_id'];
-$card = $_REQUEST['card'];
+$school = mysql_real_escape_string($_REQUEST['school_id']);
+$user = mysql_real_escape_string($_REQUEST['user_id']);
+$card = mysql_real_escape_string($_REQUEST['card']);
 
 $msg = Points::scanMiles($school, $user, $card);
 echo json_encode($msg);
