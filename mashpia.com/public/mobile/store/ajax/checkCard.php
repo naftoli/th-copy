267401ecb2741a5ea25af2c3bcd74d82efd54368
naftoli rapoport <naftolir@gmail.com>
2024-01-31 08:39:55 -0500

    try to fix issue with slowness in getting points for cards

--- mashpia.com/public/mobile/store/ajax/checkCard.php
+++ mashpia.com/public/mobile/store/ajax/checkCard.php
@@ -3,11 +3,15 @@ require $_SERVER['DOCUMENT_ROOT'] . '/db.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/class.points.php';
 
 $input = json_decode(file_get_contents('php://input'), true);
-$user = $input['user'];
-$admin = $input['admin'];
-$card = $input['card'];
+$user = mysql_real_escape_string($input['user']);
+$admin = mysql_real_escape_string($input['admin']);
+$card = mysql_real_escape_string($input['card']);
 
-$p = new Points($user);
-$msg = $p->scanMiles($card);
+// get school id from user
+$res = mysql_query("select school_id from users where user_id = $user");
+$row = mysql_fetch_assoc($res);
+$school = $row['school_id'];
+
+$msg = Points::scanMiles($school, $user, $card);
 echo json_encode($msg);
 ?>
