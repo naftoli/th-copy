654d36df595037fca7e0f55fe88ad0b63f868409
naftoli rapoport <naftolir@gmail.com>
2024-02-07 08:06:28 -0500

    fix achievement card issues for parent accounts

--- mashpia.com/public/mobile/store/ajax/checkCard.php
+++ mashpia.com/public/mobile/store/ajax/checkCard.php
@@ -1,18 +1,21 @@
 <?php
-require $_SERVER['DOCUMENT_ROOT'] . '/db.php';
-require $_SERVER['DOCUMENT_ROOT'] . '/class.points.php';
+//ini_set('display_errors', 1);
+//ini_set('error_reporting', E_ALL);
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/db.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/class.points.php';
 
 $input = json_decode(file_get_contents('php://input'), true);
 $user = mysql_real_escape_string($input['user']);
-$admin = mysql_real_escape_string($input['admin']);
 $card = mysql_real_escape_string($input['card']);
 
 // get school id from user
-$res = mysql_query("select school_id from users where user_id = $user");
+$res = mysql_query("select school_id, class_id from users where user_id = $user");
 $row = mysql_fetch_assoc($res);
 $school = $row['school_id'];
+$class = $row['class_id'];
 
-$msg = Points::scanMiles($school, $user, $card);
+$msg = Points::scanMiles($school, $class, $user, $card);
 echo $msg;
 $scanned = json_decode($msg, true);
 if ($scanned['success']) Points::updateScanned($card);
