4e4777ae998c70254678e2009b03921179fd9ad0
naftoli rapoport <naftolir@gmail.com>
2024-02-07 03:59:25 -0500

    try to fix up achievement card issues

---
+++ mashpia.com/public/fixes/fix_achievement_cards_scanned.php
@@ -0,0 +1,20 @@
+<?php
+require $_SERVER['DOCUMENT_ROOT'] . '/db.php';
+
+$ids = [];
+$sql = "SELECT 
+            achievement_card_id, COUNT(*) AS num
+        FROM
+            pointsDB.achievement_cards_scanned
+        GROUP BY achievement_card_id
+        HAVING num > 1";
+$result = mysql_query($sql);
+while ($row = mysql_fetch_assoc($result)) {
+    $ids[] = $row['achievement_card_id'];
+}
+
+foreach ($ids as $id) {
+    $sql = "delete from achievement_cards_scanned where achievement_card_id = $id";
+    mysql_query($sql);
+}
+echo "done";
