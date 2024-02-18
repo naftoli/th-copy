a788fbfb11bab8b8a631362efb0c254eb526707a
naftoli rapoport <naftolir@gmail.com>
2024-02-18 15:54:15 -0500

    refunds report

---
+++ mashpia.com/public/chidonOld/newReports/api/refunds.php
@@ -0,0 +1,22 @@
+<?php
+$admin_auth = ['school'];
+require_once $_SERVER['DOCUMENT_ROOT'] . '/header.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/api/header/db.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/class.globalSettings.php';
+$year = GlobalSettings::getChidonYear();
+
+if ($admin_user['auth'] != 'super') {
+    echo "No Permission.";
+    exit;
+}
+
+$info = [];
+$sql = "select * from family_prepaid_balances where year = :year";
+$stmt = $MASHPIA_DB->prepare($sql);
+$res = $stmt->execute([
+    ':year' => $year
+]);
+if ($res) {
+    $info = $stmt->fetchAll();
+}
+echo json_encode($info);
