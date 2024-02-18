7fa1ba7dbb0b90bbc24560bfcaf6fa71cd282a35
naftoli rapoport <naftolir@gmail.com>
2024-02-18 18:10:28 -0500

    adding and deleting reg charges

---
+++ mashpia.com/public/tasks/del_reg_charges.php
@@ -0,0 +1,53 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/api/header/db.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/class.globalSettings.php';
+$year = GlobalSettings::getChidonYear();
+
+// get uploaded csv file, parse file with following columns: id, type, amount
+if (isset($_FILES['file'])) {
+    // parse cvs
+    $file = $_FILES['file']['tmp_name'];
+    $handle = fopen($file, "r");
+    $data = fgetcsv($handle, 1000, ",");
+    $updated = 0;
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $id = $data[0];
+        $type = $data[1];
+        $amount = $data[2];
+        // update family balance
+        $stmt = $MASHPIA_DB->prepare("
+            DELETE FROM registration_charges  
+            WHERE user_id = :id 
+            AND amount = :amount 
+            AND type = :type 
+            AND year = :year
+        ");
+        if ($stmt->execute([
+            ':type'     => $type,
+            ':amount'   => $amount,
+            ':id'       => $id,
+            ':year'     => $year
+        ])) {
+            $updated++;
+        } else {
+            $stmt->errorInfo();
+            $stmt->debugDumpParams();
+            break;
+        }
+    }
+    echo "Updated $updated registration charges.";
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Fix Family Balances</title>
+</head>
+<!-- add ability to upload file -->
+<body>
+<form action="fix_family_balances.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="file" id="file">
+    <input type="submit" value="Upload">
+</form>
+</body>
