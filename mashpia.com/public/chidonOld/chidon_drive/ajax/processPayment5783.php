db9e791ed79101453731e45a858662722a606e70
naftoli rapoport <naftolir@gmail.com>
2024-02-19 05:30:57 -0500

    fix entering into chidon sihpping table

--- mashpia.com/public/chidonOld/chidon_drive/ajax/processPayment5783.php
+++ mashpia.com/public/chidonOld/chidon_drive/ajax/processPayment5783.php
@@ -67,6 +67,7 @@ $paypal_email = isset($_POST['paypal_email']) ? $_POST['paypal_email'] : '';
 define('CELEB_BOX_COST', 20);
 define('SWEATER_COST', 25);
 
+//echo "<pre>"; print_r($_POST); echo "</pre>"; exit;
 //******************* SQL QUERIES ***********************/
 $sql = "update th_chidon set paid = :paid, date_paid = now(), paid_by = :admin where year = :year and user_id = :user";
 $sqlReg = $MASHPIA_DB->prepare($sql);
@@ -533,9 +534,10 @@ function processReg() {
 function updateShipping() {
     global $MASHPIA_DB, $year, $admin_id, $shipping_charge;
 
-    $sqlInsert = "UPDATE chidon_parent_shipping 
+    $sqlInsert = "INSERT IGNORE INTO chidon_parent_shipping 
                   SET amount_paid = :amount, date_paid = now()
-                  WHERE parent_id = :admin AND year = :year";
+                  WHERE parent_id = :admin AND year = :year
+                  ON DUPLICATE KEY UPDATE amount_paid = :amount, date_paid = now()";
     $stmtInsert = $MASHPIA_DB->prepare($sqlInsert);
 
     $updated = true;
