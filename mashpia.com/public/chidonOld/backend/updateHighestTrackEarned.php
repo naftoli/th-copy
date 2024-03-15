edf86725b3b900a7eab70318ce1e4c5f9ed9a312
naftoli rapoport <naftolir@gmail.com>
2024-03-15 12:31:26 -0400

    fix up algorithm for determining highest track from tests / bcs

--- mashpia.com/public/chidonOld/backend/updateHighestTrackEarned.php
+++ mashpia.com/public/chidonOld/backend/updateHighestTrackEarned.php
@@ -42,14 +42,11 @@ foreach ($info as $row) {
     $highestTrack = $test->getHighestTrackPassed($row)['highest_track'];
     $rewardType = $row['reward_type'];
     if ($rewardType != 'highest track passed' && $rewardType != '') {
-        $highestTrack = $rewardType;
-//        if ($highestTrack == '') $highestTrack = $rewardType;
-//        else {
-//            $indexes = array_keys($tracks);
-//            $key1 = array_search($highestTrack, $indexes);
-//            $key2 = array_search($rewardType, $indexes);
-//            if ($key2 > $key1) $highestTrack = $rewardType;
-//        }
+        // figure out which one is higher, the highest track passed or the reward type
+        $indexes = array_keys($tracks);
+        $key1 = array_search($highestTrack, $indexes);
+        $key2 = array_search($rewardType, $indexes);
+        if ($key2 > $key1) $highestTrack = $rewardType;
     }
 
     $track = $highestTrack ? $tracks[$highestTrack] : 'none';
