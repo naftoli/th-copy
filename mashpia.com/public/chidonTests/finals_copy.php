a61f588179f82a2619283560ead6458ff35f7e90
naftoli rapoport <naftolir@gmail.com>
2024-03-17 13:21:58 -0400

    try to fix finals_copy.php

--- mashpia.com/public/chidonTests/finals_copy.php
+++ mashpia.com/public/chidonTests/finals_copy.php
@@ -152,10 +152,10 @@ $tooLate = false;
 // disable marking after certain dates for bc's
 if ($admin_user['auth'] != 'super') {
     $today = new DateTime();
-    $shutdown = new DateTime('2023-03-06 19:20:00');
-    if ($today >= $shutdown) {
-        $tooLate = true;
-    }
+//    $shutdown = new DateTime('2023-03-06 19:20:00');
+//    if ($today >= $shutdown) {
+//        $tooLate = true;
+//    }
 }
 ?>
 <!DOCTYPE html>
