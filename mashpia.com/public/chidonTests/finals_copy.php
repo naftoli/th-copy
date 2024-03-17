f51e01214952cf3ac434cda11c9be5d1c30d21cb
naftoli rapoport <naftolir@gmail.com>
2024-03-17 13:35:57 -0400

    fix finals_copy.php

--- mashpia.com/public/chidonTests/finals_copy.php
+++ mashpia.com/public/chidonTests/finals_copy.php
@@ -201,7 +201,7 @@ if (isset($_POST['grade'])) {
     $exceptions = [192, 63, 13, 42, 726];
     $types = $ct->getTypes();
     $tracks = array_values($types);
-    echo "<form action='finals.php' method='post' enctype='multipart/form-data'>";
+    echo "<form action='finals_copy.php' method='post' enctype='multipart/form-data'>";
     echo "<div style='float: right'><input type='submit' name='submit' value='Save' style='padding: 12px; font-size: large' /></div><br /><br />";
     foreach ($info as $school => $children) {
         if (empty($children)) continue;
@@ -265,7 +265,7 @@ if (isset($_POST['grade'])) {
     echo "</form>";
 } else {
     ?>
-  <form action="finals.php" method="post">
+  <form action="finals_copy.php" method="post">
     Choose Class: <select name="grade">
       <option value="0">All Classes</option>
           <?php
