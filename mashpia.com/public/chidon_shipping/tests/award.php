37178bb4d9d71235b591dc48ef7d5fc9e9cc724b
naftoli rapoport <naftolir@gmail.com>
2024-03-19 10:11:12 -0400

    update testing page

--- mashpia.com/public/chidon_shipping/tests/award.php
+++ mashpia.com/public/chidon_shipping/tests/award.php
@@ -11,7 +11,13 @@ require_once '../class.chidonShipping.php';
 $cs = new ChidonShipping();
 
 $user_id = $_GET['user'];
-$sql = "select * from th_chidon where year = $year and user_id = " . $user_id;
+if (strlen($user_id) == 7 && strpos($user_id, '7') === 0) {
+    $user_serial = $user_id;
+    $user_id = 0;
+}
+$sql = "select * from th_chidon where year = $year and user_id = ";
+if ($user_id) $sql .= $user_id;
+else $sql .= "(select user_id from users where user_serial = $user_serial)";
 $result = mysql_query($sql);
 $row = mysql_fetch_assoc($result);
 $award = $cs->getAwardTrack($row);
