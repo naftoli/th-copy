96eeb606261d68ac96a710c31e3a43c99867c93a
naftoli rapoport <naftolir@gmail.com>
2024-03-25 22:19:14 -0400

    fix up video files for this yr

--- mashpia.com/public/chidonOld/video/functions.php
+++ mashpia.com/public/chidonOld/video/functions.php
@@ -162,7 +162,7 @@ function createFile($name, $info, $csv = false) {
     if (is_array($info)) {
         foreach ($info as $fields) {
             if ($csv) fputcsv($fp, $fields);
-            else fputcsv($fp, $fields, "\t", '');
+            else fputcsv($fp, $fields, "\t", ' ');
 //            else fputcsv($fp, $fields, "\t", ' ');
         }
     } else {
