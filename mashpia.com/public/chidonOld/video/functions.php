b7d109030862e3d4edc091240922cf2188765030
naftoli rapoport <naftolir@gmail.com>
2024-03-31 12:51:55 -0400

    fix up video award track

--- mashpia.com/public/chidonOld/video/functions.php
+++ mashpia.com/public/chidonOld/video/functions.php
@@ -294,7 +294,7 @@ function addToSheet($child, $khk = false, $trophy = false, $track = '') {
 
     $name = trim($child['first']) . ' ' . trim($child['last']);
     $img_url = $child['user_serial'] . '.png';
-    $award = array_search($child['award_track'], $tracks);
+    $award_num = array_search($child['award_track'], $tracks);
     if (in_array($child['highest_track'], ['yesod', 'yediah'])) $trip = 0;
     else $trip = isset($child['ultimate_trip']) && intval($child['ultimate_trip']) == 1 ? 2: 1;
     $grade = 'Grade ' . $child['class_grade'];
@@ -317,7 +317,7 @@ function addToSheet($child, $khk = false, $trophy = false, $track = '') {
     $show_track = $track ?? $child['highest_track'];
     if ($khk || $trophy) {
         if ($khk) $show_track = 'khk';
-        return [$show_track, $name, $img_url, $grade, $school_name, $school_location, $school_logo, $award, $trip, '', '', '', '', '', '', ''];
+        return [$show_track, $name, $img_url, $grade, $school_name, $school_location, $school_logo, $award_num, $trip, '', '', '', '', '', '', ''];
     } else {
         // prizes
         $prize_amount = 0;
