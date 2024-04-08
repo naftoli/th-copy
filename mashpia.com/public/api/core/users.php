84ef0b362fa89f04c42c8454d5d105022d8e87f2
naftoli rapoport <naftolir@gmail.com>
2024-04-08 14:57:06 -0400

    fix missing family id

--- mashpia.com/public/api/core/users.php
+++ mashpia.com/public/api/core/users.php
@@ -54,7 +54,9 @@ class UsersRouter {
         $users = [];
         // fetch all results and parse them as models
         while( $row = $query->fetch() ){
-            $profilePicture = ( new Soldier(['mobile_pic' => $row['mobile_pic'], 'user_photo_id' => $row['user_photo_id']]) )->profilePicture();
+            $soldier = new Soldier( $row );
+            $profilePicture = $soldier->profilePicture();
+            $admin_id = $row['admin_id'] ?? $soldier->parentAccount()['admin_id'];
             $platoon = ( new Platoon(['class_grade' => $row['class_grade'], 'class_sub' => $row['class_sub']]) )->name();
             // format dates
             $dob = $row['dob']; $user_registered = $row['user_registered'];
