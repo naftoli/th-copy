aa3ea1560a80f727e452522d7c34c89a0e393f9c
naftoli rapoport <naftolir@gmail.com>
2024-05-10 13:23:23 -0400

    debug monthly raffles

--- mashpia.com/public/raffles/tasks/run_raffle/monthly_raffle.php
+++ mashpia.com/public/raffles/tasks/run_raffle/monthly_raffle.php
@@ -20,7 +20,7 @@ function monthly_raffle($raffle){
     while($prize = mysql_fetch_assoc($prize_query)) $prizes[] = $prize;
 
     echo "<pre>";
-    print_r(array_keys($schools));
+    print_r($schools);
     print_r($prizes);
     echo "</pre>";
     
