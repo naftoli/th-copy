151164347e0da6328534264f33463feb56e5eda5
naftoli rapoport <naftolir@gmail.com>
2024-05-15 17:53:51 -0400

    fix contact report to show highest earned rank

--- mashpia.com/public/lagBaomer/parent_contact_info.php
+++ mashpia.com/public/lagBaomer/parent_contact_info.php
@@ -9,12 +9,18 @@ $as = new AdminSchools( $admin_user['admin_id'], $admin_user['auth'] );
 $schools = $as->getSchools();
 
 $fields = ['Parent ID', 'Parent Name', 'Mother Name', 'Father Name', 'Phone Number', 'Mother Phone Number', 'Father Phone Number', 'Email', 'Child Serial Number', 'Child First Name', 'Child Last Name', 'Base Name', 'Rank'];
-$db_fields = ['admin_id', ['first', 'last'], 'mother', 'father', 'admin_phone_home', 'admin_phone_mobile2', 'admin_phone_mobile', 'admin_email', 'user_serial', 'child_first', 'child_last', 'school_name', 'rank_name'];
+$db_fields = ['admin_id', ['first', 'last'], 'mother', 'father', 'admin_phone_home', 'admin_phone_mobile2', 'admin_phone_mobile', 'admin_email', 'user_serial', 'child_first', 'child_last', 'school_name', 'rank_ord'];
 
+$ranks = [];
+$sql = "select rank_ord, rank_name from ranks";
+$result = mysql_query( $sql );
+while ( $row = mysql_fetch_assoc( $result ) ) {
+    $ranks[$row['rank_ord']] = $row['rank_name'];
+}
 
 $users = [];
 // create sql statement
-$sql = "select a.*, u.user_serial, u.first as child_first, u.last as child_last, s.school_name, r.rank_name, MAX( rm.rank_ord ) as rank_ord 
+$sql = "select a.*, u.user_serial, u.first as child_first, u.last as child_last, s.school_name, MAX( rm.rank_ord ) as rank_ord  
         from users u
         join schools s using ( school_id )
         join rank_marks rm using ( user_id ) 
@@ -56,7 +62,7 @@ while ( $row = mysql_fetch_assoc( $result ) ) {
             <?php foreach ( $users as $user ) : ?>
                 <tr>
                     <?php foreach ( $db_fields as $field ) : ?>
-                        <td><?= is_array( $field ) ? $user[$field[0]] . ' ' . $user[$field[1]] : $user[$field] ?></td>
+                        <td><?= is_array( $field ) ? $user[$field[0]] . ' ' . $user[$field[1]] : $field == 'rank_ord' ? $user[$ranks[$field]] : $user[$field] ?></td>
                     <?php endforeach; ?>
                 </tr>
             <?php endforeach; ?>
