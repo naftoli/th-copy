eb97625434d464f65f453732121a911561642ddf
naftoli rapoport <naftolir@gmail.com>
2024-05-16 12:25:13 -0400

    updates to rank report for lag baomer

--- mashpia.com/public/lagBaomer/rank_report.php
+++ mashpia.com/public/lagBaomer/rank_report.php
@@ -165,7 +165,6 @@ while ($row = mysql_fetch_assoc($result)) {
     $userName = $row['first'] . ' ' . $row['last'];
     $users[$row['school_name']][$row['gender']][$grade][$userName] = $row['rank_ord'];
 }
-echo "<pre>"; print_r($users); echo "</pre>"; exit;
 
 //display info
 $genderLookup = [
