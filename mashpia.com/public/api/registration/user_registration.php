a2005f4c54eb8925b9575a81587d292b9ea59e20
naftoli rapoport <naftolir@gmail.com>
2024-06-19 14:50:41 -0400

    more changes for email confirmation after enrollment

--- mashpia.com/public/api/registration/user_registration.php
+++ mashpia.com/public/api/registration/user_registration.php
@@ -678,7 +678,8 @@ Tzivos Hashem HQ</body></html>";
                     json_error('Your information has been saved but there was an error sending the confirmation email.');
                     @mail($bcc, $subject, $message, implode("\r\n", $headers));
                 }
-            } else {                $mailer = new PHPMailer(true);
+            } else {
+                $mailer = new PHPMailer();
                 try {
                     // server settings
                     $mailer->SMTPDebug = 0;
