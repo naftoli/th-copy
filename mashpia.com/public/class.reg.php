10b24fb989583e42234e5f19d2211301d9ca05ed
naftoli rapoport <naftolir@gmail.com>
2024-07-22 18:26:08 -0400

    work on yearly gift report

--- mashpia.com/public/class.reg.php
+++ mashpia.com/public/class.reg.php
@@ -6,41 +6,31 @@ class Reg
     private $user_id;
     private $registered_children;
     private $unregistered_children;
-    private $parents;
 
     public function __construct($school = 0, $grade = 0, $user = 0) {
         $this->school_id = $school;
         $this->class_id = $grade;
         $this->user_id = $user;
-        $this->registered_children = array();
-        $this->unregistered_children = array();
+        $this->registered_children = [];
     }
 
-    private function setChildren($registered = true) {
+    private function setChildren() {
         $sql = "select * from users u 
                 left join schools s using (school_id) 
-                left join classes c on u.class_id = c.class_id";
-        if ($registered) $sql .= " where u.user_registered > 0";
-        else $sql .= " where (u.user_registered = 0 or u.user_registered is null)";
-        if ($this->school_id) $sql .= " and u.school_id = $this->school_id";
-        if ($this->class_id) $sql .= " and u.class_id = $this->class_id";
-        if ($this->user_id) $sql .= " and u.user_id = $this->user_id";
+                left join classes c on u.class_id = c.class_id 
+                left join admin_auths aa on aa.id = u.user_id 
+                left join admins a using (admin_id) 
+                where (aa.auth = 'user' or aa.auth is null) 
+                and u.user_registered > 0";
+        if ($this->school_id > 0) $sql .= " and u.school_id = $this->school_id";
+        if ($this->class_id > 0) $sql .= " and u.class_id = $this->class_id";
+        if ($this->user_id > 0) $sql .= " and u.user_id = $this->user_id";
+
         $result = mysql_query($sql);
         while ($row = mysql_fetch_assoc($result)) {
-            if ($row['user_registered'] > 0) {
-                $this->registered_children[] = $row;
-            } else {
-                $this->unregistered_children[] = $row;
-            }
+            $this->registered_children[] = $row;
+            $this->parents[$row['user_id']] = $row;
         }
-    }
-
-    public function getParent($user_id) {
-        $sql = "select a.* from admins a 
-                left join users u on a.user_id = u.user_id 
-                where u.user_id = " .  $user_id;
-        $result = mysql_query($sql);
-        return mysql_fetch_assoc($result);
     }
 
     public function getRegisteredChildren() {
@@ -48,12 +38,5 @@ class Reg
             $this->setChildren();
         }
         return $this->registered_children;
-    }
-
-    public function getUnregisteredChildren() {
-        if (empty($this->unregistered_children)) {
-            $this->setChildren();
-        }
-        return $this->unregistered_children;
     }
 }
