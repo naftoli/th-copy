b9b7734234cfa7fda279661f08fe22be4a98867f
naftoli rapoport <naftolir@gmail.com>
2024-07-24 16:53:46 -0400

    rollback address autofill to base reg - only works with react version greater than what we have

--- base-commander/src/components/rows/AddressRow.jsx
+++ base-commander/src/components/rows/AddressRow.jsx
@@ -2,7 +2,6 @@ import React, { Component, Fragment } from 'react';
 import { Row, Col, Input } from 'reactstrap';
 import { PhoneNumber } from 'components/inputs';
 import memoize from 'memoize-one';
-import { AddressAutofill } from '@mapbox/search-js-react';
 
 export class AddressRow extends Component {
 
@@ -34,55 +33,51 @@ export class AddressRow extends Component {
 
         {!this.props.hideShipping &&
           <Fragment>
-            <AddressAutofill
-              accessToken='pk.eyJ1IjoibmFmdG9saXIiLCJhIjoiY2xsZTR5NHFuMDBzeTNzdWxqcnd1ZGJ0ZSJ9.7M6yPlYYDHRE-QcRUp_z9A'
-            >
-              <Col xs={12} xl={6}>
-                <label>Address 1</label>
-                <Input name="address-1" autocomplete="address-line1" id={getName('address1')} placeholder='792 Eastern Parkway'
-                       value={getValue('address1')} {...inputProps} maxLength={255}/>
-                {/* <div className='invalid-message'>Please enter a valid address</div> */}
-              </Col>
+            <Col xs={12} xl={6}>
+              <label>Address 1</label>
+              <Input name={getName('address1')} id={getName('address1')} placeholder='792 Eastern Parkway'
+                     value={getValue('address1')} {...inputProps} maxLength={255}/>
+              {/* <div className='invalid-message'>Please enter a valid address</div> */}
+            </Col>
 
-              <Col xs={12} xl={6}>
-                <label>Address 2</label>
-                <Input name="address-2" autocomplete="address-line2" id={getName('address2')}
-                       placeholder='5th Floor' value={getValue('address2')}
-                       {...inputProps} required={false} maxLength={255}/>
-              </Col>
+            <Col xs={12} xl={6}>
+              <label>Address 2</label>
+              <Input name={getName('address2')} id={getName('address2')}
+                     placeholder='5th Floor' value={getValue('address2')}
+                     {...inputProps} required={false} maxLength={255}/>
+            </Col>
 
-              <Col xs={6} xl={showPhone ? 6 : 4}>
-                <label>City</label>
-                <Input name="city" autocomplete="address-level2" id={getName('city')}
-                       value={getValue('city')} {...inputProps} placeholder='Brooklyn'
-                       pattern='^.{3,}$' title="3 or more letters" maxLength={255}/>
-                <div className='invalid-message'>Please enter 3 or more letters</div>
-              </Col>
+            <Col xs={6} xl={showPhone ? 6 : 4}>
+              <label>City</label>
+              <Input name={getName('city')} id={getName('city')}
+                     value={getValue('city')} {...inputProps} placeholder='Brooklyn'
+                     pattern='^.{3,}$' title="3 or more letters" maxLength={255}/>
+              <div className='invalid-message'>Please enter 3 or more letters</div>
+            </Col>
 
-              <Col xs={3} xl={showPhone ? 3 : 2}>
-                <label>State</label>
-                <Input name="state" autocomplete="address-level1" id={getName('state')}
-                       value={getValue('state')} {...inputProps} placeholder='NY'
-                       pattern='^[A-Za-z\s]{2,255}$' title="3 to 255 letters" maxLength={255}/>
-                <div className='invalid-message'>Please enter a valid state</div>
-              </Col>
+            <Col xs={3} xl={showPhone ? 3 : 2}>
+              <label>State</label>
+              <Input name={getName('state')} id={getName('state')}
+                     value={getValue('state')} {...inputProps} placeholder='NY'
+                     pattern='^[A-Za-z\s]{2,255}$' title="3 to 255 letters" maxLength={255}/>
+              <div className='invalid-message'>Please enter a valid state</div>
+            </Col>
 
-              <Col xs={3} xl={showPhone ? 3 : 2}>
-                <label>Zip</label>
-                <Input name="zip" autocomplete="postal-code" id={getName('postal')}
-                       value={getValue('postal')} {...inputProps} placeholder='11213'
-                       pattern='^.{3,255}$' title="3 to 255 letters" maxLength={255}/>
-                <div className='invalid-message'>Please enter 3 to 255 letters</div>
-              </Col>
+            <Col xs={3} xl={showPhone ? 3 : 2}>
+              <label>Zip</label>
+              <Input name={getName('postal')} id={getName('postal')}
+                     value={getValue('postal')} {...inputProps} placeholder='11213'
+                     pattern='^.{3,255}$' title="3 to 255 letters" maxLength={255}/>
+              <div className='invalid-message'>Please enter 3 to 255 letters</div>
+            </Col>
 
-              <Col xs={showPhone ? 6 : 12} xl={showPhone ? 6 : 4}>
-                <label>Country</label>
-                <Input name={getName('country')} id={getName('country')}
-                       value={getValue('country')} {...inputProps} placeholder='USA'
-                       pattern='^.{2,255}$' title="2 to 255 letters"/>
-                <div className='invalid-message'>Please enter 3 to 255 letters</div>
-              </Col>
-            </AddressAutofill>
+            <Col xs={showPhone ? 6 : 12} xl={showPhone ? 6 : 4}>
+              <label>Country</label>
+              <Input name={getName('country')} id={getName('country')}
+                     value={getValue('country')} {...inputProps} placeholder='USA'
+                     pattern='^.{2,255}$' title="2 to 255 letters"/>
+              <div className='invalid-message'>Please enter 3 to 255 letters</div>
+            </Col>
           </Fragment>
         }
 
