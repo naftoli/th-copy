de73a900fcbd50923f7184199fc2c51f38cd9cbf
naftoli rapoport <naftolir@gmail.com>
2024-07-24 23:18:53 -0400

    update global settings code including registration charges

--- mashpia.com/public/class.globalSettings.php
+++ mashpia.com/public/class.globalSettings.php
@@ -9,35 +9,17 @@ class GlobalSettings {
     }
 
     public static function getCurrentYear() {
-//        $sql = "select `val` from global_settings where `key` = 'current_year'";
-//        $result = mysql_query($sql);
-//        $row = mysql_fetch_assoc($result);
-//        return $row['val'];
         return self::getHelper('current_year');
     }
     
     public static function getCurYearDates() {
-//        $dates = array();
-//        $sql = "select `val` from global_settings where `key` = 'cur_year_start'";
-//        $result = mysql_query($sql);
-//        $row = mysql_fetch_assoc($result);
-//        $dates['start'] = $row['val'];
-//        $sql = "select `val` from global_settings where `key` = 'cur_year_end'";
-//        $result = mysql_query($sql);
-//        $row = mysql_fetch_assoc($result);
-//        $dates['end'] = $row['val'];
         $dates['start'] = self::getHelper('cur_year_start');
         $dates['end'] = self::getHelper('cur_year_end');
         return $dates;
     }
     
     public static function getRegistrationYear( $school_id = false ) {
-//        $sql = "select `val` from global_settings where `key` = 'registration_year'";
-//        $result = mysql_query($sql);
-//        $row = mysql_fetch_assoc($result);
-//        $year =  $row['val'];
         $year = self::getHelper('registration_year');
-
         if ( self::isAustralian( $school_id ) ) {
             // find out current month
             $month = date('m');
@@ -47,34 +29,18 @@ class GlobalSettings {
     }
     
     public static function getChidonYear() {
-//        $sql = "select `val` from global_settings where `key` = 'chidon_year'";
-//        $result = mysql_query($sql);
-//        $row = mysql_fetch_assoc($result);
-//        return $row['val'];
         return self::getHelper('chidon_year');
     }
 
     public static function getChidonRegYear() {
-//        $sql = "select `val` from global_settings where `key` = 'chidon_reg_year'";
-//        $result = mysql_query($sql);
-//        $row = mysql_fetch_assoc($result);
-//        return $row['val'];
         return self::getHelper('chidon_reg_year');
     }
     
     public static function getBirthdayYear() {
-//        $sql = "select `val` from global_settings where `key` = 'birthday_year'";
-//        $result = mysql_query($sql);
-//        $row = mysql_fetch_assoc($result);
-//        return $row['val'];
         return self::getHelper('birthday_year');
     }
 
     public static function getCharidyYear() {
-//        $sql = "select `val` from global_settings where `key` = 'charidy_year'";
-//        $result = mysql_query($sql);
-//        $row = mysql_fetch_assoc($result);
-//        return $row['val'];
         return self::getHelper('charidy_year');
     }
 
@@ -100,16 +66,22 @@ class GlobalSettings {
      * @param boolean $school
      * @return integer
      */
-    public static function getRegCost($type, bool $early_bird) {
-        $cost = 60;
+    public static function getRegCost(int $type, bool $early_bird, $myshliach = false) {
+        $cost = $myshliach ? 60 : 65;
         if ($early_bird) {
             switch ($type) {
                 case 1:
-                case 2:
                     $cost = 50;
                     break;
-                case 3:
+                case 2:
                     $cost = 55;
+                    break;
+                case 3:
+                    if ( $myshliach ) {
+                        $cost = 50;
+                    } else {
+                        $cost = 60;
+                    }
                     break;
             }
         }
@@ -120,7 +92,7 @@ class GlobalSettings {
      * have one source of truth for the date of expiry for early bird
      */
     public static function earlyBird() {
-        return new DateTime('2023-09-13 04:00:00');
+        return new DateTime('2024-09-24 04:00:00');
     }
 
     /**
@@ -138,7 +110,7 @@ class GlobalSettings {
     public static function calculateChildFee(
         $type,  $fee = null,    $is_soldier = false, 
         $early_bird = false,    $no_discount = false, 
-        $ckids = false
+        $ckids = false, $myshliach = false
     ) {
         // ckids has no fee
         if ( $ckids ) return 0;
@@ -150,23 +122,8 @@ class GlobalSettings {
             return intval($fee);
         }
 
-        $fee = self::getRegCost( $type, $early_bird );
-        return $fee > 0 ? $fee : 0;
-
-        // cast it to a float value
-//        $fee = intval( $fee );
-//        // return the final rate if requested
-//        if ( $no_discount )
-//            return $fee >= 0 ? $fee : 0;
-//        // add early bird discount ( not for type 1 )
-//        if ( $type != 1 && $early_bird && $is_soldier )
-//            $fee -= self::getEarlyBird();
-//        // add type 2 discount
-//        if ( $type == 2 && $early_bird ) {
-//            $fee -= self::getGuaranteedDiscount();
-//        }
-        // do not allow negative numbers
-//        return $fee >= 0 ? $fee : 0;
+        $fee = self::getRegCost( $type, $early_bird, $myshliach );
+        return max($fee, 0);
     }
 
     /**
@@ -181,7 +138,6 @@ class GlobalSettings {
         if ( in_array( $school_id, [ 269 ] ) ) {
             return 50;
         }
-//        $today = new DateTime();
         return 20;
     }
 
