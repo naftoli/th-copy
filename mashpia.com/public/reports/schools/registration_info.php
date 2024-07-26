51498b158e4b9d88464ec5996b90267b6875785e
naftoli rapoport <naftolir@gmail.com>
2024-07-26 19:20:14 -0400

    update registration_info.php to include test schools

--- mashpia.com/public/reports/schools/registration_info.php
+++ mashpia.com/public/reports/schools/registration_info.php
@@ -119,7 +119,7 @@ $schools = $as->getSchools();
             <thead>
             <tbody>
             <?php foreach( $schools as $school_id => $school_name ) {
-                $base = \School::find()->where(['school_id' => $school_id])->one();
+                $base = \School::find($school_id);
                 $year = GlobalSettings::getRegistrationYear( $base->school_id ); ?>
                 <tr class='base' data-school_id='<?= $base->school_id; ?>' data-year='<?= $year ?>'>
                     <td><?= $year ?></td>
