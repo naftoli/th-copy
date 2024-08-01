b41102aff74caf42b6813472af3057ba411755d4
naftoli rapoport <naftolir@gmail.com>
2024-08-01 14:26:38 -0400

    update choosing non th schools based on city/state/zip/country

--- mashpia.com/public/mobile/registration/api/getNonThSchools.php
+++ mashpia.com/public/mobile/registration/api/getNonThSchools.php
@@ -1,6 +1,6 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/db.php';
-$sql = "select * from non_th_schools order by school_name";
+$sql = "select * from non_th_schools where (city is not null or state is not null or zip is not null or country is not null) order by school_name";
 $result = mysql_query($sql);
 while ($row = mysql_fetch_assoc($result)) {
     $schools[$row['non_th_school_id']] = $row['school_name'] . ',' . $row['city'] . ',' . $row['state'] . ',' . $row['zip'] . ',' . $row['country'];
