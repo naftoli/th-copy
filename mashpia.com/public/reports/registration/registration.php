0339899e61b26a873a6f69c73a149bf3ec8366f0
naftoli rapoport <naftolir@gmail.com>
2024-08-01 17:39:24 -0400

    add note to page

--- mashpia.com/public/reports/registration/registration.php
+++ mashpia.com/public/reports/registration/registration.php
@@ -81,11 +81,19 @@ foreach ($temp as $row) {
             padding-left: 20px;
             padding-right: 20px;
         }
+        .info {
+          padding: 20px;
+          background-color: grey;
+          font-size: large;
+        }
     </style>
 </head>
 <body>
 <h1>Soldier Registration <?=$year?></h1>
 <h2>Base Totals</h2>
+<div class="info">
+  "Please Note: The balance may not be 100% accurate, since it’s based off the fee as it is today, and not when the child registered."
+</div>
 <table id="table" class="table table-striped table-condensed">
     <thead>
         <tr>
