3fbf5c62dba6cd7bf95f10f37d9d9d49ac12b3b0
naftoli rapoport <naftolir@gmail.com>
2024-08-01 22:24:50 -0400

    debug fix editing soldier in parent accounts

--- mashpia.com/public/mobile/reg/ajax/getUser.php
+++ mashpia.com/public/mobile/reg/ajax/getUser.php
@@ -8,13 +8,23 @@ $user = mysql_real_escape_string( $_POST['user'] );
 $admin_id = mysql_real_escape_string( $_COOKIE['admin'] );
 $admin_id = encrypt_decrypt('decrypt', $admin_id);
 
-$sql = "select * from users u 
-        left join th_chidon tc on tc.user_id = u.user_id 
-		left join thumbs t on t.file_id = u.user_photo_id 
-		join admin_auths aa on aa.id = u.user_id && aa.auth = 'user'
-		where u.user_id = " . $user ."
-		and aa.admin_id = " . $admin_id . "
-		and tc.year = " . $year;
+//$sql = "select * from users u
+//        left join th_chidon tc on tc.user_id = u.user_id
+//		left join thumbs t on t.file_id = u.user_photo_id
+//		join admin_auths aa on aa.id = u.user_id && aa.auth = 'user'
+//		where u.user_id = " . $user ."
+//		and aa.admin_id = " . $admin_id . "
+//		and tc.year = " . $year;
+$sql = "SELECT 
+            *
+        FROM
+            users u
+                LEFT JOIN
+            thumbs t ON t.file_id = u.user_photo_id 
+                LEFT JOIN 
+            admin_auths aa ON aa.id = u.user_id 
+        WHERE
+            u.user_id = " . $user . " AND aa.admin_id = " . $admin_id;
 $result = mysql_query( $sql );
 echo $sql;
 if ( mysql_num_rows($result) > 0 ) {
