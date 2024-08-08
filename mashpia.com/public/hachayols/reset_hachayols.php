c05b40e890e85eac35afbcfd70ec05f5c48f8274
naftoli rapoport <naftolir@gmail.com>
2024-08-07 21:40:23 -0400

    rsetting hacahyols

--- mashpia.com/public/hachayols/reset_hachayols.php
+++ mashpia.com/public/hachayols/reset_hachayols.php
@@ -34,17 +34,17 @@ foreach ($info as $row) {
 
 // set hachayol to first child in family that is lower than 6th grade unless there is no child in the family that is lower than 6th grade
 $i = 1;
-foreach ($admins as $admin_id => $admin) {
+foreach ($admins as $admin_id => $users) {
     $hachayol = null;
-    foreach ($admin as $user_id => $class_grade) {
-        if ($class_grade < 6 || !is_numeric($class_grade)) {
+    foreach ($users as $user_id => $class_grade) {
+        if (intval($class_grade) < 6 || !is_numeric($class_grade)) {
             $hachayol = $user_id;
             break;
         }
     }
     if ($hachayol === null) {
         // get first child in family
-        $hachayol = key(reset((array_keys($admin))));
+        $hachayol = first((array_keys($users)));
     }
     $sql = "UPDATE users SET hachayol = 1 WHERE user_id = " . $hachayol;
     echo $i++ . ": " . $sql . "<br />";
