60517f6f59920c66d288e8552fe2f3633d503f7b
naftoli rapoport <naftolir@gmail.com>
2024-08-09 12:19:47 -0400

    chidon experience registration

--- mashpia.com/public/chidonTests/class.chidonTests.php
+++ mashpia.com/public/chidonTests/class.chidonTests.php
@@ -24,7 +24,7 @@ class ChidonTests
     public function __construct($yr = 0) {
         global $MASHPIA_DB;
         $this->db = $MASHPIA_DB;
-        $this->year = $yr > 0 ? $yr : GlobalSettings::getChidonRegYear();
+        $this->year = $yr > 0 ? $yr : GlobalSettings::getChidonYear();
         $this->children = [];
         $this->scores = [];
         $this->marks = [];
