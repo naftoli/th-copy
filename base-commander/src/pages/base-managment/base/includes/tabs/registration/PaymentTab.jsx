972882de9c363dcde710b10fda44d28e992a00bc
naftoli rapoport <naftolir@gmail.com>
2024-08-12 14:52:49 -0400

    changes for text and process for siddur gift

--- base-commander/src/pages/base-managment/base/includes/tabs/registration/PaymentTab.jsx
+++ base-commander/src/pages/base-managment/base/includes/tabs/registration/PaymentTab.jsx
@@ -50,9 +50,9 @@ export class PaymentTab extends React.Component {
     // make sure that if the yearly gift is checked, we alert the user
     if ( this.state.terms.siddur_gift ) {
       const msg = "I realize I have committed to learning Peirush Hamilos this year, " +
-        "thanks to the Siddurim sponsored by Rabbi Moshe and Ruti Weiss.\nIf you are not committed, please click 'cancel' and uncheck the box.";
-      const commited = confirm(msg);
-      if (commited) this.props.onGiftChange(1);
+        "thanks to the Siddurim sponsored by Rabbi Moshe and Ruti Weiss.\nIf you are not committed, please click 'cancel'.";
+      if (window.confirm(msg)) this.props.onGiftChange(1);
+      else this.props.onGiftChange(0);
       // this.props.register()
     } else {
       this.props.onGiftChange(0);
@@ -124,6 +124,10 @@ export class PaymentTab extends React.Component {
           <Checkbox checked={ terms.siddur_gift } name='siddur_gift' { ...checkboxProps }>
             <b>Special for 5785:</b> I would like to receive a Siddur for each of my Chayolim and Teachers in my school
             sponsored by Rabbi Moshe and Ruti Weiss, and we are committed to learning Peirush Hamilos in my school.
+          </Checkbox>
+
+          <Checkbox checked={ !terms.siddur_gift }>
+            No, my base can't commit to learning Peirush Hamilos this year, despite the Siddurim from Rabbi Moshe and Ruti Weiss.
           </Checkbox>
         </Callout>
         
