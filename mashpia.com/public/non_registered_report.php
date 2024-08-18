91c4f3257d2f082474a4e25311fa1a27bc6b32f0
naftoli rapoport <naftolir@gmail.com>
2024-08-18 11:58:27 -0400

    updates for registrration reports

--- mashpia.com/public/non_registered_report.php
+++ mashpia.com/public/non_registered_report.php
@@ -1,151 +1,154 @@
 <?
-$admin_auth = array('school'); 
+$admin_auth = array('school');
 require('header.php');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN""http://www.w3.org/TR/html4/strict.dtd">
 <HTML>
 
-    <HEAD>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>Not Yet Registered Report</title>
-        <link href="admin_styles.css" rel="stylesheet" type="text/css">
-        <style type='text/css'>
-            table {
-                font-size: 12px;
-            }
-            th, td {
-                padding: 3px 10px;
-                vertical-align: top;
-            }
-            .page-break {
-                page-break-after: always;
-            }
-        </style>
-    </HEAD>
+<HEAD>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <title>Not Yet Registered Report</title>
+  <link href="admin_styles.css" rel="stylesheet" type="text/css">
+  <style type='text/css'>
+    table {
+      font-size: 12px;
+    }
 
-    <BODY>
-        <? include('admin_header.php'); ?>
-        <h1>Not Yet Registered Report</h1>
-        <a class="button" id='export_csv' style='display: none;'>Export To CSV</a>
-        <? 
-        require_once 'class.adminSchools.php';
-        require_once 'class.schoolsUsers.php';         
-       
-        $as = new AdminSchools( $admin_user['admin_id'], $admin_user['auth'] );
-        $schools = $as->getSchools();
-        $schoolsUsers = array();
-        $totals = array();
-        
-        foreach ( $schools as $id => $school ) {
-            $s = new SchoolsUsers( $id );
-            $schoolsUsers[$id] = $s->getUsers(false, true);
-        }
-        
-        /*
-        echo "<pre>";
-        print_r( $schoolsUsers );
-        echo "</pre>";
-         * 
-         */
-        
-        foreach ( $schoolsUsers as $school => $users ) {
-            echo "<h2>" . $schools[$school] . "</h2>";
-            echo "<table data-school_name='".$schools[$school]."' class='exported'>";
-            echo "<tr><th>Grade</th><th>Student</th><th>Serial #</th><th>Start Date</th>
+    th, td {
+      padding: 3px 10px;
+      vertical-align: top;
+    }
+
+    .page-break {
+      page-break-after: always;
+    }
+  </style>
+</HEAD>
+
+<BODY>
+<? include('admin_header.php'); ?>
+<h1>Not Yet Registered Report</h1>
+<a class="button" id='export_csv' style='display: none;'>Export To CSV</a>
+<?
+require_once 'class.adminSchools.php';
+require_once 'class.schoolsUsers.php';
+
+$as = new AdminSchools($admin_user['admin_id'], $admin_user['auth']);
+$schools = $as->getSchools();
+$schoolsUsers = array();
+$totals = array();
+
+foreach ($schools as $id => $school) {
+    $s = new SchoolsUsers($id);
+    $schoolsUsers[$id] = $s->getUsers(false, true);
+}
+
+/*
+echo "<pre>";
+print_r( $schoolsUsers );
+echo "</pre>";
+ *
+ */
+
+foreach ($schoolsUsers as $school => $users) {
+    echo "<h2>" . $schools[$school] . "</h2>";
+    echo "<table data-school_name='" . $schools[$school] . "' class='exported'>";
+    echo "<tr><th>Grade</th><th>Student</th><th>Serial #</th><th>Start Date</th>
             	<th>DOB</th><th>Email</th><th>Phone Numbers</th></tr>";
-            foreach ( $users as $user ) {
-            	//get phone numbers, email from parent account
-            	$qry = "select admin_email, admin_phone_home, admin_phone_work, admin_phone_mobile 
+    foreach ($users as $user) {
+        //get phone numbers, email from parent account
+        $qry = "select admin_email, admin_phone_home, admin_phone_work, admin_phone_mobile 
             			from admins join admin_auths aa using (admin_id) 
             			where aa.id = " . $user['user_id'] . " 
             			and aa.auth = 'user'";
-				$res = mysql_query( $qry );
-				if ( mysql_num_rows( $res ) > 0 ) {
-					$admin = mysql_fetch_assoc( $res );
-				} else {
-					$admin = null;
-				}
-								
-                $grade = $user['class_grade'] . ( empty( $user['class_sub']) ? '' : "-" . $user['class_sub'] );
-                echo "<tr><td>" . $grade . "</td><td>" . $user['first'] . " " . $user['last'] . 
-                    "</td><td>" . $user['user_serial'] . "</td><td>" . jdtogregorian( $user['user_start_date'] ) . 
-					"</td><td>" . $user['dob'] . "</td><td>";
-				if ( is_null( $admin ) ) {
-                    echo "</td><td>" . $user['email'];
-                } else {
-                	if ( !empty( $admin['email'] ) ) {
-                		echo $admin['admin_email'];
-                	} else {
-                		echo $user['email'];
-                	}
-                	echo "</td><td>";
-					if ( !empty( $admin['admin_phone_home'] ) ) {
-						echo $admin['admin_phone_home'] . "; <br />";
-					}
-					if ( !empty( $admin['admin_phone_work'] ) ) {
-						echo $admin['admin_phone_work'] . "; <br />";
-					}
-					if ( !empty( $admin['admin_phone_mobile'] ) ) {
-						echo $admin['admin_phone_mobile'];
-					}
-                }     
-                echo "</td></tr>"; 
-                if ( isset( $totals[$schools[$school]][$grade] ) ) 
-                    $totals[$schools[$school]][$grade]++;
-                else 
-                    $totals[$schools[$school]][$grade] = 1; 
-            }
-            echo "</table><br /><div class='page-break'></div>";
+        $res = mysql_query($qry);
+        if (mysql_num_rows($res) > 0) {
+            $admin = mysql_fetch_assoc($res);
+        } else {
+            $admin = null;
         }
-        
-        echo "<div class='page-break'></div>";
-        echo "<h2>Totals</h2>";
-        foreach ( $totals as $school => $info ) {
-        	$grandTotal = 0; 
-            echo "Total number of children not yet registered in " . $school . "<br />";
-            echo "<table>";
-            echo "<tr><th>Grade</th><th>Total</th></tr>";
-            foreach ( $info as $grade => $total ) {
-                echo "<tr><td>" . $grade . "</td><td>" . $total . "</td></tr>";
-                $grandTotal += $total;
+
+        $grade = $user['class_grade'] . (empty($user['class_sub']) ? '' : "-" . $user['class_sub']);
+        echo "<tr><td>" . $grade . "</td><td>" . $user['first'] . " " . $user['last'] .
+            "</td><td>" . $user['user_serial'] . "</td><td>" . jdtogregorian($user['user_start_date']) .
+            "</td><td>" . $user['dob'] . "</td><td>";
+        if (is_null($admin)) {
+            echo "</td><td>" . $user['email'];
+        } else {
+            if (!empty($admin['email'])) {
+                echo $admin['admin_email'];
+            } else {
+                echo $user['email'];
             }
-			echo "<tr><td><b>Grand Total</b></td><td><b>" . $grandTotal . "</b></td></tr>";
-            echo "</table>";
-            echo "<br /><br />";
-            echo "<div class='page-break'></div>";
+            echo "</td><td>";
+            if (!empty($admin['admin_phone_home'])) {
+                echo $admin['admin_phone_home'] . "; <br />";
+            }
+            if (!empty($admin['admin_phone_work'])) {
+                echo $admin['admin_phone_work'] . "; <br />";
+            }
+            if (!empty($admin['admin_phone_mobile'])) {
+                echo $admin['admin_phone_mobile'];
+            }
         }
-        ?>
-        <script>
-            $(document).ready( function() {
-                $('#export_csv').show().click( export_csv );
-            })
-            function export_csv() {
-                alert("Please note that is feature is in BETA and may not work correctly in all browsers/readers. "
-                    +"Please note that it does not work with IE or Microsoft Edge, "
-                    +"and has been only been tested with Microsoft Excel 2016."
-                );
-                var csvContent = "Grade,Student,Serial #,Start Date,DOB,Email,Phone Numbers,Base\n"; // the baisc csv file
-                var universalBOM = "\uFEFF";
-                
-                $.each( $('table.exported'), function( index, table ) {
-                    var school_name = table.dataset.school_name;
-                    table = $(table);
-                    $.each(table.find("tr:not(:first-child)"), function(index, tr) {
-                        var row = [];
-                        $.each($(tr).find("td"), function(index, td) {
-                            row.push($(td).text());
-                        });
-                        row.push( school_name );
-                        csvContent += row.join(',') + '\n';
-                    });
-                });
-                
-                var hiddenElement = document.createElement('a');
-                hiddenElement.href = "data:text/csv;charset=utf-8," + encodeURIComponent(universalBOM+csvContent); // set the data
-                hiddenElement.target = '_blank'; // in a new tab
-                hiddenElement.download = 'non_registered.csv'; // with this file_name
-                hiddenElement.click(); // and click it
-            }
-        </script>
-    </body>
+        echo "</td></tr>";
+        if (isset($totals[$schools[$school]][$grade]))
+            $totals[$schools[$school]][$grade]++;
+        else
+            $totals[$schools[$school]][$grade] = 1;
+    }
+    echo "</table><br /><div class='page-break'></div>";
+}
+
+echo "<div class='page-break'></div>";
+echo "<h2>Totals</h2>";
+foreach ($totals as $school => $info) {
+    $grandTotal = 0;
+    echo "Total number of children not yet registered in " . $school . "<br />";
+    echo "<table>";
+    echo "<tr><th>Grade</th><th>Total</th></tr>";
+    foreach ($info as $grade => $total) {
+        echo "<tr><td>" . $grade . "</td><td>" . $total . "</td></tr>";
+        $grandTotal += $total;
+    }
+    echo "<tr><td><b>Grand Total</b></td><td><b>" . $grandTotal . "</b></td></tr>";
+    echo "</table>";
+    echo "<br /><br />";
+    echo "<div class='page-break'></div>";
+}
+?>
+<script>
+  $(document).ready(function () {
+    $('#export_csv').show().click(export_csv);
+  })
+
+  function export_csv() {
+    alert("Please note that is feature is in BETA and may not work correctly in all browsers/readers. "
+      + "Please note that it does not work with IE or Microsoft Edge, "
+      + "and has been only been tested with Microsoft Excel 2016."
+    );
+    var csvContent = "Grade,Student,Serial #,Start Date,DOB,Email,Phone Numbers,Base\n"; // the baisc csv file
+    var universalBOM = "\uFEFF";
+
+    $.each($('table.exported'), function (index, table) {
+      var school_name = table.dataset.school_name;
+      table = $(table);
+      $.each(table.find("tr:not(:first-child)"), function (index, tr) {
+        var row = [];
+        $.each($(tr).find("td"), function (index, td) {
+          row.push($(td).text());
+        });
+        row.push(school_name);
+        csvContent += row.join(',') + '\n';
+      });
+    });
+
+    var hiddenElement = document.createElement('a');
+    hiddenElement.href = "data:text/csv;charset=utf-8," + encodeURIComponent(universalBOM + csvContent); // set the data
+    hiddenElement.target = '_blank'; // in a new tab
+    hiddenElement.download = 'non_registered.csv'; // with this file_name
+    hiddenElement.click(); // and click it
+  }
+</script>
+</body>
 </html>
--- mashpia.com/public/registered_report.php
+++ mashpia.com/public/registered_report.php
@@ -1,106 +1,94 @@
 <?
-$admin_auth = array('school'); 
+$admin_auth = array('school');
 require('header.php');
 
-if( isset($_GET['debug'])){
-	//error_reporting(E_ALL);
+if (isset($_GET['debug'])) {
+    //error_reporting(E_ALL);
     ini_set("display_errors", 1);
 }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN""http://www.w3.org/TR/html4/strict.dtd">
 <HTML>
 
-    <HEAD>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>Registered Report</title>
-        <link href="admin_styles.css" rel="stylesheet" type="text/css">
-        <style type='text/css'>
-            table {
-                font-size: 12px;
-            }
-            th, td {
-                padding: 3px 10px;
-            }
-            .page-break {
-                page-break-after: always;
-            }
-        </style>
-    </HEAD>
+<HEAD>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <title>Registered Report</title>
+  <link href="admin_styles.css" rel="stylesheet" type="text/css">
+  <style type='text/css'>
+    table {
+      font-size: 12px;
+    }
 
-    <BODY>
-        <? include('admin_header.php'); ?>
-        <h1>Registered Report</h1>
-        <? 
-        require_once 'class.adminSchools.php';  
-        require_once 'class.globalSettings.php';
-             
-        $as = new AdminSchools( $admin_user['admin_id'], $admin_user['auth'] );
-        $schools = $as->getSchools();
+    th, td {
+      padding: 3px 10px;
+    }
 
-        $year = globalSettings::getRegistrationYear();
-        $schoolsUsers = array();
-        $totals = array();
-        //ksort($schools);
-        foreach ( $schools as $id => $school ) {
-        	//echo $id . '-' . $school . "<br />";
-            // get users based on registration charges table 
-            $users = [];
-            $sql = "select u.*, c.* from users u 
-                    join classes c on u.class_id = c.class_id  
-                    join user_registration ur using (user_id) 
-                    where u.user_registered > 0 
-                    and u.school_id = " . $id . " 
-                    and ur.year = " . $year . " 
-                    group by u.user_id 
-                    order by class_grade, class_sub, last, first";
-            $result = mysql_query( $sql );
-            while ( $row = mysql_fetch_assoc( $result ) ) {
-                $users[] = $row;
-            }
-			if (!empty($users)) $schoolsUsers[$id] = $users;
-        }
-        
-        /*
-        echo "<pre>";
-        print_r( $schoolsUsers );
-        echo "</pre>";
-         * 
-         */
-        
-        foreach ( $schoolsUsers as $school => $users ) {
-            echo "<h2>" . $schools[$school] . "</h2>";
-            echo "<table>";
-            echo "<tr><th>Grade</th><th>Student</th><th>User ID</th><th>Start Date</th><th>Registered this year</th></tr>";
-            foreach ( $users as $user ) {
-                //if ( $user['user_registered'] < '2019-09-10' ) continue;
-                $grade = $user['class_grade'] . (empty( $user['class_sub'] ) ? '' : '-' . $user['class_sub']);
-                echo "<tr><td>" . $grade . "</td><td>" . $user['first'] . " " . $user['last'] . 
-                    "</td><td>" . $user['user_id'] . "</td><td>" . jdtogregorian( $user['user_start_date'] ) . 
-                    "</td><td>" . $user['user_registered'] . "</td></tr>"; 
-                if ( isset( $totals[$schools[$school]][$grade] ) ) 
-                    $totals[$schools[$school]][$grade]++;
-                else 
-                    $totals[$schools[$school]][$grade] = 1; 
-            }
-            echo "</table><br /><div class='page-break'></div>";
-        }
-        
-        echo "<div class='page-break'></div>";
-        echo "<h2>Totals</h2>";
-        foreach ( $totals as $school => $info ) {
-        	$grandTotal = 0; 
-            echo "Total number of children registered in " . $school . "<br />";
-            echo "<table>";
-            echo "<tr><th>Grade</th><th>Total</th></tr>";
-            foreach ( $info as $grade => $total ) {
-                echo "<tr><td>" . $grade . "</td><td>" . $total . "</td></tr>";
-                $grandTotal += $total;
-            }
-			echo "<tr><td><b>Grand Total</b></td><td><b>" . $grandTotal . "</b></td></tr>";
-            echo "</table>";
-            echo "<br /><br />";
-            echo "<div class='page-break'></div>";
-        }
-        ?>
-    </body>
+    .page-break {
+      page-break-after: always;
+    }
+  </style>
+</HEAD>
+
+<BODY>
+<? include('admin_header.php'); ?>
+<h1>Registered Report</h1>
+<?
+require_once 'class.adminSchools.php';
+require_once 'class.schoolsUsers.php';
+require_once 'class.globalSettings.php';
+
+$as = new AdminSchools($admin_user['admin_id'], $admin_user['auth']);
+$schools = $as->getSchools();
+
+$year = globalSettings::getRegistrationYear();
+$schoolsUsers = array();
+$totals = array();
+//ksort($schools);
+foreach ($schools as $id => $school) {
+    $s = new SchoolsUsers($id);
+    $schoolsUsers[$id] = $s->getUsers(true, true);
+}
+
+/*
+echo "<pre>";
+print_r( $schoolsUsers );
+echo "</pre>";
+ *
+ */
+
+foreach ($schoolsUsers as $school => $users) {
+    echo "<h2>" . $schools[$school] . "</h2>";
+    echo "<table>";
+    echo "<tr><th>Grade</th><th>Student</th><th>User ID</th><th>Start Date</th><th>Registered this year</th></tr>";
+    foreach ($users as $user) {
+        $grade = $user['class_grade'] . (empty($user['class_sub']) ? '' : '-' . $user['class_sub']);
+        echo "<tr><td>" . $grade . "</td><td>" . $user['first'] . " " . $user['last'] .
+            "</td><td>" . $user['user_id'] . "</td><td>" . jdtogregorian($user['user_start_date']) .
+            "</td><td>" . $user['user_registered'] . "</td></tr>";
+        if (isset($totals[$schools[$school]][$grade]))
+            $totals[$schools[$school]][$grade]++;
+        else
+            $totals[$schools[$school]][$grade] = 1;
+    }
+    echo "</table><br /><div class='page-break'></div>";
+}
+
+echo "<div class='page-break'></div>";
+echo "<h2>Totals</h2>";
+foreach ($totals as $school => $info) {
+    $grandTotal = 0;
+    echo "Total number of children registered in " . $school . "<br />";
+    echo "<table>";
+    echo "<tr><th>Grade</th><th>Total</th></tr>";
+    foreach ($info as $grade => $total) {
+        echo "<tr><td>" . $grade . "</td><td>" . $total . "</td></tr>";
+        $grandTotal += $total;
+    }
+    echo "<tr><td><b>Grand Total</b></td><td><b>" . $grandTotal . "</b></td></tr>";
+    echo "</table>";
+    echo "<br /><br />";
+    echo "<div class='page-break'></div>";
+}
+?>
+</body>
 </html>
