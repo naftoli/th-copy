c24b0e3afe3db06b4d96ad014198e2c36c8bcbab
naftoli rapoport <naftolir@gmail.com>
2024-08-26 16:06:40 -0400

    add whatsapp group links

--- mashpia.com/public/api/registration/user_registration.php
+++ mashpia.com/public/api/registration/user_registration.php
@@ -290,6 +290,7 @@ class UserRegistrationRouter {
                                         'discount'  => $discount,
                                         'trans_id'  => $trans_id,
                                         'year'      => $cthYr,
+                                        'grade'     => intval($user->platoon->class_grade),
                                     ];
 //                                    if ($user->school_id == 269) {
 //                                        // send email to anash kinder about child's registration
@@ -325,6 +326,7 @@ class UserRegistrationRouter {
                                         'recruited_by' => $recruited_by,
                                         'trans_id'  => $trans_id,
                                         'year'      => $chidonYr,
+                                        'grade'     => $user->platoon->class_grade,
                                     ];
                                 }
 
@@ -354,7 +356,8 @@ class UserRegistrationRouter {
                                         'recruited' => $recruited,
                                         'recruited_by' => $recruited_by,
                                         'trans_id'  => $trans_id,
-                                        'year'      => $chidonYr
+                                        'year'      => $chidonYr,
+                                        'grade'     => $user->platoon->class_grade,
                                     ];
                                 }
 
@@ -436,7 +439,7 @@ class UserRegistrationRouter {
                             'code'      => $code,
                             'amount'    => $amount,
                             'trans_id'  => $trans_id,
-                            'year'      => $yr
+                            'year'      => $yr,
                         ];
                     }
                 } else {
@@ -497,6 +500,15 @@ class UserRegistrationRouter {
         // get recruited by name
         $stmtRecruit = $MASHPIA_DB->prepare("select first, last from users where user_id = ? or user_serial = ?");
 
+        $links = [
+            '4' => 'https://chat.whatsapp.com/KAQkzbl7mVyFmKpX5p7ZSa',
+            '5' => 'https://chat.whatsapp.com/KYpcr9XUPdK4ovSQvO1Y8p',
+            '6' => 'https://chat.whatsapp.com/ECVmf06Wh8lIQmbhwcMYGG',
+            '7' => 'https://chat.whatsapp.com/KEVRJDkb1udJQdGCi89ckS',
+            '8' => 'https://chat.whatsapp.com/CKCF6j1gGg32WmIlLtc5YB',
+            'khk' => 'https://chat.whatsapp.com/KgGr8WvK5ex1kKHH01IhIz'
+        ];
+
         $tracks = [
             'maven'     => 'Yesod',
             'pro'       => 'Yediah',
@@ -504,6 +516,8 @@ class UserRegistrationRouter {
             'genius'    => 'Iyun'
         ];
 
+        $grade = '';
+        $khk = false;
         $cth_names = [];
         $chidon_names = [];
 
@@ -514,6 +528,10 @@ class UserRegistrationRouter {
                 }
                 if (strpos($detail['code'], 'LDE') !== false) {
                     $chidon_names[] = $detail['first'];
+                    $grade = isset($detail['grade']) ? (string)$detail['grade'] : '';
+                }
+                if ($detail['code'] == 'KHKE') {
+                    $khk = true;
                 }
             }
         }
@@ -647,6 +665,16 @@ class UserRegistrationRouter {
         }
         $message .= "<p>Total Charged: $" . $total_amount . "</p>";
         $message .= "<p>Transaction ID: " . $trans_id . "</p>";
+
+        // link for whatsapps
+        if (!empty($grade) || $khk) {
+            $message .= "<p>Join your child's WhatsApp group to stay up to date with all the latest information: ";
+            if ($khk) {
+                $message .= "<a href='" . $links['khk'] . "'>WhatsApp Group</a></p>";
+            } else {
+                $message .= "<a href='" . $links[$grade] . "'>WhatsApp Group</a></p>";
+            }
+        }
 
         // footer
         $message .= "<p><b>Customer Service</b><br />
