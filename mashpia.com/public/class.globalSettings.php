e7de1ad6c5270cbadddc6a3bce8380d3b2813d78
naftoli rapoport <naftolir@gmail.com>
2024-08-28 15:33:02 -0400

    change date or early bird

--- mashpia.com/public/class.globalSettings.php
+++ mashpia.com/public/class.globalSettings.php
@@ -84,7 +84,7 @@ class GlobalSettings {
      * have one source of truth for the date of expiry for early bird
      */
     public static function earlyBird() {
-        return new DateTime('2024-09-24 04:00:00');
+        return new DateTime('2024-09-26 04:00:00');
     }
 
     /**
