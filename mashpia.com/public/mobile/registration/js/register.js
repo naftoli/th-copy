4ff0da734a6ca9e4baeaaa54bd1dbb5c5ca05491
naftoli rapoport <naftolir@gmail.com>
2024-08-28 17:43:12 -0400

    updates for pre registration

--- mashpia.com/public/mobile/registration/js/register.js
+++ mashpia.com/public/mobile/registration/js/register.js
@@ -1554,7 +1554,7 @@ var registrationApp = function() {
             amount -= personalized_amount
 
             // add to cart
-            const index = state.users.findIndex(user => user.user_id === registering[0].meta.user_id)
+            const index = state.users.findIndex(user => user.user_id === selected_user.user_id)
             const family_id = state.users[index].parentAccount.admin_id
             const trackCode = 'RRFAM-'
             state.cart.push({
