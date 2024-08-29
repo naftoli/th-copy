8e9ee636aed04406cf79971aed55816d031cac94
naftoli rapoport <naftolir@gmail.com>
2024-08-29 18:05:38 -0400

    test sm function

--- mashpia.com/public/testSM.php
+++ mashpia.com/public/testSM.php
@@ -1,5 +1,5 @@
 <?php
 require 'db.php';
 require 'class.globalSettings.php';
-$sm = calculateSM( GlobalSettings::getRegistrationYear() );
+$sm = calculateSM( GlobalSettings::getCurrentYear() );
 echo "<pre>"; print_r($sm); echo "</pre>";
