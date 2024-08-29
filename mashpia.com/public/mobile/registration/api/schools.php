66627478bfeb184b6030e77c2ad7f0aaca1c0e28
naftoli rapoport <naftolir@gmail.com>
2024-08-29 12:13:34 -0400

    debug list of schools for on parent accounts

--- mashpia.com/public/mobile/registration/api/schools.php
+++ mashpia.com/public/mobile/registration/api/schools.php
@@ -10,10 +10,11 @@ if ( $_SERVER['REQUEST_METHOD'] == "GET" ) {
        ." AND school_id NOT IN (61, 612, 269) " // remove 'unassigned school' and 'myshliach' and 'anash kinder'
        ." ORDER BY school_name;" // alphabetically
     );
+    json_response($schools_query);
 
-    $schools = fetch_results_assoc( $schools_query );
-    if ( $schools )
-        json_response( $schools );
-    else
-        render_json_error( "Could not load schools" );
+//    $schools = fetch_results_assoc( $schools_query );
+//    if ( $schools )
+//        json_response( $schools );
+//    else
+//        render_json_error( "Could not load schools" );
 }
