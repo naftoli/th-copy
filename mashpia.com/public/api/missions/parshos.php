abddbba61d48a713c69d29fa4d6dcb9f0a965737
naftoli rapoport <naftolir@gmail.com>
2024-09-02 17:02:04 -0400

    redo file for parshos for bc site

--- mashpia.com/public/api/missions/parshos.php
+++ mashpia.com/public/api/missions/parshos.php
@@ -4,14 +4,15 @@ include_once( __DIR__ . "/../header/header.php" );
 class ParshaRouter {
 
     public function index() {
+        global $MASHPIA_DB;
         $dates = GlobalSettings::getCurYearDates();
-        $parshos = Parsha::all([
-            'order' => 'start',
-            'conditions' => [
-                'start >= ' . $dates['start'], 
-                'end <= ' . $dates['end'],
-            ]
+        $qry = "SELECT * FROM parshos WHERE start >= :start AND end <= :end";
+        $stmt = $MASHPIA_DB->prepare( $qry );
+        $stmt->execute([
+            ':start' => $dates['start'],
+            ':end' => $dates['end']
         ]);
+        $parshos = $stmt->fetchAll( PDO::FETCH_ASSOC );
         json_response( $parshos, true, true );
     }
 }
