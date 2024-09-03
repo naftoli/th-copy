0714dd8697c81e2a5424d59fbe2803c556330ba9
naftoli rapoport <naftolir@gmail.com>
2024-09-03 14:39:07 -0400

    debug non-th-schools

--- mashpia.com/public/mobile/registration/api/getNonThSchools.php
+++ mashpia.com/public/mobile/registration/api/getNonThSchools.php
@@ -4,10 +4,12 @@ $sql = "SELECT * FROM non_th_schools ORDER BY school_name";
 $result = mysql_query($sql);
 while ($row = mysql_fetch_assoc($result)) {
     $str = $row['school_name'];
-    if ($row['city']) $str .= ',' . $row['city'];
-    if ($row['state']) $str .= ',' . $row['state'];
-    if ($row['zip']) $str .= ',' . $row['zip'];
-    if ($row['country']) $str .= ',' . $row['country'];
-    $schools[$row['non_th_school_id']] = $str;
+    if (!empty($str)) {
+        if ($row['city']) $str .= ',' . $row['city'];
+        if ($row['state']) $str .= ',' . $row['state'];
+        if ($row['zip']) $str .= ',' . $row['zip'];
+        if ($row['country']) $str .= ',' . $row['country'];
+        $schools[$row['non_th_school_id']] = $str;
+    }
 }
 echo json_encode($schools);
