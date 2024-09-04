ef6e545ecee36e8701c4b489fd361cfd5f4d2bde
naftoli rapoport <naftolir@gmail.com>
2024-09-03 23:06:32 -0400

    work on non th schools issue

--- mashpia.com/public/mobile/registration/js/register.js
+++ mashpia.com/public/mobile/registration/js/register.js
@@ -2312,7 +2312,8 @@ var templates = function () {
       if (user.school.school_id === anash_kinder || user.school.school_id === myshliach) {
         $('#non_th_school_id').val(user.non_th_school_id);
         $('#non_th_school').val(user.non_th_school);
-        // alert(user.non_th_school_id, user.non_th_school)
+        alert(user.non_th_school)
+        alert(non_th_schools[user.non_th_school_id])
         if (non_th_schools[user.non_th_school_id]) {
           let school_info = non_th_schools[user.non_th_school_id].split(',')
           // alert(school_info)
