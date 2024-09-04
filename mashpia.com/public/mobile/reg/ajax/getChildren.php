1c96cd9c93bf9b4c620113f1894ab68b4b80ebcb
naftoli rapoport <naftolir@gmail.com>
2024-09-04 19:11:19 -0400

    turn off reg

--- mashpia.com/public/mobile/reg/ajax/getChildren.php
+++ mashpia.com/public/mobile/reg/ajax/getChildren.php
@@ -316,9 +316,9 @@ if ( !empty( $users ) ) {
         }
 
         // turn off chidon
-//        if (! isset($_COOKIE['naftoli'])) $children[$row['user_id']]['reg_types']['chidon'] = false;
+        if (! isset($_COOKIE['naftoli'])) $children[$row['user_id']]['reg_types']['chidon'] = false;
         // turn off chayolei
-//        if (! isset($_COOKIE['naftoli'])) $children[$row['user_id']]['reg_types']['chayolei'] = false;
+        if (! isset($_COOKIE['naftoli'])) $children[$row['user_id']]['reg_types']['chayolei'] = false;
 
         // for editing ultimate trip
 //        if (intval($row['ultimate_trip'])) {
