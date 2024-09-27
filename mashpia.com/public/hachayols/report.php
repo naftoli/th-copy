7f9ec5c80f357b7d159d10d04faacd2c1589b116
naftoli rapoport <naftolir@gmail.com>
2024-09-27 15:24:18 -0400

    fix up hachayol reports

--- mashpia.com/public/hachayols/report.php
+++ mashpia.com/public/hachayols/report.php
@@ -127,7 +127,7 @@ $stmtMissing = $MASHPIA_DB->prepare($sqlMissing);
     <script>
       window.addEventListener('DOMContentLoaded', (event) => {
         $(".toCheck").each( function () {
-          $(this).trigger('click')
+          $(this).attr('checked', 'checked')
         })
       })
       $(".hachayol").click( function () {
