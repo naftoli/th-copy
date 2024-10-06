eb2d56a64682c25fa5ec6097f3d6f915a7c67689
naftoli rapoport <naftolir@gmail.com>
2024-10-06 12:19:19 -0400

    changes to updating birthdays for all

---
+++ mashpia.com/public/updateBirthdays.php
@@ -0,0 +1,35 @@
+<?php
+ini_set('display_errors',1);
+require_once 'db.php';
+require_once 'class.heDob.php';
+require_once 'class.birthdayEn.php';
+require_once 'class.birthdayYi.php';
+require_once 'class.birthdayHe.php';
+require_once 'class.globalSettings.php';
+$year = GlobalSettings::getBirthdayYear();
+
+$users = [];
+$sql = "select user_id from users u 
+        join user_registration ur using (user_id) 
+        where user_registered > 0 
+        and ur.year = " . $year;
+$result = mysql_query($sql);
+while ( $row = mysql_fetch_assoc($result) ) {
+	$users[] = $row['user_id'];
+}
+
+$updated = 0;
+foreach ($users as $user_id) {
+	$h = new HeDob( $user_id, true );
+	$h->setHeDob();
+	// create birthday missions
+    $b = new BirthdayEn( $user_id );
+    $b->setBirthday();
+    $bi = new BirthdayYi( $user_id );
+    $bi->setBirthday();
+    $bh = new BirthdayHe( $user_id );
+    $bh->setBirthday();
+    $updated++;
+}
+echo "Updated $updated users";
+?>
