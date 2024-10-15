feab094ba4fd0524754dd93b1e2b034d2c78b341
naftoli rapoport <naftolir@gmail.com>
2024-10-15 15:55:26 -0400

    update fee amounts for chidon for editing

--- mashpia.com/public/mobile/registration/js/register.js
+++ mashpia.com/public/mobile/registration/js/register.js
@@ -2215,11 +2215,10 @@ var templates = function () {
     changeFee: function () {
       const targetDate = new Date('2024-09-26T03:00:00');
       const now = new Date();
-
       // Convert both dates to EST
       const targetEST = new Date(targetDate.toLocaleString('en-US', {timeZone: 'America/New_York'}));
       const nowEST = new Date(now.toLocaleString('en-US', {timeZone: 'America/New_York'}));
-
+      // Compare the dates
       return nowEST > targetEST;
     },
     setChidonReg: function (user) {
@@ -2234,10 +2233,10 @@ var templates = function () {
       let html = '<option value="0">Select Amount to Pay</option>'
       let fees = [20, 25, 30, 40]
       if (user.school.school_id == 61) fees = [30, 35, 40, 50]
-        // if (this.changeFee() && !Cookies.get('naftoli')) {
-        //   fees = [40]
-        //   if (user.school.school_id == 61) fees = [40, 50]
-      // }
+      if (this.changeFee() && !Cookies.get('naftoli')) {
+        fees = [40]
+        if (user.school.school_id == 61) fees = [40, 50]
+      }
       else if (user.school.school_id == 269) fees = [50, 55, 60, 70]
       for (let fee of fees) {
         html += `<option value="${fee}">$${fee}</option>`
