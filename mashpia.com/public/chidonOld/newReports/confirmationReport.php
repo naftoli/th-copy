736060760a4007290da01ad088c86f03e82f9bb4
naftoli rapoport <naftolir@gmail.com>
2024-10-31 14:14:42 -0400

    add ability for hq to update the confirmations

--- mashpia.com/public/chidonOld/newReports/confirmationReport.php
+++ mashpia.com/public/chidonOld/newReports/confirmationReport.php
@@ -37,41 +37,80 @@ foreach ($schools as $school_id => $school_name) {
     $stmt->execute([':year' => $year, ':school' => $school_id]);
     $info[$school_name] = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
+
+$superAdmin = $admin_user['auth'] == 'super';
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Confirmation Report</title>
-    <style>
-      table {
-        font-family: Arial, sans-serif;
-        font-size: 14px;
-      }
+  <title>Confirmation Report</title>
+  <style>
+    table {
+      font-family: Arial, sans-serif;
+      font-size: 14px;
+    }
 
-      tr, th, td {
-        border-bottom: #f0f0f0 1px solid;
-        padding: 10px;
-      }
-    </style>
+    tr, th, td {
+      border-bottom: #f0f0f0 1px solid;
+      padding: 10px;
+    }
+  </style>
 </head>
 <body>
-    <h1>Confirmation Report</h1>
-    <?php
-    foreach ($info as $school_name => $users) {
-        echo "<h2>$school_name</h2>";
-        echo "<table>";
-        echo "<tr><th>Grade/Class</th><th>Serial</th><th>Name</th><th>Confirmed Info</th></tr>";
-        foreach ($users as $user) {
-            $grade = $user['class_grade'] . ($user['class_sub'] ? " - " . $user['class_sub'] : "");
-            ?>
-            <tr>
-                <td><?= $grade ?></td>
-                <td><?= $user['user_serial'] ?></td>
-                <td><?= $user['first'] . " " . $user['last'] ?></td>
-                <td><?= $user['confirmed_info'] ? 'Yes': 'No' ?></td>
-            </tr>
-        <?php } ?>
-        </table>
+<h1>Confirmation Report</h1>
+<?php
+foreach ($info as $school_name => $users) {
+    echo "<h2>$school_name</h2>";
+    echo "<table>";
+    echo "<tr><th>Grade/Class</th><th>Serial</th><th>Name</th><th>Confirmed Info</th>";
+    if ($superAdmin) {
+        echo "<th></th>";
+    }
+    echo "</tr>";
+    foreach ($users as $user) {
+        $grade = $user['class_grade'] . ($user['class_sub'] ? " - " . $user['class_sub'] : "");
+        ?>
+      <tr>
+        <td><?= $grade ?></td>
+        <td><?= $user['user_serial'] ?></td>
+        <td><?= $user['first'] . " " . $user['last'] ?></td>
+        <td><?= $user['confirmed_info'] ? 'Yes' : 'No' ?></td>
+          <?php if ($superAdmin) : ?>
+            <td id="<?= $user['user_id'] ?>"><input type="checkbox"
+                                                    class="confirmed" <?= $user['confirmed_info'] ? 'checked' : '' ?> />
+            </td>
+          <?php endif; ?>
+      </tr>
     <?php } ?>
+  </table>
+<?php } ?>
 </body>
+<script>
+  // Only super admins can update confirmation status
+  <?php if ($superAdmin) : ?>
+  // Add event listeners to checkboxes on document load
+  document.addEventListener('DOMContentLoaded', function () {
+    const checkboxes = document.querySelectorAll('.confirmed')
+    checkboxes.forEach(checkbox => {
+      checkbox.addEventListener('change', async (e) => {
+        const user_id = e.target.parentElement.id
+        const confirmed = e.target.checked ? 1 : 0
+        const response = await fetch('updateConfirmation.php', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({user_id, confirmed})
+        });
+        const data = await response.json()
+        if (data.success) {
+          alert('Updated.')
+        } else {
+          alert('Error updating.')
+        }
+      })
+    })
+  })
+  <?php endif; ?>
+</script>
 </html>
---
+++ mashpia.com/public/chidonOld/newReports/updateConfirmation.php
@@ -0,0 +1,22 @@
+<?php
+$admin_auth = ['school'];
+require $_SERVER['DOCUMENT_ROOT'] . '/header.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/api/header/db.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/class.globalSettings.php';
+$year = GlobalSettings::getChidonYear();
+
+if ($admin_user['auth'] != 'super') {
+    die('Access denied');
+}
+
+$stmt = $MASHPIA_DB->prepare("
+    UPDATE th_chidon SET confirmed_info = :confirmed WHERE user_id = :user_id AND year = :year
+");
+
+// get POST info from fetch
+$info = json_decode(file_get_contents('php://input'), true);
+$user_id = $info['user_id'];
+$confirmed = $info['confirmed'];
+
+$res = $stmt->execute([':confirmed' => $confirmed, ':user_id' => $user_id, ':year' => $year]);
+echo json_encode(['success' => intval($res)]);
