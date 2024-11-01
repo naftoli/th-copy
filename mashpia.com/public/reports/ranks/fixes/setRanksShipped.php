d22d8541631a97606c98c2aac72989c9feb5b5e2
naftoli rapoport <naftolir@gmail.com>
2024-11-01 13:09:21 -0400

    create way to set rank medals as shipped

---
+++ mashpia.com/public/reports/ranks/fixes/setRanksShipped.php
@@ -0,0 +1,39 @@
+<?php
+// upload file that contains user id and rank ord and set as shipped
+$admin_auth = ['school'];
+require_once $_SERVER['DOCUMENT_ROOT'] . '/header.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/api/header/db.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/class.globalSettings.php';
+$year = GlobalSettings::getChidonRegYear();
+
+if ($admin_user['auth'] != 'super') {
+    die('Access denied');
+}
+
+$stmt = $MASHPIA_DB->prepare("INSERT IGNORE INTO rank_medals_shipped (user_id, rank_ord) VALUES (?, ?)");
+?>
+<!DOCTYPE html>
+<html>
+<body>
+    <form method="post" action="setAsShipped.php" enctype="multipart/form-data">
+        <input type="file" name="file" />
+        <input type="submit" value="Upload" />
+    </form>
+    <?php
+    if (isset($_FILES['file'])) {
+        $MASHPIA_DB->beginTransaction();
+        $file = $_FILES['file']['tmp_name'];
+        $handle = fopen($file, "r");
+        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+            $res = $stmt->execute([$data[0], $data[1]]);
+            if (!$res) {
+                $MASHPIA_DB->rollBack();
+                die('Failed to insert all records');
+            }
+        }
+        fclose($handle);
+        $MASHPIA_DB->commit();
+        echo 'Done';
+    }
+    ?>
+</body>
