3c8e5720efaf8a4a615dd12ff6c2a9a66082ac0c
naftoli rapoport <naftolir@gmail.com>
2024-11-08 13:45:40 -0500

    updates to myshliach labels

--- mashpia.com/public/class.myShliachShipLabels.php
+++ mashpia.com/public/class.myShliachShipLabels.php
@@ -1,5 +1,6 @@
-<?
+<?php
 require_once 'class.report.php';
+require_once 'class.globalSettings.php';
 
 class MyShliachShipLabels extends Report {
 	private $admins;
@@ -10,9 +11,10 @@ class MyShliachShipLabels extends Report {
 	private $ranks;
 	private $start;
 	private $end;
-	private $school; 
+	private $school;
+    private $year;
 	
-	public function __construct($previousStart = false, $id = null) {
+	public function __construct($previousStart = false, $id = 61) {
         parent::__construct($previousStart);
 		$this->start = $this->reportDates['start'];
         $this->end = $this->reportDates['end'];
@@ -22,15 +24,24 @@ class MyShliachShipLabels extends Report {
 		$this->userInfo = array();
 		$this->medals = array();
 		$this->ranks = array();
-		if ($id) {
-			$this->school = $id;
-		} else {
-			$this->school = 61;
-		}
+        $this->school = $id;
+        $this->year = GlobalSettings::getRegistrationYear($id);
 		$this->setMedals();
-		$this->setRanks();
-		$this->setParents();
+        $this->setRanks();
+        $this->setParents();
 	}
+
+    public function resetInfo() {
+        $this->admins = [];
+        $this->parents = [];
+        $this->users = [];
+        $this->userInfo = [];
+        $this->medals = [];
+        $this->ranks = [];
+        $this->setMedals();
+        $this->setRanks();
+        $this->setParents();
+    }
 	
 	private function setMedals() {
 		$sql = "SELECT s.subject_name, m.medal_name, u.user_id, u.last, u.first, mm.* 
@@ -38,11 +49,13 @@ class MyShliachShipLabels extends Report {
 				JOIN medals m USING ( medal_ord ) 
 				JOIN users u USING ( user_id ) 
 				JOIN subjects s USING ( subject_id ) 
+				JOIN user_registration ur using ( user_id ) 
 				WHERE u.user_registered > 0 
 				and mm.date_awarded >= " . $this->start . "  
 				AND mm.date_awarded <= " . $this->end . "  
 				and u.school_id = " . $this->school . " 
 				and u.medals_ranks = 1 
+				and ur.year = " .$this->year . " 
 				ORDER BY s.subject_id, mm.medal_ord";
 		$result = mysql_query($sql);
 		while ($row = mysql_fetch_assoc($result)) {
@@ -55,15 +68,26 @@ class MyShliachShipLabels extends Report {
 	}
 	
 	private function setRanks() {
-		$sql = "SELECT r.rank_name, u.user_id, u.user_serial, u.first, u.last, rm.*  
-				FROM rank_marks rm  
-				JOIN ranks r USING ( rank_ord ) 
-				JOIN users u USING ( user_id ) 
-				WHERE u.user_registered > 0 
-				and rm.date_promoted >= " . $this->start . "  
-				AND rm.date_promoted <= " . $this->end . "  
-				and u.school_id = " . $this->school . "   
-				ORDER BY rm.rank_ord";
+        // always get the highest rank and then see which books / ranks have not yet been shipped
+        $sql = "SELECT u.user_id, u.user_serial, u.first, u.last, MAX(rm.rank_ord) AS rank_ord
+            FROM rank_marks rm  
+            JOIN users u USING ( user_id ) 
+            JOIN user_registration ur using ( user_id ) 
+            WHERE u.user_registered > 0 
+            and u.school_id = " . $this->school . " 
+            and ur.year = " . $this->year . "    
+            GROUP BY u.user_id";
+//            $sql = "SELECT r.rank_name, u.user_id, u.user_serial, u.first, u.last, rm.*
+//                    FROM rank_marks rm
+//                    JOIN ranks r USING ( rank_ord )
+//                    JOIN users u USING ( user_id )
+//                    JOIN user_registration ur using ( user_id )
+//                    WHERE u.user_registered > 0
+//                    and rm.date_promoted >= " . $this->start . "
+//                    AND rm.date_promoted <= " . $this->end . "
+//                    and u.school_id = " . $this->school . "
+//                    and ur.year = " . $this->year . "
+//                    ORDER BY rm.rank_ord";
 		$result = mysql_query($sql);
 		while ($row = mysql_fetch_assoc($result)) {
 			$this->ranks[$row['user_id']][] = $row;
@@ -78,13 +102,52 @@ class MyShliachShipLabels extends Report {
 		$sql = "select * from admins a 
 				join admin_auths aa using (admin_id) 
 				where aa.id in (" . implode(',', $this->users) . ")  
-				and aa.auth = 'user'";
+				and aa.auth = 'user' 
+				order by aa.admin_id";
 		$result = mysql_query($sql);
 		while ($row = mysql_fetch_assoc($result)) {
 			$this->admins[$row['admin_id']] = $row;
 			$this->parents[$row['admin_id']][] = $row['id'];
 		}
 	}
+
+    public function getRankBooksShipped() {
+        $shipped = [];
+        $sql = "
+            SELECT 
+                *
+            FROM
+                rank_books_shipped rbs
+                    JOIN
+                users u USING (user_id)
+            WHERE
+                u.school_id = " . $this->school . " 
+        ";
+        $result = mysql_query($sql);
+        while ($row = mysql_fetch_assoc($result)) {
+            $shipped[$row['user_id']][] = $row['book_ord'];
+        }
+        return $shipped;
+    }
+
+    public function getRankMedalsShipped() {
+        $shipped = [];
+        $sql = "
+            SELECT 
+                *
+            FROM
+                rank_medals_shipped rms
+                    JOIN
+                users u USING (user_id)
+            WHERE
+                u.school_id = " . $this->school . " 
+        ";
+        $result = mysql_query($sql);
+        while ($row = mysql_fetch_assoc($result)) {
+            $shipped[$row['user_id']][] = $row['rank_ord'];
+        }
+        return $shipped;
+    }
 	
 	public function getParents() {
 		return $this->parents;
--- mashpia.com/public/myShliachShipLabels.php
+++ mashpia.com/public/myShliachShipLabels.php
@@ -1,186 +1,269 @@
 <?php
-$admin_auth = array('school'); 
-require('header.php'); 
+$admin_auth = array('school');
+require('header.php');
 
-$previous = false;
-if ( isset($_GET['go']) && $_GET['go'] == 'back' ) {
-    $previous = true; 
-    //$myshliach->setPreviousDates();
+// make sure it's hq
+if ($admin_user['auth'] != 'super') {
+    die('You are not authorized to view this page.');
+}
+
+function checkForBreak()
+{
+    global $cols, $rows;
+    if (($cols % 3) != 0) {
+        echo "<div class='space'></div>";
+    } else {
+        $cols = 0; //reset cols so that it will show new row
+        $rows++; //add row
+        if (($rows % 11) == 0) {
+            $rows = 1; //reset rows counter and add space to top of new page
+            echo "<div class='page-break'></div><div class='topSpace'></div>";
+        }
+    }
+    $cols++;
+}
+
+function getRankNames() {
+    $ranks = [];
+    $sql = "SELECT * FROM ranks";
+    $result = mysql_query($sql);
+    while ($row = mysql_fetch_assoc($result)) {
+        $ranks[$row['rank_ord']] = $row['rank_name'];
+    }
+    return $ranks;
 }
 
 require 'class.myShliachShipLabels.php';
-$myshliach = new MyShliachShipLabels($previous);
+$myshliach = new MyShliachShipLabels();
 
-//$myshliach->overrideDates( 2456960, 2457037 );
+$reportDates = $myshliach->getReportDates();
+$heDatesRanks = $myshliach->getHeReportDates();
+$rankNames = getRankNames();
 $parents = $myshliach->getParents();
 $medals = $myshliach->getMedals();
 $ranks = $myshliach->getRanks();
 $admins = $myshliach->getAdmins();
 $userInfo = $myshliach->getUserInfo();
 $heDates = $myshliach->getHeReportDates();
+$books_shipped = $myshliach->getRankBooksShipped();
+$medals_shipped = $myshliach->getRankMedalsShipped();
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="UTF-8" />
-		<link href="admin_styles.css" rel="stylesheet" type="text/css">
-		<style type="text/css">
-            .label {
-                width: 2.15in;
-                height: 1in;
-                font-size: 12px;
-                padding: 5px;
-                float: left;
-            }
-            .space {
-                width: .35in;
-                height: 1in;
-                float: left;
-                padding: 5px 20px;
-            }
-            .page-break {
-                clear: both;
-                page-break-after: always;
-            }
-            .medal {
-                width: 1in;
-                float: left;
-				font-size: 9px;
-            }
-            .name {
-                width: 2.15in;
-                font-size: 14px;
-            }
-            .topSpace {
-                height: 0.5in;
-                width: 7in;
-            }
-            .instructions {
-                width: 50%;
-            }
-            @media screen { 
-                #report_div {
-                    display: none;
-                }
-                .no-print {
-                    display: block;
-                }
-            }
-            @media print {
-                #report_div {
-                    display: block;
-                } 
-                .no-print {
-                    display: none;
-                }
-            }
-        </style>
-        <script type="text/javascript">
-            function check() {
-                if ( confirm( "Have you made sure to set your printer margins properly?\nIf not, please click 'cancel', set your margins, and then click 'print' again." ) ) 
-                    window.print();
-            }
-        </script>
-	</head>
+<head>
+  <meta charset="UTF-8"/>
+  <link href="admin_styles.css" rel="stylesheet" type="text/css">
+  <style>
+    .label {
+      width: 2.15in;
+      height: 1in;
+      font-size: 12px;
+      padding: 5px;
+      float: left;
+    }
 
-	<body>
-	<? include('admin_header.php'); ?>
-	<div class="no-print">
-        <h1>Medals Labels Report</h1>        
-            <div>
-                Current Report is calculated from <?=$heDates['start_he']?> up to <?=$heDates['end_he']?>.<br />
-                <? if ( $previous ) { ?> 
-                Click <a href='myShliachShipLabels.php'>here</a> to show next report dates.<br /><br />
-                <? } else { ?> 
-                Click <a href='myShliachShipLabels.php?go=back'>here</a> to show previous report dates.<br /><br />
-                <? } ?>
-            </div>
-            <div class='instructions'>
-                <b>Printing Instructions</b><br />
-                Please set your printer margins to the following:<br />
-                0.5 Top<br />
-                0.3 Left<br />
-                0.0 Right and Bottom<br /><br />   
-                <div align='center'>
-                    <input type='button' name='print' value='Print' onclick="check()" />    
-                </div>
-            </div>
-        </div>
-        
-        <div id="report_div" name="report_div">
-            <div class='topSpace'></div>
-            <?
-            $cols = 1; //counter for columns
-            $rows = 1; //counter for rows
-            
-			foreach ($parents as $admin => $children) {
-					
-				$parent = $admins[$admin];
-				$name = $parent['first'] . ' ' . $parent['last'];
-				$address = $parent['admin_address1'] . "<br />" . (empty($admin['admin_address2']) ? '' : 
-						$admin['admin_address2'] . "<br />") . $parent['admin_city'] . ', ' . $parent['admin_state'] . 
-					" " . $parent['admin_postal'] . "<br />" . $parent['admin_country'];
-				
-				echo "<div class='label'>";
-				echo "<span class='name'>";
-				echo "<b>" . $name . "</b><br />" . $address . "</span></div>";
-				checkForBreak();
-				
-				foreach ($children as $child) {
-					if (isset($medals[$child]) || isset($ranks[$child])) {
-											
-						if (isset($medals[$child])) {
-							$numMedals = 1;	
-							echo "<div class='label'>";
-							echo "<span class='name'>";
-							echo $userInfo[$child] . "</span><br />";
-							foreach ($medals[$child] as $medal) {
-								if ($numMedals > 8) {
-									echo "</div>";
-									checkForBreak();
-									echo "<div class='label'>";
-									echo "<span class='name'>";
-									echo $userInfo[$child] . "</span><br />";
-									$numMedals = 1;
-								}
-								if ($medal['subject_name'] == 'שבת מברכים תהילים') $medal['subject_name'] = 'תהילים';
-								echo "<span class='medal'>" . $medal['subject_name'] . "-" . $medal['medal_name'] . "</span>";
-								$numMedals++;
-							}
-							echo "</div>";	
-							checkForBreak();
-						}
-						
-						if (isset($ranks[$child])) {
-							foreach ($ranks[$child] as $rank) {
-								echo "<div class='label'>";
-								echo "<span class='name'>";
-								echo "Name : " . $userInfo[$child] . "<br />";
-								echo "Rank : " . $rank['rank_name'] . "<br />" . 
-									"Serial #: " . $rank['user_serial'];
-								echo "</span></div>";
-								checkForBreak();
-							}
-						}
-					}
-				}	
-			}
+    .space {
+      width: .35in;
+      height: 1in;
+      float: left;
+      padding: 5px 20px;
+    }
 
-			function checkForBreak() {
-				global $cols, $rows;
-				if (($cols % 3) != 0) {
-                	echo "<div class='space'></div>";
-                } else {
-                    $cols = 0; //reset cols so that it will show new row
-                    $rows++; //add row
-                    if ( ($rows % 11) == 0 ) {
-                        $rows = 1; //reset rows counter and add space to top of new page
-                        echo "<div class='page-break'></div><div class='topSpace'></div>"; 
+    .page-break {
+      clear: both;
+      page-break-after: always;
+    }
+
+    .medal {
+      width: 1in;
+      float: left;
+      font-size: 9px;
+    }
+
+    .name {
+      width: 2.15in;
+      font-size: 14px;
+    }
+
+    .topSpace {
+      height: 0.5in;
+      width: 7in;
+    }
+
+    .instructions {
+      width: 50%;
+    }
+
+    @media screen {
+      #report_div {
+        display: none;
+      }
+
+      .no-print {
+        display: block;
+      }
+    }
+
+    @media print {
+      #report_div {
+        display: block;
+      }
+
+      .no-print {
+        display: none;
+      }
+    }
+
+    select, button, input[type="button"], input[type="submit"] {
+      padding: 5px 10px;
+      font-size: 16px;
+      cursor: pointer;
+    }
+  </style>
+  <script type="text/javascript">
+    function check() {
+      if (confirm("Have you made sure to set your printer margins properly?\nIf not, please click 'cancel', set your margins, and then click 'print' again."))
+        window.print();
+    }
+  </script>
+</head>
+
+<body>
+<? include('admin_header.php'); ?>
+<div class="no-print">
+  <h1>Medals Labels Report</h1>
+  <div>
+    Current Report is calculated from <?= $heDatesRanks['start_he'] ?> up to <?= $heDatesRanks['end_he'] ?>.<br/>
+    <form action="" method="post">
+      <p>
+          <?= $myshliach->getHtmlSelect(3); ?>
+        <input type="submit" name="submit" value="Modify Report"/>
+      </p>
+    </form>
+  </div>
+  <div class='instructions'>
+    <b>Printing Instructions</b><br/>
+    Please set your printer margins to the following:<br/>
+    0.5 Top<br/>
+    0.3 Left<br/>
+    0.0 Right and Bottom<br/><br/>
+    <div align='center'>
+      <input type='button' name='print' value='Print' onclick="check()"/>
+    </div>
+  </div>
+</div>
+
+<div id="report_div" name="report_div">
+  <div class='topSpace'></div>
+    <?php
+    $cols = 1; //counter for columns
+    $rows = 1; //counter for rows
+
+    foreach ($parents as $admin => $children) {
+        $parent = $admins[$admin];
+        $name = $parent['first'] . ' ' . $parent['last'];
+        $address = $parent['admin_address1'] . "<br />" . (empty($admin['admin_address2']) ? '' :
+                $admin['admin_address2'] . "<br />") . $parent['admin_city'] . ', ' . $parent['admin_state'] .
+            " " . $parent['admin_postal'] . "<br />" . $parent['admin_country'];
+
+        echo "<div class='label'>";
+        echo "<span class='name'>";
+        echo "<b>" . $name . "</b><br />" . $address . "</span></div>";
+        checkForBreak();
+
+        foreach ($children as $child) {
+            if (isset($medals[$child]) || isset($ranks[$child])) {
+
+                if (isset($medals[$child])) {
+                    $numMedals = 1;
+                    echo "<div class='label'>";
+                    echo "<span class='name'>";
+                    echo $userInfo[$child] . "</span><br />";
+                    foreach ($medals[$child] as $medal) {
+                        if ($numMedals > 8) {
+                            echo "</div>";
+                            checkForBreak();
+                            echo "<div class='label'>";
+                            echo "<span class='name'>";
+                            echo $userInfo[$child] . "</span><br />";
+                            $numMedals = 1;
+                        }
+                        if ($medal['subject_name'] == 'שבת מברכים תהילים') $medal['subject_name'] = 'תהילים';
+                        echo "<span class='medal'>" . $medal['subject_name'] . "-" . $medal['medal_name'] . "</span>";
+                        $numMedals++;
+                    }
+                    echo "</div>";
+                    checkForBreak();
+                }
+
+                if (isset($ranks[$child])) {
+                    // add medal ranks and books that were not yet shipped
+                    // get highest rank
+                    $rank = $ranks[$child][count($ranks[$child]) - 1];
+                    $rank_ord = $rank['rank_ord'];
+
+                    // get book number
+                    if ($rank_ord > 11) {
+                        $book = 3;
+                    } else if ($rank_ord > 8) {
+                        $book = 2;
+                    } else {
+                        $book = 1;
+                    }
+
+                    // check if book was already sent
+                    if (isset($books_shipped[$child]) && in_array($book, $books_shipped[$child])) {
+                        continue;
+                    }
+
+                    echo "<div class='label'>";
+                    echo "<span class='name'>";
+                    echo "Name : " . $userInfo[$child] . "<br />";
+                    echo "Rank : " . $rankNames[$rank_ord] . "<br />" .
+                         "Rank Book #: " . $book;
+                    echo "</span></div>";
+                    checkForBreak();
+
+                    // add rank medals that were not yet shipped
+                    $ship = false;
+                    for ($idx = 1; $idx <= $rank_ord; $idx++) {
+                        if (!isset($medals_shipped[$child]) || (isset($medals_shipped[$child]) && !in_array($idx, $medals_shipped[$child]))) {
+                            $ship = true;
+                            break;
+                        }
+                    }
+
+                    if ($ship) {
+                        echo "<div class='label'>";
+                        echo "<span class='name'>";
+                        echo "Name : " . $userInfo[$child] . "<br />";
+                        echo "Rank Medals:<br />";
+                        $j = 1; // flag for when to make new label
+                        for ($idx = 1; $idx <= $rank_ord; $idx++) {
+                            if ($j > 12) {
+                                echo "</span></div>";
+                                checkForBreak();
+                                echo "<div class='label'>";
+                                echo "<span class='name'>";
+                                echo "Name : " . $userInfo[$child] . "<br />";
+                                echo "Rank Medals:<br />";
+                                $j = 1;
+                            }
+                            // check if rank was shipped
+                            if (isset($medals_shipped[$child]) && in_array($idx, $medals_shipped[$child])) {
+                                continue;
+                            }
+                            echo "<span class='medal'>" . $rankNames[$idx] . "</span>";
+                            $j++;
+                        }
+                        echo "</span></div>";
+                        checkForBreak();
                     }
                 }
-                $cols++;
-			}
-			?>
-        </div>
-	</body>
+            }
+        }
+    }
+    ?>
+</div>
+</body>
 </html>
