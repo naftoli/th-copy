1fe48917b9d3bfdc47ab7092620d421a966896d7
Naftoli Rapoport <naftolir@gmail.com>
2024-11-19 17:12:16 -0500

    fix up to enable 2 characters for hebrew names

--- base-commander/src/pages/base-managment/soldiers/components/NameRow.jsx
+++ base-commander/src/pages/base-managment/soldiers/components/NameRow.jsx
@@ -26,13 +26,13 @@ export const NameRow = ( { inst, soldier, onChange } ) => {
           <Col sm='6' dir='rtl'>
             <label htmlFor='first_he'>שם פרטי (First Name)</label>
             <Input id='first_he' value={ first_he } { ...inputProps }
-              pattern="^[\u0590-\u05FF\s\-'&quot;]{3,60}$" title="3-60 Hebrew letters"/>
+              pattern="^[\u0590-\u05FF\s\-'&quot;]{2,60}$" title="2-60 Hebrew letters"/>
             <div className='invalid-message'>3-60 <em>Hebrew</em> letters</div>
           </Col>
           <Col sm='6' dir='rtl'>
             <label htmlFor='last_he'>שם משפחה (Last Name)</label>
             <Input id='last_he' value={ last_he } { ...inputProps }
-              pattern="^[\u0590-\u05FF\s\-'&quot;]{3,60}$" title="3-60 Hebrew letters"/>
+              pattern="^[\u0590-\u05FF\s\-'&quot;]{2,60}$" title="2-60 Hebrew letters"/>
             <div className='invalid-message'>3-60 <em>Hebrew</em> letters</div>
           </Col>
         </Fragment>
