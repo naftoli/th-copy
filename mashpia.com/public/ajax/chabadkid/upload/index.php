d21e982c46bf13f2781c90fd713e35bed06def90
Naftoli Rapoport <naftolir@gmail.com>
2024-12-02 13:20:54 -0500

    debug teleshevsky getting info from mashpia

--- mashpia.com/public/ajax/chabadkid/upload/index.php
+++ mashpia.com/public/ajax/chabadkid/upload/index.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+$admin_auth = ['school'];
 // authenticate
 if (isset($_POST['auth']) && $_POST['auth'] === 'JTaMd105nT' && isset($_POST['school'])) {
     require $_SERVER['DOCUMENT_ROOT'] . '/api/header/header.php';
