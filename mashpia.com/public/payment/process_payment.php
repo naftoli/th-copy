977aa21bb0f91ebc4495a92ca77e2c078694a275
Naftoli Rapoport <naftolir@gmail.com>
2024-12-03 19:59:44 -0500

    fix up payment processing page

--- mashpia.com/public/payment/process_payment.php
+++ mashpia.com/public/payment/process_payment.php
@@ -1,9 +1,9 @@
 <?php
-echo "<pre>";
-print_r( $_POST );
-echo "</pre>";
-ini_set('display_errors',1);
-ini_set('error_reporting', E_ALL);
+//echo "<pre>";
+//print_r( $_POST );
+//echo "</pre>";
+//ini_set('display_errors',1);
+//ini_set('error_reporting', E_ALL);
 
 //$ip = $_SERVER['SERVER_ADDR'];
 //if ($ip == '39.53.201.236') {
