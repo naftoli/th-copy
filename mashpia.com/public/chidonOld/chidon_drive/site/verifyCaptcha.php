e43a9b400ffaa808ff8a00c5a9899338d1bc5684
Naftoli Rapoport <naftolir@gmail.com>
2024-12-09 12:33:54 -0500

    work on capthca

---
+++ mashpia.com/public/chidonOld/chidon_drive/site/verifyCaptcha.php
@@ -0,0 +1,18 @@
+<?php
+// check captcha
+$privatekey = '6LcPSR0UAAAAAMMBnZpu9a4Ru5sNmrfgeEYVmWPw';
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, [
+    'secret'   => $privatekey,
+    'response' => $_POST['g-recaptcha-response'],
+    'remoteip' => $_SERVER['REMOTE_ADDR']
+]);
+
+$resp = json_decode(curl_exec($ch));
+curl_close($ch);
+
+echo $resp->success ? 1 : 0;
