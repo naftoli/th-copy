b7769a9127c2aa0cf805ba4240e811ec2c420d13
Naftoli Rapoport <naftolir@gmail.com>
2024-12-10 18:58:38 -0500

    debug future report

--- mashpia.com/public/medals/get_future_report.php
+++ mashpia.com/public/medals/get_future_report.php
@@ -156,11 +156,11 @@ function getEligibleMedals($user_id) {
 
     $numMedals = 0;
     $future_missions = futureMissions($user_id);
-//    if ($user_id == 19274) {
-//        echo "<pre>";
-//        print_r($future_missions);
-//        echo "</pre>";
-//    }
+    if ($user_id == 19274) {
+        echo "<pre>";
+        print_r($future_missions);
+        echo "</pre>";
+    }
     // find out how many more medals can be earned by certain date by subject
     foreach ($user_subjects[$user_id] as $subject) {
         $future = $future_missions[$subject] ?? 0;
