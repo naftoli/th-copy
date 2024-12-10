80edef1214f76d3254f5707853d20d37dd2d3673
Naftoli Rapoport <naftolir@gmail.com>
2024-12-10 17:35:38 -0500

    working on fixing up future medals report

--- mashpia.com/public/school_possible_medals2.php
+++ mashpia.com/public/school_possible_medals2.php
@@ -53,7 +53,7 @@ $end_date = gri('end_date', unixtojd()+30);
                             end_date: $("#end_date").val(),
                         },
                         success: async function(res) {
-                            const medals = JSON.parse(res.response);
+                            const medals = JSON.parse(res);
                             const users = await get_users();
                             console.log(users, medals);
                             // create html
