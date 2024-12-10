3d7651bc9253aa0768f37f23078a5848959ce447
Naftoli Rapoport <naftolir@gmail.com>
2024-12-10 15:41:41 -0500

    working on fixing up future medals report

--- mashpia.com/public/medals/get_future_report.php
+++ mashpia.com/public/medals/get_future_report.php
@@ -61,26 +61,36 @@ function getMissionsDone($school_id) {
     return $missions_by_subject;
 }
 
-function getSubjects($user_id) {
+function getSubjects($school_id) {
     global $MASHPIA_DB;
 
     $subjects = [];
-    $sql = "select subject_id from user_tracks where enrolled = 1";
-    $result = $MASHPIA_DB->query($sql);
+    $sql = "
+    SELECT 
+        user_id, subject_id
+    FROM
+        user_tracks ut
+            JOIN
+        users u USING (user_id)
+    WHERE
+        u.school_id = :school AND ut.enrolled = 1
+    ORDER BY user_id";
+    $result = $MASHPIA_DB->prepare($sql);
     $rows = $result->fetchAll(PDO::FETCH_ASSOC);
     foreach ($rows as $row) {
-        $subjects[] = $row['subject_id'];
+        $subjects[$row['user_id']][] = $row['subject_id'];
     }
 
     return $subjects;
 }
 
-function futureMissions($subject_id, $user_id, $end_date) {
+function futureMissions($school_id, $subject_id, $end_date) {
     global $MASHPIA_DB;
 
+    $missions = [];
     $sql = "
         SELECT 
-            COUNT(*) as mission_count
+            user_id, COUNT(*) AS num_missions
         FROM
             date_tasks_missions dtm
                 JOIN
@@ -88,35 +98,37 @@ function futureMissions($subject_id, $user_id, $end_date) {
                 JOIN
             users u USING (user_id)
         WHERE
-            dtm.subject_id = :subject 
+            dtm.subject_id = :subject
                 AND dtm.end_date >= :today
                 AND dtm.end_date <= :end_date
                 AND u.school_type_id = dtm.school_type_id
                 AND ut.track_id = dtm.track_id
                 AND ut.level = dtm.level
-                AND u.user_id = :user";
+                AND u.lang_id = dtm.lang_id
+                AND u.school_id = :school
+        GROUP BY user_id";
 
     $stmt = $MASHPIA_DB->prepare($sql);
     $stmt->execute([
         ':subject'  => $subject_id,
         ':today'    => unixtojd(),
-        ':user'     => $user_id,
-        ':end_date' => $end_date
+        ':end_date' => $end_date,
+        ':school'   => $school_id
     ]);
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    return intval($row['mission_count']);
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($rows as $row) {
+        $missions[$row['user_id']][$row['subject_id']] = intval($row['num_missions']);
+    }
 }
 
 function getEligibleMedals($user_id) {
-    global $end_date, $ms, $missions_done;
+    global $end_date, $missions_done, $subjects, $ms;
 
-    // get subjects that user is enrolled into
-    $subjects = getSubjects($user_id);
+    $user_subjects = $subjects[$user_id];
 
     // find out how many more medals can be earned by certain date by subject
     $numMedals = 0;
-    foreach ($subjects as $subject) {
+    foreach ($user_subjects as $subject) {
         $future = futureMissions($subject, $user_id, $end_date);
         $current = $missions_done[$user_id][$subject] ?? 0;
         $total = $current + $future;
@@ -140,6 +152,7 @@ $end_date = $_REQUEST['end_date'];
 // get all registered users in this school
 $users = getUsers($school_id);
 $missions_done = getMissionsDone($school_id);
+$subjects = getSubjects($school_id);
 
 require_once 'class.medalsSubjects.php';
 $ms = new MedalsSubjects();
