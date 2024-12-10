bac963ca6176c2ca7b0e37c5a06fa600f95ae151
Naftoli Rapoport <naftolir@gmail.com>
2024-12-10 13:58:37 -0500

    fixing up future medals report

--- mashpia.com/public/school_possible_medals2.php
+++ mashpia.com/public/school_possible_medals2.php
@@ -162,7 +162,7 @@ $end_date = gri('end_date', unixtojd()+30);
 
                 <BR>
 
-                <button onclick="get_report(2);"><?=T_('Get Report')?></button><br /><br />
+                <button onclick="get_future_report();"><?=T_('Get Report')?></button><br /><br />
 
             </p>
 
