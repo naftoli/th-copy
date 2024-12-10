076d8fc850f5a35b43b238b0427014a48b158476
Naftoli Rapoport <naftolir@gmail.com>
2024-12-10 17:44:53 -0500

    working on fixing up future medals report

--- mashpia.com/public/school_possible_medals2.php
+++ mashpia.com/public/school_possible_medals2.php
@@ -46,12 +46,12 @@ $end_date = gri('end_date', unixtojd()+30);
             <script>
                 async function get_users() {
                     const users = await $.post('/medals/get_users.php', { school_id: $("#school_id").val() })
-                    return await users.json()
+                    return JSON.parse(users)
                 }
 
                 async function get_medals() {
                     const medals = await $.post('/medals/get_future_report.php', { school_id: $("#school_id").val(), end_date: $("#end_date").val() })
-                    return await medals.json()
+                    return JSON.parse(medals)
                 }
 
                 async function get_future_report() {
