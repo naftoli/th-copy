1da93241dc7171e0ca28729117e52b006d43ae64
naftoli rapoport <naftolir@gmail.com>
2024-12-23 16:57:06 -0500

    create medal report for hq

--- mashpia.com/public/medals/fix_shipped_issue.php
+++ mashpia.com/public/medals/fix_shipped_issue.php
@@ -84,5 +84,6 @@ if ( isset( $_POST['submit'] ) ) {
     <form action="fix_shipped_issue.php" method="post" enctype="multipart/form-data">
         <input type="file" name="file" id="file">
         <input type="submit" value="Upload" name="submit">
+    </form>
 </body>
 </html>
---
+++ mashpia.com/public/medals/medal_report.php
@@ -0,0 +1,84 @@
+<?php
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+
+$admin_auth = ['school'];
+require_once '../header.php';
+require_once '../api/header/db.php';
+require_once '../class.globalSettings.php';
+$year = GlobalSettings::getCurrentYear();
+
+// make sure only super auth can view
+if ( $admin_user['auth'] != 'super' ) {
+    echo 'You are not authorized to view this page.';
+    exit;
+}
+
+$sql = "
+    SELECT 
+        u.user_serial,
+        s.subject_id,
+        s.subject_name,
+        COUNT(mm.medal_ord) AS medals
+    FROM
+        medal_marks mm
+            JOIN
+        medals m USING (medal_ord)
+            JOIN
+        users u USING (user_id)
+            JOIN
+        subjects s USING (subject_id)
+            JOIN
+        user_registration ur USING (user_id)
+    WHERE
+        u.user_registered > 0 AND ur.year = :yee
+    GROUP BY user_id , subject_id
+    ORDER BY user_serial , subject_id , medal_ord";
+$stmt = $pdo->prepare($sql);
+$stmt->execute(['year' => $year]);
+$medals = $stmt->fetchAll();
+?>
+<DOCTYPE html>
+<html>
+<head>
+    <link href="admin_styles.css" rel="stylesheet" type="text/css">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Medals Report</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+        th, td {
+            border: 1px solid black;
+            padding: 5px;
+            text-align: center;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+</head>
+<body>
+    <?php include('admin_header.php'); ?>
+    <h1>Medals Report</h1>
+    <table>
+        <tr>
+            <th>User Serial</th>
+            <th>Subject</th>
+            <th>Medals</th>
+        </tr>
+        <?php
+        $total = 0;
+        foreach ($medals as $medal) {
+            $serial = $medal['user_serial'];
+            $subject = $medal['subject_name'];
+            $num_medals = $medal['medals'];
+            $total += $num_medals;
+            echo "<tr><td>" . $serial . "</td><td>" . $subject . "</td><td>" . $num_medals . "</td></tr>";
+        }
+        echo "<tr><th colspan='2'>Total</th><th>" . $total . "</th></tr>";
+        ?>
+    </table>
+</body>
+</html>
