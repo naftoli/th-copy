4ab307773838ecc790d7221554856b92ff295976
naftoli rapoport <naftolir@gmail.com>
2024-12-23 16:26:51 -0500

    fix up teleshevsky marking

--- mashpia.com/public/ajax/chabadkid/chidon/marks/index.php
+++ mashpia.com/public/ajax/chabadkid/chidon/marks/index.php
@@ -38,7 +38,7 @@ function checkTrackAndSchool($user_id, $level) {
 $test_num = intval($_POST['test_num']);
 $closing_dates = ChidonTests::getClosingDates();
 $today = new DateTime();
-if ($today > $closing_dates[$test_num - 1]) { // closing date of tests
+if ($today > $closing_dates[$test_num]) { // closing date of tests
     echo json_encode([
         'success' => false,
         'message' => 'The deadline has passed.'
