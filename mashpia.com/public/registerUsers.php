7214834587b1ff9c64b28306ea9a2500051f68a7
naftoli rapoport <naftolir@gmail.com>
2025-01-15 11:18:49 -0500

    fix massive mistake in qry for manually registering users

--- mashpia.com/public/registerUsers.php
+++ mashpia.com/public/registerUsers.php
@@ -10,7 +10,7 @@ $users = [59457,59458];
 foreach ($users as $user_id) {
     // register users and create private rank
     $sql = "update users set user_registered = now(), user_start_date = " . unixtojd() . " where user_id = " . $user_id;
-    $sql2 = "update registration_charges set school_id = 269, type = 'chayolei', amount = 0, date = now(), year = 5779, user_id = " . $user_id;
+    $sql2 = "update registration_charges set school_id = 269, type = 'chayolei', amount = 0, date = now(), year = 5779 where user_id = " . $user_id;
     $sql3 = "insert into rank_marks set rank_ord = 1, user_id = " . $user_id . ", date_promoted = " . unixtojd();
     mysql_query( $sql );
     mysql_query( $sql2 );
