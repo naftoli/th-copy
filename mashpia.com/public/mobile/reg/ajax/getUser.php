6e014f2108fbaf1444595d0cdf950f2743ea96d1
naftoli rapoport <naftolir@gmail.com>
2025-02-16 12:52:30 -0500

    Update Chidon registration and photo display logic
    - Modify parent_detail.html to conditionally show Chidon photo update button
    - Update getUser.php to simplify SQL query for Chidon registration
    - Adjust registration visibility based on specific cookie condition

--- mashpia.com/public/mobile/reg/ajax/getUser.php
+++ mashpia.com/public/mobile/reg/ajax/getUser.php
@@ -8,17 +8,12 @@ $user = mysql_real_escape_string( $_POST['user'] );
 $admin_id = mysql_real_escape_string( $_COOKIE['admin'] );
 $admin_id = encrypt_decrypt('decrypt', $admin_id);
 
-//$sql = "select * from users u
-//        left join th_chidon tc on tc.user_id = u.user_id
-//		left join thumbs t on t.file_id = u.user_photo_id
-//		join admin_auths aa on aa.id = u.user_id && aa.auth = 'user'
-//		where u.user_id = " . $user ."
-//		and aa.admin_id = " . $admin_id . "
-//		and tc.year = " . $year;
 $sql = "SELECT 
-            *
+            *  
         FROM
-            users u
+            th_chidon tc 
+                JOIN
+            users u ON u.user_id = tc.user_id 
                 LEFT JOIN
             thumbs t ON t.file_id = u.user_photo_id 
                 LEFT JOIN 
