c3d80d81294271c244be9324dcd6e13b036b8205
naftoli rapoport <naftolir@gmail.com>
2025-02-17 16:49:35 -0500

    find difference between kids getting prizes fro mbefore fix to after

---
+++ mashpia.com/public/chidon_shipping/tests/find_diff.php
@@ -0,0 +1,63 @@
+<?php
+$admin_auth = ['school'];
+reuire_once $_SERVER['DOCUMENT_ROOT'] . '/header.php';
+
+if ($admin_user['auth'] != 'super') {
+    die('Access denied');
+}
+
+$sql1 = "SELECT 
+            cup.user_id, tci.highest_track 
+        FROM
+            chidon_user_prizes cup
+                JOIN
+            chidon_prizes cp USING (prize_id)
+                JOIN 
+            users u USING (user_id) 
+                JOIN 
+            th_chidon tc ON tc.user_id = u.user_id AND tc.year = cup.year 
+                LEFT JOIN
+            th_chidon_info tci ON u.user_id = tci.user_id AND tc.year = tci.year 
+                JOIN 
+            classes c ON u.class_id = c.class_id 
+        WHERE
+            cup.year = 5785 AND tc.date_paid > 0 
+                AND tc.ultimate_trip = 0 AND (tci.highest_track is null OR tci.highest_track != 'yesod') 
+                group by cup.user_id";
+
+$sql2 = "SELECT 
+            cup.user_id, tci.highest_track 
+        FROM
+            chidon_user_prizes cup
+                JOIN
+            chidon_prizes cp USING (prize_id)
+                JOIN 
+            users u USING (user_id) 
+                JOIN 
+            th_chidon tc ON tc.user_id = u.user_id AND tc.year = cup.year 
+                LEFT JOIN
+            th_chidon_info tci ON u.user_id = tci.user_id AND tc.year = tci.year 
+                JOIN 
+            classes c ON u.class_id = c.class_id 
+        WHERE
+            cup.year = 5785 AND tc.date_paid > 0 
+                AND tc.ultimate_trip = 0 AND tc.user_id not in (
+                    SELECT user_id FROM registration_charges WHERE year = :year AND type = 'RRYSD'
+                )
+                group by cup.user_id";
+
+$result1 = mysql_query($sql1);
+$result2 = mysql_query($sql2);
+
+$info1 = [];
+$info2 = [];
+while ($row1 = mysql_fetch_assoc($result1)) {
+    $info1[$row1['user_id']] = $row1;
+}
+while ($row2 = mysql_fetch_assoc($result2)) {
+    $info2[$row2['user_id']] = $row2;
+}
+
+// find user ids that are in info1 but not info2
+$diff = array_diff(array_keys($info1), array_keys($info2));
+echo "<pre>"; print_r($diff); echo "</pre>";
