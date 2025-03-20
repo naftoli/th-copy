a5f35e5517065284f52fd23658959a1ad77fb063
Local Repository <repository@local.dev>
2025-03-20 13:24:03 -0400

    update and fix chidon prizes for 5786

---
+++ mashpia.com/public/chidonOld/tasks/updatePrizes.php
@@ -0,0 +1,23 @@
+<?php
+$admin_auth = ['school'];
+require_once $_SERVER['DOCUMENT_ROOT'] . '/header.php';
+
+if ($admin_user['auth'] != 'super') {
+    echo "No permission.";
+    exit;
+}
+
+$prizes = [];
+$sql = "select * from chidon_prizes where prize_id >= 452 and prize_id <= 481";
+$result = mysql_query($sql);
+while ($row = mysql_fetch_assoc($result)) {
+    $prizes[] = $row;
+}
+
+foreach ($prizes as $prize) {
+    $sql = "update prizes set prize_picture = " . $prize['prize_picture'] . " where year = 5786 and prize_name = '" . $prize['prize_name'] . "'";
+    if (! mysql_query($sql)) {
+        echo "Error: " . mysql_error() . "<br />" . $sql . "<br /><br />";
+    }
+}
+?>
