81e2bdde66c8590dc3d507e0192a8fb4caf6203a
Local Repository <repository@local.dev>
2025-03-28 13:40:56 -0400

    add ability to show video for certain kids

--- mashpia.com/public/chidonOld/video/functions.php
+++ mashpia.com/public/chidonOld/video/functions.php
@@ -11,7 +11,7 @@ function getFinalMarks() {
     return $final_marks;
 }
 
-function getChildren($school_id, $gender) {
+function getChildren($school_id, $gender, $serials = []) {
     global $year;
 
     if ($gender == 'boys') $gender = 'M';
@@ -58,9 +58,12 @@ function getChildren($school_id, $gender) {
                     LEFT JOIN 
                 non_th_schools nts ON nts.non_th_school_id = u.non_th_school_id 
             WHERE
-                tc.year = $year AND tc.date_paid > 0 
-                    AND u.gender = '$gender'";
+                tc.year = $year AND tc.date_paid > 0";
+    if ($gender != 'all') $sql .= " AND u.user_gender = '$gender'";
     if ($school_id) $sql .= " AND u.school_id = " . $school_id;
+    if ($serials) {
+        $sql .= " AND u.user_serial in (" . implode(',', $serials) . ")";
+    }
     $sql .= " GROUP BY u.user_id";
     $sql .= " ORDER BY u.school_id, class_grade , last , first";
     // echo $sql; exit;
