b82c5d680edfdf26e3df9f4ee15281eefdcb7681
Local Repository <repository@local.dev>
2025-04-04 14:46:34 -0400

    fix up qrys for fixing up tehillim marks from adar 2 to nissan

--- mashpia.com/public/tehillim/fixes/fix_adar_duplicate_marks.php
+++ mashpia.com/public/tehillim/fixes/fix_adar_duplicate_marks.php
@@ -37,7 +37,16 @@ $stmt2 = $MASHPIA_DB->prepare("
     SET 
         date_tasks_mission_id = :new_mission_id 
     WHERE
-        date_tasks_mission_id = :old_mission_id
+        date_tasks_mission_id = :old_mission_id 
+            AND user_id = :user_id
+");
+
+$stmtMissions = $MASHPIA_DB->prepare('
+    SELECT * FROM date_tasks_mission_marks WHERE date_tasks_mission_id = :mission_id
+');
+
+$stmtMissionDelete = $MASHPIA_DB->prepare("
+    DELETE FROM date_tasks_mission_marks WHERE date_tasks_mission_id = :mission_id AND user_id = :user_id 
 ");
 
 $stmt3 = $MASHPIA_DB->prepare("
@@ -46,10 +55,19 @@ $stmt3 = $MASHPIA_DB->prepare("
         date_task_id = :new_task_id 
     WHERE
         date_task_id = :old_task_id
+            AND user_id = :user_id
 ");
 
-//$success = true;
-//$MASHPIA_DB->beginTransaction();
+$stmtTasks = $MASHPIA_DB->prepare("
+    SELECT * FROM date_tasks_marks WHERE date_task_id = :task_id
+");
+
+$stmtTaskDelete = $MASHPIA_DB->prepare("
+    DELETE FROM date_tasks_marks WHERE date_task_id = :task_id AND user_id = :user_id
+");
+
+$success = true;
+$MASHPIA_DB->beginTransaction();
 foreach ($school_types as $type) {
     foreach ($ages as $age) {
         foreach ($tracks as $track) {
@@ -74,42 +92,91 @@ foreach ($school_types as $type) {
                     $new = $rows[1];
 
                     if ($old['date_tasks_mission_id'] != $new['date_tasks_mission_id']) {
-                        $res2 = $stmt2->execute([
-                            'new_mission_id' => $new['date_tasks_mission_id'],
-                            'old_mission_id' => $old['date_tasks_mission_id'],
+                        // get users that have marks with old mission id
+                        $stmtMissions->execute([
+                            'mission_id' => $old['date_tasks_mission_id']
                         ]);
-//                        if (!$res2) {
-//                            $success = false;
-////                            $stmt->debugDumpParams();
-//                            $stmt2->debugDumpParams();
-//                            print_r($stmt2->errorInfo());
-//                            break 5;
-//                        }
+                        $users = $stmtMissions->fetchAll(PDO::FETCH_ASSOC);
+
+                        foreach ($users as $user) {
+                            $user_id = $user['user_id'];
+                            $res2 = $stmt2->execute([
+                                'new_mission_id' => $new['date_tasks_mission_id'],
+                                'old_mission_id' => $old['date_tasks_mission_id'],
+                                'user_id' => $user_id
+                            ]);
+                            if (!$res2) {
+                                // check error to see if it's duplicate
+                                $error = $stmt2->errorInfo()[2];
+                                if (strstr($error, 'Duplicate') !== false || strstr($error, 'duplicate') !== false) {
+                                    // delete old mission b/c user aleady has it for new one
+                                    $del = $stmtMissionDelete->execute([
+                                        'mission_id' => $old['date_tasks_mission_id'],
+                                        'user_id' => $user_id
+                                    ]);
+                                    if (!$del) {
+                                        $success = false;
+                                        echo $stmtMissionDelete->errorInfo()[2] . "<br />";
+                                        $stmtMissionDelete->debugDumpParams();
+                                        $stmt2->debugDumpParams();
+                                        break 5;
+                                    }
+                                } else {
+                                    echo $error . "<br />";
+                                    $success = false;
+                                    break 5;
+                                }
+                            }
+                        }
                     }
 
                     if ($old['date_task_id'] != $new['date_task_id']) {
-                        $res3 = $stmt3->execute([
-                            'new_task_id' => $new['date_task_id'],
-                            'old_task_id' => $old['date_task_id'],
+                        $stmtTasks->execute([
+                            'task_id' => $old['date_task_id']
                         ]);
-//                        if (!$res3) {
-//                            $success = false;
-////                            $stmt->debugDumpParams();
-////                            $stmt2->debugDumpParams();
-//                            $stmt3->debugDumpParams();
-//                            break 5;
-//                        }
+                        $users = $stmtTasks->fetchAll(PDO::FETCH_ASSOC);
+
+                        foreach ($users as $user) {
+                            $user_id = $user['user_id'];
+                            $res3 = $stmt3->execute([
+                                'new_task_id' => $new['date_task_id'],
+                                'old_task_id' => $old['date_task_id'],
+                                'user_id' => $user_id
+                            ]);
+                            if (!$res3) {
+                                // check error to see if it's duplicate
+                                $error2 = $stmt2->errorInfo()[2];
+                                if (strstr($error2, 'Duplicate') !== false || strstr($error2, 'duplicate') !== false) {
+                                    $del2 = $stmtTaskDelete->execute([
+                                        'task_id' => $old['date_task_id'],
+                                        'user_id' => $user_id
+                                    ]);
+                                    if (!$del2) {
+                                        $success = false;
+                                        echo $stmtTaskDelete->errorInfo()[2] . "<br />";
+                                        $stmtTaskDelete->debugDumpParams();
+                                        $stmt3->debugDumpParams();
+                                        break 5;
+                                    }
+                                } else {
+                                    echo $error2 . "<br />";
+                                    $success = false;
+                                    break 5;
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
     }
 }
-echo "Done.";
-//if ($success) {
-//    $MASHPIA_DB->commit();
-//    echo "Done.";
-//} else {
-//    $MASHPIA_DB->rollBack();
-//    echo "There were errors.";
-//}
+
+//echo "Done.";
+if ($success) {
+    $MASHPIA_DB->commit();
+    echo "Done.";
+} else {
+    $MASHPIA_DB->rollBack();
+    echo "There were errors.";
+}
