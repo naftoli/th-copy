460270c9a67ccb538936c22e7aeec026e68dfec7
Local Repository <repository@local.dev>
2025-04-28 16:47:12 -0400

    updates for installments

--- mashpia.com/public/classes/authorize/getInstallments.php
+++ mashpia.com/public/classes/authorize/getInstallments.php
@@ -35,11 +35,11 @@ foreach ($info as $key => $row) {
         $installment_info['status'] = $subscription->getStatus();
         $transactions = $subscription->getArbTransactions();
 
+        $paid = 0;
+        $installment_info['num_transactions'] = count($transactions);
+        $installment_info['completed'] = 0;
+        $installment_info['failed'] = 0;
         if (! is_null($transactions)) {
-            $paid = 0;
-            $installment_info['num_transactions'] = count($transactions);
-            $installment_info['completed'] = 0;
-            $installment_info['failed'] = 0;
             foreach ($transactions as $transaction) {
                 // get last word of response
                 $resArr = explode(' ', $transaction->getResponse());
