33faa1f123527e1c5a662335c15173abd61ee834
Local Repository <repository@local.dev>
2025-04-28 17:17:03 -0400

    Revert "debug updates for installments"
    This reverts commit 9b4058b5b7424cf21231179b8230572650b911af.

--- mashpia.com/public/classes/authorize/getInstallments.php
+++ mashpia.com/public/classes/authorize/getInstallments.php
@@ -10,12 +10,12 @@ if ($admin_user['auth'] != 'super') {
 }
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/class.globalSettings.php';
-$year = GlobalSettings::getChidonYear();
+// ... existing headers and auth checks ...
 
 // Get the POST data
-//$postData = json_decode(file_get_contents('php://input'), true);
-//$year = isset($postData['year']) ? intval($postData['year']) : GlobalSettings::getChidonYear();
-
+$postData = json_decode(file_get_contents('php://input'), true);
+$year = isset($postData['year']) ? intval($postData['year']) : GlobalSettings::getChidonYear();
+echo $year; exit;
 // ... rest of your existing code using $year ...
 require_once 'Installments.php';
 use \classes\authorize\Installments as Installments;
