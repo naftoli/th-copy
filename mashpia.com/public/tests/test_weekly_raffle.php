dc11e0b480556ffc7bd323fc84814dffe9046030
Local Repository <repository@local.dev>
2025-05-05 16:26:34 -0400

    code for testing raffles

--- mashpia.com/public/tests/test_weekly_raffle.php
+++ mashpia.com/public/tests/test_weekly_raffle.php
@@ -17,7 +17,7 @@ $raffles = [435];
 foreach ($raffles as $raffle_id) {
     echo "<h3>Loading raffle $raffle_id</h3>";
     $raffle = Raffle::load($raffle_id);
-    $users = $raffle->get_eligable_user_ids(false, false, true, false, 0, true); // no specific user but do show the log
+    $users = $raffle->get_eligable_user_ids(false, false, true, false, $school_id, true); // no specific user but do show the log
     foreach ($users as $school => $user_ids) {
         echo "<p>Choosing random users from school $school</p>";
         for ($i = 0; $i < 5; $i++) {
