67fd9dbaa49a5b8a25239faf2861543fa189da99
Naftoli Rapoport <naftolir@gmail.com>
2025-06-08 20:59:58 -0400

    fix: increase memory limit to 3GB for rank ceremony zip creation

--- mashpia.com/public/rank_ceremony/createZip.php
+++ mashpia.com/public/rank_ceremony/createZip.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('error_reporting', E_ALL);
 ini_set('max_execution_time', 300);
-ini_set('memory_limit', '512M');
+ini_set('memory_limit', '3072M');
 
 $admin_auth = ['school'];
 require_once $_SERVER['DOCUMENT_ROOT'] . '/header.php';
