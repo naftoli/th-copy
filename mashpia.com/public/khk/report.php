a81b9b4a578d891c6e3100ef2c67bee11895003c
Naftoli Rapoport <naftolir@gmail.com>
2025-06-09 19:01:52 -0400

    feat: add KHK report page with CSV upload functionality for student test results

---
+++ mashpia.com/public/khk/report.php
@@ -0,0 +1,123 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+$admin_auth = ['school'];
+require_once '../header.php';
+require_once '../api/header/db.php';
+require_once '../class.adminSchools.php';
+
+$as = new AdminSchools($admin_user['admin_id'], $admin_user['auth']);
+$schools = $as->getSchools();
+
+$super = $admin_user['auth'] == 'super';
+
+$info = [];
+$passed = [];
+$not_passed = [];
+$sql = "SELECT * FROM khk_info_5785 k 
+        JOIN users u ON k.user_serial = u.user_serial
+        JOIN classes c ON u.class_id = c.class_id";
+if (!$super) {
+    $sql .= " WHERE u.school_id IN (" . implode(',', array_keys($schools)) . ")";
+}
+$sql .= " ORDER BY u.school_id, c.class_grade, c.class_sub, u.last, u.first";
+$stmt = $MASHPIA_DB->query($sql);
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    $info[$row['school_id']][] = $row;
+    if (intval($row['amount_passed']) == 4) {
+        $passed[$row['school_id']][] = $row;
+    } else {
+        $not_passed[$row['school_id']][] = $row;
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>KHK Report</title>
+    <link href="../admin_styles.css" rel="stylesheet" type="text/css">
+    <style>
+        .infobox {
+            line-height: 1.5;
+        }
+        tr, th, td {
+            padding: 5px;
+            font-size: 12px;
+            border-bottom: 1px solid #ccc;
+        }
+    </style>
+</head>
+<body>
+    <?php include '../admin_header.php'; ?>
+    <h1>KHK Report</h1>
+    <?php foreach ([$passed, $not_passed] as $info) { ?>
+    <table>
+        <thead>
+            <tr>
+                <th>Full Hebrew Name</th>
+                <th>Gender</th>
+                <th>School</th>
+                <th>Current Grade</th>
+                <th>Serial</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Amount Passed</th>
+                <th>5782</th>
+                <th>5783</th>
+                <th>5784</th>
+                <th>5785</th>
+                <th>Notes</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($info as $school_id => $rows) { ?>
+                <?php foreach ($rows as $row) { ?>
+                    <?php
+                    $he_name = $row['first_he'] . ' ' . $row['last_he'];
+                    // check if there's dbl quote in string
+                    if (strpos($he_name, '"') !== false) {
+                        $he_name = str_replace('"', '&quot;', $he_name);
+                    }
+                    ?>
+                    <tr>
+                        <td><?php echo $he_name; ?></td>
+                        <td><?php echo $row['gender']; ?></td>
+                        <td><?php echo $schools[$school_id]; ?></td>
+                        <td><?php echo $row['class_grade'] . ($row['class_sub'] ? '-' . $row['class_sub'] : ''); ?></td>
+                        <td><?php echo $row['user_serial']; ?></td>
+                        <td><?php echo $row['first']; ?></td>
+                        <td><?php echo $row['last']; ?></td>
+                        <td><?php echo $row['amount_passed']; ?></td>
+                        <td><?php echo $row['5782']; ?></td>
+                        <td><?php echo $row['5783']; ?></td>
+                        <td><?php echo $row['5784']; ?></td>
+                        <td><?php echo $row['5785']; ?></td>
+                        <td>
+                            <textarea
+                                id='<?= $row['user_id'] ?>'
+                                name='notes[<?= $row['user_id'] ?>]'
+                                rows='3'
+                                cols='50'
+                            >
+                                <?php echo $row['notes']; ?>
+                            </textarea>
+                        </td>
+                    </tr>
+                <?php } ?>
+            <?php } ?>
+        </tbody>
+    </table>
+    <?php } ?>
+</body>
+<script 
+    src="https://code.jquery.com/jquery-1.12.4.min.js" 
+    integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" 
+    crossorigin="anonymous">
+</script>
+<script>
+    // function to check hebrew characters
+    
+</script>
+</html>
---
+++ mashpia.com/public/khk/tasks/uploadFile.php
@@ -0,0 +1,97 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+$admin_auth = ['school'];
+require_once '../../header.php';
+require_once '../../api/header/db.php';
+
+// make sure we are super user
+if ($admin_user['auth'] != 'super') {
+    echo "You are not authorized to access this page.";
+    exit;
+}
+
+// upload file
+if (isset($_FILES['file'])) {
+    $file = $_FILES['file'];
+    $name = $file['name'];
+    $type = $file['type'];
+    $size = $file['size'];
+    $tmp_name = $file['tmp_name'];
+    $error = $file['error'];
+
+    if ($type != 'text/csv') {
+        echo "File is not a csv file.";
+        exit;
+    }
+    
+    if ($error == 0) {
+        $stmt = $MASHPIA_DB->prepare("
+            INSERT INTO khk_info_5785 
+            SET 
+                user_serial = :serial,
+                amount_passed = :amount_passed, 
+                5782 = :5782,
+                5783 = :5783,
+                5784 = :5784,
+                5785 = :5785
+        ");
+
+        $MASHPIA_DB->beginTransaction();
+        $success = true;
+
+        $qrys = [];
+        // open up csv file
+        $handle = fopen($tmp_name, 'r');
+        $first = true;
+        while (($data = fgetcsv($handle, 1000, ',')) !== FALSE) {
+            if ($first) {
+                $first = false;
+                continue;
+            }
+            // do something with the data
+            $user_serial = intval($data[0]);
+            $amount_passed = intval($data[1]);
+            $passed_5782 = $data[2];
+            $passed_5783 = $data[3];
+            $passed_5784 = $data[4];
+            $passed_5785 = $data[5];
+            
+            $res = $stmt->execute([
+                ':serial' => $user_serial,
+                ':amount_passed' => $amount_passed,
+                ':5782' => $passed_5782,
+                ':5783' => $passed_5783,
+                ':5784' => $passed_5784,
+                ':5785' => $passed_5785
+            ]);
+            if (!$res) {
+                $success = false;
+                break;
+            }
+        }
+        fclose($handle);
+
+        if ($success) {
+            $MASHPIA_DB->commit();
+            echo "File uploaded successfully.";
+        } else {
+            $MASHPIA_DB->rollBack();
+            echo "File upload failed.";
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Upload File</title>
+</head>
+<body>
+    <form action="uploadFile.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="file" id="file">
+        <input type="submit" value="Upload" name="submit">
+    </form>
+</body>
+</html>
