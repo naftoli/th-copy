edda86d753cce728358470d2420a3992b784b02d
Naftoli Rapoport <naftolir@gmail.com>
2025-06-12 13:50:46 -0400

    feat: implement conditional SQL logic in updateHachayols.php to handle user updates based on registration year

--- mashpia.com/public/mobile/reg/ajax/updateHachayols.php
+++ mashpia.com/public/mobile/reg/ajax/updateHachayols.php
@@ -1,16 +1,42 @@
 <?php
 $admin_auth = ['user'];
 require $_SERVER['DOCUMENT_ROOT'] . '/api/header/db.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/class.globalSettings.php';
+$year = GlobalSettings::getRegistrationYear();
 
-$stmt = $MASHPIA_DB->prepare("update users set hachayol = :val where user_id = :user");
+if ($year < 5786) {
+    $stmt = $MASHPIA_DB->prepare("update users set hachayol = :val where user_id = :user");
+} else {
+    $stmtUpdate = $MASHPIA_DB->prepare("
+        INSERT IGNORE INTO hachayols_to_give (user_id, year) VALUES (:user, :year)
+    ");
+    $stmtDelete = $MASHPIA_DB->prepare("
+        DELETE FROM hachayols_to_give WHERE user_id = :user AND year = :year
+    ");
+}
 $users = $_POST['list'];
 
 $MASHPIA_DB->beginTransaction();
 foreach ($users as $user) {
-    $res = $stmt->execute([
-        'user'  => $user['user_id'],
-        'val'   => intval($user['checked'])
-    ]);
+    if ($year < 5786) {
+        $res = $stmt->execute([
+            'user'  => $user['user_id'],
+            'val'   => intval($user['checked'])
+        ]);
+    } else {
+        $checked = intval($user['checked']);
+        if ($checked) {
+            $res = $stmtUpdate->execute([
+                'user'  => $user['user_id'],
+                'year'  => $year
+            ]);
+        } else {
+            $res = $stmtDelete->execute([
+                'user'  => $user['user_id'],
+                'year'  => $year
+            ]);
+        }
+    }
     if (!$res) {
 //        $stmt->debugDumpParams();
         $MASHPIA_DB->rollBack();
