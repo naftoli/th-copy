99a0e1c79de97622461040ab9ca83f09744d9739
Naftoli Rapoport <naftolir@gmail.com>
2025-06-17 14:05:05 -0400

    fix: correct curl_setopt function call in donate2.php for reCAPTCHA verification

--- mashpia.com/public/donate/donate2.php
+++ mashpia.com/public/donate/donate2.php
@@ -12,7 +12,7 @@ if (isset($_POST['g-recaptcha-response'])) {
 	$privatekey = '6LfOSmMrAAAAAEqoal8ixLIIpPaAStI2i_cV2_Se';
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
-	t($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
 	curl_setopt($ch, CURLOPT_POST, 1);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, [
