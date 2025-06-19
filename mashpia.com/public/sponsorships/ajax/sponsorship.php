e235f04608159709ff41aa10a1e823d05fbf4f9d
Local Repository <repository@local.dev>
2025-06-19 16:13:17 -0400

    refactor: improve input handling logic in sponsorship.php to prioritize POST data over JSON input

--- mashpia.com/public/sponsorships/ajax/sponsorship.php
+++ mashpia.com/public/sponsorships/ajax/sponsorship.php
@@ -7,7 +7,7 @@ if ($admin_user['auth'] != 'super') {
 }
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/api/header/db.php';
-$input = $_POST ?? json_decode(file_get_contents('php://input'), true);
+$input = $_POST && count($_POST) ? $_POST : json_decode(file_get_contents('php://input'), true);
 
 switch ($input['action']) {
     case 'create':
