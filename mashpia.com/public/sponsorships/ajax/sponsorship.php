7459759731e93580fd5c4e76ef07b14a55d7ba18
Local Repository <repository@local.dev>
2025-06-20 14:24:44 -0400

    refactor: remove unused image parameter from sponsorship update query in sponsorship.php to enhance code clarity

--- mashpia.com/public/sponsorships/ajax/sponsorship.php
+++ mashpia.com/public/sponsorships/ajax/sponsorship.php
@@ -85,7 +85,7 @@ function update() {
     $result = $MASHPIA_DB->prepare("
         UPDATE mashpiadb.sponsorships SET start_date = :start_date, end_date = :end_date, sponsor = :sponsor, reason = :reason, name = :name, email = :email, 
             phone = :phone, amount_paid = :amount_paid WHERE sponsorship_id = :id");
-    $res = $result->execute([':id' => $id, ':start_date' => $start_date, ':end_date' => $end_date, ':sponsor' => $sponsor, ':reason' => $reason, ':name' => $name, ':image' => $image, ':email' => $email, ':phone' => $phone, ':amount_paid' => $amount_paid]);
+    $res = $result->execute([':id' => $id, ':start_date' => $start_date, ':end_date' => $end_date, ':sponsor' => $sponsor, ':reason' => $reason, ':name' => $name, ':email' => $email, ':phone' => $phone, ':amount_paid' => $amount_paid]);
     
     if ($res) { 
         if ($change_image) {
