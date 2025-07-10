0dd385e9fc6563b2c6fb08cc2dff690d0350f7ce
Local Repository <repository@local.dev>
2025-07-10 18:54:10 -0400

    refactor: update MedalsEarned and RanksEarned classes to extend Earned; remove unused dates property and related methods for cleaner code

--- mashpia.com/public/medalsEarned.class.php
+++ mashpia.com/public/medalsEarned.class.php
@@ -1,16 +1,15 @@
 <?php
-class MedalsEarned 
+class MedalsEarned extends Earned
 {
 	private $subjects;
 	private $medals;
-	private $dates;
 	private $report;
 	
 	public function __construct() {
+		parent::__construct();
 		$this->subjects = array();
 		$this->medals = array();
 		$this->report = array();
-		$this->dates = array();
 	}
 	
 	public function getReport() {
@@ -23,7 +22,6 @@ class MedalsEarned
 	private function createReport() {
 		$this->setSubjects();
 		$this->setMedals();
-		$this->setDates();
 
 		foreach ($this->dates as $year => $date) {
 			foreach ($this->subjects as $subject => $subject_id) {
@@ -75,36 +73,12 @@ class MedalsEarned
 		}
 	}
 	
-	private function setDates() {
-		$this->dates = array(
-			5769	=>	2454718, // Sep 8, 2008 
-			5770	=>	2455082, // Sep 7, 2009 
-			5771	=>	2455439, // Aug 30, 2010 
-			5772	=>	2455810, // Sep 5, 2011 
-			5773	=>	2456174, // Sep 3, 2012 
-			5774	=>	2456531, // Aug 26, 2013
-			5775	=>	2456908, // Sep 7, 2014
-			5776	=>	2457265, // Aug 30, 2015
-			5777	=>	2457636, // Sep 4, 2016
-			5778	=>	2457885, // May 11, 2017
-			5779	=>	2458236, // April 27, 2018
-			5780	=>	2458628, // May 24, 2019
-            5781    =>  2458983, // May 13, 2020,
-            5782    =>  2459363, // May 28, 2021
-            5783    =>  2459719, // May 19, 2022
-		);
-	}
-	
 	public function getMedals() {
 		return $this->medals;
 	}
 	
 	public function getSubjects() {
 		return $this->subjects;
-	}
-	
-	public function getDates() {
-		return $this->dates;
 	}
 }
 ?>
--- mashpia.com/public/ranksEarned.class.php
+++ mashpia.com/public/ranksEarned.class.php
@@ -1,15 +1,14 @@
 <?php
-class RanksEarned 
+class RanksEarned extends Earned
 {
 	private $subjects;
 	private $medals;
-	private $dates;
 	private $report;
 	
 	public function __construct() {
+		parent::__construct();
 		$this->ranks = array();
 		$this->report = array();
-		$this->dates = array();
 	}
 	
 	public function getReport() {
@@ -21,7 +20,6 @@ class RanksEarned
 	
 	private function createReport() {
 		$this->setRanks();
-		$this->setDates();
 
 		foreach ($this->dates as $year => $date) {
 			foreach ($this->ranks as $rank_ord => $rank_name) {
@@ -47,34 +45,8 @@ class RanksEarned
 		}
 	}
 	
-	private function setDates() {
-		$this->dates = array(
-			5769	=>	2454718, // Sep 8, 2008 
-			5770	=>	2455082, // Sep 7, 2009 
-			5771	=>	2455439, // Aug 30, 2010 
-			5772	=>	2455810, // Sep 5, 2011 
-			5773	=>	2456174, // Sep 3, 2012 
-			5774	=>	2456531, // Aug 26, 2013
-			5775	=>	2456908, // Sep 7, 2014
-			5776	=>	2457265, // Aug 30, 2015
-			5777	=>	2457636, // Sep 4, 2016
-			5778	=>	2457885, // May 11, 2017 - was changed to 2457993 at some point but put back to original
-			5779	=>	2458236, // April 27, 2018
-			5780	=>	2458628, // May 24, 2019,
-            5781    =>  2458983, // May 13, 2020
-            5782    =>  2459363, // May 28, 2021
-			5783	=>  2459718, // May 19, 2022
-			5784	=>  2460073, // May 9, 2023
-			5785	=>	2460456  // May 26, 2024
-		);
-	}
-	
 	public function getRanks() {
 		return $this->ranks;
-	}
-	
-	public function getDates() {
-		return $this->dates;
 	}
 }
 ?>
