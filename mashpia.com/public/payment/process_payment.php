881124325d947dadc26a78b14627ac82fa39ac8b
Naftoli Rapoport <naftolir@gmail.com>
2025-07-17 11:55:14 -0400

    fix: change CC email to BCC in payment notification for enhanced privacy

--- mashpia.com/public/payment/process_payment.php
+++ mashpia.com/public/payment/process_payment.php
@@ -100,7 +100,7 @@ if ($charged) {
     // Email notification
     $mail_parms = [
         'to' => $email, 
-        'cc' => "cthshipping@gmail.com", // dovie
+        'bcc' => "cthshipping@gmail.com", // dovie
         'subject' => "Confirmation of Credit Card Transaction",
         'message' => "Thank you for your payment to Tzivos Hashem. Your credit card has been charged $" . $formattedAmount .
             ". Your authorization ID is: " . $response_array[4],
