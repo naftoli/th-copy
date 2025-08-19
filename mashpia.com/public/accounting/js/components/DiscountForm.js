467d1e77c96ea2303c30a8c586d3f218887dffb7
Local Repository <repository@local.dev>
2025-08-19 15:21:57 -0400

    refactor: remove step attribute from amount input in DiscountForm to simplify user input handling

--- mashpia.com/public/accounting/js/components/DiscountForm.js
+++ mashpia.com/public/accounting/js/components/DiscountForm.js
@@ -122,7 +122,6 @@
                     value={discountForm.amount}
                     onChange={(e) => handleDiscountInputChange('amount', e.target.value)}
                     placeholder="0.00"
-                    step="0.01"
                     min="0"
                     />
                 </InputGroup>
