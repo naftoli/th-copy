35aaf3668e08d1808bd18f1c42df086f80adced5
Local Repository <repository@local.dev>
2025-09-03 13:54:29 -0400

    feat: add condition to filter users registered before September 1, 2025, based on cookie value in report generation

--- mashpia.com/public/chidon_shipping/report.php
+++ mashpia.com/public/chidon_shipping/report.php
@@ -150,6 +150,8 @@ if (in_array('tc', $tables)) $sql .= " AND tc.year = " . $year;
 if ($_POST['gender'] == 'm') $sql .= " AND u.gender = 'M'";
 else if ($_POST['gender'] == 'f') $sql .= " AND u.gender = 'F'";
 
+if (isset($_COOKIE['naftoli']) && $_COOKIE['naftoli'] == 1) $sql .= " AND u.user_registered < '2025-09-01 16:55:00'";
+
 //******* ORDER BY *********//
 $sql .= " ORDER BY u.school_id";
 if (in_array('c.class_grade', $fields_chosen)) $sql .= ", c.class_grade";
