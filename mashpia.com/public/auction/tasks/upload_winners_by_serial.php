dbc0b4d10c7d65ef1da650c56bd7c175b3d70c8a
Local Repository <repository@local.dev>
2025-09-07 21:20:34 -0400

    fix: add debug output for auction ID retrieval in upload winners script

--- mashpia.com/public/auction/tasks/upload_winners_by_serial.php
+++ mashpia.com/public/auction/tasks/upload_winners_by_serial.php
@@ -12,6 +12,7 @@ $sql = "select auction_id from auctions order by auction_id desc limit 1";
 $result = mysql_query($sql);
 $row = mysql_fetch_assoc($result);
 $auction_id = intval($row['auction_id']);
+echo "Auction ID: " . $auction_id . "<br />";
 
 // get winners from file
 if ( isset( $_FILES['add'] ) ) {
