0a986a191e88efa4788dd7cd63f0e7a185dc37d9
Naftoli Rapoport <naftolir@gmail.com>
2025-09-07 23:11:58 -0400

    fix: update user eligibility table to include school name in headers

--- mashpia.com/public/raffles/shared/ajax/table_user_eligible.php
+++ mashpia.com/public/raffles/shared/ajax/table_user_eligible.php
@@ -60,11 +60,11 @@ foreach ( $schoolsUsers as $school => $users ) {
             $user_ids = $raffle->get_eligable_user_ids(false, false, false, true, $school); // get all users from school
             echo '<div align="center"><img src="../images/Mission Marathon logo.png" class="marathonLogo" /></div>';
             echo "<h2>" . $schools[$school] . " - " . $raffle->name . "</h2>";
-            echo "<div id='table-marks'><table><tr><th>Grade</th><th>Student</th><th></th></tr>";
+            echo "<div id='table-marks'><table><tr><th>School</th><th>Grade</th><th>Student</th><th></th></tr>";
             if (!empty($user_ids)) {
                 foreach ($users as $user) {
                     if (isset($user_ids[$user['user_id']])) {
-                        echo "<tr><td>" . $user['class_grade'] . (empty($user['class_sub']) ? '' : "-" . $user['class_sub']) .
+                        echo "<tr><td>" . $schools[$school] . "</td><td>" . $user['class_grade'] . (empty($user['class_sub']) ? '' : "-" . $user['class_sub']) .
                             "</td><td>" . $user['first'] . " " . $user['last'] . "</td><td><img class='flag' src='../images/5 flag.png' /></td></tr>";
                     }
                 }
