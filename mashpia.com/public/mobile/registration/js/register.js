19873465486ce3592439f7701ef1177a083b2cc4
Naftoli Rapoport <naftolir@gmail.com>
2025-09-18 15:31:51 -0400

    fix: remove outdated fee condition for specific school in registration logic

--- mashpia.com/public/mobile/registration/js/register.js
+++ mashpia.com/public/mobile/registration/js/register.js
@@ -2309,7 +2309,6 @@ var templates = function () {
             **/
       let html = '<option value="0">Select Amount to Pay</option>'
       let fees = [25, 30, 40, 50]
-      // if (user.school.school_id == 471 && this.beforeDate('2025-09-10T00:00:00')) fees = [20, 25, 30, 40, 50]
       if (user.school.school_id == 61) fees = [35, 40, 45, 50]
       else if (user.school.school_id == 269) fees = [55, 60, 65, 70]
 
