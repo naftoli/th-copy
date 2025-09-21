13313a699f7e7f6f25268520adc6f145a8b492e9
Naftoli Rapoport <naftolir@gmail.com>
2025-09-21 14:40:00 -0400

    fix: improve audio links formatting by adding line break and removing bold tags

--- mashpia.com/public/chidonOld/limud/createLimudTasks.php
+++ mashpia.com/public/chidonOld/limud/createLimudTasks.php
@@ -129,9 +129,9 @@ foreach ($info as $details) {
                 $tasks = [
                     [
                         'name'  => "Today's unit(s) are: " . $units . 
-                        ".<br /><i>You need to learn " . $minutes[$track] . " minutes per day.</i>
-                        <span class='audioLinks' style='display: none;'><strong><i>Links to the audio of today's unit(s): 
-                        " . implode(', ', $audioLinks) . "</i></strong></span>
+                        ".<br /><i>You need to learn " . $minutes[$track] . " minutes per day.</i><br />
+                        <span class='audioLinks' style='display: none;'><i>Links to the audio of today's unit(s): 
+                        " . implode(', ', $audioLinks) . "</i></span>
                         <br />I learned ___ minutes today.<br />",
                         'qty'   => 120
                     ],
