6469dce056ca7445499ce83e19b2a23c74fd61da
Naftoli Rapoport <naftolir@gmail.com>
2025-11-03 16:50:03 -0500

    feat: enhance prize auction management by adding school type associations and updating prize retrieval logic

--- mashpia.com/public/mobile/auction/ajax/getPrizes.php
+++ mashpia.com/public/mobile/auction/ajax/getPrizes.php
@@ -12,17 +12,20 @@ $sql = "select * from admin_auths where id = " . $user . " and admin_id = " . $a
 $result = mysql_query($sql);
 if (mysql_num_rows($result) > 0) {
 
-    $sql = "select school_id, class_id from users where user_id = " . $user;
+    $sql = "select school_id, class_id, school_type_id from users where user_id = " . $user;
     $result = mysql_query($sql);
     $row = mysql_fetch_assoc($result);
     $school = $row['school_id'];
     $class_id = $row['class_id'];
-    
+    $school_type_id = $row['school_type_id'];
+
     $prizes = array();
     $sql = "select p.prize_id, p.prize_name, p.prize_points, p.prize_image_id, ap.available 
             from prizes_auction p
-            join auction_prizes ap using (prize_id)
-            where ap.auction_id = " . $auction;
+            join auction_prizes ap using (prize_id) 
+            left join prizes_auction_types pat using (prize_id) 
+            where ap.auction_id = " . $auction . " 
+            and (pat.school_type_id is null or pat.school_type_id = " . $school_type_id . ")";
     $result = mysql_query($sql);
     while ($row = mysql_fetch_assoc($result)) {
         $prizes[$row['prize_points']][] = $row;
