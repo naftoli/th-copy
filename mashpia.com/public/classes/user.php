20cd23d0eca4d30d0f1b674c0f84deba5f9a8666
Naftoli Rapoport <naftolir@gmail.com>
2025-11-03 16:54:20 -0500

    Revert "fix: update user task retrieval logic to allow 12 pesukim campaign tasks only for mobile site and non registered users"
    This reverts commit d54daa7977e5f56283fd5b5568f59b75915d343b.

--- mashpia.com/public/classes/user.php
+++ mashpia.com/public/classes/user.php
@@ -464,10 +464,10 @@ class user {
 		
 		while ($row = mysql_fetch_assoc( $query ) ) {
 			if (!$this->user_registered) {
-				//only allow 12 pesukim campaign tasks and only for mobile site
-				if ($row['subject_id'] != 136 || $printing_mode) {
-					continue;
-				}
+				// only allow 12 pesukim campaign tasks
+				// if ($row['subject_id'] != 4) {
+				// 	continue;
+				// }
 			}
 			if ($row["level"] > 0 && $row["track_id"] > 0) {
 				$user_track = new user_track($row);
