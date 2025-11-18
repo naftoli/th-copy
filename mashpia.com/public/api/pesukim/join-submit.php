095595103e78779378f6adfe657272abb6e566e5
Naftoli Rapoport <naftolir@gmail.com>
2025-11-18 18:08:32 -0500

    feat: implement user registration and email confirmation process in join-submit.php

--- mashpia.com/public/api/pesukim/join-submit.php
+++ mashpia.com/public/api/pesukim/join-submit.php
@@ -1,4 +1,42 @@
 <?php
+// ini_set('display_errors', 1);
+// ini_set('error_reporting', E_ALL);
+
+function clean(&$value) {
+  if (is_array($value)) {
+    foreach ($value as $k => &$v) {
+      clean($v);
+    }
+  } else {
+    $value = mysql_real_escape_string($value);
+  }
+}
+
+function sendEmailConfirmation($email, $name) {
+  $subject = 'Welcome to Tzivos Hashem';
+
+  $msg = 'Welcome to Tzivos Hashem!<br /><br />';
+  $msg .= 'Your username is ' . $email . ' and your password is 1234<br /><br />';
+  $msg .= 'Your child ' . $name . ' is now a part of Tzivos Hashem!<br /><br />';
+  $msg .= 'To login, please use the following link: <a href="https://tzivoshashem.com/mobile">https://tzivoshashem.com/mobile</a><br /><br />';
+  $msg .= 'If you have any questions, please feel free to contact us at <a href="mailto:support@tzivoshashem.org">support@tzivoshashem.org</a>.<br /><br />';
+  $msg .= 'Thank you for joining Tzivos Hashem!<br /><br />';
+  $msg .= 'Best regards,<br /><br />';
+  $msg .= 'The Tzivos Hashem Team';
+  $msg .= '<br /><br />';
+  $msg .= 'To unsubscribe from these emails, please click <a href="https://tzivoshashem.com/mobile">here</a>.<br /><br />';
+  $msg .= 'Copyright © 2025 Tzivos Hashem. All rights reserved.';
+  $msg .= '<br /><br />';
+
+  // To send HTML mail, the Content-type header must be set
+  $headers[] = 'MIME-Version: 1.0';
+  $headers[] = 'Content-type: text/html; charset=iso-8859-1';
+  // Additional headers
+  $headers[] = 'From: Tzivos Hashem <admin@tzivoshashem.org>';
+  $headers[] = 'Reply-To: Tzivos Hashem <admin@tzivoshashem.org>';
+  return @mail($email, $subject, $msg, implode("\r\n", $headers));
+}
+
 header('Content-Type: application/json; charset=utf-8');
 
 $method = $_SERVER['REQUEST_METHOD'];
@@ -10,6 +48,7 @@ if ($method !== 'POST') {
 }
 
 $input = json_decode(file_get_contents('php://input'), true) ?: [];
+clean($input);
 
 $errors = [];
 if (empty($input['firstName']))     $errors['firstName'] = 'Required';
@@ -34,9 +73,77 @@ if ($errors) {
   exit;
 }
 
+require_once $_SERVER['DOCUMENT_ROOT'] . '/db.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/api/header/header.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/../includes/globals.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/newClasses/newParent.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/newClasses/newSoldier.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/pesukim/class.pesukim.php';
+
+$MASHPIA_DB->beginTransaction();
+// create a new user
+try {
+  // first find out if there's an admin already that exists with this email
+  $sql = "select * from admins where admin_email = '" . $input['parentEmail'] . "'";
+  $result = mysql_query($sql);
+  if (mysql_num_rows($result) > 0) {
+      $parent = mysql_fetch_object($result);
+  } else {
+      $parent = new NewParent();
+      $created = $parent->action([
+          'username' => $input['parentEmail'],
+          'hashed_pass' => password_hash('1234', PASSWORD_DEFAULT),
+          'password' => encryptPassword('1234', ENCRYPTION_KEY),
+          'admin_email' => $input['parentEmail']
+      ]);
+      if (!$created) {
+        throw new Exception('Failed to create parent account');
+      }
+      $parent->admin_id = $parent->getAdminID();
+  }
+
+  if ($parent->admin_id) {
+      $admin_id = $parent->admin_id;
+      $child = new NewSoldier($parent, $input['firstName'], $input['lastName'], $input['dob'], '', 836, 7677, '', '');
+      $child->setSchoolType( 8 ); // unaffiliated child
+      if (
+          $child->create(false)
+      ) {
+          $user_id = $child->getUserID();
+          // create private rank for child and update start date in users table
+          $sql1 = "update users set user_start_date = " . unixtojd() . " where user_start_date is null and user_id = " . $user_id;
+          $sql2 = "insert ignore into rank_marks set rank_ord = 1, user_id = " . $user_id . ", date_promoted = " . unixtojd();
+          $res1 = mysql_query($sql1);
+          $res2 = mysql_query($sql2);
+          if (!$res1 || !$res2) {
+            throw new Exception('Failed to create rank marks');
+          }
+          if ($input['referral']) {
+            $p = new Pesukim($user_id);
+            $addedRecruiter = $p->addRecruiter($input['referral']);
+            if (!$addedRecruiter) {
+              throw new Exception('Failed to add recruiter');
+            }
+          }
+          sendEmailConfirmation($input['parentEmail'], ($input['firstName'] . ' ' . $input['lastName']));
+      } else {
+          throw new Exception('Failed to create child account');
+      }
+  } else {
+    throw new Exception('Failed to create parent account');
+  }
+} catch (Exception $e) {
+  $MASHPIA_DB->rollBack();
+  http_response_code(500);
+  echo json_encode([
+    'ok' => false,
+    'error' => ['code' => 'SERVER_ERROR', 'message' => 'Internal server error: ' . $e->getMessage()]
+  ]);
+  exit;
+}
+
+$MASHPIA_DB->commit();
 http_response_code(201);
 echo json_encode([
-  'ok' => true,
-  'userId' => 'usr_' . rand(100000, 999999),
-  'next' => ['checkEmail' => true]
+  'ok' => true
 ]);
