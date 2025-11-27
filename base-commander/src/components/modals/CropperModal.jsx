a29dc0895a0f28d6b27e7823bae5dc45c9e656b9
Local Repository <repository@local.dev>
2025-11-27 17:48:41 -0500

    refactor: replace React.Fragment with shorthand syntax in CropperModal for improved readability

--- base-commander/src/components/modals/CropperModal.jsx
+++ base-commander/src/components/modals/CropperModal.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { DEFAULT_PROFILE, DEFAULT_LOGO, DEFAULT_PRIZE } from 'components/constants';
 // components
 import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap';
@@ -119,7 +119,7 @@ class CropperModal extends Component {
               Change Image
             </Button>
             <Button color="primary" onClick={ this.uploadImage } disabled={ uploading }>
-              { uploading ? <><FontAwesome icon='spinner' spin /> Uploading...</> : 'Save / Replace' }
+              { uploading ? <Fragment><FontAwesome icon='spinner' spin /> Uploading...</Fragment> : 'Save / Replace' }
             </Button>
           </ModalFooter>
         }
