a49ccfc01b355bdd542e8a444c7ec722d4c57c87
Naftoli Rapoport <naftolir@gmail.com>
2025-12-25 16:00:14 -0500

    refactor: update data iteration in new_report.php to improve clarity and maintainability by renaming loop variables

--- mashpia.com/public/name_plates/new_report.php
+++ mashpia.com/public/name_plates/new_report.php
@@ -194,23 +194,21 @@ $reasons = [
             ).then(data => {
                 console.log(data);
                 let html = '';
-                data.forEach(school_items => {
-                    Object.keys(school_items).forEach(grade_items => {
-                        Object.keys(grade_items).forEach(sub_items => {
-                            Object.keys(sub_items).forEach(children => {
-                                children.forEach(child => {
-                                    const grade = child.class_grade + (child.class_sub ? '-' + child.class_sub : '');
-                                    const name = child.first + ' ' + child.last;
-                                    const name_he = child.first_he + ' ' + child.last_he;
-                                    html += `<tr>
-                                        <td>${child.school_name}</td>
-                                        <td>${grade}</td>
-                                        <td>${child.user_serial}</td>
-                                        <td>${name}</td>
-                                        <td>${name_he}</td>
-                                        <td>Future Promotion (General / 3* General)</td>
-                                    </tr>`;
-                                });
+                data.forEach(school => {
+                    Object.keys(school).forEach(grade => {
+                        Object.keys(grade).forEach(sub => {
+                            Object.keys(sub).forEach(child => {
+                                const grade = child.class_grade + (child.class_sub ? '-' + child.class_sub : '');
+                                const name = child.first + ' ' + child.last;
+                                const name_he = child.first_he + ' ' + child.last_he;
+                                html += `<tr>
+                                    <td>${child.school_name}</td>
+                                    <td>${grade}</td>
+                                    <td>${child.user_serial}</td>
+                                    <td>${name}</td>
+                                    <td>${name_he}</td>
+                                    <td>Future Promotion (General / 3* General)</td>
+                                </tr>`;
                             });
                         });
                     });
