d54ffbf601bae0986a0bddb43d91a4cf763c2077
Naftoli Rapoport <naftolir@gmail.com>
2026-01-13 11:19:30 -0500

    fix: remove SMTPSecure setting in sendToOhel.php to streamline email configuration

--- mashpia.com/public/api/print/sendToOhel.php
+++ mashpia.com/public/api/print/sendToOhel.php
@@ -16,7 +16,6 @@ function emailToOhel($fileName) {
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.mashpia.com';                     //Set the SMTP server to send through
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->setFrom('admin@mashpia.com', 'Mashpia');
         // $mail->addAddress('ohel@ohelchabad.org');
         $mail->addAddress('naftoli@tzivoshashem.org');
