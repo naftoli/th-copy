3631687b37e3509579c82ec7d84d3c1cfa80c659
Naftoli Rapoport <naftolir@gmail.com>
2026-01-13 11:57:12 -0500

    fix: reintroduce SMTP authentication details in sendToOhel.php for email functionality

--- mashpia.com/public/api/print/sendToOhel.php
+++ mashpia.com/public/api/print/sendToOhel.php
@@ -17,6 +17,9 @@ function emailToOhel($fileName) {
         $mail->Host = 'smtp.mashpia.com';
         $mail->Port = 465;
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->SMTPAuth = true;
+        $mail->Username = '_mainaccount@mashpia.com';
+        $mail->Password = 'Naftoli@5783!';
         $mail->setFrom('admin@mashpia.com', 'Mashpia');
         // $mail->addAddress('ohel@ohelchabad.org');
         $mail->addAddress('naftoli@tzivoshashem.org');
