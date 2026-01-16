99b312f03260e34952cdf84d8612e748ec3e3252
Local Repository <repository@local.dev>
2026-01-16 15:02:14 -0500

    fix: update start date in SQL query for accurate task retrieval

--- mashpia.com/public/fixes/fix_tanya_lines_learned.php
+++ mashpia.com/public/fixes/fix_tanya_lines_learned.php
@@ -27,7 +27,7 @@ $sql = "select dtmm.*, dt.short_name from date_tasks_marks dtmm
         join date_tasks dt using (date_task_id) 
         join date_tasks_missions dtm using (date_tasks_mission_id) 
         where dt.grid_id in (21001,21002,21003,21004,21005,21006,21007,21008,21013,21014) 
-        and dtm.start_date >= 2459027 
+        and dtm.start_date >= 2460846  
         limit $limit, $end";
 $result = mysql_query($sql);
 while ($row = mysql_fetch_assoc($result)) {
