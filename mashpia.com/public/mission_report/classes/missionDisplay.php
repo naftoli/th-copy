c19db65a3eca63b16f1c75dd6f021555f56c6772
Local Repository <repository@local.dev>
2026-01-19 15:41:53 -0500

    fix: restore conditional logic for report missions in missionDisplay class to improve code clarity and ensure proper task processing

--- mashpia.com/public/mission_report/classes/missionDisplay.php
+++ mashpia.com/public/mission_report/classes/missionDisplay.php
@@ -1121,7 +1121,7 @@ abstract class MissionDisplay {
 										</div>
 									</div>
 								<?php
-							// }
+							}
 							$report_mission = false;
 							// update streak details for later use
 							$activeStreaks[$task->streak_id]['subject_id'] = $track->subject_id;
