517ddb033c38122345fa15e39244e61b71d42478
Local Repository <repository@local.dev>
2026-01-19 15:09:15 -0500

    fix: restore HTML rendering for report missions in missionDisplay class to ensure proper display

--- mashpia.com/public/mission_report/classes/missionDisplay.php
+++ mashpia.com/public/mission_report/classes/missionDisplay.php
@@ -1079,26 +1079,26 @@ abstract class MissionDisplay {
 							} 
 							// report missions should be a separate entry
 							$report_mission_html = '';
-// 							if (trim($task->short_name) == 'Report Missions') {
-// 								$report_mission_html = <<<HTML
-// 								<div class='campaign-container'>
-// 									<div class='campaign-icon'>
-// 										<img src='/mission_report/campaignLogos/{$this->campaignLogos[$track->subject_id]}' width='50' height='52' alt='{$track->subject_name}' />
-// 									</div>
-// 									<div class='campaign-items'>
-// 										<div class='campaign-name'>{$track->subject_name}</div>
-// 									</div>
-// 								</div>
-// 								<div class='task-container'>
-// 									<div class='task-stats'><b>{$accomplished_count}</b> / <b>{$total_days}</b>
-// 									{$task_type_name}</div>
-// 									<div class='task'>
-// 										<div class='task-short-name'>{$task->short_name}</div>
-// 										<div class='task-name'>{$task->task_name}</div>
-// 									</div>
-// 								</div>
-// HTML;
-// 							} else {
+							if (trim($task->short_name) == 'Report Missions') {
+								$report_mission_html = <<<HTML
+								<div class='campaign-container'>
+									<div class='campaign-icon'>
+										<img src="/mission_report/campaignLogos/{$this->campaignLogos[$track->subject_id]}" width='50' height='52' alt="$track->subject_name" />
+									</div>
+									<div class='campaign-items'>
+										<div class='campaign-name'>$track->subject_name</div>
+									</div>
+								</div>
+								<div class='task-container'>
+									<div class='task-stats'><b>$accomplished_count</b> / <b>$total_days</b>
+									$task_type_name</div>
+									<div class='task'>
+										<div class='task-short-name'>$task->short_name</div>
+										<div class='task-name'>$task->task_name</div>
+									</div>
+								</div>
+HTML;
+							} else {
 								?>
 								<div class='task-container'>
 									<div class='task-stats'><b><?=$accomplished_count?></b> 
@@ -1117,7 +1117,7 @@ abstract class MissionDisplay {
 										</div>
 									</div>
 								<?php
-							// }
+							}
 							echo $report_mission_html;
 							// update streak details for later use
 							$activeStreaks[$task->streak_id]['subject_id'] = $track->subject_id;
