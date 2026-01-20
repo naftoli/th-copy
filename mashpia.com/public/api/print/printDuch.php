ad7ec3ad82f85f84f96d401137262c3ae04dc9ef
Naftoli Rapoport <naftolir@gmail.com>
2026-01-20 15:15:24 -0500

    debug: ensure usersTmp output is displayed after filtering for registered users in printDuch.php to aid in user retrieval verification

--- mashpia.com/public/api/print/printDuch.php
+++ mashpia.com/public/api/print/printDuch.php
@@ -52,8 +52,8 @@ if ( !$user_ids ) {
     // do each class separately so that we can order the children alphabetically
     foreach ( $class_ids as $class_id ) {
         $usersTmp = \Soldier::find_all_by_class_id( [ $class_id ] );
-        $usersTmp = array_filter($usersTmp, function ($u) { return $u->user_registered; });
         echo "<pre>"; print_r($usersTmp); echo "</pre>"; exit;
+        $usersTmp = array_filter($usersTmp, function ($u) { return $u->user_registered; });
         // order users alphabetically
         usort( $usersTmp, function( $a, $b ) {
             return $a->last > $b->last;
