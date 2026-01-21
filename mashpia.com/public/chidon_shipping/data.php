5364d3ffa03c8d67af6b019d4ad6e3c640f36859
Naftoli Rapoport <naftolir@gmail.com>
2026-01-21 12:23:12 -0500

    fix: restore shipping status logic in createCSV function and enable shipping_paid retrieval in report for accurate shipping handling

--- mashpia.com/public/chidon_shipping/data.php
+++ mashpia.com/public/chidon_shipping/data.php
@@ -251,8 +251,8 @@ function createCSV($items, $year, $school_id, $shipTo = 'all') {
         if (! isset($admins[$admin_id])) $admins[$admin_id] = $row;
         $children[$user_id] = $admin_id;
         $users[$user_id] = $row;
-        // $shipping_status[$user_id] = in_array($admin_id, $shipping_paid) ? 'ship' : 'pickup';
-        $shipping_status[$user_id] = 'ship';
+        $shipping_status[$user_id] = in_array($admin_id, $shipping_paid) ? 'ship' : 'pickup';
+        // $shipping_status[$user_id] = 'ship';
     }
 
     $info = [];
