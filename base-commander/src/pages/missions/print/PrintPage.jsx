f3283babfc20e4e4a68a1a6b8663c8cf6b92cc0d
Naftoli Rapoport <naftolir@gmail.com>
2026-01-21 12:37:02 -0500

    fix: update legacyUrl in PrintPage for school_id 9 to point to the correct API endpoint

--- base-commander/src/pages/missions/print/PrintPage.jsx
+++ base-commander/src/pages/missions/print/PrintPage.jsx
@@ -82,7 +82,7 @@ class PrintPage extends Component {
 
     let legacyUrl = '';
     if (school_id === 9 && LEGACY_URL === '' && isBC(login.code)) {
-      legacyUrl = '//ssh.mashpia.com/api/print/missions';
+      legacyUrl = '//duch.mashpia.com/api/print/missions';
     } else {
       legacyUrl = `${LEGACY_URL}/api/print/missions`;
     }
