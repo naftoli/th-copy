57a60f06658ea4483b53c5957501e9ebb158a5d0
Naftoli Rapoport <naftolir@gmail.com>
2026-01-26 22:08:03 -0500

    debug: enable error display and reporting for shipping payment check script

--- mashpia.com/public/chidonOld/chidon_drive/site/enrollment/checkShippingPaid.php
+++ mashpia.com/public/chidonOld/chidon_drive/site/enrollment/checkShippingPaid.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL);
+
 require_once $_SERVER['DOCUMENT_ROOT'] . '/api/header/db.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/class.globalSettings.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/chidonOld/chidon_drive/encrypt.php';
