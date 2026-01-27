7524eac8a7aca91250388c5d6ceae4000f5eed22
Naftoli Rapoport <naftolir@gmail.com>
2026-01-26 21:42:29 -0500

    fix: comment out invitation button in parent detail and update SQL query to include chidon photo

--- mashpia.com/public/mobile/reg/ajax/getUser.php
+++ mashpia.com/public/mobile/reg/ajax/getUser.php
@@ -9,9 +9,11 @@ $admin_id = mysql_real_escape_string( $_COOKIE['admin'] );
 $admin_id = encrypt_decrypt('decrypt', $admin_id);
 
 $sql = "SELECT 
-            u.*, a.admin_country, a.admin_postal 
+            u.*, tc.chidon_photo, a.admin_country, a.admin_postal 
         FROM
             users u 
+                LEFT JOIN
+            th_chidon tc ON tc.user_id = u.user_id 
                 LEFT JOIN
             thumbs t ON t.file_id = u.user_photo_id 
                 LEFT JOIN 
@@ -19,7 +21,7 @@ $sql = "SELECT
                 LEFT JOIN
             admins a USING (admin_id) 
         WHERE
-            u.user_id = " . $user . " AND aa.admin_id = " . $admin_id;
+            u.user_id = " . $user . " AND aa.admin_id = " . $admin_id . " AND tc.year = " . $year;
 // echo $sql;
 $result = mysql_query( $sql );
 if ( mysql_num_rows($result) > 0 ) {
