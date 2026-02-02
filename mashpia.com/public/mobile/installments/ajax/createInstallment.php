2d4616e230f6f885a354e91515fd06dd77e93ed5
Naftoli Rapoport <naftolir@gmail.com>
2026-02-02 17:20:19 -0500

    fix: update installment plan email message to clarify separate registration requirement for chidon experience

--- mashpia.com/public/mobile/installments/ajax/createInstallment.php
+++ mashpia.com/public/mobile/installments/ajax/createInstallment.php
@@ -238,7 +238,7 @@ try {
     $from = 'chidon';
     $to = $admin_model->admin_email;
     $subject = 'Installment Plan Created';
-    $message = 'Your installment plan for $' . $amount . ' starting on ' . $start_date . ' has been created successfully.<br /><br /> Your subscription ID is: ' . $subscription_id . '<br /><br /> Thank you.';
+    $message = 'Your installment plan for $' . $amount . ' starting on ' . $start_date . ' has been created successfully.<br /><br /> Your subscription ID is: ' . $subscription_id . '<br /><br />Your still need to register your child(ren) for the chidon experience separately. Thank you.';
     $error = sendEmail($from, $to, $subject, $message);
     if ($error) {
         error_log('Error sending email: ' . $error);
