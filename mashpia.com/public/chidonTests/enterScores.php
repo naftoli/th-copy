1d1ea7adfd45448b7528c20f529b0bdc863845cc
Naftoli Rapoport <naftolir@gmail.com>
2026-02-02 20:03:57 -0500

    fix: refine score entry validation in enterScores.php to ensure only authorized users can access locked schools

--- mashpia.com/public/chidonTests/enterScores.php
+++ mashpia.com/public/chidonTests/enterScores.php
@@ -189,10 +189,13 @@ if ($admin_user['auth'] == 'super') {
         foreach ($children as $child) {
             // check locked
             if (
-              $admin_user['auth'] != 'super' && 
+              !$disabled &&
+              !in_array($school, $exceptions[$testNumber]) &&  
               in_array($school, $locked) && 
-              !in_array($school, $exceptions[$testNumber])
-            ) $disabled = true;
+              $admin_user['auth'] != 'super'
+            ) {
+              $disabled = true;
+            }
             $grade = $child['class_grade'] . (empty($child['class_sub']) ? '' : '-' . $child['class_sub']);
             $name = $child['first'] . ' ' . $child['last'];
             $id = $child['th_chidon_id'];
