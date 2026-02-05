32ea2bd24acf3c146691b238f585de8886d292ef
Naftoli Rapoport <naftolir@gmail.com>
2026-02-05 17:54:24 -0500

    fix: correct item key generation in data.php to use 'name' instead of 'he_name' for improved summary accuracy

--- mashpia.com/public/chidon_shipping/data.php
+++ mashpia.com/public/chidon_shipping/data.php
@@ -168,7 +168,7 @@ function addToSummary($item, $school, $status) {
     global $summary, $summary_items, $grand_summary;
 
     $key = $item['id'];
-    if (! empty(trim($item['he_name']))) $key .= 'P';
+    if (! empty(trim($item['name']))) $key .= 'P';
     $qty = isset($item['qty']) ? intval($item['qty']) : 1;
     if (! empty($status['description'])) $key .= '*';
 
