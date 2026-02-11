2e19feaeff69acb3cc6106ccf0e64bd50e0fd167
Naftoli Rapoport <naftolir@gmail.com>
2026-02-11 15:03:59 -0500

    refactor: add required class imports for task exceptions and defaults in user_track

--- mashpia.com/public/classes/user_track.php
+++ mashpia.com/public/classes/user_track.php
@@ -1,4 +1,7 @@
 <?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/class.taskExceptions.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/class.defaults.php';
+
 class user_track 
 {
 	private static $birthday_cache = [];  // user_id => [date_tasks_mission_id, ...]
