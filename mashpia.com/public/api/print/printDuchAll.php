4850563fe2403d96956d6d102a5b1f6120fcba63
Naftoli Rapoport <naftolir@gmail.com>
2026-02-12 16:42:42 -0500

    fix: update DUCH_TABS_USER_THRESHOLD to 360 for accurate user batch handling in duch report

--- mashpia.com/public/api/print/printDuchAll.php
+++ mashpia.com/public/api/print/printDuchAll.php
@@ -46,7 +46,7 @@ $num_days = $end - $start + 1;
 $days_flag = ceil( $num_days / 30 ); // number of times to multiply the number of users by to get the total number of users needed for the duch
 
 // When check_tabs=1, return JSON: useTabs (true if over threshold) and batches (user_ids per batch of DUCH_TABS_USER_THRESHOLD) so duch.php can open one page per batch.
-define( 'DUCH_TABS_USER_THRESHOLD', 375 );
+define( 'DUCH_TABS_USER_THRESHOLD', 360 );
 define( 'DUCH_GRADE_ORDER', [ 'Pre1a', '1', '2', '3', '4', '5', '6', '7', '8' ] );
 if ( ! empty( $_POST['check_tabs'] ) ) {
     header( 'Content-Type: application/json; charset=utf-8' );
