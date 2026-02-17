c1b1df08c71b44d580b8f58c83106ae204936281
Naftoli Rapoport <naftolir@gmail.com>
2026-02-17 14:03:54 -0500

    refactor: comment out debug SQL output in user_track class for improved code cleanliness

--- mashpia.com/public/classes/user_track.php
+++ mashpia.com/public/classes/user_track.php
@@ -152,7 +152,7 @@ class user_track
 			if ( ! $print_parent_tasks ) $sql .= " AND created_by_parent IS NULL";
 			if ( $this->subject_id == 21 && ! $chidonLimmud ) $sql .= " AND mission_name NOT LIKE '%Chidon Limmud%' ";
 			$sql .= " ORDER BY created_by_parent IS NULL DESC, mission_number, start_date, mission_name";
-			if ($this->subject_id == 41) echo $sql . "<br />";
+			// if ($this->subject_id == 41) echo $sql . "<br />";
 			$query = mysql_query( $sql );
 			while ( $row = mysql_fetch_assoc( $query ) ) {
 				$rows[] = $row;
