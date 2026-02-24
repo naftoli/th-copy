f3456f56402cebe4fbb7744de80abd3592b5dc6d
Local Repository <repository@local.dev>
2026-02-24 15:45:06 -0500

    chore: increase maximum execution time in chidon_pics_iyun.php and chidon_pics_khk.php
    - Set max execution time to 600 seconds to support larger image processing tasks.
    - Enhance error reporting settings for improved debugging across both report files.

--- mashpia.com/public/chidonOld/reports/chidon_pics_khk.php
+++ mashpia.com/public/chidonOld/reports/chidon_pics_khk.php
@@ -1,6 +1,7 @@
 <?php
 ini_set('display_errors',1);
 ini_set('error_reporting', E_ALL);
+ini_set('max_execution_time', 600);
 
 $admin_auth = ['school'];
 require_once $_SERVER['DOCUMENT_ROOT'] . '/header.php';
