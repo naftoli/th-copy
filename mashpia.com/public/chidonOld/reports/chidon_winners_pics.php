b7e0d585b8d3cc2c4d254886983569bc581a153c
Local Repository <repository@local.dev>
2026-03-04 21:21:33 -0500

    refactor: update Chidon winners pictures display
    - Changed the title from "Chidon Pictures" to "Chidon Winners Pictures" for clarity.
    - Refactored the HTML structure for displaying Chidon winners' pictures, improving readability and organization of the code.
    - Added a new join in the SQL query to include additional data from the th_chidon table, enhancing the information displayed.

--- mashpia.com/public/chidonOld/reports/chidon_winners_pics.php
+++ mashpia.com/public/chidonOld/reports/chidon_winners_pics.php
@@ -16,7 +16,8 @@ $limit = isset($_GET['limit']) ? $_GET['limit'] : 0;
 
 $info = [];
 $sql = "select * from th_chidon_winners tcw 
-        join users u on u.user_serial = tcw.serial  
+        join users u on u.user_serial = tcw.serial 
+        join th_chidon tc on tc.user_id = u.user_id
         where year = " . $year . " 
         order by tcw.th_chidon_winner_id";
 $result = mysql_query($sql);
@@ -56,45 +57,41 @@ while ($row = mysql_fetch_assoc($result)) {
 
 <BODY>
 <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/admin_header.php'; ?>
-<h1>Chidon Pictures</h1>
-<?php foreach ($info as $child) : ?>
-  <h2><?= $child['school'] ?></h2>
-  <table class="pics">
-    <tr>
-      <th>Serial Number</th>
-      <th>Name</th>
-      <th>School</th>
-      <th>Grade</th>
-      <th>Chidon Picture</th>
-    </tr>
-      <?php
-      foreach ($children as $child) {
-          $grade = $child['class_grade'] . ($child['class_sub'] ? '-' . $child['class_sub'] : '');
-          $img_fallbacks = [
-              ['from_db' => false, 'val' => $child['khk_photo'],    'url' => 'https://mashpia.com/mobile/reg/' . custom_urlencode($child['khk_photo'])],
-              ['from_db' => false, 'val' => $child['chidon_photo'],    'url' => 'https://mashpia.com/mobile/reg/' . custom_urlencode($child['chidon_photo'])],
-              ['from_db' => false, 'val' => $child['mobile_pic'],    'url' => 'https://mashpia.com/mobile/reg/' . custom_urlencode($child['mobile_pic'])],
-              ['from_db' => false, 'val' => $child['chidon_pic_5782'],    'url' => 'https://mashpia.com/mobile/reg/' . custom_urlencode($child['chidon_pic_5782'])],
-              ['from_db' => false, 'val' => $child['chidon_pic_5781'],    'url' => 'https://mashpia.com/mobile/reg/' . custom_urlencode($child['chidon_pic_5781'])],
-              ['from_db' => true,  'val' => $child['user_photo_id']]
-          ];
-          $img = null;
-          // find first valid image
-          foreach ($img_fallbacks as $img_fallback) {
-              if (!empty($img_fallback['val']) && $img_fallback['val'] !== 'img/addphoto.png') {
-                  $img = $img_fallback['url'];
-                  break;
-              }
-          }
-          echo "<tr><td>" . $child['serial'] . "</td><td>" . $child['name'] . "</td><td>";
-          echo $child['school'] . "</td><td>" . $child['grade'] . "</td><td>";
-          echo "<img src='" . $img . "' /></td></tr>";
-          if ($img != 'http://mashpia.com/mobile/reg/img/addphoto.png') {
-              $imgs[] = $img;
-          }
-      }
-      ?>
-  </table>
-<?php endforeach; ?>
+<h1>Chidon Winners Pictures</h1>
+<table class="pics">
+<tr>
+    <th>Serial Number</th>
+    <th>Name</th>
+    <th>School</th>
+    <th>Grade</th>
+    <th>Chidon Picture</th>
+</tr>
+    <?php 
+    foreach ($info as $child) {
+        $img_fallbacks = [
+            ['from_db' => false, 'val' => $child['khk_photo'],    'url' => 'https://mashpia.com/mobile/reg/' . custom_urlencode($child['khk_photo'])],
+            ['from_db' => false, 'val' => $child['chidon_photo'],    'url' => 'https://mashpia.com/mobile/reg/' . custom_urlencode($child['chidon_photo'])],
+            ['from_db' => false, 'val' => $child['mobile_pic'],    'url' => 'https://mashpia.com/mobile/reg/' . custom_urlencode($child['mobile_pic'])],
+            ['from_db' => false, 'val' => $child['chidon_pic_5782'],    'url' => 'https://mashpia.com/mobile/reg/' . custom_urlencode($child['chidon_pic_5782'])],
+            ['from_db' => false, 'val' => $child['chidon_pic_5781'],    'url' => 'https://mashpia.com/mobile/reg/' . custom_urlencode($child['chidon_pic_5781'])],
+            ['from_db' => true,  'val' => $child['user_photo_id']]
+        ];
+        $img = null;
+        // find first valid image
+        foreach ($img_fallbacks as $img_fallback) {
+            if (!empty($img_fallback['val']) && $img_fallback['val'] !== 'img/addphoto.png') {
+                $img = $img_fallback['url'];
+                break;
+            }
+        }
+        echo "<tr><td>" . $child['serial'] . "</td><td>" . $child['name'] . "</td><td>";
+        echo $child['school'] . "</td><td>" . $child['grade'] . "</td><td>";
+        echo "<img src='" . $img . "' /></td></tr>";
+        if ($img != 'http://mashpia.com/mobile/reg/img/addphoto.png') {
+            $imgs[] = $img;
+        }
+    }
+    ?>
+</table>
 </BODY>
 </HTML>
