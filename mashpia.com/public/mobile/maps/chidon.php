081ad154a9c2dc16989dedaf156586b3fc82d9c5
Local Repository <repository@local.dev>
2026-03-10 14:11:18 -0400

    fix: exclude specific address from geocoding in chidon.php
    - Added a condition to skip geocoding for addresses that match 'Brooklyn, NY', improving the accuracy of location data processed in the map functionality.

--- mashpia.com/public/mobile/maps/chidon.php
+++ mashpia.com/public/mobile/maps/chidon.php
@@ -122,6 +122,7 @@ foreach ($rows as $row) {
 
         const addresses = <?= json_encode($addresses); ?>;
         for (const address of addresses) {
+            if (address.address.trim() == 'Brooklyn, NY') continue;
             geocode({ address: address.address }, address);
         }
     }
