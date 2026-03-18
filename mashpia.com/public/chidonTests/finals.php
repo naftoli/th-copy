b399716e33f8c612ce92fc281fdabf76594c86d0
Local Repository <repository@local.dev>
2026-03-18 11:59:07 -0400

    fix: remove unnecessary parameter from getHighestTrack function
    - Updated the getHighestTrack function call in finals.php to remove an unused parameter, improving code clarity and maintainability.

--- mashpia.com/public/chidonTests/finals.php
+++ mashpia.com/public/chidonTests/finals.php
@@ -111,7 +111,7 @@ function getTrack($child) {
     $scores = $ct->getScores();
     $ct->calculateMarks();
     $marks = $ct->getMarks();
-    $highest_track = $ct->getHighestTrack($marks[$child['th_chidon_id']], $child['user_id'], false, 3, false, false, $child['highest_track']);
+    $highest_track = $ct->getHighestTrack($marks[$child['th_chidon_id']], $child['user_id'], false, 3, false, false);
     // check if child passed Iyun through cumulative marks
     $cumulative = $ct->calculateCumulative($child, $scores[$child['th_chidon_id']]);
     if ($cumulative == 'iyun') $highest_track = 'genius';
