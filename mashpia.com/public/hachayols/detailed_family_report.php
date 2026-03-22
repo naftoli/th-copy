2446358d1b535b208a02510be6c1ea6343bb54f8
Naftoli Rapoport <naftolir@gmail.com>
2026-03-22 14:06:31 -0400

    fix: update year retrieval in detailed family report and report files
    - Changed the method for retrieving the year in detailed_family_report.php and report.php to use the current year settings, ensuring consistent and accurate year data across the application.

--- mashpia.com/public/hachayols/detailed_family_report.php
+++ mashpia.com/public/hachayols/detailed_family_report.php
@@ -6,7 +6,7 @@ $admin_auth = ['school'];
 require_once $_SERVER['DOCUMENT_ROOT'] . '/header.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/api/header/db.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/class.globalSettings.php';
-$year = GlobalSettings::getRegistrationYear();
+$year = GlobalSettings::getCurrentYear();
 
 $super = $admin_user['auth'] == 'super';
 
