4453b99e53f203d05d203adb3af008057a5c90e4
Local Repository <repository@local.dev>
2026-04-14 13:59:33 -0400

    fix: enhance highest track assignment logic in setReps.php
    - Refined the logic to check if the highest track exists in the keys of the types array before converting it to lowercase, ensuring accurate assignment and improving overall data integrity.

--- mashpia.com/public/chidonTests/setReps.php
+++ mashpia.com/public/chidonTests/setReps.php
@@ -40,7 +40,7 @@ foreach ($info as $school => $children) {
     foreach ($children as $child) {
         $id = $child['th_chidon_id'];
         $ht = $ct->getHighestTrack($marks[$id], $child['user_id']);
-        $child['highest_track'] = in_array($ht, $types) ? strtolower($types[$ht]) : $ht;
+        $child['highest_track'] = in_array($ht, array_keys($types)) ? strtolower($types[$ht]) : $ht;
         $grade = $child['class_grade'];
         $avg = 0;
         $num = 0; // variable to know how to decide avg
